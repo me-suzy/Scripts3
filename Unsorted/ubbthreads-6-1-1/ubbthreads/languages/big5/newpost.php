e98a1cf649f04b04ade3094e28ce12633d83cfea
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:15 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/newpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/newpost.php
@@ -0,0 +1,17 @@
+<?
+$ubbt_lang['CHOOSE_NAME']  = 	"¤¹³\\°Î¦W±i¶K¡A¦]¦¹±z¥i¥H¿ï¾Ü¥ô¦ó¤@­Ó¥¼µù¥Uªº¦WºÙ¨Ó±i¶K¤å³¹¡C";
+$ubbt_lang['READ_PERM']  = 	"±z¦b³o°Q½×°Ï¥u¦³¾\\Åª¤å³¹ªºÅv­­";
+$ubbt_lang['MAKENEW_HEAD']  = 	"µoªí¤@½g·s¤å³¹©ó°Q½×°Ï:";
+$ubbt_lang['FILL_FORM']  = 	"½Ðµoªí±zªº¤å³¹¡C";
+$ubbt_lang['NO_HTML']  = 	"©êºp¡A±z¤£¯à¦b³o­Ó°Q½×°Ï¨Ï¥Î HTML »yªk¡C";
+$ubbt_lang['YES_HTML']  = 	"±z¥i¥H¦b³o¸Ì¨Ï¥Î HTML »yªk¡C";
+$ubbt_lang['NO_MARKUP'] =	"©êºp ¡A±z¤£¯à¦b¦¹¨Ï¥Î UBB ¥N½X »yªk¡C";
+$ubbt_lang['YES_MARKUP'] =	"UBB ¥N½X¥\¯à¶}±Ò¡A¦]¦¹¡A±z¥i¥H¦b±zªº¤å³¹¤º¨Ï¥Î";
+$ubbt_lang['MARK_POSTS'] =	"UBB ¥N½X¼Ð°O";
+$ubbt_lang['POST_ICON'] =	"¤ß±¡¹Ï¥Ü";
+$ubbt_lang['POST_TEXT'] =	"½Ð¿é¤J¤º®e¡G";
+$ubbt_lang['SPELL_CHECK'] =	"«÷¦rÀË¬d";
+$ubbt_lang['DO_EMAIL'] =	"§â©Ò¦³¦^À³ªº¤å³¹±H¨ì§Úªº¯u¹ê email ¸Ì¡C";
+$ubbt_lang['DO_PREVIEW'] =	"§Ú­n¹wÄý§Úªº¤å³¹";
+$ubbt_lang['CAN_ATTACH'] =	"¨Ã¥B/©Î¬Oªþ¥[¤@­ÓÀÉ®×";
+?>
