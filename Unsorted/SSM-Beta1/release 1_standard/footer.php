c855b4b2717ce91021524fca7eeac7b87f90f840
me-suzy <me.suzana@gmail.com>
2025-08-14 01:49:18 +0300

    Add Unsorted/SSM-Beta1/release 1_standard/footer.php

---
+++ Unsorted/SSM-Beta1/release 1_standard/footer.php
@@ -0,0 +1,25 @@
+<?php
+//---------------------------------------------------------------------/*
+######################################################################
+# Support Services Manager											 #
+# Copyright 2002 by Shedd Technologies International | sheddtech.com #
+# All rights reserved.												 #
+######################################################################
+# Distribution of this software is strictly prohibited except under  #
+# the terms of the STI License Agreement.  Email info@sheddtech.com  #
+# for information.  												 #
+######################################################################
+# Please visit sheddtech.com for technical support.  We ask that you #
+# read the enclosed documentation thoroughly before requesting 		 #
+# support.															 #
+######################################################################*/
+//---------------------------------------------------------------------
+?>
+<br>
+<div align="center">
+<FONT size=2 color="white"><STRONG><i>powered by</i></STRONG></FONT><br>
+<FONT color="white" size=2>
+<a href="http://scripts.sheddtech.com" style="color: white;">Support Services Manager</a><br>
+&copy; 2001-2002 STI, All Rights Reserved
+</FONT>
+</div>
