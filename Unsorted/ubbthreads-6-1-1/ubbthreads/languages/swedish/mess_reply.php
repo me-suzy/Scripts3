151e2227e12d5b09c3fe444c1bfd0ec3c9a4e3a9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:12 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/mess_reply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/mess_reply.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['EDIT_MORE']  = 	"Ändra lite till";
+$ubbt_lang['REPLY_PREV']  = 	"Förhandsgranska ditt svar till";
+$ubbt_lang['PREVIEW_MESS']  = 	"Här är en förhandsgranskning av ditt meddelande. Tryck på 'Fortsätt' om allt ser bra ut så ditt meddelande skickas iväg.  Om inte så kan du ändra i fältet nedan.";
+$ubbt_lang['MESS_PREVIEW']  = 	"Jag vill förhandsgranska mitt meddelande";
+$ubbt_lang['PRIV_SUB']  = 	"Du har ett nytt meddelande";
+$ubbt_lang['PRIV_BODY_R']  = 	"Du har fått ett privat meddelande på forumet {$config['title']}.  Du kan gå till {$config['phpurl']}/login.php för att läsa det.";
+?>
