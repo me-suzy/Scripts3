be3ff69dce0598e7c563d80c5f963e4e17e46f5d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:24 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/dosearch.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['NO_MORE']   =  "There are no more search results in this direction.";
+$ubbt_lang['NO_WORDS']  = 	"Trebuie introduse cuvinte pentru a initia cautarea.";
+$ubbt_lang['SEARCH_RES']  = 	"Rezultatele cautarii";
+$ubbt_lang['PREV_PAGE']  = 	"Pagina anterioara";
+$ubbt_lang['MAIN_SEARCH']  = 	"Pagina principala de cautare";
+$ubbt_lang['NEXT_PAGE']  = 	"Pagina urmatoare";
+$ubbt_lang['NO_MATCH']  = 	"Nu am gasit nici un rezultat care corespunde cu cererea dvs.";
+?>
