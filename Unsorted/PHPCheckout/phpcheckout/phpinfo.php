3344cd6fee83b35be8e266f2edf5f4fd54faef0f
me-suzy <me.suzana@gmail.com>
2025-08-14 00:33:38 +0300

    Add Unsorted/PHPCheckout/phpcheckout/phpinfo.php

---
+++ Unsorted/PHPCheckout/phpcheckout/phpinfo.php
@@ -0,0 +1,20 @@
+<?php require_once("adminOnly.php");?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+
+      <script language="Javascript" src="phpcheckout.js"></script>
+      <script language="Javascript">loadCSSFile();</script>
+
+<TITLE>phpinfo() -  <?php echo IMPLEMENTATIONNAME;?> - <?php echo BENEFIT;?> -  <?php echo ORGANIZATION;?> - Powered by phpcheckoutTM from DreamRiver http://wwww.dreamriver.com - Software Powers the Net - a DreamRiver Internet Software Product - php software mysql database - download - web database - dreamriver - freeware - sql - file downloader - internet application development - hot scripts - good value - database software - custom code - php e-mail software - ultimate web apps software - phpYellow Pages - EasySQL - phpTellAFriend - mysql php download - Easily create complete internet programs with php, apache, mysql and windows</TITLE>
+
+
+	</HEAD>
+
+<BODY>
+
+<?php
+echo"<h1>" . IMPLEMENTATIONNAME . "</h1>";
+echo "<h2>Results of Function phpinfo()</h2>";
+phpinfo();
+?>
