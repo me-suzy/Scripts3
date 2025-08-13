1fc30c9205963ca856d526fcb0dd6b4a72fb59dd
me-suzy <me.suzana@gmail.com>
2025-08-14 02:24:29 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/admin/doeditinclude.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/admin/doeditinclude.php
@@ -0,0 +1,51 @@
+<?
+/*
+# UBB.threads, Version 6
+# Official Release Date for UBB.threads Version6: 06/05/2002
+
+# First version of UBB.threads created July 30, 1996 (by Rick Baker).
+# This entire program is copyright Infopop Corporation, 2002.
+# For more info on the UBB.threads and other Infopop
+# Products/Services, visit: http://www.infopop.com
+
+# Program Author: Rick Baker.
+
+# You may not distribute this program in any manner, modified or otherwise,
+# without the express, written written consent from Infopop Corporation.
+
+# Note: if you modify ANY code within UBB.threads, we at Infopop Corporation
+# cannot offer you support-- thus modify at your own peril :)
+# ---------------------------------------------------------------------------
+*/
+
+// Require the library
+   require ("../main.inc.php");
+
+// -----------------
+// Get the user info
+   $userob = new user;
+   $user = $userob -> authenticate();
+   $html = new html;
+
+// -----------------------------
+// Make sure they should be here
+   if ($user['U_Status'] != 'Administrator'){
+      $html -> not_right ("You must be logged in, and be a valid administrator to access this.",$Cat);
+   }
+
+// ------------------
+// Write out the file
+   $fd = fopen("{$config['path']}/includes/$include.php","w");
+   fwrite($fd,$body);
+   fclose($fd);
+
+// ------------------------
+// Send them a confirmation
+   $html -> send_header ("The $include has been updated",$Cat,"<META HTTP-EQUIV=\"Refresh\" content=\"5;url={$config['phpurl']}/admin/main.php?Cat=$Cat\">",$user);
+   $html -> admin_table_header("The $include has been updated.");
+   echo "<TABLE BORDER=0 WIDTH=\"95%\" ALIGN=\"center\"><TR><TD class=\"cleartable\"><span class=\"onbody\">";  
+   echo "The $include has been updated";
+   echo "</span></TD></TR></TABLE>";
+   $html -> send_admin_footer();
+
+?>
