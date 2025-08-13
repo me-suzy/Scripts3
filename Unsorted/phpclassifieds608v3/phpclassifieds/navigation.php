0a513390a0887a51350e40e60de0fd07fae7c9e4
me-suzy <me.suzana@gmail.com>
2025-08-14 00:36:06 +0300

    Add Unsorted/phpclassifieds608v3/phpclassifieds/navigation.php

---
+++ Unsorted/phpclassifieds608v3/phpclassifieds/navigation.php
@@ -0,0 +1,318 @@
+<!-- ## NAVIGATION.PHP START ## -->
+
+<!-- Table 1 open -->
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tr>
+	<td width="100%">
+    
+	
+	
+	
+	
+	<!-- Table 2 open -->
+    <table border="0" width="100%" cellspacing="0" cellpadding="0" height="53">
+    <tr>
+		<td valign="top" align="left"><img border="0"  alt="" src="images/logo.gif" /></td>
+        <td valign="top" align="right">
+        
+        <!-- Table x open -->
+        <table border="0" cellspacing="0" cellpadding="0" height="21" background="images/spacerbig.gif">
+		<tr>
+    		<td background="images/topbutton_tile.gif" height="21" valign="top" align="left">
+    	
+    	
+    	
+    	
+		    	<!-- Table 3 open -->
+		      	<table border="0" cellspacing="0" cellpadding="0" background="images/topbutton_left_width.gif" height="21">
+				<tr>
+					<td background="images/topbutton_left_width.gif" height="21" nowrap="nowrap" valign="top" align="center">
+		            
+		
+					
+					
+					
+					
+						<!-- Table 4 open -->
+			          	<table border="0" cellspacing="0" cellpadding="0" height="21">
+			              <tr>
+			                <td height="15" background="images/spacerbig.gif" valign="bottom"><img border="0" alt="" src="images/spacerbig.gif" width="30" height="6" /></td>
+			                <td height="15" background="images/spacerbig.gif" valign="middle">
+			    			<a href="register.php"><img border="0"  alt="" src="images/topbutton_icon_register.gif" hspace="3" /></a></td>
+			                <td height="15" background="images/spacerbig.gif" valign="bottom" nowrap="nowrap"><a href="http://<? echo $url ?>/register.php"><? echo $la_reg ?></a></td>
+			              </tr>
+			              <tr>
+			                <td align="right" background="images/spacerbig.gif"><img border="0"  alt="" src="images/spacerbig.gif" width="1" height="6" /></td>
+			                <td align="right" background="images/spacerbig.gif"><img border="0"  alt="" src="images/spacerbig.gif" width="1" height="6" /></td>
+			                <td align="right" background="images/spacerbig.gif"><img border="0"  alt="" src="images/spacerbig.gif" width="41" height="6" /></td>
+			              </tr>
+			            </table>
+			            <!-- Table 4 closed -->
+		            
+		            
+		            
+					</td>
+				</tr>
+		      	</table>
+		      	<!-- Table 3 closed -->
+      
+      
+            
+		</td>
+    	<td background="images/topbutton_tile.gif" align="center" valign="top" height="21"><img border="0"  alt="" src="images/spacerbig.gif" width="13" height="21" /><img border="0"  alt="" src="images/topbutton_bar.gif" /><img border="0"  alt="" src="images/spacerbig.gif" width="10" height="21" /></td>
+    	<td background="images/topbutton_tile.gif" nowrap="nowrap" height="21" valign="top" align="center">
+        
+    	
+		    		<!-- Table 3 open -->    
+		    		<table border="0" cellspacing="0" cellpadding="0" height="21">
+		            <tr>
+						<td height="15" background="images/spacerbig.gif" valign="bottom"><a href="member_login.php"><img border="0"  alt="" src="images/topbutton_icon_adadd.gif" hspace="3" /></a></td>
+		                <td height="15" background="images/spacerbig.gif" valign="bottom" nowrap="nowrap"><a href="http://<? echo $url ?>//member.php"><? echo $la_add_ad ?></a></td>
+					</tr>
+					<tr>
+		                <td align="right" background="images/spacerbig.gif"><img border="0"  alt="" src="images/spacerbig.gif" width="1" height="6" /></td>
+		                <td align="right" background="images/spacerbig.gif"><img border="0"  alt="" src="images/spacerbig.gif" width="41" height="6" /></td>
+		              </tr>
+					</table>
+		        	<!-- Table 3 closed -->    
+		        	
+        	
+        	
+		</td>
+		
+		
+	    <td background="images/topbutton_tile.gif" align="center" valign="top" height="21"><img border="0"  alt="" src="images/spacerbig.gif" width="13" height="21" /><img border="0"  alt="" src="images/topbutton_bar.gif" /><img border="0"  alt="" src="images/spacerbig.gif" width="10" height="21" /></td>
+	    <td background="images/topbutton_tile.gif" nowrap="nowrap" height="21" valign="top" align="center">
+	    
+	    
+	    
+			    <!-- Table 3 open -->        
+				<table border="0" cellspacing="0" cellpadding="0" height="21">
+		              <tr>
+		                <td height="15" background="images/spacerbig.gif" valign="bottom"><a href="member_login.php"><img border="0"  alt="" src="images/topbutton_icon_memberlogin.gif" hspace="3" /></a></td>
+		                <td height="15" background="images/spacerbig.gif" valign="bottom" nowrap="nowrap"><a href="http://<? echo $url ?>/member_login.php"><? echo $la_login ?></a></td>
+		              </tr>
+		              <tr>
+		                <td align="right" background="images/spacerbig.gif"><img border="0"  alt="" src="images/spacerbig.gif" width="1" height="6" /></td>
+		                <td align="right" background="images/spacerbig.gif"><img border="0"  alt="" src="images/spacerbig.gif" width="41" height="6" /></td>
+		              </tr>
+				</table>
+				<!-- Table 3 closed -->            
+			
+	
+		</td>
+    
+		<td background="images/topbutton_tile.gif" align="center" valign="top" height="21"><img border="0"  alt="" src="images/spacerbig.gif" width="13" height="21" /><img border="0"  alt="" src="images/topbutton_bar.gif" /><img border="0"  alt="" src="images/spacerbig.gif" width="10" height="21" /></td>
+    	<td background="images/topbutton_tile.gif" valign="top" align="right" height="21">
+
+			    <!-- Table 3 open -->
+			 	<table border="0" cellspacing="0" cellpadding="0" background="images/spacerbig.gif" height="21">
+			  	<tr>
+			    	<td background="images/spacerbig.gif" valign="top" align="center">
+			            
+			    	
+			    	
+			    		<!-- Table 4 open -->
+			    		<table border="0" cellspacing="0" cellpadding="0" height="21">
+			              <tr>
+			                <td height="15" background="images/spacerbig.gif" valign="bottom"><a href="latestwap.php"><img border="0"  alt="" src="images/topbutton_icon_wap.gif" hspace="3" /></a></td>
+			                <td height="15" background="images/spacerbig.gif" valign="bottom" nowrap="nowrap" align="left"><a href="http://<? echo $url ?>/latestwap.php"><? echo $la_wap_nav ?></a></td>
+			              </tr>
+			              <tr>
+			                <td align="right" background="images/spacerbig.gif"><img border="0"  alt="" src="images/spacerbig.gif" width="1" height="6" /></td>
+			                <td align="right" background="images/spacerbig.gif" valign="top" height="6"><img border="0"  alt="" src="images/topbutton_right_bottom.gif" /></td>
+			              </tr>
+			            </table>
+			            <!-- Table 4 closed -->
+					
+			            
+			        </td>
+			        
+			        
+				<td background="images/spacerbig.gif"><img border="0"  alt="" src="images/topbutton_right_top.gif" /></td>
+				</tr>
+				</table>
+				<!-- Table 3 closed -->
+				
+    	</td>
+  	</tr>
+	</table>
+		<!-- Table 2 closed -->
+		
+			</td>
+		</tr>
+	</table>
+	<!-- Table x closed -->
+		
+	</td>
+</tr>
+</table>
+<!-- Table 1 closed -->
+
+
+
+<!-- Table 1 open -->
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tr>
+    <td width="100%">
+    
+    
+    			<!-- Table 2 open -->
+      			<table border="0" width="100%" cellspacing="0" cellpadding="0">
+        		<tr>
+          			<td>
+          				
+          			
+          			
+          				<!-- Table 3 open -->
+    					<table border="0" cellspacing="0" cellpadding="0" height="23" background="images/bottombutton_tile.gif">
+  						<tr>
+								<td height="23" valign="top" align="left" bgcolor="#FFFFFF" background="images/whitebg.gif">
+      							<img border="0"  alt="" src="images/spacerbig.gif" width="10" height="10" />
+    							</td>
+    						
+    							<td background="images/bottombutton_tile.gif" height="23" valign="top" align="left">
+    						
+    									<!-- Table 4 open -->
+      									<table border="0" width="105" cellspacing="0" cellpadding="0">
+        								<tr>
+          										<td width="100%" background="images/bottombutton_left_width.gif" valign="top" align="left" height="23">
+          							
+          										
+          										
+          											<!-- Table 5 open -->
+													<table border="0" cellspacing="0" cellpadding="0" height="23" width="100%">
+								              		<tr>
+								                	<td background="images/spacerbig.gif" height="23" width="23"><img border="0"  alt="" src="images/spacerbig.gif" width="30" height="10" />
+													</td>
+								                	<td background="images/spacerbig.gif" height="23" valign="top" align="center">
+								                		
+								                		<!-- Table 6 open -->
+														<table border="0" cellspacing="0" cellpadding="0" height="23">
+  														<tr>
+    														<td height="6"  background="images/spacerbig.gif"><img border="0"  alt="" src="images/spacerbig.gif" width="41" height="6" /></td>
+
+  														</tr>
+  														<tr>
+    
+    														<td height="17" valign="middle" align="center" nowrap="nowrap" background="images/spacerbig.gif">
+															<a href="http://<? echo $url ?>/index.php"><? echo $la_frontpage ?></a></td> 
+ 
+  														</tr>
+														</table>
+														<!-- Table 6 closed -->
+
+													</td>
+              										</tr>
+            										</table>
+            										<!-- Table 5 closed -->
+          									
+            									</td>
+        								</tr>
+      									</table>
+      									<!-- Table 4 closed -->
+    								
+      									
+      									
+      									
+							</td>
+    						<td background="images/bottombutton_tile.gif" height="23" valign="top" align="center"><img border="0"  alt="" src="images/spacerbig.gif" width="13" height="21" /><img border="0"  alt="" src="images/bottombutton_bar.gif" /><img border="0"  alt="" src="images/spacerbig.gif" width="13" height="21" /></td>
+    						<td background="images/bottombutton_tile.gif" height="23" valign="top" align="center">
+    		
+    						
+    							<!-- Table 4 open -->
+								<table border="0" cellspacing="0" cellpadding="0" height="23">
+								<tr>
+							    	<td height="6"  background="images/spacerbig.gif"><img border="0"  alt="" src="images/spacerbig.gif" width="41" height="6" /></td>
+							 	</tr>
+							 	<tr>
+							    	<td height="17" valign="middle" align="center" nowrap="nowrap" background="images/spacerbig.gif"><a href="http://<? echo $url ?>/picturebrowse.php"><? echo $la_browse ?></a></td> 
+								</tr>
+								</table>
+								<!-- Table 4 closed -->
+
+							
+									
+							</td>
+    						<td background="images/bottombutton_tile.gif" height="23" valign="top" align="center"><img border="0"  alt="" src="images/spacerbig.gif" width="13" height="21" /><img border="0"  alt="" src="images/bottombutton_bar.gif" /><img border="0"  alt="" src="images/spacerbig.gif" width="13" height="21" /></td>
+    						<td background="images/bottombutton_tile.gif" height="23" valign="top" align="center">
+
+    						
+    							<!-- Table 4 open -->
+							    <table border="0" cellspacing="0" cellpadding="0" height="23">
+							  	<tr>
+							    	<td height="6" background="images/spacerbig.gif"><img border="0"  alt="" src="images/spacerbig.gif" width="41" height="6" /></td>
+							  	</tr>
+							  	<tr>
+							    	<td height="17" valign="middle" align="center" nowrap="nowrap" background="images/spacerbig.gif"><a href="http://<? echo $url ?>/latestads.php"><? echo $la_latest ?></a></td> 
+							  	</tr>
+								</table>
+								<!-- Table 4 closed -->
+							
+							</td>
+    						
+							<td background="images/bottombutton_tile.gif" height="23" valign="top" align="center"><img border="0"  alt="" src="images/spacerbig.gif" width="13" height="21" /><img border="0"  alt="" src="images/bottombutton_bar.gif" /><img border="0"  alt="" src="images/spacerbig.gif" width="13" height="21" /></td>
+    						<td background="images/bottombutton_tile.gif" height="23" valign="top" align="center">
+    						
+    						
+    							<!-- Table 4 open -->  
+							    <table border="0" cellspacing="0" cellpadding="0" height="23">
+							    <tr>
+							    	<td height="6" background="images/spacerbig.gif"><img border="0"  alt="" src="images/spacerbig.gif" width="41" height="6" /></td>
+							    </tr>
+							    
+							    <tr>
+							        <td height="17" valign="middle" align="center" nowrap="nowrap" background="images/spacerbig.gif"><a href="http://<? echo $url ?>/mostviewed.php"><? echo $la_most ?></a></td>
+							    </tr>
+							   	</table>
+							      <!-- Table 4 closed -->
+    
+							      
+							</td>
+    				
+							<td background="images/bottombutton_tile.gif" height="23" valign="top" align="center"><img border="0"  alt="" src="images/spacerbig.gif" width="13" height="21" /><img border="0"  alt="" src="images/bottombutton_bar.gif" /><img border="0"  alt="" src="images/spacerbig.gif" width="13" height="21" /></td>
+    						<td background="images/bottombutton_tile.gif" height="23" valign="top" align="right">
+								    
+    						
+    							<!-- Table 4 open -->  
+								<table border="0" cellspacing="0" cellpadding="0" height="23">
+								<tr>
+								          <td height="6" align="right" background="images/spacerbig.gif"><img border="0"  alt="" src="images/bottombutton_right_top.gif" /></td>
+								          <td rowspan="2" height="23" width="30"><img border="0"  alt="" src="images/bottombutton_right_bottom.gif" /></td>
+								</tr>
+								<tr>
+								          <td height="17" align="center" valign="middle" nowrap="nowrap" background="images/spacerbig.gif"><a href="http://<? echo $url ?>/search.php"><? echo $la_search ?></a></td>
+								</tr>
+								</table>
+								<!-- Table 4 closed -->
+    				
+							</td>
+  					</tr>
+				</table>
+				<!-- Table 3 closed -->
+
+				
+		</td>
+
+		
+		<td align="right" valign="top">
+		</td>
+	</tr>
+	</table>
+	<!-- Table 2 open -->
+
+	</td>
+</tr>
+</table>
+<!-- Table 1 closed -->
+
+
+<!-- Table 1 open -->
+<table border="0" width="100%" bgcolor="#A9B8D1" cellspacing="0" cellpadding="0">
+  <tr>
+    <td width="100%" height="1"><img border="0"  alt="" src="images/spacerbig.gif" width="1" height="1" /></td>
+  </tr>
+</table>
+<!-- Table 1 closed -->
+
+<!-- ## NAVIGATION.PHP END ## -->
