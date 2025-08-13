bcd601e5f3d458de751990d3f5f0d92fcf959721
me-suzy <me.suzana@gmail.com>
2025-08-14 02:42:09 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/mess_reply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/mess_reply.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['EDIT_MORE']  = 	"Rediger beskjeden";
+$ubbt_lang['REPLY_PREV']  = 	"Forhåndsvisning av din private beskjed til";
+$ubbt_lang['PREVIEW_MESS']  = 	"Dette er en forhåndsvisning av din private beskjed. Hvis alt ser bra ut kan du klikke på 'Fortsett' og beskjeden vil bli sendt. Hvis ikke kan du endre beskjeden i skjemaet lenger ned på siden.";
+$ubbt_lang['MESS_PREVIEW']  = 	"Jeg vil forhåndsvise beskjeden";
+$ubbt_lang['PRIV_SUB']  = 	"Det har kommet nye beskjeder";
+$ubbt_lang['PRIV_BODY_R']  = 	"Du har mottatt en privat beskjed. Beskjeden kan leses ved å gå til {$config['phpurl']}/login.php";
+?>
