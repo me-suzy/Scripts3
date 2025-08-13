d99075683869695c25aa0cadcf2a9154222a1bdd
me-suzy <me.suzana@gmail.com>
2025-08-14 02:31:37 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/sendprivate.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/sendprivate.php
@@ -0,0 +1,13 @@
+<?
+$ubbt_lang['NOPRIVS']   = "Private messages are disabled.";
+$ubbt_lang['MESS_COPY']  = 	"I want to keep a copy of my message";
+$ubbt_lang['MESS_RECEIPT']  = 	"I want to receive a read receipt";
+$ubbt_lang['MESS_PREVIEW']  = 	"I want to preview my message";
+$ubbt_lang['ADD_BOOK']  = 	"Add to address book";
+$ubbt_lang['USE_BOOK']  = 	"or use your address book";
+$ubbt_lang['CHOOSE_ADD']  = 	"Choose a name";
+$ubbt_lang['NO_LOGGED']  = 	"Du kan ikke sende private beskeder, før du har logget ind.";
+$ubbt_lang['NO_PRIVATE']  = 	"Denne bruger ønsker ikke at modtage private beskeder.";
+$ubbt_lang['PRIV_BODY2']  = 	"Udfyld følgende felter for at sende en privat besked til denne bruger. Brugeren vil modtage den næste gang, han/hun logger ind.";
+$ubbt_lang['TEXT_DELETE']  = 	"Slet";
+?>
