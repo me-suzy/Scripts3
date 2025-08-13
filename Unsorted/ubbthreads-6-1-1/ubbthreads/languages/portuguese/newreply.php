90fc3bc5f88833a67897b255ff0058225b0791f8
me-suzy <me.suzana@gmail.com>
2025-08-14 02:45:17 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/newreply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/newreply.php
@@ -0,0 +1,19 @@
+<?
+$ubbt_lang['READ_PERM']  = 	"You have read only privileges for this forum";
+$ubbt_lang['NOT_APP']  = 	"Não é possível responder a uma mensagem não aprovada.";
+$ubbt_lang['REPLY_HEAD']  = 	"Respostas para";
+$ubbt_lang['FILL_FORM']  = 	"Preencha o formulário para colocar uma mensagem no fórum.";
+$ubbt_lang['NO_HTML']  = 	"HTML está desabilitado.";
+$ubbt_lang['YES_HTML']  = 	"HTML está habilitado.";
+$ubbt_lang['NO_MARKUP']  = 	"UBBCode está desabilitado.";
+$ubbt_lang['YES_MARKUP']  = 	"UBBCode está habilitado, pode usar";
+$ubbt_lang['MARK_POSTS']  = 	"UBBCode na sua mensagem";
+$ubbt_lang['CHOOSE_NAME']  = 	"Anonymous postings are allowed, so you may choose any unregistered name to post under.";
+$ubbt_lang['POST_ICON']  = 	"Colocar ícone";
+$ubbt_lang['POST_TEXT']  = 	"Colocar";
+$ubbt_lang['SPELL_CHECK']  = 	"Verificar ortografia";
+$ubbt_lang['DO_EMAIL']  = 	"Enviar todas as respostas ao meu mail";
+$ubbt_lang['DO_PREVIEW']  = 	"I want to preview my post";
+$ubbt_lang['CAN_ATTACH']  = 	"and/or attach a file";
+$ubbt_lang['IN_RESPONSE']  = 	"Respondendo a";
+?>
