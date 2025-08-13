4096034bc0946be38a5b7f96b9f30b3e7e45a3cc
me-suzy <me.suzana@gmail.com>
2025-08-14 01:04:48 +0300

    Add Unsorted/phpYellowPages/phpYellow/dev-mess.php

---
+++ Unsorted/phpYellowPages/phpYellow/dev-mess.php
@@ -0,0 +1,67 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+
+	<link rel=stylesheet type="text/css" href="yellow.css">
+ 
+    <?php require("util.php"); ?>
+    <?php echo "<TITLE>Developers Message - " . YOURPHPYELLOWNAME . " - " . COMPANY . " - A Dreamriver.com Software Product - Easily create classified ads or Yellow Page directories! phpYellow Pages from Dreamriver.com - php software mysql database software download apps</TITLE>"; ?>
+
+	<META NAME="keywords" CONTENT="phpYellow, php, Yellow, hope, visitors, enjoying, Yellow, Pages, software, free, gratis, software, assist, expansion, open, source, movement.">
+	<META NAME="description" CONTENT="I hope visitors are enjoying this Yellow Pages software. I offer it absolutely free of charge, <b>gratis</b>. My hope is that this software will assist in the expansion of the open source software movement.">
+	<META NAME="Author" CONTENT="Richard Creech, Web: http://www.dreamriver.com Email: richardc@dreamriver.com">
+	<META NAME="GENERATOR" CONTENT="Blood, Sweat & Tears">
+	<meta name="robots" content="noindex,nofollow">
+	</HEAD>
+
+<BODY>
+
+<?php 
+include("header.php"); 
+?>
+
+
+<h1>Developer's Message</h1>
+
+<table><tr><td><img src="appimage/51.jpg" width=150 height=221 hspace=10 vspace=2 border=0 alt="Richard Creech"></td>
+<td>
+<h2>Thank You!</h2>
+<p>phpYellow Pages has emerged into the spotlight as the web developer's download of choice 
+for creating Yellow Pages on the web. Independent Hotscripts.com consistently rates phpYellow Pages   
+higher than any other software in the classified ad category. Yep, that means <b>Best in class</b>, WOW!</p>
+
+<p>phpYellow Pages is continually upgraded. Keep sending your comments, suggestions, 
+wishlists and ideas. They have all contributed to its success.
+</p>
+
+<p>Thank you so much for your help!</p>
+
+
+<h2>Internet Application Development</h2>
+
+To create your 
+custom application on the internet please contact:
+<p></p>
+Richard Creech<br>
+<a href="http://dreamriver.com">Dreamriver.com</a><br>
+Phone 250.744.3350 Pacific Time<br>
+Email <a href="mailto:customize@dreamriver.com">customize@dreamriver.com</a>
+
+
+
+<h2>Software Powers the Net<sup>TM</SUP></h2>
+</td></tr></table>
+
+<p></p>
+
+<div align="center" class="x-small">
+<a href="http://dreamriver.com">Dreamriver.com</a> 
+Richard R. Creech 
+<a href="mailto:richardc@dreamriver.com">richardc@dreamriver.com</a>
+</div>
+
+
+
+<?php include("footer.php");?>
+
+
