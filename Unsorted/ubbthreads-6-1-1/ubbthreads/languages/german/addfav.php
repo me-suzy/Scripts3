ef9bcd47d568219b1e3e136d7675cb906582e559
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:39 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/addfav.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/addfav.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  =       "Sie haben keine Berechtigung, diesen Thread zu lesen. Daher k&ouml;nnen Sie ihn auch nicht zu Ihrer Favoriten- oder Erinnerungsliste hinzuf&uuml;gen.";
+$ubbt_lang['MAIN_ONLY']  =     "Sie k&ouml;nnen nur Haupt-Threads zu Ihrer Favoriten- oder Erinnerungsliste hinzuf&uuml;gen.";
+$ubbt_lang['DUP_ENTRY']  =     "Dies wurde bereits zu Ihrer Favoriten- oder Erinnerungsliste hinzuf&uuml;gt.";
+$ubbt_lang['ENTRY_IN']  =      "Nachricht / Thema hinzuf&uuml;gt";
+$ubbt_lang['FAV_CONFIRM']  =   "Die gew&auml;hlte Nachricht oder der Thread wurde zu Ihrer Liste hinzuf&uuml;gt. Wenn sie als Favorit hinzugef&uuml;gt wurden, werden Sie diesen Eintrag solange unter 'Einstellungen' sehen, bis Sie ihn wieder l&ouml;schen. Wurden sie als Erinnerung hinzugef&uuml;gt, verschwindet der Eintrag, sobald Sie auf die Nachricht antworten.";
+$ubbt_lang['FORUM_RETURN']  =  "Zur&uuml;ck zum Forum.";
+?>
