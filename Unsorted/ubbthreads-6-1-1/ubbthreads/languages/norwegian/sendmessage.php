fc66ecad13e86486edf6c7dbb9e798ee0b8d962d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:42:24 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/sendmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/sendmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['CHOOSE_ADD']  = 	"Velg en bruker";
+$ubbt_lang['TEXT_DELETE']  = 	"Slett";
+$ubbt_lang['NO_PRIVATE']  = 	"Denne brukeren ønsker ikke å motta private beskjeder.";
+$ubbt_lang['NO_RECORD']  = 	"Brukeren du vil sende beskjeden til finnes ikke i vårt system.";
+$ubbt_lang['MESS_PREV']  = 	"Forhåndsvis privat beskjed til";
+$ubbt_lang['PREVIEW_MESS']  = 	"Dette er en forhåndsvisning av din private beskjed. Hvis alt ser bra ut kan du klikke på 'Fortsett' og beskjeden vil bli sendt. Ønsker du å redigere beskjeden, kan du gjøre det i skjemaet lenger ned på siden.";
+$ubbt_lang['EDIT_MORE']  = 	"Rediger beskjeden";
+$ubbt_lang['PRIV_SUB']  = 	"Det har kommet nye beskjeder";
+$ubbt_lang['PRIV_BODY_R']  = 	"Du har mottatt en privat beskjed. Beskjeden kan leses ved å gå til {$config['phpurl']}/login.php";
+$ubbt_lang['MESS_SENT']  = 	"Beskjeden er sendt.";
+?>
