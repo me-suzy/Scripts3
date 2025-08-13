648f796f278dff6909c3c5b0c1b7a1d3b3e9e4b8
me-suzy <me.suzana@gmail.com>
2025-08-14 02:25:54 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/admin/purgeusers.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/admin/purgeusers.php
@@ -0,0 +1,55 @@
+<?
+/*
+# UBB.threads, Version 6
+# Official Release Date for UBB.threads Version6: 06/05/2002
+
+# First version of UBB.threads created July 30, 1996 (by Rick Baker).
+# This entire program is copyright Infopop Corporation, 2002.
+# For more info on the UBB.threads and other Infopop
+# Products/Services, visit: http://www.infopop.com
+
+# Program Author: Rick Baker.
+
+# You may not distribute this program in any manner, modified or otherwise,
+# without the express, written written consent from Infopop Corporation.
+
+# Note: if you modify ANY code within UBB.threads, we at Infopop Corporation
+# cannot offer you support-- thus modify at your own peril :)
+# ---------------------------------------------------------------------------
+*/
+
+// Require the library
+   require ("../main.inc.php");
+
+// -----------------
+// Get the user info
+   $userob = new user;
+   $user = $userob -> authenticate();
+   $html = new html;
+
+// ---------------------------------
+// Make sure they are should be here
+   if ($user['U_Status'] != 'Administrator'){
+      $html -> not_right ("You must be logged in, and be a valid administrator to access this.",$Cat);
+   }
+
+
+// ------------------------
+// Send them a page
+   $html -> send_header ("Select inactivity threshold",$Cat,0,$user);
+   $html -> admin_table_header("Select inactivity threshold");
+   $html -> open_admin_table();
+
+   echo "<tr><td class=\"lighttable\">";
+   echo "Please choose a period of inactivity.  Users who have been inactive equal to or longer than the amount of days you specify will be deleted.";
+   echo "<FORM METHOD = POST action =\"{$config['phpurl']}/admin/confirmpurgeusers.php\">";
+   echo "<INPUT TYPE=HIDDEN NAME=Cat VALUE=$Cat>";
+   echo "Inactivity period ";
+   echo "<input type=text name=\"inactivity\" size=3 maxlength=5 class=\"formboxes\">\n";
+
+   echo "<br><br>";
+   echo "<input type=submit value=\"Submit\" class=\"buttons\">";
+   echo "</form>";
+
+   $html -> close_table();
+   $html -> send_admin_footer();
