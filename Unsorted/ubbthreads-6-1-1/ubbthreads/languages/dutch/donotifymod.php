412e21cab144c94e1f209d0d4ed966e2715543d9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:27 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/donotifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/donotifymod.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['POST_PROB']  =  "Er ontstond een fout bij het doorzoeken van de database naar dit bericht.";
+$ubbt_lang['BAD_GROUP']  = 	"Verboden toegang!";
+$ubbt_lang['NO_B_INFO']  = 	"Forum niet gevonden!";
+$ubbt_lang['NOTIF_HEAD']  = 	"Heeft jou een bericht gestuurd dat je volgend onderwerp even moet bekijken";
+$ubbt_lang['TO_READ']  = 	"Dit onderwerp vind je hier:";
+$ubbt_lang['NOTIFY_SENT']  = 	"Moderator(s) gewaarschuwd";
+$ubbt_lang['NOTIFY_CONFIRM']  =  "De moderator(s) van dit forum zijn gewaarschuwd.";
+?>
