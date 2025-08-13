a6c9c249a2cb70cc5e97fbac7578f2869b194ca6
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:11 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =      "Your picture ({$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['PROF_PIC']  =       "Your Picture (Entire URL - Picture size is {$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['AUX_ONLINE']  =      "Sollen andere Benutzer sehen k&ouml;ennen, welchen Beitrag/welches Forum  sie momentan betrachten?";
+$ubbt_lang['PROF_PRIV']  = 	"M&ouml;chten Sie private Nachrichten anderer Benutzer erhalten?";
+$ubbt_lang['NO_PROF']  = 	"F&uuml;r diesen Benutzer existiert kein Benutzer-Profil";
+$ubbt_lang['PROF_HEAD']  = 	"Editieren des Benutzerprofils";
+$ubbt_lang['PROF_EDIT']  = 	"Hier k&ouml;nnen alle pers&ouml;nlichen Einstellungen ge&auml;ndert werden.";
+$ubbt_lang['BASIC_PROF']  = 	"Allgemeines Benutzer-Profil";
+$ubbt_lang['PROF_PASS']  = 	"Passwort (zwischen 4 und 20 Zeichen)";
+$ubbt_lang['VER_PASS']  = 	"Wiederholung des Passwortes";
+$ubbt_lang['EMAIL_ADD']  = 	"Email-Adresse (nicht f&uuml;r andere sichtbar)";
+$ubbt_lang['FAKE_EMAIL']  = 	"Email-Adresse (in Nachrichten angezeigt)";
+$ubbt_lang['FULL_NAME']  = 	"Voller Name";
+$ubbt_lang['PROF_SIG']  = 	"Signatur (bis zu {$config['Sig_length']} Zeichen). Innerhalb der Signatur k&ouml;nnen spezielle Markierungen verwendet werden.";
+$ubbt_lang['PROF_HOME']  = 	"Homepage (bis zu 150 Zeichen)";
+$ubbt_lang['PROF_OCC']  = 	"Besch&auml;ftigung";
+$ubbt_lang['PROF_HOBB']  = 	"Hobbies (bis zu 200 Zeichen)";
+$ubbt_lang['PROF_LOC']  = 	"Geographischer Ort (Staat, Land, etc.)";
+$ubbt_lang['PROF_BIO']  = 	"Biographie (bis zu 250 Zeichen)";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ Nummer";
+$ubbt_lang['PROF_VIS']  = 	"M&ouml;chten Sie auf dem 'Wer ist online?'-Schirm gelistet werden?";
+?>
