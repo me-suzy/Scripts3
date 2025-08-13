a8f4ec760ad4d8002996498b253c776cc98e4eb1
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:12 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/approvepost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/approvepost.php
@@ -0,0 +1,5 @@
+<?
+$ubbt_lang['REPLY_SUB']  = 	"Antwoord op jouw bericht";
+$ubbt_lang['REPLY_BOD']  = 	"heeft jouw bericht beantwoord op: .";
+$ubbt_lang['APPR_HEAD']  = 	"Bericht goedgekeurd.";
+?>
