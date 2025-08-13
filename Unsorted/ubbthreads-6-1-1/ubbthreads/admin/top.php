72dcebaa30cc3f437522d817ff91f620fa906ec8
me-suzy <me.suzana@gmail.com>
2025-08-14 02:26:31 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/admin/top.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/admin/top.php
@@ -0,0 +1,33 @@
+<?
+/*
+# UBB.threads, Version 6
+# Official Release Date for UBB.threads Version6: 06/05/2002
+
+# First version of UBB.threads created July 30, 1996 (by Rick Baker).
+# This entire program is copyright Infopop Corporation, 2002.
+# For more info on the UBB.threads and other Infopop
+# Products/Services, visit: http://www.infopop.com
+
+# Program Author: Rick Baker.
+
+# You may not distribute this program in any manner, modified or otherwise,
+# without the express, written written consent from Infopop Corporation.
+
+# Note: if you modify ANY code within UBB.threads, we at Infopop Corporation
+# cannot offer you support-- thus modify at your own peril :)
+# ---------------------------------------------------------------------------
+*/
+
+// Require the library
+   require ("../main.inc.php");
+
+// -----------------
+// Get the user info
+   $userob = new user;
+   $user = $userob -> authenticate();
+   $html = new html;
+
+   $html -> send_header ("Welcome, {$user['U_Username']}",$Cat,0,$user);
+   echo "</body></html>";
+
+?>
