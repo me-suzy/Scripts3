400f298aeef13ed06d603ae19bedd0a616159041
me-suzy <me.suzana@gmail.com>
2025-08-14 00:30:39 +0300

    Add Unsorted/PHPCheckout/phpcheckout/adminBackControl.php

---
+++ Unsorted/PHPCheckout/phpcheckout/adminBackControl.php
@@ -0,0 +1,6 @@
+<!-- start of adminBackControl.php -->
+<form name="backToAdminForm" method="post" action="admin.php">
+<input class="submit" type="submit" name="Process" value="<-- Admin">
+</td></tr></table>
+</form>
+<!-- end of adminBackControl.php -->
