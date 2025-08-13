ef7e4b729b43cd91f1db4ab357939e6523e86e3e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:05:52 +0300

    Add Unsorted/topsites/languages/norwegian.php

---
+++ Unsorted/topsites/languages/norwegian.php
@@ -0,0 +1,297 @@
+<?php
+//=================================================================\\
+// Aardvark Topsites PHP 4.1.0                                     \\
+//-----------------------------------------------------------------\\
+// Copyright (C) 2003 Jeremy Scheff - http://www.aardvarkind.com/  \\
+//-----------------------------------------------------------------\\
+// This program is free software; you can redistribute it and/or   \\
+// modify it under the terms of the GNU General Public License     \\
+// as published by the Free Software Foundation; either version 2  \\
+// of the License, or (at your option) any later version.          \\
+//                                                                 \\
+// This program is distributed in the hope that it will be useful, \\
+// but WITHOUT ANY WARRANTY; without even the implied warranty of  \\
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the   \\
+// GNU General Public License for more details.                    \\
+//-----------------------------------------------------------------\\
+// This translation is by Thomas Torjusen                          \\
+// http://www.dykkesiden.com/                                      \\
+//=================================================================\\
+
+// Global
+$LNG['g_form_submit_short'] = "Utfør";
+$LNG['g_form_submit_long'] = "Send";
+$LNG['g_form_submit_reset'] = "Nullstill";
+$LNG['g_id'] = "ID";
+$LNG['g_url'] = "URL";
+$LNG['g_title'] = "Tittel";
+$LNG['g_description'] = "Beskrivelse";
+$LNG['g_category'] = "Category"; // 4.1.0
+$LNG['g_email'] = "Email";
+$LNG['g_bannerurl'] = "Banner URL";
+$LNG['g_password'] = "Passord";
+$LNG['g_average'] = "Gjennomsnitt";
+$LNG['g_today'] = "I dag";
+$LNG['g_yesterday'] = "I går";
+$LNG['g_2days'] = "2 dager siden";
+$LNG['g_3days'] = "3 dager siden";
+$LNG['g_thismonth'] = "Denne mnd."; // 3.2.1
+$LNG['g_lastmonth'] = "Forrige mnd."; // 3.2.1
+$LNG['g_2months'] = "2 mnd. siden"; // 3.2.1
+$LNG['g_3months'] = "3 mnd. siden"; // 3.2.1
+$LNG['g_unq_pv'] = "Unike sidevisninger";
+$LNG['g_tot_pv'] = "Totale sidevisninger";
+$LNG['g_unq_in'] = "Unike treff inn";
+$LNG['g_tot_in'] = "Totale treff inn";
+$LNG['g_unq_out'] = "Unike treff ut";
+$LNG['g_tot_out'] = "Totale treff ut";
+
+// Edit Member Info
+$LNG['edit_header'] = "Endre mdlems info";
+$LNG['edit_error_id_password'] = "Vær vennlig å kontroller ditt ID nummer og passord, og prøv igjen!";
+$LNG['edit_error'] = "Feil";
+$LNG['edit_error_forgot'] = "Du glemte å:";
+$LNG['edit_error_url'] = "Legge inne en gyldig url.";
+$LNG['edit_error_email'] = "Legge inne en  gyldig mailadresse.";
+$LNG['edit_error_title'] = "Legge inn en tittel for din hjemmeside.";
+$LNG['edit_error_password'] = "Legge inn et passord.";
+$LNG['edit_error_back'] = "Vær vennlig å gå tilbake og korriger dette.";
+$LNG['edit_success'] = "Endringer er utført!";
+$LNG['edit_info_edited'] = "Din informasjon er endret.";
+
+// Gateway Page
+$LNG['gateway_header'] = "Topsites Gateway side";
+$LNG['gateway_text'] = "For å fohndre juks så er det satt opp en såkalt gateway side. Klikk nedenfor for å gå til denne topplisten.";
+$LNG['gateway_vote'] = "Gå til aktuell side og gi din stemme.";
+$LNG['gateway_novote'] = "Lurt til å komme hit? Gå videre uten å stemme på aktuell side.";
+$LNG['gateway_cheating'] = "Siden du kommer fra jukser. Denne stemmen vil derfor ikke bli tellende.";
+$LNG['gateway_enter'] = "Gå inn";
+
+// Graph
+$LNG['graph_header'] = "Graf";
+
+// Join
+$LNG['join_header'] = "Registrer deg";
+$LNG['join_error'] = "Feil";
+$LNG['join_error_forgot'] = "Du glemte å:";
+$LNG['join_error_url'] = "Legge inn en gyldig url.";
+$LNG['join_error_email'] = "Legge inn en gyldig mailadresse.";
+$LNG['join_error_title'] = "Legge inn en tittel for ditt nettsted.";
+$LNG['join_error_password'] = "Legge inn et passord.";
+$LNG['join_error_urlbanner'] = "Lege inn en gyldig banner (la dette feltet være blankt hvis du ikke har noe banner. Du kan alltids endre dette senere).  < "; // 4.0
+$LNG['join_error_back'] = "Vær vennlig å gå tilbake og korriger dette.";
+$LNG['join_error_time'] = "Det har gått mindre enn 120 sekunder siden du registrerte deg. Vennligst ikke oppdater denne siden som bekreftr din påmelding."; // 4.0
+$LNG['join_success'] = "Registreringen er fullført!";
+$LNG['join_thanks'] = "Takk skal du ha! Legg inn denne koden på din side slik at du kan bli rangert på listen. Dersom ikke denne koden legges inn så vil din side befinne seg i bunnen av listen og vi vil slette siden din ved neste gjennomgang.";
+$LNG['join_changewarning'] = "Hvis du endrer på koden så vil det kanskje ikke fungere.";
+$LNG['join_welcome'] = "Velkommen til denne topplisten!";
+$LNG['join_welcome_look'] = "Hvis det ikke ser korrekt ut så kan du benytte dene koden:";
+$LNG['join_welcome_admin'] = "Et nytt medlem har registrert seg på din toppliste";
+
+// Lost Code
+$LNG['lostcode_header'] = "Glemt kode";
+
+// Lost Password
+$LNG['lostpw_header'] = "Glemt passord";
+$LNG['lostpw_email'] = "For å velge et nytt passord for din side så klikk på linken nednefor."; // 4.0.1
+$LNG['lostpw_emailed'] = "Instruksjoner for hvordan du endrer ditt passord er blitt sendt til deg via mail.";
+$LNG['lostpw_newpassword'] = "Legg inn et nytt passord"; // 4.0.1
+$LNG['lostpw_finish'] = "Passordet ditt er blitt endret."; // 4.0.1
+$LNG['lostpw_error'] = "Kontroller at du har den rette ID og prøv igjen.";
+
+// Main Template
+$LNG['main_header'] = "Hovedside"; // 4.0
+$LNG['main_all'] = "All Sites"; // 4.2.0
+$LNG['main_method'] = "Rangeringsmetode";
+$LNG['main_members'] = "Medlemmer";
+$LNG['main_menu_main'] = "Hovedside";
+$LNG['main_menu_join'] = "Registrer";
+$LNG['main_menu_random'] = "Tilfeldig medlem";
+$LNG['main_menu_search'] = "Søk";
+$LNG['main_menu_lostcode'] = "Glemt kode";
+$LNG['main_menu_lostpw'] = "Glemt passord";
+$LNG['main_menu_edit'] = "Endre medlemsinfo";
+$LNG['main_featured'] = "Sjekk ut denne"; // 4.0.2
+$LNG['main_executiontime'] = "Script Execution Time"; // 4.0
+$LNG['main_queries'] = "SQL Spørringer"; // 4.0
+$LNG['main_powered'] = "Powered By";
+
+// Ranking Table
+$LNG['table_stats'] = "Statistikk";
+$LNG['table_unique'] = "Unike";
+$LNG['table_total'] = "Totale";
+$LNG['table_rank'] = "Rangering";
+$LNG['table_title'] = "Tittel"; // 4.0
+$LNG['table_description'] = "Beskrivelse"; // 4.0
+
+// Rate and Review
+$LNG['rate_header'] = "Ranger og kommentèr";
+$LNG['rate_rating'] = "Rangering";
+$LNG['rate_review'] = "Kommentèr";
+$LNG['rate_thanks'] = "Takk for dine innspill vedrørende denne siden!";
+$LNG['rate_error'] = "Du har allerede gitt innspill til denne siden.";
+$LNG['rate_back'] = "Tilbake til statistikk";
+
+// Search
+$LNG['search_header'] = "Søk";
+$LNG['search_internet'] = "Internet"; // 4.0
+$LNG['search_topsites'] = "Denne topplisten"; // 4.0
+$LNG['search_off'] = "Søkefunksjonn er blitt slått av for denne topplisten.";
+$LNG['search_rank'] = "Rangering";
+$LNG['search_search'] = "Søk";
+$LNG['search_for'] = "Du søkte etter";
+$LNG['search_string'] = "Søk som en string?";
+$LNG['search_nosites'] = "Dessverre, men vi fant ingen resultater som var i samsvar med ditt søk.";
+$LNG['search_prev'] = "Forrige"; // 3.2.1
+$LNG['search_next'] = "Neste"; // 3.2.1
+
+// Stats
+$LNG['stats_header'] = "Statistikk";
+$LNG['stats_rating_avg'] = "Gjennomsnittelig rangering";
+$LNG['stats_rating_num'] = "Antall rangeringer";
+$LNG['stats_rate'] = "Rangere og kommentèr denne siden";
+$LNG['stats_reviews'] = "Kommentarer";
+$LNG['stats_error_id'] = "Ugyldig ID."; // 4.0
+$LNG['stats_allreviews'] = "Vis alle kommentarer"; // 4.0
+
+// ssi.php
+$LNG['ssi_top'] = "Topp %s sider"; //4.0
+$LNG['ssi_newmembers'] = "%s siste medlemmer"; //4.0
+$LNG['ssi_join'] = "Registrer"; //4.0
+$LNG['ssi_all'] = "Alle"; //4.0
+
+// Admin > Godkjenn nye medlemmer // 4.0
+$LNG['a_approve'] = "Godkjenn"; // 4.0
+$LNG['a_approve_del'] = "Slett"; // 4.0
+$LNG['a_approve_none'] = "Det er ingen medlmmer som venter på å bli godkjent."; // 4.0
+$LNG['a_approve_done'] = "Denne siden er nå godkjent."; // 4.0
+
+// Admin > Delete
+$LNG['a_del_mems'] = "Dette medlemmet har blitt slettet.";
+$LNG['a_del_mem'] = "Slett ett medlem";
+
+// Admin > Edit Member Info
+$LNG['a_edit_password_blank'] = "La denne være blank for å beholde det eksisterende passordet"; // 4.0
+$LNG['a_edit_site_is'] = "Denne siden er"; // 4.0
+$LNG['a_edit_active'] = "Aktiv (oppført på topplisten)"; // 4.0
+$LNG['a_edit_inactive'] = "Inaktiv (Midlertidlig svartelistet - ikke oppførst på topplisten)"; // 4.0
+$LNG['a_edit_error_id'] = "Ugyldig ID.";
+$LNG['a_edit_error'] = "Feil";
+$LNG['a_edit_error_forgot'] = "Du lmte å:";
+$LNG['a_edit_error_url'] = "Legge inn en gyldig url.";
+$LNG['a_edit_error_email'] = "Legge inn en gyldig mailadresse.";
+$LNG['a_edit_error_title'] = "Legge inn en tittel.";
+$LNG['a_edit_error_password'] = "Legge inn et passord.";
+$LNG['a_edit_error_back'] = "Vær vennlig å gå tilbake for å korrigere.";
+$LNG['a_edit_success'] = "Suksess!";
+$LNG['a_edit_info_edited'] = "Medlemmets info er nå blit endret.";
+
+// Admin > Delete Review
+$LNG['a_del_rev'] = "Delete One Review";
+$LNG['a_del_rev_done'] = "The review has been successfully deleted.";
+$LNG['a_del_rev_id'] = "Review ID";
+
+// Admin > Email Members
+$LNG['a_email_addresses'] = "Dette er emailadressen til alle medlemmene.";
+
+// Admin > Find ID
+$LNG['a_find_find'] = "For å finne et medlems ID, bare bruk søkefunksjonen på din toppliste.  <a href=\"index.php?a=search\">Klikk her for å komme til søkefunksjonen</a>.";
+
+// Admin > Login
+$LNG['a_login'] = "Du er logget på.  For å logge ut kan du klikke på logg ut eller stenge din nettleser."; // 4.1.0
+$LNG['a_login_enter'] = "Enter";
+$LNG['a_login_invalidpw'] = "Ugyldig passord!";
+$LNG['a_login_session'] = "Your session has expired, please <a href=\"admin.php\">click here</a> to login again."; // 4.1.0
+
+// Admin > Logout
+$LNG['a_logout'] = "Du er logget av.";
+
+// Admin > Main
+$LNG['a_main'] = "Bruk linkene til venstre for å gå til de forskjellige områdene til admin.";
+$LNG['a_main_approve'] = "Det er %s medlemmer som venter på godkjenning.";
+
+// Admin > Menu
+$LNG['a_menu'] = "Meny";
+$LNG['a_menu_main'] = "Hovedside";
+$LNG['a_menu_approve'] = "Godkjenn nye medlemmer";
+$LNG['a_menu_delete'] = "Slett medlem";
+$LNG['a_menu_edit'] = "Endre medlem";
+$LNG['a_menu_delete_review'] = "Delete Review";
+$LNG['a_menu_email'] = "Send mail til medlemmer";
+$LNG['a_menu_find'] = "Finn en ID";
+$LNG['a_menu_logout'] = "Log ut";
+$LNG['a_menu_reset'] = "Reset Stats"; // 4.1.0
+$LNG['a_menu_settings'] = "Instillinger";
+$LNG['a_menu_version'] = "Versjonskontroll";
+
+// Admin > Reset
+$LNG['a_reset_confirm'] = "This will delete all the stats.  It will not delete your members, it will only reset all their stats to 0."; // 4.1.0
+$LNG['a_reset_stats'] = "Reset Stats"; // 4.1.0
+$LNG['a_reset_done'] = "All members' stats have been reset to 0."; // 4.1.0
+
+// Admin > Settings
+$LNG['a_s_general'] = "Generelle innstillinger";
+$LNG['a_s_list_name'] = "Navne på din toppliste";
+$LNG['a_s_deflanguage'] = "Standard språk";
+$LNG['a_s_path'] = "Full sti til topplistens katalog";
+$LNG['a_s_list_url'] = "URL til topplistens katalog";
+$LNG['a_s_templates_path'] = "Full sti til template katalogen";
+$LNG['a_s_templates_url'] = "URL til template katalogen";
+$LNG['a_s_youremail'] = "Din mailaldresse";
+
+$LNG['a_s_sql'] = "SQL Innstillinger";
+$LNG['a_s_sql_type'] = "Database Type"; // 4.1.0
+$LNG['a_s_sql_host'] = "Host";
+$LNG['a_s_sql_database'] = "Database";
+$LNG['a_s_sql_user'] = "Brukernavn";
+$LNG['a_s_sql_pass'] = "Password";
+
+$LNG['a_s_ranking'] = "Rangeringsinnstillinger";
+$LNG['a_s_categories'] = "Categories (separate with commas)"; // 4.1.0
+$LNG['a_s_numlist'] = "Antall medlemmer som listes opp pr side";
+$LNG['a_s_daymonth'] = "Behold treff for dager eller måneder?";
+$LNG['a_s_rankingmethod'] = "Rangerings metode"; // 4.0.2
+$LNG['a_s_featured'] = "Activate \"Featured Member\"?  You have to add <#featured> to template.html after you turn this on."; // 4.1.0
+$LNG['a_s_top'] = "Antall medlemmer som skal dukke opp i den spesielle topp templaten (fin å benytte på for eksempel din hovedside for å markedsføre topplisten din)";
+$LNG['a_s_adbreak'] = "Vis en reklame etter disse rangeringene (avskill med komma)";
+
+$LNG['a_s_members'] = "Medlems innstillinger";
+$LNG['a_s_active_default'] = "Nye medlemmer skal godkjennes før de dukker opp på listen";
+$LNG['a_s_delete_after'] = "Delete inactive members after this many days"; // 4.1.0
+$LNG['a_s_email_admin_on_join'] = "Send en email til deg når nye medlemmer registrer seg";
+$LNG['a_s_max_banner_width'] = "Medlemmenes maksimum bredde på banner";
+$LNG['a_s_max_banner_height'] = "Medlemmenes maksimum høyde på banner";
+$LNG['a_s_defbanner'] = "Standard banner som medlemmer som benyttes for dem som ikk legger inn noen banner";
+
+$LNG['a_s_button'] = "Knapp innstillinger";
+$LNG['a_s_ranks_on_buttons'] = "Vil du ha dine medlemmers rangering på deres knapper. Se manualen for detaljer."; // 4.0
+$LNG['a_s_button_url'] = "<b>Hvis nei</b> - URL til knappen du vil skal dukke opp på medlemmenes sider"; // 4.0
+$LNG['a_s_button_dir'] = "<b>Hvis ja</b> - URL til katalogen disse bildene er i"; // 4.0
+$LNG['a_s_button_ext'] = "<b>Hvs ja</b> - filenderlse på disse knappene (gif, png, jpg, etc.)"; // 4.0
+$LNG['a_s_button_num'] = "<b>Hvis ja</b> - Antall slike knapper som du har laget. Jo flere du lager - jo mere ressurser vil bli benyttet av scriptet"; // 4.0
+
+$LNG['a_s_searchs'] = "Søke innstillinger"; // 4.0
+$LNG['a_s_search_id'] = "<a href=\"http://www.10ringofhits.com/index.php?refer=50\" target=\"_blank\">10 Ring Of Hits</a> Affiliate ID - se manualen for flere detaljer"; // 4.0
+$LNG['a_s_search'] = "Søke egenskapene";
+$LNG['a_s_searchresults'] = "Søkeresultater pr side";
+
+$LNG['a_s_other'] = "Andre innstillinger";
+$LNG['a_s_gzip'] = "Aktiver gzip - gzip sparer båndbredde, men benytter litt mer CPU kraft";
+$LNG['a_s_timeoffset'] = "Tidsinnstillnger for din server";
+$LNG['a_s_gateway'] = "Gateway side for å forhindre juksing på treff inn";
+
+$LNG['a_s_on'] = "På";
+$LNG['a_s_off'] = "Av";
+$LNG['a_s_days'] = "Dager";
+$LNG['a_s_months'] = "Måneder";
+$LNG['a_s_yes'] = "Ja";
+$LNG['a_s_no'] = "Nei";
+
+$LNG['a_s_updated'] = "Dine instillinger har blitt endret.";
+
+// Admin > Version Check
+$LNG['a_version_using'] = "De benytter den siste versjonen av dette scriptet.";
+$LNG['a_version_your'] = "Din versjon";
+$LNG['a_version_latest'] = "Siste versjon";
+$LNG['a_version_new'] = "Ny versjon er tilgjngelig: <a href=\"http://www.aardvarkind.com/index.php?page=topsitesphp\">Klikk her</a>.";
+?>
