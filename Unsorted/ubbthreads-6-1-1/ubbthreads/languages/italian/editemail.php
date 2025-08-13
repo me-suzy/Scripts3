60c0329d80381126a58234f040e56994efd6387c
me-suzy <me.suzana@gmail.com>
2025-08-14 02:40:22 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"Non riusciamo a trovare un profilo per il Nome Utente";
+$ubbt_lang['EMAIL_HEAD']  = 	"Modifica le notifiche email per";
+$ubbt_lang['PROF_EDIT']  = 	"Puoi modificare ciascuna delle informazioni qui sotto.";
+$ubbt_lang['PROF_SUBS']  = 	"Notifiche email / Sottoscrizioni";
+$ubbt_lang['PROF_NOTIF']  = 	"Vuoi notifiche in email quando riceverai messaggi privati?";
+$ubbt_lang['PROF_REPLIES']  = 	"Vuoi che tutte le risposte ai messaggi che hai inserito ti siano inviati con una email?";
+$ubbt_lang['SUB_STAT']  = 	"Stato di sottoscrizione per i seguenti boards.  (Ricevi un archivio giornaliero in questo board al tuo Reale indirizzo email.)";
+$ubbt_lang['SUB_SHOW']  = 	"(I - Iscrivimi ; C - Cancellami)";
+$ubbt_lang['TEXT_S']  = 	"I";
+$ubbt_lang['TEXT_U']  = 	"C";
+?>
