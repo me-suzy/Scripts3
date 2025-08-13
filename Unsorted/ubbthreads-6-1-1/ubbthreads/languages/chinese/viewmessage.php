18277dd32cd9d301c586021fb8c168692c6bc152
me-suzy <me.suzana@gmail.com>
2025-08-14 02:30:24 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/viewmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/viewmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['ABOUT']  = 	"¹ØÓÚ";
+$ubbt_lang['RR_YOUR_MESS']  = 	"Äú¼Ä¸ø";
+$ubbt_lang['RR_READ']  = 	"µÄÐÅ¼þ£¬¶Ô·½ÒÑ¾­¶Á¹ýÁË¡£";
+$ubbt_lang['RR_NOTIFY']  = 	"ÄúÒÑ¾­ÊÕµ½ÁË¶Ô·½¶Á¹ýÐÅ¼þµÄ¶ÁÐÅÊÕ¾ÝÁË";
+$ubbt_lang['READ_RECEIPT']  = 	"ÏµÍ³Ñ¶Ï¢£ºÊÕ¼þÕßÒÑ¾­¶Á¹ýÐÅÁË";
+$ubbt_lang['TEXT_REC']  = 	"ÊÕÐÅÈÕÆÚ";
+$ubbt_lang['SENT']  = 	"¼ÄÐÅÊ±¼ä";
+$ubbt_lang['DELETE_MESS']  = 	"É¾³ýÕâÆªÎÄÕÂ";
+$ubbt_lang['REPLY_MESS']  = 	"»ØÓ¦ÕâÆªÎÄÕÂ";
+$ubbt_lang['RETURN_START']  = 	"»Øµ½ÄúµÄÊ×Ò³";
+?>
