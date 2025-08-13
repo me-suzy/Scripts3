bf7e909ee750a71659581ca3130e01521061ba54
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:50 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/dorateuser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/dorateuser.php
@@ -0,0 +1,7 @@
+<?
+$ubbt_lang['INVALID_RATING'] = "That is an invalid rating.";
+$ubbt_lang['NOMORERATE'] = "Du har redan betygsatt denna användare.";
+$ubbt_lang['THANKS'] = "Tack.";
+$ubbt_lang['RATED']	= "Ditt betyg för denna användare har mottagits.";
+$ubbt_lang['CANTRATE']  = "Du kan inte betygsätta dig själv.";
+?>
