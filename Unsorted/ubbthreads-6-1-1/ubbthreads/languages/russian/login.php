4fef72b12ae3605aceba5067f9663ec2837d9683
me-suzy <me.suzana@gmail.com>
2025-08-14 02:48:07 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/login.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/login.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['FORGOT_PASS']   = "Forget your password?";
+$ubbt_lang['FORGOT_ENTER']  = "Enter your email address or username that you reg
+istered with to have a temporary password mailed to you.";
+$ubbt_lang['REMEMBER_ME']   =  "Remember me on each visit.";
+$ubbt_lang['LOGIN_PROMPT']  = 	"Ïîæàëóéñòà, çàðåãèñòðèðóéòåñü";
+$ubbt_lang['LOGIN_PROMPT2']  = 	"Enter your Username and Password to login.  If you do not have a username you can";
+$ubbt_lang['REG_ONE']  = 	"register one";
+$ubbt_lang['PASSWORD_TEXT']  = 	"Ïàðîëü";
+?>
