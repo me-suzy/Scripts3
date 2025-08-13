cb763382ba57b768aec247bf355325909faca8a5
me-suzy <me.suzana@gmail.com>
2025-08-14 01:53:23 +0300

    Add Unsorted/stdClassifieds-1-03/stdClassifieds/view_image.php

---
+++ Unsorted/stdClassifieds-1-03/stdClassifieds/view_image.php
@@ -0,0 +1,12 @@
+<?php
+
+/* D.E. Classifieds v1.03 
+   Copyright © 2002 Frank E. Fitzgerald 
+   Distributed under the GNU GPL .
+   See the file named "LICENSE".  */
+
+echo '<form><input type=button value="Close Window"  onClick="window.close();"></form>';
+
+echo '<img src="get_blob.php?the_id='.$the_id.'">';
+
+?>
