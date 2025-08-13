6a86982bc54c4b39cd4d614eccfb6cce68cf0f2d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:29 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/showprofile.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/showprofile.php
@@ -0,0 +1,27 @@
+<?
+$ubbt_lang['RATING']    =       "Rating";
+$ubbt_lang['T_RATES']   =       "Total Rates";
+$ubbt_lang['RATE_USER'] =       "Rate this user";
+$ubbt_lang['STAR1'] =           "1 star";
+$ubbt_lang['STAR2'] =           "2 stars";
+$ubbt_lang['STAR3'] =           "3 stars";
+$ubbt_lang['STAR4'] =           "4 stars";
+$ubbt_lang['STAR5'] =           "5 stars";
+$ubbt_lang['DORATE']    =       "Submit rating";
+$ubbt_lang['YOURATED']  =       "You rated this user a ";
+$ubbt_lang['PROF_FOR']  = 	"Személyes adatok:";
+$ubbt_lang['TEXT_NAME']  = 	"Név";
+$ubbt_lang['USER_TITLE']  = 	"Címe";
+$ubbt_lang['TOTAL_POSTS']  = 	"Összes üzenet";
+$ubbt_lang['TEXT_HOME']  = 	"Home-lap";
+$ubbt_lang['TEXT_OCCU']  = 	"Foglalkozás";
+$ubbt_lang['TEXT_HOBBY']  = 	"Hobbi";
+$ubbt_lang['TEXT_LOC']  = 	"Lakhely";
+$ubbt_lang['TEXT_BIO']  = 	"Ismertetõ";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ Szám";
+$ubbt_lang['REGGED_ON']  = 	"Regisztráció ideje:";
+$ubbt_lang['EDIT_T_U']  = 	"A tag adatainak módosítása";
+$ubbt_lang['ADD_BOOK']  = 	"Add to address book";
+$ubbt_lang['SHOW_ALL_P']  = 	"Az összes tag üzenetét mutassa";
+$ubbt_lang['NO_LONGER']  = 	"Ez a tag már nincs az adatbázisban.";
+?>
