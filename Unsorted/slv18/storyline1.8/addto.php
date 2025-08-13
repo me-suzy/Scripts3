560543d013d3a6283002e016628334c6169ffb62
me-suzy <me.suzana@gmail.com>
2025-08-14 01:46:03 +0300

    Add Unsorted/slv18/storyline1.8/addto.php

---
+++ Unsorted/slv18/storyline1.8/addto.php
@@ -0,0 +1,13 @@
+<?
+include_once("base/define.inc.php");
+include_once("base/main.inc.php");
+
+$display = new MakeDisplay;
+
+$files["title"] = SL_TITLE." &#8226 .: Add Chapter :.";
+$files["body"] = SL_ROOT_PATH."/set/main/addto.inc.php";
+
+$display->CheckTemp($files);
+$display->DisplayTemp();
+
+?>
