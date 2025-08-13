e5902413ed4c22674c1ab509965365bef16314b1
me-suzy <me.suzana@gmail.com>
2025-08-14 00:42:37 +0300

    Add Unsorted/phpListPro-1.80 nullified/phpListPro-1.80n/lang_hellenic.php

---
+++ Unsorted/phpListPro-1.80 nullified/phpListPro-1.80n/lang_hellenic.php
@@ -0,0 +1,943 @@
+<?
+#################################################################################################
+#
+#  project           	: phpListPro
+#  filename          	: lang_hellenic.php
+#  version           	: 1.71
+#  last modified by  	: Vagelis Rossetos
+#  purpose           	: Language File
+#  last modified     	: 9/09/2001
+#
+#################################################################################################
+
+#  Category Settings
+
+
+#################################################################################################
+
+
+
+
+
+$category[0]            = "¼ëåò ïé êáôçãïñßåò";                // must be all Entries
+
+
+$category[1]            = "Êáôçãïñßá 1";
+
+
+$category[2]            = "Êáôçãïñßá 2";
+
+
+$category[3]            = "Êáôçãïñßá 3";
+
+
+$category[4]            = "Êáôçãïñßá 4";
+
+
+$category[5]            = "Êáôçãïñßá 5";
+
+
+$category[6]            = "Êáôçãïñßá 6";
+
+
+
+
+
+
+
+
+#  Language Settings
+
+
+#################################################################################################
+
+
+
+
+
+$lang[metatag]        	= "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-7\">";
+
+
+
+
+
+$lang[rank] 		= "#";
+
+
+$lang[site] 		= "Sites";
+
+
+$lang[votes]		= "In";
+
+
+$lang[hits] 		= "Out";
+
+
+$lang[rate] 		= "Âáèìïß";
+
+
+$lang[random] 		= "Ôõ÷áßï Link";
+
+
+
+
+
+$lang[in_msg1]		= "¸ñ÷åóôå áðï";
+
+
+$lang[in_msg2]		= "Ðáñáêáëþ ðáôÞóôå óôï óÞìá [Vote] ãéá íá äþóåôå ôçí øÞöï óáò óå áõôü ôï site.";
+
+
+$lang[in_msg3]		= "Ìðåßôå óôï $list_name ÷ùñßò íá øçößóåôå.";
+
+
+$lang[in_chose]		= "Ðáñáêáëïýìå âáèìïëïãßóôå ôï site ðïõ åðéóêåõôÞêáôå.";
+
+
+$lang[in_vote]		= "Äþóôå ôçí øÞöï óáò !";
+
+
+
+
+
+$lang[anti_cheat_msg] 	= "<center><font color=red>¸÷åôå Þäç øçöÞóåé ìÝóá óôéò ôåëåõôáßåò $vote_timeout þñåò ! Ç øÞöïò óáò äåí ìÝôñçóå.</font></center>";
+
+
+$lang[vote_log_msg] 	= "<center><font color=red>Óáò åõ÷áñéóôïýìå! Ç øÞöïò óáò ìÝôñçóå !</font></center>";
+
+
+$lang[not_found_msg] 	= "<center><font color=red>ËÁÈÏÓ: Áõôü ôï site äåí âñÝèçêå !!!</font></center>";
+
+
+$lang[tilt_msg] 	= "<center><font color=red>Áõôü ôï site åßíáé ðñïóùñéíÜ åêôüò ëßóôáò ! CHEAT-Detection! ÄïêéìÜóôå áñãüôåñá !</font></center>";
+
+
+$lang[nocookie_msg] 	= "<center><font color=red>Ï Browser óáò äåí Ý÷åé åíåñãïðïéçìÝíá Cookies ! Ç øÞöïò óáò  äåí ìÝôñçóå !</font></center>";
+
+
+$lang[ban_msg] 		= "<center><font color=red>Óáò Ý÷åé áöáéñåèåß ôï äéêáßïìá ôçò øÞöïõ. Ç øÞöïò óáò äåí ìÝôñçóå !</font></center>";
+
+
+
+
+
+$lang[emailconfirmed]	= "Ôï E-mail óáò åðéâåâáéþèçêå åðéôõ÷þò";
+
+
+$lang[emailnotconf]	= "Ôï E-mail óáò äåí Ý÷åé åðéâåâáéùèåß ! Ðáñáêáëïýìå åðéêïéíùíßóôå ìå ôïí Webmaster.";
+
+
+$lang[redirect]		= "Ðñïþèçóç óôï Underground Topsites... Ðáñáêáëïýìå ðåñéìÝíåôå...";
+
+
+$lang[confirmation]	= "Åðéâåâáßùóç";
+
+
+
+
+
+$lang[pages]		= "Óåëßäåò: ";
+
+
+$lang[useronline]	= "×ñÞóôçò online";
+
+
+$lang[usersonline]	= "×ñÞóôåò online";
+
+
+$lang[userssatisfied]	= "×ñÞóôåò åðéóêåýôçêáí ôï Underground Topsites";
+
+
+$lang[sitesindb]	= "Sites óôçí âÜóç äåäïìÝíùí ìáò";
+
+
+$lang[inoutresets]	= "Ôá In's/Out's Ìçäåíßæïíôáé êÜèå";
+
+
+$lang[days]		= "ìÝñåò";
+
+
+$lang[next]		= " åðüìåíïò ìçäåíéóìüò";
+
+
+
+
+
+$lang[addsite]		= "ÅããñáöÞ";
+
+
+$lang[addsitedesc]	= "ÅããñáöÞ íÝïõ site";
+
+
+$lang[editsite]		= "ÁëëáãÞ ðñïößë";
+
+
+$lang[editsitedesc]	= "ÁëëáãÞ ðñïóùðéêþí óôïé÷åßùí";
+
+
+$lang[helpsite]		= "ÂïÞèåéá";
+
+
+$lang[helpsitedesc]	= "ÂïÞèåéá óôïí êþäéêá øçöïöïñßáò ãéá ôç óåëßäá óáò";
+
+
+
+
+
+$lang[error]		= "Åíôïðßóôçêáí ëÜèç ! Ðáñáêáëïýìå ðçãáßíôå ðßóù íá ôá äéïñèþóåôå êáé îáíáðñïóðáèÞóôå !";
+
+
+$lang[error_duplicate]	= "Áõôü ôï site åßíáé Þäç êáôá÷ùñçìÝíï!";
+
+
+$lang[error_emptysiten]	= "Äåí Ý÷åôå ãñÜøåé ôï üíïìá ôïõ site óáò";
+
+
+$lang[error_emptysitea]	= "Äåí Ý÷åôå ãñÜøåé ôçí äéåýèõíóç ôïõ site óáò";
+
+
+$lang[error_emptypass]	= "Äåí Ý÷åôå ãñÜøåé ôïí êùäéêü óáò";
+
+
+$lang[error_emptyemail]	= "Äåí Ý÷åôå ãñÜøåé ôï E-mail óáò";
+
+
+$lang[error_emptyname]	= "Äåí Ý÷åôå ãñÜøåé ôï üíïìá óáò";
+
+
+$lang[error_emptydesc]	= "Äåí Ý÷åôå ãñÜøåé ôçí ðåñéãñáöÞ ôïõ site óáò";
+
+
+$lang[error_bannerw]	= "Äåí Ý÷åôå ãñÜøåé Ýãêõñï ðëÜôïò banner";
+
+
+$lang[error_bannerh]	= "Äåí Ý÷åôå ãñÜøåé Ýãêõñï ýøïò banner";
+
+
+$lang[error_banners]	= "Ôï ìÝãåèïò ôïõ banner óáò õðåñâáßíåé ôï åðéôñåðôü üñéï";
+
+
+$lang[error_bannernok]	= "Ç äéåýèõíóç ãéá ôï banner ðïõ Ý÷åôå äþóåé äåí åßíáé óùóôÞ";
+
+
+$lang[error_baddesc]	= "ÅóöáëìÝíç/åò ëÝîåéò âñÝèçêáí óôçí ðåñéãñáöÞ ôïõ site óáò";
+
+
+$lang[error_badname]	= "ÅóöáëìÝíç/åò ëÝîåéò âñÝèçêáí óôï üíïìá ôïõ site óáò";
+
+
+
+
+
+$lang[back]		= "ÅðéóôñïöÞ";
+
+
+$lang[done]		= "ÏÊ";
+
+
+$lang[submit]		= "Êáôá÷þñçóç";
+
+
+$lang[search]           = "Search";
+
+
+$lang[getlost]		= "Îå÷Üóáôå ôï ID êáé/Þ ôï Password óáò ?";
+
+
+$lang[getid]		= "ÁðïóôïëÞ ID/Password";
+
+
+$lang[editsite]		= "ÁëëáãÞ ðñïößë";
+
+
+
+
+
+$lang[enterdata]	= "Äþóôå ôá óôïé÷åßá óáò";
+
+
+$lang[password]		= "Password: ";
+
+
+$lang[siteid]		= "Site ID: ";
+
+
+$lang[emailaddr]	= "Äéåýèõíóç Email: ";
+
+
+$lang[yourname]		= "Ôï üíïìá óáò: ";
+
+
+$lang[sitetitle]	= "Ôßôëïò ôïõ site: ";
+
+
+$lang[siteurl]		= "Äéåýèõíóç ôïõ site: ";
+
+
+$lang[sitedesc]		= "ÐåñéãñáöÞ ôïõ site óáò: ";
+
+
+$lang[bannerurl]	= "Banner URL: ";
+
+
+$lang[bannerw]		= "ÐëÜôïò ôïõ Banner: ";
+
+
+$lang[bannerh]		= "¾øïò ôïõ Banner: ";
+
+
+
+
+
+$lang[getlost2]		= "Îå÷Üóáôå ôï site ID êáé ôï Password óáò ?";
+
+
+$lang[edityoursite]	= "ÁëëáãÞ ðñïößë";
+
+
+
+
+
+$lang[getidok]		= "Ôï site âñÝèçêå êáé ôá óôïé÷åßá ðïõ æçôÞóáôå óáò Ý÷ïõí óôáëåß!";
+
+
+$lang[getidnok]		= "Äåí âñÝèçêå site êáôá÷ùñçìÝío ìå áõôÞ ôç äéåýèçíóç E-mail!";
+
+
+$lang[editidnok]	= "Äåí âñÝèçêå site ìå áõôÜ ôá óôïé÷åßá !";
+
+
+$lang[editsuccess]	= "Ôï ðñïößë óáò Üëëáîå åðéôõ÷þò!";
+
+
+
+
+
+$lang[addthanks]	= "Åõ÷áñéóôïýìå ! Ôï site óáò êáôá÷ùñÞèçêå";
+
+
+$lang[addloginid]	= "Ôï log-in ID óáò åßíáé: ";
+
+
+$lang[addsendmail]	= ", åíá E-mail åðéâåâáßùóçò Ý÷åé óôáëåß áðï åìÜò óôçí äéåýèõíóç ðïõ äþóáôå.";
+
+
+$lang[addhint]		= "Èá ÷ñåéáóôåßôå ôï ID êáé ôï Password óáò ãéá ìåëëïíôéêÝò áëëáãÝò óôï ðñïößë ôïõ site óáò.";
+
+
+$lang[addapproval]	= "Ç áßôçóç óáò ðáñåëÞöèç êáé èá åëåãèåß áðï ôï Ôeam ôïõ Underground ìÝóá óôéò åðüìåíåò 6 þñåò.";
+
+
+$lang[addplease]	= "Ðáñáêáëïýìå âÜëôå ìßá (Þ ðåñéóóüôåñò öïñÝò) Ýíáí áðï ôïõò áêüëïõèïõò HTML êþäéêåò (ôïõëÜ÷éóôïí Ýíáí ðïõ íá åìöáíßæåé åéêüíá) óôçí êåíôñéêÞ óáò óåëßäá ïõôùò þóôå ïé åðéóêÝðôåò íá ìðïñïýí íá óáò øçößæïõí !";
+
+
+
+
+
+$lang[help]		= "ÂïÞèåéá";
+
+
+$lang[helpreplace1]	= "Ðáñáêáëïýìå áëëÜîôå ôá <b>XXXXXXXX</b> ìå ôï site ID óáò ðïõ óáò Ý÷ïõìå óôßëåé óôï E-mail ôçò åããñáöÞò óáò....";
+
+
+$lang[helpreplace2]	= "";
+
+
+##$lang[helpreplace2]	= "Replace <b>www.yourdomain.com</b> with the URL to your VoteImage.";
+
+
+
+
+
+$lang[addrules]		= "Êáíüíåò ãéá ôçí åããñáöÞ";
+
+
+$lang[addyoursite]	= "Êáôá÷ùñÞóôå ôï site óáò !";
+
+
+
+
+
+$lang[rules]		= "<ul>
+
+
+			    <li>Ç Âáèìïëïãßá åîáñôÜôáé ìüíï áðï ôá åéóåñ÷üìåíá Hits (Votes)
+
+
+			    <li>ÄÝí åðéôñÝðåôáé ç ÷ñÞóç popup windows , JavaScript Þ ïðïéïõäÞðïôå Üëëïõ ôñüðïõ ðïõ ìðïñåß íá êÜíåé ôïí åðéóêÝðôç óáò íá øçöÞóåé ÷ùñßò íá ôï èÝëåé !
+
+
+			    <li>Áðáãïñåýåôáé íá õðï÷ñåþíåôå ôïí åðéóêÝðôç íá øçößóåé ãéá íá áðïêôÞóåé ðñüóâáóç óôï ðåñéå÷üìåíï ôùí óåëßäùí óáò
+
+
+			    <li>Äåí åðéôñÝðåôáé ç êáôá÷þñçóç áíÜëïãùí Topsite Lists (Óå áõôÞí ôç ðåñßðôùóç ïé åíäéáöåñüìåíïé ìðïñïýí íá åðéêïéíùíïýí ìå åìÜò ãéá áíôáëáãÞ Link/Banner)
+
+
+			    <li>Åßóôå õðï÷ñåïìÝíïé íá Ý÷åôå ôïí êþäéêá ìå ôï óçìá ôïõ Underground Topsites óôçí êåíôñéêÞ óáò óåëßäá
+
+
+			    <li>Ôï Banner óôç ëßóôá åìöáíßæåôáé ìüíï ãéá ôá 10 ðñþôá sites
+
+
+			    <li>Ç âáèìïëïãßá ìçäåíßæåôáé êÜèå 15 ìÝñåò
+
+
+			    <li>!!! ÅðéôñÝðåôáé íá êëÝâåôå , áñêåß íá ìçí ôï êáôáëÜâïõìå... !!!
+
+
+			    <li>Óå ðåñßðôùóç ðïõ äåí ôçñoýíôáé ôá ðáñáðÜíù ôï site óáò èá äéáãñáöôåß áðï ôï Underground Topsites ×ùñßò ðñïåéäïðïßçóç !
+
+
+			    <li>Ïé óõíåñãÜôåò ìáò êÜíïõí ôáêôéêïýò åëÝã÷ïõò óôá sites ìÝëç ãéá ôçí ïìáëÞ ôÞñçóç ôùí ðáñáðÜíù
+
+
+			    <li>Ïé êáíüíåò óõìåôï÷Þò ôïõ Underground Topsites ìðïñåß íá áëëÜîïõí ÷ùñßò êáìßá ðñïåéäïðïßçóç
+
+
+			   </ul>";
+
+
+
+
+
+
+
+
+#  Help Language Forms
+
+
+#################################################################################################
+
+
+
+
+
+function help($id) {
+
+
+    global $list_name,$list_url,$voteimage_1,$voteimage_2,$voteimage_3;
+
+
+
+
+
+echo "<hr><b><li>TextLink:</b><br>";
+
+
+echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt$list_name&lt/a&gt\n";
+
+
+
+
+
+
+
+
+if ($voteimage_1) {
+
+
+  echo "<p><hr><b><li>PictureLink:</b><br>\n";
+
+
+  echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt<br>&ltimg src=\"http://www.underground.gr/images/vote_image1.gif\" alt=\"Ìðåßôå óôï $list_name êáé øçößóôå ãé'áõôü ôï Site !!!\" border=0&gt&lt/a&gt\n";
+
+
+  echo "<p><center><img src=\"$voteimage_1\"></center><br>\n";
+
+
+  }
+
+
+
+
+
+if ($voteimage_2) {
+
+
+  echo "<p><hr><b><li>PictureLink 2:</b><br>\n";
+
+
+  echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt<br>&ltimg src=\"http://www.underground.gr/images/vote_image2.gif\" alt=\"Ìðåßôå óôï $list_name êáé øçößóôå ãé'áõôü ôï Site !!!\" border=0&gt&lt/a&gt\n";
+
+
+  echo "<p><center><img src=\"$voteimage_2\"></center><br>\n";
+
+
+  }
+
+
+
+
+
+if ($voteimage_3) {
+
+
+  echo "<p><hr><b><li>PictureLink 3:</b><br>\n";
+
+
+  echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt<br>&ltimg src=\"http://www.underground.gr/images/vote_image3.gif\" alt=\"Ìðåßôå óôï $list_name êáé øçößóôå ãé'áõôü ôï Site !!!\" border=0&gt&lt/a&gt\n";
+
+
+  echo "<p><center><img src=\"$voteimage_3\"></center><br>\n";
+
+
+  echo "<p>";
+
+
+  }
+
+
+}
+
+
+
+
+
+#  Mail Language Forms
+
+
+#################################################################################################
+
+
+
+
+
+function mail_getidpwd($result) {		// Mail Form for sending lost ID/Password
+
+
+    global $list_name,$list_url,$admin_email;
+
+
+
+
+
+mail("$result[email]", "Login-Data request áðï $list_name", "Hello $result[name],
+
+
+ÁõôÜ åßíáé ôá ðñïóùðéêÜ óáò óôïé÷åßá áðï ôï $list_name.
+
+
+
+
+
+Ôï site óáò:
+
+
+$result[site_name]
+
+
+$result[site_addr]
+
+
+
+
+
+Óçìåéþóôå ôá ðáñáêÜôù !
+
+
+Site code: $result[id]
+
+
+Password: $result[password]
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+HTML êþäéêáò ãéá ôçí åéêüíá øçöïöïñßáò (ÐñÝðåé íá ôïðïèåôçèåß óôçí êåíôñéêÞ óáò óåëßäá !) :
+
+
+
+
+
+<a href=\"$list_url/in.php?site=$result[id]\" target=\"_blank\"><img src=\"http://www.underground.gr/images/vote_image1.gif\" border=\"0\"></a>
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+function mail_newnotifyuser($email_address,$webmaster_name,$id,$password,$site_name,$site_address,$site_description) {
+
+
+    global $list_name,$list_url,$admin_email,$secret,$webmasterapproval,$emailapproval;
+
+
+
+
+
+if ($webmasterapproval) {$webmastermsg="\nÇ áßôçóç óáò ðáñåëÞöèç êáé èá åëåãèåß áðï ôï Ôeam ôïõ Underground ìÝóá óôéò åðüìåíåò 6 þñåò.\n";} else {$webmastermsg="";}
+
+
+if ($emailapproval) {$emailmsg="\nÐáñáêáëïýìå ðáôÞóôå áõôü ôï link ãéá íá åðéâåâáéþóåôå ôçí êáôá÷ùñçóç óáò :\n$list_url/confirm.php?check=".urlencode(encrypt($id,$secret))."\n\n";} else {$emailmsg="";}
+
+
+
+
+
+mail("$email_address", "ÊáëùóÞëèåò $webmaster_name óôï $list_name", "Ó'åõ÷áñéóôïýìå $webmaster_name ðïõ ãñÜöôçêåò óôï $list_name.
+
+
+$webmastermsg$emailmsg
+
+
+Ôï site óáò:
+
+
+$site_name ($site_address)
+
+
+
+
+
+Óçìåéþóôå áõôÜ ãéá ìåëëïíôéêÞ ÷ñÞóç!
+
+
+Site ID: $id
+
+
+Password: $password
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+HTML êþäéêáò ãéá ôçí åéêüíá øçöïöïñßáò (ÐñÝðåé íá ôïðïèåôçèåß óôçí êåíôñéêÞ óáò óåëßäá !) :
+
+
+
+
+
+<a href=\"$list_url/in.php?site=$id\" target=\"_blank\"><img src=\"http://www.underground.gr/images/vote_image1.gif\" alt=\"Ìðåßôå óôï $list_name êáé øçößóôå áõôü ôï Site !!!\" border=0></a>
+
+
+
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_newnotifyadm($email_address,$webmaster_name,$id,$password,$site_name,$site_address,$site_description) {
+
+
+    global $list_name,$list_url,$admin_email,$notify_email;
+
+
+
+
+
+mail("$notify_email", "NOTIFY $webmaster_name joined the $list_name", "$webmaster_name åããñÜöôçêå $list_name.
+
+
+
+
+
+Site ID: $id
+
+
+Password: $password
+
+
+¼íïìá ôïõ site: $site_name
+
+
+Äéåýèõíóç ôïõ site: $site_address
+
+
+
+
+
+Email: $email_address
+
+
+
+
+
+Banner URL (if any): $banner_address
+
+
+
+
+
+ÐåñéãñáöÞ:
+
+
+$site_description
+
+
+
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_editnotifyadm($email_address,$webmaster_name,$id,$password,$site_name,$site_address,$site_description) {
+
+
+    global $list_name,$list_url,$admin_email,$notify_email;
+
+
+
+
+
+mail("$notify_email", "NOTIFY $webmaster_name edited on $list_name", "$webmaster_name edited on $list_name.
+
+
+
+
+
+Site ID: $id
+
+
+Password: $password
+
+
+¼íïìá ôïõ site: $site_name
+
+
+Äéåýèõíóç ôïõ site: $site_address
+
+
+
+
+
+Email: $email_address
+
+
+
+
+
+Banner address (if any): $banner_address
+
+
+
+
+
+ÐåñéãñáöÞ:
+
+
+$site_description
+
+
+
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_tiltuser($result) {		// Mail Form for sending User on TILT
+
+
+    global $list_name,$list_url,$admin_email,$tilt_insp_timeout;
+
+
+
+
+
+mail("$result[email]", "ALERT Cheating-Detection from $result[site_name] on $list_name",
+
+
+"ÁãáðçôÝ ößëå êáé óõíåñãÜôç,
+
+
+
+
+
+Ôï óýóôçìá êáôáìÝôñçóçò øÞöùí (Cheating-Detection) ôïõ $list_name åíôüðéóå ìÞ äßêáéïõò øÞöïõò áðï ôï site óïõ !
+
+
+
+
+
+ID: $result[id]
+
+
+$result[site_name] ($result[site_addr])
+
+
+
+
+
+Ôï site óïõ äÝí ìðïñåß íá äÝ÷åôå øÞöïõò óôï $list_name ($list_url) ãéá $tilt_insp_timeout ëåðôÜ.
+
+
+Ïé åéóåñ÷üìåíåò øÞöïé èá ìåôñïýí êáíïíéêÜ ìåôÜ ôçí ëÞîç áõôÞò ôçò ÷ñïíéêÞò ðåñéüäïõ !
+
+
+
+
+
+ÖéëéêÜ,
+
+
+
+
+
+Underground Topsites list Administrator.
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_tiltadmin($result) {		// Mail Form for sending lost ID/Password
+
+
+    global $list_name,$list_url,$admin_email,$notify_email,$tilt_insp_timeout;
+
+
+
+
+
+mail("$notify_email",  "NOTIFY Cheating-Detection áðï $result[site_name] óôï $list_name",
+
+
+"ÃåéÜ óïõ áöåíôéêü,
+
+
+
+
+
+the Cheating-Detection at $list_name has detected some strange votings for this site.
+
+
+
+
+
+ID: $result[id]
+
+
+$result[site_name] ($result[site_addr])
+
+
+
+
+
+Áõôü ôï site åßíáé áðåíåñãïðïéçìÝíï $list_name ($list_url) ãéá $tilt_insp_timeout minutes.
+
+
+
+
+
+Cheating-Detection (Tilt-Protection)
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+#  End Language
+
+
+#################################################################################################
+
+
+?>
