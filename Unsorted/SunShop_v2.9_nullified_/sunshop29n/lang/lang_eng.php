7fec8254e0d74c2eb4ac7235bf16cce93aa9d09b
me-suzy <me.suzana@gmail.com>
2025-08-14 01:55:39 +0300

    Add Unsorted/SunShop_v2.9_nullified_/sunshop29n/lang/lang_eng.php

---
+++ Unsorted/SunShop_v2.9_nullified_/sunshop29n/lang/lang_eng.php
@@ -0,0 +1,293 @@
+<?PHP
+$lang_version = "1.1";
+$lang_lang = "English";
+
+// Category Menu Display
+$lang_cat[best] = "Bestsellers";
+$lang_cat[spec] = "Specials";
+$lang_cat[next] = "next";
+$lang_cat[prev] = "previous";
+
+// Shipping Select Display
+$lang_ship[tax] = "Tax";
+$lang_ship[shipping] = "Shipping";
+$lang_ship[total] = "Total";
+$lang_ship[subtotal] = "Subtotal";
+$lang_ship[ship_address] = "Shipping Address";
+$lang_ship[edit_address] = "Edit Your Address";
+$lang_ship[comments] = "Comments";
+$lang_ship[coupon] = "Coupon";
+$lang_ship[shipaddressok] = "Please make sure your shipping address is correct. Select the \"Secure Area\" button to proceed to the secure area.";
+$lang_ship[shipmethod] = "Please select your shipping method and make sure your shipping address is correct. Select the \"Secure Area\" button to proceed to the secure.";
+$lang_ship[ground] = "UPS Ground";
+$lang_ship[nda] = "UPS Next Day Air";
+$lang_ship[sda] = "UPS Second Day Air";
+$lang_ship[tds] = "UPS Three Day Select";
+$lang_ship[cs] = "UPS Canada Standard";
+$lang_ship[wwe] = "UPS Worldwide Express";
+$lang_ship[wwep] = "UPS Worldwide Express Plus";
+$lang_ship[usps_express] = "USPS Express";
+$lang_ship[usps_priority] = "USPS Priority";
+$lang_ship[usps_parcel] = "USPS Standard";
+$lang_ship[pretotal] = "Pre Total";
+$lang_ship[discount] = "Discount";
+
+// Payment Select Display
+$lang_pay[pm] = "Payment Method";
+$lang_pay[cc] = "Credit Card";
+$lang_pay[pc] = "Personal Check";
+$lang_pay[mo] = "Money Order";
+$lang_pay[fo] = "Fax Order";
+$lang_pay[pp] = "PayPal";
+$lang_pay[po] = "Purchase Order";
+
+// Show Cart Display
+$lang_cart[outofstock] = "That item is currently out of stock or you have selected a quantity greater then the quantity in stock, please contact us or try again at a later time.";
+$lang_cart[noitems] = "Your cart is empty";
+$lang_cart[checkout] = "If you would like to remove items from your cart, simply set the quantity to 0 and click the \"Update Cart\" button.";
+$lang_cart[shopmore] = "Shop More";
+$lang_cart[update] = "Update Cart";
+$lang_cart[checkout2] = "To Checkout";
+
+
+// Search Display
+$lang_search[results] = "Your search returned";
+$lang_search[results2] = "results";
+$lang_search[tip1] = "Make sure you enter a keyword.";
+$lang_search[tip2] = "Select a category to search in.";
+$lang_search[found1] = "Viewing";
+$lang_search[found2] = "to";
+$lang_search[found3] = "of";
+
+// Item Display
+$lang_item[SKU] = "SKU#";
+$lang_item[price] = "Price";
+$lang_item[quantity] = "Quantity";
+$lang_item[add] = "Add";
+$lang_item[tocart] = "Add To Cart";
+$lang_item[tell] = "Tell A Friend";
+$lang_item[sale] = "Sale Description";
+$lang_item[poverview] = "Product Overview";
+$lang_item[details] = "Product Details";
+$lang_item[free] = "To receive this discount include the free ones in your cart as well, the difference will be takin off at checkout";
+
+// Top Icons Display
+$lang_icons[cart] = "View Cart";
+$lang_icons[account] = "View Account";
+$lang_icons[help] = "Get Help";
+
+// Product List Display
+$lang_prod[bottom1] = "product(s) found. Viewing";
+$lang_prod[bottom2] = "thru";
+$lang_prod[bottum3] = "Product Page";
+
+// Login Or Signup Page
+$lang_los[los] = "Please Login";
+$lang_los[los_message] = "To continue please login to the right with your assigned username and password. If you have lost your username and password you can have it emailed to you by using the <a href='index.php?action=login'>email form</a>.";
+$lang_los[los_message2] = "To continue please login with your assigned username and password. If you have lost your username and password you can have it emailed to you by using the <a href='index.php?action=login'>email form</a>.";
+$lang_los[new_users] = "New Users";
+$lang_los[reg_message] = "By registering with us you will be able to order products online. Registering is free and easy and there is no obligation of any kind.";
+$lang_los[login] = "Login";
+$lang_los[pass] = "Password";
+$lang_los[r_pass] = "Retype Password";
+$lang_los[f_name] = "Full Name";
+$lang_los[phone] = "Phone";
+$lang_los[email] = "Email";
+$lang_los[s_address] = "Shipping Address";
+$lang_los[address] = "Address";
+$lang_los[address1] = "Address 1";
+$lang_los[address2] = "Address 2";
+$lang_los[city] = "City";
+$lang_los[sp] = "State/Province";
+$lang_los[zp] = "Zip/Postal Code";
+$lang_los[country] = "Country";
+$lang_los[b_address] = "Billing Address (If Different Then Above)";
+$lang_los[required] = "Required";
+$lang_los[u_exists] = "Username Already Exists";
+$lang_los[no_match] = "Passwords Did Not Match";
+$lang_los[i_name] = "Invalid Name";
+$lang_los[cont] = "Continue";
+$lang_los[reset] = "Reset";
+$lang_los[b_message] = "Please enter all shipping information as it appears on the credit card. In order to calculate shipping costs correctly, make sure all shipping information is 100% accurate. You may be held responsible for all shipments returned due to an invalid address.";
+
+// Account Information
+$lang_account[message] = "Please make sure that we have the most current information in our database. You may edit your information or view your order status below.";
+$lang_account[pinfo] = "Personal Information";
+$lang_account[corders] = "Current Orders";
+$lang_account[order] = "Order #";
+$lang_account[date] = "Date";
+$lang_account[pstatus] = "Payment Status";
+$lang_account[status] = "Status";
+$lang_account[no_orders] = "No Pending Orders";
+
+// Add Account (Items enclosed in <!--  --> tags will be replaced with variables.)
+$lang_adduser[error] = "There was an error while storing your information in our database. Please wait a few minutes and try your submission again. If you continue to have problems please email us.";
+$lang_adduser[ty_email] = "Thank you for signing up at <!-- title -->. Your login information is as follows:\n\nLogin: <!-- username -->\nPassword: <!-- password -->\n\nIf you need any additional help please see our online help page located at:\n<!-- shopurl -->?action=help\nThank you for shopping at <!-- title -->\n\n<!-- contactemail -->";
+$lang_adduser[ty_subject] = "Your <!-- title --> Account";
+$lang_adduser[ty] = "Thank You";
+$lang_adduser[screen_message] = "You have been successfully added into our client database. You may now log in using the login area to the right. Once logged in, you may begin shopping or change your account information. Thank you for creating an account with <!-- title -->.";
+
+// Update Account
+$lang_updateuser[error] = "There was an error while updating your information in our database. Please wait a few minutes and try your submission again. If you continue to have problems please let us know.";
+$lang_updateuser[succ] = "You have successfully updated your account. You may proceed adding items to your cart	or checking out. If you changed your password you will need to log back in.";
+
+// Login Area
+$lang_login[acc_login] = "Account Login";
+$lang_login[uname] = "USERNAME";
+$lang_login[pass] = "PASSWORD";
+$lang_login[fpass] = "Forgot Password?";
+$lang_login[nuser] = "New User?";
+$lang_login[welcome] = "Welcome Back";
+$lang_login[welcome_no] = "Welcome";
+$lang_login[acc] = "Account";
+$lang_login[ip] = "IP Address";
+$lang_login[cart] = "Your Cart";
+$lang_login[sub] = "Subtotal";
+$lang_login[out] = "LOGOUT";
+
+// No Account Required Checkout
+$lang_no_acc[personal] = "Checkout Step 1";
+$lang_no_acc[cont] = "Checkout Continue";
+$lang_no_acc[step1] = "To continue please fill out the form below with your shipping and billing information.";
+
+// Header
+$lang_header[cats] = "Category Index";
+$lang_header[search] = "Search";
+
+// Footer
+$lang_footer[spec] = "Current Specials";
+$lang_footer[nospecials] = "No current specials at this time.";
+
+// Index (Items enclosed in <!-- --> tags will be replaced with variables)
+$lang_index[welcome] = "Welcome to <!-- title -->";
+$lang_index[best] = "Today's Bestsells";
+$lang_index[ship] = "Select A Shipping Method";
+$lang_index[proceed] = "Proceed To Checkout";
+$lang_index[pricelist] = "Pricelist Summary";
+$lang_index[update] = "Updating Account";
+$lang_index[acc_update] = "Account Update";
+$lang_index[friend] = "Tell A Friend";
+$lang_index[friend_mess] = "I was surfing the net and I found this product that you may be interested in. Just follow the link and it will take you right to the product. Hope you like it!\n\n<!-- shopurl -->index.php?action=item&id=<!-- item -->\n\nRegards,\n<!-- name -->";
+$lang_index[fform_mess] = "Please enter your friends email address below to tell them about the product you have selected. The email will be sent with the message you specify below.";
+$lang_index[fmess] = "Message";
+$lang_index[femail] = "Friends Email";
+$lang_index[send] = "Send It";
+$lang_index[fsubject] = "Something You May Like";
+$lang_index[fsent] = "Tell A Friend Email Sent";
+$lang_index[fsent_mess] = "Done! The email has been sent to <strong><!-- email --></strong> with the product you selected. Thanks for taking the time to share our products with your friends.";
+$lang_index[lost_pass_mess] = "Your requested login information is as follows:\n\nLogin: <!-- username -->\nPassword: <!-- password -->\n\nIf you did not request this information please ignore this message. Thank you for shopping at <!-- title -->\n\n<!-- contactemail -->";
+$lang_index[lost_pass_sub] = "<!-- title --> Login Information";
+$lang_index[lost_pass_head] = "Emailing Login Information";
+$lang_index[lost_pass_sent] = "Email Sent";
+$lang_index[lost_pass_error] = "Email Error";
+$lang_index[pass_sent_mess] = "An email with your login information has been sent to your email address at <!-- email -->.";
+$lang_index[pass_sent_error] = "The email address <!-- email --> is not in our database. Please check the address and try again.";
+$lang_index[invalid_login] = "Invalid Login";
+$lang_index[invalid_login_head] = "Login Error";
+$lang_index[invalid_login_mess] = "You have submitted invalid login information. Please try your login again. If you continue to have problems please <a href='mailto:<!-- contactemail -->'>contact us</a>.";
+$lang_index[forgot_pass] = "Forgot Your Password?";
+$lang_index[email] = "Email";
+$lang_index[email_button] = "Send Login Info";
+$lang_index[new_account] = "New Account";
+$lang_index[update_account] = "Update Account";
+$lang_index[update_acc_error] = "There was an error while updating your information in our database. Please	wait a few minutes and try your submission again. If you continue to have problems please let us know.";
+$lang_index[error] = "Error";
+$lang_index[search] = "Search for";
+$lang_index[search_res] = "Search Results";
+$lang_index[faq_head] = "Frequently Asked Questions";
+$lang_index[ov_head] = "Overview";
+$lang_index[account] = "Your Account";
+$lang_index[not_logged] = "Not Logged In";
+$lang_index[edit] = "Edit Account";
+$lang_index[help] = "Help";
+$lang_index[your_cart] = "Your Cart";
+$lang_index[items] = "Items";
+$lang_index[view_item] = "View Item";
+$lang_index[view_cat] = "View Category";
+$lang_index[prod] = "Products";
+$lang_index[not_found] = "Page Not Found!";
+$lang_index[not_mess] = "Your requested query was not found please check the url and try again. Please <a href='index.php'>return</a> to the main page.";
+$lang_index[info] = "More Info";
+
+// FAQ. You may add your own or change any below. Be sure maintian number order. (Items enclosed in <!-- --> tags will be replaced with variables)
+$lang_faq_ques[0] = "Is my order secure?";
+$lang_faq_ans[0] = "When you are in the final checkout mode, you should see a lock shown on your browser somewhere at the bottom of your screen. This means that the order is secure. This is only important when submitting credit card information.";
+$lang_faq_ques[1] = "Can I view the status of my orders?";
+$lang_faq_ans[1] = "Yes you can do this at anytime. Simply click the \"View Account\" button located on the top of each page.";
+$lang_faq_ques[2] = "Who do I contact with my questions?";
+$lang_faq_ans[2] = "You may email your questions to <a href=\"mailto:<!-- contactemail -->\"><!-- contactemail --></a> anytime or call us at <!-- shopphone -->.";
+//$lang_faq_ques[3] = "";
+//$lang_faq_ans[3] = "";
+//$lang_faq_ques[4] = "";
+//$lang_faq_ans[4] = "";
+//$lang_faq_ques[5] = "";
+//$lang_faq_ans[5] = "";
+
+$lang_faq_ov[pararaph1] = "When you first enter the shop, you are assigned a shopping cart that holds your purchases. Whenever you see an item that you want to purchase, you can add it to your cart by entering the number of items to add and clicking on the \"Add To Cart\" button for that item. You will then see the contents of your shopping cart, including the item(s) that you just added.";
+$lang_faq_ov[pararaph2] = "At any time, you can view or change the contents of your cart by clicking on the \"View Shopping Cart\" link at the top of the page. You can remove items from your cart by simply setting the quantity to 0 and updating your cart.";
+$lang_faq_ov[pararaph3] = "When you have finished collecting items, you can click on the \"Checkout\" button located in the \"View Shopping Cart\" area, assuming that you are logged in. You will be given the chance to verify your purchase before proceeding to checkout.";
+$lang_faq_ov[pararaph4] = "If you are not logged in you will need to either create an account, or login in order to use the shopping cart.";
+
+// Checkout Display
+$lang_checkout[final] = "Final Checkout";
+$lang_checkout[quantity] = "Quantity";
+$lang_checkout[product] = "Product";
+$lang_checkout[options] = "Options";
+$lang_checkout[price] = "Price";
+$lang_checkout[coupon] = "Coupon Discount";
+$lang_checkout[total] = "Final Total";
+$lang_checkout[ccinfo] = "Credit Card Information";
+$lang_checkout[noc] = "Name On Card";
+$lang_checkout[cn] = "Card Number";
+$lang_checkout[ed] = "Expiration Date";
+$lang_checkout[ct] = "Card Type";
+$lang_checkout[cvv2] = "CVV2";
+$lang_checkout[what] = "What is this?";
+$lang_checkout[po] = "Purchase Order Information";
+$lang_checkout[comp] = "Company Name";
+$lang_checkout[ponum] = "P.O. Number";
+$lang_checkout[submit] = "Submit Transaction";
+$lang_checkout[reset] = "Reset";
+
+// Order Confirmation (Items enclosed in <!-- --> tags will be replaced with variables)
+$lang_confirm[out1] = "Item #";
+$lang_confirm[out2] = "Item Name";
+$lang_confirm[out3] = "Quantity";
+$lang_confirm[out4] = "Options";
+$lang_confirm[out5] = "Price";
+// Email Message
+$lang_email[subject] = "Your Order Confirmation: <!-- title -->";
+$lang_email[part1ns] = "Thank you for shopping at <!-- title -->.\n\nYour Order Total: <!-- currency --><!-- total -->\nCustomer ID: <!-- userid -->\nTransaction #: <!-- transaction -->\n\n"; 
+$lang_email[part1] = "Thank you for shopping at <!-- title -->.\n\nCoupon Savings: <!-- currency --><!-- savings -->\nYour Order Total: <!-- currency --><!-- total -->\nCustomer ID: <!-- userid -->\nTransaction #: <!-- transaction -->\n\n"; 
+$lang_email[part2_cc] = "For added security, confidential account information is subject to verification by your financial institution. To track the status of your order, visit <!-- shopurl --> ";
+$lang_email[part2_po] = "To track the status of your order, visit <!-- shopurl --> ";
+$lang_email[part2_fax] = "In order to complete your order we must receive your payment. Please fax it asap to <!-- shopfax -->.\n\nTo track the status of your order, visit <!-- shopurl --> ";
+$lang_email[part2_cm] = "In order to complete your order you must send your <!-- payment -->, payable to \"<!-- payable -->\", along with the form to:\n\n<!-- companyname -->\n<!-- shopaddress -->\n<!-- shopcity -->, <!-- shopstate --> <!-- shopzip -->\n\nTo track the status of your order, visit <!-- shopurl --> ";
+$lang_email[part2_paypal] = "In order to complete your order you must submit your payment via PayPal to: <!-- paypalemail -->\n\nTo track the status of your order, visit <!-- shopurl -->\n\n";
+$lang_email[part3] = " and click on the \"View Account\" link to learn if your order has been processed or shipped.\n\n";
+$lang_email[part4] = "If you have questions or comments, please send them to <!-- contactemail -->\n\nSincerely,\n<!-- companyname -->\n";
+$lang_email[admin_subject] = "New Order Made: <!-- title -->";
+$lang_email[admin_body] = "A new order has been placed. The order information is below.\n<!-- shopurl -->admin/\n\n----------------Start Transaction---------------\n\n";
+// Screen Message
+$lang_screen[title] = "Transaction Complete!";
+$lang_screen[part1ns] = "Thank you for shopping at <!-- title -->.<br><br><b>Your Order Total:</b> <!-- currency --><!-- total --><br><b>Customer ID:</b> <!-- userid --><br><b>Transaction #:</b> <!-- transaction --><br><br>"; 
+$lang_screen[part1] = "Thank you for shopping at <!-- title -->.<br><br><b>Coupon Savings:</b> <!-- currency --><!-- savings --><br><b>Your Order Total:</b> <!-- currency --><!-- total --><br><b>Customer ID:</b> <!-- userid --><br><b>Transaction #:</b> <!-- transaction --><br><br>"; 
+$lang_screen[part2_cc] = "For added security, confidential account information is subject to verification by your financial institution. To track the status of your order, visit <a href='<!-- shopurl -->'><!-- shopurl --></a> ";
+$lang_screen[part2_po] = "To track the status of your order, visit <a href='<!-- shopurl -->'><!-- shopurl --></a> ";
+$lang_screen[part2_fax1] = "In order to complete your order we must receive your payment. Please fax it asap to <!-- shopfax -->. We accept the following credit cards.";
+$lang_screen[part2_fax_out1] = "Name On Card";
+$lang_screen[part2_fax_out2] = "Card Number";
+$lang_screen[part2_fax_out3] = "Expiration Date";
+$lang_screen[part2_fax_out4] = "Card Type";
+$lang_screen[part2_fax_out5] = "CVV2";
+$lang_screen[part2_fax_out6] = "Signature";
+$lang_screen[part2_fax_out7] = "What is this?";
+$lang_screen[part2_fax2] = "To track the status of your order, visit <a href='<!-- shopurl -->'><!-- shopurl --></a> ";
+$lang_screen[part2_cm] = "In order to complete your order you must send your <!-- payment -->, payable to \"<!-- payable -->\", along with the form to:<br><br><!-- companyname --><br><!-- shopaddress --><br><!-- shopcity -->, <!-- shopstate --> <!-- shopzip --><br><br>To track the status of your order, visit <a href='<!-- shopurl -->'><!-- shopurl --></a> ";
+$lang_screen[part2_paypal1] = "In order to complete your order you must submit your payment via PayPal to: <!-- paypalemail -->";
+$lang_screen[part2_paypal2] = "To track the status of your order, visit <a href='<!-- shopurl -->'><!-- shopurl --></a> ";
+$lang_screen[part2_paypal3] = "Use the button above to pay now.";
+$lang_screen[part3] = "and click on the \"View Account\" link to learn if your order has been processed or shipped.<br><br>";
+$lang_screen[part4] = "If you have questions or comments, please send them to <a href='mailto:<!-- contactemail -->'><!-- contactemail --></a><br><br>Sincerely,<br><!-- companyname -->";
+
+?>
