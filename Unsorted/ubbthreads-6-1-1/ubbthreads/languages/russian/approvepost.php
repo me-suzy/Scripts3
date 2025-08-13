edbe70eb59f4bf6f4f7a7361b7542aa5de9066b2
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:32 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/approvepost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/approvepost.php
@@ -0,0 +1,5 @@
+<?
+$ubbt_lang['REPLY_SUB']  = 	"Îòâåò íà Âàøå ñîîáùåíèå";
+$ubbt_lang['REPLY_BOD']  = 	"replied to your post at the site: .";
+$ubbt_lang['APPR_HEAD']  = 	"Âàøå ñîîáùåíèå ïðèíÿòî.";
+?>
