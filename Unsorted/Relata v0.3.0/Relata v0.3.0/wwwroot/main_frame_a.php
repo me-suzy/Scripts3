9f1f886a2f9af467163db13bfd3768660dd6a294
me-suzy <me.suzana@gmail.com>
2025-08-14 01:21:35 +0300

    Add Unsorted/Relata v0.3.0/Relata v0.3.0/wwwroot/main_frame_a.php

---
+++ Unsorted/Relata v0.3.0/Relata v0.3.0/wwwroot/main_frame_a.php
@@ -0,0 +1,63 @@
+<?
+
+//******************************************************************************
+/*
+GPL Copyright Notice
+
+Relata
+Copyright (C) 2001-2002 Stratabase, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+//******************************************************************************
+
+/* 
+output the top frame
+
+Relates to:
+/a.js 
+/templates/a.htm
+/templates/a.css
+
+*/
+
+include("config.inc.php");
+
+//  We use the following features:
+//  sess   for session variables
+//  auth   for login checks, also required for user variables
+
+// perform all startup functions
+page_open(array(
+	"sess" => "relata_session", 
+	"auth" => "relata_challenge_auth"
+	));
+
+// start a new template
+$template = start_template();
+
+// set template file
+$template->set_file("a","main_frame_a.htm");
+
+// set template variables
+$template->set_var("SESSION_ID",$session_id);
+
+// print the template
+$template->parse("output","a");
+$template->p("output");
+
+page_close();
+
+?>
