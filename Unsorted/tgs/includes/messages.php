bc60148c2b8c10e4c01933d2a1dc63cc0e3ef1d8
me-suzy <me.suzana@gmail.com>
2025-08-14 02:01:37 +0300

    Add Unsorted/tgs/includes/messages.php

---
+++ Unsorted/tgs/includes/messages.php
@@ -0,0 +1,22 @@
+<?php
+// All messages will come here
+$M_Usernamenotfound = "Invalid username or password";
+$M_PaypalChargesUpdated = "Paypal fee updated";
+$M_PaypalChargesInserted = "Paypal fee inserted";
+$M_InfoUpdated = "Info updated successfully";
+$M_InfoAdded = "Site added successfully";
+$M_NoRecordFound = "No Record Found";
+$M_RecordsDeleted = "Records deleted successfully";
+$M_CategoryAdded = "Category added successfully";
+$M_CategoryDeleted = "Category deleted successfully";
+$M_UsernamePresent = "This username already present";
+$M_EmailSendFailure = "Cannot send email";
+$M_NoReminderAddedd = "No reminder added previously";
+$M_ReminderDeleted = "Reminder deleted successfully";
+$M_RegisterSuccess = "You are registered successfully";
+$M_RedirectPayPal = "Wait... Redirecting to paypal";
+$M_NotRegister = "Sorry, You are not registered successfully.<br>Try some other time.";
+$M_MessageUpdated = "Message updated successfully";
+$M_MessageSaved = "Message saved";
+// Messages will end here
+?>
