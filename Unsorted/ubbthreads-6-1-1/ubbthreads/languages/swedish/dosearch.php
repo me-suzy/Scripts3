2000de5631d5d5df6ce1b9da6efdcd468ea15538
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:52 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/dosearch.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_WORDS']  = 	"Inget sökord!";
+$ubbt_lang['SEARCH_RES']  = 	"Sökresultat";
+$ubbt_lang['PREV_PAGE']  = 	"Föregående sida";
+$ubbt_lang['MAIN_SEARCH']  = 	"Huvudsöksidan";
+$ubbt_lang['NEXT_PAGE']  = 	"Nästa sida";
+$ubbt_lang['NO_MATCH']  = 	"Inga träffar.";
+?>
