4c603fdf43091afd0bd9141ef7ed3c45b679a01e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:52:17 +0300

    Add Unsorted/ubs_adv/chatconfig.php

---
+++ Unsorted/ubs_adv/chatconfig.php
@@ -0,0 +1,51 @@
+<?php
+
+// Place your settings between the two speach marks like the examples
+
+$color1 = "#EFEFEF";
+$color2 = "#F5F5F5";
+$color3 = "#666666";
+$text = "#666666";
+
+// These are the text field colours
+
+$color4 = "#CCCCCC";
+$color5 = "#F5F5F5";
+$color6 = "#FFFFFF";
+
+// Link Colours
+
+$link1 = "#FF9900";
+$link2 = "#666666";
+$link3 = "#FF9900";
+
+// These are the colours for the post backgrounds and border
+
+$table1 = "#F5F5F5";
+$table2 = "#F7F7F7";
+$table_bdr = "#CCCCCC";
+
+// Time Adjust (enter 12 to -12 to alter the time displayed under the posters name)
+
+$time_a = "0";
+
+// Max Characters allowed to be posted in the message field
+
+$max_char = "300";
+
+// Swear filter
+
+$word1 = "fuck";
+	$censor1 = "f**k";
+$word2 = "shit";
+	$censor2 = "s**t";
+$word3 = "bastard";
+	$censor3 = "b*****d";
+$word4 = "ass";
+	$censor4 = "a$$";
+
+// Scroll (Yes or No)
+
+$scroll = "yes";
+
+?>
