5b98816818a1b4dadf05ad1c8c09152c506f207d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:43:58 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/sendprivate.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/sendprivate.php
@@ -0,0 +1,13 @@
+<?
+$ubbt_lang['NOPRIVS']   = "Private messages are disabled.";
+$ubbt_lang['MESS_COPY']  = 	"Zachowaj kopiê wiadomo¶ci";
+$ubbt_lang['MESS_RECEIPT']  = 	"Potwierdzenie przeczytania";
+$ubbt_lang['MESS_PREVIEW']  = 	"Podgl±d przed wys³aniem";
+$ubbt_lang['ADD_BOOK']  = 	"Dodaj do ksi±¿ki adresowej";
+$ubbt_lang['USE_BOOK']  = 	"Mo¿esz wybraæ adresata z ksi±¿ki adresowej";
+$ubbt_lang['CHOOSE_ADD']  = 	"Wybierz adresata";
+$ubbt_lang['NO_LOGGED']  = 	"¯eby wys³aæ wiadomo¶æ prywatn± musisz siê zalogowaæ.";
+$ubbt_lang['NO_PRIVATE']  = 	"Ten u¿ytkownik zablokowa³ przyjmowanie wiadomo¶ci prywatnych.";
+$ubbt_lang['PRIV_BODY2']  = 	"Wype³nij poni¿sze pola.  Adresat otrzyma Twoj± wiadomo¶æ przy nastêpnym zalogowaniu.";
+$ubbt_lang['TEXT_DELETE']  = 	"Skasuj";
+?>
