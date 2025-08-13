95cbbc42799859fb956b24119b360adea4082a63
me-suzy <me.suzana@gmail.com>
2025-08-14 00:13:11 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/admin/auctions.menu.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/admin/auctions.menu.php
@@ -0,0 +1,36 @@
+<?#//v.1.0.0
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+?>
+ 
+<TABLE WIDTH=100% CELLPADDING=2 CELLSPACING="0" BORDER="0">
+	<TR> 
+		<TD WIDTH="3%"><B></B></TD>
+		<TD WIDTH="97%"><B><FONT FACE="Tahoma, Verdana" SIZE="2"> 
+			<? print $MSG_386; ?>
+			</FONT></B></TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"> <FONT FACE="Tahoma, Verdana" SIZE="2"><IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./listauctions.php" CLASS="links"> 
+			<? print $MSG_067;?>
+			</A> </FONT></TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%">&nbsp;</TD>
+	</TR>
+	<? 
+											if($SETTINGS[newsletter] == 1){
+								?>
+	<?
+								}
+								else{
+								"";
+								}
+							 ?>
+</TABLE>
