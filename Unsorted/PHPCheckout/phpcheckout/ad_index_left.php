ed69baa26d9ff9469d43275566c13f2085ef353b
me-suzy <me.suzana@gmail.com>
2025-08-14 00:30:50 +0300

    Add Unsorted/PHPCheckout/phpcheckout/ad_index_left.php

---
+++ Unsorted/PHPCheckout/phpcheckout/ad_index_left.php
@@ -0,0 +1,17 @@
+<?php if(ORGANIZATION == "DreamRiver"):?>
+	<div class="required">
+	Easily create your own Yellow Pages AND get paid for listings
+	</div>
+	<a href="http://www.dreamriver.com/phpYellow/"><img src="user/pypbook.gif" width="150" height="22" border=0 alt="Go to the phpYellow Pages sample site" vspace=5></a><br>
+	<div class="note"><a href="showcase.php?productnumber=2">Premium Edition |</a> <a href="showcase.php?productnumber=1">Free Edition |</a> <a href="http://www.dreamriver.com/phpYellow/">Demo</a></div>
+<?php else:?>
+	<div align="left" class="required">
+		To edit this ad:
+		<ol>
+			<li>in an html editor open ad_index_left.php 
+			<li>modify this file
+			<li>save this file to web server
+		</ol>
+	</div>
+<?php endif;?>
+
