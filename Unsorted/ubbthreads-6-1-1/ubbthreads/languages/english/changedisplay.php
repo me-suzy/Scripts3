b7a1d45b5dbb18dd4a60e140eebbf4a6a90b1ed6
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:48 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"Invalid Posts per page.";
+$ubbt_lang['FLAT_BAD']  = 	"Flat Posts per page must be between 1 and 99.";
+$ubbt_lang['TEXTAREA_NO']  = 	"Invalid Textarea value.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Display preferences modified.";
+?>
