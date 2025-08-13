a840768bfa70f226cf7d08e3f17d491bf53a4dae
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:39 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/login.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/login.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['FORGOT_PASS']   = "Forget your password?";
+$ubbt_lang['FORGOT_ENTER']  = "Enter your email address or username that you reg
+istered with to have a temporary password mailed to you.";
+$ubbt_lang['REMEMBER_ME']   =  "Remember me on each visit.";
+$ubbt_lang['LOGIN_PROMPT']  = 	"Conectare";
+$ubbt_lang['LOGIN_PROMPT2']  = 	"Introduceti numele si parola pentru a va conecta. Daca nu aveti un cont, va puteti";
+$ubbt_lang['REG_ONE']  = 	"inregistreaza";
+$ubbt_lang['PASSWORD_TEXT']  = 	"Parola";
+?>
