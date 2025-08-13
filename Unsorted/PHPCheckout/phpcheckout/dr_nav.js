ea24f3e131b79ada9fc9b8fe53c1e4f95b310ccf
me-suzy <me.suzana@gmail.com>
2025-08-14 00:31:28 +0300

    Add Unsorted/PHPCheckout/phpcheckout/dr_nav.js

---
+++ Unsorted/PHPCheckout/phpcheckout/dr_nav.js
@@ -0,0 +1,46 @@
+<!-- This script and many more are available free online at -->
+<!-- The JavaScript Source!! http://javascript.internet.com -->
+<!-- Begin
+    <!--
+         // ID     parent ID
+    AddMenu("1"  ,  "1"   ,  "<b>Products</b>"       ,  ""  ,  ""  , "showcaseIndex.php");
+    AddMenu("2"  ,  "1"  ,   "Feature Product"     ,  ""  ,  ""  , "featureProduct.php");  
+    AddMenu("19"  ,  "1"  ,   "Purchase"     ,  ""  ,  ""  , "startBuy.php");
+	 AddMenu("3"  ,  "1"   ,  "phpYellow Pages >>"      ,  ""  ,  ""  , "");  
+		 AddMenu("4" ,  "3"   ,  "Premium Edition"    ,  ""  ,  ""  , "showcase.php?productnumber=2");  
+	    AddMenu("5",  "3"   ,  "Free Edition"   ,  ""  ,  ""  , "showcase.php?productnumber=1"); 
+	    AddMenu("101",  "3"   ,  "Sample Site"   ,  ""  ,  ""  , "http://www.dreamriver.com/phpYellow/"); 
+    AddMenu("6" ,  "1"   ,  "EasySQL"  ,  ""  ,  ""  , "showcase.php?productnumber=3");
+    AddMenu("7"  ,  "1"   ,  "Admin-Login-Only", "",  ""  , "showcase.php?productnumber=15");    
+    //AddMenu("8"  ,  "1"   ,  "Tell-A-Friend", "",  ""  , "showcase.php?productnumber=4"); 
+    AddMenu("9"  ,  "1"   ,  "phpcheckout >>", "",  ""  , ""); 
+    AddMenu("10"  ,  "9"   ,  "Pro Edition", "",  ""  , "showcase.php?productnumber=5");
+    AddMenu("11"  ,  "9"   ,  "Free Edition", "",  ""  , "showcase.php?productnumber=8");
+    AddMenu("12"  ,  "9"   ,  "Sample Site", "",  ""  , "sampleSite.php");
+
+    AddMenu("20" ,  "20"  ,   "<b>Services</b>"    ,  ""  ,  ""  , "services.php");   
+    AddMenu("21" ,  "20"  ,   "Custom Apps"    ,  ""  ,  ""  , "");
+    AddMenu("22" ,  "20"  ,   "Gateway Setup"    ,  ""  ,  ""  , "showcase.php?productnumber=12");
+    AddMenu("23" ,  "20"  ,   "Soft Installs"    ,  ""  ,  ""  , "showcase.php?productnumber=10");
+    AddMenu("24" ,  "20"  ,   "Refer"    ,  ""  ,  ""  , "");
+    AddMenu("25" ,  "20"  ,   "Get a Quote"    ,  ""  ,  ""  , "contact.php");
+
+
+    AddMenu("30" ,  "30" ,   "<b>Members</b>"       ,  ""  ,  ""  , "members.php");  
+    AddMenu("31" ,  "30" ,   "Register"      ,  ""  ,  ""  , "register.php"); 	 
+    AddMenu("32" ,  "30" ,   "Login"         ,  ""  ,  ""  , "login.php"); 
+    AddMenu("33" ,  "30" ,   "Newsletter >>"      ,  ""  ,  ""  , "newsletter.php");
+    AddMenu("34" ,  "33" ,   "Subscribe"      ,  ""  ,  ""  , "newsSubscribe.php?goal=Subscribe");
+    AddMenu("35" ,  "33" ,   "Unsubscribe"      ,  ""  ,  ""  , "newsUnsubscribe.php?goal=Unsubscribe");
+    AddMenu("36" ,  "30" ,   "Survey >>"      ,  ""  ,  ""  , "survey.php");
+	    AddMenu("37" ,  "36" ,   "Take"      ,  ""  ,  ""  , "surveyTake.php");
+	    AddMenu("38" ,  "36" ,   "Results"      ,  ""  ,  ""  , "surveyResults.php");
+    AddMenu("39" ,  "30" ,   "Link To Us"      ,  ""  ,  ""  , "link2us.php");
+		//AddMenu("38" ,  "30" ,   "PHP Webring"      ,  ""  ,  ""  , "webring.php");
+
+
+    AddMenu("40" ,  "40" ,   "<b>Company</b>"    ,  ""  ,  ""  , "company.php");  	 
+    AddMenu("41" ,  "40" ,   "Contact"      ,  ""  ,  ""  , "contact.php");	 
+    AddMenu("42" ,  "40" ,   "Privacy Policy"      ,  ""  ,  ""  , "privacyPolicy.php");	 
+    AddMenu("43" ,  "40" ,   "Shipping Policy"      ,  ""  ,  ""  , "shipping.php");
+	 Build();
