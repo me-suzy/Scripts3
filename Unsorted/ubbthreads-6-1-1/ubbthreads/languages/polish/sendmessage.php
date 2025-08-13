e5a520e27e95e2ad1c95d83b05789dc59dc126a9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:43:56 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/sendmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/sendmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['CHOOSE_ADD']  = 	"Wybierz adresata";
+$ubbt_lang['TEXT_DELETE']  = 	"Skasuj";
+$ubbt_lang['NO_PRIVATE']  = 	"Ten u¿ytkownik zablokowa³ przyjmowanie wiadomo¶ci prywatnych.";
+$ubbt_lang['NO_RECORD']  = 	"Podany przez Ciebie adresat nie istnieje.";
+$ubbt_lang['MESS_PREV']  = 	"Podgl±d wiadomo¶ci do ";
+$ubbt_lang['PREVIEW_MESS']  = 	"Poni¿ej jest podgl±d Twojej wiadomo¶ci. Wci¶nij 'Wprowadz' lub popraw poni¿ej.";
+$ubbt_lang['EDIT_MORE']  = 	"Popraw";
+$ubbt_lang['PRIV_SUB']  = 	"Masz nowe wiadomo¶ci";
+$ubbt_lang['PRIV_BODY_R']  = 	"Otrzymale¶ prywatn± wiadomo¶æ na forum . Adres forum to {$config['phpurl']}/login.php";
+$ubbt_lang['MESS_SENT']  = 	"Wiadomo¶æ zosta³a wys³ana.";
+?>
