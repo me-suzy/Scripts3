d74cc03e966865d6c16d594112bf02da7a945db9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:25 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/addfav.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/addfav.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  =  "You do not have permission to view this thread so you cannot add it to your favorites/reminders list.";
+$ubbt_lang['MAIN_ONLY']  =  "You can only add main topics to your favorites or reminder lists.";
+$ubbt_lang['DUP_ENTRY']  =  "This has already been added to your favorites/reminder list.";
+$ubbt_lang['ENTRY_IN']  =  "Post/Thread added";
+$ubbt_lang['FAV_CONFIRM']  =  "The selected post/thread has been added to your list.  You will see this entry in the 'My Home' until you remove it if you added it as a Favorite.  If you added it as a Reminder it will go away once you make a reply to the post.";
+$ubbt_lang['FORUM_RETURN']  =  "Return to forum now.";
+?>
