ee95f2de9e7141600cf8738e7f2b0a97a3e12e92
me-suzy <me.suzana@gmail.com>
2025-08-14 02:23:43 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/admin/deletecat.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/admin/deletecat.php
@@ -0,0 +1,67 @@
+<?
+/*
+# UBB.threads, Version 6
+# Official Release Date for UBB.threads Version6: 06/05/2002
+
+# First version of UBB.threads created July 30, 1996 (by Rick Baker).
+# This entire program is copyright Infopop Corporation, 2002.
+# For more info on the UBB.threads and other Infopop
+# Products/Services, visit: http://www.infopop.com
+
+# Program Author: Rick Baker.
+
+# You may not distribute this program in any manner, modified or otherwise,
+# without the express, written written consent from Infopop Corporation.
+
+# Note: if you modify ANY code within UBB.threads, we at Infopop Corporation
+# cannot offer you support-- thus modify at your own peril :)
+# ---------------------------------------------------------------------------
+*/
+
+// Require the library
+   require ("../main.inc.php");
+
+// -----------------
+// Get the user info
+   $userob = new user;
+   $user = $userob -> authenticate();
+   $html = new html;
+
+// -----------------------------
+// Make sure they should be here
+   if ($user['U_Status'] != 'Administrator'){
+      $html -> not_right ("You must be logged in, and be a valid administrator to access this.",$Cat);
+   }
+
+// ----------------------------------------------------
+// Let's make sure there are no boards in this category
+   $query = "
+    SELECT * FROM {$config['tbprefix']}Boards
+    WHERE Bo_Cat = '$Number'
+   ";
+   $sth = $dbh -> do_query($query);
+   list ($check) = $dbh -> fetch_array($sth);
+   $dbh -> finish_sth($sth);
+
+   if ($check) {
+      $html -> not_right("There are forums in this category, so you cannot delete it until you move them to a different category.",$Cat);
+   }
+
+// --------------------------------------
+// Delete the category from the category table 
+   $query = "
+    DELETE FROM {$config['tbprefix']}Category 
+    WHERE Cat_Number = '$Number'
+   ";
+   $dbh -> do_query($query);
+
+// ------------------------
+// Send them a confirmation
+   $html -> send_header ("The category has been deleted.",$Cat,"<META HTTP-EQUIV=\"Refresh\" content=\"5;url={$config['phpurl']}/admin/main.php?Cat=$Cat\">",$user);
+   $html -> admin_table_header("The category has been deleted.");
+   echo "<TABLE BORDER=0 WIDTH=\"95%\" ALIGN=\"center\"><TR><TD class=\"cleartable\"><span class=\"onbody\">";  
+   echo "The category has been deleted.";
+   echo "</span></TD></TR></TABLE>";
+   $html -> send_admin_footer();
+
+?>
