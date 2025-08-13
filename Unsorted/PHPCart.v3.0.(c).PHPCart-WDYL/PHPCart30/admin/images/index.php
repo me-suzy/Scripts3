68b3fa6519bc9431e864270ec45558eec60e98da
me-suzy <me.suzana@gmail.com>
2025-08-14 00:28:56 +0300

    Add Unsorted/PHPCart.v3.0.(c).PHPCart-WDYL/PHPCart30/admin/images/index.php

---
+++ Unsorted/PHPCart.v3.0.(c).PHPCart-WDYL/PHPCart30/admin/images/index.php
@@ -0,0 +1,107 @@
+<html>
+<head>
+<title>Welcome to......</title>
+</head>
+
+<body topmargin="0" leftmargin="0">
+
+<div align="center">
+  <center>
+  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr>
+      <td width="50%"><img border="0" src="top1.gif" width="298" height="66"><img border="0" src="top2.jpg" width="338" height="66"></td>
+      <td width="50%" background="top3.gif">
+        <p align="center"><font face="Verdana" size="1">Logged in:<br>
+        <b><i>
+$PHP_AUTH_USER</i></b></font><b><font size="2" face="Verdana"><br>
+        <br>
+        Settings</font></b></p>
+      </td>
+    </tr>
+    <tr>
+      <td width="100%" colspan="2">
+        <div align="center">
+          <table border="0" cellpadding="0" cellspacing="0" width="85%">
+            <tr>
+              <td width="100%">
+                <hr>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </td>
+    </tr>
+  </table>
+  </center>
+</div>
+
+<div align="center">
+  <center>
+  <table border="0" cellpadding="0" width="95%" bgcolor="#808080" cellspacing="1" height="100">
+    <tr>
+      <td width="100%">
+        <div align="center">
+          <center>
+          <table border="0" cellpadding="0" cellspacing="0" width="100%" height="100">
+            <tr>
+              <td width="100%" bgcolor="#FFFFFF">
+                <div align="center">
+                  <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                    <tr>
+                      <td width="100%">
+                        <p align="center"><font size="2" face="Verdana"><b> PHPCart Admin Interface!</b></font></td>
+                    </tr>
+                  </table>
+                </div>
+                <p align="center"><br>
+                <br>
+                <br>
+                <font size="2" face="Verdana"><b>NOTHING TO SEE HERE</b></font>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+              </td>
+            </tr>
+          </table>
+          </center>
+        </div>
+      </td>
+    </tr>
+  </table>
+  </center>
+</div>
+
+<div align="center">
+  <center>
+  <table border="0" cellpadding="0" width="80%">
+    <tr>
+      <td width="100%" bgcolor="#C0C0C0">
+        <div align="center">
+          <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <tr>
+              <td width="100%" bgcolor="#FFFFFF">&nbsp;</td>
+            </tr>
+          </table>
+        </div>
+      </td>
+    </tr>
+  </table>
+  </center>
+</div>
+
+</body>
+
+</html>
