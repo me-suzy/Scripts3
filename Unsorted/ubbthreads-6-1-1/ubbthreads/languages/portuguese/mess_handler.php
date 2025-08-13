045d38ca681a90487671de52df49047fc8a577de
me-suzy <me.suzana@gmail.com>
2025-08-14 02:45:11 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/mess_handler.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/mess_handler.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['REPLY_HEAD']   =   "Use this form to send a private message to this user";
+$ubbt_lang['MESS_COPY']  =     "I want to keep a copy of this message";
+$ubbt_lang['DELETE_MESS']  = 	"Apagar esta mensagem";
+$ubbt_lang['REPLY_MESS']  = 	"Responder a esta mensagem";
+$ubbt_lang['RETURN_START']  = 	"Retornar a página inicial";
+$ubbt_lang['MESS_RECEIPT']  = 	"I want to receive a read receipt";
+$ubbt_lang['MESS_PREVIEW']  = 	"I want to preview my message";
+$ubbt_lang['ORIG_MESS']  = 	"Mensagem Original";
+?>
