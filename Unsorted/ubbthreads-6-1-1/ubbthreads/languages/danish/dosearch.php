14439000a374bbb6f07caa0cd06c70877a7d5e6f
me-suzy <me.suzana@gmail.com>
2025-08-14 02:31:00 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/dosearch.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['NO_MORE']   =  "There are no more search results in this direction.";
+$ubbt_lang['NO_WORDS']  = 	"Der skal indtastes et eller flere søgeord, før søgningen kan begynde.";
+$ubbt_lang['SEARCH_RES']  = 	"Resultatet af søgningen";
+$ubbt_lang['PREV_PAGE']  = 	"Forrige side";
+$ubbt_lang['MAIN_SEARCH']  = 	"Søgesiden";
+$ubbt_lang['NEXT_PAGE']  = 	"Næste side";
+$ubbt_lang['NO_MATCH']  = 	"Søgningen gav intet resultat.";
+?>
