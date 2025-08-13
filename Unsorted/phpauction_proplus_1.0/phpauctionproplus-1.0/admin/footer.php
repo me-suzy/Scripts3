c74be3a8c6deed4defc322c4fd90ccc47578d82c
me-suzy <me.suzana@gmail.com>
2025-08-14 00:13:59 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/admin/footer.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/admin/footer.php
@@ -0,0 +1,24 @@
+<?#//v.1.0.0
+
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+
+?>
+
+<TABLE WIDTH="740" BORDER=0 ALIGN=LEFT>
+<TR><TD>
+<CENTER>
+				
+				<FONT FACE=Verdana,Arial SIZE=1 COLOR="#999999">
+				<? print $MSG_260; ?>
+				</FONT>
+</CENTER>
+</TD></TR>
+</TABLE>
+
+</BODY>
+</HTML>
