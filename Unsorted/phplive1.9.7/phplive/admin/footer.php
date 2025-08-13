be140f62ba8ec5cacafc77e2914de1c4835a9fb6
me-suzy <me.suzana@gmail.com>
2025-08-14 00:46:58 +0300

    Add Unsorted/phplive1.9.7/phplive/admin/footer.php

---
+++ Unsorted/phplive1.9.7/phplive/admin/footer.php
@@ -0,0 +1,10 @@
+<br>
+<table cellspacing=1 cellpadding=2 border=0 width="90%">
+<tr bgColor="#C4C4E1">
+	<td align="center">
+	<!-- DO NOT REMOVE THE COPYRIGHT NOTICE OF "&nbsp; OSI Codes Inc." -->
+	<span class="smalltxt"><? echo $LANG['DEFAULT_BRANDING'] ?> &copy; OSI Codes Inc.</span>
+	</td>
+</tr>
+</table>
+<br>
