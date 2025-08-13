4a18b71bfbe259ca3b5cb57fe54d3c6f91c2aa04
me-suzy <me.suzana@gmail.com>
2025-08-14 00:44:00 +0300

    Add Unsorted/phpListPro_1.80nullified/phpListPro-1.80n/lang_russian.php

---
+++ Unsorted/phpListPro_1.80nullified/phpListPro-1.80n/lang_russian.php
@@ -0,0 +1,928 @@
+<?
+#################################################################################################
+#
+#  project           	: phpListPro
+#  filename          	: lang_russian.php
+#  version           	: 1.71
+#  last modified by  	: Eugene Kravtsov
+#  purpose           	: Language File
+#  last modified     	: 07/21/2001
+#
+#################################################################################################
+
+#  Category Settings
+
+
+#################################################################################################
+
+
+
+
+
+$category[0]            = "All Entries";                // must be all Entries
+
+
+$category[1]            = "Category 1";
+
+
+$category[2]            = "Category 2";
+
+
+$category[3]            = "Category 3";
+
+
+$category[4]            = "Category 4";
+
+
+$category[5]            = "Category 5";
+
+
+$category[6]            = "Category 6";
+
+
+
+
+
+
+
+
+#  Language Settings
+
+
+#################################################################################################
+
+
+
+
+
+$lang[metatag]        	= "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1251\">";
+
+
+
+
+
+$lang[rank] 		= "#";
+
+
+$lang[site] 		= "Ñàéòû";
+
+
+$lang[votes]		= "In";
+
+
+$lang[hits] 		= "Out";
+
+
+$lang[rate] 		= "Ðåéòèíã";
+
+
+$lang[random] 		= "Ñëó÷àéíàÿ ññûëêà";
+
+
+
+
+
+$lang[in_msg1]		= "Òåïåðü âû çàïîëíÿåòå ôîðìó";
+
+
+$lang[in_msg2]		= "Ïîæàëóéñòà, íàæìèòå êíîïêó [Ãîëîñîâàòü çà ýòîò ñàéò], ÷òîáû ïðîãîëîñîâàòü çà ýòîò ñàéò.";
+
+
+$lang[in_msg3]		= "Ââåäèòå $list_name áåç ãîëîñîâàíèÿ.";
+
+
+$lang[in_chose]		= "Ïîæàëóéñòà, âûáåðèòå âàø ðåéòèíã";
+
+
+$lang[in_vote]		= "Ãîëîñîâàòü çà ýòîò ñàéò";
+
+
+
+
+
+$lang[anti_cheat_msg] 	= "<center><font color=red>Âû óæå ïðîãîëîñîâàëè â òå÷åíèè ïîñëåäíèõ $vote_timeout ÷àñîâ! Âàø ãîëîñ íå áûë çàñ÷èòàí.</font></center>";
+
+
+$lang[vote_log_msg] 	= "<center><font color=red>Ñïàñèáî! Âàø ãîëîñ áûë çàñ÷èòàí!</font></center>";
+
+
+$lang[not_found_msg] 	= "<center><font color=red>ÎØÈÁÊÀ: Ñàéò ÍÅ íàéäåí !!!</font></center>";
+
+
+$lang[tilt_msg] 		= "<center><font color=red>Ýòîò ñàéò âðåìåííî íåâèäèì ïîñëå îáíàðóæåíèÿ íàêðóòêè! Ïîïðîáóéòå ïîçæå!</font></center>";
+
+
+$lang[nocookie_msg] 	= "<center><font color=red>Âàø áðàóçåð ÍÅ ïîääåðæèâàåò cookie! Âàø ãîëîñ íå áûë çàñ÷èòàí.</font></center>";
+
+
+$lang[ban_msg] 		= "<center><font color=red>Âàì ÇÀÏÐÅÙÅÍÎ ãîëîñîâàòü â ýòîì ñïèñêå. Âàø ãîëîñ íå áûë çàñ÷èòàí..</font></center>";
+
+
+
+
+
+$lang[emailconfirmed]	= "E-mail ïîäòâåðæä¸í óñïåøíî!";
+
+
+$lang[emailnotconf]		= "E-mail ÍÅ ïîäòâåðæä¸í!!! ÏÎÆÀËÓÉÑÒÀ, ñâÿæèòåñü ñ Âåáìàñòåðîì.";
+
+
+$lang[redirect]		= "Ðåäèðåêò ê ñïèñêó. Ïîæàëóéñòà, ïîäîæäèòå ...";
+
+
+$lang[confirmation]		= "Ïîäòâåðæäåíèå";
+
+
+
+
+
+$lang[pages]		= "Ñòðàíèöû:";
+
+
+$lang[useronline]		= "ïîëüçîâàòåëü îí-ëàéí";
+
+
+$lang[usersonline]		= "ïîëüçîâàòåëåé îí-ëàéí";
+
+
+$lang[userssatisfied]	= "ïîëüçîâàòåëåé óäîâëåòâîðåíî";
+
+
+$lang[sitesindb]		= "ñàéòîâ â íàøåé áàçå äàííûõ";
+
+
+$lang[inoutresets]		= "In/Out îáíîâëÿþòñÿ êàæäûå";
+
+
+$lang[days]		= "äíåé";
+
+
+$lang[next]		= "ñëåäóþùåå";
+
+
+
+
+
+$lang[addsite]		= "Äîáàâèòü ñàéò";
+
+
+$lang[addsitedesc]		= "Äîáàâèòü íîâûé ñàéò";
+
+
+$lang[editsite]		= "Ðåäàêòèðîâàòü ñàéò";
+
+
+$lang[editsitedesc]		= "Ðåäàêòèðîâàòü ñóùåñòâóþùèé ñàéò";
+
+
+$lang[helpsite]		= "Ïîìîùü";
+
+
+$lang[helpsitedesc]		= "Ïîìîùü ïî âñòàâêå HTML";
+
+
+
+
+
+$lang[error]		= "Îøèáêà! Ïîæàëóéñòà, âåðíèòåñü íàçàä è èñïðàâòå èõ!";
+
+
+$lang[error_duplicate]	= "Íàéäåí äóáëèðóþùèé àäðåñ ñàéòà!";
+
+
+$lang[error_emptysiten]	= "Âû íå ââåëè èìÿ âàøåãî ñàéòà";
+
+
+$lang[error_emptysitea]	= "Âû íå ââåëè àäðåñ âàøåãî ñàéòà";
+
+
+$lang[error_emptypass]	= "Âû íå ââåëè ïàðîëü";
+
+
+$lang[error_emptyemail]	= "Âû íå ââåëè âàø e-mail àäðåñ";
+
+
+$lang[error_emptyname]	= "Âû íå ââåëè âàøå èìÿ";
+
+
+$lang[error_emptydesc]	= "Âû íå ââåëè îïèñàíèå âàøåãî ñàéòà";
+
+
+$lang[error_bannerw]	= "Âû íå ââåëè äåéñòâèòåëüíóþ øèðèíó áàííåðà";
+
+
+$lang[error_bannerh]	= "Âû íå ââåëè äåéñòâèòåëüíóþ âûñîòó áàííåðà";
+
+
+$lang[error_banners]	= "Ðàçìåð ôàéëà âàøåãî áàííåðà ïðåâûøàåò ëèìèò";
+
+
+$lang[error_bannernok]	= "URL âàøåãî áàííåðà íå äåéñòâèòåëåí.";
+
+
+$lang[error_baddesc]	= "Ïëîõîå ñëîâî îáíàðóæåíî â îïèñàíèè âàøåãî ñàéòà";
+
+
+$lang[error_badname]	= "Ïëîõîå ñëîâî îáíàðóæåíî â èìåíè âàøåãî ñàéòà";
+
+
+
+
+
+$lang[back]		= "Íàçàä";
+
+
+$lang[done]		= "Ãîòîâî";
+
+
+$lang[submit]		= "Ïðèíÿòü";
+
+
+$lang[search]           = "Search";
+
+
+$lang[getlost]		= "Çàáûëè ID èëè ïàðîëü?";
+
+
+$lang[getid]		= "Ïîëó÷èòü ID/ïàðîëü";
+
+
+$lang[editsite]		= "Ðåäàêòèðîâàòü ñàéò";
+
+
+
+
+
+$lang[enterdata]		= "Ââåäèòå âàøè äàííûå";
+
+
+$lang[password]		= "Ïàðîëü: ";
+
+
+$lang[siteid]		= "ID ñàéòà: ";
+
+
+$lang[emailaddr]		= "e-mail àäðåñ: ";
+
+
+$lang[yourname]		= "Âàøå èìÿ: ";
+
+
+$lang[sitetitle]		= "Íàçâàíèå ñàéòà: ";
+
+
+$lang[siteurl]		= "URL càéòà: ";
+
+
+$lang[sitedesc]		= "Îïèñàíèå càéòà: ";
+
+
+$lang[bannerurl]		= "URL áàííåðà: ";
+
+
+$lang[bannerw]		= "Øèðèíà áàííåðà: ";
+
+
+$lang[bannerh]		= "Âûñîòà áàííåðà: ";
+
+
+
+
+
+$lang[getlost2]		= "Ïîëó÷èòü ïîòåðÿííûå ID è ïàðîëü ñàéòà";
+
+
+$lang[edityoursite]		= "Ðåäàêòèðîâàòü âàø ñàéò";
+
+
+
+
+
+$lang[getidok]		= "Ñàéò íàéäåí è èíôîðìàöèÿ áûëà îòîñëàíà!";
+
+
+$lang[getidnok]		= "Íå íàéäåí ñàéò ñ ýòèì å-mail àäðåñîì!";
+
+
+$lang[editidnok]		= "Íå íàéäåí ñàéò ñ ýòèìè äàííûìè!";
+
+
+$lang[editsuccess]		= "Ñàéò îáíîâë¸í óñïåøíî!";
+
+
+
+
+
+$lang[addthanks]		= "Ñïàñèáî! Âàø ñàéò áûë äîáàâëåí!";
+
+
+$lang[addloginid]		= "Âàø ID äëÿ âõîäà: ";
+
+
+$lang[addsendmail]		= ", ìû ïîñëàëè ïîäòâåðæäåíèå íà âàø e-mail àäðåñ.";
+
+
+$lang[addhint]		= "Âàì ïîíàäîáèòñÿ ýòîò ID è âàø ïàðîëü, ÷òîáû ðåäàêòèðîâàòü âàøè äàííûå.";
+
+
+$lang[addapproval]		= "Ïîæàëóéñòà, áóäòå òåðïåëèâû, Âåáìàñòåð ïðîâåðèò âàøè äàííûå êàê ìîæíî ñêîðåå.";
+
+
+$lang[addplease]		= "Ïîæàëóéñòà, äîáàâüòå îäèí (èëè áîëüøå) èç ñëåäóþùèõ êîäîâ HTML íà âàø ñàéò, òàê ÷òî ïîñåòèòåëè ìîãóò ãîëîñîâàòü çà âàñ!";
+
+
+
+
+
+$lang[help]		= "ÏÎÌÎÙÜ";
+
+
+$lang[helpreplace1]		= "Çàìåíèòå <b>XXXXXXXX</b> âàøèì ID, êîòîðûé ìû ïîñëàëè íà âàø e-mail àäðåñ.";
+
+
+$lang[helpreplace2]		= "Çàìåíèòå <b>www.yourdomain.com</b>  URL âàøåãî áàííåðà äëÿ ãîëîñîâàíèÿ.";
+
+
+
+
+
+$lang[addrules]		= "Ïðàâèëà äëÿ ðåãèñòðàöèè";
+
+
+$lang[addyoursite]		= "Äîáàâèòü âàø ñàéò";
+
+
+
+
+
+$lang[rules]		= "<ul>
+
+
+			    <li>Ðåéòèíã îñíîâàí òîëüêî íà ÂÕÎÄßÙÈÕ õèòàõ
+
+
+			    <li>Ñêðèïòû ÇÀÏÐÅÙÅÍÛ äëÿ âõîäÿùèõ õèòîâ
+
+
+			    <li>Òîëüêî ËÓ×ØÈÅ 10 ñàéòîâ îáîçíà÷åíû áàííåðàìè
+
+
+			    <li>Ñ÷¸ò÷èê áóäåò îáíîâëÿòüñÿ ïåðèîäè÷åñêè
+
+
+			    <li>!!! Íàêðóòêà ÇÀÏÐÅÙÅÍÀ !!!
+
+
+			   </ul>";
+
+
+
+
+
+
+
+
+#  Help Language Forms
+
+
+#################################################################################################
+
+
+
+
+
+function help($id) {
+
+
+    global $list_name,$list_url,$voteimage_1,$voteimage_2,$voteimage_3;
+
+
+
+
+
+echo "<hr><b><li>TextLink:</b><br>";
+
+
+echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt$list_name&lt/a&gt\n";
+
+
+
+
+
+
+
+
+if ($voteimage_1) {
+
+
+  echo "<p><hr><b><li>PictureLink:</b><br>\n";
+
+
+  echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt<br>&ltimg src=\"http://www.yourdomain.com/vote_image1.gif\" alt=\"Ââåäèòå $list_name è Ãîëîñóéòå çà ýòîò ñàéò !!!\" border=0&gt&lt/a&gt\n";
+
+
+  echo "<p><center><img src=\"$voteimage_1\"><br>Ýòî áàííåð. Ïîæàëóéñòà, ñîõðàíèòå åãî íà âàøåì ñåðâåðå..<br>(Ùåë÷îê ïðàâîé êíîïêîé ìûøè - Save as ...)</center><br>\n";
+
+
+  }
+
+
+
+
+
+if ($voteimage_2) {
+
+
+  echo "<p><hr><b><li>PictureLink 2:</b><br>\n";
+
+
+  echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt<br>&ltimg src=\"http://www.yourdomain.com/vote_image2.gif\" alt=\"Ââåäèòå $list_name è Ãîëîñóéòå çà ýòîò ñàéò !!!\" border=0&gt&lt/a&gt\n";
+
+
+  echo "<p><center><img src=\"$voteimage_2\"><br>Ýòî áàííåð. Ïîæàëóéñòà, ñîõðàíèòå åãî íà âàøåì ñåðâåðå..<br>(Ùåë÷îê ïðàâîé êíîïêîé ìûøè - Save as ...</center><br>\n";
+
+
+  }
+
+
+
+
+
+if ($voteimage_3) {
+
+
+  echo "<p><hr><b><li>PictureLink 3:</b><br>\n";
+
+
+  echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt<br>&ltimg src=\"http://www.yourdomain.com/vote_image3.gif\" alt=\"Ââåäèòå $list_name è Ãîëîñóéòå çà ýòîò ñàéò !!!\" border=0&gt&lt/a&gt\n";
+
+
+  echo "<p><center><img src=\"$voteimage_3\"><br>Ýòî áàííåð. Ïîæàëóéñòà, ñîõðàíèòå åãî íà âàøåì ñåðâåðå..<br>(Ùåë÷îê ïðàâîé êíîïêîé ìûøè - Save as ...)</center><br>\n";
+
+
+  echo "<p>";
+
+
+  }
+
+
+}
+
+
+
+
+
+#  Mail Language Forms
+
+
+#################################################################################################
+
+
+
+
+
+function mail_getidpwd($result) {		// Mail Form for sending lost ID/Password
+
+
+    global $list_name,$list_url,$admin_email;
+
+
+
+
+
+mail("$result[email]", "Çàïðîñ äàííûõ äëÿ âõîäà èç $list_name", "Çäðàâñòâóéòå $result[name],
+
+
+ýòî èíôîðìàöèÿ âàøåãî ñàéòà èç $list_name.
+
+
+
+
+
+Âàø ñàéò:
+
+
+$result[site_name]
+
+
+$result[site_addr]
+
+
+
+
+
+Íå çàáóäüòå!
+
+
+ID ñàéòà: $result[id]
+
+
+Ïàðîëü: $result[password]
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+Êîä áàííåðà äëÿ ãîëîñîâàíèÿ:
+
+
+
+
+
+<a href=\"$list_url/in.php?site=$result[id]\" target=\"_blank\"><img src=\"www.yourdomain.com/vote_image1.gif\" border=\"0\"></a>
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_newnotifyuser($email_address,$webmaster_name,$id,$password,$site_name,$site_address,$site_description) {
+
+
+    global $list_name,$list_url,$admin_email,$secret,$webmasterapproval,$emailapproval;
+
+
+
+
+
+if ($webmasterapproval) {$webmastermsg="\nÏîæàëóéñòà, áóäòå òåðïåëèâû, Âåáìàñòåð ïðîâåðèò âàøè äàííûå êàê ìîæíî ñêîðåå.\n";} else {$webmastermsg="";}
+
+
+if ($emailapproval) {$emailmsg="\nÏîæàëóéñòà, íàæìèòå íà ýòó ññûëêó, ÷òîáû ïîäòâåðäèòü âàøå ó÷àñòèå:\n$list_url/confirm.php?check=".urlencode(encrypt($id,$secret))."\n\n";} else {$emailmsg="";}
+
+
+
+
+
+mail("$email_address", "$webmaster_name, äîáðî ïîæàëîâàòü â $list_name", "$webmaster_name, ñïàñèáî çà ó÷àñòèå â $list_name.
+
+
+$webmastermsg$emailmsg
+
+
+Âàø ñàéò:
+
+
+$site_name ($site_address)
+
+
+
+
+
+Íå çàáóäüòå!
+
+
+ID ñàéòà: $id
+
+
+Ïàðîëü: $password
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+Êîä áàííåðà äëÿ ãîëîñîâàíèÿ:
+
+
+
+
+
+<a href=\"$list_url/in.php?site=$id\" target=\"_blank\"><img src=\"http://www.yourdomain.com/$voteimage_1\" alt=\"Enter $list_name and Vote for this Site !!!\" border=0></a>
+
+
+
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_newnotifyadm($email_address,$webmaster_name,$id,$password,$site_name,$site_address,$site_description) {
+
+
+    global $list_name,$list_url,$admin_email,$notify_email;
+
+
+
+
+
+mail("$notify_email", "ÍÎÒÈÔÈÊÀÖÈß $webmaster_name çàðåãèñòðèðîâàëñÿ íà $list_name", "$webmaster_name joined the $list_name.
+
+
+
+
+
+Êîä ñàéòà: $id
+
+
+Ïàðîëü: $password
+
+
+Èìÿ ñàéòà: $site_name
+
+
+Àäðåñ ñàéòà: $site_address
+
+
+
+
+
+E-mail: $email_address
+
+
+
+
+
+Àäðåñ áàííåðà (åñëè åñòü): $banner_address
+
+
+
+
+
+Îïèñàíèå:
+
+
+$site_description
+
+
+
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_editnotifyadm($email_address,$webmaster_name,$id,$password,$site_name,$site_address,$site_description) {
+
+
+    global $list_name,$list_url,$admin_email,$notify_email;
+
+
+
+
+
+mail("$notify_email", "ÍÎÒÈÔÈÊÀÖÈß $webmaster_name ðåäàêòèðîâàë íà $list_name", "$webmaster_name edited on $list_name.
+
+
+
+
+
+Êîä ñàéòà: $id
+
+
+Ïàðîëü: $password
+
+
+Èìÿ ñàéòà: $site_name
+
+
+Àäðåñ ñàéòà: $site_address
+
+
+
+
+
+E-mail: $email_address
+
+
+
+
+
+Àäðåñ áàííåðà (åñëè åñòü): $banner_address
+
+
+
+
+
+Îïèñàíèå:
+
+
+$site_description
+
+
+
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_tiltuser($result) {		// Mail Form for sending User on TILT
+
+
+    global $list_name,$list_url,$admin_email,$tilt_insp_timeout;
+
+
+
+
+
+mail("$result[email]", "ÂÍÈÌÀÍÈÅ Äåòåêòîð Íàêðóòêè äëÿ $result[site_name] íà $list_name",
+
+
+"Ïðèâåò Âåáìàñòåð,
+
+
+
+
+
+íàø Äåòåêòîð Íàêðóòêè íà $list_name îáíàðóæèë ñòðàííûå ãîëîñà çà âàø ñàéò
+
+
+
+
+
+ID: $result[id]
+
+
+$result[site_name] ($result[site_addr])
+
+
+
+
+
+Òàêèì îáðàçîì, âàø ñàéò íåâèäèì íà $list_name ($list_url) íà $tilt_insp_timeout ìèíóò.
+
+
+Íîâûå ïîäîçðèòåëüíûå ãîëîñà â òå÷åíèè ýòîãî âðåìåíè óâåëè÷èâàþò òàéìàóò !
+
+
+
+
+
+Èñêðåííå
+
+
+
+
+
+Âåáìàñòåð
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_tiltadmin($result) {		// Mail Form for sending lost ID/Password
+
+
+    global $list_name,$list_url,$admin_email,$notify_email,$tilt_insp_timeout;
+
+
+
+
+
+mail("$notify_email",  "ÍÎÒÈÔÈÊÀÖÈß Äåòåêòîð íàêðóòêè äëÿ $result[site_name] íà $list_name",
+
+
+"Çäðàâñòâóéòå Ìàñòåð,
+
+
+
+
+
+Äåòåêòîð Íàêðóòêè íà $list_name îáíàðóæèë ñòðàííûå ãîëîñà çà ýòîò ñàéò.
+
+
+
+
+
+ID: $result[id]
+
+
+$result[site_name] ($result[site_addr])
+
+
+
+
+
+Òàêèì îáðàçîì, ýòîò ñàéò íåâèäèì íà $list_name ($list_url) íà $tilt_insp_timeout ìèíóò.
+
+
+
+
+
+Cheating-Detection (Tilt-Protection)
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+#  End Language
+
+
+#################################################################################################
+
+
+?>
