a2abafd1b1c921b462170a08e71a42f5d70a556e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:07:49 +0300

    Add Unsorted/topstory10/inc/header.php

---
+++ Unsorted/topstory10/inc/header.php
@@ -0,0 +1,14 @@
+<!doctype html public "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title> .: Top Story - Basic - :. </title>
+<meta name="Generator" content="EditPlus">
+<meta name="Author" content="">
+<meta name="Keywords" content="">
+<meta name="Description" content="">
+<link rel="stylesheet" type="text/css" href="<? echo $tst["url"] ?>/lib/topStory.css">
+</head>
+<body bgcolor="#FFFFFF">
+<table class="text" align="center" width="600" cellpadding="3" cellspacing="1">
+<tr>
+	<td height="300" valign="top">
