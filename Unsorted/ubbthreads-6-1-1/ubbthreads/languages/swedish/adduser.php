be3dad815e3e4d2dce16a72d33fb557fdbb1f725
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:29 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =    "Innan du kan logga in måste ditt användarkonto godkännas av en Administratör. Du kommer att meddelas via e-post när detta har skett.";
+$ubbt_lang['APPROVED'] = "Ditt användarkonto för {$config['title']} på {$config['phpurl']} har godkänts.";
+$ubbt_lang['NOTAPPROVED'] = "Ditt användarkonto för {$config['title']} på {$config['phpurl']} godkändes inte. Orsaken för detta är:";
+$ubbt_lang['NEWREGBODY'] = "En ny användare har registrerat sig på {$config['title']}. För att godkänna detta konto, gå till";
+$ubbt_lang['NEWREG'] = "Ny användarregistration på";
+$ubbt_lang['INVALID_DATE'] = "Du valde ett felaktigt datum.";
+$ubbt_lang['TOOYOUNG']  =       "För att kunna registrera dig på detta forum måste du ha din målsmans godkännande. För att göra detta kan du använda detta <a href=\"{$config['phpurl']}/coppaform.php\">formulär</a>.";
+$ubbt_lang['UNDER13'] = "Tyvärr så tillåter vi inte barn under 13 år att registrera användarkonton.";
+$ubbt_lang['BAD_UNAME']  = 	"Du kan bara ha alfabetiska tecken i ditt användarnamn och tyvärr ännu inte ÅÄÖ.  Specialtecken tillåts inte så att det inte ska vara möjligt att fejka andras användarnamn.  Du kan däremot använda \"_\"-ecknet som ersättning för mellanslag.";
+$ubbt_lang['LONG_NAME']  = 	"Användarnamnet har inte rätt längd.";
+$ubbt_lang['BAD_EMAIL']  = 	"Den e-postdomänen är inte giltig för registrering här!";
+$ubbt_lang['USER_EXISTS']  = 	"Det användarnamnet finns redan.";
+$ubbt_lang['NO_MULTI']  = 	"Det finns redan ett användarkonto på den e-postadressen.";
+$ubbt_lang['PASS_MATCH']  = 	"Lösenordet & Verifieringen är inte lika.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Lösenordet måste vara mellan 4 & 20 tecken.";
+$ubbt_lang['ILL_PASS']  = 	"Lösenordet har ogiltiga tecken.  Endast alfabetiska tecken är tillåtna.";
+$ubbt_lang['WEL_MSG']  = 	"Välkommen!  Hoppas ni ska trivas här hos oss och kanske hitta många nya vänner! Ta dig gärna tid till att fylla i din profil då det blir mycket lättare för andra att se vem du är då och lättare att smälta in i gemenskapen! Dessutom kan du anpassa forumsystemet så att det fungerar som du själv vill genom att ändra visningsinställningarna.";
+$ubbt_lang['PASS_SUB']  = 	"Ditt lösenord för";
+$ubbt_lang['PASS_BODY1']  = 	"Någon från IP-adress";
+$ubbt_lang['PASS_BODY2']  = 	"registrerade användarnamnet";
+$ubbt_lang['PASS_BODY3']  = 	"Lösenordet för detta användarnamn är ";
+$ubbt_lang['NEW_CONFIRM']  = 	"Användarnamn har registrerats.";
+$ubbt_lang['NEW_BODY']  = 	"Ditt användarnamn har reserverats. Du kommer inom kort att få ett e-postmeddelande med lösenordet, förutsatt att du fyllt i din e-postadress korrekt.";
+?>
