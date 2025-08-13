566b66e843a13039a1ca222e8741f0eecccf10d5
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:59 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/newreply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/newreply.php
@@ -0,0 +1,19 @@
+<?
+$ubbt_lang['READ_PERM']  = 	"In dit forum heb jij alleen leesrechten.";
+$ubbt_lang['NOT_APP']  = 	"Een bericht dat nog niet goedgekeurt is, kan niet beantwoord worden.";
+$ubbt_lang['REPLY_HEAD']  = 	"Beantwoorden van een bericht in dit forum";
+$ubbt_lang['FILL_FORM']  = 	"Vul onderstaand formulier volledig in.";
+$ubbt_lang['NO_HTML']  = 	"HTML is uitgeschakeld.";
+$ubbt_lang['YES_HTML']  = 	"HTML is ingeschakeld.";
+$ubbt_lang['NO_MARKUP']  = 	"UBBCode is uitgeschakeld.";
+$ubbt_lang['YES_MARKUP']  = 	"UBBCode is ingeschakeld. Voor informatie over UBBCode";
+$ubbt_lang['MARK_POSTS']  = 	"klik hier";
+$ubbt_lang['CHOOSE_NAME']  = 	"Anonieme berichten worden toegestaan, dus kun je onder een willekeurige naam berichten plaatsen.";
+$ubbt_lang['POST_ICON']  = 	"Bericht icoon";
+$ubbt_lang['POST_TEXT']  = 	"Bericht";
+$ubbt_lang['SPELL_CHECK']  = 	"Spellingcontrole";
+$ubbt_lang['DO_EMAIL']  = 	"Email alle reacties naar mijn email adres";
+$ubbt_lang['DO_PREVIEW']  = 	"Ik wil eerst een voorbeeld bekijken";
+$ubbt_lang['CAN_ATTACH']  = 	"en/of een bestand bijvoegen";
+$ubbt_lang['IN_RESPONSE']  = 	"Ik antwoord op dit bericht:";
+?>
