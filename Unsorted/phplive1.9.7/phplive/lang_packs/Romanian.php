3bc5ce6ca689be991430d77a003120abe3fc1e03
me-suzy <me.suzana@gmail.com>
2025-08-14 00:48:41 +0300

    Add Unsorted/phplive1.9.7/phplive/lang_packs/Romanian.php

---
+++ Unsorted/phplive1.9.7/phplive/lang_packs/Romanian.php
@@ -0,0 +1,84 @@
+<?
+$LANG = ARRAY() ;	// MUST initialize, DO NOT CHANGE
+/*******************************************************************
+* Language: Romanian
+* Contributed by:
+*	Name: Stefaniu Criste
+*	Company: ['di'zain']
+*	URL: http://www.dizain.ro
+*
+* ------------------------------------------------------------------
+* DO NOT REMOVE COMMENTS.  JUST MODIFY THE ACTUAL TEXT.
+* DO NOT REMOVE THE orig TEXT!  THEY ARE THERE FOR REFERENCE.
+* ------------------------------------------------------------------
+* NOTE: If you are going to submit a language pack, please keep
+* everything basic and do not mention any company name, personal name,
+* copyrighted name, or such.  Keep all translations generic, simple
+* and catered to all audiences and companies.  Basically, your
+* translated language pack should be ready to use without being touched.
+*******************************************************************/
+
+
+
+/***************** ONLY MAKE THE CHANGE BELOW ******************/
+
+
+/*** branded string located troughout 99% of the pages */
+//orig: PHP <i>Live!</i> &copy; 2001 OSI Codes, New York, New York
+$LANG['DEFAULT_BRANDING'] = "Powered by <a href='http://www.PHPLiveSupport.com' target='new'>PHP <i>Live!</i></a>" ;
+
+/*** chat page titles */
+//orig: Request Live! Support Request
+$LANG['TITLE_SUPPORTREQUEST'] = "Solicitare Live! Support" ;
+//orig: You are now speaking with a Live! Support
+$LANG['TITLE_SUPPORCHAT'] = "Sunteti în contact cu un operator Live! Support" ;
+//orig: Please leave a message
+$LANG['TITLE_LEAVEMESSAGE'] = "Vã rugãm sã lãsati un mesaj" ;
+//orig: Printer Friendly View
+$LANG['TITLE_PRINTVIEW'] = "Afisare formatatã pentru tipãrire" ;
+
+$LANG['WORD_BACK'] = "Înapoi" ;
+$LANG['WORD_CLOSE'] = "Închide" ;
+$LANG['WORD_NAME'] = "Nume" ;
+$LANG['WORD_EMAIL'] = "Email" ;
+$LANG['WORD_SUBJECT'] = "Subiect" ;
+$LANG['WORD_MESSAGE'] = "Mesaj" ;
+$LANG['WORD_SEND'] = "Trimite" ;
+$LANG['WORD_COMPANY'] = "Companie" ;
+$LANG['WORD_DEPARTMENT'] = "Departament" ;
+$LANG['WORD_DAY'] = "Zi" ;
+
+//orig: Welcome to our Live! support help desk.
+$LANG['CHAT_REQUEST_TITLE'] = "Bun venit în Live! support help desk." ;
+//orig: Please select the department you would like to reach.
+$LANG['CHAT_REQUEST_SELECTDEPT'] = "Vã rugãm sã alegeti departamentul dorit." ;
+//orig: Input your Screen Name used during this session:
+$LANG['CHAT_REQUEST_INPUTSCREENNAME'] = "Introduceti Input your Screen Name used during this session." ;
+//orig: Welcome %%user%%! Please hold while we contact a representative. If a representative does not respond in few seconds, then he/she is not available at this time.
+$LANG['CHAT_GREETING'] = "Bun venit %%user%%! Vã rugãm sã asteptati pânã vom contacta un reprezentant. Dacã operatorul nu rãspunde în câteva secunde, înseamnã cã nu este disponibil pentru moment.";
+//orig: Printer Friendly
+$LANG['CHAT_PRINTER_FRIENDLY'] = "Bun de tipar" ;
+//orig: Your party has left this session.
+$LANG['CHAT_PARTYLEFTSESSION'] = "Corespondentul dumneavoastrã a pãrãsit sesiunea." ;
+//orig: You are now speaking with
+$LANG['CHAT_REQUEST_NOWSPEAKINGWITH'] = "În acest moment discutati cu" ;
+//orig: Department is currently unavailable. Please leave a message.
+$LANG['MESSAGE_BOX_MESSAGE'] = "Departamentul nu este momentan disponibil. Vã rugãm sã lãsati un mesaj." ;
+//orig: Email is invalid format (example: someone@somewhere.com)
+$LANG['MESSAGE_BOX_JS_A_INVALIDEMAIL'] = "Email is invalid format (example: someone@somewhere.com)" ;
+//orig: Thank you.  Your message has been sent to:
+$LANG['MESSAGE_BOX_SENT'] = "Vã multumim. Mesajul dumneavoastrã a fost trimis la:" ;
+//orig: Items with (*) must be filled.
+$LANG['MESSAGE_BOX_JS_A_ALLFIELDSSUP'] = "Câmpurile cu (*) trebuie completate." ;
+//orig: Department not available.  Please leave a message.
+$LANG['CHAT_DEPT_NOTAVAIL'] = "Departamentul nu este disponibil.  Vã rugãm sã lãsati un mesaj." ;
+//orig: Session did not create.  Request did not send.
+$LANG['CHAT_NOTCREATE'] = "Sesiunea nu a fost creatã. Cererea nu a fost trimisã." ;
+
+//orig: Please hold while being transferred to
+$LANG['CHAT_CALL_TRANSFER'] = "Vã rugãm sã asteptati pânã când vã transferãm la" ;
+//orig: What is your question?
+$LANG['CHAT_REQUEST_QUESTION'] = "Ce întrebare aveti?" ;
+//orig: A copy of the chat transcript has been sent to:
+$LANG['CHAT_TRANSCRIPT_SENT'] = "A copy of the chat transcript has been sent to:" ;
+?>
