2feab370bf0cf2c8bae584d9103e3b5b4d80afc9
me-suzy <me.suzana@gmail.com>
2025-08-14 01:03:28 +0300

    Add Unsorted/phpx/menu_files/figure.js

---
+++ Unsorted/phpx/menu_files/figure.js
@@ -0,0 +1,17 @@
+MENU_ITEMS0 = [
+    ['Home', null,
+        ['Contact', 'index.php?id=2',
+            ['352352333333', null],
+            ['44444', 'index.php?id=6'],
+            ['Stats Page1', 'index.php?id=4']
+        ],
+        ['Links', null,
+            ['PHPX HomePage', 'http://www.phpx.org',
+                ['Support', 'http://www.thisrand.com']
+            ],
+            ['PHP Homepage', 'http://www.php.net'],
+            ['MySQL Homepage', 'http://www.mysql.com']
+        ],
+        ['Search', 'index.php?id=3']
+    ]
+];
