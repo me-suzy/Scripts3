4fe79602125079d39ed69ea79e53370093f2179f
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:59 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/sendmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/sendmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['CHOOSE_ADD']  = 	"Alege un nume";
+$ubbt_lang['TEXT_DELETE']  = 	"Sterge";
+$ubbt_lang['NO_PRIVATE']  = 	"Acest utilizator nu accepta mesaje private.";
+$ubbt_lang['NO_RECORD']  = 	"Nu avem nici o inregistrare despre utilizatorul caruia incercati sa-i trimiteti mesajul.";
+$ubbt_lang['MESS_PREV']  = 	"Previzualizeaza mesajul catre";
+$ubbt_lang['PREVIEW_MESS']  = 	"Acesta este mesajul in forma finala. Daca totul este in regula apasati pe 'Continua' si mesajul va fi trimis. Daca nu, folositi formularul de mai jos pentru a-l edita in continuare.";
+$ubbt_lang['EDIT_MORE']  = 	"Editeaza mai departe";
+$ubbt_lang['PRIV_SUB']  = 	"Aveti mesaje noi";
+$ubbt_lang['PRIV_BODY_R']  = 	"Ati primit un mesaj privat pe forum. Puteti merge la {$config['phpurl']}/login.php pentru a-l vedea.";
+$ubbt_lang['MESS_SENT']  = 	"Mesajul privat a fost trimis.";
+?>
