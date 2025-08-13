b3522f1344e2fd7f6c338928ce37a4444c388ca6
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:49 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "Move this post & any replies";
+$ubbt_lang['DELETE_THREAD']  =  "Delete this thread";
+$ubbt_lang['NO_EDIT']  = 	"Nem tudom ellenõrizni az jogait az üzenet módosításához.";
+$ubbt_lang['EDITTIME']  = 	"This post can no longer be edited because the maximum
+$ubbt_lang['PEDIT_HEAD']  = 	"Üzenet módositása";
+$ubbt_lang['PEDIT_BODY']  = 	"Törölheti vagy módosíthatja az üzenetet.  Ha a törlést választja és volt válasz az üzenetre akkor az üzenet címe \"Üzenet törölve\" lesz.  A többi üzenet érintetlen marad.";
+$ubbt_lang['EDIT_POLL']  = 	"Because this post has a poll in it, you may delete it but not edit it.";
+$ubbt_lang['POST_ICON']  = 	"Ikon";
+$ubbt_lang['MARK_EDIT']  = 	"Mark as Edited?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Üzenet rendben";
+$ubbt_lang['PEDIT_DELETE']  = 	"Üzenet törlése";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Üzenet elfogadása";
+$ubbt_lang['RET_NORM']  = 	"Vissza a normál dátumhoz";
+$ubbt_lang['MAKE_STICKY']  = 	"Az üzenet a helyén marad";
+$ubbt_lang['MANAGE_THREAD']  = 	"Témakör menedzselése";
+$ubbt_lang['OPEN_THREAD']  = 	"Témakör nyitása";
+$ubbt_lang['CLOSE_THREAD']  = 	"Témakör zárása";
+$ubbt_lang['UNKEEP_THREAD']  = 	"Témakör meg nem tartása";
+$ubbt_lang['KEEP_THREAD']  = 	"Témakör megtartása";
+$ubbt_lang['MOVE_THREAD']  = 	"Üzenet áthelyezése";
+?>
