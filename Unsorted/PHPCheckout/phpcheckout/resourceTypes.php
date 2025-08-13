461554e88c7dfd87c3179a7a4e065b8f8b5cc4d2
me-suzy <me.suzana@gmail.com>
2025-08-14 00:33:52 +0300

    Add Unsorted/PHPCheckout/phpcheckout/resourceTypes.php

---
+++ Unsorted/PHPCheckout/phpcheckout/resourceTypes.php
@@ -0,0 +1,20 @@
+<?php echo"\n\n\n";?>
+<!-- START OF resourceTypes.inc -->				
+<select name="resource" size=1>
+	<option value="<?php echo $resource;?>" SELECTED><?php echo $resource;?></option>
+	<!-- edit any option below this line - delete, add or change -->							
+	<option value="article">article</option>
+	<option value="audio">audio</option>
+	<option value="certificate">certificate</option>
+	<option value="digital_art">digital art</option>
+	<option value="eBook">eBook</option>
+	<option value="manual">manual</option>
+	<option value="movie">movie</option>
+	<option value="music">music</option>
+	<option value="service">service</option>
+	<option value="software">software</option>
+	<option value="none">none</option>
+	<option value="other">other</option>
+	<option value="video">video</option>
+</SELECT>
+<!-- END OF resourceTypes.inc -->
