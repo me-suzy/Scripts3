074f2c0b9f79921d44b3ad9fcb3a532b3f840a7b
me-suzy <me.suzana@gmail.com>
2025-08-14 01:41:06 +0300

    Add Unsorted/SiteWorks.Pro.4.0.Retail/SiteWorksPro/books.php

---
+++ Unsorted/SiteWorks.Pro.4.0.Retail/SiteWorksPro/books.php
@@ -0,0 +1,42 @@
+<?php include_once("templates/top.php"); ?>
+
+	<!-- Start Books -->
+	<div align="center">
+		<center>
+		<table width="96%" border="0" cellspacing="0" cellpadding="0" align="center">
+			<tr>
+				<td width="100%" colspan="2" class="BodyHeader2">
+					<span class="BodyHeading">
+						<br><?php echo $siteName . " Recommended Books"; ?><br><br>
+					</span>
+				</td>
+			</tr>
+			<tr>
+				<td width="100%" height="20" colspan="2" valign="top">
+				<?php
+				
+					$tResult = mysql_query("select pk_tId, tName from tbl_Topics order by tName");
+					
+					while($tRow = mysql_fetch_array($tResult))
+					{
+						$bResult = mysql_query("select bTitle, bURL from tbl_Books where bTopicIds = '{$tRow["pk_tId"]}' or bTopicIds like '{$tRow["pk_tId"]},%' or bTopicIds like '%, {$tRow["pk_tId"]},%' or bTopicIds like '%, {$tRow["pk_tId"]}' order by bTitle asc");
+
+						if(mysql_num_rows($bResult) > 0)
+						{
+							echo "<img src='images/gd.gif'><span class='BodyHeading4'> " . $tRow["tName"] . "</span><br>";
+							while($bRow = mysql_fetch_array($bResult))
+							{
+								echo "&nbsp;&nbsp;&nbsp;&nbsp;<img src='images/join.gif'>&nbsp;&nbsp;<a href='" . $bRow["bURL"] . "' target='_blank'><span class='Link2'>" . $bRow["bTitle"] . "</span></a><br>";
+							}
+							echo "<br>";
+						}
+					}
+				?>
+				</td>
+			</tr>
+		</table>
+		</center>
+	</div>
+	<!-- End Books -->
+
+<?php include_once("templates/bottom.php"); ?>
