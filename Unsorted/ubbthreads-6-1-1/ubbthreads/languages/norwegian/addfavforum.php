7dac653bba6b798669dba7759397c565cc9c7f9a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:41:21 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/addfavforum.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/addfavforum.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  =  "You do not have permission to view this forum so you cannot add it to your favorites list.";
+$ubbt_lang['DUP_ENTRY']  =  "This forum has already been added to your favorites list.";
+$ubbt_lang['FORUM_IN']  =  "Forum added";
+$ubbt_lang['FORUM_CONFIRM']  =  "The selected forum has been added to your favorite forums list.  You will see this forum in  your 'My Home' until you remove it.";
+$ubbt_lang['FORUM_RETURN']  =  "Return to forum now.";
+$ubbt_lang['TOO_MANY']  =  "You may only have 83 forums in your favorites list";
+?>
