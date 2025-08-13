54d61b64611054356344bd64933e18ea8b133770
me-suzy <me.suzana@gmail.com>
2025-08-14 02:45:54 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/addfav.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/addfav.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  = 	"Nu aveti dreptul sa vedeti acest thread asa ca nu-l puteti adauga in lista celor favorite/de revazut.";
+$ubbt_lang['MAIN_ONLY']  = 	"Nu puteti adauga decit topic-urile principale in lista celor favorite/de revazut.";
+$ubbt_lang['DUP_ENTRY']  = 	"Acesta a fost deja adaugat in lista celor favorite/de revazut.";
+$ubbt_lang['ENTRY_IN']  = 	"Mesaj/Thread adaugat";
+$ubbt_lang['FAV_CONFIRM']  = 	"Mesajul/Thread-ul au fost adaugate in lista.<br><br>De acum inainte le veti putea accesa si din 'My Home', pina in momentul in care le veti sterge de acolo.<br><br>Daca le-ati adaugat in lista 'de revazut' vor disparea din 'Control Panel' in momentul in care raspundeti la ele.<br>";
+$ubbt_lang['FORUM_RETURN']  = 	"Intoarcere la forum.";
+?>
