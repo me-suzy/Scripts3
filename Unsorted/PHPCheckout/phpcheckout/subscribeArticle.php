e4938e13fbfbbd35bb13cc4547cbec3311a726c4
me-suzy <me.suzana@gmail.com>
2025-08-14 00:34:15 +0300

    Add Unsorted/PHPCheckout/phpcheckout/subscribeArticle.php

---
+++ Unsorted/PHPCheckout/phpcheckout/subscribeArticle.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+	<HEAD>
+		<LINK rel=stylesheet type="text/css" href="phpcheckout.css">
+		
+		<?php require_once("configure.php");?>
+		
+<TITLE>Subscribe Article -  <?php echo IMPLEMENTATIONNAME;?> - <?php echo BENEFIT;?> -  <?php echo ORGANIZATION;?> - Powered by phpcheckoutTM from DreamRiver http://wwww.dreamriver.com - Software Powers the Net - a DreamRiver Internet Software Product - php software mysql database - download - web database - dreamriver - freeware - sql - file downloader - internet application development - hot scripts - good value - database software - custom code - php e-mail software - ultimate web apps software - phpYellow Pages - EasySQL - phpTellAFriend - mysql php download - Easily create complete internet programs with php, apache, mysql and windows</TITLE>
+
+	</HEAD> 
+
+<body>
+
+<!-- START of body -->
+
+<?php include("header.php");?>
+
+<table width="100%">
+	<!-- start of primary content FOR PAGE -->
+	<tr>
+
+		<!-- start MAIN COLUMN -->
+		<td valign="top">
+		<!-- PUT CONTENT BELOW HERE !!! -->
+		
+		
+		<h1>Don't let your web site embarrass you</h1>
+<i>by Tim Hicks, TRH Communications</i>
+<p>
+Robert Browning wrote:
+<br>" ...a man's reach should exceed his grasp
+<br>Or what's a heaven for?" 
+<p>
+I'll bet he would have made a lousy painter or window-washer - or Web site builder. 
+<p>
+A Web site is like a puppy. It starts with enthusiasm, but if you don't give it regular care and attention it will embarrass you later. 
+<p>
+Your site's size and complexity should be constrained by your ability to maintain its accuracy, timeliness and freshness. 
+<p>
+Some short-lived features may be beyond your ability to replace them frequently. So don't build them in the first place. It's better to have fewer pages than to have stale ones. 
+<p>
+If you have a good programmer on your Web team, you can put in some simple automated content management tools. 
+<p>
+Put a hidden comment in each page that indicates when it should be updated next, then regularly run a program that scans your pages looking for those that have reached their "best-before" date. Change that best-before date every time you update its page. Different pages will have different "shelf lives." 
+<p>
+The grocery store analogy is appropriate. Some of what's on a grocery store's shelves will keep indefinitely, but a month-old newspaper or cabbage won't do much for the ambience. So appoint a stockperson today to make sure everything is fresh every day. 
+
+<p align="center"><small>&copy; TRH Communications, Victoria, B.C. <a href="http://www.trh.bc.ca">http://www.trh.bc.ca</a></small></p>
+		
+		<!-- PUT CONTENT ABOVE HERE !!! -->
+		</td>
+	</tr>
+</table>
+
+
+
+<?php include("footer.php");?>
+
+	
