960fb5f617129ce7a416aa6e91d7750a59bea492
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:02 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/mess_handler.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/mess_handler.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['REPLY_HEAD']   =   "Use this form to send a private message to this user";
+$ubbt_lang['MESS_COPY']  =     "I want to keep a copy of this message";
+$ubbt_lang['DELETE_MESS']  = 	"Üzenet törlése";
+$ubbt_lang['REPLY_MESS']  = 	"Válasz";
+$ubbt_lang['RETURN_START']  = 	"Vissza a kezdõlapra";
+$ubbt_lang['MESS_RECEIPT']  = 	"I want to receive a read receipt";
+$ubbt_lang['MESS_PREVIEW']  = 	"I want to preview my message";
+$ubbt_lang['ORIG_MESS']  = 	"Eredeti üzenet";
+?>
