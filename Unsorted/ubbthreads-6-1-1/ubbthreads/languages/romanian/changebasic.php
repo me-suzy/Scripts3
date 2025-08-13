e0cbc13a39eab29b3ed2c17947aa9a4d78786be7
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:06 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/changebasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/changebasic.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['PIXEL_TOO_BIG']="Your picture exceeds the ({$theme['PictureWidth']}x{$theme['PictureHeight']}) pixel limitation width a width/height of ";
+$ubbt_lang['FILE_TOO_BIG']="Pictures can be no larger than {$config['avatarsize']} bytes.";
+$ubbt_lang['BAD_PIC']  = 	"Formatul imaginii este incorect.";
+$ubbt_lang['PASS_MATCH']  = 	"Parolele difera.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Parola trebuie sa aiba intre 4 si 20 de caractere.";
+$ubbt_lang['HOME_TOO_LONG']  = 	"Intrarea din campul paginii de web este prea lunga.";
+$ubbt_lang['OCC_TOO_LONG']  = 	"Intrarea din campul Ocupatie este prea lunga.";
+$ubbt_lang['HOBB_TOO_LONG']  = 	"Intrarea din campul Hobiuri este prea lunga.";
+$ubbt_lang['LOC_TOO_LONG']  = 	"Intrarea din campul Localizare este prea lunga.";
+$ubbt_lang['SIG_TOO_LONG']  = 	"Semnatura este prea lunga.";
+$ubbt_lang['BIO_TOO_LONG']  = 	"Intrarea din campul Bio este prea lunga.";
+$ubbt_lang['BASIC_MOD']  = 	"Profilul principal a fost modificat.";
+?>
