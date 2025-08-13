b82a5afcfff0affe1af2519e004d43ae16efebef
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:09 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/showprofile.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/showprofile.php
@@ -0,0 +1,27 @@
+<?
+$ubbt_lang['RATING']    =       "Rating";
+$ubbt_lang['T_RATES']   =       "Total Rates";
+$ubbt_lang['RATE_USER'] =       "Rate this user";
+$ubbt_lang['STAR1'] =           "1 star";
+$ubbt_lang['STAR2'] =           "2 stars";
+$ubbt_lang['STAR3'] =           "3 stars";
+$ubbt_lang['STAR4'] =           "4 stars";
+$ubbt_lang['STAR5'] =           "5 stars";
+$ubbt_lang['DORATE']    =       "Submit rating";
+$ubbt_lang['YOURATED']  =       "You rated this user a ";
+$ubbt_lang['PROF_FOR']  = 	"Profil pentru";
+$ubbt_lang['TEXT_NAME']  = 	"Nume";
+$ubbt_lang['USER_TITLE']  = 	"Titlu";
+$ubbt_lang['TOTAL_POSTS']  = 	"Numar mesaje";
+$ubbt_lang['TEXT_HOME']  = 	"Pagina web";
+$ubbt_lang['TEXT_OCCU']  = 	"Ocupatie";
+$ubbt_lang['TEXT_HOBBY']  = 	"Hobiuri";
+$ubbt_lang['TEXT_LOC']  = 	"Localizare";
+$ubbt_lang['TEXT_BIO']  = 	"Bio";
+$ubbt_lang['PROF_ICQ']  = 	"Numar ICQ";
+$ubbt_lang['REGGED_ON']  = 	"Inregistrat la";
+$ubbt_lang['EDIT_T_U']  = 	"Editeaza acest utilizator";
+$ubbt_lang['ADD_BOOK']  = 	"Adauga in agenda";
+$ubbt_lang['SHOW_ALL_P']  = 	"Afiseaza toate mesajele acestui utilizator";
+$ubbt_lang['NO_LONGER']  = 	"Acest utilizator nu mai este in baza de date.";
+?>
