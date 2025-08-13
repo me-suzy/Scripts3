2a6b1976eefb98dba609da41892d608309a3aec2
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:41 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/coppaform.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/coppaform.php
@@ -0,0 +1,21 @@
+<?
+
+$ubbt_lang['PARENT_APP'] = "Målsmans godkännande behövs";
+$ubbt_lang['PARENT_APP_2'] = "Din målsman måste godkänna vår registrering. Du kommer inte att kunna registrera ett användarkonto innan vi fått detta godkännande. För att skapa ditt konto, skriv ut denna sida och följ instruktionerna nedan.";
+$ubbt_lang['MORE_INFO'] = "Kontakta <a href=\"mailto:{$config['emailaddy']}\">{$config['emailaddy']}</a> för att få mer information";
+$ubbt_lang['LOGIN_NAME'] = "Användarnamn";
+$ubbt_lang['LOGIN_NAME2'] = "Om det första användarnamnet skulle vara upptaget så använder vi det alternativa användarnamnet.";
+$ubbt_lang['YOUR_EMAIL'] = "E-postadress";
+$ubbt_lang['BIRTHDATE'] = "Födelsedatum";
+$ubbt_lang['PARENT_INST'] = "Instruktioner för målsman";
+$ubbt_lang['PARENT_INST2'] = "Skriv under formuläret nedan och skicka in det till oss.";
+$ubbt_lang['COPPA_INST'] = "COPPA-instruktioner";
+$ubbt_lang['AGREE'] = "JA: Jag har sett över informatoinen som mitt barn har fyllt i och läst integritetspolicyn för webbplatsen. Jag förstår att profilinformationen kan ändras genom att använda användarnamnet och lösenordet för denna registrering.";
+$ubbt_lang['FULLNAME'] = "Målsmans för- och efternamne";
+$ubbt_lang['SIGNATURE'] = "Underskrift";
+$ubbt_lang['RELATION'] = "Förhållande till barnet";
+$ubbt_lang['TELE'] = "Telefonnummer (eller annat kontaktsätt offline)";
+$ubbt_lang['PARENT_EMAIL'] = "Målsmans e-postadress";
+$ubbt_lang['DATE'] = "Datum";
+$ubbt_lang['REG_DATE'] = "Registreringsdatum";
+$ubbt_lang['QUESTIONS'] = "Vid frågor, skicka e-post till";
