0824c93336a980e3e312550d0b665eb63e939e51
me-suzy <me.suzana@gmail.com>
2025-08-14 00:55:09 +0300

    Add Unsorted/phpmanager_nullified1/phpmanager-nullified/phpmanager/phpManager/templates/default/order/thankyou.php

---
+++ Unsorted/phpmanager_nullified1/phpmanager-nullified/phpmanager/phpManager/templates/default/order/thankyou.php
@@ -0,0 +1,31 @@
+<?php
+
+include ("../../../variables.php");
+
+?>
+
+<html>
+<head>
+<title>
+Thank You
+</title>
+<LINK rel="stylesheet" type="text/css" href="templates/<?php echo $template ?>/styles.css">
+</head>
+<body>
+
+<table align="center" width="600" cellspacing="0" cellpadding="5" style="border: 1 solid #000000">
+<tr>
+	<td width="600" align="center" class="summary_message">
+	- Signup Complete -
+	</td>
+</tr>
+<tr>
+	<td width="600" height="350" align="center" class="main_bold">
+	Thank you for your custom. Full instructions have been emailed to the supplied email
+	address on how to access your new hosting account.
+	<br>
+	<br>
+	If you have any further questions, please don't hesitate to contact us.
+	</td>
+</tr>
+</table>
