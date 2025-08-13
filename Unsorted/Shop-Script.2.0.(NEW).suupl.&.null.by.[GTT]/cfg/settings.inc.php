86f6f82cf6052b91f72952022bc16dca14afe532
me-suzy <me.suzana@gmail.com>
2025-08-14 01:39:07 +0300

    Add Unsorted/Shop-Script.2.0.(NEW).suupl.&.null.by.[GTT]/cfg/settings.inc.php

---
+++ Unsorted/Shop-Script.2.0.(NEW).suupl.&.null.by.[GTT]/cfg/settings.inc.php
@@ -0,0 +1,16 @@
+<?
+	$tax = "5";
+	$products_count = "10";
+	$cols_count = "2";
+	$dark_color = "CD8181";
+	$middle_color = "F0B5B5";
+	$light_color = "FFEEEE";
+	$shopname = "Shop-Script 2.0";
+	$shopurl = "www.shop-script.com";
+	$inn = "in";
+	$kpp = "kpp";
+	$reciever = "name";
+	$rs = "rs";
+	$ks = "ks";
+	$bik = "bik";
+?>
