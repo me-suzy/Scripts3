ed45ba1c6f8198be43190bf3270c4f63768dd12f
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:16 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "Diese Nachricht und alle Antworten verschieben";
+$ubbt_lang['DELETE_THREAD']  =  "Diesen Thread löschen";
+$ubbt_lang['NO_EDIT']  = 	"Leider ist die &Uuml;berpr&uuml;fung Ihrer Erlaubnis, diese Nachricht zu &auml;ndern, fehlgeschlagen.";
+$ubbt_lang['EDITTIME']  = 	"Diese Nachricht kann nicht mehr ge&auml;ndert werden, da die Editierzeit abgelaufen ist";
+$ubbt_lang['PEDIT_HEAD']  = 	"Nachricht editieren";
+$ubbt_lang['PEDIT_BODY']  = 	"Sie k&ouml;nnen diesen Teil editieren oder l&ouml;schen.  Falls Sie ihn l&ouml;schen m&ouml;chten und Antworten auf diese Nachricht existieren, wird der Betreff auf 'Diese Nachricht wurde gel&ouml;scht' ge&auml;ndert.  Dies geschieht zur korrekten Erhaltung der gestaffelten Anzeige.";
+$ubbt_lang['EDIT_POLL']  = 	"Da diese Nachricht eine Umfrage enth&auml;lt, ist ein nachtr&auml;gliches Ver&auml;ndern nicht m&ouml;glich. Die Nachricht kann aber gel&ouml;scht werden.";
+$ubbt_lang['POST_ICON']  = 	"Symbol f&uuml;r die Nachricht:";
+$ubbt_lang['MARK_EDIT']  = 	"Als ge&auml;ndert kennzeichnen";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Nachricht ändern";
+$ubbt_lang['PEDIT_DELETE']  = 	"Nachricht löschen";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Nachricht freigeben";
+$ubbt_lang['RET_NORM']  = 	"Zurueck zum richtigen Datum";
+$ubbt_lang['MAKE_STICKY']  = 	"Immer ganz oben anzeigen";
+$ubbt_lang['MANAGE_THREAD']  = 	"Diesen Thread verwalten:";
+$ubbt_lang['OPEN_THREAD']  = 	"Öffnen";
+$ubbt_lang['CLOSE_THREAD']  = 	"Schliessen";
+$ubbt_lang['UNKEEP_THREAD']  = 	"Löschschutz entfernen";
+$ubbt_lang['KEEP_THREAD']  = 	"Löschschutz";
+$ubbt_lang['MOVE_THREAD']  = 	"Verschieben";
+?>
