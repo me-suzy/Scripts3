246700c5b596f06183779ef6f29d6d040f01a9c6
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:02 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/approvepost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/approvepost.php
@@ -0,0 +1,5 @@
+<?
+$ubbt_lang['REPLY_SUB']  = 	"Respuestas a tu mensaje";
+$ubbt_lang['REPLY_BOD']  = 	"contest&oacute; a tu mensaje en: .";
+$ubbt_lang['APPR_HEAD']  = 	"Tu mensaje ha sido admitido.";
+?>
