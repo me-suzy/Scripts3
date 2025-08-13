793464842a6a5e6654aa59d894b0fc406de8822d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:31:46 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/showmembers.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/showmembers.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['USER_STATUS']  = 	"Status";
+$ubbt_lang['TEXT_HOME']  = 	"Netsted";
+$ubbt_lang['TOTAL_POSTS']  = 	"Samlede antal indlæg";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ-nummer";
+$ubbt_lang['REGGED_ON']  = 	"Registrede sig selv som bruger den";
+$ubbt_lang['PAGE_TEXT']  = 	"Sider";
+?>
