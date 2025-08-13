8a8d14cb1a678a4eb0b57a986eeab3afbbefefdd
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:41 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/addfavforum.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/addfavforum.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  = 	"Sie haben keine Berechtigung, dieses Forum zu lesen. Daher k&ouml;nnen Sie es auch nicht zu Ihrer Favoritenliste hinzuf&uuml;gen.";
+$ubbt_lang['DUP_ENTRY']  = 	"Dieses Forum wurde bereits zu Ihrer Favoritenliste hinzuf&uuml;gt.";
+$ubbt_lang['FORUM_IN']  = 	"Forum hinzuf&uuml;gt";
+$ubbt_lang['FORUM_CONFIRM']  = "Das gew&auml;hlte Forum wurde zu Ihrer Favoritenliste hinzuf&uuml;gt. Sie werden diesen Eintrag solange unter 'Einstellungen' sehen, bis Sie ihn wieder l&ouml;schen.";
+$ubbt_lang['FORUM_RETURN']  = 	"Zur&uuml;ck zum Forum.";
+$ubbt_lang['TOO_MANY']  = 	"Sie k&ouml;nnen maximal 83 Foren in Ihrer Favoritenliste haben.";
+?>
