d5049335232e96710ab9f154bc09214e382c8244
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:14 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =    "Votre compte doit-être validé par un administrateur avant que vous puissiez vous identifier.  Vous recevrez un email dès que cette opération sera effectuée.";
+$ubbt_lang['APPROVED'] = "Votre compte pour {$config['title']} à l'adresse {$config['phpurl']} a été validé.";
+$ubbt_lang['NOTAPPROVED'] = "Votre compte pour {$config['title']} à l'adresse {$config['phpurl']} n'a pas été validé.  La raison en est : ";
+$ubbt_lang['NEWREGBODY'] = "Un nouveau membre s'est enregistré sur {$config['title']}. Pour valider ce compte, merci d'aller sur";
+$ubbt_lang['NEWREG'] = "Nouvel utilisateur enregistré sur";
+$ubbt_lang['INVALID_DATE'] = "Cette date n'est pas correcte.";
+$ubbt_lang['TOOYOUNG']  =       "L'inscription à ce forum requière une autorisation de vos parents ou gardiens.  Vous pouvez utiliser le <a href=\"{$config['phpurl']}/coppaform.php\">Formulaire d'autorisation</a> pour cela.";
+$ubbt_lang['UNDER13'] = "Désolé, nous n'autorisons pas les enfants agés de moins de 13 ans à s'inscrire.";
+$ubbt_lang['BAD_UNAME']  = 	"Vous ne pouvez pas utiliser de caractères HTML ou d'espaces avant ou après votre nom d'usager.";
+$ubbt_lang['LONG_NAME']  = 	"Le nom d'usager n'est pas de la longueur appropriée.";
+$ubbt_lang['BAD_EMAIL']  = 	"Vous n'êtes pas autorisé à utiliser le domaine de l'adresse e-mail comme Identifiant";
+$ubbt_lang['USER_EXISTS']  = 	"Ce nom d'utilisateur est déjà utilisé.";
+$ubbt_lang['NO_MULTI']  = 	"Plusieurs comptes ne sont pas autorisés pour la même adresse email et un autre utilisateur utilise déjà l'adresse email que vous avez fournie.";
+$ubbt_lang['PASS_MATCH']  = 	"Les mots de passe ne sont pas identiques.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Le mot de passe doit faire entre 4 et 20 caractères.";
+$ubbt_lang['ILL_PASS']  = 	"Le mot de passe contient des caractères non autorisés.  Merci de n'utiliser que des caractères alphanumériques.";
+$ubbt_lang['WEL_MSG']  = 	"Veuillez prendre le temps d'éditer votre profil. En éditant votre profil, vous pourez personaliser les informations vous concernant, personnaliser une signature qui sera ajoutée à chacun de vos messages et choisir vos propriétés d'affichage par défaut (ordre de tri, mode et vue d'affichage, mode plat ou discussion, etc.).";
+$ubbt_lang['PASS_SUB']  = 	"Votre mot de passe pour";
+$ubbt_lang['PASS_BODY1']  = 	"Quelqu'un utilisant l'adresse IP";
+$ubbt_lang['PASS_BODY2']  = 	"a enregistré le nom d'usager";
+$ubbt_lang['PASS_BODY3']  = 	"Le mot de passe pour ce nom est";
+$ubbt_lang['NEW_CONFIRM']  = 	"Le nom d'usager a été enregistré.";
+$ubbt_lang['NEW_BODY']  = 	"Votre nom d'usager a été réservé. Vous devriez recevoir sous peu un email contenant votre mot de passe. Une fois le mot de passe en votre possession que vous pourrez vous identifier et poster avec ce nom. Merci d'avoir pris le temps d'enregistré un compte sur nos forums. Pensez à visiter 'Mon Centre' et à éditer votre profil avant de poster sur les forums. La personnalisation de votre profil vous permettra d'adapter la disposition des forums selon vos préférences et besoins particuliers.";
+?>
