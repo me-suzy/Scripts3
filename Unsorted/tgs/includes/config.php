0e3dd4d52c2e1ad2c01e76319707f6dd191f0534
me-suzy <me.suzana@gmail.com>
2025-08-14 02:01:28 +0300

    Add Unsorted/tgs/includes/config.php

---
+++ Unsorted/tgs/includes/config.php
@@ -0,0 +1,23 @@
+<?php
+
+	// Enter the full URL to the script directory. Be sure to include "http://" at the beginning
+	$script_url = "http://www.yoursite.com";
+
+	// MySQL database host name. By default this is set to the computer running the script
+
+	$database_host = "localhost";
+	$database_user = "user_user";
+	$database_password = "password";
+	$database_name ="user_database";
+
+	// Paypal info
+	$paypal_successurl = "http://www.yoursite.com/thankyou.php";
+	$paypal_cancelurl = "http://www.yoursite.com/cancel.php";
+
+	// Site Name
+	$sitename = "YourSite";
+
+	// Logo Url
+	$siteLogo = "http://www.yoursite.com/images/logo.gif";
+
+	?>
