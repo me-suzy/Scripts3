9aa7a4ddd4afd70284dbc8756184e6492490d427
me-suzy <me.suzana@gmail.com>
2025-08-14 00:46:10 +0300

    Add Unsorted/phplive1.9.7/phplive/chat_box.php

---
+++ Unsorted/phplive1.9.7/phplive/chat_box.php
@@ -0,0 +1,22 @@
+<?
+	/*******************************************************
+	* COPYRIGHT OSI CODES - PHP Live!
+	*******************************************************/
+	include_once("./web/conf-init.php") ;
+	// include_once("./system.php") ;	// comment out to save load time (not used)
+
+	// initialize
+?>
+<?
+	// functions
+?>
+<?
+	// conditions
+?>
+<html>
+<head>
+<title> Chat [box main] </title>
+<!-- copyright OSI Codes Inc. http://www.osicodes.com [DO NOT DELETE] -->
+</head>
+<body marginheight="2" marginwidth="2" topmargin="2" leftmargin="2"><!-- mystery --></body>
+</html>
