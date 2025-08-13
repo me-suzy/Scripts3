6af72263ba0d79b551a4aad1b082adca5e199139
me-suzy <me.suzana@gmail.com>
2025-08-14 01:47:35 +0300

    Add Unsorted/slv18/storyline1.8/set/main/story.inc.php

---
+++ Unsorted/slv18/storyline1.8/set/main/story.inc.php
@@ -0,0 +1,95 @@
+<?
+
+$cl = new TheCleaner();
+
+$dl = new TheDB();
+$dl->connect() or die($dl->getError());
+$dl->debug=false;
+
+$table = $dl->select("s.*, u.*, c.*, cg.*, sg.*",
+	"sl18_stories s
+		LEFT JOIN sl18_chapters c ON c.csid=s.sid 
+		LEFT JOIN sl18_users u ON u.uid=c.cuid 
+		LEFT JOIN sl18_category cg ON cg.caid=s.scid 
+		LEFT JOIN sl18_subcategory sg ON sg.subid=s.ssubid",
+	array('s.sid'=>$_GET["no"]),
+	"ORDER BY cid ASC"
+	);
+
+$n = count($table);
+
+?>
+
+<table border='0' width='100%' cellspacing='4'>
+	<tr>
+		<td class='cleardis' colspan='2'>
+
+<?
+
+if (  $n == 0 ) {
+
+	print "This story does not exist";
+
+} else {
+
+?>
+			<form name='form'>
+			<table border='0' width='100%'>
+				<tr class='catdis'>
+					<td width='70%'>
+						<a href='main.php?cat=<?=$table[0]["scid"]?>'><?=$cl->preva($table[0]["caname"])?></a> > 
+						<a href='main.php?list=<?=$table[0]["ssubid"]?>'><?=$cl->preva($table[0]["subname"])?></a> > 
+						<?=$cl->preva($table[0]["sname"])?> > 
+						<select onChange='javascript:formHandler()' name='chapnav'>
+<?
+	for($i=0;$i<$n;$i++) {
+		$o = $i+1;
+		if ($o == 1)
+			print "<option value='story.php?no=" . $table[0]["sid"] . "'>" .$o. ". " . $cl->preva($table[$i]["cname"]) . "</option> \n";
+		else
+			print "<option value='story.php?no=" . $table[0]["sid"] . "&chapter=" . $o . "'>" .$o. ". " . $cl->preva($table[$i]["cname"]) . "</option> \n";		
+	}
+?>
+						</select> 
+<?
+	if ($table[0]["sadd"] != 1) {
+		print "<a href='addto.php?no=" . $table[0]["sid"] . "''>";
+		print "<img src='" . SL_ROOT_URL . "/base/html/Default/images/0.gif' border='0' alt='Add chapter to " . $table[0]["sname"] . "'></a>";
+	}
+?>
+					</td>
+					<td width='15%'>
+						Author: <a href='authors.php?no=<?=$table[0]["uid"]?>'><?=$table[0]["upenname"]?></a>
+					</td>
+					<td width='15%'>
+						Hits: <?=$table[0]["sthits"] = $table[0]["sthits"]+1?>
+					</td>
+				</tr><tr>
+					<td>
+						<?=$cl->prevb($table[0]["cbody"])?>
+					</td>
+				</tr>
+			</table>
+			</form>
+<?
+
+$dl->update("sl18_stories",array('sthits'=>$table[0]["sthits"]),array('sid'=>$_GET["no"]));
+}
+
+?>
+
+		</td>
+	</tr>
+
+	<? if ($n != 0) { ?>
+
+	<tr>
+		<td class='cleardis' width='50%'>
+			<center><a href='review.php?set=add&no=<?=$table[0]["sid"]?>'>Review <?=$cl->preva($table[0]["sname"])?></a></center>
+		</td>
+		<td class='cleardis' width='50%'>
+			<center><? addrating($cl->preva($table[0]["sname"]),$table[0]["sid"]) ?></center>
+		</td>
+	</tr>
+	<? } ?>
+</table>
