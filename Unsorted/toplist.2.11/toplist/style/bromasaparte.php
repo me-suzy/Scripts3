7272c4cc67d89f6dc7480ffa6f0ca049bcb4d1d0
me-suzy <me.suzana@gmail.com>
2025-08-14 02:05:22 +0300

    Add Unsorted/toplist.2.11/toplist/style/bromasaparte.php

---
+++ Unsorted/toplist.2.11/toplist/style/bromasaparte.php
@@ -0,0 +1,23 @@
+<?
+
+// BromasAparte style for Best Top List by Jose - http://www.best-scripts.tk
+// Support: support@best-scripts.tk
+//
+// Copyright 2002 (c) Szymon Kosok
+
+echo "<style>
+
+body{background:#003366;color:#FFFFFF;font-family:verdana;font-size:11pt;}
+a{color:#FFFFFF;font-family:verdana;font-size:11pt;}
+a:visited{color:#FFFFFF;font-family:verdana;font-size:11pt;}
+a:hover{color:#FFFFFF;font-family:verdana;font-size:11pt;}
+td{font-family:verdana;font-size:11pt;}
+input{font-family:verdana;font-size:11pt;}
+textarea{font-family:verdana;font-size:11pt;}
+</style>";
+
+$tableheader = "#F7AB1F";
+$tablebody = "#065479";
+$tableheadfont = "#C0C0C0";
+$tableborder = "#065479";
+?>
