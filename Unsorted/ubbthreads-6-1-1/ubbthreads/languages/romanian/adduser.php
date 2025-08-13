61ad515a024d991ef5ead6b712f058b1e26fc0ae
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:00 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =    "Your account needs to be approved by an Administrator before you can login.  You will receive an email once this has been done.";
+$ubbt_lang['APPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} has been approved.";
+$ubbt_lang['NOTAPPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} was not approved.  The reason for this is:";
+$ubbt_lang['NEWREGBODY'] = "A new user has registered at {$config['title']}. To approve this account, please visit";
+$ubbt_lang['NEWREG'] = "New User Registration at";
+$ubbt_lang['INVALID_DATE'] = "That is an invalid date.";
+$ubbt_lang['TOOYOUNG']  =       "Registration for this message board will require approval of your parent or guardian.  You may use the <a href=\"{$config['phpurl']}/coppaform.php\">Permission form</a> for this.";
+$ubbt_lang['UNDER13'] = "Sorry, we do not allow children under 13 to register.";
+$ubbt_lang['BAD_UNAME']  = 	"Nu puteti folosi HTML in nume si nu puteti avea spatii inainte sau dupa acesta.";
+$ubbt_lang['LONG_NAME']  = 	"Numele nu are lungimea corespunzatoare.";
+$ubbt_lang['BAD_EMAIL']  = 	"Nu aveti voie sa inregistrati un cont folosind o adresa de email din domeniul";
+$ubbt_lang['USER_EXISTS']  = 	"Acest nume este deja folosit.";
+$ubbt_lang['NO_MULTI']  = 	"Nu sint permise conturi multiple pe aceeasi adresa, iar noi avem deja un utilizator cu adresa de email pe care ati introdus-o.";
+$ubbt_lang['PASS_MATCH']  = 	"Parolele difera.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Parola trebuie sa aiba intre 4 si 20 de caractere.";
+$ubbt_lang['ILL_PASS']  = 	"Parola contine caractere ilegale. Va rugam sa folositi doar caractere alfanumerice.";
+$ubbt_lang['WEL_MSG']  = 	"Va rugam sa va editati profilul. In acest fel puteti completa unele informatii despre dvs, introduce o semnatura (care va fi adaugata la toate mesajele) si alege diversi parametrii legati de afisare (ordinea de sortare, modul de afisare etc).";
+$ubbt_lang['PASS_SUB']  = 	"Parola dvs. pentru";
+$ubbt_lang['PASS_BODY1']  = 	"Cineva de la IP-ul";
+$ubbt_lang['PASS_BODY2']  = 	"a inregistrat numele";
+$ubbt_lang['PASS_BODY3']  = 	"Parola pentru acest cont este";
+$ubbt_lang['NEW_CONFIRM']  = 	"Numele a fost inregistrat.";
+$ubbt_lang['NEW_BODY']  = 	"Numele a fost rezervat. In curind veti primi un email cu parola initiala. Dupa primirea parolei va puteti conecta si face post-uri sub acest nume. Multumim pentru inregistrarea utilizatorului pe forum-ul nostru. Va rugam sa va editati profilul inainte de a posta pe forum. In acest fel aveti posibilitatea sa schimbati dupa gustul si nevoile dvs. anumite setari ale forum-ului.";
+?>
