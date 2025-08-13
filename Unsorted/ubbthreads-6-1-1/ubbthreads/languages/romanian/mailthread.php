88d7efd5dca78078ae3d0625fe1e4ecfbf57b6c4
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:41 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/mailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/mailthread.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['MAIL_FRIEND']  = 	"Trimite acest thread unui prieten";
+$ubbt_lang['FRIEND_HEAD']  = 	"Introduceti adresa de email a persoanei careia vrei sa-i trimiteti mesajul";
+$ubbt_lang['SENT_FROM1']  = 	"Email-ul va fi trimis de pe adresa cu care v-ati inregistrat";
+$ubbt_lang['SENT_FROM2']  = 	"folosind nick-ul";
+$ubbt_lang['USE_REAL']  = 	"Daca vreti sa folositi numele real, introduceti-l aici:";
+$ubbt_lang['WHAT_EMAIL']  = 	"Adresa la care trimit:";
+$ubbt_lang['SEND_IT']  = 	"Trimite!";
+?>
