891cecc88fcbb4e7abc91019ddea5b4db0f5933e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:43:54 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/search.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/search.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['SEARCH_INFO']  = 	"Aby przeszukaæ forum wype³nij poni¿szy formularz.";
+$ubbt_lang['SEARCH_FORUM']  = 	"Forum do przeszukania";
+$ubbt_lang['ALL_FORUMS']  = 	"Wszystkie";
+$ubbt_lang['SEARCH_WORDS']  = 	"Szukane s³owa";
+$ubbt_lang['SEARCH_OPT']  = 	"Opcje";
+$ubbt_lang['SEARCH_ENT']  = 	"Znajd¼ ca³e wyra¿enie";
+$ubbt_lang['SEARCH_AND']  = 	"Znajd¼ wszystkie s³owa";
+$ubbt_lang['SEARCH_OR']  = 	"Znajd¼ conajmniej jedno s³owo";
+$ubbt_lang['SEARCH_USER']  = 	"Po autorze";
+$ubbt_lang['SEARCH_DATE']  = 	"Okres czasu";
+$ubbt_lang['SEARCH_1DAY']  = 	"Nowsze ni¿ 1 dzieñ";
+$ubbt_lang['SEARCH_2DAY']  = 	"Nowsze ni¿ 2 dni";
+$ubbt_lang['SEARCH_1WEEK']  = 	"Nowsze ni¿ tydzieñ";
+$ubbt_lang['SEARCH_2WEEK']  = 	"Nowsze ni¿ 2 tygodnie";
+$ubbt_lang['SEARCH_3WEEK']  = 	"Nowsze ni¿ 3 tygodnie";
+$ubbt_lang['SEARCH_1MONTH']  = 	"Nowsze ni¿ miesi±c";
+$ubbt_lang['SEARCH_3MONTH']  = 	"Nowsze ni¿ 3 miesi±ce";
+$ubbt_lang['SEARCH_6MONTH']  = 	"Nowsze ni¿ 6 miesiêcy";
+$ubbt_lang['SEARCH_1YEAR']  = 	"Nowsze ni¿ rok";
+$ubbt_lang['SEARCH_ALLPOST']  = 	"Wszystkie wiadomo¶ci";
+$ubbt_lang['SEARCH_NUM']  = 	"Ile znalezionych wiadomo¶ci pokazaæ na jednej stronie (ignorowane przy przeszukiwaniu wiêcej ni¿ jednego forum)";
+?>
