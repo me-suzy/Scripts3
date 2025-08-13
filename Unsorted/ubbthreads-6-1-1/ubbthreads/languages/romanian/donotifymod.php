19db49b606cef9d6404b59cd7a3ff3a394545d8a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:18 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/donotifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/donotifymod.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['POST_PROB']  = 	"A aparut o problema la cautarea mesajului in baza de date.";
+$ubbt_lang['BAD_GROUP']  = 	"Acces interzis!";
+$ubbt_lang['NO_B_INFO']  = 	"Forum-ul nu exista!";
+$ubbt_lang['NOTIF_HEAD']  = 	"e de parere ca ar trebui sa te uiti peste mesajul intitulat";
+$ubbt_lang['TO_READ']  = 	"Mesajul poate fi gasit la adresa:";
+$ubbt_lang['NOTIFY_SENT']  = 	"Moderator(ii) au fost anuntati";
+$ubbt_lang['NOTIFY_CONFIRM']  = 	"Moderatorul/moderatorii acestui forum au fost anuntati.";
+?>
