b0dd109ab9155ad04057f2286e58b5f22088ca59
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:22 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/dorateuser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/dorateuser.php
@@ -0,0 +1,7 @@
+<?
+$ubbt_lang['INVALID_RATING'] = "That is an invalid rating.";
+$ubbt_lang['NOMORERATE'] = "Ya has valorado a este usurio anteriormente.";
+$ubbt_lang['THANKS'] = "Muchas gracias.";
+$ubbt_lang['RATED']	= "Se ha anotado tu valoración por este miembro.";
+$ubbt_lang['CANTRATE']  = "No puedes votarte a ti mismo.";
+?>
