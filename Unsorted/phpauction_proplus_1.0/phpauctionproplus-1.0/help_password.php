fd1f0b656e5abbe6df115a9252637e99cbbc245e
me-suzy <me.suzana@gmail.com>
2025-08-14 00:12:10 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/help_password.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/help_password.php
@@ -0,0 +1,45 @@
+<?#//v.1.0.0
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+
+
+	// Include messages file	
+   require('./includes/messages.inc.php');
+  
+   // Connect to sql server & inizialize configuration variables
+   require('./includes/config.inc.php');
+   
+
+	
+?>
+
+<HTML>
+<HEAD>
+<TITLE></TITLE>
+
+
+</HEAD>
+
+<BODY  BGCOLOR="#FFFFFF" TEXT="#08428C" LINK="#08428C" VLINK="#08428C" ALINK="#08428C">
+
+<SCRIPT Language=PHP>
+
+require("header.php");
+
+
+	print "<FONT FACE=\"Verdana,Helvetica,Arial\" SIZE=\"4\">";
+ 	print "<BR><CENTER><B>$MSG_164</B><BR><BR>$MSG_188</CENTER>";
+
+
+	print "<FONT FACE=\"Verdana,Helvetica,Arial\" SIZE=\"2\">";
+ 	print "<BR><CENTER><B>$MSG_186</B></CENTER>";
+
+</SCRIPT>
+
+<? require("./footer.php"); ?>
+</BODY>
+</HTML>
