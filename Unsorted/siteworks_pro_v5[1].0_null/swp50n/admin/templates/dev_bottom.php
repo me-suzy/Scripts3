883a0458311b40c40e55a58421abccafb08d46ff
me-suzy <me.suzana@gmail.com>
2025-08-14 01:45:14 +0300

    Add Unsorted/siteworks_pro_v5[1].0_null/swp50n/admin/templates/dev_bottom.php

---
+++ Unsorted/siteworks_pro_v5[1].0_null/swp50n/admin/templates/dev_bottom.php
@@ -0,0 +1,22 @@
+            </td>
+          </tr>
+          <tr>
+            <td width="79%" valign="top" height="26">
+              <p style="margin-top: 10; margin-bottom: 10" align="center">
+				<span class="BodyText">
+					<?php echo $appName . " v" . $appVersion; ?> <!--CyKuH [WTN]-->SiteCubed.com, <?php echo date("Y"); ?>.
+				</span>
+			  </p>
+            </td>
+          </tr>
+        </table>
+      </div>
+  <center>
+    </td>
+  </tr>
+  </table>
+</div>
+</body>
+</html>
+
+<?php ob_end_flush(); ?>
