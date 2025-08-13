fe4c5c896621ad3718b5c3a57b12c1a9b70e0f46
me-suzy <me.suzana@gmail.com>
2025-08-14 01:51:55 +0300

    Add Unsorted/Stardevelop_livehelp[v1.2][nullified]/livehelp/admin/control_panel/settings_toolbar.php

---
+++ Unsorted/Stardevelop_livehelp[v1.2][nullified]/livehelp/admin/control_panel/settings_toolbar.php
@@ -0,0 +1,41 @@
+<?php
+/*
+stardevelop.com Live Help
+International Copyright stardevelop.com
+
+You may not distribute this program in any manner,
+modified or otherwise, without the express, written
+consent from stardevelop.com
+
+You may make modifications, but only for your own 
+use and within the confines of the License Agreement.
+All rights reserved.
+
+Selling the code for this program without prior 
+written consent is expressly forbidden. Obtain 
+permission before redistributing this program over 
+the Internet or in any other medium.  In all cases 
+copyright and header must remain intact.  
+*/
+?>
+<table border="0" cellspacing="1" cellpadding="1">
+  <tr> 
+    <td width="50" height="22"><div align="center"><a href="./settings_index.php?<?php echo(SID); ?>"><img src="../../icons/utilities.gif" alt="<?php echo($language['general']); ?>" width="22" height="22" border="0"></a></div></td>
+    <td width="50" height="22"><div align="center"><a href="settings_display.php?<?php echo(SID); ?>"><img src="../../icons/appearance_small.gif" alt="<?php echo($language['display']); ?>" width="22" height="22" border="0"></a></div></td>
+    <td width="50" height="22"><div align="center"><a href="./settings_links.php?<?php echo(SID); ?>"><img src="../../icons/network.gif" alt="<?php echo($language['links']); ?>" width="22" height="22" border="0"></a></div></td>
+    <td width="50" height="22"><div align="center"><a href="./settings_time.php?<?php echo(SID); ?>"><img src="../../icons/clock.gif" alt="<?php echo($language['time']); ?>" width="22" height="22" border="0"></a></div></td>
+    <td width="50" height="22"><div align="center"><a href="./settings_code.php?<?php echo(SID); ?>"><img src="../../icons/code.gif" alt="<?php echo($language['code']); ?>" width="22" height="22" border="0"></a></div></td>
+  </tr>
+  <tr> 
+    <td width="50"><div align="center"> <font size="1" face="Verdana, Arial, Helvetica, sans-serif"> 
+        <a href="./settings_index.php?<?php echo(SID); ?>" class="normlink"><?php echo($language['general']); ?></a></font></div></td>
+    <td width="50"><div align="center"><font size="1" face="Verdana, Arial, Helvetica, sans-serif"> 
+        <a href="settings_display.php?<?php echo(SID); ?>" class="normlink"><?php echo($language['display']); ?></a></font></div></td>
+    <td width="50"><div align="center"><font size="1" face="Verdana, Arial, Helvetica, sans-serif"><a href="./settings_links.php?<?php echo(SID); ?>" class="normlink"><?php echo($language['links']); ?></a></font> 
+      </div></td>
+    <td width="50"><div align="center"><font size="1" face="Verdana, Arial, Helvetica, sans-serif"> 
+        <a href="./settings_time.php?<?php echo(SID); ?>" class="normlink"><?php echo($language['time']); ?></a></font></div></td>
+    <td width="50"><div align="center"><font size="1" face="Verdana, Arial, Helvetica, sans-serif"><a href="./settings_code.php?<?php echo(SID); ?>" class="normlink"><?php echo($language['code']); ?></a></font> 
+      </div></td>
+  </tr>
+</table>
