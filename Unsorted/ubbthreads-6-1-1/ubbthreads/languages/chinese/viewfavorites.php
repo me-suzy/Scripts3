e7a7c2be660ce970593c67c233ae6d7bc5d936a2
me-suzy <me.suzana@gmail.com>
2025-08-14 02:30:22 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/viewfavorites.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/viewfavorites.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['VIEW_FAV']  =  "¹Û¿´ËùÓÐÎÒ×î°®µÄ»°Ìâ";
+$ubbt_lang['VIEW_REM']  =  "¹Û¿´ËùÓÐ±¸ÍüÂ¼ÖÐµÄÎÄÕÂ";
+$ubbt_lang['LAST_POST']  =  "×îºóÕÅÌùÊ±¼ä";
+$ubbt_lang['REMOVE']     =  "ÒÆ³ý";
+?>
