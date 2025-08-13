3db8ec7f846c95b4771a9af781920ae80f311c18
me-suzy <me.suzana@gmail.com>
2025-08-14 01:00:04 +0300

    Add Unsorted/phpUploadCenter/include/lt.php

---
+++ Unsorted/phpUploadCenter/include/lt.php
@@ -0,0 +1,237 @@
+<?
+
+// Header and footer files
+
+$headerpage="include/header.htm";
+$footerpage="include/footer.htm";
+$infopage="include/info.htm";
+
+$charsetencoding="windows-1257";
+$uploadcentercaption="SatTV - Upload Center";
+$uploadcentermessage="Pastaba:&nbsp;&nbsp; Koduotos TV &#382;i&#363;r&#279;jimas be registracijos yra neteis&#279;tas!<br>Visi failai &#303;ra patalpinti tik pa&#382;intiniais tikslais!<br>Niekas neatsako u&#382; fail&#363; turin&#303;, juose gali <br>&nbsp;b&#363;ti virusai arba pakenkti j&#363;s&#371; sistemai!";
+
+$mess=array(
+"0" => "",
+"1" => "Sausis",
+"2" => "Vasaris",
+"3" => "Kovas",
+"4" => "Balandis",
+"5" => "Geguþë",
+"6" => "Birþelis",
+"7" => "Liepa",
+"8" => "Rugpjûtis",
+"9" => "Rugsëjis",
+"10" => "Spalis",
+"11" => "Lapkritis",
+"12" => "Gruodis",
+"13" => "Ðiandien",
+"14" => "Vakar",
+"15" => "Failo vardas",
+"16" => "Reitingas",
+"17" => "Dydis",
+"18" => "Pakrautas",
+"19" => "Apraðymas",
+"20" => "Pakrauti faila",
+"21" => "Jûsø failas",
+"22" => "Apraðymas failo",
+"23" => "Persisiøsti",
+"24" => "Rûðiavimas",
+"25" => "Namø puslapis",
+"26" => "Failas",
+"27" => "Spausdinimas",
+"28" => "Uþdaryti",
+"29" => "Eiti atgal",
+"30" => "Ðis failas buvo iðtrintas",
+"31" => "Negaliu atidaryti failo",
+"32" => "Eiti atgal",
+"33" => "Siuntimo klaida !",
+"34" => "Jûs turite pasirinkti faila",
+"35" => "Atgal",
+"36" => "Failas",
+"37" => "Persiøstas sëkmingai",
+"38" => "Failas",
+"39" => "Jau egzistuoja",
+"40" => "Failas persiøstas sëkmingai",
+"41" => "Kalba sëkmingai pasirinkta",
+"42" => "Sveiki atvykæ á SatTV - Upload Center!",
+"43" => "Iðnaudota ",
+"44" => "Parodyti visø dienø failus",
+"45" => "Paþeistas ZIP archyvas!",
+"46" => "Archyvo sudëtis:",
+"47" => "Data/Laikas",
+"48" => "Direktorija",
+"49" => " Yra uþdrausta ákrauti failà vardø %s",
+"50" => "virðytas maksimalus leistinas failo dydis",
+
+"51" => "Informacija",
+"52" => "Pasirinkit rëmelá",
+"53" => "Rëmelis",
+"54" => "Sveiki atvykæ",
+"55" => "Einamasis laikas",
+"56" => "Vartotojas",
+"57" => "Vartuotojo vardas",
+"58" => "Registras",
+"59" => "Registracija",
+"60" => "Sekmadienis",
+"61" => "Pirmadienis",
+"62" => "Antradienis",
+"63" => "Treèiadienis",
+"64" => "Ketvirtadienis",
+"65" => "Penktadienis",
+"66" => "Ðeðtadienis",
+"67" => "Iðsiusti",
+"68" => "Mail file",
+"69" => "File has been mailed to %s address.",
+"70" => "File uploaded by user: %s",
+"71" => "Prijungimas",
+"72" => "Atjungimas",
+"73" => "Áëjimas",
+"74" => "Anonimas",
+"75" => "Paprastasis Vartotojas",
+"76" => "Áteisintas Vartotojas",
+"77" => "Administratorius",
+"78" => "Privati zona",
+"79" => "Bendra zona",
+"80" => "Jus ávedët neteisingà áskaitos vardà ar slaptaþodá.",
+"81" => "Mano profilis",
+"82" => "Þiûrëti/pakeisti mano profilá",
+"83" => "Slaptaþodis",
+"84" => "Pasirinkit kalbà",
+"85" => "Pasirinkit laiko zonà",
+"86" => "Jûsø einamasis laikas",
+"88" => "Praðau ávesti aktyvø e-mail adresà.",
+"89" => "Jûsø aktyvacijos kodas bus iðsiøstas á jûsø e-mailà.",
+"90" => "File uploaded: ",
+"91" => "Praðau ávesti jûsø e-mail adresà, panauduotà registracijai.",
+"92" => "File size: ",
+"93" => "Praðau paraðykit jûsø vardà ir e-mail adresà",
+"94" => "Registracija bûtina. Praðau uþsiregistruoti.",
+"95" => " Registracija nebûtina. Jûs galite uþsiregistruoti jai norite suteikti savo vardà visiems jûsø ákrautiems failams. Niekas daugiau negales ákrauti failus jûsø vardø.",
+
+"96" => "Skin selected.",
+"97" => "Refresh",
+"98" => "Please, enter your login name and password",
+"99" => "Still not registered? - Register here!",
+"100" => "Forgotten your password?",
+"101" => "Please, go %s back %s and try again.",
+"102" => "You succesfully logged out.",
+"103" => "User name is invalid. The name must be not longer than 12 symbols and can consists of latin symbols and digits only. Name can also contain '-', '_', and space symbols inside.",
+"104" => "The '%s' you picked has been taken.",
+"105" => "Confirm password",
+"106" => "Passwords do not match.",
+"107" => "The format of entered e-mail address is invalid.",
+"108" => "Thank you for registering. Please do not forget your password as it has been encrypted in our database and we cannot retrieve it for you. However, should you forget your password we provide an easy to use script to generate and email a new, random, password.",
+"109" => "You can %s enter to Upload Center here. %s",
+"110" => "Your activation code has been e-mailed to you. You must activate your account within 2 days or account will be automatically removed.",
+"111" => "
+Dear %s,
+
+Thank You for registration at our Upload Center.
+
+Because your security is importance to us, your account will need to be activated upon receipt.
+
+Your personal activation code is: %s
+(please note: this is not your password)
+
+Activating Your account is simple:
+1. Visit us at $installurl/activate.php and we will guide you through the process
+2. Enter your account name and activation code.
+3. Click on the 'Activate account' button.
+
+Regards,
+$admin_name
+Email: mailto:$admin_email
+Web Page: $installurl",
+"112" => "Activate account",
+"113" => "Please, activate your account",
+"114" => "Activation code",
+"115" => "Your account now is active.",
+"116" => "Please %s enter here %s.",
+"117" => "The entered account name or activation code is invalid.",
+"118" => "Account already active.",
+"119" => "I wish to receive to my e-mail everyday digest of uploaded files.",
+"120" => "Change your password.",
+"121" => "Your old password",
+"122" => "The entered account name is not exists.",
+"123" => "The entered e-mail address is not valid.",
+"124" => "Your new password has been sent to your e-mail.",
+"125" => "
+Dear Upload Center user,
+
+Your new Upload Center password is %s
+
+Regards,
+$admin_name
+Email: mailto:$admin_email
+Web Page: $installurl",
+"126" => "Customize your account settings",
+"127" => "Apply",
+"128" => "Your profile saved.",
+"129" => "Your password changed.",
+"130" => "You typed invalid previous password.",
+"131" => "You must specify your new password.",
+"132" => "CONFIGURATION",
+"133" => "Upload",
+"134" => "Language & timezone",
+"135" => "Account statistics",
+"136" => "Your account has been created:",
+"137" => "User management",
+"138" => "Viewer (view only)",
+"139" => "Uploader (upload only)",
+"140" => "Account '%s' changed successfully",
+"141" => "Latest",
+"142" => "All",
+"143" => "New e-mail address takes effect after confirmation. Confirmation code has been e-mailed to your new mail address. See instructions inside the letter.",
+"144" => "
+Dear %s,
+
+Because your security is importance to us, your new e-mail address will need to be confirmed upon receipt.
+
+Your personal confirmation code is: %s
+
+Activating e-mail address is simple:
+1. Visit us at $installurl/confirm.php and we will guide you through the process
+2. Enter your account name and confirmation code.
+3. Click on the 'Confirm' button.
+
+Regards,
+$admin_name
+Email: mailto:$admin_email
+Web Page: $installurl",
+"145" => "Please, confirm your new e-mail address.",
+"146" => "Confirmation code",
+"147" => "Confirm",
+"148" => "Nothing to confirm.",
+"149" => "The entered account name or confirmation code is invalid.",
+"150" => "Your new e-mail address '%s' confirmed.",
+"151" => "Files uploaded",
+"152" => "Files downloaded",
+"153" => "Files e-mailed",
+"154" => "Account created",
+"155" => "Last access time",
+"156" => "Status",
+"157" => "Active status",
+"158" => "Receive digest",
+"159" => "e-mail",
+"160" => "Total:",
+"161" => "account(s)",
+"162" => "Delete account",
+"163" => "Shown %s account(s) of %s",
+"164" => "Configure Upload Center",
+"165" => "Edit files",
+"166" => "Edit file",
+"167" => "File %s has been changed succesfully",
+"168" => "Save",
+"169" => "Delete",
+"170" => "Delete files",
+"171" => "Mirror",
+"172" => "Yes",
+"173" => "No",
+"174" => "Active",
+"175" => "Inactive",
+"176" => "Unautorized",
+"177" => "Sorry, but server could not execute the mail program.",
+"178" => "Your registration failed. Please, try again later.",
+"179" => "Please, try again later."
+);
+?>
