af88c416f771d1c9855c9cae4c9593110717f311
me-suzy <me.suzana@gmail.com>
2025-08-14 02:41:55 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"Vi kan dessverre ikke finne en profil for brukernavnet";
+$ubbt_lang['EMAIL_HEAD']  = 	"E-post-innstillinger for";
+$ubbt_lang['PROF_EDIT']  = 	"Innstillingene under kan endres.";
+$ubbt_lang['PROF_SUBS']  = 	"E-post-innstillinger/abonnement";
+$ubbt_lang['PROF_NOTIF']  = 	"Ønsker du å få beskjed pr. e-post når du mottar private beskjeder?";
+$ubbt_lang['PROF_REPLIES']  = 	"Ønsker du å få alle svar på dine innlegg sendt til deg i e-post?";
+$ubbt_lang['SUB_STAT']  = 	"Abonnementsstatus for følgende fora (motta daglig sammendrag av alle innlegg til forumet).";
+$ubbt_lang['SUB_SHOW']  = 	"(A = Abonner, AA = Avbryt abonnement)";
+$ubbt_lang['TEXT_S']  = 	"A";
+$ubbt_lang['TEXT_U']  = 	"AA";
+?>
