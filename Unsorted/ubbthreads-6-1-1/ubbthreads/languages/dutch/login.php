9b03fd654d0f2647b41e38f5fcb3db6c303e5a80
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:49 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/login.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/login.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['FORGOT_PASS']   = "Wachtwoord vergeten?";
+$ubbt_lang['FORGOT_ENTER']  = "Vul de gebruikersnaam en het email adres (dat je gebruikt hebt voor registratie) in.";
+$ubbt_lang['REMEMBER_ME']   =  "Wachtwoord opslaan.";
+$ubbt_lang['LOGIN_PROMPT']  =  "Inloggen a.u.b.";
+$ubbt_lang['LOGIN_PROMPT2']  = 	"Vul je gebruikersnaam en wachtwoord in.  Als je nog niet over deze gegevens beschikt, kun je";
+$ubbt_lang['REG_ONE']  = 	"registreren";
+$ubbt_lang['PASSWORD_TEXT']  = 	"Wachtwoord";
+$ubbt_lang['BUTT_FORGOT']  = 	"Wachtwoord opvragen";
+?>
