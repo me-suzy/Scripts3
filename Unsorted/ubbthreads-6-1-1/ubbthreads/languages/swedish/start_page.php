ad59116e92f4757eb0958f2161f3c1db0e4a4549
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:42 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"Förfrågan om lösenord";
+$ubbt_lang['PASS_REQ_BOD1']  = 	"någon från IP-adress";
+$ubbt_lang['PASS_REQ_BOD2']  = 	"Förfrågan om tillfälligt lösenord för användare";
+$ubbt_lang['PASS_REQ_BOD3']  = 	"För webbplatsen '{$config['title']}'.  Det här är ett tillfälligt lösenord som kan användas om du har glömt ditt vanliga lösenord.  Om det inte är du som efterfrågat ett nytt lösenord så kan du ignorera detta brev.  Det tillfälliga lösenordet för detta användarnamnet är --";
+$ubbt_lang['PASS_MAILED']  = 	"Lösenord skickat";
+$ubbt_lang['PASS_MAILED2']  = 	"Ett tillfälligt lösenord för denna användare har skickats.";
+$ubbt_lang['NO_EMAIL']  = 	"Kan inte hitta e-postadressen för";
+?>
