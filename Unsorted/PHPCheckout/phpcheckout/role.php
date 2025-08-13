08c228820ebee2fa339c0f09e1d40bde79929ff8
me-suzy <me.suzana@gmail.com>
2025-08-14 00:33:54 +0300

    Add Unsorted/PHPCheckout/phpcheckout/role.php

---
+++ Unsorted/PHPCheckout/phpcheckout/role.php
@@ -0,0 +1,16 @@
+<!-- start of role.php -->
+<SELECT NAME="role" SIZE="1">
+	<OPTION VALUE="<?php echo $role;?>" SELECTED><?php echo $role;?></OPTION>
+	<OPTION VALUE="Amateur">Amateur</OPTION>
+	<OPTION VALUE="Recreational">Recreational</OPTION>									
+	<OPTION VALUE="Champion">Champion</OPTION>
+	<OPTION VALUE="Developer">Developer</OPTION>
+	<OPTION VALUE="Director">Director</OPTION>
+	<OPTION VALUE="Economic Buyer">Economic Buyer</OPTION>
+	<OPTION VALUE="Project Manager">Project Manager</OPTION>
+	<OPTION VALUE="Technical Buyer">Technical Buyer</OPTION>
+	<OPTION VALUE="Technical Lead">Technical Lead</OPTION>
+	<OPTION VALUE="Webmaster">Webmaster</OPTION>
+	<OPTION VALUE="No match">No match</OPTION>			
+	</SELECT>
+<!-- end of role.php -->
