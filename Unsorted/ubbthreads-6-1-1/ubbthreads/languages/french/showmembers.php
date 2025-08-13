c777886efc7555e908d3c4da9e53a0b99ca20412
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:21 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/showmembers.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/showmembers.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['USER_STATUS']  = 	"Statut";
+$ubbt_lang['TEXT_HOME']  = 	"Page personnelle";
+$ubbt_lang['TOTAL_POSTS']  = 	"Nombre de Messages";
+$ubbt_lang['PROF_ICQ']  = 	"Numéro ICQ";
+$ubbt_lang['REGGED_ON']  = 	"Enregistré le";
+$ubbt_lang['PAGE_TEXT']  = 	"Pages";
+?>
