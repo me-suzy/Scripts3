0127c09d747ddc9f52079691763074c288f23ddb
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:07 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/login.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/login.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['FORGOT_PASS']   = "§Ú§Ñ°O§Úªº±K½X¤F?";
+$ubbt_lang['FORGOT_ENTER']  = "½Ð¿é¤J±zªº Email «H½c©Î¬Oµù¥Uªº±b¸¹¡A¥»¨t²Î±N·|±H°e¤@«Ê¥]§tÁ{®É±K½Xªº«H¥ó¨ì±zªº«H½c¤¤¡C";
+
+$ubbt_lang['REMEMBER_ME']   =  "°O¦í§Úªº±b¸¹¤Î±K½X¡C";
+$ubbt_lang['LOGIN_PROMPT2'] =	"½Ð¿é¤J±b¸¹©M±K½Xµn¤J¨t²Î¡A¦pªG±z¬O·s¶iºô¤Í¡A©|¥¼µù¥U±MÄÝ±b¸¹¡A½Ð°O±o¥ýµù¥U¨ú±o±MÄÝ±b¸¹¡A¤~¥i¥H¦s¨ú¥»¯¸¬ÛÃö°Q½×°Ï¤Î¥\¯à¡I";
+$ubbt_lang['LOGIN_PROMPT'] =	"½Ð¥ýµn¤J";
+$ubbt_lang['REG_ONE'] =	"[ÂI«ö¦¹¥ß§Yµù¥U]";
+$ubbt_lang['PASSWORD_TEXT'] =	"³qÃö±K½X";
+?>
