3644c3c0748cca8a1940e4e3f7cca2524170bf4d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:11 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"Pedido de contrase&ntilde;a";
+$ubbt_lang['PASS_REQ_BOD1']  = 	"Alguien con la direcci&oacute;n IP";
+$ubbt_lang['PASS_REQ_BOD2']  = 	"pedir una contrase&ntilde;a temporal para el Miembro del foro";
+$ubbt_lang['PASS_REQ_BOD3']  = 	"para la pagina web '{$config['title']}'.  Esta es una contrase&ntilde;a temporal, la cual, puedes usar si has olvidado tu contrase&ntilde;a original.  Si no has solicitado esto, se ruega simplemente ignorar este mensaje.  La contrase&ntilde;a temporal para este miembro del foro es --";
+$ubbt_lang['PASS_MAILED']  = 	"contrase&ntilde;a Enviada";
+$ubbt_lang['PASS_MAILED2']  = 	"Una contrase&ntilde;a temporal ha sido enviada por correo a este miembro del foro.";
+$ubbt_lang['NO_EMAIL']  = 	"No se puede encontrar la direcci&oacute;n de email para";
+?>
