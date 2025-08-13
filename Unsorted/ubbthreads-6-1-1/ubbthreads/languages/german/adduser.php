04b7e271332438e89c09f9b71cc47f377449bb81
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:44 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =    "Your account needs to be approved by an Administrator before you can login.  You will receive an email once this has been done.";
+$ubbt_lang['APPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} has been approved.";
+$ubbt_lang['NOTAPPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} was not approved.  The reason for this is:";
+$ubbt_lang['NEWREGBODY'] = "A new user has registered at {$config['title']}. To approve this account, please visit";
+$ubbt_lang['NEWREG'] = "New User Registration at";
+$ubbt_lang['INVALID_DATE'] = "Das angegebene Datum ist nicht korrekt.";
+$ubbt_lang['TOOYOUNG']  =       "Es kann kein neues Benutzerkonto angelegt werden, da Du j&uuml;nger als 13 bist. Im Einklang mit dem <a href=\"http://www.ftc.gov/opa/1999/9910/childfinal.htm\">COPPA</a>-Gesetz k&ouml;nnen wir ohne elterliche Genehmigung kein Benutzerkonto anlegen.";
+$ubbt_lang['UNDER13'] =         "Kinder unter 13 Jahren d&uuml;rfen sich leider nicht registrieren.";
+$ubbt_lang['BAD_UNAME']  = 	"Sie d&uuml;rfen keine HTML-Zeichen sowie Leerzeichen am Anfang oder Ende des Benutzernamens verwenden.";
+$ubbt_lang['LONG_NAME']  = 	"Der Benutzername hat nicht die richtige L&auml;nge.";
+$ubbt_lang['BAD_EMAIL']  = 	"Sie haben keine Erlaubnis, einen Benutzernamen von folgender Email-Dom&auml;ne zu registrieren:";
+$ubbt_lang['USER_EXISTS']  = 	"Dieser Benutzername ist schon vergeben.";
+$ubbt_lang['NO_MULTI']  = 	"Mehrere Benutzer mit derselben Email-Adresse sind nicht erlaubt und es existiert bereits ein Benutzer unter der von Ihnen angegebenen Email-Adresse.";
+$ubbt_lang['PASS_MATCH']  = 	"Die Passw&ouml;rter stimmen nicht &uuml;berein.";
+$ubbt_lang['PASS_TOO_LONG']  = "Das Passwort mu&szlig; zwischen 4 und 20 Zeichen lang sein.";
+$ubbt_lang['ILL_PASS']  = 	"Das Pa&szlig;wort beinhaltet nicht erlaubte Zeichen.  Bitte benutzen Sie nur alphanumerische Zeichen.";
+$ubbt_lang['WEL_MSG']  = 	"Bitte nehmen Sie sich etwas Zeit, um Ihr Profil zu editieren. Sie k&ouml;nnen ein paar Infos &uuml;ber Sich selbst angeben, eine Signatur einrichten und die Darstellung des Forums anpassen (Sortierreihenfolge, Anzeigemodus, flacher oder gestaffelter Modus, etc.).";
+$ubbt_lang['PASS_SUB']  = 	"Ihr Passwort f&uuml;r";
+$ubbt_lang['PASS_BODY1']  = 	"Jemand hat von der IP-Adresse";
+$ubbt_lang['PASS_BODY2']  = 	"folgenden Benutzernamen registriert:";
+$ubbt_lang['PASS_BODY3']  = 	"Das Passwort lautet";
+$ubbt_lang['NEW_CONFIRM']  = 	"Benutzername wurde registriert.";
+$ubbt_lang['NEW_BODY']  = 	"Ihr Benutzername wurde reserviert.  Sie sollten in K&uuml;rze eine eMail mit einem Pa&szlig;wort erhalten.  Mit diesem Pa&szlig;wort k&ouml;nnen Sie sich sofort in das Forum einloggen und Nachrichten hinzufügen.  Vielen Dank f&uuml;r die Registrierung.  Bitte nehmen Sie sich nach dem ersten Einloggen etwas Zeit, um Ihr pers&ouml;nliches Profil zu bearbeiten.  Es besteht die Möglichkeit, das Layout des Forums individuell Ihrem pers&ouml;nlichen Geschmack anzupassen.";
+?>
