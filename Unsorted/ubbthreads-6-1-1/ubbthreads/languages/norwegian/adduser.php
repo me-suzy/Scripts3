2466acd56ea02b8c666cf9e34252531f288bba0e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:41:25 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =    "Your account needs to be approved by an Administrator before you can login.  You will receive an email once this has been done.";
+$ubbt_lang['APPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} has been approved.";
+$ubbt_lang['NOTAPPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} was not approved.  The reason for this is:";
+$ubbt_lang['NEWREGBODY'] = "A new user has registered at {$config['title']}. To approve this account, please visit";
+$ubbt_lang['NEWREG'] = "New User Registration at";
+$ubbt_lang['INVALID_DATE'] = "That is an invalid date.";
+$ubbt_lang['TOOYOUNG']  =       "Registration for this message board will require approval of your parent or guardian.  You may use the <a href=\"{$config['phpurl']}/coppaform.php\">Permission form</a> for this.";
+$ubbt_lang['UNDER13'] = "Sorry, we do not allow children under 13 to register.";
+$ubbt_lang['BAD_UNAME']  = 	"Det er ikke tillatt å bruke HTML-tegn i brukernavnet eller å sette inn mellomrom før eller etter brukernavnet.";
+$ubbt_lang['LONG_NAME']  = 	"Brukernavnet er ikke langt nok.";
+$ubbt_lang['BAD_EMAIL']  = 	"Det er ikke tillatt å registrere et brukernavn fra dette e-postdomenet";
+$ubbt_lang['USER_EXISTS']  = 	"Det ønskede brukernavn er allerede brukt av en annen.";
+$ubbt_lang['NO_MULTI']  = 	"Det er ikke mulig å registrere flere enn ett brukernavn pr. e-postadresse, og det er allerede registrert en bruker med e-postadressen du skrev inn.";
+$ubbt_lang['PASS_MATCH']  = 	"Passordene du oppga er ikke like.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Passordet skal være på mellom 4 og 20 tegn.";
+$ubbt_lang['ILL_PASS']  = 	"Passordet inneholder ulovlige tegn.  Vennligst bruk kun alfanumeriske tegn. (a-z og 0-9)";
+$ubbt_lang['WEL_MSG']  = 	"Det kan være en god ide å endre profil. Ved å endre din profil har du bl.a. mulighet for å skrive supplerende opplysninger om deg selv, skrive en signatur som vil bli satt inn i bunnen av alle dine innlegg, samt velge hvordan du vil at forumet skal se ut.";
+$ubbt_lang['PASS_SUB']  = 	"Passordet for";
+$ubbt_lang['PASS_BODY1']  = 	"En person fra IP-adressen";
+$ubbt_lang['PASS_BODY2']  = 	"har registrert brukernavnet";
+$ubbt_lang['PASS_BODY3']  = 	"Passordet for dette brukernavn er";
+$ubbt_lang['NEW_CONFIRM']  = 	"Brukernavnet er nå registrert.";
+$ubbt_lang['NEW_BODY']  = 	"Ditt brukernavn har blitt reservert. Du vil om et øyeblikk motta en e-post med passordet. Så snart du har passordet, kan du logge deg inn og sende innlegg med dette brukernavnet. Takk for at du deltar i vårt forum. Det kan være en god ide å endre din profil før du sender innlegg. Ved å endre din profil kan du skreddersy forumet til dine behov.";
+?>
