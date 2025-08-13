6936204a8714de5ecdd2c9268e069c1581167405
me-suzy <me.suzana@gmail.com>
2025-08-14 01:05:18 +0300

    Add Unsorted/phpYellowPages/phpYellow/installHello.php

---
+++ Unsorted/phpYellowPages/phpYellow/installHello.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Install phpYellow Pages - Dreamriver.com - Fine Internet Applications Since 1995</title>
+</head>
+
+<body bgcolor="navy" text="white">
+
+
+<?php 
+/*
+If you can read these comments then php is NOT working.
+Contact your internet service provider and ask them to enable php on your web server.
+This installation can not proceed.
+Click cancel to stop this installation.
+Nothing will work properly without php.
+*/
+echo"<font size=7 color=\"orange\"><b>Hello World!</b></font>";
+?>
+
+
+</body>
+</html>
+
+
+	
