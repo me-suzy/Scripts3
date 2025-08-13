451451f60e9d1d2a11ac910d69649544b6fb4e0d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:36 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/showmembers.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/showmembers.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['USER_STATUS']  = 	"¨­¥÷";
+$ubbt_lang['TEXT_HOME']  = 	"ºô­¶";
+$ubbt_lang['TOTAL_POSTS']  = 	"µoªí¤å³¹Á`¼Æ";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ ¸¹½X";
+$ubbt_lang['REGGED_ON']  = 	"µù¥U¤é´Á";
+$ubbt_lang['PAGE_TEXT']  = 	"­¶¼Æ";
+?>
