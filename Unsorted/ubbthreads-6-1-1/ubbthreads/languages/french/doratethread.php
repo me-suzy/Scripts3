03d3330f1d7bc7c46d06997950453d82eb042a11
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:34 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/doratethread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/doratethread.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['INVALID_RATING'] = "That is an invalid rating.";
+$ubbt_lang['NOMORERATE'] = "Vous avez déjà évalué ce sujet.";
+$ubbt_lang['THANKS'] = "Merci.";
+$ubbt_lang['RATED']	= "L'évaluation de ce sujet a été enregistrée.";
+?>
