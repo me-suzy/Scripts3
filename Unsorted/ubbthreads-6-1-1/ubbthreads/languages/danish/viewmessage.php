b3366510eec9d8761ed44cef9fa5bec031f7a5ad
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:00 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/viewmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/viewmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['ABOUT']  = 	"about";
+$ubbt_lang['RR_YOUR_MESS']  = 	"Your private message to";
+$ubbt_lang['RR_READ']  = 	"has been read.";
+$ubbt_lang['RR_NOTIFY']  = 	"You have received a Read Receipt";
+$ubbt_lang['READ_RECEIPT']  = 	"Read Receipt";
+$ubbt_lang['TEXT_REC']  = 	"Modtaget";
+$ubbt_lang['SENT']  = 	"Sent";
+$ubbt_lang['DELETE_MESS']  = 	"Slet dette indlæg";
+$ubbt_lang['REPLY_MESS']  = 	"Svar på dette indlæg";
+$ubbt_lang['RETURN_START']  = 	"Tilbage til den første side";
+?>
