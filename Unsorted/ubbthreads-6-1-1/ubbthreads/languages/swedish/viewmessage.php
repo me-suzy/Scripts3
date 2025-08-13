2a6603edf8a338657c952d6bdef4b6fa99178507
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:51 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/viewmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/viewmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['ABOUT']  = 	"Ämne";
+$ubbt_lang['RR_YOUR_MESS']  = 	"Ditt privata meddelande till";
+$ubbt_lang['RR_READ']  = 	"har lästs.";
+$ubbt_lang['RR_NOTIFY']  = 	"Du har mottagit ett läskvitto";
+$ubbt_lang['READ_RECEIPT']  = 	"Läskvitto";
+$ubbt_lang['TEXT_REC']  = 	"Mottagit";
+$ubbt_lang['SENT']  = 	"Skickat";
+$ubbt_lang['DELETE_MESS']  = 	"Radera detta meddelande";
+$ubbt_lang['REPLY_MESS']  = 	"Svara på meddelandet";
+$ubbt_lang['RETURN_START']  = 	"Återgå till din startsida";
+?>
