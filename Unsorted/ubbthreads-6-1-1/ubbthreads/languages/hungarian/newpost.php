6cb115f97134d5a840e81235a14cde701fa6e2cf
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:07 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/newpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/newpost.php
@@ -0,0 +1,17 @@
+<?
+$ubbt_lang['READ_PERM']  = 	"You have read only privileges for this forum";
+$ubbt_lang['CHOOSE_NAME']  = 	"Névtelen üzenõ elfogadva, így Ön választhat olyan nevet ami még nincs regisztrálva.";
+$ubbt_lang['MAKENEW_HEAD']  = 	"Új üzenet írása - fórum";
+$ubbt_lang['FILL_FORM']  = 	"Töltse ki a következõ ürlapot az üzenet elküldéséhez.";
+$ubbt_lang['NO_HTML']  = 	"HTML kód nincs engedélyezve, így nem használhat üzeneteiben HTML kódot.";
+$ubbt_lang['YES_HTML']  = 	"HTML kód engedélyezve, így használhat HTML kódokat üzeneteiben.";
+$ubbt_lang['NO_MARKUP']  = 	"Jelölések tiltva, így nem használhat jelöléseket az üzeneteiben.";
+$ubbt_lang['YES_MARKUP']  = 	"Jelölések engedélyezve, így használhat jelöléseket üzenetében.";
+$ubbt_lang['MARK_POSTS']  = 	"jelölések az ön üzenetében";
+$ubbt_lang['POST_ICON']  = 	"Ikon";
+$ubbt_lang['POST_TEXT']  = 	"Üzenet";
+$ubbt_lang['SPELL_CHECK']  = 	"Helyesírás ellenõrzés";
+$ubbt_lang['DO_EMAIL']  = 	"Az összes választ kérem e-mail -ben postázni.";
+$ubbt_lang['DO_PREVIEW']  = 	"Meg szeretném nézni üzeneteimet elküldés elõtt";
+$ubbt_lang['CAN_ATTACH']  = 	"és/vagy fájl csatolása";
+?>
