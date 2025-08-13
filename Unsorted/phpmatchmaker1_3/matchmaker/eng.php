c3b9fb97a44c1d1ef23acd056bb3d42fcebbc4a2
me-suzy <me.suzana@gmail.com>
2025-08-14 00:55:21 +0300

    Add Unsorted/phpmatchmaker1_3/matchmaker/eng.php

---
+++ Unsorted/phpmatchmaker1_3/matchmaker/eng.php
@@ -0,0 +1,47 @@
+<?
+
+/* Interests - set the number below to one higher
+	 number than there is interests. If you wish to use all
+	 fields, set $num_interestfields to 21.  */
+
+$num_interestfields = "11";
+$i1_text = "Film / Cinema";
+$i2_text = "Animal";
+$i3_text = "Photo";
+$i4_text = "Engine / Cars";
+$i5_text = "Pop Music";
+$i5_text = "Classical Muick";
+$i5_text = "Rock Music";
+$i6_text = "Sport";
+$i7_text = "Resturant";
+$i8_text = "Horse";
+$i9_text = "Theater";
+$i10_text = "Museum";
+$i11_text = "";
+$i12_text = "";
+$i13_text = "";
+$i14_text = "";
+$i15_text = "";
+$i16_text = "";
+$i17_text = "";
+$i18_text = "";
+$i19_text = "";
+$i20_text = "";
+
+// Profile.php
+$la_gender = "Gender";
+$la_age = "Age";
+$la_height = "Height (cm)";
+$la_weight = "Weight (kg)";
+$la_marrital = "Marrital Status";
+$la_build = "Build";
+$la_hair = "Hair Color";
+$la_eye = "Eye Color";
+$la_place = "Place";
+$la_occ = "Occupation";
+$la_relig = "Religion";
+$la_edu = "Education";
+$la_children = "Children";
+$la_about_you = "About me";
+
+?>
