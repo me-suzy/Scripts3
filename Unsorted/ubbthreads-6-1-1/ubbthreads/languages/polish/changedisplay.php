989e4248480bf559a590d2849fca4afc42871796
me-suzy <me.suzana@gmail.com>
2025-08-14 02:43:02 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"Liczba wiadomo¶ci na stronê nie mo¿e byæ u¿yta.";
+$ubbt_lang['FLAT_BAD']  = 	"Liczba wiadomo¶ci na stronie powinna byæ od 1 do 99.";
+$ubbt_lang['TEXTAREA_NO']  = 	"D³ugo¶æ pola wiadomo¶ci nie mo¿e byæ u¿yta.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Twoje ustawienia wy¶wietlania zosta³ zmienione.";
+?>
