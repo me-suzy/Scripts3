a742f35054998a7c5a1abcd99c715001c0aaf2be
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:07 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/showmembers.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/showmembers.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['USER_STATUS']  = 	"Statut";
+$ubbt_lang['TEXT_HOME']  = 	"Pagina web";
+$ubbt_lang['TOTAL_POSTS']  = 	"Mesaje";
+$ubbt_lang['PROF_ICQ']  = 	"Numar ICQ";
+$ubbt_lang['REGGED_ON']  = 	"Inregistrat la";
+$ubbt_lang['PAGE_TEXT']  = 	"Pagini";
+?>
