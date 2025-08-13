c0c2406a2003a86db5ab8b02352c94798357f501
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:02 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/approvepost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/approvepost.php
@@ -0,0 +1,5 @@
+<?
+$ubbt_lang['REPLY_SUB']  = 	"Raspuns la post-ul tau";
+$ubbt_lang['REPLY_BOD']  = 	"a raspuns la post-ul tau pe site-ul: .";
+$ubbt_lang['APPR_HEAD']  = 	"Post-ul a fost aprobat.";
+?>
