42fa01f3a6a228cbfff834abeac34d73d5fb511b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:30 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"No se ha podido encontrar un perfil del Miembro con este nombre";
+$ubbt_lang['EMAIL_HEAD']  = 	"Editar los avisos por correo de";
+$ubbt_lang['PROF_EDIT']  = 	"Puedes editar cualquiera de los datos siguientes:";
+$ubbt_lang['PROF_SUBS']  = 	"Avisos por Email / Subscripciones";
+$ubbt_lang['PROF_NOTIF']  = 	"&iquest;Deseas ser avisado por email cuando recibes mensajes privados?";
+$ubbt_lang['PROF_REPLIES']  = 	"&iquest;Deseas que todas las respuestas a tus mensajes te sean enviadas por correo?";
+$ubbt_lang['SUB_STAT']  = 	"Estado de subscripci&oacute;n para los siguientes tableros.  (Recibe en tu cuenta real de email una recopilaci&oacute;n cada d&iacute;a de todos los mensajes del d&iacute;a que se hagan a este tablero.)";
+$ubbt_lang['SUB_SHOW']  = 	"(A - Apuntarme ; D - Desapuntarme)";
+$ubbt_lang['TEXT_S']  = 	"A";
+$ubbt_lang['TEXT_U']  = 	"D";
+?>
