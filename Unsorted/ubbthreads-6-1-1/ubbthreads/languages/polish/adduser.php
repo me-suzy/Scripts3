b101122a9c6681bbe8da95f1d41b7ca57c45cb7c
me-suzy <me.suzana@gmail.com>
2025-08-14 02:42:55 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =    "Your account needs to be approved by an Administrator before you can login.  You will receive an email once this has been done.";
+$ubbt_lang['APPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} has been approved.";
+$ubbt_lang['NOTAPPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} was not approved.  The reason for this is:";
+$ubbt_lang['NEWREGBODY'] = "A new user has registered at {$config['title']}. To approve this account, please visit";
+$ubbt_lang['NEWREG'] = "New User Registration at";
+$ubbt_lang['INVALID_DATE'] = "That is an invalid date.";
+$ubbt_lang['TOOYOUNG']  =       "Registration for this message board will require approval of your parent or guardian.  You may use the <a href=\"{$config['phpurl']}/coppaform.php\">Permission form</a> for this.";
+$ubbt_lang['UNDER13'] = "Sorry, we do not allow children under 13 to register.";
+$ubbt_lang['BAD_UNAME']  = 	"Nazwa u¿ytkownika nie mo¿e zawieraæ kodu HTML oraz nie mo¿e zaczynaæ siê i konæczyæ spacj±";
+$ubbt_lang['LONG_NAME']  = 	"Nazwa u¿ytkownika ma niew³a¶ciw± d³ugo¶æ.";
+$ubbt_lang['BAD_EMAIL']  = 	"Nie mo¿na za³o¿yæ konta z adresem email w domenie";
+$ubbt_lang['USER_EXISTS']  = 	"Taki u¿ytkownik ju¿ istnieje.";
+$ubbt_lang['NO_MULTI']  = 	"Danemu adresowi email mo¿e odpowiadaæ tylko jeden u¿ytkownik, a istnieje ju¿ u¿ytkownik z takim adresem.";
+$ubbt_lang['PASS_MATCH']  = 	"Has³a siê nie zgadzaj± ze sob±.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Has³o powinno mieæ 4-20 znaków.";
+$ubbt_lang['ILL_PASS']  = 	"Niedozwolone znaki w ha¶le.  Dozwolone jedynie znaki alfanumeryczne.";
+$ubbt_lang['WEL_MSG']  = 	"Zachêcam do edycji Ustawieñ. Mo¿esz w nich wpisaæ trochê informacji o sobie, zdefiniowaæ podpis, który bêdzie automatycznie dodawany do wszystkich Twoich wiadomo¶ci, jak równie¿ mo¿esz dostowaæ wygl±d forum do Twoich potrzeb (sortowanie, sposób pokazywania wiadomo¶ci itp.).";
+$ubbt_lang['PASS_SUB']  = 	"Twoje has³o do";
+$ubbt_lang['PASS_BODY1']  = 	"Kto¶ z adresu";
+$ubbt_lang['PASS_BODY2']  = 	"zarejestrowa³ konto";
+$ubbt_lang['PASS_BODY3']  = 	"Has³o dla tego konta jest";
+$ubbt_lang['NEW_CONFIRM']  = 	"U¿ytkownik zosta³ poprawnie zarejestrowany.";
+$ubbt_lang['NEW_BODY']  = 	"Konto zarezerwowane.  Niebawem dostaniesz email z has³em.  Bêdziesz mog³ siê wtedy zalogowaæ i zacz±æ pisaæ na forum. Zachêcam do edycji Ustawieñ. Dziêki nim mo¿esz dostosowaæ forum do swoich wymagañ.";
+?>
