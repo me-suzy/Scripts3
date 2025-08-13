88b43bc9d42f41532a428927a2829d3ae8d8422e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:47 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/newpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/newpost.php
@@ -0,0 +1,17 @@
+<?
+$ubbt_lang['CHOOSE_NAME']  = 	"Se permite dejar mensajes an&oacute;nimamente, por lo que puedes escoger cualquier nombre que no est&eacute; registrado para poner este mensaje.";
+$ubbt_lang['READ_PERM']  = 	"En este foro solo tienes permisos de lectura.";
+$ubbt_lang['MAKENEW_HEAD']  = 	"Crear un Nuevo Mensaje - Foro";
+$ubbt_lang['FILL_FORM']  = 	"Rellena este formulario para enviar un mensaje al foro.";
+$ubbt_lang['NO_HTML']  = 	"No se permite HTML.";
+$ubbt_lang['YES_HTML']  = 	"Se permite HTML.";
+$ubbt_lang['NO_MARKUP']  = 	"No se permiten Etiquetas UBB.";
+$ubbt_lang['YES_MARKUP']  = 	"Se permiten Etiquetas UBB, puedes usarlas";
+$ubbt_lang['MARK_POSTS']  = 	"Etiquetas UBB en tus mensajes";
+$ubbt_lang['POST_ICON']  = 	"Insertar Icono";
+$ubbt_lang['POST_TEXT']  = 	"Enviar";
+$ubbt_lang['SPELL_CHECK']  = 	"Revisar ortograf&iacute;a";
+$ubbt_lang['DO_EMAIL']  = 	"Deseo recibir todas las respuestas a mi mensaje por email.";
+$ubbt_lang['DO_PREVIEW']  = 	"Deseo previsualizar mi mensaje";
+$ubbt_lang['CAN_ATTACH']  = 	"y/o adjuntar un archivo";
+?>
