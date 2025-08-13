4eb008fde287a4f99da658a168744c4abefb688c
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:02 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/dopoll.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/dopoll.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NO_VOTE']  = 	"Sie haben vergessen, abzustimmen!";
+$ubbt_lang['POLL_REGED']  = 	"Nur eingeloggte, registrierte Benutzer k&ouml;nnen an dieser Umfrage teilnehmen.";
+$ubbt_lang['POLL_IP']  = 	"Von dieser IP-Adresse wurde für diese Umfrage bereits abgestimmt.";
+$ubbt_lang['POLL_USER']  = 	"Unter Ihrem Benutzernamen wurde bereits für diese Umfrage abgestimmt.";
+?>
