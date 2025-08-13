d13e681743402689aef47e2e82b00122a724ad02
me-suzy <me.suzana@gmail.com>
2025-08-14 02:41:05 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"Richiesta per password";
+$ubbt_lang['PASS_REQ_BOD1']  = 	"Qualcuno dall'indirizzo IP ";
+$ubbt_lang['PASS_REQ_BOD2']  = 	"requested a temporary password for the Username";
+$ubbt_lang['PASS_REQ_BOD3']  = 	"for the website '{$config['title']}'.  This is a temporary password that can be used if you have forgotten your original password.  If you did not request this, please ignore this message.  The temporary password for this Username is --";
+$ubbt_lang['PASS_MAILED']  = 	"Password inviata in email";
+$ubbt_lang['PASS_MAILED2']  = 	"A temporary password for this user has been mailed.";
+$ubbt_lang['NO_EMAIL']  = 	"Non può trovare l'indirizzo email per";
+?>
