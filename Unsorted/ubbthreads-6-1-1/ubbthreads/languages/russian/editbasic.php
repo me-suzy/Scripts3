7aa8de1af35e71f687d70dffd246f3e0dace92c8
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:55 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =      "Your picture ({$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['PROF_PIC']  =       "Your Picture (Entire URL - Picture size is {$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['AUX_ONLINE']  =      "Do you want users to see the actual forum/post you are looking at?";
+$ubbt_lang['PROF_PRIV']  = 	"Accept private messages from other users?";
+$ubbt_lang['PROF_SIG']  = 	"Signature (up to  {$config['Sig_length']} characters).  You may use UBBCode in your signature";
+$ubbt_lang['NO_PROF']  = 	"Íå óäà¸òñÿ íàéòè ïðîôèëü äëÿ èìåíè ïîëüçîâàòåëÿ";
+$ubbt_lang['PROF_HEAD']  = 	"Ðåäàêòèðîâàíèå ïðîôèëÿ";
+$ubbt_lang['PROF_EDIT']  = 	"Âû ìîæåòå èçìåíèòü ëþáóþ ïðèâåä¸ííóþ èíôîðìàöèþ.";
+$ubbt_lang['BASIC_PROF']  = 	"Áàçîâàÿ èíôîðìàöèÿ î ïîëüçîâàòåëå (áàçîâûé ïðîôèëü)";
+$ubbt_lang['PROF_PASS']  = 	"Ïàðîëü (îò 4 äî 20 çíàêîâ)";
+$ubbt_lang['VER_PASS']  = 	"Ïàðîëü (åù¸ ðàç)";
+$ubbt_lang['EMAIL_ADD']  = 	"Àäðåñ e-mail (íåäîñòóïåí äðóãèì ïîëüçîâàòåëÿì)";
+$ubbt_lang['FAKE_EMAIL']  = 	"Àäðåñ e-mail, ïîÿâëÿþùèéñÿ íà Âàøèõ ñîîáùåíèÿõ.";
+$ubbt_lang['FULL_NAME']  = 	"Ïîëíîå èìÿ";
+$ubbt_lang['PROF_HOME']  = 	"WEB-ñòðàíèöà (íå áîëåå 150 çíàêîâ)";
+$ubbt_lang['PROF_OCC']  = 	"Çàíÿòîñòü";
+$ubbt_lang['PROF_HOBB']  = 	"Õîááè (íå áîëåå 200 çíàêîâ)";
+$ubbt_lang['PROF_LOC']  = 	"Ãåîãðàôè÷åñêîå ïîëîæåíèå (Ñòðàíà, Ðåãèîí, Ãîðîä, è ò.ä.)";
+$ubbt_lang['PROF_BIO']  = 	"Êðàòêàÿ áèîãðàôèÿ (íå áîëåå 250 çíàêîâ)";
+$ubbt_lang['PROF_ICQ']  = 	"Íîìåð ICQ";
+$ubbt_lang['PROF_VIS']  = 	"Õîòèòå ëè Âû, ÷òîáû ïîëüçîâàòåëè âèäåëè Âàñ íà ýêðàíå &quot;Êòî â îíëàéíå&quot;?";
+?>
