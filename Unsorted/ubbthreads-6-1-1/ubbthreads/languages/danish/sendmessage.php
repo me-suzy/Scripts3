ebc08aec83f2c55c7d162b66c2f33fdc607c730c
me-suzy <me.suzana@gmail.com>
2025-08-14 02:31:36 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/sendmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/sendmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['CHOOSE_ADD']  = 	"Choose a name";
+$ubbt_lang['TEXT_DELETE']  = 	"Slet";
+$ubbt_lang['NO_PRIVATE']  = 	"Denne bruger ønsker ikke at modtage private beskeder.";
+$ubbt_lang['NO_RECORD']  = 	"Den bruger, du vil sende beskeden til, findes ikke i vores system.";
+$ubbt_lang['MESS_PREV']  = 	"Preview private message to";
+$ubbt_lang['PREVIEW_MESS']  = 	"The following is a preview of your message.  If every thing looks ok then you can click 'Continue' and your message will be sent.  If not then use the bottom form below to edit some more.";
+$ubbt_lang['EDIT_MORE']  = 	"Edit some more";
+$ubbt_lang['PRIV_SUB']  = 	"Der er kommet nye beskeder";
+$ubbt_lang['PRIV_BODY_R']  = 	"Du har modtaget en privat besked fra forumerne på . Beskeden kan læses ved at gå til {$config['phpurl']}/login.php";
+$ubbt_lang['MESS_SENT']  = 	"Dit indlæg er sendt.";
+?>
