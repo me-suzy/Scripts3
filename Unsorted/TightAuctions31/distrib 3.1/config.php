e205d607eef9ba77b55de4200de0c9ddc6434677
me-suzy <me.suzana@gmail.com>
2025-08-14 02:02:37 +0300

    Add Unsorted/TightAuctions31/distrib 3.1/config.php

---
+++ Unsorted/TightAuctions31/distrib 3.1/config.php
@@ -0,0 +1,11 @@
+<?
+  $SiteName   = "";			// name of your website.
+  $DomainName = "";		// e.g. www.something.com
+  $EmailDomain = "";		// e.g. something.com (without "www." in front)
+  $HomeInstall = "";		// the installation directory.  Don't forget to add 
+ 						// the forward slash to the beginning ( e.g. /MyAuctions )
+
+  $DB_Username = "";
+  $DB_Password = "";
+  $DB_Database = "";
+?>
