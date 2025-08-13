10e60d7722b4220dbd7263d42fdf48c1bb4a245a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:44:43 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/domailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/domailthread.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['START']  = 	"has forwarded a link to a post entitled";
+$ubbt_lang['START2']  = 	"on '{$config['title']}'! The post is below.";
+$ubbt_lang['TO_READ']  = 	"View this thread on the web at"
+$ubbt_lang['NOTE_FROM']   =    "You have received a notification about this post from "
+$ubbt_lang['POST_BELOW']  =    "The actual post is below";
+$ubbt_lang['EMAIL_SENT']  =    "Email has been sent";
+$ubbt_lang['EMAIL_CONFIRM']  =  "An email of this post has been sent to";
+$ubbt_lang['FORUM_RETURN']   =  "Return to forum now!"; 
+?>
