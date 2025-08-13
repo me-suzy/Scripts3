02a9f62f63c3a7686bdcedddec618aec31dd3809
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:11 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/sendprivate.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/sendprivate.php
@@ -0,0 +1,13 @@
+<?
+$ubbt_lang['NOPRIVS']   = "Private messages are disabled.";
+$ubbt_lang['MESS_COPY']  = 	"Ik wil een kopie van dit bericht opslaan";
+$ubbt_lang['MESS_RECEIPT']  = 	"Ik wil graag een ontvangstbevestiging ontvangen";
+$ubbt_lang['MESS_PREVIEW']  = 	"Ik wil eerst een voorbeeld bekijken";
+$ubbt_lang['ADD_BOOK']  = 	"Aan je adresboekje toevoegen";
+$ubbt_lang['USE_BOOK']  = 	"of gebruik je adresboekje";
+$ubbt_lang['CHOOSE_ADD']  = 	"Kies een naam";
+$ubbt_lang['NO_LOGGED']  = 	"Je kunt geen privé-berichten versturen als je niet aangemeld bent..";
+$ubbt_lang['NO_PRIVATE']  = 	"Dit lid accepteert geen privé-berichten.";
+$ubbt_lang['PRIV_BODY2']  = 	"Vul onderstaande velden in om een privé-bericht te versturen.";
+$ubbt_lang['TEXT_DELETE']  = 	"Wissen";
+?>
