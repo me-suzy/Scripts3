402a8766db3965f174e9d74b7a7bc02251a80626
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:19 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/viewfavorites.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/viewfavorites.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['VIEW_FAV']  = 	"Thread-uri favorite";
+$ubbt_lang['VIEW_REM']  = 	"Mesaje de revazut";
+$ubbt_lang['LAST_POST']  = 	"Ultimul mesaj";
+$ubbt_lang['REMOVE']  = 		"Sterge";
+?>
