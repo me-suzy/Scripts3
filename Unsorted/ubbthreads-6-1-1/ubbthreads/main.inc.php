b6958426df2d0414d594f85563a932265542b3a8
me-suzy <me.suzana@gmail.com>
2025-08-14 02:21:59 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/main.inc.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/main.inc.php
@@ -0,0 +1,18 @@
+<? 
+
+// THIS NEEDS TO BE SET, OTHERWISE ADMIN SCRIPTS WILL NOT WORK
+// THIS IS THE ACTUAL PATH TO YOUR PHP INSTALLATION
+   $thispath = "";
+
+// PATH TO YOUR config.inc.php file.  BY DEFAULT THIS IS THE
+// SAME AS $thispath, BUT IF YOU MOVE config.inc.php TO ANY
+// OTHER LOCATION YOU MUST SPECIFY IT HERE.
+   $configdir = "";
+
+// DO NOT EDIT ANYTHING BELOW THIS LINE!
+   include("$thispath/ubbt.inc.php");
+
+// Page load times
+   $timea = getmicrotime();
+
+?>
