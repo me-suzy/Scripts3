001f16dda84747d35ce94fb67c661744c55a0a29
me-suzy <me.suzana@gmail.com>
2025-08-14 02:23:34 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/admin/dbcommand.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/admin/dbcommand.php
@@ -0,0 +1,55 @@
+<?
+/*
+# UBB.threads, Version 6
+# Official Release Date for UBB.threads Version6: 06/05/2002
+
+# First version of UBB.threads created July 30, 1996 (by Rick Baker).
+# This entire program is copyright Infopop Corporation, 2002.
+# For more info on the UBB.threads and other Infopop
+# Products/Services, visit: http://www.infopop.com
+
+# Program Author: Rick Baker.
+
+# You may not distribute this program in any manner, modified or otherwise,
+# without the express, written written consent from Infopop Corporation.
+
+# Note: if you modify ANY code within UBB.threads, we at Infopop Corporation
+# cannot offer you support-- thus modify at your own peril :)
+# ---------------------------------------------------------------------------
+*/
+
+// Require the library
+   require ("../main.inc.php");
+
+// -----------------
+// Get the user info
+   $userob = new user;
+   $user = $userob -> authenticate();
+   $html = new html;
+
+// -----------------------------
+// Make sure they should be here
+   if ($user['U_Status'] != 'Administrator'){
+      $html -> not_right ("You must be logged in, and be a valid administrator to access this.",$Cat);
+   }
+
+// ------------------------
+// Send them a page 
+   $html -> send_header ("Execute a sql command",$Cat,0,$user);
+   $html -> admin_table_header("Execute a sql command");
+   $html -> open_admin_table();
+
+   echo "<TR><TD CLASS=\"lighttable\">";
+   echo "Use the box below to execute a sql command.<br />";
+
+   echo "<FORM METHOD = POST action =\"{$config['phpurl']}/admin/dbdocommand.php\">";
+   echo "<input type=hidden name=Cat value=$Cat>";
+   echo "<textarea class=formboxes name=command rows={$theme['TextRows']} cols={$theme['TextCols']}></textarea>";
+   echo "<p>";
+   echo "<input type=submit value=\"Submit\" class=\"buttons\">";
+   echo "</form>";  
+
+   $html -> close_table();
+   $html -> send_admin_footer();
+
+?>
