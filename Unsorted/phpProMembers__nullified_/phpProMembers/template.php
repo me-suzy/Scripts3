a05a9017eb68ede221710839910c5ec223f68804
me-suzy <me.suzana@gmail.com>
2025-08-14 00:57:56 +0300

    Add Unsorted/phpProMembers__nullified_/phpProMembers/template.php

---
+++ Unsorted/phpProMembers__nullified_/phpProMembers/template.php
@@ -0,0 +1,26 @@
+<?php
+/*********************************************************************/
+/* Program Name         : phpProMembers                              */
+/* Home Page            : http://www.gacybertech.com                 */
+/* Retail Price         : $149.99 United States Dollars              */
+/* WebForum Price       : $000.00 Always 100% Free                   */
+/* xCGI Price           : $000.00 Always 100% Free                   */
+/* Supplied by          : South [WTN]                                */
+/* Nullified by         : CyKuH [WTN]                                */
+/* Distribution         : via WebForum and Forums File Dumps         */
+/*********************************************************************/
+$page_account = "";
+require "include.php";
+if ($include_template == "1") {
+	include "$template_directory/header.php";	
+}
+?>	
+<!--Insert Your NORMAL HTML Below This Line -->
+
+<!--Do Not Edit Below This Line -->
+<?php
+// Adding footer if we got it.
+if ($include_template == "1") {
+	include "$template_directory/footer.php";	
+}
+?>	
