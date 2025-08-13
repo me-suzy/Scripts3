53ae2dad568f232a64d04a308e84acc56fcd5429
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:13 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/includes/header.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/includes/header.php
@@ -0,0 +1,21 @@
+<br />
+<table border="0" cellpadding="2" cellspacing="0" width="95%" class="tableborders" align="center">
+  <tr>
+    <td align="center">
+        <table border="0" cellpadding="2" cellspacing="0" width="100%">
+          <tr>
+          <td width="50%" class="welcome">
+            <b>UBB.threads</b>
+          </td>
+          <td width="50%" align="right" class="welcome">
+            <b>Infopop Corporation.</b>
+          </td>
+          </tr>
+          <tr>
+            <td width="100%" colspan="3" height="1" class="tableborders">
+            </td>
+          </tr>
+       </table>
+     </td>
+   </tr>
+</table>
