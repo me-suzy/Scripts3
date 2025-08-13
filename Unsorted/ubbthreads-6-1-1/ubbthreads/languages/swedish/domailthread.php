da3d080291cc1e1d870a0abe8a9524443e7feb17
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:44 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/domailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/domailthread.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['START']  = 	"har vidarebefodrat en länk till ett inlägg med ämnet";
+$ubbt_lang['START2']  = 	"på '{$config['title']}'! Inlägget visas nedan.";
+$ubbt_lang['TO_READ']  = 	"Visa denna tråd på webben på";
+$ubbt_lang['EMAIL_SENT']  = 	"E-post har skickats";
+$ubbt_lang['EMAIL_CONFIRM']  =  "Ett e-postmeddelande med detta inlägg har skickats till";
+$ubbt_lang['FORUM_RETURN']   =  "Återvänd till forumet nu!";
+$ubbt_lang['NOTE_FROM']   =    "Du har fått information om detta inlägg från ";
+$ubbt_lang['POST_BELOW']  =    "Inlägget visas nedan";
+?>
