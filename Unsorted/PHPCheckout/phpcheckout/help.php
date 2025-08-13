2f58ec1050b48f1ee066533caf0f64c7b0c1e1b8
me-suzy <me.suzana@gmail.com>
2025-08-14 00:31:47 +0300

    Add Unsorted/PHPCheckout/phpcheckout/help.php

---
+++ Unsorted/PHPCheckout/phpcheckout/help.php
@@ -0,0 +1,99 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+	<HEAD>
+		<?php require_once("configure.php");?>
+      <script language="Javascript" src="phpcheckout.js"></script>
+      <script language="Javascript">loadCSSFile();</script>
+		<TITLE>Help -  <?php echo IMPLEMENTATIONNAME;?> - <?php echo BENEFIT;?> -  <?php echo ORGANIZATION;?> - Powered by phpcheckoutTM from DreamRiver http://wwww.dreamriver.com - Software Powers the Net - a DreamRiver Internet Software Product - php software mysql database - download - web database - dreamriver - freeware - sql - file downloader - internet application development - hot scripts - good value - database software - custom code - php e-mail software - ultimate web apps software - phpYellow Pages - EasySQL - phpTellAFriend - mysql php download - Easily create complete internet programs with php, apache, mysql and windows</TITLE>
+	</HEAD> 
+
+<body>
+<!-- START of body -->
+
+<?php include("header.php");?>
+
+
+<h1>Surfer Help File</h1>
+
+<p>This help file contains information of interest to and dedicated for 
+the average visitor browsing this site.</p>
+
+
+<h2><a name="email">Email</a></h2>
+<p>A valid email address is needed to register your digital product download. 
+Registration is free and automatic.</p>
+ 
+<p>If you subscribe to the newsletter your email address may also be used to 
+inform you of product news. Your email address is needed to subscribe or to 
+unsubscribe from the newsletter.</p>
+
+<h3>Selectable by Developer</h3>
+<p>Your product choice may sent to you via email attachment. If so, the email you 
+provide in the email field is used as the recipient address. This email address 
+must be valid in order for you to receive digital product as an email 
+attachment.</p>
+
+
+<h2><a name="privacy">Privacy</a></h2>
+<UL>
+<LI>High - you will never hear from us
+<LI>Medium - you will accept some email only from us
+<LI>Low - you will accept any information from us or others.
+</UL>
+
+
+<h2><a name="newsletter">Newsletter</a></h2>
+<p>The owner of the phpcheckout Digital Download System<sup>TM</SUP> may offer a 
+newsletter. If so, you may subscribe by checking the newsletter box during 
+the download process. Unsubscribe functionality is also easily available.</p>
+
+
+
+<h2>Delivery Method</h2>
+<p>The administrator may select to offer product delivery via several methods.</p> 
+<ul>
+<li>download via email body
+<li>download via email attachment
+<li>download via HTTP URL
+<LI>download via FTP URL
+<LI>download via CRT
+<li>download via special message/other
+</ul> 
+
+
+<h2><a name="password">Password</a></h2>
+<p>New? Make up your own password. The password is used to confirm 
+user details, and may be used in allowing users to update their 
+registration details, or newsletter subscribes or unsubscribes.</p>
+
+<p><br></p>
+
+<h2><a name="hitsthisproduct">Hits this Product</a></h2>
+<p>The number of hits per product can be set by the system administrator.
+The default logic is to increment a single product hit counter when:</p>
+
+<ul>
+<li>the specific product <b>Showcase</b> page is loaded or
+<li>the <b>Free Download</b> page is loaded or
+<li>the internet payment transaction is completed or 
+<li>the <b>Featured Product</b> page is loaded ( and the product is on it )
+</ul>
+
+<p>This means that the &quot;Hits this Product&quot; is really 
+page views per product. In no way does it reflect actual downloads.
+</p>
+
+<p>The &quot;Hits this Product&quot; counter is periodically reset, and so 
+the counter may not accurately reflect total hits. Why bother with counters at all? 
+The counter is a useful measure of website activity, interest and 
+usage patterns. Data may be processed to yield pertinent business 
+information and to improve the usefulness of the website for the 
+visitor.
+</p>
+
+
+<p><br></p>
+
+
+
+<?php include("footer.php");?>
