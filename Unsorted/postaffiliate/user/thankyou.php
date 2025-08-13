f7f5ad3d7d076cab99a0365d9e387d4799b2a50e
me-suzy <me.suzana@gmail.com>
2025-08-14 01:09:30 +0300

    Add Unsorted/postaffiliate/user/thankyou.php

---
+++ Unsorted/postaffiliate/user/thankyou.php
@@ -0,0 +1,13 @@
+<?PHP
+  include_once "../affconfig.php";
+  include_once "./lang/$language";
+  
+  include "header.php"; 
+?>        
+  <p align=center><font size=3><b>Thank You For Joining!</b></font><br><br>
+  <font size=2>Please check your email for the account information email we sent you.<br><br></font>
+  <p>
+  You can log in <a href=index.php>here.<a/>
+  </p>
+        
+<? include "footer.php"; ?>
