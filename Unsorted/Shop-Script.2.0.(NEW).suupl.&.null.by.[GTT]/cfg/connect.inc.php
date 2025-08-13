49c591c6fa4e98d7ba3ead3cfecbf820b172b1dc
me-suzy <me.suzana@gmail.com>
2025-08-14 01:39:04 +0300

    Add Unsorted/Shop-Script.2.0.(NEW).suupl.&.null.by.[GTT]/cfg/connect.inc.php

---
+++ Unsorted/Shop-Script.2.0.(NEW).suupl.&.null.by.[GTT]/cfg/connect.inc.php
@@ -0,0 +1,29 @@
+<?
+
+	//database connection settings
+
+
+
+	define('DBMS', 'MYSQL'); // database system
+
+
+
+	define('DB_HOST', 'localhost'); // database host
+
+	define('DB_USER', ''); // username
+
+	define('DB_PASS', ''); // password
+
+	define('DB_NAME', 'ss2_kvit'); // database name
+
+	define('ADMIN_LOGIN', 'MANAGER'); //administrator's login
+
+
+
+	//database tables
+
+	include("tables.inc.php");
+
+
+
+?>
