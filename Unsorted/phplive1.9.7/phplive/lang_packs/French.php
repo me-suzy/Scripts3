59a8f2040bb38f82fcc0bd12aaf3aceac44d1933
me-suzy <me.suzana@gmail.com>
2025-08-14 00:48:36 +0300

    Add Unsorted/phplive1.9.7/phplive/lang_packs/French.php

---
+++ Unsorted/phplive1.9.7/phplive/lang_packs/French.php
@@ -0,0 +1,85 @@
+<?
+$LANG = ARRAY() ;		// MUST initialize, DO NOT CHANGE
+/*******************************************************************
+* Language: French
+* Contributed by:
+*	Name: Pinky and Gaspard 
+*	Company: Axs Marine
+*	URL: http://www.kygaroo.com http://www.ryogasp.com
+*	Contact: gaspard <gaspard@europe.com>
+*
+* ------------------------------------------------------------------
+* DO NOT REMOVE COMMENTS.  JUST MODIFY THE ACTUAL TEXT.
+* DO NOT REMOVE THE orig TEXT!  THEY ARE THERE FOR REFERENCE.
+* ------------------------------------------------------------------
+* NOTE: If you are going to submit a language pack, please keep
+* everything basic and do not mention any company name, personal name,
+* copyrighted name, or such.  Keep all translations generic, simple
+* and catered to all audiences and companies.  Basically, your
+* translated language pack should be ready to use without being touched.
+*******************************************************************/
+
+
+
+/***************** ONLY MAKE THE CHANGE BELOW ******************/
+
+
+/*** branded string located troughout 99% of the pages */
+//orig: PHP <i>Live!</i> &copy; 2001 OSI Codes, New York, New York
+$LANG['DEFAULT_BRANDING'] = "Powered by <a href='http://www.PHPLiveSupport.com' target='new'>PHP <i>Live!</i></a>" ;
+
+/*** chat page titles */
+//orig: Request Live! Support Request
+$LANG['TITLE_SUPPORTREQUEST'] = "Demandez de l`aide en Live!" ;
+//orig: You are now speaking with a Live! Support
+$LANG['TITLE_SUPPORCHAT'] = "Vous &ecirc;tes en communication avec notre service d`aide en Live!" ;
+//orig: Please leave a message
+$LANG['TITLE_LEAVEMESSAGE'] = "Veuillez laisser un message" ;
+//orig: Printer Friendly View
+$LANG['TITLE_PRINTVIEW'] = "Imprimer" ;
+
+$LANG['WORD_BACK'] = "Retour" ;
+$LANG['WORD_CLOSE'] = "Fermer" ;
+$LANG['WORD_NAME'] = "Nom" ;
+$LANG['WORD_EMAIL'] = "E-mail" ;
+$LANG['WORD_SUBJECT'] = "Sujet" ;
+$LANG['WORD_MESSAGE'] = "Message" ;
+$LANG['WORD_SEND'] = "Envoyer" ;
+$LANG['WORD_COMPANY'] = "Soci&eacute;t&eacute;" ;
+$LANG['WORD_DEPARTMENT'] = "Service" ;
+$LANG['WORD_DAY'] = "Jour" ;
+
+//orig: Welcome to our Live! support help desk.
+$LANG['CHAT_REQUEST_TITLE'] = "Bienvenue sur notre Aide en Live!" ;
+//orig: Please select the department you would like to reach.
+$LANG['CHAT_REQUEST_SELECTDEPT'] = "Veuillez sp&eacute;cifier le service que vous souhaitez joindre." ;
+//orig: Input your Screen Name used during this session:
+$LANG['CHAT_REQUEST_INPUTSCREENNAME'] = "Veuillez saisir votre Pseudonyme pour cette session:" ;
+//orig: Welcome %%user%%! Please hold while we contact a representative. If a representative does not respond in few seconds, then he/she is not available at this time.
+$LANG['CHAT_GREETING'] = "Bienvenue %%user%%! Veuillez patienter pendant que nous contactons votre interlocuteur/trice. Si vous n`avez pas de r&eacute;ponse dans quelques secondes, c`est qu` il/elle n`est pas disponible pour le moment." ;
+//orig: Printer Friendly
+$LANG['CHAT_PRINTER_FRIENDLY'] = "Version Imprimable" ;
+//orig: Your party has left this session.
+$LANG['CHAT_PARTYLEFTSESSION'] = "Votre correspondant a quitt&eacute; cette session." ;
+//orig: You are now speaking with
+$LANG['CHAT_REQUEST_NOWSPEAKINGWITH'] = "Vous &ecirc;tes actuellement en conversation avec" ;
+//orig: Department is currently unavailable. Please leave a message.
+$LANG['MESSAGE_BOX_MESSAGE'] = "Ce service n`est pas disponible pour le moment. Merci de laisser un message." ;
+//orig: Thank you.  Your message has been sent to:
+$LANG['MESSAGE_BOX_SENT'] = "Merci.  Votre message a &eacute;t&eacute; envoy&eacute; &agrave;:" ;
+//orig: Items with (*) must be filled.
+$LANG['MESSAGE_BOX_JS_A_ALLFIELDSSUP'] = "Les champs avec (*) doivent &ecirc;tre saisis." ;
+//orig: Email is invalid format (example: someone@somewhere.com)
+$LANG['MESSAGE_BOX_JS_A_INVALIDEMAIL'] = "L`email est format inadmissible (exemple: someone@somewhere.com)" ;
+//orig: Department not available.  Please leave a message.
+$LANG['CHAT_DEPT_NOTAVAIL'] = "Ce service n`est pas disponible.  Merci de laisser un message." ;
+//orig: Session did not create.  Request did not send.
+$LANG['CHAT_NOTCREATE'] = "La session n`a pas &eacute;t&eacute; cr&eacute;&eacute;e.  La demande n`a pas &eacute;t&eacute; envoy&eacute;e." ;
+
+//orig: Please hold while being transferred to
+$LANG['CHAT_CALL_TRANSFER'] = "Tenez s`il vous plaît tout en étant transféré à" ;
+//orig: What is your question?
+$LANG['CHAT_REQUEST_QUESTION'] = "Quelle est votre question?" ;
+//orig: A copy of the chat transcript has been sent to:
+$LANG['CHAT_TRANSCRIPT_SENT'] = "A copy of the chat transcript has been sent to:" ;
+?>
