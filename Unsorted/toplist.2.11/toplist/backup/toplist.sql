1f95a62904c91797625159a4228087a1604f56c9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:05:15 +0300

    Add Unsorted/toplist.2.11/toplist/backup/toplist.sql

---
+++ Unsorted/toplist.2.11/toplist/backup/toplist.sql
@@ -0,0 +1,127 @@
+INSERT INTO toplista VALUES('testbunk','http://testbunk','testbunk','http://testbunk.com/banner.gif','test@testbunk.com','0','0','9','y','922b3871bd1bfd77c8a73f8949cbc857','0','0','testbunk','1','0','0')
+
+INSERT INTO toplista VALUES('Test','http://www.xtina.pl','Test','','nookie@xtina.pl','0','0','5','y','bee63cbe956805386e8b26f3c54a8d58','0','0','nookie','1','0','0')
+
+INSERT INTO toplista VALUES('test','http://www.net4u.net','tetst tets','http://www.sdasd.gif','dasdasdao9n.at','0','0','6','y','a39401275d1b300aa789fb22aea4148a','0','0','waterstone','3','0','0')
+
+INSERT INTO toplista VALUES('Not4kids.Net','http://www.not4kids.net','Sex','http://www.not4kids.net/banner01.gif','no@host.com','0','0','8','y','1e7c1eac9e131fe3a01d7594c071bf8c','0','0','not4kids','1','0','0')
+
+INSERT INTO toplista_news VALUES('asdasdasd
+asdasdasdas','asdas','June 4, 2002 | 08:23','1')
+
+INSERT INTO toplista_news VALUES('news?','admin','June 6, 2002 | 07:10','2')
+
+INSERT INTO toplista_options VALUES('Best Top List','default','nookie-ftp.wox.org/nookie/toplist','nookie@xtina.pl','202cb962ac59075b964b07152d234b70','nookie-ftp.wox.org','14400','1031256865','90','70','470','1')
+
+INSERT INTO toplista_menu VALUES('1','1','1','1','1','1','1','1','1','1','1')
+
+INSERT INTO toplista_stats VALUES('64.34.233.253','2002-05-30 17:04:54','out','1')
+
+INSERT INTO toplista_stats VALUES('64.34.233.253','2002-05-30 17:05:31','out','1')
+
+INSERT INTO toplista_stats VALUES('216.77.214.55','2002-05-31 01:23:11','out','1')
+
+INSERT INTO toplista_stats VALUES('24.157.77.113','2002-05-31 04:39:40','out','1')
+
+INSERT INTO toplista_stats VALUES('12.237.138.36','2002-05-31 07:54:58','out','1')
+
+INSERT INTO toplista_stats VALUES('217.98.196.71','2002-05-31 21:48:37','out','1')
+
+INSERT INTO toplista_stats VALUES('217.39.168.35','2002-05-31 22:36:56','out','1')
+
+INSERT INTO toplista_stats VALUES('217.98.196.71','2002-06-01 08:41:58','in','2')
+
+INSERT INTO toplista_stats VALUES('217.98.196.71','2002-06-01 08:55:23','out','1')
+
+INSERT INTO toplista_stats VALUES('217.98.196.71','2002-06-01 08:56:42','out','1')
+
+INSERT INTO toplista_stats VALUES('217.215.55.89','2002-06-01 22:47:39','out','1')
+
+INSERT INTO toplista_stats VALUES('217.215.55.89','2002-06-01 22:47:58','out','3')
+
+INSERT INTO toplista_stats VALUES('217.215.55.89','2002-06-01 22:48:56','out','0')
+
+INSERT INTO toplista_stats VALUES('142.177.225.159','2002-06-01 23:07:33','out','2')
+
+INSERT INTO toplista_stats VALUES('192.115.134.58','2002-06-02 01:51:07','out','1')
+
+INSERT INTO toplista_stats VALUES('64.233.215.15','2002-06-02 02:22:06','out','0')
+
+INSERT INTO toplista_stats VALUES('194.212.200.243','2002-06-02 13:46:32','out','5')
+
+INSERT INTO toplista_stats VALUES('194.212.200.243','2002-06-02 13:47:23','out','6')
+
+INSERT INTO toplista_stats VALUES('193.219.167.143','2002-06-03 19:00:52','out','5')
+
+INSERT INTO toplista_stats VALUES('68.10.162.167','2002-06-03 20:04:48','out','5')
+
+INSERT INTO toplista_stats VALUES('200.54.134.207','2002-06-04 04:35:22','out','5')
+
+INSERT INTO toplista_stats VALUES('65.80.6.69','2002-06-04 07:27:50','out','5')
+
+INSERT INTO toplista_stats VALUES('62.90.233.8','2002-06-04 08:23:11','in','7')
+
+INSERT INTO toplista_stats VALUES('62.90.233.8','2002-06-04 08:23:18','out','7')
+
+INSERT INTO toplista_stats VALUES('62.90.233.8','2002-06-04 08:23:28','out','7')
+
+INSERT INTO toplista_stats VALUES('62.90.233.8','2002-06-04 08:28:14','in','8')
+
+INSERT INTO toplista_stats VALUES('62.90.233.8','2002-06-04 08:29:21','in','8')
+
+INSERT INTO toplista_stats VALUES('217.98.196.71','2002-06-04 09:26:27','out','8')
+
+INSERT INTO toplista_stats VALUES('195.92.168.165','2002-06-04 19:18:35','out','8')
+
+INSERT INTO toplista_stats VALUES('195.92.168.166','2002-06-04 19:46:06','out','8')
+
+INSERT INTO toplista_stats VALUES('213.60.20.183','2002-06-05 21:06:43','out','5')
+
+INSERT INTO toplista_stats VALUES('217.98.196.71','2002-06-05 22:23:19','in','5')
+
+INSERT INTO toplista_stats VALUES('217.98.196.71','2002-06-05 22:28:14','in','8')
+
+INSERT INTO toplista_stats VALUES('217.98.196.71','2002-06-05 22:31:45','in','8')
+
+INSERT INTO toplista_stats VALUES('217.98.196.71','2002-06-05 22:35:45','in','8')
+
+INSERT INTO toplista_stats VALUES('203.134.17.8','2002-06-06 13:43:05','out','9')
+
+INSERT INTO toplista_stats VALUES('208.190.129.213','2002-06-06 19:14:24','out','8')
+
+INSERT INTO toplista_stats VALUES('213.23.55.148','2002-06-06 22:01:15','out','8')
+
+INSERT INTO toplista_stats VALUES('217.98.196.71','2002-06-07 19:21:49','out','9')
+
+INSERT INTO toplista_stats VALUES('217.98.196.71','2002-06-07 19:22:05','out','5')
+
+INSERT INTO toplista_stats VALUES('203.31.216.38','2002-06-07 22:08:04','out','6')
+
+INSERT INTO toplista_stats VALUES('203.31.216.38','2002-06-07 22:10:56','out','6')
+
+INSERT INTO toplista_functions VALUES('1','1','1','1','1','1','1','1','1','1','1')
+
+INSERT INTO toplista_comments VALUES('Nookie','noone@nowhere.com ;-)','2002-06-01 10:52:44','Testing comments counting ;)','1','1')
+
+INSERT INTO toplista_comments VALUES('Nook','noone@nowhere.com ;-)','2002-06-02 12:37:37','Test','2','5')
+
+INSERT INTO toplista_comments VALUES('Test :)','noone@nowhere.com ;-)','2002-06-02 12:37:52','Test lol','3','5')
+
+INSERT INTO toplista_comments VALUES('Nook','noone@nowhere.com ;-)','2002-06-02 13:07:27','Bug :]','4','5')
+
+INSERT INTO toplista_comments VALUES('Uppssss','asd@asda','2002-06-04 08:30:35','You can just delete cookies or not allow it.. and vote 10000 times :(','5','8')
+
+INSERT INTO toplista_comments VALUES('Nook','noone@nowhere.com ;-)','2002-06-05 23:58:49','Test ;)','6','5')
+
+INSERT INTO toplista_comments VALUES('Nook','noone@nowhere.com ;-)','2002-06-05 23:59:11','lol buuuug :(','7','5')
+
+INSERT INTO toplista_comments VALUES('Nook','noone@nowhere.com ;-)','2002-06-06 00:41:15','Fixed! :)','8','5')
+
+INSERT INTO toplista_comments VALUES('xove','fred@xove.ca','2002-06-07 22:09:43','Cool','9','6')
+
+INSERT INTO toplista_categories VALUES('General','1')
+
+INSERT INTO toplista_categories VALUES('Test','3')
+
+INSERT INTO toplista_categories VALUES('Business','6')
+
