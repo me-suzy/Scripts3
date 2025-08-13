78b72f858eb505be2652a3c0997fc59a30c0feb4
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:30 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/viewfavorites.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/viewfavorites.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['VIEW_FAV']  =  "Alle favoriete onderwerpen bekijken";
+$ubbt_lang['VIEW_REM']  =  "Alle items in 'niet vergeten' bekijken";
+$ubbt_lang['LAST_POST']  =  "Laatste bericht";
+$ubbt_lang['REMOVE']     =  "Wissen";
+?>
