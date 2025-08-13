33fc212ab24d0c6104b611e1249182aff1bcac5b
me-suzy <me.suzana@gmail.com>
2025-08-14 00:01:27 +0300

    Add Unsorted/phpaffiliate/user/thankyou.php

---
+++ Unsorted/phpaffiliate/user/thankyou.php
@@ -0,0 +1,11 @@
+<?PHP
+
+
+        include "header.290"; 
+        
+        print "<p align=center><font size=3><b>Thank You For Joining!</b></font><br><br>"; 
+        print "<font size=2>Please check your email for the account information email we sent you.<br><br></font>";
+        
+        include "footer.290"; 
+        
+        ?>
