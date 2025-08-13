8d718fa1ada67562dbd7bcd73ca3cd39665c81ee
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:16 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/approvepost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/approvepost.php
@@ -0,0 +1,5 @@
+<?
+$ubbt_lang['REPLY_SUB']  = 	"Réponse à votre message";
+$ubbt_lang['REPLY_BOD']  = 	"a répondu à votre message sur le site : ";
+$ubbt_lang['APPR_HEAD']  = 	"Le message a été approuvé.";
+?>
