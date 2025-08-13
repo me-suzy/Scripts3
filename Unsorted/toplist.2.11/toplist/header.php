1a033f1ede0bf0bf58e43605e051f550dcb350a1
me-suzy <me.suzana@gmail.com>
2025-08-14 02:04:46 +0300

    Add Unsorted/toplist.2.11/toplist/header.php

---
+++ Unsorted/toplist.2.11/toplist/header.php
@@ -0,0 +1,10 @@
+<?
+banned(3);
+?>
+<head>
+<title><? echo $option['nameoflist']; ?> powered by Best Top List</title>
+<meta name=keywords content=<? echo "\"$keywords\""; ?>>
+<meta name=description content=<? echo "\"$description\""; ?>>
+<meta name=author content="Szymon Kosok">
+</head>
+<body><center><img src=images/logo.jpg border=0></center><br><br>
