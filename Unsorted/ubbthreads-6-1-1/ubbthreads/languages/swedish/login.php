5b4480a47b459da776d5264e8dcaced5888bd254
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:07 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/login.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/login.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['FORGOT_PASS']   = "Har du glömt ditt lösenord?";
+$ubbt_lang['FORGOT_ENTER']  = "Fyll i din e-postadress eller ditt användarnamn som du registrerade dig med för att få ett tillfälligt lösenord skickat till din e-postadress.";
+$ubbt_lang['REMEMBER_ME']   =  "Kom ihåg mig vid varje besök.";
+$ubbt_lang['LOGIN_PROMPT']  = 	"Logga in";
+$ubbt_lang['LOGIN_PROMPT2']  = 	"Fyll i användarnamn och lösenord för att logga in.  Om du inte har ett användarnamn ";
+$ubbt_lang['REG_ONE']  = 	"registrera ett";
+$ubbt_lang['PASSWORD_TEXT']  = 	"Lösenord";
+$ubbt_lang['BUTT_FORGOT']  = 	"Jag har glömt mitt lösenord";
+?>
