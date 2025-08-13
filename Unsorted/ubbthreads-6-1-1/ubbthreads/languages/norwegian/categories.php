3dec5713a1ac896c0ce2632a7f7ef6d91ca024d9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:41:28 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/categories.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/categories.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['NEW_MEM'] = "Welcome to our newest member,";
+$ubbt_lang['VIEW_PAST']  = 	"Se de siste innleggene";
+$ubbt_lang['PAST24']  = 	"Siste 24 timer";
+$ubbt_lang['PAST48']  = 	"Siste 48 timer";
+$ubbt_lang['PAST7']  = 	"Siste 7 dager";
+$ubbt_lang['THREAD_TEXT']  = 	"Emner";
+$ubbt_lang['CAT_FORUMS']  = 	"Forum";
+$ubbt_lang['REGED_USERS']  = 	"Registrerte brukere";
+$ubbt_lang['WEL_PRIV_NO']  = 	"Du er ikke logget inn.";
+$ubbt_lang['CAT_TEXT']  = 	"Kategori";
+$ubbt_lang['POSTS_TEXT']  = 	"Innlegg";
+$ubbt_lang['NEW_TEXT']  = 	"nye";
+$ubbt_lang['NOT_APPROVED']  = 	"Ikke godkjent";
+?>
