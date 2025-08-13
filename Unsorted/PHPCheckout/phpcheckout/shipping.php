7b88b76d04718502dac759adbc18a3fef5d46790
me-suzy <me.suzana@gmail.com>
2025-08-14 00:34:05 +0300

    Add Unsorted/PHPCheckout/phpcheckout/shipping.php

---
+++ Unsorted/PHPCheckout/phpcheckout/shipping.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+	<HEAD>
+		<?php require_once("configure.php");?>
+		<script language="Javascript" src="phpcheckout.js"></script>
+		<script language="Javascript">loadCSSFile();</script>
+		
+		<TITLE>Shipping Policy - phpcheckoutTM - a Dreamriver Software Product - php software mysql database - download - web database - dreamriver - freeware - sql - file downloader - internet application development - hot scripts - good value - database software - custom code - php e-mail software - ultimate web apps software - phpYellow Pages - EasySQL - phpTellAFriend - mysql php download</TITLE>
+
+	</HEAD> 
+
+<body>
+
+<!-- START of body -->
+
+<?php include("header.php");?>
+
+<blockquote>
+
+<table width="100%">
+	<!-- start of primary content FOR PAGE -->
+	<tr>
+		<!-- start MAIN COLUMN -->
+		<td valign="top">
+		<!-- PUT CONTENT BELOW HERE !!! -->
+
+
+
+		<h1>Shipping Policy</h1>
+
+		<?php include("shippingPolicy.php");?>
+
+		
+<h3>How to Get Your Retail Product</h3>
+<ul>
+	<li>your customer account will be automatically registered for you when you purchase
+	<li>if you prefer you can <a href="register.php">register your customer account now</a>
+	<li>when payment is confirmed you will receive an email
+	<li>the email will ask you to login to your customer account</a>
+	<li>you may then easily retrieve your retail purchase after logging in
+</ul>
+		<p><br></p>
+		<p><br></p>
+		<p><br></p>
+
+</blockquote>
+
+		<!-- PUT CONTENT ABOVE HERE !!! -->
+		</td>
+	</tr>
+</table>
+
+
