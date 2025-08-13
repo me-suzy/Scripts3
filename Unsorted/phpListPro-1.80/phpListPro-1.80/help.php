9a2f55804ea2d256bdc46b66d9177866dfcd4cc3
me-suzy <me.suzana@gmail.com>
2025-08-14 00:41:00 +0300

    Add Unsorted/phpListPro-1.80/phpListPro-1.80/help.php

---
+++ Unsorted/phpListPro-1.80/phpListPro-1.80/help.php
@@ -0,0 +1,67 @@
+<?
+
+################################################################################################
+
+#
+
+#  project           	: phpListPro
+
+#  filename          	: help.php
+
+#  last modified by  	: Erich Fuchs
+
+#  e-mail            	: office@smartisoft.com
+
+#  purpose           	: Display HELP
+
+#
+
+#################################################################################################
+
+
+
+
+
+#  Include Configs & Variables
+
+#################################################################################################
+
+require("config.php");
+
+include($helpheader);
+
+
+
+echo "<table class=\"standard\"><tr><td>";
+
+echo "<center><b>$lang[help]</b><hr></center>";
+
+echo "</td></tr><td>\n";
+
+echo "$lang[helpreplace1]<br>";
+
+echo "$lang[helpreplace2]<p>";
+
+echo "$lang[addplease]<p>";
+
+help("XXXXXXXX");
+
+echo "
+
+</td></tr></table>
+
+<p>
+
+<FORM action=\"$list_url/list.php$catlink\" onclick=\"exit=false\" method=\"POST\">
+
+<INPUT TYPE=\"submit\" name=\"Done_submit\" VALUE=\"$lang[done]\">
+
+<p>
+
+";
+
+
+
+include($helpfooter);
+
+?>
