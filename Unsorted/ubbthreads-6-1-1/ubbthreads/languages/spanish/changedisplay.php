a651793b381ce40e4b3bfab067fbd21349afa336
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:07 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"N&uacute;mero no v&aacute;lido de mensajes por p&aacute;gina.";
+$ubbt_lang['FLAT_BAD']  = 	"El n&uacute;mero de mensajes a mostrar en la Vista de Aguila debe estar entre 1 y 99.";
+$ubbt_lang['TEXTAREA_NO']  = 	"Valor del texto no v&aacute;lido.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Las Preferencias de Presentación han sido Modificadas.";
+?>
