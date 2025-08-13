621a780df0cfd96cc4038fe8a67ca599bd62ea8f
me-suzy <me.suzana@gmail.com>
2025-08-14 01:07:13 +0300

    Add Unsorted/PHP_PPC_Banner/PHP_PPC_Banner/banner/admin/settings.inc.php

---
+++ Unsorted/PHP_PPC_Banner/PHP_PPC_Banner/banner/admin/settings.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+$username = "root";
+$password = "";
+$host =     "localhost";
+$database = "adverts";
+$debug = 0;
+
+$cost_per_click = 5;
+
+$default_banner_img = "http://www.ace-installer.com/bigbanner1.gif";
+$default_banner_url = "http://www.ace-installer.com/testbanner.gif";
+
+$webmaster_email = 'webmaster@ACE-INSTALLKER.COM';
+
+$url_to_paypal_php = "http://www.site,.comn/paypal.php";
+
+$admin_username = "admin";
+$admin_password = "admin password";
+
+?>
