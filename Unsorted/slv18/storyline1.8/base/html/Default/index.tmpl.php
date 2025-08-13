e8527732e31597677e0a3ae2d05e27ad7f138963
me-suzy <me.suzana@gmail.com>
2025-08-14 01:47:11 +0300

    Add Unsorted/slv18/storyline1.8/base/html/Default/index.tmpl.php

---
+++ Unsorted/slv18/storyline1.8/base/html/Default/index.tmpl.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title> <%% TITLE %%> </title>
+<link rel='stylesheet' href='<?=SL_ROOT_URL?>/base/html/Default/style.css'>
+</head>
+
+<body>
+
+<%% BANNER %%>
+
+<%% MENU %%>
+
+<%% BODY %%>
+
+<%% FOOTER %%>
+
+<%% STATS %%>
+
+<! -- Please leave this notice intact and visable -->
+<%% COPYRIGHT %%>
+<! -- Please leave this notice intact and visable -->
+
+</body>
+</html>
