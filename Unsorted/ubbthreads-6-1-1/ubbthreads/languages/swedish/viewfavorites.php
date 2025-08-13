988a058b4e5e7c6c186ad1f8c54e1b9769c59899
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:49 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/viewfavorites.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/viewfavorites.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['VIEW_FAV']  =  "Visar alla favorittrådar";
+$ubbt_lang['VIEW_REM']  =  "Visar alla trådpåminnelser";
+$ubbt_lang['LAST_POST']  =  "Senaste inlägg";
+$ubbt_lang['REMOVE']     =  "Ta bort";
+?>
