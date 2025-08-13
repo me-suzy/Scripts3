0c38e8623477b9f9bad348efd32f1d59476a0d46
me-suzy <me.suzana@gmail.com>
2025-08-14 02:40:24 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "Move this post & any replies";
+$ubbt_lang['DELETE_THREAD']  =  "Delete this thread";
+$ubbt_lang['NO_EDIT']  = 	"Non possiamo verificare i tuoi permessi per modifcare questo messaggio.";
+$ubbt_lang['EDITTIME']  = 	"This post can no longer be edited because the maximum
+$ubbt_lang['PEDIT_HEAD']  = 	"Modifica messaggio";
+$ubbt_lang['PEDIT_BODY']  = 	"Puoi editare ogni cosa qui sotto per cambiare il messaggio, oppure puoi cancellarlo.  Se scegli di cancellarlo, e ci sono messaggi in risposta, il Soggetto del messaggio sarà variato in \"Questo messaggio è stato cancellato\".  Questo per mantenere intatto il formato dell'argomento.";
+$ubbt_lang['EDIT_POLL']  = 	"Because this post has a poll in it, you may delete it but not edit it.";
+$ubbt_lang['POST_ICON']  = 	"Posta Icona";
+$ubbt_lang['MARK_EDIT']  = 	"Mark as Edited?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Cambia questo messaggio";
+$ubbt_lang['PEDIT_DELETE']  = 	"Cancella questo messaggio";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Approva questo messaggio";
+$ubbt_lang['RET_NORM']  = 	"Return to normal date";
+$ubbt_lang['MAKE_STICKY']  = 	"Make post sticky";
+$ubbt_lang['MANAGE_THREAD']  = 	"Manage this thread";
+$ubbt_lang['OPEN_THREAD']  = 	"Open this thread";
+$ubbt_lang['CLOSE_THREAD']  = 	"Close this thread";
+$ubbt_lang['UNKEEP_THREAD']  = 	"Unkeep this thread";
+$ubbt_lang['KEEP_THREAD']  = 	"Keep this thread";
+$ubbt_lang['MOVE_THREAD']  = 	"Move this thread";
+?>
