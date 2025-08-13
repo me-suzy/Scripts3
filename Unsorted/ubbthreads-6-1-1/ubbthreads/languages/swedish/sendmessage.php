5fa29559025aa0a944a528342f2421bc4950ae49
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:27 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/sendmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/sendmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['CHOOSE_ADD']  = 	"Välj ett namn";
+$ubbt_lang['TEXT_DELETE']  = 	"Radera";
+$ubbt_lang['NO_PRIVATE']  = 	"Den här användaren tar inte emot privata meddelanden.";
+$ubbt_lang['NO_RECORD']  = 	"Det användarnamnet finns inte i vår databas.";
+$ubbt_lang['MESS_PREV']  = 	"Förhandsgranska privat meddelande till";
+$ubbt_lang['PREVIEW_MESS']  = 	"Här är en förhandsgranskning av ditt meddelande. Tryck på 'Fortsätt'-knappen om allt ser bra ut så skickas meddelandet. Om inte, redigera i fältet nedan.";
+$ubbt_lang['EDIT_MORE']  = 	"Redigera mer";
+$ubbt_lang['PRIV_SUB']  = 	"Du har ett nytt meddelande";
+$ubbt_lang['PRIV_BODY_R']  = 	"Du har fått ett privat meddelande på forumet {$config['title']}.  Du kan gå till {$config['phpurl']}/login.php för att läsa det.";
+$ubbt_lang['MESS_SENT']  = 	"Ditt meddelande har skickats.";
+?>
