219ab555cc46893793e5d61b05aa2d990548caf7
me-suzy <me.suzana@gmail.com>
2025-08-14 02:44:52 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/dosearch.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['NO_MORE']   =  "There are no more search results in this direction.";
+$ubbt_lang['NO_WORDS']  = 	"Faltaram as palavras de busca!";
+$ubbt_lang['SEARCH_RES']  = 	"Resultados da busca";
+$ubbt_lang['PREV_PAGE']  = 	"Página anterior";
+$ubbt_lang['MAIN_SEARCH']  = 	"Página principal de busca";
+$ubbt_lang['NEXT_PAGE']  = 	"Página seguinte";
+$ubbt_lang['NO_MATCH']  = 	"Nenhum retorno.";
+?>
