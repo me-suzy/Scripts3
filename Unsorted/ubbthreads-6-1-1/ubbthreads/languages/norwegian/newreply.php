1f11c693bb72b10ccf67dc85bc18b0eef4821787
me-suzy <me.suzana@gmail.com>
2025-08-14 02:42:14 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/newreply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/newreply.php
@@ -0,0 +1,19 @@
+<?
+$ubbt_lang['READ_PERM']  = 	"Du har bare leserettigheter i dette forum";
+$ubbt_lang['NOT_APP']  = 	"Det er ikke mulig å svare på et innlegg som ennå ikke har blitt godkjent.";
+$ubbt_lang['REPLY_HEAD']  = 	"Svar på";
+$ubbt_lang['FILL_FORM']  = 	"Fyll ut feltene under for å skrive et innlegg til forumet.";
+$ubbt_lang['NO_HTML']  = 	"Det er ikke mulig å bruke HTML i dette forum.";
+$ubbt_lang['YES_HTML']  = 	"Det er mulig å bruke HTML, når du skriver innlegg til dette forum.";
+$ubbt_lang['NO_MARKUP']  = 	"Det er ikke mulig å bruke tekstformatering i dette forum.";
+$ubbt_lang['YES_MARKUP']  = 	"Tekstformatering er mulig, så det er tillatt å bruke";
+$ubbt_lang['MARK_POSTS']  = 	"tekstformatering i dine innlegg";
+$ubbt_lang['CHOOSE_NAME']  = 	"Anonyme innlegg er tillatt, så du kan velge hvilket som helst uregistrerte navn til å sende innlegg.";
+$ubbt_lang['POST_ICON']  = 	"Ikon til innlegget";
+$ubbt_lang['POST_TEXT']  = 	"Innlegg";
+$ubbt_lang['SPELL_CHECK']  = 	"Stavekontroll";
+$ubbt_lang['DO_EMAIL']  = 	"Send alle svar til min e-postadresse";
+$ubbt_lang['DO_PREVIEW']  = 	"Jeg vil forhåndsvise innlegget før det sendes";
+$ubbt_lang['CAN_ATTACH']  = 	"og/eller legge ved en fil";
+$ubbt_lang['IN_RESPONSE']  = 	"Svar på";
+?>
