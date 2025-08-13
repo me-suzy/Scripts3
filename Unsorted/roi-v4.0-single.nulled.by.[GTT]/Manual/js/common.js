18c75eaaaabe00493e9442e079389140c3ece9a0
me-suzy <me.suzana@gmail.com>
2025-08-14 01:26:23 +0300

    Add Unsorted/roi-v4.0-single.nulled.by.[GTT]/Manual/js/common.js

---
+++ Unsorted/roi-v4.0-single.nulled.by.[GTT]/Manual/js/common.js
@@ -0,0 +1,83 @@
+<!-- common functions :::
+
+// 2use popup() function: <a href="javascript:;" onClick="javascript:popup('name', '#link', 0, 0, 300, 150, 0, 0, 1, 1, 1); return false;"></a>
+function popup(name, url, left, top, width, height, toolbar, menubar, statusbar, scrollbar, resizable){
+	var wint = (screen.height - height) / 2;
+	var winl = (screen.width - width) / 2;
+	toolbar_str = toolbar ? 'yes' : 'no';
+	menubar_str = menubar ? 'yes' : 'no';
+	statusbar_str = statusbar ? 'yes' : 'no';
+	scrollbar_str = scrollbar ? 'yes' : 'no';
+	resizable_str = resizable ? 'yes' : 'no';
+	window.open(url, name, 'left='+winl+',top='+wint+',width='+width+',height='+height+',toolbar='+toolbar_str+',menubar='+menubar_str+',status='+statusbar_str+',scrollbars='+scrollbar_str+',resizable='+resizable_str);
+}
+
+function MM_swapImgRestore() { //v3.0
+	var i,x,a=document.MM_sr;
+	for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() { //v3.0
+	var d=document;
+	if(d.images){ 
+		if(!d.MM_p) d.MM_p=new Array();
+		var i,j=d.MM_p.length,a=MM_preloadImages.arguments;
+		for(i=0; i<a.length; i++)
+		if (a[i].indexOf("#")!=0){
+		d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];
+		}
+	}
+}
+
+function MM_findObj(n, d) { //v4.01
+	var p,i,x;
+	if(!d) d=document;
+	if((p=n.indexOf("?"))>0&&parent.frames.length) {
+		d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);
+	}
+	if(!(x=d[n])&&d.all) x=d.all[n];
+	for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+	for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+	if(!x && d.getElementById) x=d.getElementById(n);
+	return x;
+}
+
+function MM_swapImage() { //v3.0
+	var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+	if ((x=MM_findObj(a[i]))!=null){
+		document.MM_sr[j++]=x;
+		if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];
+	}
+}
+
+function urlencode(ch) {
+   ch = ch.replace(/[ ]/g,"+") 
+   return escape(ch) 
+}
+function urldecode(ch) {
+   ch = ch.replace(/[+]/g," ")
+   return unescape(ch)
+}
+
+function popupimg(name, url, width, height){
+	var imgss;
+	winl = (screen.width) ? (screen.width - width) / 2 : 0;
+	wint = (screen.height) ? (screen.height - height) / 2 : 0;
+	var winname = urlencode(name);
+	var settings = 'left='+winl+',top='+wint+',width='+width+',height='+height+',toolbar=no,menubar=no,status=yes,scrollbars=yes,resizable=yes';
+	myimgss = window.open('','imgpoint',settings);
+	myimgss.document.write('<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1251"><title>'+urldecode(winname)+' // gallery</title></head><body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0"><table height="100%" width="100%" border="0" cellpadding="0" cellspacing="0"><tr><td align="center" valign="middle"><a href="javascript:self.close();"><img src="'+url+'" name="fullbimg" alt="Click to close the window..." width="'+width+'" height="'+height+'" border="0"></a></td></tr></table></body></html>');
+	if(myimgss.window.focus) {
+		myimgss.window.focus();
+	}
+}
+
+function Dating(){
+   var d, s = 'Today is: ';
+   d = new Date();
+   s += d.getDate() + "/";
+   s += (d.getMonth() + 1) + "/";
+   s += d.getYear();
+   return(s);
+}
+// -->
