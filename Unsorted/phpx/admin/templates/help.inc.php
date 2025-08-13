4738e376249a683a65e7c058b4e368f920271e5b
me-suzy <me.suzana@gmail.com>
2025-08-14 01:01:36 +0300

    Add Unsorted/phpx/admin/templates/help.inc.php

---
+++ Unsorted/phpx/admin/templates/help.inc.php
@@ -0,0 +1,19 @@
+<?php
+#$Id: help.inc.php,v 1.1 2003/07/08 18:30:19 ryan Exp $
+?>
+
+<html>
+<head>
+<title><?php print "$prog_title - $title"; ?> </title>
+<link rel="stylesheet" type="text/css" href="templates/style.css"></link>
+
+
+</head>
+<body>
+
+<?php print "$body"; ?>
+
+</body>
+</html>
+
+
