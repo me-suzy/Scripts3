0e1e848b43e76c6c1803ac9c86c77056b7357ab7
me-suzy <me.suzana@gmail.com>
2025-08-14 00:46:38 +0300

    Add Unsorted/phplive1.9.7/phplive/request_fbottom.php

---
+++ Unsorted/phplive1.9.7/phplive/request_fbottom.php
@@ -0,0 +1,30 @@
+<?
+	/*******************************************************
+	* COPYRIGHT OSI CODES - PHP Live!
+	*******************************************************/
+	$l = ( isset( $HTTP_GET_VARS['l'] ) ) ? $HTTP_GET_VARS['l'] : "" ;
+	if ( !file_exists( "web/$l/$l-conf-init.php" ) || !file_exists( "web/conf-init.php" ) )
+	{
+		print "<font color=\"#FF0000\">[Configuration Error: config files not found!] Exiting...</font>" ;
+		exit ;
+	}
+	include_once("./web/$l/$l-conf-init.php") ;
+	include_once("./system.php") ;
+?>
+<?
+	// initialize
+?>
+<?
+	// functions
+?>
+<?
+	// conditions
+?>
+<html>
+<head>
+<title> [frame bottom] </title>
+<!-- copyright OSI Codes Inc. http://www.osicodes.com [DO NOT DELETE] -->
+</head>
+<body background="pics/frames/<? echo $FRAME ?>/bottom.gif" bgColor="<? echo $CHAT_REQUEST_BACKGROUND ?>" text="<? echo $TEXT_COLOR ?>" link="<? echo $LINK_COLOR ?>" alink="<? echo $ALINK_COLOR ?>" vlink="<? echo $VLINK_COLOR ?>" marginheight="0" marginwidth="0" topmargin="0" leftmargin="0">
+</body>
+</html>
