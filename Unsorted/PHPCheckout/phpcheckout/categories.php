a853b86f0ddb867b7005df435b6f87fc8be486d5
me-suzy <me.suzana@gmail.com>
2025-08-14 00:31:00 +0300

    Add Unsorted/PHPCheckout/phpcheckout/categories.php

---
+++ Unsorted/PHPCheckout/phpcheckout/categories.php
@@ -0,0 +1,15 @@
+<?php echo"\n\n\n";?>
+<!-- START OF categories.php -->				
+<SELECT NAME="category" SIZE=1>
+	<OPTION VALUE="<?php echo $category;?>" SELECTED><?php echo $category;?></OPTION>
+	<!-- edit any option below this line - delete, add or change -->							
+	<OPTION VALUE="internet">internet</OPTION>
+	<OPTION VALUE="graphics">graphics</OPTION>
+	<OPTION VALUE="utilities">utilities</OPTION>
+	<OPTION VALUE="sound">sound</OPTION>	
+	<OPTION VALUE="games">games</OPTION>
+	<OPTION VALUE="database">database</OPTION>	
+	<OPTION VALUE="other">other</OPTION>
+	<OPTION VALUE="none">none</OPTION>
+</SELECT>
+<!-- END OF categories.php -->	
