a9df991f93aed3b457557888931e5a029336c8d0
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:14 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"F&uuml;r diesen Benutzer existiert kein Benutzer-Profil";
+$ubbt_lang['EMAIL_HEAD']  = 	"Email-Benachrichtigungen &auml;ndern f&uuml;r";
+$ubbt_lang['PROF_EDIT']  = 	"Hier k&ouml;nnen alle pers&ouml;nlichen Einstellungen ge&auml;ndert werden.";
+$ubbt_lang['PROF_SUBS']  = 	"Email-Benachrichtigungen / -Subskriptionen";
+$ubbt_lang['PROF_NOTIF']  = 	"M&ouml;chten Sie eine Email-Benachrichtigung, wenn private Nachrichten an Sie eingehen?";
+$ubbt_lang['PROF_REPLIES']  = 	"M&ouml;chten Sie alle Antworten auf Ihre Nachrichten auch per Email bekommen?";
+$ubbt_lang['SUB_STAT']  = 	"Subskriptions/Abo-Status f&uuml;r folgende Foren.  (Sie erhalten einmal t&auml;glich eine Mail mit allen neuen Nachrichten dieses Forums.)";
+$ubbt_lang['SUB_SHOW']  = 	"(A - Abo ; k - kein Abo)";
+$ubbt_lang['TEXT_S']  = 	"A";
+$ubbt_lang['TEXT_U']  = 	"k";
+?>
