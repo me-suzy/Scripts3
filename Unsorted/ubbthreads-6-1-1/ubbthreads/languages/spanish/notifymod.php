9406316864ebf5b6fa18814601e0d25155b6a9af
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:52 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/notifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/notifymod.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NOTIFY_MOD']  =  	"Avisar al Moderador";
+$ubbt_lang['NOTIFY_HEAD']  = 	"Esto env&iacute;a un mensaje de ALERTA al moderador o moderadores del tablero para que lo miren por si tiene contenido il&iacute;cito.";
+$ubbt_lang['SEND_IT']  =  "¡Si, quiero enviar un AVISO al moderador!";
+$ubbt_lang['NO_POST']  = 	"Se detect&oacute; un problema localizando este mensaje en la base de datos.";
+?>
