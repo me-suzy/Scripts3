867293f2af6cfc9364a3d14332c0b1459393e5e1
me-suzy <me.suzana@gmail.com>
2025-08-14 00:31:20 +0300

    Add Unsorted/PHPCheckout/phpcheckout/customize.php

---
+++ Unsorted/PHPCheckout/phpcheckout/customize.php
@@ -0,0 +1,67 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+	<head>
+
+		<script language="Javascript" src="appinstaller.js"></script>
+		<script language="Javascript">loadCSSFile();</script>
+		<title>Install Helper - a DreamRiver Software Product - Easily install DreamRiver Software - php software mysql database - download - web database - dreamriver - freeware - sql - file downloader - internet application development - hot scripts - good value - database software - custom code - php e-mail software - ultimate web apps software - phpYellow Pages - EasySQL - phpTellAFriend - mysql php download</title>
+	</head> 
+
+<body>
+<!-- START of body -->
+
+<h1>DreamRiver Install Helper</h1>
+
+<H2>phpcheckout<sup>TM</sup></H2>
+
+
+<form name="startForm">
+<table bgcolor="silver" cellspacing=3 cellpadding=10 width="80%">
+<tr>
+	<th colspan=2>Install Helper</th>
+</tr>
+<tr>
+	<td bgcolor="white" valign="top">
+		<p><br></p>
+
+		<h4>After Installation Customization</h4>
+		
+
+		<p>
+		These steps will quickly create your own 'look and feel':
+
+		<ul>
+			<li>modify header.php and footer.php</li>
+			<li>change the theme name in phpcheckout.js on line 17 from 'themeMuted.css' to 'themeBright.css'</li>
+			<li>modify or create the .css file. Hint: start by using *theme*.css files in /phpcheckout</li>
+			<li>change the .css class called 'favcolor' and 'favcolor2' to dramatically alter your pages</li>
+			<li>create your own ads on index.php</li>
+			<li>add your own resources by going to <i>> admin.php > Work with Items > Add</i></li>
+			<li>complete the Featured Product and Service page templates</li>
+			<li>edit the generic menu in nav_dist.js</li>
+			<li>for more advanced users, change the layout of the page structures</li>
+			<li>save every file before you start so you always have a working backup copy to return to</li>
+			<li>HAVE FUN!</li>
+		</ul>
+
+		</p>
+
+ 
+
+	</td>
+	<td bgcolor="ivory" align="center">
+		<img src="installBOP.jpg" width="180" height="180" border=0>
+	</td>
+</tr>
+<tr>
+	<td align="right" colspan=2>
+		<hr>
+		&nbsp;
+	</td>
+</tr>
+</table>
+</form>
+
+
+</body>
+</html>
