715edb6847580f01f24b11462befc68d07465f4b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:25 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/addfav.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/addfav.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  =  "Eftersom du inte har tillåtelse att läsa denna tråd så kan du inte lägga till det i din favorit/påminnelselista.";
+$ubbt_lang['MAIN_ONLY']  =  "Du kan bara lägga till huvudinlägg till din favorit- eller påminnelselista.";
+$ubbt_lang['DUP_ENTRY']  =  "Detta är redan inlagt i din favorit/påminnelselista.";
+$ubbt_lang['ENTRY_IN']  =  "Inlägg/Tråd tillagd";
+$ubbt_lang['FAV_CONFIRM']  =  "Inlägget/Tråden har lagts till din lista. Du kommer nu se detta i din 'Kontrollpanel' tills du tagit bort det om du lagt in det i favoritlistan. Om du lagt in det som en påminnelse så kommer den försvinna så fort du har svarat på inlägget.";
+$ubbt_lang['FORUM_RETURN']  = "Återvänd till forumet nu.";
+?>
