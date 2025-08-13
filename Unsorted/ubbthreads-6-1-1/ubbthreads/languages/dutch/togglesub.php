1f1426ff2c6ec009555ef66b053e892f459fdaa5
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:26 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/togglesub.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/togglesub.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['UN_HEADER']  = 	"Notificatie verwijderd";
+$ubbt_lang['HEADER']  = 	"Notificatie toegevoegd";
+$ubbt_lang['UN_BODY']  = 	"Voor volgend forum is de notificatie verwijderd: ";
+$ubbt_lang['BODY']  =          "Voor volgend forum is de notificatie toegevoegd: ";
+?>
