243c751d186e075d822d7f93a2721f4f423e6c0a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:20 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/showprofile.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/showprofile.php
@@ -0,0 +1,27 @@
+<?
+$ubbt_lang['RATING']    =       "Waarderingen";
+$ubbt_lang['T_RATES']   =       "Totaal aantal waarderingen";
+$ubbt_lang['RATE_USER'] =       "Waardeer dit lid";
+$ubbt_lang['STAR1'] =           "1 ster";
+$ubbt_lang['STAR2'] =           "2 sterren";
+$ubbt_lang['STAR3'] =           "3 sterren";
+$ubbt_lang['STAR4'] =           "4 sterren";
+$ubbt_lang['STAR5'] =           "5 sterren";
+$ubbt_lang['DORATE']    =       "Verstuur waardering";
+$ubbt_lang['YOURATED']  =       "Je hebt dit lid gewaardeerd met ";
+$ubbt_lang['PROF_FOR']  = 	"Profiel van";
+$ubbt_lang['TEXT_NAME']  = 	"Volledige naam";
+$ubbt_lang['USER_TITLE']  = 	"Titel";
+$ubbt_lang['TOTAL_POSTS']  = 	"Totaal aantal berichten";
+$ubbt_lang['TEXT_HOME']  = 	"Homepage";
+$ubbt_lang['TEXT_OCCU']  = 	"Beroep";
+$ubbt_lang['TEXT_HOBBY']  = 	"Hobby's";
+$ubbt_lang['TEXT_LOC']  = 	"Woonplaats/provincie";
+$ubbt_lang['TEXT_BIO']  = 	"Over jezelf";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ Nummer";
+$ubbt_lang['REGGED_ON']  = 	"Lid sinds";
+$ubbt_lang['EDIT_T_U']  = 	"Profiel bewerken";
+$ubbt_lang['ADD_BOOK']  = 	"In adresboekje";
+$ubbt_lang['SHOW_ALL_P']  = 	"Berichten van dit lid";
+$ubbt_lang['NO_LONGER']  = 	"Dit lid komt niet meer in onze database voor.";
+?>
