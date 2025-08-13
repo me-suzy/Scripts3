6cd90cf90291c152f46b7d444e460728869ae09c
me-suzy <me.suzana@gmail.com>
2025-08-14 01:04:56 +0300

    Add Unsorted/phpYellowPages/phpYellow/flags.php

---
+++ Unsorted/phpYellowPages/phpYellow/flags.php
@@ -0,0 +1,11 @@
+<table border=0 cellpadding=3 cellspacing=20>
+<tr>
+	<td><img src="appimage/usa.gif" width="46" height="32" border=0 alt="U.S.A."><br>U.S.A.</td>
+	<td><img src="appimage/uk.gif" width="46" height="32" border=0 alt="United Kingdom"><br>UK</td>
+	<td><img src="appimage/australia.gif" width="46" height="32" border=0 alt="Australia"><br>Australia</td>
+	<td><img src="appimage/canada.gif" width="46" height="32" border=0 alt="Canada"><br>Canada</td>
+	<td><img src="appimage/germany.gif" width="46" height="32" border=0 alt="Germany"><br>Germany</td>	
+	<td><img src="appimage/brazil.gif" width="46" height="32" border=0 alt="Brazil"><br>Brazil</td>
+	<td><img src="appimage/india.gif" width="46" height="32" border=0 alt="India"><br>India</td>
+</tr>
+</table>
