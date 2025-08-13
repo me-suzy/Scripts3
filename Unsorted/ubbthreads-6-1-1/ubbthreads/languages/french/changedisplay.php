e6b724627cdd27dc06f0216ea61d7ce84a2f42f7
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:21 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"La valeur des messages par page ne peut pas être utilisée.";
+$ubbt_lang['FLAT_BAD']  = 	"Le nombre de messages plat par page doit-être compris entre 1 et 99.";
+$ubbt_lang['TEXTAREA_NO']  = 	"La valeur de la zone de texte ne peut pas être utilisée.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Vos préférences d'affichage ont été modifiées.";
+?>
