3873db4231a400c42956f82f88493c8f59885fee
me-suzy <me.suzana@gmail.com>
2025-08-14 00:33:25 +0300

    Add Unsorted/PHPCheckout/phpcheckout/offline.php

---
+++ Unsorted/PHPCheckout/phpcheckout/offline.php
@@ -0,0 +1,8 @@
+<p style="font-size:36px;color:peachpuff;font-weight:bold;font-family:Arial;">
+Offline for Maintenance<br>
+This website is temporarily offline for regularly scheduled maintenance.<br> 
+We apologize for any inconvenience.<br> 
+Bookmark this page and visit us later. 
+</p>
+
+
