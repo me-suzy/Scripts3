266d0e0ebef0c6a7710545b71538eb1dfed42f39
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:07 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/dosearch.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['NO_MORE']   =  "There are no more search results in this direction.";
+$ubbt_lang['NO_WORDS']  = 	"Sie m&uuml;ssen einige Schl&uuml;sselw&ouml;rter f&uuml;r die Suche eingeben.";
+$ubbt_lang['SEARCH_RES']  = 	"Suchergebnis f&uuml;r Anfrage";
+$ubbt_lang['PREV_PAGE']  = 	"Vorherige Seite";
+$ubbt_lang['MAIN_SEARCH']  = 	"Such-Seite";
+$ubbt_lang['NEXT_PAGE']  = 	"N&auml;chste Seite";
+$ubbt_lang['NO_MATCH']  = 	"Leider lieferte Ihre Anfrage kein Ergebnis";
+?>
