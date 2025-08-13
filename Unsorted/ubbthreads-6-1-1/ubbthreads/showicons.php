4a29c7109d5d6eeae6807298128ccff65d574f26
me-suzy <me.suzana@gmail.com>
2025-08-14 02:22:34 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/showicons.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/showicons.php
@@ -0,0 +1,34 @@
+<?
+/*
+# UBB.threads, Version 6
+# Official Release Date for UBB.threads Version6: 06/05/2002
+
+# First version of UBB.threads created July 30, 1996 (by Rick Baker).
+# This entire program is copyright Infopop Corporation, 2002.
+# For more info on the UBB.threads and other Infopop
+# Products/Services, visit: http://www.infopop.com
+
+# Program Author: Rick Baker.
+
+# You may not distribute this program in any manner, modified or otherwise,
+# without the express, written written consent from Infopop Corporation.
+
+# Note: if you modify ANY code within UBB.threads, we at Infopop Corporation
+# cannot offer you support-- thus modify at your own peril :)
+# ---------------------------------------------------------------------------
+*/
+
+
+// Require the library
+   require ("main.inc.php");
+   require ("languages/${$config['cookieprefix']."w3t_language"}/showicons.php");         
+
+// ---------------------
+// Send the page to them
+   $html = new html;
+   $html -> send_header($ubbt_lang['POST_ICON'],$Cat,0,0,0,0);
+
+   include("$thispath/templates/$tempstyle/showicons.tmpl"); 
+// -------------
+// Send a footer
+   $html -> send_footer();
