9ae0488f9a1061046f8c90af2225f60c59e8c4e9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:58 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"Anfrage Passwort";
+$ubbt_lang['PASS_REQ_BOD1']  = "Jemand von der IP Adresse";
+$ubbt_lang['PASS_REQ_BOD2']  = "hat ein neues Paßwort für den Benutzernamen";
+$ubbt_lang['PASS_REQ_BOD3']  = "für das '{$config['title']}' angefordert.  Dies ist ein temporäres Paßwort, dass benutzt werden kann, wenn Sie Ihr normales vergessen haben.  Falls Sie das Paßwort nicht angefordert haben, ignorieren Sie bitte diese Nachricht.  Das Paßwort für diesen Benutzernamen ist";
+$ubbt_lang['PASS_MAILED']  = 	"Passwort per Email geschickt.";
+$ubbt_lang['PASS_MAILED2']  = 	"Ein tempor&auml;res Pa&szlig;wort f&uuml;r diesen Benutzer wurde versendet.";
+$ubbt_lang['NO_EMAIL']  = 	"Kann Email-Adresse nicht finden f&uuml;r Benutzer";
+?>
