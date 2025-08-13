f4164b6363676b415e5598b06ec9ae74f3289ce0
me-suzy <me.suzana@gmail.com>
2025-08-14 00:59:51 +0300

    Add Unsorted/phpUploadCenter/include/cz.php

---
+++ Unsorted/phpUploadCenter/include/cz.php
@@ -0,0 +1,236 @@
+<?
+
+// Header and footer files
+
+$headerpage="include/header.htm";
+$footerpage="include/footer.htm";
+$infopage="include/info.htm";
+
+$charsetencoding="windows-1250";
+$uploadcentercaption="SatTV - Upload Center";
+$uploadcentermessage="Info:Sledování placené TV bez zaplacené abonence je nelegální!<br>Vechny soubory lze pouít pouze pro vzdìlávací úèely!<br> Neneseme ádnou odpovìdnost za pøípadné kody zpùsobené<br>pouitím zde uloených souborù!";
+
+$mess=array(
+"0" => "",
+"1" => "Leden",
+"2" => "Únor",
+"3" => "Bøezen",
+"4" => "Duben",
+"5" => "Kvìten",
+"6" => "Èerven",
+"7" => "Èervenec",
+"8" => "Srpen",
+"9" => "Záøí",
+"10" => "Øíjen",
+"11" => "Listopad",
+"12" => "Prosinec",
+"13" => "Dnes",
+"14" => "Vèera",
+"15" => "Jméno souboru",
+"16" => "Rating",
+"17" => "Velikost",
+"18" => "Nahráno dne",
+"19" => "Poznámka",
+"20" => "Upload souboru",
+"21" => "Lokální soubor",
+"22" => "Poznámka k souboru",
+"23" => "Download",
+"24" => "Order",
+"25" => "Home",
+"26" => "Soubor",
+"27" => "Tisk",
+"28" => "Zavøít",
+"29" => "Zpìt",
+"30" => "Soubor byl odstanìn",
+"31" => "Nemohu otevøít soubor",
+"32" => "Zpìt",
+"33" => "Chyba pøi uploadu souboru !",
+"34" => "Musí vybrat soubor",
+"35" => "Zpìt",
+"36" => "Soubor",
+"37" => "byl úspìnì nahrán",
+"38" => "Soubor s tímto jménem",
+"39" => "ji existuje",
+"40" => "Soubor byl úspìnì nahtán",
+"41" => "Jazyk byl úspìnì vybrán (pøeloil SATHACK_cz)",
+"42" => "Vítej v Sat-TV Upload Centru!",
+"43" => "Obsazený prostor na disku",
+"44" => "Zobraz soubory z posledních dní",
+"45" => "Vadný ZIP archiv!",
+"46" => "Obsah archivu:",
+"47" => "Datum/Èas",
+"48" => "Adresáø",
+"49" => "Je zakázáno nahrávat soubor s tímto jménem %s",
+"50" => "pøekroèil maximální dovolenou velikost",
+"51" => "Informace",
+"52" => "Vyber Skin",
+"53" => "Skin",
+"54" => "Vítej",
+"55" => "Aktuální èas",
+"56" => "User",
+"57" => "Username",
+"58" => "Registruj",
+"59" => "Registrace",
+"60" => "Nedìle",
+"61" => "Pondìlí",
+"62" => "Úterý",
+"63" => "Støeda",
+"64" => "Ètvrtek",
+"65" => "Pátek",
+"66" => "Sobota",
+"67" => "Poli",
+"68" => "Mail file",
+"69" => "File has been mailed to %s address.",
+"70" => "File uploaded by user: %s",
+"71" => "Login",
+"72" => "Logout",
+"73" => "Enter",
+"74" => "Anonym",
+"75" => "Normal User",
+"76" => "Power User",
+"77" => "Administrátor",
+"78" => "Privátní zóna",
+"79" => "Veøejná zóna",
+"80" => "patné uivatelské jmého nebo heslo.",
+"81" => "Mùj profil",
+"82" => "Ukázat/editovat mùj profil",
+"83" => "Heslo",
+"84" => "Vyber jazyk",
+"85" => "Vyber èasovou zónu",
+"86" => "Vá aktuální èas",
+"88" => "Prosím vlo platnou e-mailovou adresu.",
+"89" => "Aktivaèní kód bude zaslán na Vá e-mail.",
+"90" => "File uploaded: ",
+"91" => "Prosím vlo e-mailovou adrsu zadanou pøi registraci.",
+"92" => "File size: ",
+"93" => "Prosím napi svoje jmého a heslo",
+"94" => "Registrace je nutná. Prosím zaregisruj se.",
+"95" => "Registrace není nutná. Pokud se zaregistujete bude Vae uivatelské jméno pøidáno k nahranému souboru a nikdo dalí nemùe pouít Vae jméno k nahranému souboru.",
+
+"96" => "Skin selected.",
+"97" => "Refresh",
+"98" => "Please, enter your login name and password",
+"99" => "Still not registered? - Register here!",
+"100" => "Forgotten your password?",
+"101" => "Please, go %s back %s and try again.",
+"102" => "You succesfully logged out.",
+"103" => "User name is invalid. The name must be not longer than 12 symbols and can consists of latin symbols and digits only. Name can also contain '-', '_', and space symbols inside.",
+"104" => "The '%s' you picked has been taken.",
+"105" => "Confirm password",
+"106" => "Passwords do not match.",
+"107" => "The format of entered e-mail address is invalid.",
+"108" => "Thank you for registering. Please do not forget your password as it has been encrypted in our database and we cannot retrieve it for you. However, should you forget your password we provide an easy to use script to generate and email a new, random, password.",
+"109" => "You can %s enter to Upload Center here. %s",
+"110" => "Your activation code has been e-mailed to you. You must activate your account within 2 days or account will be automatically removed.",
+"111" => "
+Dear %s,
+
+Thank You for registration at our Upload Center.
+
+Because your security is importance to us, your account will need to be activated upon receipt.
+
+Your personal activation code is: %s
+(please note: this is not your password)
+
+Activating Your account is simple:
+1. Visit us at $installurl/activate.php and we will guide you through the process
+2. Enter your account name and activation code.
+3. Click on the 'Activate account' button.
+
+Regards,
+$admin_name
+Email: mailto:$admin_email
+Web Page: $installurl",
+"112" => "Activate account",
+"113" => "Please, activate your account",
+"114" => "Activation code",
+"115" => "Your account now is active.",
+"116" => "Please %s enter here %s.",
+"117" => "The entered account name or activation code is invalid.",
+"118" => "Account already active.",
+"119" => "I wish to receive to my e-mail everyday digest of uploaded files.",
+"120" => "Change your password.",
+"121" => "Your old password",
+"122" => "The entered account name is not exists.",
+"123" => "The entered e-mail address is not valid.",
+"124" => "Your new password has been sent to your e-mail.",
+"125" => "
+Dear Upload Center user,
+
+Your new Upload Center password is %s
+
+Regards,
+$admin_name
+Email: mailto:$admin_email
+Web Page: $installurl",
+"126" => "Customize your account settings",
+"127" => "Apply",
+"128" => "Your profile saved.",
+"129" => "Your password changed.",
+"130" => "You typed invalid previous password.",
+"131" => "You must specify your new password.",
+"132" => "CONFIGURATION",
+"133" => "Upload",
+"134" => "Language & timezone",
+"135" => "Account statistics",
+"136" => "Your account has been created:",
+"137" => "User management",
+"138" => "Viewer (view only)",
+"139" => "Uploader (upload only)",
+"140" => "Account '%s' changed successfully",
+"141" => "Latest",
+"142" => "All",
+"143" => "New e-mail address takes effect after confirmation. Confirmation code has been e-mailed to your new mail address. See instructions inside the letter.",
+"144" => "
+Dear %s,
+
+Because your security is importance to us, your new e-mail address will need to be confirmed upon receipt.
+
+Your personal confirmation code is: %s
+
+Activating e-mail address is simple:
+1. Visit us at $installurl/confirm.php and we will guide you through the process
+2. Enter your account name and confirmation code.
+3. Click on the 'Confirm' button.
+
+Regards,
+$admin_name
+Email: mailto:$admin_email
+Web Page: $installurl",
+"145" => "Please, confirm your new e-mail address.",
+"146" => "Confirmation code",
+"147" => "Confirm",
+"148" => "Nothing to confirm.",
+"149" => "The entered account name or confirmation code is invalid.",
+"150" => "Your new e-mail address '%s' confirmed.",
+"151" => "Files uploaded",
+"152" => "Files downloaded",
+"153" => "Files e-mailed",
+"154" => "Account created",
+"155" => "Last access time",
+"156" => "Status",
+"157" => "Active status",
+"158" => "Receive digest",
+"159" => "e-mail",
+"160" => "Total:",
+"161" => "account(s)",
+"162" => "Delete account",
+"163" => "Shown %s account(s) of %s",
+"164" => "Configure Upload Center",
+"165" => "Edit files",
+"166" => "Edit file",
+"167" => "File %s has been changed succesfully",
+"168" => "Save",
+"169" => "Delete",
+"170" => "Delete files",
+"171" => "Mirror",
+"172" => "Yes",
+"173" => "No",
+"174" => "Active",
+"175" => "Inactive",
+"176" => "Unautorized",
+"177" => "Sorry, but server could not execute the mail program.",
+"178" => "Your registration failed. Please, try again later.",
+"179" => "Please, try again later."
+);
+?>
