effccc14826cfe09b407f0859de3605e3365acd8
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:54 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/controlpanel.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/controlpanel.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['SHOW_ALL']  = 	"...alle anzeigen";
+$ubbt_lang['PERSONAL']  = 	"Pers&ouml;nliche Informationen, eMail, Passwort, usw...";
+$ubbt_lang['DISPLAY']  = 	"Darstellungsoptionen, Anzahl dargestellter Threads, Sprache, Farbe, usw...";
+$ubbt_lang['SUB_UN']  = 	"eMail-Benachrichtigungen, -Subskriptionen, usw...";
+$ubbt_lang['YOUR_VIEW']  = 	"Forenbesucher sehen Sie als:";
+$ubbt_lang['YOUR_VIEW_WITH']  =  "mit";
+$ubbt_lang['VIEW_POSTS']  = 	"Nachrichten";
+$ubbt_lang['TEXT_TOTAL']  = 	"Gesamt";
+$ubbt_lang['TEXT_UNREAD']  = 	"Ungelesen";
+$ubbt_lang['EDIT_ICON']  = 	"&Auml;ndern";
+$ubbt_lang['WEL_CONTROL']  = 	"Einstellungen f&uuml;r ";
+$ubbt_lang['MAIN_CONFIG']  = 	"Haupteinstellungen";
+$ubbt_lang['EDIT_TEXT']  = 	"&Auml;ndern";
+$ubbt_lang['PRIVATE_BOX']  = 	"Briefkasten (private Nachrichten)";
+$ubbt_lang['FRIENDS_ON']  = 	"Freunde Online";
+$ubbt_lang['FROM_ADDR']  = 	"(aus Adre&szlig;buch)";
+$ubbt_lang['FAV_THREADS']  = 	"Favoriten-Threads";
+$ubbt_lang['TEXT_REMOVE']  = 	"L&ouml;schen";
+$ubbt_lang['REM_THREADS']  = 	"'Antwort schreiben'-Merkzettel";
+$ubbt_lang['SEND_A_PM']  = 	"Senden";
+$ubbt_lang['FAV_FORUMS']  = 	"Favoriten-Foren";
+$ubbt_lang['BY_TEXT']  = 	"von";
+?>
