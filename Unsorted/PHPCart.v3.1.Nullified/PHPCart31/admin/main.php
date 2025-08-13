25b377bfa610dc6641f13d23352b8ae983b605ec
me-suzy <me.suzana@gmail.com>
2025-08-14 00:29:52 +0300

    Add Unsorted/PHPCart.v3.1.Nullified/PHPCart31/admin/main.php

---
+++ Unsorted/PHPCart.v3.1.Nullified/PHPCart31/admin/main.php
@@ -0,0 +1,47 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+// Program Name         : PHPCart                                          //
+// Release Version      : 3.1                                              //
+// Program Author       : Mr B Wiencierz                                   //
+// Home Page            : http://www.phpcart.co.uk                         //
+// Supplied by          : CyKuH [WTN]                                      //
+// Nullified by         : CyKuH [WTN]                                      //
+// Distribution         : via WebForum, ForumRU and associated file dumps  //
+//                                                                         //
+//                All code is ©2000-2004 Barry Wiencierz.                  //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
+require("hf.php");
+pageHeader();?>
+<div align="center">
+  <center>
+  <table border="0" cellpadding="5" cellspacing="5" width="100%">
+    <tr>
+      <td width="100%"><B><script language="javascript" type="text/javascript">document.write(news_title10);</script></B>
+<script language="javascript" type="text/javascript">document.write(news_content10);</script>
+<B><script language="javascript" type="text/javascript">document.write(news_title9);</script></B>
+<script language="javascript" type="text/javascript">document.write(news_content9);</script>
+<B><script language="javascript" type="text/javascript">document.write(news_title8);</script></B>
+<script language="javascript" type="text/javascript">document.write(news_content8);</script>
+<B><script language="javascript" type="text/javascript">document.write(news_title7);</script></B>
+<script language="javascript" type="text/javascript">document.write(news_content7);</script>
+<B><script language="javascript" type="text/javascript">document.write(news_title6);</script></B>
+<script language="javascript" type="text/javascript">document.write(news_content6);</script>
+<B><script language="javascript" type="text/javascript">document.write(news_title5);</script></B>
+<script language="javascript" type="text/javascript">document.write(news_content5);</script>
+<B><script language="javascript" type="text/javascript">document.write(news_title4);</script></B>
+<script language="javascript" type="text/javascript">document.write(news_content4);</script>
+<B><script language="javascript" type="text/javascript">document.write(news_title3);</script></B>
+<script language="javascript" type="text/javascript">document.write(news_content3);</script>
+<B><script language="javascript" type="text/javascript">document.write(news_title2);</script></B>
+<script language="javascript" type="text/javascript">document.write(news_content2);</script>
+<B><script language="javascript" type="text/javascript">document.write(news_title);</script></B>
+<script language="javascript" type="text/javascript">document.write(news_content);</script>
+
+</td>
+    </tr>
+  </table>
+  </center>
+</div>
+<?pageFooter();?>
