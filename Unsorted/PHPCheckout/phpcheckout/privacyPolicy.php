5526ba01de95cdc942edfada1f95833fe2482d39
me-suzy <me.suzana@gmail.com>
2025-08-14 00:33:41 +0300

    Add Unsorted/PHPCheckout/phpcheckout/privacyPolicy.php

---
+++ Unsorted/PHPCheckout/phpcheckout/privacyPolicy.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+	<HEAD>
+		<?php require_once("configure.php");?>
+		<script language="Javascript" src="phpcheckout.js"></script>
+		<script language="Javascript">loadCSSFile();</script>
+		
+		<TITLE>Privacy Policy -  <?php echo IMPLEMENTATIONNAME;?> - <?php echo BENEFIT;?> -  <?php echo ORGANIZATION;?> - Powered by phpcheckoutTM from DreamRiver http://wwww.dreamriver.com - Software Powers the Net - a DreamRiver Internet Software Product - php software mysql database - download - web database - dreamriver - freeware - sql - file downloader - internet application development - hot scripts - good value - database software - custom code - php e-mail software - ultimate web apps software - phpYellow Pages - EasySQL - phpTellAFriend - mysql php download - Easily create complete internet programs with php, apache, mysql and windows</TITLE>
+
+	</HEAD> 
+
+<body>
+
+<!-- START of body -->
+
+<?php include("header.php");?>
+
+<blockquote>
+
+<table width="100%">
+	<!-- start of primary content FOR PAGE -->
+	<tr>
+
+		<!-- start MAIN COLUMN -->
+		<td valign="top">
+		<!-- PUT CONTENT BELOW HERE !!! -->
+
+
+
+
+     <h1>Privacy Policy</h1>
+
+     
+     
+
+
+<table class="favcolor">
+<tr>
+	<td><img src="appimage/privacy.png" width="107" height="94" border=0 alt="Private"></td>
+	<td>
+		<H2>Your Data is Private</H2>
+		<p>
+WE DO NOT sell, rent, or trade any information. Period.
+</b> 
+</p>
+
+<p>
+If you have any questions, feel free to <A HREF="contact.php">contact us directly.</A>
+</p>	
+
+</td>
+</tr>
+</table>
+
+
+</blockquote>
+
+		<!-- PUT CONTENT ABOVE HERE !!! -->
+		</td>
+	</tr>
+</table>
+
+
+
+
+
+</body>
+</html>
