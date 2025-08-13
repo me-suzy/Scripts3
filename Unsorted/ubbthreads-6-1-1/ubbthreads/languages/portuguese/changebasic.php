8b4a44a54ea1b729b30b659e4ddc07fac7419bc3
me-suzy <me.suzana@gmail.com>
2025-08-14 02:44:33 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/changebasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/changebasic.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['PIXEL_TOO_BIG']="Your picture exceeds the ({$theme['PictureWidth']}x{$theme['PictureHeight']}) pixel limitation width a width/height of ";
+$ubbt_lang['FILE_TOO_BIG']="Pictures can be no larger than {$config['avatarsize']} bytes.";
+$ubbt_lang['BAD_PIC']  =       "Improper picture type.";
+$ubbt_lang['PASS_MATCH']  = 	"As senhas diferem.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"A senha deve ter entre 4 e 20 caracteres.";
+$ubbt_lang['HOME_TOO_LONG']  = 	"O campo do endereço é muito comprido.";
+$ubbt_lang['OCC_TOO_LONG']  = 	"O campo de ocupação é muito comprido.";
+$ubbt_lang['HOBB_TOO_LONG']  = 	"O campo de hobbies é muito comprido.";
+$ubbt_lang['LOC_TOO_LONG']  = 	"O campo de localização é muito comprido.";
+$ubbt_lang['SIG_TOO_LONG']  = 	"O campo de assinatura é muito comprido.";
+$ubbt_lang['BIO_TOO_LONG']  = 	"O campo de biografia é muito comprido.";
+$ubbt_lang['BASIC_MOD']  = 	"O perfil básico foi alterado.";
+?>
