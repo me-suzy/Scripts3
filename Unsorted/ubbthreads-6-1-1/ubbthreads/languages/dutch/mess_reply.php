111a1a879ae7855aafd1c9a6ea4619b911fabdcf
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:54 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/mess_reply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/mess_reply.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['EDIT_MORE']  = 	"Bewerken";
+$ubbt_lang['REPLY_PREV']  = 	"Bekijk jouw antwoord aan";
+$ubbt_lang['PREVIEW_MESS']  = 	"Dit is een voorbeeld van jouw bericht.  Als alles er nu goed uitziet, druk dan op 'Doorgaan' om je bericht te versturen.  Je kunt ook onderaan de pagina veranderingen aanbrengen en dan op de onderste knop drukken.";
+$ubbt_lang['MESS_PREVIEW']  = 	"Ik wil eerst een voobeeld zien";
+$ubbt_lang['PRIV_SUB']  = 	"Je hebt nieuwe berichten";
+$ubbt_lang['PRIV_BODY_R']  = 	"Je hebt een privébericht ontvangen via {$config['title']}.  Je kunt nu naar {$config['phpurl']}/login.php gaan om het bericht te bekijken.";
+?>
