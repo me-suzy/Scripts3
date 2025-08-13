5faf0af598ad8e9751fcf4b018200de8c1d81551
me-suzy <me.suzana@gmail.com>
2025-08-14 00:16:41 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/usermail_prepay.inc.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/usermail_prepay.inc.php
@@ -0,0 +1,67 @@
+#
+# --Confirmation e-mail file
+# 
+# 			This file contains the message your customers
+# 			will receive as a confirmation after the registration
+#			process.
+#			Lines starting with # will be skipped.
+#			Blank lines will be maintained.
+#
+#			Change the message below as needed considering the 
+#			following tags to reflect your customer's personal data:
+#
+#        --------------------------------------------------------
+#			TAG SYNTAX				EFFECT
+#        --------------------------------------------------------
+#
+#			<#c_id#>							customer ID
+#			<#c_name#>						name
+#			<#c_nick#>						nick
+#			<#c_address#> 					address
+#			<#c_city#> 			   		city
+#			<#c_prov#>  					province
+#			<#c_country#> 					country
+#			<#c_zip#> 						zip
+#			<#c_phone#> 					phone number
+#			<#c_email#> 					e-mail address
+#			<#c_confirmation_page #>        confirm page
+#
+#        --------------------------------------------------------
+#
+#			USAGE:
+#			Insert the above tags in the text of your message			
+#			where you want each value to appear.			
+#			Mofidy the message to reflect your needs.
+#
+# 
+# 
+#  
+
+Dear <#c_name#>,
+
+Thank you for registering at <#c_sitename#>.  Your user information appears below.
+Please visit us often at <#c_siteurl#>.
+
+Your Profile
+--------------------
+# ID: <#c_id#>					
+Username: <#c_nick#>
+Password: <#c_password#>
+# Full Name: <#c_name#>
+Address: <#c_address#>
+City: <#c_city#>
+Province/Region: <#c_prov#>  
+Country: <#c_country#>
+ZIP Code: <#c_zip#>
+Telephone: <#c_phone#>
+E-mail: <#c_email#>
+
+For your registration to be activated, we need to process the sign up fee payment charging your account.
+You will receive a payment confirmation e-mail as soon as the transaction is completed. 
+ 
+If you have received this message in error, please reply to this email or
+write to <#c_adminemail#>.  
+
+
+
+
