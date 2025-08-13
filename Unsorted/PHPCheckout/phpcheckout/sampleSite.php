d1909b81407b7e693cc49bd9f0c79c8b4b085b13
me-suzy <me.suzana@gmail.com>
2025-08-14 00:33:59 +0300

    Add Unsorted/PHPCheckout/phpcheckout/sampleSite.php

---
+++ Unsorted/PHPCheckout/phpcheckout/sampleSite.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+	<HEAD>
+		<?php include_once("configure.php");?>
+      <script language="Javascript" src="phpcheckout.js"></script>
+      <script language="Javascript">loadCSSFile();</script>
+		<TITLE>Sample Site -  <?php echo IMPLEMENTATIONNAME;?> - <?php echo BENEFIT;?> -  <?php echo ORGANIZATION;?> - Powered by phpcheckoutTM from DreamRiver http://wwww.dreamriver.com - Software Powers the Net - a DreamRiver Internet Software Product - php software mysql database - download - web database - dreamriver - freeware - sql - file downloader - internet application development - hot scripts - good value - database software - custom code - php e-mail software - ultimate web apps software - phpYellow Pages - EasySQL - phpTellAFriend - mysql php download - Easily create complete internet programs with php, apache, mysql and windows</TITLE>
+		<meta name="Generator" content="Custom Handmade">
+		<meta name="Author" content="DreamRiver.com, Richard Creech, http://www.dreamriver.com">
+		<meta name="Keywords" content="PHPCHECKOUT, checkout, php, check, out, php checkout, contact, shop, shopping, shopper, sales, catalog, online, retail, retailer, download, downloading, make, money, sell, product, products">
+		<meta name="Description" content="Contact DreamRiver.com about PHPCHECKOUT by visiting this page. Select one of many communication methods including phone, stealth email or snail mail options. Profit from your own digital downloads with PHPCHECKOUT.">
+</HEAD> 
+
+<body>
+<!-- START of body -->
+
+
+<?php include_once("header.php");?>
+
+<blockquote>	
+
+<?php if(FPSTATUS == 'Online' ):?>
+<table width="100%">
+	<!-- start of primary content FOR PAGE -->
+	<tr>
+
+
+
+		<!-- start MAIN COLUMN -->
+		<td valign="top">
+		<!-- PUT CONTENT RIGHT HERE !!! -->
+		
+
+
+
+
+
+<h1>Sample Site</h1>
+
+<p>
+DreamRiver's phpCheckout is a live sample site. 
+
+<br><br>
+We use phpCheckout Pro Edition<sup>TM</sup> for our own digital sales. In fact, we've used phpCheckout<sup>TM</sup> exclusively since July of 2001. We use it everyday and so can you.
+
+<br><br>
+
+<a href="http://www.dreamriver.com/phpcheckout/showcase.php?productnumber=8">Download a copy of the Free Edition</a> and see how easy it can be to put yourself online and start showcasing your own digital items.
+</p>
+
+
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+
+		
+		<!-- END OF CORE CONTENT !!! -->
+		</td>
+	</tr>
+</table>
+<?php else:include('offline.php');endif; // on or offline ?>
+
+<p><br></p>
+
+
+
+</blockquote>	
+
+
+
+
+</body>
+</html>
