e32606e6ff1290b6c8a3abeae2ff225ce6b7dcb9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:45:07 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/login.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/login.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['FORGOT_PASS']   = "Forget your password?";
+$ubbt_lang['FORGOT_ENTER']  = "Enter your email address or username that you reg
+istered with to have a temporary password mailed to you.";
+$ubbt_lang['REMEMBER_ME']   =  "Remember me on each visit.";
+$ubbt_lang['LOGIN_PROMPT']  = 	"Efetuar login";
+$ubbt_lang['LOGIN_PROMPT2']  = 	"Informe nome de usuário e senha.  Caso não tenha registro você pode";
+$ubbt_lang['REG_ONE']  = 	"registrar uma";
+$ubbt_lang['PASSWORD_TEXT']  = 	"Senha";
+?>
