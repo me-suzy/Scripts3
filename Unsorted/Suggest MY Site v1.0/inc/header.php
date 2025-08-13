b532b51c660c51e0d78a17c9c223b5e6a82f5d8a
me-suzy <me.suzana@gmail.com>
2025-08-14 01:54:58 +0300

    Add Unsorted/Suggest MY Site v1.0/inc/header.php

---
+++ Unsorted/Suggest MY Site v1.0/inc/header.php
@@ -0,0 +1,8 @@
+<!doctype html public "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title>SUGGEST MY  SITE v1.0 </title>
+</head>
+<link rel="stylesheet"   type="text/css"  href="inc/css1.css">
+<body>
+
