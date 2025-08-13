8085d742c47bf0259df91c4d050f04bc23013e7a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:10 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/search.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/search.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['SEARCH_INFO']  = 	"Complétez les informations ci-dessous pour effectuer une recherche à travers les forums.";
+$ubbt_lang['SEARCH_FORUM']  = 	"Forum à rechercher";
+$ubbt_lang['ALL_FORUMS']  = 	"Tous les forums";
+$ubbt_lang['SEARCH_WORDS']  = 	"Mots à rechercher";
+$ubbt_lang['SEARCH_OPT']  = 	"Options de recherche";
+$ubbt_lang['SEARCH_ENT']  = 	"Phrase entière";
+$ubbt_lang['SEARCH_AND']  = 	"Et";
+$ubbt_lang['SEARCH_OR']  = 	"Ou";
+$ubbt_lang['SEARCH_USER']  = 	"Par nom d'usager";
+$ubbt_lang['SEARCH_DATE']  = 	"Intervalle de Date";
+$ubbt_lang['SEARCH_1DAY']  = 	"Messages postés depuis 1 jour";
+$ubbt_lang['SEARCH_2DAY']  = 	"Messages postés depuis 2 jours";
+$ubbt_lang['SEARCH_1WEEK']  = 	"Messages postés depuis 1 semaine";
+$ubbt_lang['SEARCH_2WEEK']  = 	"Messages postés depuis 2 semaines";
+$ubbt_lang['SEARCH_3WEEK']  = 	"Messages postés depuis 3 semaines";
+$ubbt_lang['SEARCH_1MONTH']  = 	"Messages postés depuis 1 mois";
+$ubbt_lang['SEARCH_3MONTH']  = 	"Messages postés depuis 3 mois";
+$ubbt_lang['SEARCH_6MONTH']  = 	"Messages postés depuis 6 mois";
+$ubbt_lang['SEARCH_1YEAR']  = 	"Messages postés depuis 1 an";
+$ubbt_lang['SEARCH_ALLPOST']  = 	"Tous les messages";
+$ubbt_lang['SEARCH_NUM']  = 	"Nombre de résultats à afficher par page.";
+?>
