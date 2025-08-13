002711c8009e31aec89a5d19cee1eb93b70ec675
me-suzy <me.suzana@gmail.com>
2025-08-14 01:05:17 +0300

    Add Unsorted/phpYellowPages/phpYellow/installFinish.php

---
+++ Unsorted/phpYellowPages/phpYellow/installFinish.php
@@ -0,0 +1,49 @@
+
+<html>
+<head>
+<?php require_once("util.php");?>
+<title>Install phpYellow Pages - Dreamriver.com - Fine Internet Applications Since 1995</title>
+</head>
+
+<body bgcolor="navy" text="white">
+
+
+<blockquote>
+
+
+<table border=10 width=600 align="center" bgcolor="gray" cellpadding=0 cellspacing=0><tr>
+<td bgcolor="yellow" align="center"><font color="blue">100%</font></td>
+</tr></table>
+
+<h1>Install - Congratulations!</h1>
+
+
+<p>
+You have successfully completed a phpYellow Pages installation.
+</p>
+
+
+
+		
+<div style="color:yellow;font-weight:bold;">
+<ul>
+<li>to proceed click &quot;Go to Index Page&quot; or &quot;Go to Administration&quot;</li>
+</ul>
+</div>
+
+
+
+<form name="installForm">
+<input type="button" name="goIndexButton" value="Go to Index Page" onClick='location.href="index.php"'>
+<input type="button" name="goAdminButton" value="Go to Administration" onClick='location.href="<?php echo ADMINHOME;?>"'>
+</form>
+
+
+</blockquote>
+
+
+</body>
+</html>
+
+
+	
