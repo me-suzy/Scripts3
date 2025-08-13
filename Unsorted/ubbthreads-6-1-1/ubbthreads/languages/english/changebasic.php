94cc1ad6d653ea0bd556b76e37ce027300b6b6e6
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:46 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/changebasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/changebasic.php
@@ -0,0 +1,14 @@
+<?
+$ubbt_lang['PIXEL_TOO_BIG']="Your picture exceeds the ({$theme['PictureWidth']}x{$theme['PictureHeight']}) pixel limitation width a width/height of ";
+$ubbt_lang['FILE_TOO_BIG']="Pictures can be no larger than {$config['avatarsize']} bytes.";
+$ubbt_lang['PASS_MATCH']  = 	"Passwords do not match.";
+$ubbt_lang['BAD_PIC']  = 	"Improper picture type.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Password needs to be between 4 and 20 characters.";
+$ubbt_lang['HOME_TOO_LONG']  = 	"Homepage too long.";
+$ubbt_lang['OCC_TOO_LONG']  = 	"Occupation too long.";
+$ubbt_lang['HOBB_TOO_LONG']  = 	"Hobbies too long.";
+$ubbt_lang['LOC_TOO_LONG']  = 	"Location too long.";
+$ubbt_lang['SIG_TOO_LONG']  = 	"Signature too long.";
+$ubbt_lang['BIO_TOO_LONG']  = 	"Bio too long.";
+$ubbt_lang['BASIC_MOD']  = 	"Basic profile modified.";
+?>
