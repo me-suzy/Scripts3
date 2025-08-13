579307b4d12988fe5afabd401c36d80db0bcad6c
me-suzy <me.suzana@gmail.com>
2025-08-14 02:44:59 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"Perfil do usuário não encontrado";
+$ubbt_lang['EMAIL_HEAD']  = 	"Modificar notificação mail para";
+$ubbt_lang['PROF_EDIT']  = 	"As informações embaixo podem ser alteradas.";
+$ubbt_lang['PROF_SUBS']  = 	"Notificações e assinaturas por mail";
+$ubbt_lang['PROF_NOTIF']  = 	"Deseja notificação por mail ao receber mensagens privadas?";
+$ubbt_lang['PROF_REPLIES']  = 	"Deseja receber as respostas a sua mensagem pelo mail?";
+$ubbt_lang['SUB_STAT']  = 	"Status de assinatura dos fóruns.  (Receber um arquivo diário de todas as mensagens colocadas nesse fórum.)";
+$ubbt_lang['SUB_SHOW']  = 	"(A - Assinar ; C - Cancelar)";
+$ubbt_lang['TEXT_S']  = 	"A";
+$ubbt_lang['TEXT_U']  = 	"C";
+?>
