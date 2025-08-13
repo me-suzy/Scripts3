e04f18488679702df155d2c8f9e83933b01baf08
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:35 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/newreply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/newreply.php
@@ -0,0 +1,19 @@
+<?
+$ubbt_lang['READ_PERM']  = 	"Sie haben nur Lese-Berechtigung für dieses Forum";
+$ubbt_lang['NOT_APP']  = 	"Sie k&ouml;nnen nicht auf eine Nachricht antworten, die noch nicht freigegeben ist.";
+$ubbt_lang['REPLY_HEAD']  = 	"Antwort auf";
+$ubbt_lang['FILL_FORM']  = 	"F&uuml;llen Sie das folgende Formular aus, um Ihre Nachricht zu schicken.";
+$ubbt_lang['NO_HTML']  = 	"HTML ist in diesem Forum nicht erlaubt, deshalb ist der Gebrauch in Nachrichten untersagt.";
+$ubbt_lang['YES_HTML']  = 	"HTML ist in diesem Forum erlaubt, deshalb ist der Gebrauch in Nachrichten erlaubt.";
+$ubbt_lang['NO_MARKUP']  = 	"Markierungen sind in diesem Forum nicht erlaubt, deshalb ist der Gebrauch in Nachrichten untersagt.";
+$ubbt_lang['YES_MARKUP']  = 	"Erlaubt sind";
+$ubbt_lang['MARK_POSTS']  = 	"spezielle Markierungen";
+$ubbt_lang['CHOOSE_NAME']  = 	"Anonyme Nachrichten sind erlaubt, daher k&ouml;nnen Sie jeden Benutzernamen verwenden, der noch nicht registriert ist.";
+$ubbt_lang['POST_ICON']  = 	"Symbol für die Nachricht:";
+$ubbt_lang['POST_TEXT']  = 	"Nachricht:";
+$ubbt_lang['SPELL_CHECK']  = 	"Rechtschreibpr&uuml;fung";
+$ubbt_lang['DO_EMAIL']  = 	"Alle Antworten als Email an meine echte Email-Adresse";
+$ubbt_lang['DO_PREVIEW']  = 	"Voransicht der Nachricht";
+$ubbt_lang['CAN_ATTACH']  = 	"und/oder eine Datei anh&auml;ngen";
+$ubbt_lang['IN_RESPONSE']  = 	"in Bezug auf";
+?>
