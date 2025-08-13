5cb649c85425eae40c220535a7e4c5633aa4fe87
me-suzy <me.suzana@gmail.com>
2025-08-14 00:34:07 +0300

    Add Unsorted/PHPCheckout/phpcheckout/shippingPolicy.php

---
+++ Unsorted/PHPCheckout/phpcheckout/shippingPolicy.php
@@ -0,0 +1,19 @@
+<!-- shippingPolicy.php -->
+		<h2>Digital Item Distribution</h2>
+		<ul>
+		
+		<li style="color:red;font-weight:bold;">All sales are FINAL. No returns.</li>
+		
+		<li>internet orders (example: PayPal ) are distributed within one (1) BUSINESS day</li>
+		
+		<li>we distribute by 4 different internet methods using <b>DreamRiver's</b> <i>phpcheckout</i></a><sup>TM</sup></li>
+		
+		<li>typical distribution method is Retail Product Distribution via HTTP: direct internet download</LI>
+
+		<li>all other (non internet) orders are distributed one (1) BUSINESS day after confirmation of receipt of payment</li> 
+		
+		<li>a valid email address is required for customer registration, receipt and distribution</li>
+		
+		</ul>
+<!-- shippingPolicy.php -->
+
