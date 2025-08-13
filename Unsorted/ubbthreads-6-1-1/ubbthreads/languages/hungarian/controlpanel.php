146c0e1d153a87ba835029bbd21c053c126874a8
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:28 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/controlpanel.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/controlpanel.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['SHOW_ALL']     =  "...view all";
+$ubbt_lang['PERSONAL']     =  "Personal information, email, password, etc.";
+$ubbt_lang['DISPLAY']      =  "Display preferences, number of shown threads, languages, colors...";
+$ubbt_lang['SUB_UN']       =  "Subscribe / Unsubscribe from receiving board posts by email, change message notifications, etc.";
+$ubbt_lang['YOUR_VIEW']    =  "Board users see you as:";
+$ubbt_lang['YOUR_VIEW_WITH']  =  "with";
+$ubbt_lang['VIEW_POSTS']      =  "posts";
+$ubbt_lang['TEXT_TOTAL']      =  "Total";
+$ubbt_lang['TEXT_UNREAD']     =  "Unread";
+$ubbt_lang['EDIT_ICON']       =  "Edit";
+$ubbt_lang['WEL_CONTROL']  =  "My Home for ";
+$ubbt_lang['MAIN_CONFIG']  =  "Main Configuration";
+$ubbt_lang['EDIT_TEXT']    =  "Edit";
+$ubbt_lang['PRIVATE_BOX']    =  "Message Box";
+$ubbt_lang['FRIENDS_ON']     =  "Friends Online";
+$ubbt_lang['FROM_ADDR']      =  "(from address book)";
+$ubbt_lang['FAV_THREADS']    =  "Favorite Threads";
+$ubbt_lang['TEXT_REMOVE']    =  "Remove";
+$ubbt_lang['REM_THREADS']    =  "Post Reminders";
+$ubbt_lang['SEND_A_PM']      =  "Send a PM";
+$ubbt_lang['FAV_FORUMS']     =  "Favorite Forums";
+$ubbt_lang['BY_TEXT']        =  "by";
+?>
