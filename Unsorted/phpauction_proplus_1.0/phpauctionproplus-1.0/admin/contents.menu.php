29553dbd5be02ecbb6177755b8f3268acc6fdc98
me-suzy <me.suzana@gmail.com>
2025-08-14 00:13:31 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/admin/contents.menu.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/admin/contents.menu.php
@@ -0,0 +1,46 @@
+<?#//v.1.0.0
+
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+
+?>
+<TABLE WIDTH=100% CELLPADDING=2 CELLSPACING="0" BORDER="0">
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"><FONT FACE="Tahoma, Verdana" SIZE="2"><IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./listhelp.php" CLASS="links"> 
+			<? print $MSG_916;?>
+			</A></FONT></TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"> <FONT FACE="Tahoma, Verdana" SIZE="2"><IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./news.php" CLASS="links"> 
+			<? print $MSG_516; ?>
+			</A> </FONT> </TD>
+	</TR>
+	<? 
+											if($SETTINGS[newsletter] == 1){
+								?>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"> <FONT FACE="Verdana, Verdana, Arial, Helvetica, sans-serif" SIZE="2"font COLOR="navy"> 
+			</FONT><FONT FACE="Tahoma, Verdana" SIZE="2"><IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"></FONT><FONT FACE="Verdana, Verdana, Arial, Helvetica, sans-serif" SIZE="2"font COLOR="navy"><A HREF="./newsletter.php" CLASS="links"> 
+			<?=$MSG_607?>
+			</A> </FONT> </TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"><FONT FACE="Tahoma, Verdana" SIZE="2"><IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./banners.php" CLASS="links"> 
+			<? print $MSG_599; ?>
+			</A></FONT></TD>
+	</TR>
+	<?
+								}
+								else{
+								"";
+								}
+							 ?>
+</TABLE>
