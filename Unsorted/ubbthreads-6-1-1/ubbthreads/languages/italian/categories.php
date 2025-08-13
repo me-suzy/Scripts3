76aa1b3d25df4452ff945cb141ba51fd4abbf450
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:54 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/categories.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/categories.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['NEW_MEM'] = "Welcome to our newest member,";
+$ubbt_lang['VIEW_PAST']  =     "View recent messages";
+$ubbt_lang['PAST24']  =        "Past 24 hours";
+$ubbt_lang['PAST48']  =        "Past 48 hours";
+$ubbt_lang['PAST7']  =         "Past 7 days";
+$ubbt_lang['THREAD_TEXT']  = 	"Threads";
+$ubbt_lang['CAT_FORUMS']  = 	"Forums";
+$ubbt_lang['REGED_USERS']  = 	"Utenti registrati.";
+$ubbt_lang['WEL_PRIV_NO']  = 	"Non hai fatto il login.";
+$ubbt_lang['CAT_TEXT']  = 	"Category";
+$ubbt_lang['POSTS_TEXT']  = 	"messaggi";
+$ubbt_lang['NEW_TEXT']  = 	"new";
+$ubbt_lang['NOT_APPROVED']  = 	"NA";
+?>
