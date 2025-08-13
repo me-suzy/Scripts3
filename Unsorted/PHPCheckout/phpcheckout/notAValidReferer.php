f868a72513c0896801e819c8667f80f5e1485f63
me-suzy <me.suzana@gmail.com>
2025-08-14 00:33:23 +0300

    Add Unsorted/PHPCheckout/phpcheckout/notAValidReferer.php

---
+++ Unsorted/PHPCheckout/phpcheckout/notAValidReferer.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+	<HEAD>
+		<?php include_once("configure.php");?>
+      <script language="Javascript" src="phpcheckout.js"></script>
+      <script language="Javascript">loadCSSFile();</script>
+		<TITLE>Not A Valid Referrer -  <?php echo IMPLEMENTATIONNAME;?> - <?php echo BENEFIT;?> -  <?php echo ORGANIZATION;?> - Powered by phpcheckoutTM from DreamRiver http://wwww.dreamriver.com - Software Powers the Net - a DreamRiver Internet Software Product - php software mysql database - download - web database - dreamriver - freeware - sql - file downloader - internet application development - hot scripts - good value - database software - custom code - php e-mail software - ultimate web apps software - phpYellow Pages - EasySQL - phpTellAFriend - mysql php download - Easily create complete internet programs with php, apache, mysql and windows</TITLE>
+	</HEAD> 
+
+<body>
+<!-- notAValidReferer.php -->
+<!-- START of body -->
+
+
+<?php include("header.php");?>
+
+
+<h1>Not a Valid Referer</h1>
+<p>Your referer address of <?php echo $visitorReferer;?> does not match a valid referer. 
+<br>Your site administrator may see and modify the REFERER in configure.php.</p>
+
+<!-- end of notAValidReferer.php -->
+
+
+
+
+
