f163117e7cc5431a490dd1a44fcdfbf5ae4466d8
me-suzy <me.suzana@gmail.com>
2025-08-14 01:13:09 +0300

    Add Unsorted/PredictionLeague038/PredictionLeague/DateClass.php

---
+++ Unsorted/PredictionLeague038/PredictionLeague/DateClass.php
@@ -0,0 +1,27 @@
+<?php
+/*********************************************************
+ * Author: John Astill
+ * Date  : 12th December
+ * File  : DateClass.php
+ * Desc  : Class for representing and testing Date formats
+ ********************************************************/
+class DateClass
+{
+  var $mday;  // Day of month
+  var $month; // Month of year
+  var $year;  // Year
+   
+  /*******************************************************
+  *  Add $num articles of $artnr to the cart
+  * @param a first date value
+  * @param b second date value
+  * @return <0 if a<b
+  * @return >0 if a>b
+  * @return =0 if a=b
+  *******************************************************/
+  function compare($a, $b) {
+    //TODO
+    return 0;
+  }
+}
+?>
