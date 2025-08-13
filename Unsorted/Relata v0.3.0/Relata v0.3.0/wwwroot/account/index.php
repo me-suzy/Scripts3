9737881a39f6830a35396a1f09d83cdf2360f54d
me-suzy <me.suzana@gmail.com>
2025-08-14 01:22:02 +0300

    Add Unsorted/Relata v0.3.0/Relata v0.3.0/wwwroot/account/index.php

---
+++ Unsorted/Relata v0.3.0/Relata v0.3.0/wwwroot/account/index.php
@@ -0,0 +1,63 @@
+<?
+//******************************************************************************
+/*
+GPL Copyright Notice
+
+Relata
+Copyright (C) 2001-2002 Stratabase, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+//******************************************************************************
+
+include("../config.inc.php");
+
+//  We use the following features:
+//  sess   for session variables 
+//  auth   for login checks, also required for user variables
+
+
+// perform all startup functions
+page_open(array(
+	"sess" => "relata_session", 
+	"auth" => "relata_challenge_Auth"
+	));
+
+header ("Cache-Control: no-cache, must-revalidate");  // HTTP/1.1
+header ("Pragma: no-cache");                          // HTTP/1.0
+header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
+
+// start a new template
+$template = start_template();
+
+// set template files
+$template->set_file("frameset","winframeset.htm");
+
+// set template variables
+$template->set_var(array(
+	"LEFT"		=>	"account_left.php?session_id=$session_id",
+	"MAIN"		=>	"account_main.php?session_id=$session_id",
+	"LEFT_WIDTH"=>	"190",
+	"WINID"		=>	$winid
+));
+
+// print the template
+$template->parse("output","frameset");
+$template->p("output");
+
+// page close functions
+page_close();
+
+?>
