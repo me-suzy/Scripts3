ca575299537266bed7db6208a6c3b3e2aea1e11a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:45:25 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/search.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/search.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['SEARCH_INFO']  = 	"Entre os dados para busca.";
+$ubbt_lang['SEARCH_FORUM']  = 	"Fórum";
+$ubbt_lang['ALL_FORUMS']  = 	"Todos os fóruns";
+$ubbt_lang['SEARCH_WORDS']  = 	"Palavras a buscar";
+$ubbt_lang['SEARCH_OPT']  = 	"Opções da busca";
+$ubbt_lang['SEARCH_ENT']  = 	"Frase inteira";
+$ubbt_lang['SEARCH_AND']  = 	"E";
+$ubbt_lang['SEARCH_OR']  = 	"OU";
+$ubbt_lang['SEARCH_USER']  = 	"Por nome de usuário";
+$ubbt_lang['SEARCH_DATE']  = 	"Faixa de datas";
+$ubbt_lang['SEARCH_1DAY']  = 	"Menos de 1 dia";
+$ubbt_lang['SEARCH_2DAY']  = 	"Menos de 2 dias";
+$ubbt_lang['SEARCH_1WEEK']  = 	"Menos de 1 semana";
+$ubbt_lang['SEARCH_2WEEK']  = 	"Menos de 2 semanas";
+$ubbt_lang['SEARCH_3WEEK']  = 	"Menos de 3 semanas";
+$ubbt_lang['SEARCH_1MONTH']  = 	"Menos de 1 mês";
+$ubbt_lang['SEARCH_3MONTH']  = 	"Menos de 3 mêses";
+$ubbt_lang['SEARCH_6MONTH']  = 	"menos de 6 mêses";
+$ubbt_lang['SEARCH_1YEAR']  = 	"Menos de 1 ano";
+$ubbt_lang['SEARCH_ALLPOST']  = 	"Todas as mensagens";
+$ubbt_lang['SEARCH_NUM']  = 	"Numero de retornos por página ( Não funciona quando a busca é em todos os fóruns)";
+?>
