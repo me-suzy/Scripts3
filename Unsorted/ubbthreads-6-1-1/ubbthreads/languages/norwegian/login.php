7d00cfb15518db3576d256030755fa620a1efc8e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:42:03 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/login.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/login.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['FORGOT_PASS']   = "Forget your password?";
+$ubbt_lang['FORGOT_ENTER']  = "Enter your email address or username that you reg
+istered with to have a temporary password mailed to you.";
+$ubbt_lang['REMEMBER_ME']   =  "Remember me on each visit.";
+$ubbt_lang['LOGIN_PROMPT']  = 	"Logg inn";
+$ubbt_lang['LOGIN_PROMPT2']  = 	"Tast inn ditt brukernavn og passord. Hvis du ennå ikke har et brukernavn kan du";
+$ubbt_lang['REG_ONE']  = 	"registrere et";
+$ubbt_lang['PASSWORD_TEXT']  = 	"Passord";
+?>
