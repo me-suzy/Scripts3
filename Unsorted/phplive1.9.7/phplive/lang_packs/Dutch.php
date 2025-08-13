d1e3bd29548b7d88660f46f2ea466bbe05cc2b3b
me-suzy <me.suzana@gmail.com>
2025-08-14 00:48:33 +0300

    Add Unsorted/phplive1.9.7/phplive/lang_packs/Dutch.php

---
+++ Unsorted/phplive1.9.7/phplive/lang_packs/Dutch.php
@@ -0,0 +1,84 @@
+<?
+$LANG = ARRAY() ;		// MUST initialize, DO NOT CHANGE
+/*******************************************************************
+* Language: Dutch
+* Contributed by:
+*	Name: Marco de Moulin <marco@point.nl>
+*	Company: Point.NL
+*	URL: http://www.point.nl
+*
+* ------------------------------------------------------------------
+* DO NOT REMOVE COMMENTS.  JUST MODIFY THE ACTUAL TEXT.
+* DO NOT REMOVE THE orig TEXT!  THEY ARE THERE FOR REFERENCE.
+* ------------------------------------------------------------------
+* NOTE: If you are going to submit a language pack, please keep
+* everything basic and do not mention any company name, personal name,
+* copyrighted name, or such.  Keep all translations generic, simple
+* and catered to all audiences and companies.  Basically, your
+* translated language pack should be ready to use without being touched.
+*******************************************************************/
+
+
+
+/***************** ONLY MAKE THE CHANGE BELOW ******************/
+
+
+/*** branded string located troughout 99% of the pages */
+//orig: PHP <i>Live!</i> &copy; 2001 OSI Codes, New York, New York
+$LANG['DEFAULT_BRANDING'] = "Powered by <a href='http://www.PHPLiveSupport.com' target='new'>PHP <i>Live!</i></a>" ;
+
+/*** chat page titles */
+//orig: Request Live! Support Request
+$LANG['TITLE_SUPPORTREQUEST'] = "Neem Live! Contact Op" ;
+//orig: You are now speaking with a Live! Support
+$LANG['TITLE_SUPPORCHAT'] = "U spreekt nu met Live! Ondersteuning" ;
+//orig: Please leave a message
+$LANG['TITLE_LEAVEMESSAGE'] = "Laat een bericht achter" ;
+//orig: Printer Friendly View
+$LANG['TITLE_PRINTVIEW'] = "Printbaar overzicht" ;
+
+$LANG['WORD_BACK'] = "Terug" ;
+$LANG['WORD_CLOSE'] = "Afsluiten" ;
+$LANG['WORD_NAME'] = "Naam" ;
+$LANG['WORD_EMAIL'] = "E-mail" ;
+$LANG['WORD_SUBJECT'] = "Onderwerp" ;
+$LANG['WORD_MESSAGE'] = "Bericht" ;
+$LANG['WORD_SEND'] = "Verstuur" ;
+$LANG['WORD_COMPANY'] = "Organisatie" ;
+$LANG['WORD_DEPARTMENT'] = "Afdeling" ;
+$LANG['WORD_DAY'] = "Dag" ;
+
+//orig: Welcome to our Live! support help desk.
+$LANG['CHAT_REQUEST_TITLE'] = "Welkom bij de Live! help desk." ;
+//orig: Please select the department you would like to reach.
+$LANG['CHAT_REQUEST_SELECTDEPT'] = "Selecteer de afdeling die u wilt bereiken." ;
+//orig: Input your Screen Name used during this session:
+$LANG['CHAT_REQUEST_INPUTSCREENNAME'] = "Geef uw naam om voor dit gesprek:" ;
+//orig: Welkocom %%user%%! Please hold while we contact a representative. If a representative does not respond in few seconds, then he/she is not available at this time.
+$LANG['CHAT_GREETING'] = "Welkom %%user%%! Een ogenblik geduld. Wij verbinden u door met één van onze vertegenwoordigers. Als u geen reactie krijgt binnen enkele seconden dan is hij of zij op dit moment niet beschikbaar." ;
+//orig: Printer Friendly
+$LANG['CHAT_PRINTER_FRIENDLY'] = "Printer Vriendelijk" ;
+//orig: Your party has left this session.
+$LANG['CHAT_PARTYLEFTSESSION'] = "De andere partij heeft deze sessie verlaten." ;
+//orig: You are now speaking with
+$LANG['CHAT_REQUEST_NOWSPEAKINGWITH'] = "U spreekt nu met" ;
+//orig: Department is currently unavailable. Please leave a message.
+$LANG['MESSAGE_BOX_MESSAGE'] = "Deze afdeling is momenteel niet bereikbaar. U kunt een bericht achterlaten." ;
+//orig: Thank you.  Your message has been sent to:
+$LANG['MESSAGE_BOX_SENT'] = "Dank u.  Uw bericht is gestuurd aan:" ;
+//orig: Items with (*) must be filled.
+$LANG['MESSAGE_BOX_JS_A_ALLFIELDSSUP'] = "Onderdelen met (*) zijn verplicht om in te vullen." ;
+//orig: Email is invalid format (example: someone@somewhere.com)
+$LANG['MESSAGE_BOX_JS_A_INVALIDEMAIL'] = "Email is invalid format (example: someone@somewhere.com)" ;
+//orig: Department not available.  Please leave a message.
+$LANG['CHAT_DEPT_NOTAVAIL'] = "Afdeling is niet beschikbaar. U kunt een bericht achterlaten." ;
+//orig: Session did not create.  Request did not send.
+$LANG['CHAT_NOTCREATE'] = "De sessie is niet gemaakt.  Uw aanvraag is niet verzonden." ;
+
+//orig: Please hold while being transferred to
+$LANG['CHAT_CALL_TRANSFER'] = "" ;
+//orig: What is your question?
+$LANG['CHAT_REQUEST_QUESTION'] = "" ;
+//orig: A copy of the chat transcript has been sent to:
+$LANG['CHAT_TRANSCRIPT_SENT'] = "A copy of the chat transcript has been sent to:" ;
+?>
