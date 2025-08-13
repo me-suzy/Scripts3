ab426d34e708464bdc407d2867336339b5e0ec82
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:10 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/mess_handler.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/mess_handler.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['REPLY_HEAD']   =   "Använd detta formulär för att skicka ett privat meddelande till denna användare.";
+$ubbt_lang['MESS_COPY']  =     "Jag vill behålla en kopia av meddelandet";
+$ubbt_lang['DELETE_MESS']  = 	"Radera detta meddelande";
+$ubbt_lang['REPLY_MESS']  = 	"Svara på meddelandet";
+$ubbt_lang['RETURN_START']  = 	"Återgå till din startsida";
+$ubbt_lang['MESS_RECEIPT']  = 	"Jag vill mottaga ett läskvitto";
+$ubbt_lang['MESS_PREVIEW']  = 	"Jag vill förhandsgranska mitt meddelande";
+$ubbt_lang['ORIG_MESS']  = 	"Originalmeddelande";
+?>
