b99dbfee5a0d0341ba2837ccf4eb1f317dbf1328
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:44 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/dopoll.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/dopoll.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NO_VOTE']  = 	"±z¨S¿ï¾Ü¤@­Ó¿ï¶µ";
+$ubbt_lang['POLL_REGED']  = 	"¥u¦³µù¥U¨Ï¥ÎªÌ¤~¯à§ë²¼¡C";
+$ubbt_lang['POLL_IP'] =	"®Ú¾Ú°O¿ý¡A¤w¸g¦³¤H±q±z¥Ø«e¤W½uªº IP ¦ì§}§ë¹L²¼¤F¡C";
+$ubbt_lang['POLL_USER']  = 	"±z¤w¸g§ë¹L²¼¤F¡C";
+?>
