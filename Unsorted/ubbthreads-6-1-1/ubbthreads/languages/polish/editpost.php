e54b65456dfbf04549e418a57c5ef937f5089b04
me-suzy <me.suzana@gmail.com>
2025-08-14 02:43:29 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "Move this post & any replies";
+$ubbt_lang['DELETE_THREAD']  =  "Delete this thread";
+$ubbt_lang['NO_EDIT']  = 	"Nie mo¿na sprawdziæ Twojego prawa do zmiany wiadomo¶ci.";
+$ubbt_lang['EDITTIME']  = 	"Tej wiadomo¶ci nie mo¿na ju¿ edytowaæ - zosta³a wys³ana zbyt dawno";
+$ubbt_lang['PEDIT_HEAD']  = 	"Edytuj wiadomo¶æ";
+$ubbt_lang['PEDIT_BODY']  = 	"Mo¿esz zmieniæ wiadomo¶æ lub j± skasowaæ. Je¶li skasujesz wiadomo¶æ, która mia³a odpowiedzi to tytu³ tej wiadomo¶ci zostanie zmieniony na \"Wiadomo¶æ skasowana\".  Dziêki temu wygl±d w±tku zostanie niezmieniony.";
+$ubbt_lang['EDIT_POLL']  = 	"Tej wiadomo¶ci nie mo¿na edytowaæ, poniewa¿ zawiera ankietê.";
+$ubbt_lang['POST_ICON']  = 	"Ikonka do wiadomo¶ci";
+$ubbt_lang['MARK_EDIT']  = 	"Zaznaczyæ jako edytowan±?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Zmieñ tê wiadomo¶æ";
+$ubbt_lang['PEDIT_DELETE']  = 	"Skasuj tê wiadomo¶æ";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Zaakceptuj tê wiadomo¶æ";
+$ubbt_lang['RET_NORM']  = 	"Przywróæ normaln± datê";
+$ubbt_lang['MAKE_STICKY']  = 	"Uczyñ wiadomo¶æ zawsze pierwsz±";
+$ubbt_lang['MANAGE_THREAD']  = 	"Edytuj w±tek";
+$ubbt_lang['OPEN_THREAD']  = 	"Otwórz ten w±tek";
+$ubbt_lang['CLOSE_THREAD']  = 	"Zamknij ten w±tek";
+$ubbt_lang['UNKEEP_THREAD']  = 	"Nie zachowuj tego w±tku";
+$ubbt_lang['KEEP_THREAD']  = 	"Zachowaj ten w±tek";
+$ubbt_lang['MOVE_THREAD']  = 	"Przenie¶ ten w±tek";
+?>
