8e7d98a7ad44810249ea9e34775010ce09cbe87e
me-suzy <me.suzana@gmail.com>
2025-08-14 01:02:41 +0300

    Add Unsorted/phpx/includes/modulesInsert.inc.php

---
+++ Unsorted/phpx/includes/modulesInsert.inc.php
@@ -0,0 +1,9 @@
+<?php
+#$Id: modulesInsert.inc.php,v 1.2 2003/09/15 15:01:29 ryan Exp $
+/*if (substr_count($this->body, ":ENGINES:") > 0){
+    require_once("includes/engines.inc.php");
+    $engine = new engine($this->textArray, $this->templates);
+    $result = $engine->body;
+    $this->body = str_replace(":ENGINES:", $result, $this->body);
+}*/
+?>
