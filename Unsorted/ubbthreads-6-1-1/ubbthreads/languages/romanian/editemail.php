29b866263f3379670312c2978dab800e8078c9d5
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:31 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"Nu am gasit profilul utilizatorului";
+$ubbt_lang['EMAIL_HEAD']  = 	"Editeaza optiunile legate de notificarile email pentru";
+$ubbt_lang['PROF_EDIT']  = 	"Puteti edita oricare din informatiile care urmeaza.";
+$ubbt_lang['PROF_SUBS']  = 	"Notificari Email / Subscrieri";
+$ubbt_lang['PROF_NOTIF']  = 	"Doriti instiintari prin email atunci cind primiti mesaje private?";
+$ubbt_lang['PROF_REPLIES']  = 	"Doriti sa primiti prin email raspunsurile la post-urile dvs?";
+$ubbt_lang['SUB_STAT']  = 	"Starea subscrierii pentru urmatoarele forumuri (primeste zilnic o arhiva cu toate post-urile de pe acest forum prin email, trimis la adresa de email reala).";
+$ubbt_lang['SUB_SHOW']  = 	"(A - Aboneaza; R - Renunta)";
+$ubbt_lang['TEXT_S']  = 	"A";
+$ubbt_lang['TEXT_U']  = 	"R";
+?>
