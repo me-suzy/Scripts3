51ed942fddf4d126c956a263bd5d758b3ca0d9c6
me-suzy <me.suzana@gmail.com>
2025-08-14 01:11:42 +0300

    Add Unsorted/powerdownload303/pdl303/pdl-inc/pdl_flop.inc.php

---
+++ Unsorted/powerdownload303/pdl303/pdl-inc/pdl_flop.inc.php
@@ -0,0 +1,48 @@
+<?
+$release_res = $db_handler->sql_query("SELECT SUM(downloads) AS downloads, SUM(size) AS size, release.* FROM $sql_table[files] AS files, $sql_table[release] AS release WHERE release.release_id=files.release_id AND release.released='Y' GROUP BY release_id ORDER BY downloads ASC LIMIT 0,$settings[top_count]");
+$count = 0;
+$total = $db_handler->sql_num_rows($release_res);
+
+$usetext = false;
+if(preg_match("/\{text\}/",$template[flop_row])) $usetext = true;
+
+$release_rows = "";
+while($release_row = $db_handler->sql_fetch_array($release_res))
+ {
+  $count++;
+  $release_row[count] = $count;
+  $release_row[id] = $release_row[release_id];
+  $release_row[name] = stripslashes($release_row[name]);
+  if($settings['shortname'] > 0)
+   {
+    if(strlen($release_row['name']) > $settings['shortname'])
+     {
+      $release_row['name'] = substr($release_row['name'],0,$settings['shortname']-3)."...";
+     }
+   }
+
+  if($usetext == true)
+   {
+    $release_row[text] = stripslashes($release_row[text]);
+    if(!$release_row[text])
+     { $release_row[text] = "N/A"; }
+    elseif($settings[trenn_durch] == "zeichen")
+     {
+      $release_row[text] = str_replace($settings[trenn_string],"",$release_row[text]);
+      if(strlen($release_row[text]) <= $settings[trenn_zeichen])
+       { $release_row[text] = $release_row[text]; }
+      else
+       { $release_row[text] = substr($release_row[text],0,$settings[trenn_zeichen])."..."; }
+     }
+    elseif($settings[trenn_durch] == "string")
+     {
+      $text = explode($settings[trenn_string], $release_row[text]);
+      $release_row[text] = $text[0];
+     }
+    if($release_row[text] != "N/A") $release_row[text] = bbcode($release_row[text],$settings[badwords_releases],$settings[smilies],$settings[glossary],$settings[bb_code],$settings[html_releases]);
+   }
+  $release_rows.= replace($template[flop_row], $release_row);
+ }
+
+echo replace($template[flop_box], $release_rows);
+?>
