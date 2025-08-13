4d46c0ff7810fe1cc6b0ab55d3ddaceecc615afc
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:11 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/controlpanel.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/controlpanel.php
@@ -0,0 +1,27 @@
+<?
+$ubbt_lang['SHOW_ALL']     = 	"...toate";
+$ubbt_lang['PERSONAL']     = 	"Informatii personale, adresa de email, parola, etc.";
+$ubbt_lang['DISPLAY']      = 	"Optiuni legate de afisare, numar mesaje, culori...";
+$ubbt_lang['SUB_UN']       = 	"Aboneaza / dezaboneaza la diferitele notificari existente.";
+$ubbt_lang['YOUR_VIEW']    = 	"Pentru ceilalti utilizatori apareti ca:";
+$ubbt_lang['YOUR_VIEW_WITH']  = 	"avind";
+$ubbt_lang['VIEW_POSTS']  = 	"mesaje";
+$ubbt_lang['TEXT_TOTAL']  = 	"Total";
+$ubbt_lang['TEXT_UNREAD']  = 	"Necitite";
+$ubbt_lang['EDIT_ICON']  = 	"Editeaza";
+$ubbt_lang['WEL_CONTROL']  = 	"My Home pentru ";
+$ubbt_lang['MAIN_CONFIG']  = 	"Main Configuration";
+$ubbt_lang['EDIT_TEXT']  = 	"Editeaza";
+$ubbt_lang['BASIC_TEXT']  = 	"Basic";
+$ubbt_lang['DISPLAY_TEXT']  = 	"Display";
+$ubbt_lang['SUBS_TEXT']  = 	"Subscriptions";
+$ubbt_lang['PRIVATE_BOX']  = 	"Mesaje private";
+$ubbt_lang['FRIENDS_ON']  = 	"Prieteni online";
+$ubbt_lang['FROM_ADDR']  = 	"(din agenda)";
+$ubbt_lang['FAV_THREADS']  = 	"Subiecte favorite";
+$ubbt_lang['TEXT_REMOVE']  = 	"Sterge";
+$ubbt_lang['REM_THREADS']  = 	"Mesaje de revazut";
+$ubbt_lang['SEND_A_PM']  = 	"Trimite un mesaj";
+$ubbt_lang['FAV_FORUMS']  = 	"Forumuri favorite";
+$ubbt_lang['BY_TEXT']  = 	"scris de";
+?>
