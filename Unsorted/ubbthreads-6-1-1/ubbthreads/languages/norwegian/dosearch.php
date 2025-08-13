0753d337049c1a085377783c903e1f797dc21bbd
me-suzy <me.suzana@gmail.com>
2025-08-14 02:41:49 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/dosearch.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['NO_MORE']   =  "There are no more search results in this direction.";
+$ubbt_lang['NO_WORDS']  = 	"Du må skrive ett eller flere søkeord for å søke.";
+$ubbt_lang['SEARCH_RES']  = 	"Resultat av søket";
+$ubbt_lang['PREV_PAGE']  = 	"Forrige side";
+$ubbt_lang['MAIN_SEARCH']  = 	"Søkesiden";
+$ubbt_lang['NEXT_PAGE']  = 	"Neste side";
+$ubbt_lang['NO_MATCH']  = 	"Søket gav ingen treff.";
+?>
