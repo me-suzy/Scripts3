2d8c378f15fb6a10bab36a2bc49b5b9cfbbf17a2
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:17 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/newreply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/newreply.php
@@ -0,0 +1,19 @@
+<?
+$ubbt_lang['READ_PERM']  = 	"Du har endast läsrättigheter i detta forum.";
+$ubbt_lang['NOT_APP']  = 	"Du kan inte svara på ett inlägg som inte har godkänts.";
+$ubbt_lang['REPLY_HEAD']  = 	"Svara på";
+$ubbt_lang['FILL_FORM']  = 	"Fyll i fältet nedan för att skriva ett inlägg på forumet.";
+$ubbt_lang['NO_HTML']  = 	"Html är avstängs.";
+$ubbt_lang['YES_HTML']  = 	"Html är aktiverat.";
+$ubbt_lang['NO_MARKUP']  = 	"UBBCode är avstängt.";
+$ubbt_lang['YES_MARKUP']  = 	"UBBCode är aktiverat, så du kan använda";
+$ubbt_lang['MARK_POSTS']  = 	"UBBCode i ditt inlägg";
+$ubbt_lang['CHOOSE_NAME']  = 	"Anonyma inlägg är tillåtna, så du kan välja valfritt oregistrerat namn till ditt inlägg.";
+$ubbt_lang['POST_ICON']  = 	"Inläggsikon";
+$ubbt_lang['POST_TEXT']  = 	"Skriv inlägg";
+$ubbt_lang['SPELL_CHECK']  = 	"Kontrollera stavning";
+$ubbt_lang['DO_EMAIL']  = 	"Skicka alla svar till min riktiga e-postadress";
+$ubbt_lang['DO_PREVIEW']  = 	"Jag vill förhandsgranska mitt inlägg";
+$ubbt_lang['CAN_ATTACH']  = 	"och/eller bifoga en fil";
+$ubbt_lang['IN_RESPONSE']  = 	"I svar till";
+?>
