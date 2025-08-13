d47ad0f771e91c04d7f67b0f4d392e196cad2e53
me-suzy <me.suzana@gmail.com>
2025-08-14 02:45:44 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/togglesub.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/togglesub.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['UN_HEADER']  = 	"You have been unsubscribed";
+$ubbt_lang['HEADER']  = 	"You have been subscribed";
+$ubbt_lang['UN_BODY']  = 	"You have been unsubscribed from the forum: ";
+$ubbt_lang['BODY']  =          "You have been subscribed to the forum: ";
+?>
