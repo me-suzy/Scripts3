b937d62d1723feb349ffc594449bafe5f5c577c5
me-suzy <me.suzana@gmail.com>
2025-08-14 02:43:21 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/dosearch.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['NO_MORE']   =  "There are no more search results in this direction.";
+$ubbt_lang['NO_WORDS']  = 	"Musisz wpisaæ jakie¶ s³owo do znalezienia.";
+$ubbt_lang['SEARCH_RES']  = 	"Wynik zapytania o";
+$ubbt_lang['PREV_PAGE']  = 	"Poprzednia strona";
+$ubbt_lang['MAIN_SEARCH']  = 	"G³ówna strona wyszukiwania";
+$ubbt_lang['NEXT_PAGE']  = 	"Nastêpna strona";
+$ubbt_lang['NO_MATCH']  = 	"Nie znaleziono ¿adnych wiadomo¶ci spe³niaj±cych podane kryterium.";
+?>
