b074f59a9448a59a2f52566171ea5220c2e27750
me-suzy <me.suzana@gmail.com>
2025-08-14 01:04:43 +0300

    Add Unsorted/phpYellowPages/phpYellow/clickByCategoryForm.php

---
+++ Unsorted/phpYellowPages/phpYellow/clickByCategoryForm.php
@@ -0,0 +1,63 @@
+<?php echo"\n\n\n";?>
+<!-- start of clickByCategoryForm.php -->
+<h2>Click by Category</h2>
+
+<p>Click on a category to find a product or service.</p>
+
+<table>
+<tr>
+<!-- first column -->
+
+<td valign="top">
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Aerospace&recordDisplay=complete">Aerospace</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Association&recordDisplay=complete">Association</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Automotive&recordDisplay=complete">Automotive</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Banking&recordDisplay=complete">Banking</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Chemicals&recordDisplay=complete">Chemicals</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Computer+Hardware&recordDisplay=complete">Computer Hardware</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Computer+Software&recordDisplay=complete">Computer Software</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Conglomerates&recordDisplay=complete">Conglomerates</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Consulting&recordDisplay=complete">Consulting</a>
+</td>
+<!-- second column -->
+<td valign="top">
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Defense&recordDisplay=complete">Defense</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Diversified+Services&recordDisplay=complete">Diversified Services</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Drugs&recordDisplay=complete">Drugs</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Education&recordDisplay=complete">Education</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Electronics&recordDisplay=complete">Electronics</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Energy&recordDisplay=complete">Energy</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Financial+Services&recordDisplay=complete">Financial Services</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Food-Beverage-Tobacco&recordDisplay=complete">Food-Beverage-Tobacco</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Government&recordDisplay=complete">Government</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Health+Products&recordDisplay=complete">Health Products</a>
+</td>
+<!-- third column -->
+<td valign="top">
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Health+Services&recordDisplay=complete">Health Services</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Import-Export&recordDisplay=complete">Import-Export</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Insurance&recordDisplay=complete">Insurance</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Internet+Products&recordDisplay=complete">Internet-Products</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Internet+Services&recordDisplay=complete">Internet-Services</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=International+Trade&recordDisplay=complete">International Trade</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Law&recordDisplay=complete">Law</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Leisure&recordDisplay=complete">Leisure</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Manufacturing&recordDisplay=complete">Manufacturing</a>
+</td>
+<!-- fourth column -->
+<td valign="top">
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Marketing&recordDisplay=complete">Marketing</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Materials-Construction&recordDisplay=complete">Materials-Construction</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Media&recordDisplay=complete">Media</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Metals-Mining&recordDisplay=complete">Metals-Mining</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Real+Estate&recordDisplay=complete">Real Estate</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Retail&recordDisplay=complete">Retail</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Specialty+Retail&recordDisplay=complete">Specialty Retail</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Telecommunications&recordDisplay=complete">Telecommunications</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Transportation&recordDisplay=complete">Transportation</a><br>
+<a href="<?php echo INSTALLPATH;?>yellowresult.php?goal=<?php if (file_exists("premiumSearchForm.php")){echo("Advanced+");}?>Search&category=Utilities&recordDisplay=complete">Utilities</a><br>
+</td>
+</tr>
+</table>
+<!-- end of clickByCategoryForm.php -->
+<?php echo"\n\n\n";?>
