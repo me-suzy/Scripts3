fbdfe1d159f327e82cbc42d686933bd3dd47c72b
me-suzy <me.suzana@gmail.com>
2025-08-14 00:32:49 +0300

    Add Unsorted/PHPCheckout/phpcheckout/loginRetrieveItemForm.php

---
+++ Unsorted/PHPCheckout/phpcheckout/loginRetrieveItemForm.php
@@ -0,0 +1,6 @@
+<form name="loginRetrieveItemForm" method="post" action="proDownloadRetail.php">
+	<input type="submit" name="submit" class="submit" value="Retrieve Newest Version">
+	<input type="hidden" name="customerid" value="<?php echo $customerid;?>">
+	<input type="hidden" name="productnumber" value="<?php echo $productnumber;?>">
+	<input type="hidden" name="via" value="<?php echo $via;?>">
+</form>
