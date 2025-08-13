04c65302496359ba593fcc3aa8d162c47769fbb2
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:57 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"Il valore dei messaggi per pagina non può essere utilizzato.";
+$ubbt_lang['FLAT_BAD']  = 	"Flat Posts per page must be between 1 and 99.";
+$ubbt_lang['TEXTAREA_NO']  = 	"Il valore area di testo non può essere utilizzato.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Le tue preferenze del display sono state modificate.";
+?>
