743239ebadb4d2913633e498c1bf7da7782ec0c4
me-suzy <me.suzana@gmail.com>
2025-08-14 01:16:24 +0300

    Add Unsorted/propublish1_1/propublish/search_include.php

---
+++ Unsorted/propublish1_1/propublish/search_include.php
@@ -0,0 +1,7 @@
+	<form method="post" action="search.php">
+	<input type="hidden" name="side" value="let">
+	<p align="left">
+	<input type="text" name="find_str" size="14" style="font-family: verdena 10px; font-size: 10px;"><br>
+	<input type="submit" style="font-family: verdena: 10px; font-size: 10px; "  value="Search now!" name="submit"><br>
+	
+	</form>
