0dc18d305021319e28ad7d2a22d7638e3665d696
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:53 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/mess_handler.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/mess_handler.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['REPLY_HEAD']   =   "Gebruik dit forumulier om dit lid een privébericht te sturen";
+$ubbt_lang['MESS_COPY']  =     "Ik wil een kopie van dit bericht bewaren";
+$ubbt_lang['DELETE_MESS']  = 	"Wissen";
+$ubbt_lang['REPLY_MESS']  = 	"Beantwoorden";
+$ubbt_lang['MESS_RECEIPT']  = 	"Ik wil een ontvangstbevestiging ontvangen";
+$ubbt_lang['MESS_PREVIEW']  = 	"Ik wil het bericht eerst bekijken";
+$ubbt_lang['ORIG_MESS']  = 	"Origineel bericht";
+?>
