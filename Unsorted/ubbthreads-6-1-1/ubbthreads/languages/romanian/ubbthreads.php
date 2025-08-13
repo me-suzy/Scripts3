3d4a8ae2e38f3616d71ddd66d2d3734f4da50ea4
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:17 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/ubbthreads.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/ubbthreads.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['NEW_MEM'] = "Welcome to our newest member,";
+$ubbt_lang['VIEW_PAST']  = 	"Mesajele recente";
+$ubbt_lang['PAST24']  = 		"Ultimele 24 ore";
+$ubbt_lang['PAST48']  = 		"Ultimele 48 ore";
+$ubbt_lang['PAST7']  = 		"Ultimele 7 zile";
+$ubbt_lang['THREAD_TEXT']  = 	"Subiecte";
+$ubbt_lang['OFFSET_TIME']  = 	"Data ajustata este: ";
+$ubbt_lang['REGED_USERS']  = 	"Utilizatori inregistrati";
+$ubbt_lang['WEL_PRIV_NO']  = 	"Nu sinteti conectat.";
+$ubbt_lang['POSTS_TEXT']  = 	"Mesaje";
+$ubbt_lang['LAST_POST']  = 	"Ultimul mesaj";
+$ubbt_lang['NEW_BOARD']  = 	"Board nou";
+$ubbt_lang['NEW_TEXT']  = 	"noi";
+$ubbt_lang['NOT_APPROVED']  = 	"NA";
+?>
