89aacc72f6314f5f2d75d21cb6550740e692d891
me-suzy <me.suzana@gmail.com>
2025-08-14 02:34:52 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/showprofile.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/showprofile.php
@@ -0,0 +1,27 @@
+<?
+$ubbt_lang['RATING']    =       "Rating";
+$ubbt_lang['T_RATES']   =       "Total Rates";
+$ubbt_lang['RATE_USER'] =       "Rate this user";
+$ubbt_lang['STAR1'] =           "1 star";
+$ubbt_lang['STAR2'] =           "2 stars";
+$ubbt_lang['STAR3'] =           "3 stars";
+$ubbt_lang['STAR4'] =           "4 stars";
+$ubbt_lang['STAR5'] =           "5 stars";
+$ubbt_lang['DORATE']    =       "Submit rating";
+$ubbt_lang['YOURATED']  =       "You rated this user a ";
+$ubbt_lang['PROF_FOR']  = 	"Profile for";
+$ubbt_lang['TEXT_NAME']  = 	"Name";
+$ubbt_lang['USER_TITLE']  = 	"Title";
+$ubbt_lang['TOTAL_POSTS']  = 	"Total Posts";
+$ubbt_lang['TEXT_HOME']  = 	"Homepage";
+$ubbt_lang['TEXT_OCCU']  = 	"Occupation";
+$ubbt_lang['TEXT_HOBBY']  = 	"Hobbies";
+$ubbt_lang['TEXT_LOC']  = 	"Location";
+$ubbt_lang['TEXT_BIO']  = 	"Bio";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ Number";
+$ubbt_lang['REGGED_ON']  = 	"Registered on";
+$ubbt_lang['EDIT_T_U']  = 	"Edit this user";
+$ubbt_lang['ADD_BOOK']  = 	"Add to address book";
+$ubbt_lang['SHOW_ALL_P']  = 	"Show all user's posts";
+$ubbt_lang['NO_LONGER']  = 	"This user is no longer in our database.";
+?>
