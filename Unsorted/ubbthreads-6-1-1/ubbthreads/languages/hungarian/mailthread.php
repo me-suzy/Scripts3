d29449d50f82a6977a5b08dd3abd3fd26e2ded86
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:01 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/mailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/mailthread.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['MAIL_FRIEND']  = 	"Email this thread to a friend";
+$ubbt_lang['FRIEND_HEAD']  = 	"Fill out the email address of the person you wish to send this to.";
+$ubbt_lang['SENT_FROM1']  = 	"The email will be sent from your registered email address";
+$ubbt_lang['SENT_FROM2']  = 	"using your Username";
+$ubbt_lang['USE_REAL']  = 	"If you want to use your real name instead, enter it here:";
+$ubbt_lang['WHAT_EMAIL']  = 	"Email address to send to:";
+$ubbt_lang['SEND_IT']  = 	"Send it!";
+?>
