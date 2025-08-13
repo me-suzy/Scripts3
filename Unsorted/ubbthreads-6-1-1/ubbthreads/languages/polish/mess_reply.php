31ab31d7a7682cbdb32637ec92129b3b1737a577
me-suzy <me.suzana@gmail.com>
2025-08-14 02:43:41 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/mess_reply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/mess_reply.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['EDIT_MORE']  = 	"Popraw i wy¶lij";
+$ubbt_lang['REPLY_PREV']  = 	"Podgl±d odpowiedzi dla u¿ytkownika ";
+$ubbt_lang['PREVIEW_MESS']  = 	"Poni¿ej widaæ podgl±d Twojej odpowiedzi. Wci¶nij 'Dalej', ¿eby j± wys³aæ lub popraw poni¿ej.";
+$ubbt_lang['MESS_PREVIEW']  = 	"Podgl±d wiadomo¶ci przed wys³aniem";
+$ubbt_lang['PRIV_SUB']  = 	"Masz nowe wiadomo¶ci";
+$ubbt_lang['PRIV_BODY_R']  = 	"Otrzymale¶ prywatn± wiadomo¶æ na forum. Adres forum to {$config['phpurl']}/login.php";
+?>
