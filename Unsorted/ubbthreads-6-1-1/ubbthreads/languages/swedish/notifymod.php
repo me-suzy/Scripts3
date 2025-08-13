7e4766c19269bcb78b9a976a372de4207aa95aca
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:21 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/notifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/notifymod.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NOTIFY_MOD']  =  	"Meddela moderator";
+$ubbt_lang['NOTIFY_HEAD']  = 	"Med denna funktion kan du meddela moderatorerna för detta forum att de ska kontrollera detta inlägg.";
+$ubbt_lang['SEND_IT']  =  "Ja, meddela moderatorerna om detta inlägg!";
+$ubbt_lang['NO_POST']  = 	"Ett problem uppstod när detta inlägg skulle hämtas från databasen.";
+?>
