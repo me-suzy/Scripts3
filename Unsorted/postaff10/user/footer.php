e4bf3c65a0b1d22f1654c3a0583049b01ba1aee3
me-suzy <me.suzana@gmail.com>
2025-08-14 01:08:01 +0300

    Add Unsorted/postaff10/user/footer.php

---
+++ Unsorted/postaff10/user/footer.php
@@ -0,0 +1,15 @@
+      </td>
+    </tr>
+    </table>
+    </td>
+  </tr>
+  <tr>
+  <td align=center bgcolor="#006699" height=20>
+  <b><font color=white>
+  based on <a href=http://www.webradev.com/postaffiliate/><font color=white>POST Affiliate - www.webradev.com/postaffiliate/</font></a>
+  </font></b>
+  </td>
+  </tr>
+  </table>
+</body>
+</html>
