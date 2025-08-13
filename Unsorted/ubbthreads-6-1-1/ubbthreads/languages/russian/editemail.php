9b2538f198fc1ef1e070043cdeeab29174ed905b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:59 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"Íå óäà¸òñÿ íàéòè ïðîôèëü äëÿ èìåíè ïîëüçîâàòåëÿ";
+$ubbt_lang['EMAIL_HEAD']  = 	"Èçìåíåíèå ïàðàìåòðîâ óâåäîìëåíèÿ ïî e-mail äëÿ";
+$ubbt_lang['PROF_EDIT']  = 	"Âû ìîæåòå èçìåíèòü ëþáóþ ïðèâåä¸ííóþ èíôîðìàöèþ.";
+$ubbt_lang['PROF_SUBS']  = 	"Óâåäîìëåíèÿ/Ïîäïèñêà ïî e-mail";
+$ubbt_lang['PROF_NOTIF']  = 	"Õîòèòå ëè Âû ïîëó÷àòü óâåäîìëåíèÿ ïî e-mail ïðè ïîÿâëåíèè ëè÷íûõ ñîîáùåíèé íà Âàøå èìÿ?";
+$ubbt_lang['PROF_REPLIES']  = 	"Õîòèòå ëè Âû ïîëó÷àòü âñå îòâåòû íà ÂÀØÈ ñîîáùåíèÿ ïî e-mail?";
+$ubbt_lang['SUB_STAT']  = 	"Ñòàòóñ ïîäïèñêè íà ïåðå÷èñëåííûå Äîñêè. (Ïîëó÷åíèå åæåäíåâíîãî àðõèâà ÂÑÅÕ ñîîáùåíèé çàíåñ¸ííûõ íà Äîñêè ÷åðåç ýëåêòðîííóþ ïî÷òó.)";
+$ubbt_lang['SUB_SHOW']  = 	"(Ï - Ïîäïèñêà åñòü ; Í - Íåò ïîäïèñêè)";
+$ubbt_lang['TEXT_S']  = 	"Ï";
+$ubbt_lang['TEXT_U']  = 	"Í";
+?>
