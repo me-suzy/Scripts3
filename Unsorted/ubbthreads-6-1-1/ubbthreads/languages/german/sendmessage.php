324daa5f4dbbaab92a4f8096ec42d9099f7f88cb
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:45 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/sendmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/sendmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['CHOOSE_ADD']  = 	"Namen auswählen";
+$ubbt_lang['TEXT_DELETE']  = 	"L&ouml;schen";
+$ubbt_lang['NO_PRIVATE']  = 	"Dieser Benutzer akzeptiert keine privaten Nachrichten.";
+$ubbt_lang['NO_RECORD']  = 	"Der Benutzername, an den Sie die Nachricht senden m&ouml;chten, existiert nicht in unserer Datenbank.";
+$ubbt_lang['MESS_PREV']  = 	"Voransicht der Nachricht an";
+$ubbt_lang['PREVIEW_MESS']  = 	"Dies ist eine Voransicht Ihrer Nachricht. Wenn alles korrekt aussieht, klicken Sie auf 'Weiter', um sie endg&uuml;ltig abzuschicken. Ansonsten k&ouml;nnen unten noch &Auml;nderungen vorgenommen werden.";
+$ubbt_lang['EDIT_MORE']  = 	"Nachricht weiter ver&auml;ndern";
+$ubbt_lang['PRIV_SUB']  = 	"Sie haben neue Nachrichten";
+$ubbt_lang['PRIV_BODY_R']  = 	"Sie haben eine private Nachricht über das Forum erhalten .  Sie k&ouml;nnen direkt zu {$config['phpurl']}/login.php gehen, um sie zu lesen.";
+$ubbt_lang['MESS_SENT']  = 	"Ihre Nachricht wurde verschickt.";
+?>
