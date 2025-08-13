bf660ed876f0d17b288adf72461c3ef312ed6541
me-suzy <me.suzana@gmail.com>
2025-08-14 02:43:16 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/dopoll.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/dopoll.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NO_VOTE']  = 	"Nie wybra³e¶ ¿adnej opcji!";
+$ubbt_lang['POLL_REGED']  = 	"Tylko zarejestrowani u¿ytkownicy mog± g³osowaæ";
+$ubbt_lang['POLL_IP']  = 	"Z tego adresu IP ju¿ g³osowano.";
+$ubbt_lang['POLL_USER']  = 	"Ju¿ raz g³osowa³e¶.";
+?>
