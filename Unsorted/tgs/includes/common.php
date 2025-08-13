645a90848146a683bdb8123f1aa712025bce31ab
me-suzy <me.suzana@gmail.com>
2025-08-14 02:01:27 +0300

    Add Unsorted/tgs/includes/common.php

---
+++ Unsorted/tgs/includes/common.php
@@ -0,0 +1,12 @@
+<?
+
+// This will be the Email Address visitors will contact you with
+$contactEmail = "support@yoursite.com";
+
+// This is the charge you for Plan A members (should be less than Plan B)
+$planAcharge = "0.00";
+
+// This is the charge you for Plan B members (should be more than Plan B)
+$planBcharge = "0.00";
+
+?>
