c8fededc730642262b7055dbf7d931b826fbb624
me-suzy <me.suzana@gmail.com>
2025-08-14 01:44:29 +0300

    Add Unsorted/siteworks_pro_v5[1].0_null/swp50n/admin/server_array.php

---
+++ Unsorted/siteworks_pro_v5[1].0_null/swp50n/admin/server_array.php
@@ -0,0 +1,18 @@
+<?php
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//   Program Name         : SiteWorks Professional                           //
+//   Release Version      : 5.0                                              //
+//   Program Author       : SiteCubed Pty. Ltd.                              //
+//   Supplied by          : CyKuH [WTN]                                      //
+//   Nullified by         : CyKuH [WTN]                                      //
+//   Packaged by          : WTN Team                                         //
+//   Distribution         : via WebForum, ForumRU and associated file dumps  //
+//                                                                           //
+//                       WTN Team `2000 - `2002                              //
+///////////////////////////////////////////////////////////////////////////////
+print_r($_SERVER);
+
+echo realpath("server_array.php");
+
+?>
