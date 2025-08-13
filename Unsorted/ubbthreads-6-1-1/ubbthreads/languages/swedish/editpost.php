5e9bc58680d23a681ffbb972994ceebafb5bc35e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:01 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "Flytta detta inlägg och eventuella svar";
+$ubbt_lang['NO_EDIT']  = 	"Du kan inte redigera detta inlägg.";
+$ubbt_lang['EDITTIME']  = 	"Detta inlägg kan inte ändras beroende på att dess ändringstid löpt ut";
+$ubbt_lang['PEDIT_HEAD']  = 	"Redigera inlägg";
+$ubbt_lang['PEDIT_BODY']  = 	"Redigera eller radera detta inlägg.";
+$ubbt_lang['EDIT_POLL']  = 	"Beroende på att detta inlägg har ett röstformulär kan du inte redigera det men det går att radera.";
+$ubbt_lang['POST_ICON']  = 	"Inläggsikon";
+$ubbt_lang['MARK_EDIT']  = 	"Markera som ändrad?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Ändra detta inlägg";
+$ubbt_lang['PEDIT_DELETE']  = 	"Radera detta inlägg";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Godkänn detta inlägg";
+$ubbt_lang['RET_NORM']  = 	"Återgå till normalt datum";
+$ubbt_lang['MAKE_STICKY']  = 	"Gör inlägget fästande";
+$ubbt_lang['MANAGE_THREAD']  = 	"Kontrollera denna tråd";
+$ubbt_lang['DELETE_THREAD']  =  "Radera denna tråd";
+$ubbt_lang['OPEN_THREAD']  = 	"Öppna denna tråd";
+$ubbt_lang['CLOSE_THREAD']  = 	"Stäng denna tråd";
+$ubbt_lang['UNKEEP_THREAD']  = 	"Behåll inte denna tråd";
+$ubbt_lang['KEEP_THREAD']  = 	"Behåll denna tråd";
+$ubbt_lang['MOVE_THREAD']  = 	"Flytta denna tråd";
+?>
