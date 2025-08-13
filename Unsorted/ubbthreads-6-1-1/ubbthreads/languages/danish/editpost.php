59b349b68fed0004d1b7c566fd69015dc6daf8fd
me-suzy <me.suzana@gmail.com>
2025-08-14 02:31:09 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "Move this post & any replies";
+$ubbt_lang['DELETE_THREAD']  =  "Delete this thread";
+$ubbt_lang['NO_EDIT']  = 	"Det ser desværre ud til, at du ikke har tilladelse til at redigere dette indlæg.";
+$ubbt_lang['EDITTIME']  = 	"This post can no longer be edited because the maximum edit time has expired";
+$ubbt_lang['PEDIT_HEAD']  = 	"Rediger dette indlæg";
+$ubbt_lang['PEDIT_BODY']  = 	"Du har mulighed for at ændre i indlægget og at slette det. Hvis du sletter indlægget, og der er svar på det, så vil emnet på indlægget blive ændret til 'Dette indlæg er slettet'.";
+$ubbt_lang['EDIT_POLL']  = 	"Because this post has a poll in it, you may delete it but not edit it.";
+$ubbt_lang['POST_ICON']  = 	"Ikon til indlægget";
+$ubbt_lang['MARK_EDIT']  = 	"Mark as Edited?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Ændr dette indlæg";
+$ubbt_lang['PEDIT_DELETE']  = 	"Slet dette indlæg";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Godkend dette indlæg";
+$ubbt_lang['RET_NORM']  = 	"Return to normal date";
+$ubbt_lang['MAKE_STICKY']  = 	"Make post sticky";
+$ubbt_lang['MANAGE_THREAD']  = 	"Manage this thread";
+$ubbt_lang['OPEN_THREAD']  = 	"Open this thread";
+$ubbt_lang['CLOSE_THREAD']  = 	"Close this thread";
+$ubbt_lang['UNKEEP_THREAD']  = 	"Unkeep this thread";
+$ubbt_lang['KEEP_THREAD']  = 	"Keep this thread";
+$ubbt_lang['MOVE_THREAD']  = 	"Move this thread";
+?>
