d1b650b6f348270997ecc3429cdb635b55ee24c0
me-suzy <me.suzana@gmail.com>
2025-08-14 02:30:21 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/ubbthreads.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/ubbthreads.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['NEW_MEM'] = "»¶Ó­ÐÂ¼ÓÈëµÄÍøÓÑ£º";
+$ubbt_lang['VIEW_PAST']  = 	"¹Û¿´×î½üµÄÎÄÕÂ";
+$ubbt_lang['PAST24']  = 	"×î½üÒ»Ìì";
+$ubbt_lang['PAST48']  = 	"×î½üÁ½Ìì";
+$ubbt_lang['PAST7']  = 	"×î½üÒ»ÖÜ";
+$ubbt_lang['THREAD_TEXT']  = 	"»°Ìâ";
+$ubbt_lang['OFFSET_TIME'] =	"ÏµÍ³Ê±¼ä£º";
+$ubbt_lang['REGED_USERS'] =	"Î»×¢²áÊ¹ÓÃÕß¡£";
+$ubbt_lang['WEL_PRIV_NO']  = 	"Äú»¹Ã»ÓÐµÇÈë¡£";
+$ubbt_lang['POSTS_TEXT']  = 	"ÎÄÕÂ";
+$ubbt_lang['LAST_POST']  = 	"×îºóÕÅÌùÊ±¼ä";
+$ubbt_lang['NEW_BOARD']  = 	"ÐÂ°å";
+$ubbt_lang['NEW_TEXT']  = 	"ÐÂ";
+$ubbt_lang['NOT_APPROVED'] =	"ÉÐÎ´ºË¿É";
+?>
