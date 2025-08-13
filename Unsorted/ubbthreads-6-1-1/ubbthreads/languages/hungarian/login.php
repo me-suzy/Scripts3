a3286e6a3963a2597d8fb62d567cf0487f17f670
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:59 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/login.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/login.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['FORGOT_PASS']   = "Forget your password?";
+$ubbt_lang['FORGOT_ENTER']  = "Enter your email address or username that you registered with to have a temporary password mailed to you.";
+$ubbt_lang['REMEMBER_ME']   =  "Remember me on each visit.";
+$ubbt_lang['LOGIN_PROMPT']  = 	"Lépjen be";
+$ubbt_lang['LOGIN_PROMPT2']  = 	"Írja be a nevet és jelszavát a belépéshez.  Ha még nincs regisztrálva";
+$ubbt_lang['REG_ONE']  = 	"regisztráljon egyet";
+$ubbt_lang['PASSWORD_TEXT']  = 	"Jelszó";
+?>
