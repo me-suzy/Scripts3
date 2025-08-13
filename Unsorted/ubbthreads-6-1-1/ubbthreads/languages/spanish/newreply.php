0a25548de3c7b052df57b520cecc975a61ccc12c
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:48 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/newreply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/newreply.php
@@ -0,0 +1,19 @@
+<?
+$ubbt_lang['READ_PERM']  = 	"En este foro &uacute;nicamente tienes permiso de lectura.";
+$ubbt_lang['NOT_APP']  = 	"No se te permite contestar a un mensaje que no ha sido admitido.";
+$ubbt_lang['REPLY_HEAD']  = 	"Contestar a";
+$ubbt_lang['FILL_FORM']  = 	"Rellena este formulario para enviar un mensaje al foro.";
+$ubbt_lang['NO_HTML']  = 	"No se permite HTML.";
+$ubbt_lang['YES_HTML']  = 	"Se permite HTML.";
+$ubbt_lang['NO_MARKUP']  = 	"El Etiquetador no est&aacute; permitido.";
+$ubbt_lang['YES_MARKUP']  = 	"El Etiquetador est&aacute; permitido, por lo que puedes usarlo";
+$ubbt_lang['MARK_POSTS']  = 	"Etiquetador en tus mensajes";
+$ubbt_lang['CHOOSE_NAME']  = 	"Se permite poner mensajes an&oacute;nimamente, por lo que puedes escoger cualquier nombre que no est&eacute; registrado para poner este mensaje.";
+$ubbt_lang['POST_ICON']  = 	"Insertar Icono";
+$ubbt_lang['POST_TEXT']  = 	"Enviar";
+$ubbt_lang['SPELL_CHECK']  = 	"Revisar ortograf&iacute;a";
+$ubbt_lang['DO_EMAIL']  = 	"Deseo recibir todas las respuestas a mi mensaje por email.";
+$ubbt_lang['DO_PREVIEW']  = 	"Deseo previsualizar mi mensaje";
+$ubbt_lang['CAN_ATTACH']  = 	"y/o adjuntar un archivo";
+$ubbt_lang['IN_RESPONSE']  = 	"En respuesta a";
+?>
