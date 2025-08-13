f8f13c05e24cc95710e711e0e68c962ce098665d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:30:42 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"Det ønskede antal indlæg pr. side er ugyldigt.";
+$ubbt_lang['FLAT_BAD']  = 	"Flat Posts per page must be between 1 and 99.";
+$ubbt_lang['TEXTAREA_NO']  = 	"Den ønskede størrelse på tekstfeltet er ugyldig.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Indstillingerne for udseendet er nu blevet ændret.";
+?>
