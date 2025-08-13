30b1e6333d39df874362e074ed1e1fcf1abd3824
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:09 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/addfav.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/addfav.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  =  "Vous n'êtes pas autorisé à voir ce sujet donc vous ne pouvez pas l'ajouter à votre liste de favoris et pense-bête.";
+$ubbt_lang['MAIN_ONLY']  =  "Vous ne pouvez ajouter que les sujets principaux à votre liste de favoris et pense-bête.";
+$ubbt_lang['DUP_ENTRY']  =  "Ce Message/Sujet a déjà été ajouté à votre liste de Favoris/Pense-bête.";
+$ubbt_lang['ENTRY_IN']  =  "Message/Sujet ajouté";
+$ubbt_lang['FAV_CONFIRM']  =  "Le message/sujet choisis a été ajouté à votre liste.  Si vous l'avez ajouté à vos favoris vous verrez cette entrée dans 'Mon Centre' jusqu'à ce que vous la supprimiez.  Si vous l'avez ajouté comme pense-bête il disparaitra une fois que vous aurez répondu à ce message.";
+$ubbt_lang['FORUM_RETURN']  =  "Retourner sur le forum.";
+?>
