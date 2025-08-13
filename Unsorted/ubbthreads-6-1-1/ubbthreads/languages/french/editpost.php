29e763cccfeeb8a01602de5eaa56b14c7c82e461
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:46 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "Déplacer ce message et les éventuelles réponses";
+$ubbt_lang['NO_EDIT']  =   "Nous ne pouvons pas vérifier vos permissions pour éditer ce message.";
+$ubbt_lang['EDITTIME']  = 	"Ce message ne peut plus être édité car le temps d'édition est écoulé.";
+$ubbt_lang['PEDIT_HEAD']  = 	"Éditer le message";
+$ubbt_lang['PEDIT_BODY']  = 	"Editez ou effacez ce message : ";
+$ubbt_lang['EDIT_POLL']  = 	"Comme ce message contient un sondage vous pouvez le supprimer mais pas l'éditer.";
+$ubbt_lang['POST_ICON']  = 	"Icône du Message";
+$ubbt_lang['MARK_EDIT']  = 	"Marquer comme Edité ?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Modifier ce message";
+$ubbt_lang['PEDIT_DELETE']  = 	"Effacer ce message";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Approuver ce message";
+$ubbt_lang['RET_NORM']  = 	"Replacer à la date normale";
+$ubbt_lang['MAKE_STICKY']  = 	"Fixer Message";
+$ubbt_lang['MANAGE_THREAD']  = 	"Gérer ce sujet";
+$ubbt_lang['DELETE_THREAD']  =  "Effacer ce sujet";
+$ubbt_lang['OPEN_THREAD']  = 	"Ouvrir ce sujet";
+$ubbt_lang['CLOSE_THREAD']  = 	"Fermer ce sujet";
+$ubbt_lang['UNKEEP_THREAD']  = 	"Ne plus conserver ce sujet";
+$ubbt_lang['KEEP_THREAD']  = 	"Conserver ce sujet";
+$ubbt_lang['MOVE_THREAD']  = 	"Déplacer ce sujet";
+?>
