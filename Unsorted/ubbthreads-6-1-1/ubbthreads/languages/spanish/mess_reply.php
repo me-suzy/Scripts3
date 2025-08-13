beda0e84ef221a4fcc765a73410025834cee7815
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:43 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/mess_reply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/mess_reply.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['EDIT_MORE']  = 	"Seguir editando";
+$ubbt_lang['REPLY_PREV']  = 	"Previsualizar la contestaci&oacute;n a";
+$ubbt_lang['PREVIEW_MESS']  = 	"Esto que ves es una previsualizaci&oacute;n de c&oacute;mo quedar&aacute; tu mensaje. Si parece estar bien pulsa 'Continuar' y se enviar&aacute; tu mensaje.  De lo contrario, si ves que deseas hacer alg&uacute;n cambio, usa el formulario de abajo para seguir editando el mensaje.";
+$ubbt_lang['MESS_PREVIEW']  = 	"Deseo previsualizar mi mensaje";
+$ubbt_lang['PRIV_SUB']  = 	"Tienes mensajes nuevos";
+$ubbt_lang['PRIV_BODY_R']  = 	"Hola, este mensaje es para informarte de que has recibido un mensaje privado en los foros de {$config['title']}.  Puedes ir a {$config['phpurl']}/login.php para leerlo.  Saludos.";
+?>
