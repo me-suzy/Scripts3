40e93fb8fcf42f6035d82f89da54830de69cccf6
me-suzy <me.suzana@gmail.com>
2025-08-14 02:48:57 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/addfavforum.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/addfavforum.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  =  "No tienes autorizaci&oacute;n para ver este tablero, por lo tanto no puedes incluirlo en tu lista de favoritos.";
+$ubbt_lang['DUP_ENTRY']  =  "Este tablero ya ha sido a&ntilde;adido a tu lista de favoritos.";
+$ubbt_lang['FORUM_IN']  =  "Tablero (foro) a&ntilde;adido";
+$ubbt_lang['FORUM_CONFIRM']  =  "El tablero (foro) escogido ha sido a&ntilde;adido a tu lista de Tableros favoritos.  Podr&aacute;s verlo desde tu 'Panel de Control' hasta que decidas quitarlo.";
+$ubbt_lang['FORUM_RETURN']  =  "Volver r&aacute;pidamente al foro.";
+$ubbt_lang['TOO_MANY']  =  "&Uacute;nicamente se pueden tener 83 foros en tu lista de favoritos.";
+?>
