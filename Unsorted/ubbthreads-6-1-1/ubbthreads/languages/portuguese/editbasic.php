888065c530c48e0008c45ecb509a385c60242bdc
me-suzy <me.suzana@gmail.com>
2025-08-14 02:44:55 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =      "Your picture ({$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['PROF_PIC']  =       "Your Picture (Entire URL - Picture size is {$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['AUX_ONLINE']  =      "Do you want users to see the actual forum/post you are looking at?";
+$ubbt_lang['PROF_PRIV']  = 	"Accept private messages from other users?";
+$ubbt_lang['NO_PROF']  = 	"Perfil do usuário não encontrado";
+$ubbt_lang['PROF_HEAD']  = 	"Editar perfil de";
+$ubbt_lang['PROF_EDIT']  = 	"As informações embaixo podem ser alteradas.";
+$ubbt_lang['BASIC_PROF']  = 	"Perfil básico";
+$ubbt_lang['PROF_PASS']  = 	"Senha (de 4 a 20 caracteres)";
+$ubbt_lang['VER_PASS']  = 	"Verificar senha";
+$ubbt_lang['EMAIL_ADD']  = 	"Endereço mail (não será disponível a outros usuários)";
+$ubbt_lang['FAKE_EMAIL']  = 	"Mostrar endereço mail nas mensagens";
+$ubbt_lang['FULL_NAME']  = 	"Nome completo";
+$ubbt_lang['PROF_SIG']  = 	"Assinatura (até  {$config['Sig_length']} caracteres).  É permitido UBBCode na assinatura";
+$ubbt_lang['PROF_HOME']  = 	"Página pessoal (até 150 caracteres)";
+$ubbt_lang['PROF_OCC']  = 	"Ocupação";
+$ubbt_lang['PROF_HOBB']  = 	"Hobbies (até 200 caracteres)";
+$ubbt_lang['PROF_LOC']  = 	"Localização geográfica (Estado, país, etc.)";
+$ubbt_lang['PROF_BIO']  = 	"Biografia (até 250 caracteres)";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ Number";
+$ubbt_lang['PROF_VIS']  = 	"Visível na tela dos usuários conetados?";
+?>
