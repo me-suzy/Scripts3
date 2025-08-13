d2232e2b333e2f481020f4b331fd1b1d282cb68a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:26 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/addfavforum.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/addfavforum.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  =  "Eftersom du inte har tillåtelse att läsa detta forum kan du inte lägga till det i din favoritlista.";
+$ubbt_lang['DUP_ENTRY']  =  "Detta forum finns redan i din favoritlista.";
+$ubbt_lang['FORUM_IN']  =  "Forum tillagt";
+$ubbt_lang['FORUM_CONFIRM']  =  "Forumet du valde har lagts till i din lista över favoritforum. Detta forum kommer nu finnas på din 'Kontrollpanel' tills du tar bort det.";
+$ubbt_lang['FORUM_RETURN']  =  "Återvänd till forumet nu.";
+$ubbt_lang['TOO_MANY']  =  "Du kan inte ha fler än 83 forum i din favoritlista.";
+?>
