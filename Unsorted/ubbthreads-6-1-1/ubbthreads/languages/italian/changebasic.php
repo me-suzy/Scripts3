c48a7384ee88d53b0097188ed178756834e7ca1d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:56 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/changebasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/changebasic.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['PIXEL_TOO_BIG']="Your picture exceeds the ({$theme['PictureWidth']}x{$theme['PictureHeight']}) pixel limitation width a width/height of ";
+$ubbt_lang['FILE_TOO_BIG']="Pictures can be no larger than {$config['avatarsize']} bytes.";
+$ubbt_lang['BAD_PIC']  =       "Improper picture type.";
+$ubbt_lang['PASS_MATCH']  = 	"Password errate.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"La Password deve essere tra i 4 e i 20 caratteri.";
+$ubbt_lang['HOME_TOO_LONG']  = 	"La tua homepage è troppo lunga.";
+$ubbt_lang['OCC_TOO_LONG']  = 	"L'occupazione inserita è troppo lunga.";
+$ubbt_lang['HOBB_TOO_LONG']  = 	"Gli hobby inseriti sono troppo lunghi.";
+$ubbt_lang['LOC_TOO_LONG']  = 	"La tua locazione è troppo lunga.";
+$ubbt_lang['SIG_TOO_LONG']  = 	"La tua firma è troppo lunga.";
+$ubbt_lang['BIO_TOO_LONG']  = 	"La tua biografia è troppo lunga.";
+$ubbt_lang['BASIC_MOD']  = 	"Il tuo profilo di base è stato modificato.";
+?>
