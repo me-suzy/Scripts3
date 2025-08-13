4bd9af33940d258c9a8c9fdcea56dec219250f7a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:28 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/addpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/addpost.php
@@ -0,0 +1,33 @@
+<?
+$ubbt_lang['NOTEXT'] =	"NT = No Text (Ingen text)"; // OJ HACK
+$ubbt_lang['FILESALLOWED']  =  "Filen du vill ladda upp måste ha följande ändelse";
+$ubbt_lang['FILESEXCLUDE']  = 	"Filen du vill ladda upp kan inte ha följande ändelse";
+$ubbt_lang['POST_ICON']  =     "Inläggsikon";
+$ubbt_lang['SPELL_CHECK']  = 	"Kontrollera stavning";
+$ubbt_lang['SPELL_BODY']  = 	"Här nedan visas stavningskontrollen av ditt inlägg.";
+$ubbt_lang['NAME_TAKEN']  = 	"Användarnamnet du försöker använda för inlägg används redan av en registrerad användare.";
+$ubbt_lang['BAD_UNAME']  = 	"Du kan bara ha alfabetiska tecken i ditt användarnamn och tyvärr ännu inte ÅÄÖ.  Specialtecken tillåts inte så att det inte ska vara möjligt att fejka andras användarnamn.  Du kan däremot använda \"_\"-ecknet som ersättning för mellanslag.";
+$ubbt_lang['LONG_NAME']  = 	"Användarnamnet har för få tecken.";
+$ubbt_lang['READ_PERM']  = 	"Du har endast läsrättigheter till detta forum.";
+$ubbt_lang['LOCKED']  = 	"Tråd låst.";
+$ubbt_lang['FILE_TOO_BIG']  = 	"Filen är för stor.";
+$ubbt_lang['TWO_POLLS']  = 	"Du får bara rösta en gång.";
+$ubbt_lang['PREV_POST']  = 	"Förhandsgranska inlägg";
+$ubbt_lang['SUB_VOTE']  = 	"Spara röst";
+$ubbt_lang['POST_BODY']  = 	"Här är en förhandsgranskning av ditt inlägg. Om allt ser bra ut tryck på 'Fortsätt' så kommer ditt inlägg sparas, om inte använd fältet nedanför för att redigera.";
+$ubbt_lang['YES_FILE2']  = 	"Du kan bifoga en fil om du vill (filen får inte vara större än {$config['filesize']} bytes).";
+$ubbt_lang['NO_FILE2']  = 	"Det är tillåtet att ladda upp filer, men du måste använda en browser som är komtaibel med Mozilla 4+ för att kunna göra det.";
+$ubbt_lang['EDIT_MORE']  = 	"Redigera mer";
+$ubbt_lang['POST_TEXT']  = 	"Posta";
+$ubbt_lang['DO_EMAIL']  = 	"Skicka alla svar till min riktiga e-postadress";
+$ubbt_lang['DO_PREVIEW']  = 	"Jag vill förhandsgranska mitt inlägg";
+$ubbt_lang['CAN_ATTACH']  = 	"och/eller bifoga en fil";
+$ubbt_lang['NO_DUPS']  = 	"Detta inlägg finns redan.";
+$ubbt_lang['UNREGED_USER']  = 	"Oregistrerad";
+$ubbt_lang['REPLY_SUB']  = 	"Svar till ditt inlägg";
+$ubbt_lang['REPLY_BOD']  = 	"svarade på ditt inlägg på vårt forum: ";
+$ubbt_lang['POST_ENTERED']  = 	"Ditt inlägg har sparats.";
+$ubbt_lang['MOD_CONFIRM']  = 	"Det här forumet styrs av Administratörerna och inlägget måste granskas och godkännas innan inlägget läggs upp i forumet.";
+$ubbt_lang['FORUM_RETURN']  = 	"Återgå till forumet";
+$ubbt_lang['VIEW_POST']  = 	"Titta på ditt inlägg";
+?>
