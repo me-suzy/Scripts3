7a48af0822eb4dc873354b90d3ecb00443c2550f
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:35 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/changebasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/changebasic.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['PIXEL_TOO_BIG']="Your picture exceeds the ({$theme['PictureWidth']}x{$theme['PictureHeight']}) pixel limitation width a width/height of ";
+$ubbt_lang['FILE_TOO_BIG']="Pictures can be no larger than {$config['avatarsize']} bytes.";
+$ubbt_lang['BAD_PIC']  =       "Improper picture type.";
+$ubbt_lang['PASS_MATCH']  = 	"Ïàðîëè íå ñîâïàäàþò.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Ïàðîëü äîëæåí ñîäåðæàòü îò 4 äî 20 çíàêîâ.";
+$ubbt_lang['HOME_TOO_LONG']  = 	"Ïîëå &quot;WEB-ñòðàíèöà&quot; ïðåâûøàåò äîïóñòèìóþ äëèíó.";
+$ubbt_lang['OCC_TOO_LONG']  = 	"Ïîëå &quot;Çàíÿòîñòü&quot; ïðåâûøàåò äîïóñòèìóþ äëèíó.";
+$ubbt_lang['HOBB_TOO_LONG']  = 	"Ïîëå &quot;Õîááè&quot; ïðåâûøàåò äîïóñòèìóþ äëèíó.";
+$ubbt_lang['LOC_TOO_LONG']  = 	"Ïîëå &quot;Ðàñïîëîæåíèå&quot; ïðåâûøàåò äîïóñòèìóþ äëèíó.";
+$ubbt_lang['SIG_TOO_LONG']  = 	"Âàøà ïîäïèñü ñëèøêîì äëèííà.";
+$ubbt_lang['BIO_TOO_LONG']  = 	"Âàøà &quot;êðàòêàÿ áèîãðàôèÿ&quot; ñëèøêîì äëèííà.";
+$ubbt_lang['BASIC_MOD']  = 	"Áàø áàçîâûé ïðîâèëü óñïåøíî èçìåí¸í.";
+?>
