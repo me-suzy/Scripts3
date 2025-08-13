dc78e48c3812768a904d4021b48ecfdc82bb4666
me-suzy <me.suzana@gmail.com>
2025-08-14 00:31:58 +0300

    Add Unsorted/PHPCheckout/phpcheckout/installCancel.php

---
+++ Unsorted/PHPCheckout/phpcheckout/installCancel.php
@@ -0,0 +1,61 @@
+<?php require_once("configure.php");?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+	<head>
+
+		<script language="Javascript" src="appinstaller.js"></script>
+		<script language="Javascript">loadCSSFile();</script>
+		<title>Install Helper - a DreamRiver Software Product - Easily install DreamRiver Software  - php software mysql database - download - web database - dreamriver - freeware - sql - file downloader - internet application development - hot scripts - good value - database software - custom code - php e-mail software - ultimate web apps software - phpYellow Pages - EasySQL - phpTellAFriend - mysql php download</title>
+	</head> 
+
+<body>
+<!-- START of body -->
+
+<h1>DreamRiver Install Helper</h1>
+
+<H2>phpcheckout<sup>TM</sup></H2>
+
+<form name="startForm">
+<table bgcolor="silver" cellspacing=3 cellpadding=10 width="80%">
+<tr>
+	<th colspan=2>Install Helper</th>
+</tr>
+<tr>
+	<td bgcolor="white" valign="top">
+		<h4>Cancel Installation</h4>
+		
+
+		<p>
+		You have asked the Install Helper to cancel. 
+		<br><br>
+		<b>Do you really want to cancel?</b>
+		</p>
+
+		
+
+		
+		<p>
+		To cancel, please click on the &quot;Finish&quot; button.<br>
+		To continue, please click on the &quot;Back&quot; button.
+		</p>
+
+		<p></p>
+	</td>
+	<td bgcolor="ivory" align="center">
+		<img src="bop210x180.jpg" width="210" height="180" border=0>
+	</td>
+</tr>
+<tr>
+	<td align="right" colspan=2>
+		<hr>
+		<input type="button" name="back" value="Back" onclick="history.back(1)">
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<input type="button" name="cancel" value="Finish" class="submit" onclick='location.href="http://www.dreamriver.com"'>
+	</td>
+</tr>
+</table>
+</form>
+
+
+</body>
+</html>
