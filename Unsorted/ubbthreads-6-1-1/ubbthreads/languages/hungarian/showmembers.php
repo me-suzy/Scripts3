88ae0539a75f8a739c05b273477d7c9c01e0b9a2
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:27 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/showmembers.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/showmembers.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['USER_STATUS']  = 	"Állapot";
+$ubbt_lang['TEXT_HOME']  = 	"Home-lap";
+$ubbt_lang['TOTAL_POSTS']  = 	"Összes üzenet";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ Szám";
+$ubbt_lang['REGGED_ON']  = 	"Regisztráció ideje:";
+$ubbt_lang['PAGE_TEXT']  = 	"Oldal";
+?>
