30df8d5f9d384a16bdec860b705c84cb3de05167
me-suzy <me.suzana@gmail.com>
2025-08-14 00:48:31 +0300

    Add Unsorted/phplive1.9.7/phplive/lang_packs/Danish.php

---
+++ Unsorted/phplive1.9.7/phplive/lang_packs/Danish.php
@@ -0,0 +1,84 @@
+<?
+$LANG = ARRAY() ;		// MUST initialize, DO NOT CHANGE
+/*******************************************************************
+* Language: Danish
+* Contributed by:
+*	Name: Theis Andersen Samsig <tas@mediaone.dk>
+*	Company: Mediaone Services
+*	URL: http://www.mediaone.dk
+*
+* ------------------------------------------------------------------
+* DO NOT REMOVE COMMENTS.  JUST MODIFY THE ACTUAL TEXT.
+* DO NOT REMOVE THE orig TEXT!  THEY ARE THERE FOR REFERENCE.
+* ------------------------------------------------------------------
+* NOTE: If you are going to submit a language pack, please keep
+* everything basic and do not mention any company name, personal name,
+* copyrighted name, or such.  Keep all translations generic, simple
+* and catered to all audiences and companies.  Basically, your
+* translated language pack should be ready to use without being touched.
+*******************************************************************/
+
+
+
+/***************** ONLY MAKE THE CHANGE BELOW ******************/
+
+
+/*** branded string located troughout 99% of the pages */
+//orig: PHP <i>Live!</i> &copy; 2001 OSI Codes, New York, New York
+$LANG['DEFAULT_BRANDING'] = "Powered by <a href='http://www.PHPLiveSupport.com' target='new'>PHP <i>Live!</i></a>" ;
+
+/*** chat page titles */
+//orig: Request Live! Support Request
+$LANG['TITLE_SUPPORTREQUEST'] = "Anmod om Live! Support" ;
+//orig: You are now speaking with a Live! Support
+$LANG['TITLE_SUPPORCHAT'] = "Du snakker nu med en Live! Support agent" ;
+//orig: Please leave a message
+$LANG['TITLE_LEAVEMESSAGE'] = "Efterlad venligst en besked" ;
+//orig: Printer Friendly View
+$LANG['TITLE_PRINTVIEW'] = "Udskrifts venligt format" ;
+
+$LANG['WORD_BACK'] = "Tilbage" ;
+$LANG['WORD_CLOSE'] = "Luk" ;
+$LANG['WORD_NAME'] = "Navn" ;
+$LANG['WORD_EMAIL'] = "Email" ;
+$LANG['WORD_SUBJECT'] = "Emne" ;
+$LANG['WORD_MESSAGE'] = "Besked" ;
+$LANG['WORD_SEND'] = "Send" ;
+$LANG['WORD_COMPANY'] = "Firma" ;
+$LANG['WORD_DEPARTMENT'] = "Afdeling" ;
+$LANG['WORD_DAY'] = "Dag" ;
+
+//orig: Welcome to our Live! support help desk.
+$LANG['CHAT_REQUEST_TITLE'] = "Velkommen til vores Live! support help desk." ;
+//orig: Please select the department you would like to reach.
+$LANG['CHAT_REQUEST_SELECTDEPT'] = "V&aelig;lg venligst den afdeling du gerne vil kontakte." ;
+//orig: Input your Screen Name used during this session:
+$LANG['CHAT_REQUEST_INPUTSCREENNAME'] = "Skriv venligst deres navn." ;
+//orig: Welcome %%user%%! Please hold while we contact a representative. If a representative does not respond in few seconds, then he/she is not available at this time.
+$LANG['CHAT_GREETING'] = "Velkommen %%user%%! Vent venligst og de vil blive betjent af den f&oslash;rst ledige agent. Hvis de ikke bliver betjent inden for nogle f&aring; sekunder er der desv&aelig;rre ingen ledige agenter p&aring; nuv&aelig;rende tidspunkt." ;
+//orig: Printer Friendly
+$LANG['CHAT_PRINTER_FRIENDLY'] = "Udskrifts venlig" ;
+//orig: Your party has left this session.
+$LANG['CHAT_PARTYLEFTSESSION'] = "Den anden deltager har forladt samtalen." ;
+//orig: You are now speaking with
+$LANG['CHAT_REQUEST_NOWSPEAKINGWITH'] = "Du taler nu med" ;
+//orig: Department is currently unavailable. Please leave a message.
+$LANG['MESSAGE_BOX_MESSAGE'] = "Afdelingen er i &oslash;jeblikket ikke ledig. Efterlad venligst en besked." ;
+//orig: Thank you.  Your message has been sent to:
+$LANG['MESSAGE_BOX_SENT'] = "Tak.  Din besked er blevet sendt til:" ;
+//orig: Items with (*) must be filled.
+$LANG['MESSAGE_BOX_JS_A_ALLFIELDSSUP'] = "Felter markeret med (*) skal udfyldes." ;
+//orig: Email is invalid format (example: someone@somewhere.com)
+$LANG['MESSAGE_BOX_JS_A_INVALIDEMAIL'] = "Email is invalid format (example: someone@somewhere.com)" ;
+//orig: Department not available.  Please leave a message.
+$LANG['CHAT_DEPT_NOTAVAIL'] = "Afdelingen er ikke ledig. Efterlad venligst en besked." ;
+//orig: Session did not create.  Request did not send.
+$LANG['CHAT_NOTCREATE'] = "Forbindelsen kunne ikke etableres.  Anmodningen kunne ikke gennemf&oslash;res." ;
+
+//orig: Please hold while being transferred to
+$LANG['CHAT_CALL_TRANSFER'] = "" ;
+//orig: What is your question?
+$LANG['CHAT_REQUEST_QUESTION'] = "" ;
+//orig: A copy of the chat transcript has been sent to:
+$LANG['CHAT_TRANSCRIPT_SENT'] = "A copy of the chat transcript has been sent to:" ;
+?>
