8cbf49747107444b0981a8e6988824f72a20da65
me-suzy <me.suzana@gmail.com>
2025-08-14 02:34:11 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"We could not find a profile for the Username";
+$ubbt_lang['EMAIL_HEAD']  = 	"Edit email notifications for";
+$ubbt_lang['PROF_EDIT']  = 	"You can edit any of the information below.";
+$ubbt_lang['PROF_SUBS']  = 	"Email Notifications / Subscriptions";
+$ubbt_lang['PROF_NOTIF']  = 	"Do you want email notifications when you receive private messages?";
+$ubbt_lang['PROF_REPLIES']  = 	"Do you want all replies to posts that you make emailed to you?";
+$ubbt_lang['SUB_STAT']  = 	"Subscription status for the following forums.  (Receive a daily archive of all posts to this forum via your Real email address.)";
+$ubbt_lang['SUB_SHOW']  = 	"(S - Subscribe ; U - Unsubscribe)";
+$ubbt_lang['TEXT_S']  = 	"S";
+$ubbt_lang['TEXT_U']  = 	"U";
+?>
