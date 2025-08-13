447ac7347c276f464e4d76d21fdbd9bce81fee8e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:38 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/showmembers.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/showmembers.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['USER_STATUS']  = 	"Status";
+$ubbt_lang['TEXT_HOME']  = 	"Hemsida";
+$ubbt_lang['TOTAL_POSTS']  = 	"Inlägg";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ-nummer";
+$ubbt_lang['REGGED_ON']  = 	"Registrerad den";
+$ubbt_lang['PAGE_TEXT']  = 	"Sidor";
+?>
