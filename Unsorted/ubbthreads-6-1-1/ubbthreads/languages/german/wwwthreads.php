176fc7656a4f22fb3ffe517d206add008c577a96
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:12 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/wwwthreads.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/wwwthreads.php
@@ -0,0 +1,17 @@
+<?
+$ubbt_lang[VIEW_PAST] =    "Zeige die neusten Nachrichten";
+$ubbt_lang[PAST24] =       "Die letzen 24 Stunden";
+$ubbt_lang[PAST48] =       "Die letzen 48 Stunden";
+$ubbt_lang[PAST7] =        "Die letzen 7 Tage";
+$ubbt_lang[THREAD_TEXT] =	"Threads";
+$ubbt_lang[OFFSET_TIME] =	"Aktuelles Datum: ";
+$ubbt_lang[REGED_USERS] =	"Registrierte Benutzer.";
+$ubbt_lang[WEL_PRIV_NO] =	"Du bist nicht angemeldet.";
+$ubbt_lang[POSTS_TEXT] =	"Nachrichten";
+$ubbt_lang[LAST_POST] =	"Letzte Nachricht";
+$ubbt_lang[NEW_BOARD] =	"Neues Forum";
+$ubbt_lang[NEW_TEXT] =	"neu";
+$ubbt_lang[NOT_APPROVED] =	"NF";
+
+
+?>
