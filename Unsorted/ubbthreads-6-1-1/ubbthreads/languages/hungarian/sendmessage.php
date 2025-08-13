2a6b4a2cf5f475ca6c11b8f188414e2f44bd25d2
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:19 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/sendmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/sendmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['CHOOSE_ADD']  = 	"Choose a name";
+$ubbt_lang['TEXT_DELETE']  = 	"Törlés";
+$ubbt_lang['NO_PRIVATE']  = 	"Ez a tag nem fogad privát üzenetet.";
+$ubbt_lang['NO_RECORD']  = 	"Nincs ilyen nevü regisztált felhasználó, akinek üzenetet szeretne küldeni.";
+$ubbt_lang['MESS_PREV']  = 	"Preview private message to";
+$ubbt_lang['PREVIEW_MESS']  = 	"The following is a preview of your message.  If everything looks ok then you can click 'Continue' and your message will be sent.  If not then use the bottom form below to edit some more.";
+$ubbt_lang['EDIT_MORE']  = 	"Edit some more";
+$ubbt_lang['PRIV_SUB']  = 	"Üzenet érkezett";
+$ubbt_lang['PRIV_BODY_R']  = 	"Önnek privát üzenete érkezett a következõ fórumon: .  A  {$config['phpurl']}/login.php címen olvashatja el.";
+$ubbt_lang['MESS_SENT']  = 	"Az ön üzenete engedélyezve.";
+?>
