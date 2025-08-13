f27ebe0d560127f2938609feda61decede55a136
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:32 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"¨C­¶Åã¥Üªº¤å³¹¼Æ¤£¯à¨Ï¥Î¡C";
+$ubbt_lang['FLAT_BAD']  = 	"¥­©Z¼Ò¦¡¨C­¶Åã¥Ü¤å±i¥²¶·¤¶©ó 1 ©M 99 ¤§¶¡¡C";
+$ubbt_lang['TEXTAREA_NO']  = 	"¤å¦r°Ïªº­È¤£¯à¨Ï¥Î¡C";
+$ubbt_lang['DISPLAY_MOD']  = 	"±zªºÅã¥Ü³ß¦n¤w¸g§ó§ï¤F¡C";
+?>
