58e73e4ab940be20a43fcdab20ea96e97717b436
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:00 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =    "Tu cuenta de acceso necesita ser aprobada por el Administrador antes de que puedas utilizarla.  Recibir&aacute;s un email avis&aacute;ndote de cuando tu cuenta esté aprobada.";
+$ubbt_lang['APPROVED'] = "Tu cuenta de acceso a {$config['title']} en {$config['phpurl']} ha sido aprobada y aceptada.";
+$ubbt_lang['NOTAPPROVED'] = "Tu cuenta de acceso a {$config['title']} en {$config['phpurl']} ha sido rechazada.  Los motivos de la negativa son:";
+$ubbt_lang['NEWREGBODY'] = "Un nuevo usuario se ha registrado en {$config['title']}. Para aprobar y aceptar esta cuenta, visita";
+$ubbt_lang['NEWREG'] = "Nuevo Registro de Usuario en";
+$ubbt_lang['INVALID_DATE'] = "Esa es una fecha inv&aacute;lida.";
+$ubbt_lang['TOOYOUNG']  =       "El registro a este foro requerirá aprobación de tus padres o tutores.  Para ello debes utilizar el <a href=\"{$config['phpurl']}/coppaform.php\">Formulario de Permiso</a>.";
+$ubbt_lang['UNDER13'] = "Lo siento, no se autorizan menores de 13 a&ntilde;os en el foro.";
+$ubbt_lang['BAD_UNAME']  = 	"En el nombre del miembro no se permite  NI c&oacute;digo HTML ni espacios en blanco antes o despu&eacute;s del nombre.";
+$ubbt_lang['LONG_NAME']  = 	"El nombre de Miembro no tiene la longitud correcta.";
+$ubbt_lang['BAD_EMAIL']  = 	"No se te permite registrar un nombre de miembro desde el dominio";
+$ubbt_lang['USER_EXISTS']  = 	"El miembro del foro ya existe.";
+$ubbt_lang['NO_MULTI']  = 	"Ya existe una cuenta con esa direcci&oacute;n de email.";
+$ubbt_lang['PASS_MATCH']  = 	"Las contrase&ntilde;as no concuerdan.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"La contrase&ntilde;a necesita tener entre 4 y 20 letras o n&uacute;meros.";
+$ubbt_lang['ILL_PASS']  = 	"La contrase&ntilde;a tiene letras no permitidas.  Se ruega usar &uacute;nicamente letras normales (simbolos, acentos, y dem&aacute;s no).";
+$ubbt_lang['WEL_MSG']  = 	"&iexcl;Bienvenido a nuestros foros!  Se ruega tomarse un momento para editar tu perfil de modo que otros puedan conocerte mejor y poder disfrutar de todas las opciones.";
+$ubbt_lang['PASS_SUB']  = 	"Tu contrase&ntilde;a para";
+$ubbt_lang['PASS_BODY1']  = 	"Alguien con la direcci&oacute;n IP";
+$ubbt_lang['PASS_BODY2']  = 	"registr&oacute; el Nombre de Miembro.";
+$ubbt_lang['PASS_BODY3']  = 	"Seguramente eres t&uacute; esa persona, la contrase&ntilde;a para acceder a los tableros es ";
+$ubbt_lang['NEW_CONFIRM']  = 	"Ya tienes el Nombre reservado para ser registrado...";
+$ubbt_lang['NEW_BODY']  = 	"En estos momentos el nombre (apodo) que has escogido est&aacute; siendo reservado.<br>En breves segundos recibir&aacute;s un mensaje electr&oacute;nico con tu contrase&ntilde;a.  Ya puedes abrir tu programa de correo electr&oacute;nico (email) y buscar el mensaje que te hemos enviado mientras le&iacute;s esto.  En el email ver&aacute;s una contrase&ntilde;a con la que podr&aacute;s identificarte en el foro usando el nombre que acabas de registrar.<br><br>Aqu&iacute; te espero...  :-)";
+?>
