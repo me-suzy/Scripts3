34bdb48da83c96c76897c9819329f30783224a09
me-suzy <me.suzana@gmail.com>
2025-08-14 02:40:49 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/search.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/search.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['SEARCH_INFO']  = 	"Compila le informazioni qui sotto per ricercare attraverso i forum.";
+$ubbt_lang['SEARCH_FORUM']  = 	"Forum da cercare";
+$ubbt_lang['ALL_FORUMS']  = 	"Tutti i Forum";
+$ubbt_lang['SEARCH_WORDS']  = 	"Parole da cercare";
+$ubbt_lang['SEARCH_OPT']  = 	"Opzioni di Ricerca";
+$ubbt_lang['SEARCH_ENT']  = 	"Frase Intera";
+$ubbt_lang['SEARCH_AND']  = 	"E";
+$ubbt_lang['SEARCH_OR']  = 	"O";
+$ubbt_lang['SEARCH_USER']  = 	"Per Nome Utente";
+$ubbt_lang['SEARCH_DATE']  = 	"Spazio di Tempo";
+$ubbt_lang['SEARCH_1DAY']  = 	"Più recente di 1 giorno";
+$ubbt_lang['SEARCH_2DAY']  = 	"Più recente di 2 giorni";
+$ubbt_lang['SEARCH_1WEEK']  = 	"Più recente di 1 settimana";
+$ubbt_lang['SEARCH_2WEEK']  = 	"Più recente di 2 settimane";
+$ubbt_lang['SEARCH_3WEEK']  = 	"Più recente di 3 settimane";
+$ubbt_lang['SEARCH_1MONTH']  = 	"Più recente di 1 mese";
+$ubbt_lang['SEARCH_3MONTH']  = 	"Più recente di 3 mesi";
+$ubbt_lang['SEARCH_6MONTH']  = 	"Più recente di 6 mesi";
+$ubbt_lang['SEARCH_1YEAR']  = 	"Più recente di 1 anno";
+$ubbt_lang['SEARCH_ALLPOST']  = 	"Tutti i messaggi";
+$ubbt_lang['SEARCH_NUM']  = 	"Numero di risultati da mostrare per pagina ( Questo è ignorato quando si effettua la ricerca su tutti i forum)";
+?>
