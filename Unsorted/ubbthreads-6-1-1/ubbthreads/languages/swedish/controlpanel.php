c44ac2c3cd3e2cfae836daf64e151d453516d9cc
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:39 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/controlpanel.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/controlpanel.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['SHOW_ALL']     =  "...visa alla";
+$ubbt_lang['PERSONAL']     =  "Personlig information, e-postadress, lösenord, osv.";
+$ubbt_lang['DISPLAY']      =  "Visningsinställningar, antal visade trådar, språk, färger osv.";
+$ubbt_lang['SUB_UN']       =  "Prenumeration av inlägg på forum till din e-postadress, ändra meddelandenotifiering osv.";
+$ubbt_lang['YOUR_VIEW']    =  "Forumanvändare ser dig som:";
+$ubbt_lang['YOUR_VIEW_WITH']  =  "med";
+$ubbt_lang['VIEW_POSTS']      =  "inlägg";
+$ubbt_lang['TEXT_TOTAL']      =  "Totalt";
+$ubbt_lang['TEXT_UNREAD']     =  "Olästa";
+$ubbt_lang['EDIT_ICON']       =  "Ändra";
+$ubbt_lang['WEL_CONTROL']  =  "Kontrollpanel för ";
+$ubbt_lang['MAIN_CONFIG']  =  "Huvudinställningar";
+$ubbt_lang['EDIT_TEXT']    =  "Ändra";
+$ubbt_lang['PRIVATE_BOX']    =  "Brevlåda";
+$ubbt_lang['FRIENDS_ON']     =  "Inloggade vänner";
+$ubbt_lang['FROM_ADDR']      =  "(från adressboken)";
+$ubbt_lang['FAV_THREADS']    =  "Favorittrådar";
+$ubbt_lang['TEXT_REMOVE']    =  "Ta&nbsp;bort";
+$ubbt_lang['REM_THREADS']    =  "Inläggspåminnelser";
+$ubbt_lang['SEND_A_PM']      =  "Skicka ett meddelande";
+$ubbt_lang['FAV_FORUMS']     =  "Favoritforum";
+$ubbt_lang['BY_TEXT']        =  "av";
+?>
