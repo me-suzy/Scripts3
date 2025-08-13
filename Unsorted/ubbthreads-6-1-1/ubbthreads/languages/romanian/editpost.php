1f62e68b688be086b3ae12a87fe3e41f86c567b2
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:32 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  = 	"Muta mesajul impreuna cu toate raspunsurile";
+$ubbt_lang['DELETE_THREAD']  = 	"Sterge acest thread";
+$ubbt_lang['NO_EDIT']  = 	"Nu putem verifica drepturile dvs. pentru a edita acest post.";
+$ubbt_lang['EDITTIME']  = 	"Acest mesaj nu poate fi modificat pentru ca timpul maxim permis pentru editare a expirat";
+$ubbt_lang['PEDIT_HEAD']  = 	"Editeaza mesajul";
+$ubbt_lang['PEDIT_BODY']  = 	"Puteti edita orice in cadrul post-ului, sau il puteti sterge. Daca il stergeti si exista raspunsuri la post-ul respectiv, subiectul va fi schimbat in \"Acest post a fost sters\" (pentru a pastra intact formatul thread-ului).";
+$ubbt_lang['EDIT_POLL']  = 	"Acest post contine un sondaj si nu poate fi editat (poate fi doar sters).";
+$ubbt_lang['POST_ICON']  = 	"Icon pentru mesaj";
+$ubbt_lang['MARK_EDIT']  = 	"Marchez ca Editat?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Modifica";
+$ubbt_lang['PEDIT_DELETE']  = 	"Sterge";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Aproba";
+$ubbt_lang['RET_NORM']  = 	"Intoarcere la data normala";
+$ubbt_lang['MAKE_STICKY']  = 	"Fixeaza";
+$ubbt_lang['MANAGE_THREAD']  = 	"Administrare thread";
+$ubbt_lang['OPEN_THREAD']  = 	"Deschide";
+$ubbt_lang['CLOSE_THREAD']  = 	"Inchide";
+$ubbt_lang['UNKEEP_THREAD']  = 	"Deselecteaza";
+$ubbt_lang['KEEP_THREAD']  = 	"Pastreaza";
+$ubbt_lang['MOVE_THREAD']  = 	"Muta";
+?>
