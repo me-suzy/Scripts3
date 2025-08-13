62566e1ed640dd08c0e0af4b0f50f9a2c2f3fa22
me-suzy <me.suzana@gmail.com>
2025-08-14 00:33:28 +0300

    Add Unsorted/PHPCheckout/phpcheckout/os.php

---
+++ Unsorted/PHPCheckout/phpcheckout/os.php
@@ -0,0 +1,17 @@
+		<SELECT NAME="os" SIZE=1>
+				<OPTION VALUE="<?php echo $os;?>" SELECTED><?php echo $os;?></option>
+<!-- edit any option below this line - delete, add or change -->				
+				<OPTION VALUE="Linux">Linux</OPTION>				
+				<OPTION VALUE="MacIntosh">MacIntosh</OPTION>
+				<OPTION VALUE="OS/2">OS/2</OPTION>								
+				<OPTION VALUE="Unix">Unix</OPTION>
+				<OPTION VALUE="Web Server">Web Server</OPTION>
+				<OPTION VALUE="Windows 3x">Windows 3x</OPTION>				
+				<OPTION VALUE="Windows 95">Windows 95</OPTION>
+				<OPTION VALUE="Windows 98">Windows 98</OPTION>					
+				<OPTION VALUE="Windows 2000">Windows 2000</OPTION>
+				<OPTION VALUE="Windows X">Windows X</OPTION>								
+				<OPTION VALUE="Windows NT">Windows NT</OPTION>
+				<OPTION VALUE="Other">Other</OPTION>
+				<OPTION VALUE="none">none</OPTION>				
+		</SELECT>
