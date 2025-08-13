f7ead13179d5522e426f5be2d0f54574e3598fc1
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:41 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"Voor deze gebruikersnaam kon geen profiel worden gevonden";
+$ubbt_lang['EMAIL_HEAD']  = 	"Pas email notificaties aan voor";
+$ubbt_lang['PROF_EDIT']  = 	"Onderstaande informatie kan nu aangepast worden.";
+$ubbt_lang['PROF_SUBS']  = 	"Email notificaties / aanmeldingen";
+$ubbt_lang['PROF_NOTIF']  = 	"Wil je een e-mail notificatie ontvangen als iemand je een privébericht heeft gestuurd?";
+$ubbt_lang['PROF_REPLIES']  = 	"Wil je alle antwoorden op berichten door jou geplaatst, via email ontvangen?";
+$ubbt_lang['SUB_STAT']  = 	"Notificatiestatus voor onderstaande forums.  (Voor ieder aangevinkt forum ontvang je dagelijks een email met daarin een samenvatting van nieuwe berichten.)";
+$ubbt_lang['SUB_SHOW']  = 	"(A - <u>A</u>anschakelen ; U - <u>U</u>itschakelen)";
+$ubbt_lang['TEXT_S']  = 	"A";
+$ubbt_lang['TEXT_U']  = 	"U";
+?>
