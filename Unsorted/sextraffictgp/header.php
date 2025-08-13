4a1df29bbbcbdb15135d7b0d5dce5ba5ac9c35cc
me-suzy <me.suzana@gmail.com>
2025-08-14 01:37:44 +0300

    Add Unsorted/sextraffictgp/header.php

---
+++ Unsorted/sextraffictgp/header.php
@@ -0,0 +1,20 @@
+<?php
+include("stconfig.php");
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE><?=$sitetitle?></TITLE>
+<META NAME="Generator" CONTENT="SexTraffic.net">
+<META NAME="Keywords" CONTENT="<?=$keywords?>">
+<META NAME="Description" CONTENT="<?=$content?>">
+</HEAD>
+<body bgcolor="<?=$background?>" text="<?=$text?>" leftmargin="0" bottomMargin="0" rightMargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<style type=text/css>
+<!--
+A:link{text-decoration: none; color: <?=$linkcolor?>;}
+A:visited{text-decoration: none; color: <?=$linkcolor?>;}
+A:active{text-decoration: none; color: <?=$linkcolor2?>;}
+A:hover{text-decoration: none; color: <?=$linkcolor2?>;}
+-->
+</style>
