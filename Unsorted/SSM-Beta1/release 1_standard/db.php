ebb5fa5a011edf8a2458536e0891f7337736b3f6
me-suzy <me.suzana@gmail.com>
2025-08-14 01:49:17 +0300

    Add Unsorted/SSM-Beta1/release 1_standard/db.php

---
+++ Unsorted/SSM-Beta1/release 1_standard/db.php
@@ -0,0 +1,10 @@
+<?php
+//THIS FILE IS WRITTEN BY THE INSTALLATION WIZARD
+//DO NOT EDIT
+//	database variables
+$setting["dbuser"]="root";
+$setting["dbpass"]="";
+$setting["dbhost"]="localhost";
+$setting["dbselect"]="installtest";
+$table_pre="pre_";
+?>
