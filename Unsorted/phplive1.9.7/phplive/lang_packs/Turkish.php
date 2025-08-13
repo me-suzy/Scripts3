7b5d7ce52c803cbc96d542e03cea4f4f429ff476
me-suzy <me.suzana@gmail.com>
2025-08-14 00:48:44 +0300

    Add Unsorted/phplive1.9.7/phplive/lang_packs/Turkish.php

---
+++ Unsorted/phplive1.9.7/phplive/lang_packs/Turkish.php
@@ -0,0 +1,84 @@
+<?
+$LANG = ARRAY() ;		// MUST initialize, DO NOT CHANGE
+/*******************************************************************
+* Language: Turkish
+* Contributed by: Guvenc Kaplan
+*	Name: Nate
+*	Company: OSI Codes
+*	URL: http://www.osicodes.com
+*
+* ------------------------------------------------------------------
+* DO NOT REMOVE COMMENTS.  JUST MODIFY THE ACTUAL TEXT.
+* DO NOT REMOVE THE orig TEXT!  THEY ARE THERE FOR REFERENCE.
+* ------------------------------------------------------------------
+* NOTE: If you are going to submit a language pack, please keep
+* everything basic and do not mention any company name, personal name,
+* copyrighted name, or such.  Keep all translations generic, simple
+* and catered to all audiences and companies.  Basically, your
+* translated language pack should be ready to use without being touched.
+*******************************************************************/
+
+
+
+/***************** ONLY MAKE THE CHANGE BELOW ******************/
+
+
+/*** branded string located troughout 99% of the pages */
+//orig: PHP <i>Live!</i> &copy; 2001 OSI Codes, New York, New York
+$LANG['DEFAULT_BRANDING'] = "<a href='http://www.PHPLiveSupport.com' target='new'>PHP <i>Live!</i> katkýlarýyla</a>" ;
+
+/*** chat page titles */
+//orig: Request Live! Support Request
+$LANG['TITLE_SUPPORTREQUEST'] = "Canlý destek talep et" ;
+//orig: You are now speaking with a Live! Support
+$LANG['TITLE_SUPPORCHAT'] = "Þu anda canlý olarak destek hattýmýzla görüþüyorsunuz" ;
+//orig: Please leave a message
+$LANG['TITLE_LEAVEMESSAGE'] = "Lütfen mesaj býrakýn" ;
+//orig: Printer Friendly View
+$LANG['TITLE_PRINTVIEW'] = "Yazýcý dostu görünüm" ;
+
+$LANG['WORD_BACK'] = "Geri" ;
+$LANG['WORD_CLOSE'] = "Kapat" ;
+$LANG['WORD_NAME'] = "Ýsim" ;
+$LANG['WORD_EMAIL'] = "Email" ;
+$LANG['WORD_SUBJECT'] = "Konu" ;
+$LANG['WORD_MESSAGE'] = "Mesaj" ;
+$LANG['WORD_SEND'] = "Gönder" ;
+$LANG['WORD_COMPANY'] = "Þirket" ;
+$LANG['WORD_DEPARTMENT'] = "Departman" ;
+$LANG['WORD_DAY'] = "Gün" ;
+
+//orig: Welcome to our Live! support help desk.
+$LANG['CHAT_REQUEST_TITLE'] = "Canlý destek hattýmýza hoþgeldiniz!" ;
+//orig: Please select the department you would like to reach.
+$LANG['CHAT_REQUEST_SELECTDEPT'] = "Ulaþmak istediðiniz departmaný seçiniz." ;
+//orig: Input your Screen Name used during this session:
+$LANG['CHAT_REQUEST_INPUTSCREENNAME'] = "Bu oturumda ekranda gözükmesini istediðiniz isminizi giriniz." ;
+//orig: Welcome %%user%%! Please hold while we contact a representative. If a representative does not respond in few seconds, then he/she is not available at this time.
+$LANG['CHAT_GREETING'] = "%%user%%, hoþgeldiniz! Lütfen bir destek hattý temsilcisine ulaþýrken bekleyiniz. Eðer birkaç saniye içinde cevap alamýyorsanýz, temsilcimiz þu anda ulaþýlamýyor demektir." ;
+//orig: Printer Friendly
+$LANG['CHAT_PRINTER_FRIENDLY'] = "Yazýcý Dostu" ;
+//orig: Your party has left this session.
+$LANG['CHAT_PARTYLEFTSESSION'] = "Þahýs oturumu terketti." ;
+//orig: You are now speaking with
+$LANG['CHAT_REQUEST_NOWSPEAKINGWITH'] = "Þu anda konuþtuðunuz kiþi: " ;
+//orig: Department is currently unavailable. Please leave a message.
+$LANG['MESSAGE_BOX_MESSAGE'] = "Departman þu anda ulaþýlabilir deðil. Lütfen mesaj býrakýn." ;
+//orig: Thank you.  Your message has been sent to:
+$LANG['MESSAGE_BOX_SENT'] = "Teþekkürler. Mesajýnýz gönderildi: " ;
+//orig: Items with (*) must be filled.
+$LANG['MESSAGE_BOX_JS_A_ALLFIELDSSUP'] = "(*) iþaretli alanlarýn doldurulmasý zorunludur." ;
+//orig: Department not available.  Please leave a message.
+$LANG['CHAT_DEPT_NOTAVAIL'] = "Departmana ulaþýlamýyor. Lütfen mesaj býrakýn." ;
+//orig: Email is invalid format (example: someone@somewhere.com)
+$LANG['MESSAGE_BOX_JS_A_INVALIDEMAIL'] = "Email is invalid format (example: someone@somewhere.com)" ;
+//orig: Session did not create.  Request did not send.
+$LANG['CHAT_NOTCREATE'] = "Oturum açýlamadý. Talebiniz gönderilemedi." ;
+
+//orig: Please hold while being transferred to
+$LANG['CHAT_CALL_TRANSFER'] = "Lütfen transfer olurken bekleyin: " ;
+//orig: What is your question?
+$LANG['CHAT_REQUEST_QUESTION'] = "Sorunuz nedir?" ;
+//orig: A copy of the chat transcript has been sent to:
+$LANG['CHAT_TRANSCRIPT_SENT'] = "A copy of the chat transcript has been sent to:" ;
+?>
