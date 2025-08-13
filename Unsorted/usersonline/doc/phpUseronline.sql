5182aab0adbe959b9e472afcd20fdb0e05afc6e1
me-suzy <me.suzana@gmail.com>
2025-08-14 02:59:21 +0300

    Add Unsorted/usersonline/doc/phpUseronline.sql

---
+++ Unsorted/usersonline/doc/phpUseronline.sql
@@ -0,0 +1,12 @@
+#
+# Table structure for table 'useronline'
+#
+
+CREATE TABLE useronline (
+   timestamp int(15) DEFAULT '0' NOT NULL,
+   ip varchar(40) NOT NULL,
+   file varchar(100) NOT NULL,
+   PRIMARY KEY (timestamp),
+   KEY ip (ip),
+   KEY file (file)
+);
