13b18b8197a93c6e1b0b638f991d2229b381c74c
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:32 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/dopoll.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/dopoll.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NO_VOTE']  = 	"Vous n'avez voté pour aucune des propositions !";
+$ubbt_lang['POLL_REGED']  = 	"Seul les membres identifiés peuvent voter.";
+$ubbt_lang['POLL_IP']  = 	"Nous avons déjà un vote enregistré dans ce sondage provenant de l'adresse IP.";
+$ubbt_lang['POLL_USER']  = 	"Nous avons déjà un vote enregistré dans ce sondage pour ce nom.";
+?>
