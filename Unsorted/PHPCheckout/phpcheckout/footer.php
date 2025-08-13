037f700c38b96f2e0b90900c6e9d348a12708a0f
me-suzy <me.suzana@gmail.com>
2025-08-14 00:31:41 +0300

    Add Unsorted/PHPCheckout/phpcheckout/footer.php

---
+++ Unsorted/PHPCheckout/phpcheckout/footer.php
@@ -0,0 +1,53 @@
+<?php echo"\n\n\n";?>
+<!-- start of footer.php -->
+<table class="favcolor2" width="100%" border=0 cellpadding=0 cellspacing=0>
+
+
+	
+
+		<!-- row 1 -->
+	<tr class="favcolor2" width="100%">
+		<td colspan=3 align="center">
+			<p><?php include("navControls.php");?></p>
+      </td>											
+	</tr>
+
+
+
+
+	<!-- row 2 -->
+	<tr width="100%">
+      <td colspan=3>
+
+<!-- this table has 3 cells, 1 for each of the two flags and 1 for the nav -->
+<table width="100%" class="headerColor">
+	<tr>
+		<td valign="middle" align="center">
+			<img src="appimage/flags/flagUSA97x50.png" width="97" height="50" border=0 alt="Made in North America">
+		</td>
+      <td width="550" align="center">
+			<img src="appimage/bowFalls550x60S2TB.png" width="550" height="63" border=0 alt="DreamRiver Software Powers the NetTM">
+		</td>
+		<td valign="middle" align="center">
+			<img src="appimage/flags/flagCDN97x50.png" width="97" height="50" border=0 alt="Made in North America">
+		</td>
+	</tr>
+</table>
+
+		
+		</td>
+	</tr>
+
+	
+
+	<!-- row 3 -->
+	<tr class="favcolor" width="100%">
+		<td colspan=3>
+         <span class="sitedesign" style="text-align : center;">All Pages &copy;Copyright 2002 <?php echo ORGANIZATION;?>. All Rights Reserved. <a href="privacyPolicy.php">Privacy Policy.</a></span>
+			<div class="sitedesign">Powered by phpCheckout<sup>TM</sup> from <a href="http://www.dreamriver.com/">DreamRiver.com</a></div>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
+<!-- end of footer.php -->
