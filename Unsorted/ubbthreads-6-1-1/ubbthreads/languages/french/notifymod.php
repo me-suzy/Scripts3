a784e09a67fa9990a1862db26fdff724d027f65b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:05 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/notifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/notifymod.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NOTIFY_MOD']  =  	"Informer le moderateur";
+$ubbt_lang['NOTIFY_HEAD']  = 	"Cette action va informer le(s) moderateur(s) de ce forum de consulter ce message.";
+$ubbt_lang['SEND_IT']  =  "Oui, informer le(s) moderateur(s) de ce message !";
+$ubbt_lang['NO_POST']  = 	"Nous avons rencontré un problème lors de la recherche de ce message dans notre base de données.";
+?>
