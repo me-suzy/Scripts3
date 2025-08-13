fe61904915e25ab1468525c81454a44b5fa812fb
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:51 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/mailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/mailthread.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['MAIL_FRIEND']  = 	"Email dit onderwerp naar een vriend";
+$ubbt_lang['FRIEND_HEAD']  = 	"Vul het email address van de persoon die je dit bericht wilt sturen in.";
+$ubbt_lang['SENT_FROM1']  = 	"Deze e-mail wordt verstuurd vanuit het e-mail adres waarmee je geregistreerd hebt";
+$ubbt_lang['SENT_FROM2']  = 	"en wordt voorzien van jouw gebruikersnaam";
+$ubbt_lang['USE_REAL']  = 	"Als je liever jouw echte naam in wilt vullen, doe je dat hier:";
+$ubbt_lang['WHAT_EMAIL']  = 	"Email adres van de ontvanger:";
+$ubbt_lang['SEND_IT']  = 	"Versturen!";
+?>
