5b2213738d4a3c9325746cd70e6aef4a1646de64
me-suzy <me.suzana@gmail.com>
2025-08-14 02:48:20 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/notifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/notifymod.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NOTIFY_MOD']  =  	"Notify moderator";
+$ubbt_lang['NOTIFY_HEAD']  = 	"This action will notify the moderator(s) of this board to take a look at this post.";
+$ubbt_lang['SEND_IT']  =  "Yes, notify moderator(s) of this post!";
+$ubbt_lang['NO_POST']  = 	"There was a problem looking up this post in our database.";
+?>
