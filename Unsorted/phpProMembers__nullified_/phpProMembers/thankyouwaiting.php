605eed7a6931369828b739e4ad36b61ebc9a466b
me-suzy <me.suzana@gmail.com>
2025-08-14 00:58:00 +0300

    Add Unsorted/phpProMembers__nullified_/phpProMembers/thankyouwaiting.php

---
+++ Unsorted/phpProMembers__nullified_/phpProMembers/thankyouwaiting.php
@@ -0,0 +1,31 @@
+<?php
+/*********************************************************************/
+/* Program Name         : phpProMembers                              */
+/* Home Page            : http://www.gacybertech.com                 */
+/* Retail Price         : $149.99 United States Dollars              */
+/* WebForum Price       : $000.00 Always 100% Free                   */
+/* xCGI Price           : $000.00 Always 100% Free                   */
+/* Supplied by          : South [WTN]                                */
+/* Nullified by         : CyKuH [WTN]                                */
+/* Distribution         : via WebForum and Forums File Dumps         */
+/*********************************************************************/
+$page_account = "form";
+	require "include.php";
+	if ($include_template == "1") {
+		include "$template_directory/header.php";	
+	}
+?>	
+<center>
+<FONT color="#000000" size="2" face="Arial, Helvetica, sans-serif">
+<center><h3>Thank You For Purchasing A Subscription From 
+
+<?php echo $site_name ?>.  Your account is under review and you will be notified when you are
+able to log in to your new account.</h3></center>
+</FONT>
+</center>
+<?php
+// Adding footer if we got it.
+if ($include_template == "1") {
+	include "$template_directory/footer.php";	
+}
+?>	
