5213d259ccd674d97002c65432651e30a371959d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:29 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/togglesub.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/togglesub.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['UN_HEADER']  = 	"Vous avez été des-inscrit";
+$ubbt_lang['HEADER']  = 	"Vous avez été inscrit";
+$ubbt_lang['UN_BODY']  = 	"Vous avez été des-inscrit du forum : ";
+$ubbt_lang['BODY']  =          "Vous avez été inscrit au forum : ";
+?>
