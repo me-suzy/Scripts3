d988a4fa5745a426e185a030eddfbcc918707f3b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:26 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/search.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/search.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['SEARCH_INFO']  = 	"Fyll i fälten nedan för att söka i forumen.";
+$ubbt_lang['SEARCH_FORUM']  = 	"Forum att söka i";
+$ubbt_lang['ALL_FORUMS']  = 	"Alla forum";
+$ubbt_lang['SEARCH_WORDS']  = 	"Ord att söka efter";
+$ubbt_lang['SEARCH_OPT']  = 	"Sökalternativ";
+$ubbt_lang['SEARCH_ENT']  = 	"Hela frasen";
+$ubbt_lang['SEARCH_AND']  = 	"och";
+$ubbt_lang['SEARCH_OR']  = 	"eller";
+$ubbt_lang['SEARCH_USER']  = 	"Av användare";
+$ubbt_lang['SEARCH_DATE']  = 	"Datumintervall";
+$ubbt_lang['SEARCH_1DAY']  = 	"Nyare än 1 dag";
+$ubbt_lang['SEARCH_2DAY']  = 	"Nyare än 2 dagar";
+$ubbt_lang['SEARCH_1WEEK']  = 	"Nyare än 1 vecka";
+$ubbt_lang['SEARCH_2WEEK']  = 	"Nyare än 2 veckor";
+$ubbt_lang['SEARCH_3WEEK']  = 	"Nyare än 3 veckor";
+$ubbt_lang['SEARCH_1MONTH']  = 	"Nyare än 1 månad";
+$ubbt_lang['SEARCH_3MONTH']  = 	"Nyare än 2 månader";
+$ubbt_lang['SEARCH_6MONTH']  = 	"Nyare än 6 månader";
+$ubbt_lang['SEARCH_1YEAR']  = 	"Nyare än 1 år";
+$ubbt_lang['SEARCH_ALLPOST']  = 	"Alla inlägg";
+$ubbt_lang['SEARCH_NUM']  = 	"Antal träffar att visa per sida.";
+?>
