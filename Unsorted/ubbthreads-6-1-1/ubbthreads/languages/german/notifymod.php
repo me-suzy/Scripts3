4a87823388061b204313ae716029401d6eb284f9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:38 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/notifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/notifymod.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NOTIFY_MOD']  =  	"Moderator benachrichtigen";
+$ubbt_lang['NOTIFY_HEAD']  = 	"Dies wird den Moderator dieses Forums informieren, einen Blick auf die Nachricht zu werfen und sie gegebenenfalls zu l&ouml;schen.";
+$ubbt_lang['SEND_IT']  =  "Ja, Moderator informieren!";
+$ubbt_lang['NO_POST']  = 	"Es gab ein Problem beim Auffinden der Nachricht in der Datenbank.";
+?>
