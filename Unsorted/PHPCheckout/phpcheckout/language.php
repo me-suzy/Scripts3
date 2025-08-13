acef90c59d0d4532295b7cb1597cdb5d8c4adb05
me-suzy <me.suzana@gmail.com>
2025-08-14 00:32:31 +0300

    Add Unsorted/PHPCheckout/phpcheckout/language.php

---
+++ Unsorted/PHPCheckout/phpcheckout/language.php
@@ -0,0 +1,16 @@
+				
+<SELECT NAME="language" SIZE=1>
+	<OPTION VALUE="<?php echo $language;?>" SELECTED><?php echo $language;?></OPTION>
+	<!-- edit any option below this line - delete, add or change -->							
+	<OPTION VALUE="asp">ASP</OPTION>
+	<OPTION VALUE="Cold Fusion">Cold Fusion</OPTION>
+	<OPTION VALUE="c++">c++</OPTION>
+	<OPTION VALUE="Perl">Perl</OPTION>				
+	<OPTION VALUE="PHP">PHP</OPTION>
+	<OPTION VALUE="PL1/SQL">PL1/SQL</OPTION>
+	<OPTION VALUE="Java">Java</OPTION>
+	<OPTION VALUE="vb">Visual Basic</OPTION>					
+	<OPTION VALUE="vbscript">VBScript</OPTION>				
+	<OPTION VALUE="other">other</OPTION>
+	<OPTION VALUE="none">none</OPTION>
+</SELECT>
