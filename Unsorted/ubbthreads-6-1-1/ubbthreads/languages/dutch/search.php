879c73212b7babcbca618405e06e4def70307142
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:08 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/search.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/search.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['SEARCH_INFO']  = 	"Vul onderstaande velden in om een zoekopdracht uit te voeren.";
+$ubbt_lang['SEARCH_FORUM']  = 	"Forum of categorie waarin je wilt zoeken:";
+$ubbt_lang['ALL_FORUMS']  = 	"Alle forums";
+$ubbt_lang['SEARCH_WORDS']  = 	"Woord(en) waarnaar je wilt zoeken:";
+$ubbt_lang['SEARCH_OPT']  = 	"Zoek opties:";
+$ubbt_lang['SEARCH_ENT']  = 	"Volledige frase";
+$ubbt_lang['SEARCH_AND']  = 	"En";
+$ubbt_lang['SEARCH_OR']  = 	"Of";
+$ubbt_lang['SEARCH_USER']  = 	"Op gebruikersnaam";
+$ubbt_lang['SEARCH_DATE']  = 	"Datum van het bericht:";
+$ubbt_lang['SEARCH_1DAY']  = 	"1 dag of nieuwer";
+$ubbt_lang['SEARCH_2DAY']  = 	"2 dagen of nieuwer";
+$ubbt_lang['SEARCH_1WEEK']  = 	"1 week of nieuwer";
+$ubbt_lang['SEARCH_2WEEK']  = 	"2 weken of nieuwer";
+$ubbt_lang['SEARCH_3WEEK']  = 	"3 weken of nieuwer";
+$ubbt_lang['SEARCH_1MONTH']  = 	"1 maand of nieuwer";
+$ubbt_lang['SEARCH_3MONTH']  = 	"3 maanden of nieuwer";
+$ubbt_lang['SEARCH_6MONTH']  = 	"6 maanden of nieuwer";
+$ubbt_lang['SEARCH_1YEAR']  = 	"1 jaar of nieuwer";
+$ubbt_lang['SEARCH_ALLPOST']  = 	"Alle berichten";
+$ubbt_lang['SEARCH_NUM']  = 	"Gevonden resultaten per pagina:";
+?>
