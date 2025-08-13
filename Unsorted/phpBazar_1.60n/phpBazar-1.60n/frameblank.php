e426be483d878a0d07aa770c890ea41960730869
me-suzy <me.suzana@gmail.com>
2025-08-14 00:23:41 +0300

    Add Unsorted/phpBazar_1.60n/phpBazar-1.60n/frameblank.php

---
+++ Unsorted/phpBazar_1.60n/phpBazar-1.60n/frameblank.php
@@ -0,0 +1,54 @@
+<?
+
+#################################################################################################
+#
+#  project           	: phpBazar
+#  filename          	: frameblank.php
+#  last modified by  	: Erich Fuchs
+#  supplied by          : Reseting
+#  nullified by	        : CyKuH [WTN]
+#  purpose           	:
+#
+#################################################################################################
+
+
+
+
+
+#  Include Configs & Variables
+
+#################################################################################################
+
+require ("config.php");
+
+
+
+#  Main
+
+#################################################################################################
+
+echo"
+
+<html>
+
+ <head>
+
+ <title></title>
+
+ <link rel=\"stylesheet\" type=\"text/css\" href=\"$STYLE\">
+
+ <meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\">
+
+ <meta name=\"generator\" content=\"Manual\">
+
+ </head>
+
+";
+
+
+
+#  End
+
+#################################################################################################
+
+?>
