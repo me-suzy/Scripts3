a49135728252e8068081e31c088a6e6b15bfb2ce
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:36 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"Ogiltigt antal inlägg per sida.";
+$ubbt_lang['FLAT_BAD']  = 	"Antal inlägg per sida vid platt visning måste vara mellan 1 och 99.";
+$ubbt_lang['TEXTAREA_NO']  = 	"Ogiltigt textvärde.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Visningsinställningar ändrade.";
+?>
