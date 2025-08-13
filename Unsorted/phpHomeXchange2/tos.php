7e36d966e7a8528f1fabfe52fc5fca85c324e5cf
me-suzy <me.suzana@gmail.com>
2025-08-14 00:40:17 +0300

    Add Unsorted/phpHomeXchange2/tos.php

---
+++ Unsorted/phpHomeXchange2/tos.php
@@ -0,0 +1,8 @@
+<?php include "header.inc"; ?>
+ 
+
+it is VERY important you create a TOS [Terms Of Service] and put a link to it on the sign up page and even every page!
+
+
+
+<?php include "footer.inc"; ?>
