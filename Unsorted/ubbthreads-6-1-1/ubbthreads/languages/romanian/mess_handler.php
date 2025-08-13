3665573e47d428250e3ab36d3c04c8ee2cf136f9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:42 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/mess_handler.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/mess_handler.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['REPLY_HEAD']  = 	"Foloseste formularul pentru a trimite un mesaj privat acestui utilizator";
+$ubbt_lang['MESS_COPY']  = 	"Vreau sa pastrez o copie a acestui mesaj";
+$ubbt_lang['DELETE_MESS']  = 	"Sterge";
+$ubbt_lang['REPLY_MESS']  = 	"Raspunde";
+$ubbt_lang['RETURN_START']  = 	"Pagina anterioara";
+$ubbt_lang['MESS_RECEIPT']  = 	"Vreau sa primesc o confirmare de citire";
+$ubbt_lang['MESS_PREVIEW']  = 	"Vreau sa previzualizez measajul";
+$ubbt_lang['ORIG_MESS']  = 	"Mesajul original";
+?>
