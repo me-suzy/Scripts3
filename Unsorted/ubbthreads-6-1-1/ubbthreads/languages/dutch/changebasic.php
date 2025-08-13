747800162e88fec88e3ea09339d748ee7eabfe02
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:15 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/changebasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/changebasic.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['PIXEL_TOO_BIG']="Jouw afbeelding is groter dan ({$theme['PictureWidth']}x{$theme['PictureHeight']}) pixels. De limiet is";
+$ubbt_lang['FILE_TOO_BIG']="Afbeeldingen mogen niet groter zijn dan {$config['avatarsize']} bytes.";
+$ubbt_lang['PASS_MATCH']  = 	"De paswoorden komen niet overeen.";
+$ubbt_lang['BAD_PIC']  = 	"Onjuist bestandstype.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Jouw wachtwoord moet tussen 4 en 20 tekens lang zijn.";
+$ubbt_lang['HOME_TOO_LONG']  = 	"Homepage te lang.";
+$ubbt_lang['OCC_TOO_LONG']  = 	"Beroep te lang.";
+$ubbt_lang['HOBB_TOO_LONG']  = 	"Hobby's te lang.";
+$ubbt_lang['LOC_TOO_LONG']  = 	"Woonplaats te lang.";
+$ubbt_lang['SIG_TOO_LONG']  = 	"Handtekening te lang.";
+$ubbt_lang['BIO_TOO_LONG']  = 	"'over jezelf' te lang.";
+$ubbt_lang['BASIC_MOD']  = 	"Jouw basisprofiel is nu aangepast.";
+?>
