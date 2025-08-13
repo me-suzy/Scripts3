c6fd02b66d0b54300e5eb835cce048987e457f8c
me-suzy <me.suzana@gmail.com>
2025-08-14 02:31:07 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"Vi kan desværre ikke finde en profil for det angivne brugernavn";
+$ubbt_lang['EMAIL_HEAD']  = 	"E-breve-indstillinger for";
+$ubbt_lang['PROF_EDIT']  = 	"Nedenstående indstillinger kan ændres.";
+$ubbt_lang['PROF_SUBS']  = 	"E-breve-indstillinger/abonnement";
+$ubbt_lang['PROF_NOTIF']  = 	"Ønsker du besked pr. e-post, når du modtager private beskeder?";
+$ubbt_lang['PROF_REPLIES']  = 	"Ønsker du at få alle svar på dine indlæg sendt til dig i et e-brev?";
+$ubbt_lang['SUB_STAT']  = 	"Abonnementsstatus for følgende fora. (Modtag dagligt sammendrag af alle indlæg til forumet.)";
+$ubbt_lang['SUB_SHOW']  = 	"(A = Abonner, AA = Afbryd abonnement)";
+$ubbt_lang['TEXT_S']  = 	"A";
+$ubbt_lang['TEXT_U']  = 	"AA";
+?>
