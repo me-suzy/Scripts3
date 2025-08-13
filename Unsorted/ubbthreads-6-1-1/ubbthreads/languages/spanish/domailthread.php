c17b9e7387053df2ec49adca9a55332b6b72dbef
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:15 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/domailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/domailthread.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['START']  = 	"te ha enviado un enlace a un mensaje titulado";
+$ubbt_lang['START2']  = 	"en el '{$config['title']}'! El mensaje está debajo.";
+$ubbt_lang['TO_READ']  = 	"Puedes ver este tema en la siguiente dirección"
+$ubbt_lang['NOTE_FROM']   =    "Has recibido un Aviso acerca de este mensaje por parte de "
+$ubbt_lang['POST_BELOW']  =    "El mensaje es el siguiente:";
+$ubbt_lang['EMAIL_SENT']  =    "El Email ha sido enviado";
+$ubbt_lang['EMAIL_CONFIRM']  =  "Un email con este mensaje ha sido enviado a";
+$ubbt_lang['FORUM_RETURN']   =  "Volver al foro"; 
+?>
