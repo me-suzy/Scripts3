515f374584d694edd90d8cfa59b2dbd13f919a05
me-suzy <me.suzana@gmail.com>
2025-08-14 02:23:37 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/admin/dbdooptimize.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/admin/dbdooptimize.php
@@ -0,0 +1,61 @@
+<?
+/*
+# UBB.threads, Version 6
+# Official Release Date for UBB.threads Version6: 06/05/2002
+
+# First version of UBB.threads created July 30, 1996 (by Rick Baker).
+# This entire program is copyright Infopop Corporation, 2002.
+# For more info on the UBB.threads and other Infopop
+# Products/Services, visit: http://www.infopop.com
+
+# Program Author: Rick Baker.
+
+# You may not distribute this program in any manner, modified or otherwise,
+# without the express, written written consent from Infopop Corporation.
+
+# Note: if you modify ANY code within UBB.threads, we at Infopop Corporation
+# cannot offer you support-- thus modify at your own peril :)
+# ---------------------------------------------------------------------------
+*/
+
+// Require the library
+   require ("../main.inc.php");
+
+// -----------------
+// Get the user info
+   $userob = new user;
+   $user = $userob -> authenticate();
+   $html = new html;
+
+// -----------------------------
+// Make sure they should be here
+   if ($user['U_Status'] != 'Administrator'){
+      $html -> not_right ("You must be logged in, and be a valid administrator to access this.",$Cat);
+   }
+
+// -----------------------------------------
+// Optimize the tables
+   $query = "
+    OPTIMIZE TABLE {$config['tbprefix']}Users
+   ";
+   $dbh -> do_query($query);
+   $query = "
+    OPTIMIZE TABLE {$config['tbprefix']}Posts
+   ";
+   $dbh -> do_query($query);
+   $query = "
+    OPTIMIZE TABLE {$config['tbprefix']}Messages
+   ";
+   $dbh -> do_query($query);
+
+
+// ------------------------
+// Send them a confirmation
+   $html -> send_header ("Tables Optimized",$Cat,"<META HTTP-EQUIV=\"Refresh\" content=\"5;url={$config['phpurl']}/admin/main.php?Cat=$Cat\">",$user);
+   $html -> admin_table_header("Tables Optimized");
+   echo "<TABLE BORDER=0 WIDTH=\"95%\" ALIGN=\"center\"><TR><TD class=\"cleartable\"><span class=\"onbody\">";  
+   echo " The {$config['tbprefix']}Users, {$config['tbprefix']}Messages and {$config['tbprefix']}Posts tables have been optimized. You will now be returned to the main administration page.";
+   echo "</span></TD></TR></TABLE>";
+   $html -> send_admin_footer();
+
+?>
