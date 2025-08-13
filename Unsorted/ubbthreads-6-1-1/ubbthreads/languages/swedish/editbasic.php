be20e61aa1ac1e8816ba8008c8debf095075adc1
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:56 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =      "Din bild ({$theme['PictureWidth']}x{$theme['PictureHeight']} max pixlar, kan vara mindre) (.gif, .jpg, .png tillåtna)";
+$ubbt_lang['PROF_PIC']  =       "Din bild (Komplett URL - Bildstorlek är {$theme['PictureWidth']}x{$theme['PictureHeight']} max pixlar, kan vara mindre) (.gif, .jpg, .png tillåtna)";
+$ubbt_lang['AUX_ONLINE']  =      "Vill du att andra användare ska kunna se vilka forum/inlägg du tittar på?";
+$ubbt_lang['PROF_PRIV']  = 	"Acceptera privata meddelanden från andra användare?";
+$ubbt_lang['NO_PROF']  = 	"Hittar ingen profil för det användarnamnet";
+$ubbt_lang['PROF_HEAD']  = 	"Redigera profil för";
+$ubbt_lang['PROF_EDIT']  = 	"Du kan ändra all info nedan.";
+$ubbt_lang['BASIC_PROF']  = 	"Grundprofil";
+$ubbt_lang['PROF_PASS']  = 	"Lösenord (mellan 4 och 20 tecken)";
+$ubbt_lang['VER_PASS']  = 	"Verifiera lösenordet";
+$ubbt_lang['EMAIL_ADD']  = 	"E-postadress (endast för vårt administrativa användande... kommer INTE vara synligt för andra användare)";
+$ubbt_lang['FAKE_EMAIL']  = 	"E-postadress (Synlig för alla)";
+$ubbt_lang['FULL_NAME']  = 	"Fullständigt namn";
+$ubbt_lang['PROF_SIG']  = 	"Signatur (upp till {$config['Sig_length']} tecken). Du kan använda <a href=\"{$config['phpurl']}/faq_swedish.php\" target=_new>UBBCode</a> i din signatur";
+$ubbt_lang['PROF_HOME']  = 	"Hemsida (upp till 150 tecken)";
+$ubbt_lang['PROF_OCC']  = 	"Sysselsättning";
+$ubbt_lang['PROF_HOBB']  = 	"Hobbyer (upp till 200 tecken)";
+$ubbt_lang['PROF_LOC']  = 	"Hemort (Stad, län, etc.)";
+$ubbt_lang['PROF_BIO']  = 	"Presentation (upp till 200 tecken)";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ-nummer";
+$ubbt_lang['PROF_VIS']  = 	"Vill du vara synlig på \"Inloggade\"-sidan?";
+?>
