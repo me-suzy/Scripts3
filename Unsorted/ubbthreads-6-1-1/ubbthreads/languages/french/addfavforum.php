2f86f30b2cb3c415a16a8b74f1320796b43c0689
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:11 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/addfavforum.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/addfavforum.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  =  "Vous n'êtes pas autorisé à voir ce forum donc vous ne pouvez pas l'ajouter à votre liste de favoris.";
+$ubbt_lang['DUP_ENTRY']  =  "Ce forum a déjà été ajouté à votre liste de favoris.";
+$ubbt_lang['FORUM_IN']  =  "Forum ajouté";
+$ubbt_lang['FORUM_CONFIRM']  =  "Le forum selectionné a été ajouté à votre liste de forums favoris.  Vous verrez ce forum dans 'Mon Centre' jusqu'à ce que vous l'enleviez.";
+$ubbt_lang['FORUM_RETURN']  =  "Retourner sur le forum.";
+$ubbt_lang['TOO_MANY']  =  "Vous ne pouvez avoir qu'un maximum de 83 forums dans votre liste de favoris";
+?>
