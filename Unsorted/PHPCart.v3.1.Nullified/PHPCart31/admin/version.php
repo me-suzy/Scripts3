595f79c4085c88340cdd7d972a86ae66b8d62f89
me-suzy <me.suzana@gmail.com>
2025-08-14 00:29:59 +0300

    Add Unsorted/PHPCart.v3.1.Nullified/PHPCart31/admin/version.php

---
+++ Unsorted/PHPCart.v3.1.Nullified/PHPCart31/admin/version.php
@@ -0,0 +1,16 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+// Program Name         : PHPCart                                          //
+// Release Version      : 3.1                                              //
+// Program Author       : Mr B Wiencierz                                   //
+// Home Page            : http://www.phpcart.co.uk                         //
+// Supplied by          : CyKuH [WTN]                                      //
+// Nullified by         : CyKuH [WTN]                                      //
+// Distribution         : via WebForum, ForumRU and associated file dumps  //
+//                                                                         //
+//                All code is ©2000-2004 Barry Wiencierz.                  //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
+$version = "3.1";
+?>
