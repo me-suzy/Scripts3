0a72ae16c634e1ee4bf901d241b890d05ba26006
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:29 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/domailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/domailthread.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['START']  = 	"a fais suivre un lien vers un message intitulé";
+$ubbt_lang['START2']  = 	"sur '{$config['title']}'! Le message est ci-dessous.";
+$ubbt_lang['TO_READ']  = 	"Voir ce sujet sur internet à l'adresse"
+$ubbt_lang['NOTE_FROM']   =    "Vous avez reçu une notification à propos de ce message de la part de "
+$ubbt_lang['POST_BELOW']  =    "Le message est retranscrit ci-dessous";
+$ubbt_lang['EMAIL_SENT']  =    "L'Email a été envoyé";
+$ubbt_lang['EMAIL_CONFIRM']  =  "Un email contenant ce message a été envoyé à";
+$ubbt_lang['FORUM_RETURN']   =  "Retourner à présent sur le forum";
+?>
