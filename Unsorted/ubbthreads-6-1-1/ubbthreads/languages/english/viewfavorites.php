d6e42f346a711eb1f207c2a15e930dbc95dcf64b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:02 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/viewfavorites.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/viewfavorites.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['VIEW_FAV']  =  "Viewing all favorite threads";
+$ubbt_lang['VIEW_REM']  =  "Viewing all post reminders";
+$ubbt_lang['LAST_POST']  =  "Last Post";
+$ubbt_lang['REMOVE']     =  "Remove";
+?>
