035eabf08bd642a974fe34c0bca1564942e6990e
me-suzy <me.suzana@gmail.com>
2025-08-14 01:39:09 +0300

    Add Unsorted/Shop-Script.2.0.(NEW).suupl.&.null.by.[GTT]/cfg/tables.inc.php

---
+++ Unsorted/Shop-Script.2.0.(NEW).suupl.&.null.by.[GTT]/cfg/tables.inc.php
@@ -0,0 +1,18 @@
+<?
+	//Shop-Script database tables
+	define('PRODUCTS_TABLE', 'SS_products');
+	define('CUSTOMERS_TABLE', 'SS_customers');
+	define('ORDERS_TABLE', 'SS_orders');
+	define('SHOPPING_CARTS_TABLE', 'SS_shopping_carts');
+	define('RELATED_PRODUCTS_TABLE', 'SS_related_items');
+	define('MAILING_LIST_TABLE', 'SS_subscribers');
+	define('ORDERED_CARTS_TABLE', 'SS_ordered_carts');
+	define('NEWS_TABLE', 'SS_news_table');
+	define('DISCUSSIONS_TABLE', 'SS_discussions');
+	define('CATEGORIES_TABLE', 'SS_categories');
+	define('PRODUCT_OPTIONS_TABLE', 'SS_product_options');
+	define('PRODUCT_OPTIONS_VALUES_TABLE', 'SS_product_options_values');
+	define('SHIPPING_METHODS_TABLE', 'SS_shipping_methods');
+	define('PAYMENT_TYPES_TABLE', 'SS_payment_types');
+
+?>
