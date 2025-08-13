2f27b55b63be0725f128bed8d01113e87f971cf1
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:52 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/notifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/notifymod.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NOTIFY_MOD']  =  	"Anunta moderatorul";
+$ubbt_lang['NOTIFY_HEAD']  = 	"Aceasta operatie va anunta moderatorul/moderatorii forumului pentru a verifica mesajul selectat";
+$ubbt_lang['SEND_IT']  = 	"Da, anunta moderator(ii)";
+$ubbt_lang['NO_POST']  = 	"A aparut o problema la cautarea mesajului in baza de date.";
+?>
