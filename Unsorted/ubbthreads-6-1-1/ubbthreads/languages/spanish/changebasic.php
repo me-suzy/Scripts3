25754e258cb0262fb82fc07cdb78f704f0cc61b1
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:05 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/changebasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/changebasic.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['PIXEL_TOO_BIG']="Tu foto sobrepasa las medidas permitidas ({$theme['PictureWidth']}x{$theme['PictureHeight']}) en pixeles de ancho x altura ";
+$ubbt_lang['FILE_TOO_BIG']="Las fotos no deben de tener m&aacute;s de {$config['avatarsize']} bytes.";
+$ubbt_lang['BAD_PIC']  =       "Formato de foto no v&aacute;lida.";
+$ubbt_lang['PASS_MATCH']  = 	"Las contrase&ntilde;as no concuerdan.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"La contrase&ntilde;a necesita tener entre 4 y 20 letras o n&uacute;meros.";
+$ubbt_lang['HOME_TOO_LONG']  = 	"P&aacute;gina Web demasiado larga.";
+$ubbt_lang['OCC_TOO_LONG']  = 	"Ocupaci&oacute;n demasiado larga.";
+$ubbt_lang['HOBB_TOO_LONG']  = 	"Aficiones demasiado larga.";
+$ubbt_lang['LOC_TOO_LONG']  = 	"Lugar demasiado largo.";
+$ubbt_lang['SIG_TOO_LONG']  = 	"Firma demasiado larga.";
+$ubbt_lang['BIO_TOO_LONG']  = 	"Biograf&iacute;a demasiado larga.";
+$ubbt_lang['BASIC_MOD']  = 	"Perfil b&aacute;sico modificado.";
+?>
