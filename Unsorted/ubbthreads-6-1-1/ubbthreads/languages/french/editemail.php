835d6d044a5a949b175c6d9d34cdf52da8509fb6
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:44 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"Nous ne pouvons pas trouver de profil associé à cet identifiant";
+$ubbt_lang['EMAIL_HEAD']  = 	"Éditer les notifications par email pour";
+$ubbt_lang['PROF_EDIT']  = 	"Vous pouvez éditer n'importe quelle des informations ci-dessous.";
+$ubbt_lang['PROF_SUBS']  = 	"Notifications par Email / Abonnements";
+$ubbt_lang['PROF_NOTIF']  = 	"Voulez-vous des notifications par email lorsque vous recevez des messages privés ?";
+$ubbt_lang['PROF_REPLIES']  = 	"Voulez-vous recevoir par email toutes les réponses aux messages que vous faites ?";
+$ubbt_lang['SUB_STAT']  = 	"Statut des abonnements pour les groupes de discussion suivants. (Recevoir quotidiennement une archive de tous les messages à ce groupe de discussion par email.)";
+$ubbt_lang['SUB_SHOW']  = 	"(A - Abonné ; D - Désabonné)";
+$ubbt_lang['TEXT_S']  = 	"A";
+$ubbt_lang['TEXT_U']  = 	"D";
+?>
