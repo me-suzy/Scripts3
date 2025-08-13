e4edc1ec855fd40995d3336dd49f0651f4f5380d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:44:48 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/doratethread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/doratethread.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['INVALID_RATING'] = "That is an invalid rating.";
+$ubbt_lang['NOMORERATE'] = "You have already rated this thread.";
+$ubbt_lang['THANKS'] = "Thank you.";
+$ubbt_lang['RATED']	= "Your rating for this thread has been accepted.";
+?>
