0f8b16b417d64ccece21d1980312f06b2955f746
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:25 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"Üzenet / lap érték nem használható.";
+$ubbt_lang['FLAT_BAD']  = 	"A laponkénti üzenet számnak 1 és 99 között kell lennie.";
+$ubbt_lang['TEXTAREA_NO']  = 	"Szövegmezõ érték nem használható.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Az Ön képernyõ beállításai módosítva.";
+?>
