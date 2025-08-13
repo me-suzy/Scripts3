53e9189c693f8326034481ff4721e39d9dc67a6e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:19 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/dopoll.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/dopoll.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NO_VOTE']  = 	"Nu ati ales nici una din optiunile prezentate!";
+$ubbt_lang['POLL_REGED']  = 	"Doar utilizatorii inregistrati pot vota.";
+$ubbt_lang['POLL_IP']  = 	"Cineva cu acest IP a votat deja." ;
+$ubbt_lang['POLL_USER']  = 	"Ati votat deja in acest sondaj.";
+?>
