0207b2aeee44faf317d34a187af6be69146431ea
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:57 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/mess_reply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/mess_reply.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['EDIT_MORE']  = 	"Editer d'avantage";
+$ubbt_lang['REPLY_PREV']  = 	"Voir un aperçu de votre réponse pour";
+$ubbt_lang['PREVIEW_MESS']  = 	"Ce qui suis est un aperçu de votre message.  Si tout vous semble correct vous pouvez cliquer sur 'Continuer' et votre message sera envoyé.  Si ce n'est pas le cas vous pouvez utiliser le formulaire situé en bas de page.";
+$ubbt_lang['MESS_PREVIEW']  = 	"Je souhaite voir un aperçu de mon message";
+$ubbt_lang['PRIV_SUB']  = 	"Vous avez de nouveaux messages";
+$ubbt_lang['PRIV_BODY_R']  = 	"Vous avez reçu un nouveau message privé sur les forums de {$config['title']}. Vous pouvez le consulter en allant sur à {$config['phpurl']}/login.php .";
+?>
