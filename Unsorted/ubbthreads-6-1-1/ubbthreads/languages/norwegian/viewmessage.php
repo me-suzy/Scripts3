f9ae0a9b85191502edfc9e77ef0093d90424250a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:42:45 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/viewmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/viewmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['ABOUT']  = 	"om";
+$ubbt_lang['RR_YOUR_MESS']  = 	"Din private beskjed til";
+$ubbt_lang['RR_READ']  = 	"har blitt lest.";
+$ubbt_lang['RR_NOTIFY']  = 	"Du har mottatt en lesebekreftelse";
+$ubbt_lang['READ_RECEIPT']  = 	"Lesebekreftelse";
+$ubbt_lang['TEXT_REC']  = 	"Mottatt";
+$ubbt_lang['SENT']  = 		"Sendt";
+$ubbt_lang['DELETE_MESS']  = 	"Slett dette innlegget";
+$ubbt_lang['REPLY_MESS']  = 	"Svar på dette innlegget";
+$ubbt_lang['RETURN_START']  = 	"Tilbake til startsiden";
+?>
