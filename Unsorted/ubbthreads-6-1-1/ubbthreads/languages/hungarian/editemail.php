5baf084ce81d7a73952b1401f8b6c5139b40213e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:48 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"Nem találtam adatot a következõ névhez:";
+$ubbt_lang['EMAIL_HEAD']  = 	"E-mail figyelmeztetés beállítása:";
+$ubbt_lang['PROF_EDIT']  = 	"Módosíthatja a következõ információkat.";
+$ubbt_lang['PROF_SUBS']  = 	"E-mail figyelmeztetés / fel és leiratkozások";
+$ubbt_lang['PROF_NOTIF']  = 	"Szeretne e-mail figyelmeztetést kapni, ha privát üzenete érkezik?";
+$ubbt_lang['PROF_REPLIES']  = 	"Szeretné, hogy az üzenetére érkezõ válaszokat elküldjük Önnek e-mail-ben?";
+$ubbt_lang['SUB_STAT']  = 	"Feliratkozás a következõ témakörökre.  (Az egész napi összes hozzászólást postázzuk Önnek e-mail -ben.)";
+$ubbt_lang['SUB_SHOW']  = 	"(F - Feliratkozás ; L - Leiratkozás)";
+$ubbt_lang['TEXT_S']  = 	"F";
+$ubbt_lang['TEXT_U']  = 	"L";
+?>
