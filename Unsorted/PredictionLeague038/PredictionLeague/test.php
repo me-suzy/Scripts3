436157e5b5092a6d6bb515d0916503c4a0000911
me-suzy <me.suzana@gmail.com>
2025-08-14 01:14:25 +0300

    Add Unsorted/PredictionLeague038/PredictionLeague/test.php

---
+++ Unsorted/PredictionLeague038/PredictionLeague/test.php
@@ -0,0 +1,16 @@
+<!--
+  Author: John Astill (c) 2002
+-->
+<html>
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+PHP enabled : <?php echo "Yes"?>
+<p>
+<a href="AdminMenu.html">back</a>
+<p>
+If your ISP enabled the display of PHP Info, it will be displayed below.<br>
+<?php phpinfo() ?>
+</body>
+</html>
