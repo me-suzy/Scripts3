e3fbac8d0f8a95b0e5af07240b2d305281e5c4f7
me-suzy <me.suzana@gmail.com>
2025-08-14 01:05:45 +0300

    Add Unsorted/phpYellowPages/phpYellow/rankForm.php

---
+++ Unsorted/phpYellowPages/phpYellow/rankForm.php
@@ -0,0 +1,8 @@
+<!-- start of rankForm.php -->
+<select name="rank" class="input">
+<option value="<?php echo $rank;?>" SELECTED><?php echo $rank;?></option>
+<option value="0">Basic</option>
+<option value="1">Preferred</option>
+<option value="2">First Page</option>
+</select>
+<!-- end of rankForm.php -->
