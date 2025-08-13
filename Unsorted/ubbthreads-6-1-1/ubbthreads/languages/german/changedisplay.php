935198f6ee273fd93f8b9212906f4032c973b778
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:51 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"Wert von 'Nachrichten je Seite' kann nicht benutzt werden.";
+$ubbt_lang['FLAT_BAD']  =      "Die Anzahl der flachen Nachrichten pro Seite mu&szlig; zwischen 1 und 99 liegen.";
+$ubbt_lang['TEXTAREA_NO']  = 	"Wert von 'Textbereich' kann nicht benutzt werden.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Ihre Darstellungs-Einstellung wurde ge&auml;ndert.";
+?>
