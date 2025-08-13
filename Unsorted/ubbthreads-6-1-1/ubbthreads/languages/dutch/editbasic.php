c84fded6d98e69d38ca918cd38050e3ba0b8aad5
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:38 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =	"Jouw afbeelding ({$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, of kleiner) (.gif, .jpg, .png is toegestaan)";
+$ubbt_lang['PROF_PIC']  = 	"Jouw afbeelding (Volledige URL - Het formaat is maximaal {$theme['PictureWidth']}x{$theme['PictureHeight']} pixels, of kleiner) (.gif, .jpg, .png is toegestaan)";
+$ubbt_lang['PROF_PRIV']  = 	"Wil je privé-berichten (PM's) van andere leden kunnen ontvangen?";
+$ubbt_lang['NO_PROF']  = 	"Er kon geen profiel worden gevonden voor gebruiker";
+$ubbt_lang['PROF_HEAD']  = 	"Profiel bewerken van";
+$ubbt_lang['PROF_EDIT']  = 	"Alle onderstaande informatie kan aangepast worden.";
+$ubbt_lang['BASIC_PROF']  = 	"Basisprofiel";
+$ubbt_lang['PROF_PASS']  = 	"Wachtwoord (tussen 4 en 20 tekens)";
+$ubbt_lang['VER_PASS']  = 	"Controleer wachtwoord";
+$ubbt_lang['EMAIL_ADD']  = 	"Email adres (alleen voor administratief gebruik...niet zichtbaar voor anderen)";
+$ubbt_lang['FAKE_EMAIL']  = 	"Email adres (Dit adres wordt publiekelijk weergegeven)";
+$ubbt_lang['FULL_NAME']  = 	"Volledige naam";
+$ubbt_lang['PROF_SIG']  = 	"Handtekening (tot {$config['Sig_length']} tekens).  Je kunt <a href=\"{$config['phpurl']}/faq_dutch.php#html\" target=\"_new\">UBBCode</a> in je handtekening plaatsen";
+$ubbt_lang['PROF_HOME']  = 	"Homepage (tot 150 tekens)";
+$ubbt_lang['PROF_OCC']  = 	"Beroep";
+$ubbt_lang['PROF_HOBB']  = 	"Hobby's (tot 200 tekens)";
+$ubbt_lang['PROF_LOC']  = 	"Woonplaats of provincie";
+$ubbt_lang['PROF_BIO']  = 	"Over jezelf (tot 250 characters)";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ nummer";
+$ubbt_lang['PROF_VIS']  = 	"Wil je zichtbaar zijn op de \"Who's Online\" pagina?";
+$ubbt_lang['AUX_ONLINE']  = 	"Wil je dat anderen kunnen zien welk onderwerp je leest?";
+?>
