ba9e35bc21c4cbbe700dbd62e8ece56ac029c7ec
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:46 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/togglesub.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/togglesub.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['UN_HEADER']  = 	"Du har avbeställt prenumeration";
+$ubbt_lang['HEADER']  = 	"Du har beställt prenumeration";
+$ubbt_lang['UN_BODY']  = 	"Du har avbeställt prenumeration på forumet: ";
+$ubbt_lang['BODY']  =          "Du har beställt prenumeration på forumet: ";
+?>
