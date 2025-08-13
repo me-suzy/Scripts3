33e98b62acb6af1194368e23fab029204a4233e1
me-suzy <me.suzana@gmail.com>
2025-08-14 02:48:37 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/showprofile.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/showprofile.php
@@ -0,0 +1,27 @@
+<?
+$ubbt_lang['RATING']    =       "Rating";
+$ubbt_lang['T_RATES']   =       "Total Rates";
+$ubbt_lang['RATE_USER'] =       "Rate this user";
+$ubbt_lang['STAR1'] =           "1 star";
+$ubbt_lang['STAR2'] =           "2 stars";
+$ubbt_lang['STAR3'] =           "3 stars";
+$ubbt_lang['STAR4'] =           "4 stars";
+$ubbt_lang['STAR5'] =           "5 stars";
+$ubbt_lang['DORATE']    =       "Submit rating";
+$ubbt_lang['YOURATED']  =       "You rated this user a ";
+$ubbt_lang['PROF_FOR']  = 	"Ëè÷íûé ïðîôèëü";
+$ubbt_lang['TEXT_NAME']  = 	"Èìÿ";
+$ubbt_lang['USER_TITLE']  = 	"Title";
+$ubbt_lang['TOTAL_POSTS']  = 	"Âñåãî ñîîáùåíèé";
+$ubbt_lang['TEXT_HOME']  = 	"WEB-Ñòðàíèöà";
+$ubbt_lang['TEXT_OCCU']  = 	"Çàíÿòîñòü";
+$ubbt_lang['TEXT_HOBBY']  = 	"Õîááè";
+$ubbt_lang['TEXT_LOC']  = 	"Ãåî Ðàñïîëîæåíèå";
+$ubbt_lang['TEXT_BIO']  = 	"Êðàòêàÿ áèîãðàôèÿ";
+$ubbt_lang['PROF_ICQ']  = 	"Íîìåð ICQ";
+$ubbt_lang['REGGED_ON']  = 	"Çàðåãèñòðèðîâàí(à)";
+$ubbt_lang['EDIT_T_U']  = 	"Edit this user";
+$ubbt_lang['ADD_BOOK']  = 	"Add to address book";
+$ubbt_lang['SHOW_ALL_P']  = 	"Show all user's posts";
+$ubbt_lang['NO_LONGER']  = 	"Ýòîãî ïîëüçîâàòåëÿ íåò â íàøåé áàçå äàííûõ.";
+?>
