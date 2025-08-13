165ba2c572fbd0359eade0bf38daf69bd6b69289
me-suzy <me.suzana@gmail.com>
2025-08-14 00:16:33 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/status.inc.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/status.inc.php
@@ -0,0 +1,15 @@
+<?#//v.1.0.0
+  
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+
+
+  $STATUS = array("1" => "<FONT COLOR=GREEN><b>Active</b></FONT>",
+  				  "2" => "<FONT COLOR=red><b>Not active</B></FONT>"
+  				  );
+  
+?>
