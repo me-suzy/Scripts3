c9eb17455b971ab06f49bff3b150b97f71e2088e
me-suzy <me.suzana@gmail.com>
2025-08-14 00:16:51 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/lib/signuppayment_pay.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/lib/signuppayment_pay.php
@@ -0,0 +1,20 @@
+<?#//v.1.0.0
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+	
+	#// Set $AMOUNT variable
+	$AMOUNT = $SETTINGS[signupvalue];
+
+	#// Get user ID
+	$USER_ID = $TPL_id_hidden;
+	
+	#// Iclude template
+	include "templates/template_buy_credits_paypal_pay_php.html";
+	
+	
+?>		
+
