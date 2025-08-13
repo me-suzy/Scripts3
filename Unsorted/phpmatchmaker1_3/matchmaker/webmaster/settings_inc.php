91a00b2ee0b15066c681c197f01df0cc825e7d47
me-suzy <me.suzana@gmail.com>
2025-08-14 00:56:48 +0300

    Add Unsorted/phpmatchmaker1_3/matchmaker/webmaster/settings_inc.php

---
+++ Unsorted/phpmatchmaker1_3/matchmaker/webmaster/settings_inc.php
@@ -0,0 +1,12 @@
+<?
+$from_adress="webmaster@a4.no";
+$url_of_site="http://localhost/mmatchmaker";
+$phpmmname="PHP MatchMaker";
+$set_thmbsize="100x100";
+$set_orgsize="250x250";
+$set_piclimit="47000";
+$set_banlist="";
+$set_magic="on";
+$set_magic_q="80";
+$set_magic_q="80";
+?>
