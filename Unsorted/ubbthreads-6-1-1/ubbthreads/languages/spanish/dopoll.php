2a6f96b19162e797730a669906dcde8f50bca18d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:19 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/dopoll.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/dopoll.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NO_VOTE']  = 	"&iexcl;No has seleccionado la opci&oacute;n a votar!";
+$ubbt_lang['POLL_REGED']  = 	"&Uacute;nicamente miembros registrados y que se han identificado como tales pueden votar.";
+$ubbt_lang['POLL_IP']  = 	"Ya tenemos un voto realizado desde esta misma direcci&oacute;n IP para esta encuesta.";
+$ubbt_lang['POLL_USER']  = 	"Ya se registr&oacute; anteriormente un voto en esta encuesta bajo tu nombre.";
+?>
