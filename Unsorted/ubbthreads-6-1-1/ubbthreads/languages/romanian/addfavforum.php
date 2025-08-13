f36bafb3508981fb94190e9ab44ef18fe9f2fc67
me-suzy <me.suzana@gmail.com>
2025-08-14 02:45:55 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/addfavforum.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/addfavforum.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  = 	"Nu aveti dreptul sa vedeti acest forum asa ca nu-l puteti adauga in lista celor favorite.";
+$ubbt_lang['DUP_ENTRY']  = 	"Acest forum a fost deja adugat in lista celor favorite.";
+$ubbt_lang['FORUM_IN']  = 	"Forum adaugat";
+$ubbt_lang['FORUM_CONFIRM']  = 	"Forumul ales a fost adaugat in lista celor favorite.<br><br>De acum inainte il veti putea accesa si din 'My Home', pina in momentul in care il veti sterge de acolo.<br>";
+$ubbt_lang['FORUM_RETURN']  = 	"Intoarcere la forum";
+$ubbt_lang['TOO_MANY']  = 	"Nu puteti avea decit 83 de forumuri in lista celor favorite";
+?>
