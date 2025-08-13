b6b044330d57651accbea0a0035b5681085a26b4
me-suzy <me.suzana@gmail.com>
2025-08-14 02:41:27 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/approvepost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/approvepost.php
@@ -0,0 +1,5 @@
+<?
+$ubbt_lang['REPLY_SUB']  = 	"Det har kommet svar på ditt innlegg";
+$ubbt_lang['REPLY_BOD']  = 	"svarte på ditt innlegg på nettforumet: .";
+$ubbt_lang['APPR_HEAD']  = 	"Innlegget er godkjent.";
+?>
