cdf82b4cb4326db8df3b3cefa94fdbd31b7cd64a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:20 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/sendprivate.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/sendprivate.php
@@ -0,0 +1,13 @@
+<?
+$ubbt_lang['NOPRIVS']   = "Private messages are disabled.";
+$ubbt_lang['MESS_COPY']  = 	"I want to keep a copy of my message";
+$ubbt_lang['MESS_RECEIPT']  = 	"I want to receive a read receipt";
+$ubbt_lang['MESS_PREVIEW']  = 	"I want to preview my message";
+$ubbt_lang['ADD_BOOK']  = 	"Add to address book";
+$ubbt_lang['USE_BOOK']  = 	"or use your address book";
+$ubbt_lang['CHOOSE_ADD']  = 	"Choose a name";
+$ubbt_lang['NO_LOGGED']  = 	"Ön nem tud privát üzenetet küldeni senkinek sem amíg be nem lépett.";
+$ubbt_lang['NO_PRIVATE']  = 	"Ez a tag nem fogad privát üzenetet.";
+$ubbt_lang['PRIV_BODY2']  = 	"Töltse ki az ûrlapot a privát üzenet küldéséhez a következõ tagnak.  Õ a következõ belépésekor meg fogja kapni az ön üzenetét.";
+$ubbt_lang['TEXT_DELETE']  = 	"Törlés";
+?>
