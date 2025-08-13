8aefb9d76cb12b0b57b15e286eb7cc83d19d6ad1
me-suzy <me.suzana@gmail.com>
2025-08-14 00:58:40 +0300

    Add Unsorted/phpProMembers__nullified_/phpProMembers/admin/admin_view_details_waiting.php

---
+++ Unsorted/phpProMembers__nullified_/phpProMembers/admin/admin_view_details_waiting.php
@@ -0,0 +1,335 @@
+<?php
+/*********************************************************************/
+/* Program Name         : phpProMembers                              */
+/* Home Page            : http://www.gacybertech.com                 */
+/* Retail Price         : $149.99 United States Dollars              */
+/* WebForum Price       : $000.00 Always 100% Free                   */
+/* xCGI Price           : $000.00 Always 100% Free                   */
+/* Supplied by          : South [WTN]                                */
+/* Nullified by         : CyKuH [WTN]                                */
+/* Distribution         : via WebForum and Forums File Dumps         */
+/*********************************************************************/
+	$page_account = "admin";
+require "include.php";
+	include "templates/header.php";	
+
+	$this_id = $GLOBALS["id"];
+?>
+<br>
+<center>
+<a href="admin_search_awaiting.php?approve=1&id=<?php echo $this_id ?>"><font color="red" face="arial"><b>approve</b></font></a>
+&nbsp;&nbsp;&nbsp;
+<a href="admin_waiting_edit.php?approve=0&id=<?php echo $this_id ?>"><font color="red" face="arial"><b>edit</b></font></a>
+&nbsp;&nbsp;&nbsp;
+<a href="admin_search_awaiting.php?approve=0&id=<?php echo $this_id ?>"><font color="red" face="arial"><b>deny</b></font></a>
+</center>
+<br>
+
+<?php
+	$get_total_members = "SELECT * FROM members_waiting_approval WHERE id = \"$this_id\"";
+	$result = mysql_query($get_total_members);
+	while($row = mysql_fetch_object($result)) {
+?>
+<TABLE border="1" bordercolor="#000000" width="400" cellspacing="3" cellpadding="3">
+	<TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				First Name:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->first_name ?>
+			</font>
+		</td>
+	</TR>
+	<TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Last Name:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->last_name ?>
+			</font>
+		</td>
+	</TR>
+	<TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Address1:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->address1 ?>
+			</font>
+		</td>
+	</TR>
+	<TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Address2:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->address2 ?>
+			</font>
+		</td>
+	</TR>
+	<TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				City:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->city ?>
+			</font>
+		</td>
+	</TR>
+	<TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				State:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->state ?>
+			</font>
+		</td>
+	</TR>
+	<TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Postal Code:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->postal_code ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Country:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->country ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Telephone Number:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->telephone_number ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				E-Mail Address:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->e_mail_address ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				User Name:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->user_name ?>
+			</font>
+		</td>
+	</TR>
+<TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Password:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->user_password ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Payment Type:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->payment_type ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Account Type:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->account_type_name ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Name On Card:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->name_on_card ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Card Number:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->card_number ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Expiration Date:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->exp_date ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Billing Address1:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->billing_address1 ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Billing Address2:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->billing_address2 ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Billing City:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->billing_city ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Billing State:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->billing_state ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Billing Postal Code:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->billing_postal_code ?>
+			</font>
+		</td>
+	</TR><TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Billing Country:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->billing_country ?>
+			</font>
+		</td>
+	</TR>
+	<TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Start Date:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->start_date ?>
+			</font>
+		</td>
+	</TR>
+	<TR>
+		<td width="40%" bgcolor="#cccccc">
+			<font color="#000000" face="arial">
+				Paid Until Date:
+			</font>
+		</td>
+		<td width="60%" bgcolor="#ffffff">
+			<font color="#000000" face="arial">
+				<?php echo $row->paid_until_date ?>
+			</font>
+		</td>
+	</TR>
+</Table>
+<br>
+<center>
+<a href="admin_search_awaiting.php?approve=1&id=<?php echo $this_id ?>"><font color="red" face="arial"><b>approve</b></font></a>
+&nbsp;&nbsp;&nbsp;
+<a href="admin_waiting_edit.php?approve=0&id=<?php echo $this_id ?>"><font color="red" face="arial"><b>edit</b></font></a>
+&nbsp;&nbsp;&nbsp;
+<a href="admin_search_awaiting.php?approve=0&id=<?php echo $this_id ?>"><font color="red" face="arial"><b>deny</b></font></a>
+</center>
+<br>
+<?php
+		}
+?>
+
+<?php
+include "templates/footer.php";	
+?>	
