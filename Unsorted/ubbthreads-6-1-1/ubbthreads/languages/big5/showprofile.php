d61f971292ab60e3db95f560047b581fd021d2cc
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:38 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/showprofile.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/showprofile.php
@@ -0,0 +1,27 @@
+<?
+$ubbt_lang['RATING']    =       "µû¤À";
+$ubbt_lang['T_RATES']   =       "Á`µû¤À";
+$ubbt_lang['RATE_USER'] =       "¬°¦¹¨Ï¥ÎªÌ¥´¤À¼Æ";
+$ubbt_lang['STAR1'] =           "1 Áû¬P";
+$ubbt_lang['STAR2'] =           "2 Áû¬P";
+$ubbt_lang['STAR3'] =           "3 Áû¬P";
+$ubbt_lang['STAR4'] =           "4 Áû¬P";
+$ubbt_lang['STAR5'] =           "5 Áû¬P";
+$ubbt_lang['DORATE']    =       "½T©w";
+$ubbt_lang['YOURATED']  =       "±z¹ï¦¹¨Ï¥ÎªÌªºµû¤À¬O";
+$ubbt_lang['PROF_FOR']  = 	"­Ó¤HÀÉ®×";
+$ubbt_lang['TEXT_NAME']  = 	"©m¦W/¥N¸¹";
+$ubbt_lang['USER_TITLE']  = 	"µ¥¯Å";
+$ubbt_lang['TOTAL_POSTS']  = 	"µoªí¤å³¹Á`¼Æ";
+$ubbt_lang['TEXT_HOME']  = 	"ºô­¶";
+$ubbt_lang['TEXT_OCCU']  = 	"Â¾·~";
+$ubbt_lang['TEXT_HOBBY']  = 	"¿³½ì";
+$ubbt_lang['TEXT_LOC']  = 	"©Ò¦b¦a";
+$ubbt_lang['TEXT_BIO']  = 	"¦Û§Ú¤¶²Ð";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ ¸¹½X";
+$ubbt_lang['REGGED_ON']  = 	"µù¥U¤é´Á";
+$ubbt_lang['EDIT_T_U']  = 	"½s¿è³o¦ì¨Ï¥ÎªÌ";
+$ubbt_lang['ADD_BOOK']  = 	"¥[¤J§Ú¨p¤Hªº³q°T¿ý·í¤¤";
+$ubbt_lang['SHOW_ALL_P']  = 	"Åã¥Ü³o¦ì¨Ï¥ÎªÌªº©Ò¦³¤å³¹";
+$ubbt_lang['NO_LONGER']  = 	"³o¦ì¨Ï¥ÎªÌ¤£¦b¨t²Î¤¤¡C";
+?>
