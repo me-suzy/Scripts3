b0e6238ad51757cb6fe3ee71b08f6401175220eb
me-suzy <me.suzana@gmail.com>
2025-08-14 02:43:28 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"Nie mo¿na znale¼æ opis tego u¿ytkownika.";
+$ubbt_lang['EMAIL_HEAD']  = 	"Zmieñ powiadomienia poczt± i prenumeratê dla konta";
+$ubbt_lang['PROF_EDIT']  = 	"Mo¿esz zmieniæ dowolne informacje poni¿ej.";
+$ubbt_lang['PROF_SUBS']  = 	"Powiadomienia poczt± / Prenumerata";
+$ubbt_lang['PROF_NOTIF']  = 	"Czy chcesz otrzymywaæ powiadomienie poczt±, gdy dostaniesz prywatn± wiadomo¶æ?";
+$ubbt_lang['PROF_REPLIES']  = 	"Czy chcesz otrzymywaæ poczt± odpowiedzi na Twoje wiadomo¶ci?";
+$ubbt_lang['SUB_STAT']  = 	"Prenumerata (Wszystkie wiadomo¶ci bêd± okresowo wysy³ane na Twój adres email)";
+$ubbt_lang['SUB_SHOW']  = 	"(P - prenumerowane ; N - nieprenumerowane)";
+$ubbt_lang['TEXT_S']  = 	"P";
+$ubbt_lang['TEXT_U']  = 	"N";
+?>
