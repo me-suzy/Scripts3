9d341f026f906968aab59e8f5313ff5d4835c8eb
me-suzy <me.suzana@gmail.com>
2025-08-14 01:17:06 +0300

    Add Unsorted/propublish1_1/propublish/admin/language/nor.php

---
+++ Unsorted/propublish1_1/propublish/admin/language/nor.php
@@ -0,0 +1,219 @@
+<?
+
+// menu_inc.php
+$la_art_list = "Artikkeloversikt";
+$la_new_art = "Ny artikkel";
+$la_new_art_html = "Ny html artikkel";
+$la_change_user = "Endre bruker";
+$la_cat = "Kategorier";
+$la_level = "Nivåer";
+$la_setup = "Instillinger";
+
+// category.php
+$la_cat_saved = "Kategori er lagret!";
+$la_editing = "Redigerer nå";
+$la_deleted_cat = "Kategori er slettet";
+$la_catname = "Kategorinavn";
+
+
+
+// user.php
+$la_users = "Brukere";
+$la_user_saved = "Brukeren lagret!";
+$la_now_editing = "Redigerer nå";
+$la_this_author = "Denne forfatteren har";
+$la_this_author2 = "artikkel. Ønsker du slette brukeren ?";
+$la_deleted_user = "Bruker er slettet";
+$la_notall = "Du har ikke lov til dette";
+$la_user_title = "Registrering / endring";
+$la_firstname = "Fornavn";
+$la_lastname = "Etternavn";
+$la_about = "Om";
+$la_accesslevel = "Adgangsnivå";
+$la_levels = "Nivåer";
+$la_stat_for = "Statistikk for";
+$la_ant_art = "Antall artikler";
+$la_read = "Lest";
+$la_name = "Navn";
+$la_change = "Endre";
+$la_new_user = "Ny bruker";
+
+
+
+// editor.php / editor_html.php
+$la_newseditor = "Nyhetseditor";
+$la_back_news = "Tilbake til nyhetsliste";
+$la_mand_error1 = "Obligatorisk felt er ikke fyllt ut";
+$la_saved = "Artikkelen er lagret !";
+$la_edit_now = "Redigerer nå";
+$la_newedit = "Registrering / endring";
+$la_tagsapproved = "Tegn tillatt";
+$la_title = "Tittel";
+$la_intro = "Intro";
+$la_content = "Innhold";
+$la_status = "Status";
+$la_activated = "Aktivert";
+$la_deactivated = "Deaktivert";
+$la_pic = "Bilder";
+$la_num_pic = "Antall bilder tilknyttet denne artikkel:";
+$la_pic_up = "Bildeopplasting/redigering";
+$la_rem_save = "Du må lagre artikkelen før du kan laste opp bilder";
+$la_save = "Lagre";
+$la_new_art_validate = "Ny artikkel inne for validering";
+$la_new_art_validate2 = " Hei\nLogg inn på $set_url for å godkjenne artikkelen. Artikkelen har tittelen";
+$la_edit_sh = "Normal editor høyde";
+$la_edit_sw = "Normal editor bredde";
+$la_edit_wh = "WYSIWYG editor høyde";
+$la_edit_ww = "WYSIWYG editor bredde";
+
+
+// editor_html.php
+$la_size = "Størrelse";
+$la_size1 = "Veldig liten";
+$la_size2 = "Liten";
+$la_size3 = "Medium";
+$la_size4 = "Stor";
+$la_size5 = "Større";
+$la_size6 = "Størst";
+$la_show_html = "Vis HTML taggene, ikke bruk dem (oversetter fet skrift til &lt;b&gt;)";
+
+
+
+// list.php
+$la_art = "Artikkeloversikt for";
+$la_num_art_page = "Antall artikler per side";
+$la_art_approved = "Artikkelen er godkjent";
+$la_art_upg = "Artikkelen er oppgradert til toppnyhet";
+$la_art_upg_fp = "Artikkelen er oppgradert til forsidenyhet";
+$la_art_dwn_fp = "Artikkelen er nedrgradert fra forsidenyhet";
+$la_sure = "Er du sikker på at du vil slette ?";
+$la_y = "Ja";
+$la_n = "No";
+$la_delete_conf = "Artikkelen er slettet!";  
+$la_article = "Artikkel";
+$la_first_pub = "Først pub";
+$la_cat = "Kategori";
+$la_list_feedb = "Vurdering"; 
+$la_list_change = "Endre";
+$la_details = "Detaljer";
+$la_del = "Slett";
+$la_fp = "FP";
+$la_nfp = "!FP";
+$la_approve = "Godkjenn";
+$la_prev = "Forrige";
+$la_next = "Neste";
+$la_stats_header = "Statistikk for";
+$la_author = "Forfatter";
+$la_art_shown = "Artikkelen vist (totalt)";
+$la_art_grade = "Karakter";
+$la_art_voters = "Antall stemmer";
+$la_art_created = "Opprettet";
+$la_art_changed = "Sist endret";
+$la_list_opt = "Ta med";
+$la_date = "Dato";
+$la_stat = "Stat";
+$la_respage = "Velg resultatark";
+$la_perpage = "per side";
+
+// auth.php
+$la_member_funk_info = "Medlemsfunksjon";
+$la_member_funk_1 = "Denne tjenesten krever en høyere sikkerhetsklarering enn du er godkjent for. Du har i dag nivå";
+$la_member_funk2 = "men påkrevd nivå på denne siden er";
+$la_login_as = "Du er innlogget som";
+$la_withlevel = "med level";
+$la_demands_login = "Denne tjenesten krever innlogging. Vennligst logg inn på ny, og kom tilbake til denne side etterpå.";
+
+
+// art.php
+$la_by = "av";
+$la_changed = "endret";
+$la_news = "Nyheter";
+$la_print = "Skriv ut";
+$la_rate_this = "Vurder denne artikkelen";
+$la_tellafriend = "Tips en venn om denne artikkelen";
+
+// search.php
+$la_search_result = "SØKERESULTAT";
+$la_search_found1 = "Søket resulterte i";
+$la_search_found2 = "treff under ditt søk på";
+$la_search_header = "<h3>SØK</h3><br>Her kan du lete på alle artikler.";
+$la_search_now = "Søk nå!";
+
+// feedback.php 
+$la_thanks = "Takk for votes, og for eventuelle kommentarer for hvordan vi kan gjøre denne artikkelen bedre";
+$la_email_admin_subj = "Ny vurdering av artikkel";
+$la_voted = "Vurdering som følger";
+$la_article = "Artikkel:";
+$la_grade = "Karakter:";
+$la_comment = "Kommentar:"; 
+$la_email = "Epost"; 
+$la_feedback_info = "Ved å gi en grade og/eller kommentar gir du oss verdifull tilbakemelding slik at vi kan gjøre nettstedet enda bedre! NB: Alle felter er valgfrie.";
+$la_bad = "Dårlig";
+$la_perfect = "Perfekt";
+$la_feedbak = "Feedback";
+
+// tellafriend.php
+$la_tellafriend = "Fortell en venn";
+$la_tellafriend_sent = "Artikkeltipset er sendt til";
+$la_tellafriend_subj = "Tips fra en bekjent om en artikkel";
+$la_tellafriend_msg = "ønsket å tipse deg om denne artikkelen";
+$la_sent = "Sendt fra:";
+$la_tellafriend_comment = "Under følger eventuelle kommentarer fra avsender:";
+$la_tellafriend_error1 = "Feil, avsenders eller mottakers epostadresse ikke oppgitt.";
+$la_tellafriend_intro = "Her kan du tipse en venn eller bekjent om denne artikkelen. Bare tast inn mottaker, avsender og eventuell kommentar";
+$la_reciever_mail = "Mottaker - epostadr.";
+$la_sender_mail = "Avsender - epostadr.";
+$la_close = "Lukk";
+
+// func.php
+$la_read_more = "Les mer";
+
+// login.php
+$la_login_wel = "Velkommen";
+$la_login_ready = "Du er nå innlogget, og kan registrere nye artikler eller endre dine artikler";
+$la_newslist = "Nyhetsliste";
+$la_error = "Feilmelding";
+$la_error_2 = "Personen med din epostadresse og ditt passord kunne ikke bli funnet.";
+$la_passwd = "Passord";
+$la_login = "Logge inn";
+$la_login_logout1 = "Utlogget!";
+$la_login_logout2 = "Du er nå vellykket utlogget.";
+$la_logout = "Logge ut";
+
+// level.php
+$la_level_saved = "Nivå er lagret!";
+$la_deleted_level = "Level er slettet";
+$la_levelname = "Levelnavn";
+$la_level = "Nivå";
+
+// img.php
+$la_picupload = "Bildeopplastning";
+$la_return = "Returner til nyhetseditor";
+$la_pic_success1 = "Bildet er nå lastet opp. Bruk koden";
+$la_pic_success2 = "der hvor bildet skal dukke opp i artikkelen.";
+$la_upload_error = "Bildet er større enn tillatt.";
+$la_img_already = "Bilder opplastet til denne artikkel allerede:";
+$la_del_img = "Følgende bilde er nå slettet:";
+$la_img = "Bilde";
+
+// setup.php
+$la_config = "Instillinger";
+$la_change_config = "Her kan du endre instillinger for systemet.";
+$la_saved_conf = "Instillinger lagret!";
+$la_language = "Språk";
+$la_email_admin = "Webmasters epostadresse";
+$la_email_admin1 = "Epostadresse hvor melding om ny artikkel for godkjenning sendes.";
+$la_url1 = "Url til artikkelfilen art.php. Ikke ta med art.php, bare http://www.domain.com/news f.eks.";
+$la_html_editor = "Html editor";
+$la_html_editor1 = "Skal være avmerket dersom du tillater bruk av html editor.";
+$la_norm_editor = "Normal editor";
+$la_norm_editor1 = "Skal være avmerket dersom du tillater bruk av vanlig editor.";
+$la_news_frontpage = "Antall nyheter på forsiden";
+$la_news_frontpage1 = "Listes opp på standardform.";
+$la_inform = "Informer webmaster";
+$la_inform1 = "Dersom du ønsker å bli varslet når en ny artikkel kommer inn for validering, velg denne.";
+$la_validate = "Autovalidering?";
+$la_validate1 = "Med dette valget aktivert bli alle artikler publisert med en gang, uten behov for validering.";
+
+
+?> 
