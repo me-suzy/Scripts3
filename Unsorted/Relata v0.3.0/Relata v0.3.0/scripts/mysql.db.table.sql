ffb19ec100d7814829730578b7753535abb0c076
me-suzy <me.suzana@gmail.com>
2025-08-14 01:21:10 +0300

    Add Unsorted/Relata v0.3.0/Relata v0.3.0/scripts/mysql.db.table.sql

---
+++ Unsorted/Relata v0.3.0/Relata v0.3.0/scripts/mysql.db.table.sql
@@ -0,0 +1,8 @@
+#
+# You need to change the following INSERT variables to match your setup.
+#
+# locahost = is your sql server hostname or ip address.
+# relata = is your relata administrator acccount
+
+INSERT INTO db VALUES ( 'localhost', 'relata', 'relata', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'N', 'Y', 'Y', 'Y');
+
