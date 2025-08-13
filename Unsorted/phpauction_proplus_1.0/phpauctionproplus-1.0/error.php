ce3480f99ec150b1c27c568ae9df310e9f48f492
me-suzy <me.suzana@gmail.com>
2025-08-14 00:11:52 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/error.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/error.php
@@ -0,0 +1,33 @@
+<?#//v.1.0.0
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+
+   require('./includes/messages.inc.php');
+   require('./includes/config.inc.php');
+
+
+?>
+
+<HTML>
+<HEAD>
+<TITLE><? print $SETTINGS[sitename]?></TITLE>
+
+
+</HEAD>
+
+<BODY  BGCOLOR="#FFFFFF" >
+
+<?
+
+require("header.php");
+include "templates/template_error_php.html";
+include "./footer.php"; 
+
+
+?>
+</BODY>
+</HTML>
