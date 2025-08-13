2c33c4dbeb459f8fadba2f50c3d5ce718adf67ea
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:28 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/mess_handler.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/mess_handler.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['REPLY_HEAD']   =   "Antwort auf eine private Nachricht an den Benutzer";
+$ubbt_lang['MESS_COPY']  =     "Ich m&ouml;chte eine Kopie dieser Nachricht behalten";
+$ubbt_lang['DELETE_MESS']  = 	"Nachricht löschen";
+$ubbt_lang['REPLY_MESS']  = 	"Auf diese Nachricht antworten";
+$ubbt_lang['RETURN_START']  = 	"Zurück zum Postfach";
+$ubbt_lang['MESS_RECEIPT']  = 	"Ich m&ouml;chte eine Lesebest&auml;tigung erhalten";
+$ubbt_lang['MESS_PREVIEW']  = 	"Ich m&ouml;chte eine Voransicht meiner Nachricht";
+$ubbt_lang['ORIG_MESS']  = 	"Original-Nachricht";
+?>
