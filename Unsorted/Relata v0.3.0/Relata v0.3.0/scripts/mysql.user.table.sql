45ba421e5a4b3e6de88972ac71ac35c35e69e037
me-suzy <me.suzana@gmail.com>
2025-08-14 01:21:15 +0300

    Add Unsorted/Relata v0.3.0/Relata v0.3.0/scripts/mysql.user.table.sql

---
+++ Unsorted/Relata v0.3.0/Relata v0.3.0/scripts/mysql.user.table.sql
@@ -0,0 +1,8 @@
+﻿#
+# You need to change the following INSERT variables to match your setup.
+#
+# localhost = your SQL server hostname or IP address.
+# relata = your Relata administrator acccount
+# password = NEEDS TO BE CHANGED
+
+INSERT INTO user VALUES ( 'localhost', 'relata', 'password', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N');
