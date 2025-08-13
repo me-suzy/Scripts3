b44472907bb783e46e3a1d9d0b17c3b3fe254c3e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:17 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"Ongeldige berichten per pagina.";
+$ubbt_lang['FLAT_BAD']  = 	"Platte berichten per pagina moet tussen 1 en 99 ingesteld zijn.";
+$ubbt_lang['TEXTAREA_NO']  = 	"Ongeldige tekstinvoer.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Geef de veranderde instellingen weer.";
+?>
