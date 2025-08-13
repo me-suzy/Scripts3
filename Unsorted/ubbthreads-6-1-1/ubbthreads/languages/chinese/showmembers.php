862632f8dca3d34c663b45a83461d48af1a7d8ca
me-suzy <me.suzana@gmail.com>
2025-08-14 02:30:11 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/showmembers.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/showmembers.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['USER_STATUS']  = 	"Éí·Ý";
+$ubbt_lang['TEXT_HOME']  = 	"ÍøÒ³";
+$ubbt_lang['TOTAL_POSTS']  = 	"·¢±íÎÄÕÂ×ÜÊý";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ ºÅÂë";
+$ubbt_lang['REGGED_ON']  = 	"×¢²áÈÕÆÚ";
+$ubbt_lang['PAGE_TEXT']  = 	"Ò³Êý";
+?>
