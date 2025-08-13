ab1bcca1e0b104fb71093bb1d405816a568d27bb
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:18 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =    "Your account needs to be approved by an Administrator before you can login.  You will receive an email once this has been done.";
+$ubbt_lang['APPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} has been approved.";
+$ubbt_lang['NOTAPPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} was not approved.  The reason for this is:";
+$ubbt_lang['NEWREGBODY'] = "A new user has registered at {$config['title']}. To approve this account, please visit";
+$ubbt_lang['NEWREG'] = "New User Registration at";
+$ubbt_lang['INVALID_DATE'] = "That is an invalid date.";
+$ubbt_lang['TOOYOUNG']  =       "Registration for this message board will require approval of your parent or guardian.  You may use the <a href=\"{$config['phpurl']}/coppaform.php\">Permission form</a> for this.";
+$ubbt_lang['UNDER13'] = "Sorry, we do not allow children under 13 to register.";
+$ubbt_lang['PASS_BODY1']  = 	"Someone from the ip address";
+$ubbt_lang['PASS_BODY2']  = 	"registered the Username";
+$ubbt_lang['PASS_BODY3']  = 	"The password for this Username is";
+$ubbt_lang['NEW_BODY']  = 	"Your username has been reserved.  You should be receiving an email shortly with your password";
+$ubbt_lang['BAD_UNAME']  = 	"A névben nem használhat HTML kódot, vagy szóközt elõtte vagy utána.";
+$ubbt_lang['LONG_NAME']  = 	"A név nem elég hosszú.";
+$ubbt_lang['BAD_EMAIL']  = 	"Errõl az e-mail domain-rõl nem fogadunk el tagi regisztrációt.";
+$ubbt_lang['USER_EXISTS']  = 	"Ez a név már foglalt.";
+$ubbt_lang['NO_MULTI']  = 	"Többszörös regisztrálás nem lehetséges egy e-mail címrõl. A megadott címrõl már valaki regisztrálta magát.";
+$ubbt_lang['PASS_MATCH']  = 	"A jelszavak nem egyeznek meg.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"A jelszó hossza 4 és 20 karakter között lehet.";
+$ubbt_lang['ILL_PASS']  = 	"A jelszó érvénytelen karaktert is tartalmaz.  Kérem hogy csak betüket és számokat használjon.";
+$ubbt_lang['WEL_MSG']  = 	"Kérem állítsa be az adatait. Az adatmódosításnál be tudja állítani személyes adatait, aláírást tud hozzáfûzni üzeneiethez, beállíthatja a képernyõ paramétereket (sorrend, megjelenési mód, egy lapon megjelenítendõ üzenetek száma stb...).";
+$ubbt_lang['PASS_SUB']  = 	"A jelszava:";
+$ubbt_lang['NEW_CONFIRM']  = 	"A név regisztrálva.";
+?>
