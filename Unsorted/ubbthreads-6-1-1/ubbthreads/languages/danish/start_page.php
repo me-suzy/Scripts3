87d6ae80ef300f357d45d9902a51391ccf41fac9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:31:51 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"Adgangskode";
+$ubbt_lang['PASS_REQ_BOD1']  = 	"En person fra IP-adressen";
+$ubbt_lang['PASS_REQ_BOD2']  = 	"requested a temporary password for the Username";
+$ubbt_lang['PASS_REQ_BOD3']  = 	"for the website '$config[ttle]'.  This is a temporary password that can be used if you have forgotten your original password.  If you did not request this, please ignore this message.  The temporary password for this Username is --";
+$ubbt_lang['PASS_MAILED']  = 	"Adgangskoden er afsendt";
+$ubbt_lang['PASS_MAILED2']  = 	"A temporary password for this user has been mailed.";
+$ubbt_lang['NO_EMAIL']  = 	"Vi kan desværre ikke finde e-postadressen for";
+?>
