51b88a4b73da0ac53754881b00c8a524c7a72a6a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:45:29 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/sendprivate.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/sendprivate.php
@@ -0,0 +1,13 @@
+<?
+$ubbt_lang['NOPRIVS']   = "Private messages are disabled.";
+$ubbt_lang['MESS_COPY']  = 	"I want to keep a copy of my message";
+$ubbt_lang['MESS_RECEIPT']  = 	"I want to receive a read receipt";
+$ubbt_lang['MESS_PREVIEW']  = 	"I want to preview my message";
+$ubbt_lang['ADD_BOOK']  = 	"Add to address book";
+$ubbt_lang['USE_BOOK']  = 	"or use your address book";
+$ubbt_lang['CHOOSE_ADD']  = 	"Choose a name";
+$ubbt_lang['NO_LOGGED']  = 	"Não é possível enviar mensagem privada antes de fazer login.";
+$ubbt_lang['NO_PRIVATE']  = 	"O usuário não recebe mensagens privadas.";
+$ubbt_lang['PRIV_BODY2']  = 	"Preeencha o formulário para enviar uma mensagem privada ao usuário.";
+$ubbt_lang['TEXT_DELETE']  = 	"Apagar";
+?>
