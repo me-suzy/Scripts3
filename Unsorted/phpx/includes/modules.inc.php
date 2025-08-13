c3174c6d92d97975cef31a4046fa3556509cc514
me-suzy <me.suzana@gmail.com>
2025-08-14 01:02:40 +0300

    Add Unsorted/phpx/includes/modules.inc.php

---
+++ Unsorted/phpx/includes/modules.inc.php
@@ -0,0 +1,11 @@
+<?php
+#$Id: modules.inc.php,v 1.1 2003/09/04 17:59:46 ryan Exp $
+//switch($this->page){
+//    case contact:
+//       require_once("includes/contact.inc.php");
+//        $contact = new contactModule($this->textArray, $this->templates);
+//        $this->body = $contact->body;
+//        $this->title = "Contact";
+//        break;
+//}
+?>
