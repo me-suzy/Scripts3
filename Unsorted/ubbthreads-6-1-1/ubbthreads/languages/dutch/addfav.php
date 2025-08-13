dfb6070fc043876cf36d60d546482bb43b7fdaeb
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:05 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/addfav.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/addfav.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  =  "Omdat je geen rechten hebt dit onderwerp te bekijken kun je het ook niet als 'Favoriet' of 'Niet vergeten!' toevoegen.";
+$ubbt_lang['MAIN_ONLY']  =  "Je kunt alleen het hoofdonderwerp als 'Favoriet' of 'Niet vergeten!' toevoegen.";
+$ubbt_lang['DUP_ENTRY']  =  "Dit onderwerp komt al in jouw 'Favoriet' of 'Niet vergeten!' lijst voor.";
+$ubbt_lang['ENTRY_IN']  =  "Bericht of onderwerp toegevoegd";
+$ubbt_lang['FAV_CONFIRM']  =  "Het geselecteerde bericht of onderwerp is aan de lijst toegevoegd.  De lijst kun je vinden onder 'Mijn profiel' tot op het moment dat je het verwijderd.  Als je het bericht als 'Niet vergeten!' hebt opgeslagen, vervalt de herinnering zodra je het bericht beantwoord hebt.";
+$ubbt_lang['FORUM_RETURN']  =  "Ga nu terug naar het forum.";
+?>
