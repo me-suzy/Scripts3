f2253aa9d99237e9c442f8ba00c346622824ceba
me-suzy <me.suzana@gmail.com>
2025-08-14 01:40:17 +0300

    Add Unsorted/Shop-Script.2.0.(NEW).suupl.&.null.by.[GTT]/includes/language/english.php

---
+++ Unsorted/Shop-Script.2.0.(NEW).suupl.&.null.by.[GTT]/includes/language/english.php
@@ -0,0 +1,357 @@
+<?
+/*****************************************************************************
+ *                                                                           *
+ * Shop-Script 2.0                                                           *
+ * Copyright (c) 2002-2003 Articus consulting group. All rights reserved.    *
+ *                                                                           *
+ ****************************************************************************/
+
+
+
+//language file
+
+//		ENGLISH		//
+
+//default character set, that will be used
+define('DEFAULT_CHARSET', 'iso-8859-1');
+
+//miscellanious: buttons and links
+define('LINK_TO_HOMEPAGE', 'Home');
+define('PRODUCTS_BEST_CHOISE', 'Best choice');
+define('MORE_INFO_ON_PRODUCT', 'more info...');
+define('ENLARGE_PICTURE', 'enlarge...');
+define('DISCUSS_ITEM_LINK', 'discuss');
+define('POSTS_FOR_ITEM_STRING', 'post(s)');
+define('ADD_TO_CART_STRING', 'add to cart');
+define('LIST_PRICE', 'List price');
+define('CURRENT_PRICE', 'Price');
+define('YOU_SAVE', 'You save');
+define('IN_STOCK', 'In stock');
+define('VOTING_FOR_ITEM_TITLE', 'Rate this item');
+define('MARK_EXCELLENT', 'Excellent');
+define('MARK_GOOD', 'Good');
+define('MARK_AVERAGE', 'Average');
+define('MARK_POOR', 'Poor');
+define('MARK_PUNY', 'Puny');
+define('VOTE_BUTTON', 'Rate!');
+define('VOTES_FOR_ITEM_STRING', 'vote(s)');
+
+define('MY_ACCOUNT_LINK', 'My account');
+define('REGISTER_LINK', 'Register');
+define('CHANGE_CURRENCY_LINK', 'Change currency');
+define('LOGOUT_LINK', 'Logout');
+define('ADVANCED_SEARCH_LINK', 'advanced search');
+define('FORGOT_PASSWORD_LINK', 'forgot password?');
+define('ADMINISTRATE_LINK', '>> ADMINISTRATE <<');
+define('TERMINATE_ACCOUNT_LINK', 'unsubscribe');
+
+define('ANSWER_YES', 'yes');
+define('ANSWER_NO', 'no');
+define('POST_BUTTON', 'Post');
+define('OK_BUTTON', 'OK');
+define('RESET_BUTTON', 'Reset');
+define('SAVE_BUTTON', 'Save');
+define('DELETE_BUTTON', 'Delete');
+define('CLOSE_BUTTON', 'Close');
+define('CANCEL_BUTTON', 'Cancel');
+define('UPDATE_BUTTON', 'Update');
+define('ADD_BUTTON', 'Add');
+define('CONFIG_BUTTON', 'Configure...');
+define('EDIT_BUTTON', 'Edit');
+define('FIND_BUTTON', 'Find!');
+define('COMPLETE_ORDERS_BUTTON', 'Complete order(s)');
+
+//misc strings
+define('STRING_BEFORE', 'before');
+define('STRING_AFTER', 'after');
+define('STRING_SHOW', 'show');
+define('STRING_NUMBER', 'number');
+define('STRING_AUTHORIZATION', 'Authorization');
+define('STRING_RELATED_ITEMS', 'Related items');
+define('STRING_NUMBER_ONLY', 'number only');
+define('STRING_UNIVERSAL_CURRENCY', 'universal currency');
+define('STRING_EMPTY_CATEGORY', 'no products');
+define('STRING_NO_ORDERS', '< no orders >');
+define('STRING_EMPTY_LIST', '<b><font color=brown>EMPTY LIST</font></b>');
+define('STRING_SEARCH', 'Search:');
+define('STRING_SEARCH_IN_RESULTS', 'search in results');
+define('STRING_THANK_YOU', 'Thank you!');
+define('STRING_SUCCESSFUL_ACCOUNT_TERMINATION', 'Your account has been successfully terminated. Thanks for shopping!');
+define('STRING_PASSWORD_SENT', 'Your password has been mailed to');
+define('STRING_CANT_FIND_USER_IN_DB', 'Sorry, but login wasn`t found in the database');
+define('STRING_PRICELIST', 'Price list');
+define('STRING_SELECT_CURRENCY_TYPE', 'Please select currency type which you want prices to be shown in:');
+define('STRING_FORGOT_PASSWORD_FIX', '<u>Forgot password?</u> Please input your login:');
+define('STRING_ACOOUNT_UPDATE_SUCCESSFUL', 'Update successful!<br>Message with updated information has been mailed to you.');
+define('STRING_REGISTRATION_SUCCESSFUL', 'Registration successful!<br>Message with your information has been mailed to you.');
+define('STRING_GREETINGS', 'Hello and welcome to Shop-Script!');
+define('STRING_CC_SELECTED', 'You`ve selected credit card as payment type. Please, provide following information.');
+define('STRING_CC_NUMBER', 'Credit card number:');
+define('STRING_CC_EXP_DATE', 'Expiration date (MMYY):');
+define('STRING_FOUND', 'Found ');
+define('STRING_PRODUCTS', 'product(s)');
+define('STRING_CATEGORIES', 'category(ies)');
+define('STRING_SEARCH_TIP', '<font color=gray>// searching for matches in name and description of the products/categories //</font>');
+define('STRING_ADVANCED_SEACH_TITLE', 'Advanced search form');
+define('STRING_NO_MATCHES_FOUND', '// no matches found //');
+define('STRING_ADV_SEARCH_ANY', 'any');
+define('STRING_PRICE_FROM', 'from');
+define('STRING_PRICE_TO', 'to');
+define('STRING_ORDER_ID', 'Order ID');
+define('STRING_CHECK_YOUR_ORDER', 'Please check your order');
+define('STRING_CHOOSE_PAYMENT_SHIPPING_TYPES', 'Please select payment and shipping types');
+define('STRING_ORDER_CONFIRMATION', 'Order confirmation');
+define('STRING_CASH_PAYMENT', 'Cash');
+define('STRING_NOT_CASH_PAYMENT', 'Not cash');
+define('STRING_TAX', 'Tax');
+define('STRING_SHIPPING_TYPE', 'Shipping');
+define('STRING_PAYMENT_TYPE', 'Payment');
+define('STRING_CUSTOMER_COMMENTS', 'Comments/suggestions');
+define('STRING_ORDER_PLACED', 'Thank you for you order!<br>Our manager will contact you in the nearest time');
+define('STRING_YOUR_ORDER', 'Your order');
+define('STRING_SHIPPING_ADDRESS', 'Shipping address');
+define('STRING_PLACE_ORDER', 'Place order!');
+define('STRING_CONTACT_INFORMATION', 'CONTACT INFORMATION');
+define('STRING_UPDATE_DETAILS', 'Update details');
+define('STRING_REGISTRATION_FORM', 'Registration form');
+define('STRING_ORDER_CONTINUE_TIP', 'To continue ordering you should be registered customer.<br>
+	If you already are, please use form to the right, else fill registration form.');
+define('STRING_REQUIRED', '<font color=red>*</font> required');
+define('STRING_AUTHORIZATION_FIELDS', 'AUTHORIZATION FIELDS');
+define('STRING_SHOPPING_HISTORY', 'Shopping history');
+define('STRING_ORDER_NOT_COMPLETED', 'Not completed');
+define('STRING_ORDER_COMPLETED', 'Completed');
+define('STRING_CONTACTS', 'Contact information');
+define('STRING_ALL', 'all');
+define('STRING_DEFAULT', 'default');
+define('STRING_NEXT', 'next');
+define('STRING_PREVIOUS', 'prev');
+
+define('STRING_NAME', 'Name');
+define('STRING_DESCRIPTION', 'Description');
+define('STRING_SHIPPING_LUMPSUM', 'Minimal cost');
+define('STRING_SHIPPING_PERCENT', 'Percent of order total');
+define('STRING_PAYMENT_CALCTAX', 'calculate tax for this payment type');
+
+//item discussion phrases
+
+define('DISCUSSION_TITLE', 'discussion');
+define('DISCUSSION_NO_POSTS_ON_ITEM_STRING', 'no posts on this product');
+define('DISCUSSION_NICKNAME', 'Nickname');
+define('DISCUSSION_SUBJECT', 'Subject');
+define('DISCUSSION_BODY', 'Post');
+define('DISCUSSION_DELETE_POST_LINK', 'delete post');
+define('DISCUSSION_ADD_MESSAGE', 'New post');
+
+//shopping cart
+
+define('CART_CONTENT_EMPTY', '(no items)');
+define('CART_CONTENT_NOT_EMPTY', 'item(s): ');
+define('CART_TITLE', 'Your shopping cart');
+define('CART_CLEAR', 'clear');
+define('CART_UPDATE', 'Update');
+define('CART_PROCEED_TO_CHECKOUT', 'Proceed to checkout >>');
+define('CART_EMPTY', 'Your shopping cart is empty');
+
+//table titles
+
+define('TABLE_PRODUCT_NAME', 'Product name');
+define('TABLE_PRODUCT_QUANTITY', 'Quantity');
+define('TABLE_PRODUCT_COST', 'Cost');
+define('TABLE_TOTAL', 'Total:');
+define('TABLE_ORDER_TIME', 'Order time');
+define('TABLE_ORDERED_PRODUCTS', 'Ordered products');
+define('TABLE_ORDER_TOTAL', 'Order total');
+define('TABLE_CUSTOMER', 'Customer');
+define('TABLE_ORDER_COMMENTS', 'Comments');
+
+//different admin strings
+
+define('ADMIN_TITLE', 'ADMINISTRATIVE TOOLS');
+
+define('ADMIN_NEW_ORDERS', 'New orders');
+define('ADMIN_CATEGORIES_PRODUCTS', 'Categories and products');
+define('ADMIN_PRODUCT_OPTIONS', 'Product extra options');
+define('ADMIN_SETTINGS', 'System settings');
+define('ADMIN_STATISTICS', 'Statistics');
+define('ADMIN_CUSTOMERS', 'Customers');
+define('ADMIN_VOTING', 'Poll');
+define('ADMIN_SHIPPING_PAYMENT', 'Shipping and payment types');
+define('ADMIN_CC_PROCESSING', 'Credit card processing');
+define('ADMIN_IMPORT_FROM_EXCEL', 'Import from Excel');
+define('ADMIN_NEWS', 'News');
+define('ADMIN_ABOUT_PAGE', 'About page');
+define('ADMIN_SHIPPING_PAGE', 'Shipping and delivery page');
+define('ADMIN_SYNCHRONIZE_TOOLS', 'Synchronization tools');
+define('ADMIN_AUX_INFO', 'Auxiliary information');
+define('ADMIN_BACK_TO_SHOP', '>> <u>back to the shop</u> &lt;&lt;');
+
+define('ADMIN_PRIMARY_COLUMN', 'Primary column: ');
+define('ADMIN_PRIMARY_COLUMN_DESC', '(please select a column that uniquely indicates a product)<br><b>Please, be careful when selecting this column. Wrong choice can give undefined result(s)!</b>');
+
+define('ADMIN_CHOOSE_CREDIT_CARD_TYPE', 'Please choose payment type that assumes<br>credit card processing using payment gateway');
+define('ADMIN_CONNECTION_TYPE', 'Connection type:');
+define('ADMIN_CHOOSE_GATEWAY', 'Please choose payment gateway');
+define('ADMIN_UPDATE_SUCCESSFUL', '<font color=blue><b>Update successful!</b></font>');
+define('ADMIN_NO_PRODUCT_OPTIONS', 'there are no additional product options available');
+define('ADMIN_ADD_NEW_OPTION', '<b>Add new option:</b>');
+define('ADMIN_UPDATE_GC_VALUE_BUTTON', 'Update `Products Count` value for categories');
+define('ADMIN_ROOT_WARNING', '<font color=red>All products in the root directory are not shown to users!</font>');
+define('ADMIN_ABOUT_PRICES', '<font class=small>// prices are actual for the time of order and shown without taxes //</font>');
+define('ADMIN_NEW_NEWSARTICLE', 'Post news');
+define('ADMIN_CURRENT_DATE', 'Date');
+define('ADMIN_SEND_NEWS_TO_SUBSCRIBERS', 'Send news to subscribers');
+define('ADMIN_NEWS_SUBSCRIBERS', 'News subscribers');
+define('ADMIN_ALL_USERS', 'All customers:');
+define('ADMIN_USERS_WITH_LOGIN_ON', 'Users with login on');
+define('ADMIN_EXPORT_USER_TO_FILE', 'Export users to Excel file');
+define('ADMIN_EXPORT_DB_TO_FILE', 'Export database to SQL file');
+define('ADMIN_USERS_EXPORTED_TO', 'Users successfully exported to Excel file');
+define('ADMIN_DB_EXPORTED_TO', 'Database successfully exported to SQL file');
+define('ADMIN_IMPORT_FROM_SQL', 'Import database from SQL-file<br>(use files made by Shop-Script)<br>
+	<font color=red><b>All your current content will be erased!</b></font>');
+define('ADMIN_SHOP_NAME', 'Shop name');
+define('ADMIN_SHOP_URL', 'Shop URL');
+define('ADMIN_DESKTOP', 'DESKTOP');
+define('ADMIN_MAX_PRODUCTS_COUNT_PER_PAGE', 'Maximum products count per page');
+define('ADMIN_MAX_COLUMNS_PER_PAGE', 'Maximum columns per page');
+define('ADMIN_MAIN_COLORS', 'Three main colors used for table drawing:');
+define('ADMIN_COLOR', 'Color');
+define('ADMIN_CURRENCY_TYPES', 'CURRENCY_TYPES');
+define('ADMIN_CURRENCIES_DESCRIPTION', '
+All currency types are shown in the following table.<br>
+Currency name is shown to the left,<br>it`s value compare to "universal currency" value is in the right column.
+<br>
+To delete a currency just leave<br>
+corresponding fields empty.<br><br>
+');
+define('ADMIN_POPULAR_PRODUCTS', 'The most <b>popular</b> products');
+define('ADMIN_SALABLE_PRODUCTS', '<b>Bestsellers</b>');
+define('ADMIN_OUT_OF_STOCK', '<b><font color=red>Out of stock!</font><b>');
+define('ADMIN_SYNCR_DESC', 'Here you can export current database content to SQL-file (make a backup).<br>
+Then it`s easy to import (restore) it back in this shop or any other, based on Shop-Script<br><br>
+Only content of product and categories tables are possible to export.
+');
+define('ADMIN_START_NEW_POLL', 'start new poll');
+define('ADMIN_POLL_WARNING', '// all results of previous voting will be erased //');
+define('ADMIN_POLL_QUESTION', 'Poll question:');
+define('ADMIN_POLL_OPTIONS', 'Answer options:<br><font class=average color=#555555>(each in a<br>separate line)</font>');
+
+define('ADMIN_CATEGORY_TITLE', 'Categories');
+define('ADMIN_CATEGORY_NEW', 'Create new category');
+define('ADMIN_CATEGORY_PARENT', 'Parent:');
+define('ADMIN_CATEGORY_MOVE_TO', 'Move to:');
+define('ADMIN_CATEGORY_NAME', 'Category name:');
+define('ADMIN_CATEGORY_LOGO', 'Logo:');
+define('ADMIN_CATEGORY_ROOT', 'Root');
+define('ADMIN_CATEGORY_DESC', 'Description');
+
+define('ADMIN_PRODUCT_TITLE', 'Products');
+define('ADMIN_PRODUCT_NEW', 'Create new product');
+define('ADMIN_PRODUCT_PARENT', 'Parent');
+define('ADMIN_PRODUCT_CODE', 'Product code');
+define('ADMIN_PRODUCT_NAME', 'Product name');
+define('ADMIN_PRODUCT_RATING', 'Rating');
+define('ADMIN_PRODUCT_PRICE', 'Price');
+define('ADMIN_PRODUCT_LISTPRICE', 'List price');
+define('ADMIN_PRODUCT_INSTOCK', 'In stock');
+define('ADMIN_PRODUCT_PICTURE', 'Picture');
+define('ADMIN_PRODUCT_THUMBNAIL', 'Thumbnail');
+define('ADMIN_PRODUCT_BIGPICTURE', 'Enlarged picture');
+define('ADMIN_PRODUCT_ROOT', 'Root');
+define('ADMIN_PRODUCT_DESC', 'Description');
+define('ADMIN_PRODUCT_BRIEF_DESC', 'Brief description');
+define('ADMIN_PRODUCT_SHOW_IN_SPECIAL_OFFERS', 'Show in special offers');
+define('ADMIN_PRODUCT_SOLD', 'Sold');
+define('ADMIN_PRODUCT_VOTES', 'Votes');
+
+define('ADMIN_MAIN_MENU', 'Main menu');
+define('ADMIN_IMPORT_FROM_EXCEL_ABOUT', 'About pricelist structure');
+define('ADMIN_DB_UPDATE_SUCCESSFUL', 'Database update successful!');
+define('ADMIN_EXCEL_DESC1', 'Save your pricelist as CSV file and then upload it using following form.<p>Shop-Script will ask you to associate columns from your file with columns<br>in the database and then will update database content.<p>It also recognizes categories (rows with defined columns "Name" (neccessary) and "Description" (optional) - other columns should be empty).<br>
+You can add one or more `!` symbols before the name of the category to set hierarchy (subcategories). More info in example:
+<p><a href="example.csv">Download Excel price list example...</a>');
+
+define('ADMIN_EXCEL_UPDATE_DB', '<b>Update database</b>');
+define('ADMIN_EXCEL_CLEAR_DB', '<b>Clear database:</b>');
+define('ADMIN_EXCEL_CLEAR_DB_DESC', 'Completely clear database (in this option you may not choose your .CSV-file).');
+
+define('ADMIN_IGNORE', '- ignore column');
+define('ADMIN_ADD_AS_NEW_PARAMETER', '- add as an extra parameter of item');
+
+define('ADMIN_IMPORT_DESC1', 'Your file contains following columns.<br>Please associate each column with a database field.<br>If you choose "add as an extra parameter of item" you can also<br>set the name of this extra parameter in the leftmost column.');
+
+define('ADMIN_IMPORT_DESC2', 'If a product was found in the file and in the database (primary columns match), update product`s details.<br>Otherwise - if product wasn`t found in the database - insert it into database.<br>Finally - if product was found in the database only - take no changes');
+
+
+define('ADMIN_CUSTOMER_TITLE', 'Customers');
+define('CUSTOMER_LOGIN', 'Login:');
+define('CUSTOMER_PASSWORD', 'Password:');
+define('CUSTOMER_EMAIL', 'E-mail:');
+define('CUSTOMER_FIRST_NAME', 'First name:');
+define('CUSTOMER_LAST_NAME', 'Last name:');
+define('CUSTOMER_MIDDLE_NAME', 'Middle name:');
+define('CUSTOMER_COUNTRY', 'Country:');
+define('CUSTOMER_ZIP', 'ZIP:');
+define('CUSTOMER_STATE', 'State:');
+define('CUSTOMER_CITY', 'City:');
+define('CUSTOMER_ADDRESS', 'Address:');
+define('CUSTOMER_PHONE_NUMBER', 'Phone number:');
+define('CUSTOMER_SUBSCRIBE_FOR_NEWS', 'Subscribe for newsletter');
+define('CUSTOMER_CONFIRM_PASSWORD', 'Confirm password:');
+
+define('ADMIN_RELATED_PRODUCTS_SELECT', 'Please, select a product');
+define('ADMIN_SELECTED_PRODUCTS', 'Selected products:');
+
+define('ADMIN_PICTURE_NOT_UPLOADED', '(picture not uploaded)');
+
+
+//errors
+
+define('ERROR_FAILED_TO_UPLOAD_FILE', '<b><font color=red>Failed to upload file to the server. Make sure your server allows you to write files on it</font></b>');
+define('ERROR_CANT_FIND_REQUIRED_PAGE', 'Sorry, but requested page wasn`t found of the server');
+define('ERROR_CANT_READ_FILE', 'Couldn`t read file');
+define('ERROR_INPUT_PRODUCT_NAME', 'Please input at least product name');
+define('ERROR_FORBIDDEN', 'Forbidden! You don`t have permission to access this file');
+define('ERROR_WRONG_PASSWORD', 'Wrong login and/or password');
+define('ERROR_INPUT_LOGIN', 'Please input login');
+define('ERROR_INPUT_EMAIL', 'Please input your email');
+define('ERROR_LOGIN_SHOULD_START_WITH_LATIN_SYMBOL', 'Login should start with a latin symbol');
+define('ERROR_WRONG_PASSWORD_CONFIRMATION', 'Wrong password confirmation');
+define('ERROR_INPUT_NAME', 'Please input your name');
+define('ERROR_INPUT_COUNTRY', 'Please input country');
+define('ERROR_INPUT_CITY', 'Please input city');
+define('ERROR_INPUT_ZIP', 'Please input ZIP');
+define('ERROR_INPUT_STATE', 'Please input state');
+define('ERROR_INPUT_ADDRESS', 'Please input your address');
+define('ERROR_INPUT_PHONE', 'Please input your phone number');
+define('ERROR_INPUT_OTCH', 'Please input your middle name');
+define('ERROR_USER_ALREADY_EXISTS', 'Sorry, user with such login is already registered');
+define('ERROR_UPDATE_COLUMN_IS_NOT_SET', 'Primary column is not defined. Update failed.');
+
+
+//questions
+
+define('QUESTION_DELETE_PICTURE', 'Delete picture?');
+define('QUESTION_DELETE_CONFIRMATION', 'Delete?');
+
+//emails
+define('EMAIL_DELETE_CUSTOMER_SUBJECT', 'Your account has been closed');
+define('EMAIL_FORGOT_PASSWORD_SUBJECT', 'Your password');
+define('EMAIL_ADMIN_ORDER_NOTIFICATION_SUBJECT', 'New order!');
+define('EMAIL_CUSTOMER_ORDER_NOTIFICATION_SUBJECT', 'Your order');
+define('EMAIL_MESSAGE_PARAMETERS', 'Content-Type: text/plain; charset="'.DEFAULT_CHARSET.'"');
+define('EMAIL_HELLO', 'Hello');
+define('EMAIL_SINCERELY', 'Sincerely');
+define('EMAIL_THANK_YOU_FOR_SHOPPING_AT', 'Thank you for shopping at');
+define('EMAIL_ORDER_WILL_BE_SHIPPED_TO', 'Your order will be shipped to');
+define('EMAIL_OUR_MANAGER_WILL_CONTACT_YOU', 'Our manager will contact you very soon.');
+define('EMAIL_YOUVE_MODIFIED_YOUR_DATA_AT', 'You`ve modified your registration data at');
+define('EMAIL_YOUVE_BEEN_REGISTERED_AT', 'You`ve been successfully registered at');
+define('EMAIL_REGISTRATION', 'Registration');
+define('EMAIL_YOUR_REGTRATION_INFO', 'Your registration info:');
+define('EMAIL_NEWS_OF', 'News of');
+define('EMAIL_YOUR_PASSWORD', 'Your password');
+define('EMAIL_ACCOUNT_CLOSED', 'Your account has been closed by our manager`s will. Thanks for shopping!');
+
+?>
