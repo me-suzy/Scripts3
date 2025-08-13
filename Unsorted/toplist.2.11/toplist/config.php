9cfd850b432479e6c4f8ee7e47715f52c684ab77
me-suzy <me.suzana@gmail.com>
2025-08-14 02:04:41 +0300

    Add Unsorted/toplist.2.11/toplist/config.php

---
+++ Unsorted/toplist.2.11/toplist/config.php
@@ -0,0 +1,40 @@
+<?
+
+// Config file for Best Top List by Szymon Kosok - http://www.best-scripts.tk
+// Support: support@best-scripts.tk
+//
+// Please edit variables
+//
+// Copyright 2002 (c) Szymon Kosok
+
+// MySQL Connection variables
+
+$dbhost = "localhost"; // database host
+
+$dbuser = "sean"; // your database username
+
+$dbpass = "73xwf48"; // your databse password
+
+$dbname = "balernet_com"; // name of database
+
+// home page
+
+$homepagename = "Bailernet.Com Top Websites On The Internet"; // Name of your home page (not Top List!)
+
+$homepageurl = "http://www.bailernet.com"; // url to your home page
+
+// meta tags
+
+$keywords = "best, top, list, music, php, web host, web sites, free, link exchange"; // keyword of your top list
+
+$description = "Best Top websites on the internet List";
+
+// language
+
+$language = "english";
+
+// version
+
+$version = "2.11";
+
+?>
