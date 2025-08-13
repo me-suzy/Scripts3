040d617c7e1424e17f512a8a245c15f0602ad103
me-suzy <me.suzana@gmail.com>
2025-08-14 01:47:08 +0300

    Add Unsorted/slv18/storyline1.8/base/html/Default/copyright.inc.php

---
+++ Unsorted/slv18/storyline1.8/base/html/Default/copyright.inc.php
@@ -0,0 +1,8 @@
+<table border='0' width='100%'>
+	<tr>
+		<td class='heavydis'>
+			<center class='small'>Powered by Storyline v1.8.0 &copy; 
+			<a href='http://www.iodesigns.co.uk' target='_blank'>IO Designs 2002</a></center>
+		</td>
+	</tr>
+</table>
