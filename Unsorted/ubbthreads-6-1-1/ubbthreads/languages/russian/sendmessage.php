10b7faaff581a6843ff94fcc9f1afd1ebca24a4f
me-suzy <me.suzana@gmail.com>
2025-08-14 02:48:27 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/sendmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/sendmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['CHOOSE_ADD']  = 	"Choose a name";
+$ubbt_lang['TEXT_DELETE']  = 	"Óäàëèòü";
+$ubbt_lang['NO_PRIVATE']  = 	"Ýòîò ïîëüçîâàòåëü íå æåëàåò ïðèíèìàòü ëè÷íûå ñîîáùåíèÿ.";
+$ubbt_lang['NO_RECORD']  = 	"We have no record for the Username that you are trying to send this message to.";
+$ubbt_lang['MESS_PREV']  = 	"Preview private message to";
+$ubbt_lang['PREVIEW_MESS']  = 	"The following is a preview of your message.  If everything looks ok then you can click 'Continue' and your message will be sent.  If not then use the bottom form below to edit some more.";
+$ubbt_lang['EDIT_MORE']  = 	"Edit some more";
+$ubbt_lang['PRIV_SUB']  = 	"Âû èìååòå íîâûå ñîîáùåíèÿ";
+$ubbt_lang['PRIV_BODY_R']  = 	"You have received a private message on the forums at .  You can go to {$config['phpurl']}/login.php to view it.";
+$ubbt_lang['MESS_SENT']  = 	"Your message has been sent.";
+?>
