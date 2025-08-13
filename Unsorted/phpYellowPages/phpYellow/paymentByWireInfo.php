ec652d60adba607b6c034dea40635802c546ddd8
me-suzy <me.suzana@gmail.com>
2025-08-14 01:05:38 +0300

    Add Unsorted/phpYellowPages/phpYellow/paymentByWireInfo.php

---
+++ Unsorted/phpYellowPages/phpYellow/paymentByWireInfo.php
@@ -0,0 +1,27 @@
+<!-- start of paymentByWireInfo.php -->
+<table><tr><td>
+<h3>Bank Information</h3>
+Royal Bank of Canada<br>
+Transit: 8060-003<br>
+
+<p>600 - 777 Royal Oak Drive,
+Victoria, B.C.<br>
+Canada V8X 4VI</p>
+
+<p>Tel: ( 250 ) 356-4756<br>
+Fax: ( 250 ) 356-3424</p>
+
+
+
+<h3>Bank Account Information</h3>
+Pay To: Dreamriver.com<br>
+Legal Name: ABC Software<br>
+</td></tr></table>
+
+
+
+
+
+
+
+<!-- end of paymentByWireInfo.php -->
