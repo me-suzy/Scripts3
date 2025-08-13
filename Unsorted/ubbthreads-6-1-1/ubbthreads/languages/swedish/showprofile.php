6e06ad7bd324c15cbe7b61a898a5c4bd6176afbc
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:39 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/showprofile.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/showprofile.php
@@ -0,0 +1,27 @@
+<?
+$ubbt_lang['RATING']    =       "Betyg";
+$ubbt_lang['T_RATES']   =       "Antal betyg";
+$ubbt_lang['RATE_USER'] =       "Betygsätt denna användare";
+$ubbt_lang['STAR1'] =           "1 stjärna";
+$ubbt_lang['STAR2'] =           "2 stjärnor";
+$ubbt_lang['STAR3'] =           "3 stjärnor";
+$ubbt_lang['STAR4'] =           "4 stjärnor";
+$ubbt_lang['STAR5'] =           "5 stjärnor";
+$ubbt_lang['DORATE']    =       "Skicka betyg";
+$ubbt_lang['YOURATED']  =       "Du gav denna användare betyget ";
+$ubbt_lang['PROF_FOR']  = 	"Profil för";
+$ubbt_lang['TEXT_NAME']  = 	"Namn";
+$ubbt_lang['USER_TITLE']  = 	"Titel";
+$ubbt_lang['TOTAL_POSTS']  = 	"Inlägg";
+$ubbt_lang['TEXT_HOME']  = 	"Hemsida";
+$ubbt_lang['TEXT_OCCU']  = 	"Sysselsättning";
+$ubbt_lang['TEXT_HOBBY']  = 	"Hobbyer";
+$ubbt_lang['TEXT_LOC']  = 	"Hemort";
+$ubbt_lang['TEXT_BIO']  = 	"Presentation";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ-nummer";
+$ubbt_lang['REGGED_ON']  = 	"Registrerad den";
+$ubbt_lang['EDIT_T_U']  = 	"Redigera användaren";
+$ubbt_lang['ADD_BOOK']  = 	"Lägg till i adressboken";
+$ubbt_lang['SHOW_ALL_P']  = 	"Visa användarens alla inlägg";
+$ubbt_lang['NO_LONGER']  = 	"Den här användaren finns inte längre i vår databas.";
+?>
