d4267ad46cee57fd49aafad1c6f81e3f6756cf6c
me-suzy <me.suzana@gmail.com>
2025-08-14 01:44:14 +0300

    Add Unsorted/siteworks_pro_v5[1].0_null/swp50n/admin/config.default.php

---
+++ Unsorted/siteworks_pro_v5[1].0_null/swp50n/admin/config.default.php
@@ -0,0 +1,100 @@
+<?php
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//   Program Name         : SiteWorks Professional                           //
+//   Release Version      : 5.0                                              //
+//   Program Author       : SiteCubed Pty. Ltd.                              //
+//   Supplied by          : CyKuH [WTN]                                      //
+//   Nullified by         : CyKuH [WTN]                                      //
+//   Packaged by          : WTN Team                                         //
+//   Distribution         : via WebForum, ForumRU and associated file dumps  //
+//                                                                           //
+//                       WTN Team `2000 - `2002                              //
+///////////////////////////////////////////////////////////////////////////////
+$isSetup = 'no'; 
+
+$appName = 'SiteWorks Professional'; 
+
+$appVersion = '5.0'; 
+
+$siteName = "Your Site"; 
+
+$siteURL = "http://www.yoursite.com"; 
+
+$siteLogo = "images/logo.gif"; 
+
+$template = "template1"; 
+
+$ezinePopup = true; 
+
+$showNumUsers = false; 
+
+$showFeaturedBook = true; 
+
+$showAuthorImages = true; 
+
+$showXML = true; 
+
+$showHandy = "Handy Tip"; 
+
+$showMy2c = "My 2c Worth"; 
+
+$siteClosed = false; 
+
+$siteCloseMessage = "This site is closed due to maintenance being performed by our developers. Please come back at a later time.\r\n\r\nIf you would like to contact us, you can email us at: <<email>>"; 
+
+$siteComments = true; 
+
+$siteCommentsApprove = false; 
+
+$siteCommentsShow = "3"; 
+
+$siteKeywords = "Enter, keywords, that, describe, your, site, here"; 
+
+$siteDescription = "Enter a short description of your site here."; 
+
+$adminName = "Your Name"; 
+
+$adminEmail = "you@yoursite.com"; 
+
+$dbServer = ""; 
+
+$dbUser = ""; 
+
+$dbPass = ""; 
+
+$dbDatabase = ""; 
+
+$ibMaxFileSize = "50000"; 
+
+$ibTypes = array("image/gif", "image/jpg", "image/pjpeg", "image/jpeg");
+
+$recsPerPage = 20; 
+
+$authorsPerPage = 10; 
+
+$newsPerPage = 10; 
+
+$articlesPerPage = 20; 
+
+$isVBulletinForum = false; 
+
+$isPHPBBForum = false; 
+
+$forumDBServer = "[None]"; 
+
+$forumDBUser = "[None]"; 
+
+$forumDBPass = "[None]"; 
+
+$forumDBName = "[None]"; 
+
+$forumDBPath = "[None]"; 
+
+$forumDBPrefix = "[None]"; 
+
+$authorEmail = "Hi <<AuthorName>>,\r\nYour login details for <<SiteName>> (<<SiteURL>>/admin) are shown below:\r\n\r\nUsername: <<Username>>\r\nPassword: <<Password>>\r\n\r\nRegards,\r\n<<AdminName>>\r\nmailto:<<AdminEmail>>\r\n<<SiteURL>>"; 
+
+
+
+?>
