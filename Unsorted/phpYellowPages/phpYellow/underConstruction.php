beb8a95910a3cdd1e046d6776786616373976b96
me-suzy <me.suzana@gmail.com>
2025-08-14 01:05:53 +0300

    Add Unsorted/phpYellowPages/phpYellow/underConstruction.php

---
+++ Unsorted/phpYellowPages/phpYellow/underConstruction.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<BODY>
+
+<p style="font-size:120px;color:red;font-weight:bold;font-family:Arial;">Under<br>Construction</p>
+
+</body>
+</html>
+
+
