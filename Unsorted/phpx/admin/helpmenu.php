da1ab094285cebb7dfe5b5ae3bd7440729ef6c3b
me-suzy <me.suzana@gmail.com>
2025-08-14 01:00:37 +0300

    Add Unsorted/phpx/admin/helpmenu.php

---
+++ Unsorted/phpx/admin/helpmenu.php
@@ -0,0 +1,10 @@
+<?php
+# $Id: helpmenu.php,v 1.1 2003/07/08 18:29:56 ryan Exp $
+//! Creates Help menu on left side of pop-up frame
+require("includes/help.inc.php");
+require("includes/auth.inc.php");
+
+new helpDocs("menu");
+
+?>
+
