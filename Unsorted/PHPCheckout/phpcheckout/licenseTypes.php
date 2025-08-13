3b680f6186f101e4b9ac9df7cd54b60a8bcbfbbd
me-suzy <me.suzana@gmail.com>
2025-08-14 00:32:32 +0300

    Add Unsorted/PHPCheckout/phpcheckout/licenseTypes.php

---
+++ Unsorted/PHPCheckout/phpcheckout/licenseTypes.php
@@ -0,0 +1,42 @@
+<select name="license" size="1">
+<option value="<?php echo $license;?>" SELECTED><?php echo $license;?></option>
+<!-- edit any license below this line - delete, add or change -->
+
+<option value="Artifex Public License (AFPL)">Artifex Public License (AFPL)
+<option value="Artistic License">Artistic License
+<option value="BSD License">BSD License
+<option value="Eiffel Forum License (EFL)">Eiffel Forum License (EFL)
+<option value="Free For Educational Use">Free For Educational Use
+<option value="Free for non-commercial use">Free for non-commercial use
+<option value="Free To Use But Restricted">Free To Use But Restricted
+<option value="Freely Distributable">Freely Distributable
+<option value="Freeware">Freeware
+<option value="GNU Free Documentation License (FDL)">GNU Free Documentation License (FDL)
+<option value="GNU General Public License (GPL)">GNU General Public License (GPL)
+<option value="GNU Lesser General Public License (LGPL)">GNU Lesser General Public License (LGPL)
+<option value="IBM Public License">IBM Public License
+<option value="MIT/X Consortium License">MIT/X Consortium License
+<option value="MITRE Collaborative Virtual Workspace License (CVW)">MITRE Collaborative Virtual Workspace License (CVW)
+<option value="Mozilla Public License (MPL)">Mozilla Public License (MPL)
+<option value="Netscape Public License (NPL)">Netscape Public License (NPL)
+<option value="Nokia Open Source License (NOKOS)">Nokia Open Source License (NOKOS)
+<option value="OSI Approved">OSI Approved
+<option value="Other">Other
+<option value="Proprietary License">Proprietary License
+<option value="Proprietary License with Free Trial">Proprietary License with Free Trial
+<option value="Proprietary License with Source">Proprietary License with Source
+<option value="Public Domain">Public Domain
+<option value="Python License">Python License
+<option value="QT Public License (QPL)">QT Public License (QPL)
+<option value="Ricoh Source Code Public License">Ricoh Source Code Public License
+<option value="Shareware">Shareware
+<option value="SUN Binary Code License">SUN Binary Code License
+<option value="SUN Community Source License">SUN Community Source License
+<option value="The Apache License">The Apache License
+<option value="The Clarified Artistic License">The Clarified Artistic License
+<option value="The PHP License">The PHP License
+<option value="Voxel Public License (VPL)">Voxel Public License (VPL)
+<option value="zlib/libpng License">zlib/libpng License
+<option value="Zope Public License (ZPL)">Zope Public License (ZPL)
+<option value="Not Applicable">Not Applicable	
+	</select>
