375d13626ec900ba7e2b7abe3b0e5369b79c2d75
me-suzy <me.suzana@gmail.com>
2025-08-14 02:31:34 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/search.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/search.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['SEARCH_INFO']  = 	"Udfyld felterne herunder for at søge.";
+$ubbt_lang['SEARCH_FORUM']  = 	"Hvilket forum skal der søges i?";
+$ubbt_lang['ALL_FORUMS']  = 	"Alle fora";
+$ubbt_lang['SEARCH_WORDS']  = 	"Søg efter disse ord";
+$ubbt_lang['SEARCH_OPT']  = 	"Søgemuligheder";
+$ubbt_lang['SEARCH_ENT']  = 	"Hele sætningen";
+$ubbt_lang['SEARCH_AND']  = 	"Og";
+$ubbt_lang['SEARCH_OR']  = 	"Eller";
+$ubbt_lang['SEARCH_USER']  = 	"Søg efter brugernavn";
+$ubbt_lang['SEARCH_DATE']  = 	"Dato";
+$ubbt_lang['SEARCH_1DAY']  = 	"Nyere end 1 dag";
+$ubbt_lang['SEARCH_2DAY']  = 	"Nyere end 2 dage";
+$ubbt_lang['SEARCH_1WEEK']  = 	"Nyere end 1 uge";
+$ubbt_lang['SEARCH_2WEEK']  = 	"Nyere end 2 uger";
+$ubbt_lang['SEARCH_3WEEK']  = 	"Nyere end 3 uger";
+$ubbt_lang['SEARCH_1MONTH']  = 	"Nyere end 1 måned";
+$ubbt_lang['SEARCH_3MONTH']  = 	"Nyere end 3 måneder";
+$ubbt_lang['SEARCH_6MONTH']  = 	"Nyere end 6 måneder";
+$ubbt_lang['SEARCH_1YEAR']  = 	"Nyere end 1 år";
+$ubbt_lang['SEARCH_ALLPOST']  = 	"All posts";
+$ubbt_lang['SEARCH_NUM']  = 	"Antal resultater, der skal vises pr. side (Dette bliver ignoreret, når der søges i alle fora)";
+?>
