6583a32948cddf876676daf978aabb2ac9c4deb4
me-suzy <me.suzana@gmail.com>
2025-08-14 02:20:29 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/altertable-5.4.2-5.4.3.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/altertable-5.4.2-5.4.3.php
@@ -0,0 +1,73 @@
+<?
+/*
+# UBB.threads, Version 6
+# Official Release Date for UBB.threads Version6: 06/05/2002
+
+# First version of UBB.threads created July 30, 1996 (by Rick Baker).
+# This entire program is copyright Infopop Corporation, 2002.
+# For more info on the UBB.threads and other Infopop
+# Products/Services, visit: http://www.infopop.com
+
+# Program Author: Rick Baker.
+
+# You may not distribute this program in any manner, modified or otherwise,
+# without the express, written written consent from Infopop Corporation.
+
+# Note: if you modify ANY code within UBB.threads, we at Infopop Corporation
+# cannot offer you support-- thus modify at your own peril :)
+# ---------------------------------------------------------------------------
+*/      
+// Require the library
+   require ("main.inc.php");  
+
+// Postgres handles the INT type differently.  So we need to prepare for this
+  $int4  = "INT(4) UNSIGNED";
+  $int9  = "INT(9) UNSIGNED";
+  $int11 = "INT(11) UNSIGNED";
+  $int1  = "INT(1) UNSIGNED";
+
+// Althout Oracle currently isn't supported, this is here for future versions
+// Oracle uses VARCHAR2 instead of VARCHAR and does not have TEXT
+  $varchar = "varchar";
+  $text    = "text";
+
+  if ($config['dbtype'] == "postgres") {
+    $int4  = "INT4";
+    $int9  = "INT4";
+    $int11 = "INT4";
+    $int1  = "INT4";
+  }
+  elseif ($config['dbtype'] == "Oracle") {
+    $int4    = "NUMBER(4)";
+    $int9    = "NUMBER(9)";
+    $int11   = "NUMBER(11)";
+    $int1    = "NUMBER(1)";
+    $varchar = "VARCHAR2";
+    $text    = "VARCHAR(4000)"; // Oracle8 allows 4000, Oracle 7 allows 2000
+  }
+
+//
+// Figure out if the SQL server supports AUTO INCREMENT
+  $autoincs = "";
+
+  if ($config['dbtype'] == 'mysql') {
+    $autoincs = "AUTO_INCREMENT";
+  }
+
+  echo "Adding fields to store the signatures and edit times separately...\n";
+  $query = " 
+    ALTER TABLE {$config['tbprefix']}Posts
+    ADD   B_Signature $varchar(255),
+    ADD   B_LastEdit $int9,
+    ADD   B_LastEditBy $varchar(64)
+  ";
+  $dbh -> do_query($query);
+
+// Disconnect
+  echo "Done...<br>"; 
+
+  if ($auto) {
+    echo "<br><br><a href=\"upgrade.php\">Click here</a> to see if there are any more altertables that need to be run.";
+   }
+?>
+
