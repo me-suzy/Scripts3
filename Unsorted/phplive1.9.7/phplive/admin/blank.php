b350ebe82f8f14234c409ce41663f2d4eb011a8d
me-suzy <me.suzana@gmail.com>
2025-08-14 00:46:53 +0300

    Add Unsorted/phplive1.9.7/phplive/admin/blank.php

---
+++ Unsorted/phplive1.9.7/phplive/admin/blank.php
@@ -0,0 +1,21 @@
+<?
+	/*******************************************************
+	* COPYRIGHT OSI CODES - PHP Live!
+	*******************************************************/
+?>
+<html>
+<head>
+<title> Operator [ visitor traffic monitor ] </title>
+<!-- copyright OSI Codes Inc. http://www.osicodes.com [DO NOT DELETE] -->
+<link rel="Stylesheet" href="../css/base.css">
+
+<script language="JavaScript">
+<!--
+//-->
+</script>
+
+</head>
+<body bgColor="#FFFFFF" text="#000000" link="#35356A" vlink="#35356A" alink="#35356A" marginheight="0" marginwidth="0" topmargin="0" leftmargin="2">
+<!-- copyright OSI Codes, http://www.osicodes.com [DO NOT DELETE] -->
+</body>
+</html>
