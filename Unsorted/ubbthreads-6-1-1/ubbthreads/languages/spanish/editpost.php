c1f7ede192d44633bafcd68eb007e0b0d3a6c8d6
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:32 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "Mover este mensaje y contestaciones";
+$ubbt_lang['DELETE_THREAD']  =  "Borrar este tema";
+$ubbt_lang['NO_EDIT']  = 	"No puedes editar este mensaje.";
+$ubbt_lang['EDITTIME']  = 	"Este mensaje no puede ser editado puesto que ya caduc&oacute; el plazo de edici&oacute;n";
+$ubbt_lang['PEDIT_HEAD']  = 	"Editar Mensaje";
+$ubbt_lang['PEDIT_BODY']  = 	"Editar o Borrar este mensaje.";
+$ubbt_lang['EDIT_POLL']  = 	"Puesto que este mensaje tiene una encuesta puedes borrarla pero no editarla.";
+$ubbt_lang['POST_ICON']  = 	"Insertar Icono";
+$ubbt_lang['MARK_EDIT']  = 	"&iquest;Sellar como editado?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Cambiar este mensaje";
+$ubbt_lang['PEDIT_DELETE']  = 	"Borrar este mensaje";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Aceptar este mensaje";
+$ubbt_lang['RET_NORM']  = 	"Volver a fecha normal";
+$ubbt_lang['MAKE_STICKY']  = 	"Mensaje persistente";
+$ubbt_lang['MANAGE_THREAD']  = 	"Manejar esta cadena";
+$ubbt_lang['OPEN_THREAD']  = 	"Abrir esta cadena";
+$ubbt_lang['CLOSE_THREAD']  = 	"Cerrar esta cadena";
+$ubbt_lang['UNKEEP_THREAD']  = 	"No guardar esta cadena";
+$ubbt_lang['KEEP_THREAD']  = 	"Guardar esta cadena";
+$ubbt_lang['MOVE_THREAD']  = 	"Mover esta cadena";
+?>
