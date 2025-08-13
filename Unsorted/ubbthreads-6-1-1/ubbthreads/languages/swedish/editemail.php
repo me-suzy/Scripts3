84cddf139fe4c169507874454fb1ffcd730a22ed
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:59 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/editemail.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/editemail.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['ADMIN_EMAIL'] = "From time to time we send out emails regarding this community.  Do you wish to receive these emails?";
+$ubbt_lang['HTMLEMAIL'] = "Do you want your emails in HTML format or plain text.";
+$ubbt_lang['HTMLFORMAT'] = "In HTML";
+$ubbt_lang['PLAINTEXT']  = "In plain text";
+$ubbt_lang['NO_PROF']  = 	"Kunde inte hitta en profil för användarnamnet";
+$ubbt_lang['EMAIL_HEAD']  = 	"Redigera e-postinställningar för";
+$ubbt_lang['PROF_EDIT']  = 	"Du kan ändra valfri info nedan.";
+$ubbt_lang['PROF_SUBS']  = 	"E-postunderrättelse / Vidarebefodrande";
+$ubbt_lang['PROF_NOTIF']  = 	"Vill du ha e-postunderrättelse när du fått ett privat meddelande?";
+$ubbt_lang['PROF_REPLIES']  = 	"Vill du ha e-postunderrättelse när du fått svar på ditt inlägg?";
+$ubbt_lang['SUB_STAT']  = 	"E-postmeddelande om alla inlägg i följande forum. (Du kommer få ett dagligt arkiv över alla inlägg i ett forum till din riktiga e-postadress.)";
+$ubbt_lang['SUB_SHOW']  = 	"(B - Beställ ; A - Avbeställ)";
+$ubbt_lang['TEXT_S']  = 	"B";
+$ubbt_lang['TEXT_U']  = 	"A";
+?>
