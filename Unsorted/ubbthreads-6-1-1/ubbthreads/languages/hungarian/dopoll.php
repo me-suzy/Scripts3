de627b86851bf30397b1347f6db1eb8efcca0413
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:36 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/dopoll.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/dopoll.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NO_VOTE']  = 	"You didn't vote for anything!";
+$ubbt_lang['POLL_REGED']  = 	"Only registered users that are logged in are allowed to vote.";
+$ubbt_lang['POLL_IP']  = 	"We already have a vote for this poll from this IP address.";
+$ubbt_lang['POLL_USER']  = 	"We already have a vote for this poll from your Username.";
+?>
