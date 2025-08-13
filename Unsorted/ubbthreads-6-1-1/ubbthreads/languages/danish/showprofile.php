52dfb9c5ade8f5dc346152c6f10ff2a65847fd20
me-suzy <me.suzana@gmail.com>
2025-08-14 02:31:48 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/showprofile.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/showprofile.php
@@ -0,0 +1,27 @@
+<?
+$ubbt_lang['RATING']    =       "Rating";
+$ubbt_lang['T_RATES']   =       "Total Rates";
+$ubbt_lang['RATE_USER'] =       "Rate this user";
+$ubbt_lang['STAR1'] =           "1 star";
+$ubbt_lang['STAR2'] =           "2 stars";
+$ubbt_lang['STAR3'] =           "3 stars";
+$ubbt_lang['STAR4'] =           "4 stars";
+$ubbt_lang['STAR5'] =           "5 stars";
+$ubbt_lang['DORATE']    =       "Submit rating";
+$ubbt_lang['YOURATED']  =       "You rated this user a ";
+$ubbt_lang['PROF_FOR']  = 	"Profil for";
+$ubbt_lang['TEXT_NAME']  = 	"Navn";
+$ubbt_lang['USER_TITLE']  = 	"Title";
+$ubbt_lang['TOTAL_POSTS']  = 	"Samlede antal indlæg";
+$ubbt_lang['TEXT_HOME']  = 	"Netsted";
+$ubbt_lang['TEXT_OCCU']  = 	"Beskæftigelse";
+$ubbt_lang['TEXT_HOBBY']  = 	"Fritidsinteresser";
+$ubbt_lang['TEXT_LOC']  = 	"By/land";
+$ubbt_lang['TEXT_BIO']  = 	"Om brugeren";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ-nummer";
+$ubbt_lang['REGGED_ON']  = 	"Registrede sig selv som bruger den";
+$ubbt_lang['EDIT_T_U']  = 	"Edit this user";
+$ubbt_lang['ADD_BOOK']  = 	"Add to address book";
+$ubbt_lang['SHOW_ALL_P']  = 	"Show all user's posts";
+$ubbt_lang['NO_LONGER']  = 	"Denne bruger findes ikke længere i vores database.";
+?>
