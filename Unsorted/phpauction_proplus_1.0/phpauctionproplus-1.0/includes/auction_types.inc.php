21f90ffe6d5323da636c0f4fbfd30b75033c62f1
me-suzy <me.suzana@gmail.com>
2025-08-14 00:15:31 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/auction_types.inc.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/auction_types.inc.php
@@ -0,0 +1,14 @@
+<?#//v.1.0.0
+  
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+
+	$auction_types = array ( 
+		1 => "Standard auction",
+		2 => "Dutch auction"
+	);
+?>
