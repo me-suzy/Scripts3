14d1f551479bf102405bb1941339bd3a0a0dc9b9
me-suzy <me.suzana@gmail.com>
2025-08-14 01:09:24 +0300

    Add Unsorted/postaffiliate/user/members_only.php

---
+++ Unsorted/postaffiliate/user/members_only.php
@@ -0,0 +1,18 @@
+<?
+  session_start();
+
+  include "../affconfig.php";
+  include "./lang/$language";
+
+  if(!aff_check_security())
+  {
+    aff_redirect('index.php');
+    exit;
+  }
+  
+  include "header.php"; 
+
+  echo "<br><H4>Welcome</h4>";
+  
+  include "footer.php";          
+?>
