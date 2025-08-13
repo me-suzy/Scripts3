c8e87fb13819569bb45895afb3103d5b72c68405
me-suzy <me.suzana@gmail.com>
2025-08-14 02:24:43 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/admin/dokeepthreads.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/admin/dokeepthreads.php
@@ -0,0 +1,77 @@
+<?
+/*
+# UBB.threads, Version 6
+# Official Release Date for UBB.threads Version6: 06/05/2002
+
+# First version of UBB.threads created July 30, 1996 (by Rick Baker).
+# This entire program is copyright Infopop Corporation, 2002.
+# For more info on the UBB.threads and other Infopop
+# Products/Services, visit: http://www.infopop.com
+
+# Program Author: Rick Baker.
+
+# You may not distribute this program in any manner, modified or otherwise,
+# without the express, written written consent from Infopop Corporation.
+
+# Note: if you modify ANY code within UBB.threads, we at Infopop Corporation
+# cannot offer you support-- thus modify at your own peril :)
+# ---------------------------------------------------------------------------
+*/
+
+// Require the library
+   require ("../main.inc.php");
+
+// -----------------
+// Get the user info
+   $userob = new user;
+   $user = $userob -> authenticate();
+   $html = new html;
+
+// -----------------------------
+// Make sure they should be here
+   if ( ($user['U_Status'] != 'Moderator') && ($user['U_Status'] != 'Administrator')){
+      $html -> not_right ("You must be logged in, and be a valid administrator or moderator to access this.",$Cat);
+   }
+
+// --------------------------------------
+// If this is a single thread, we do this
+   $Keyword_q = addslashes($Keyword);
+   $Closed_q = "K";
+   if ($Option == "single") {
+      $query = "
+        UPDATE {$config['tbprefix']}Posts
+        SET    B_Kept  = '$Closed_q'
+        WHERE  B_Main  = '$Number'
+      ";
+      $dbh -> do_query($query);
+   }
+   else {
+   // --------------------------------------------------------
+   // Lets cycle through the list and close the proper threads
+      for ( $i=1;$i<=$Total;$i++){
+         if( $HTTP_POST_VARS[$i] ) {
+            $query = "
+              UPDATE {$config['tbprefix']}Posts 
+              SET   B_Kept  = '$Closed_q'
+              WHERE B_Main  = '$HTTP_POST_VARS[$i]' 
+            ";
+            $dbh -> do_query($query);
+         }
+      }
+   }
+
+// -------------------------------------------------------------
+// Send them a page (Either the admin page or the postlist page)
+   if (!$from) {
+      $html -> send_header ("Threads kept.",$Cat,"<META HTTP-EQUIV=\"Refresh\" content=\"5;url={$config['phpurl']}/admin/main.php?Cat=$Cat\">",$user);
+   }
+   else {
+     $html -> send_header ("Threads kept.",$Cat,"<META HTTP-EQUIV=\"Refresh\" content=\"5;url={$config['phpurl']}/postlist.php?Cat=$Cat&Board=$Keyword&page=$page&view=$view&sb=$sb&o=$o\">",$user);
+   }
+   $html -> admin_table_header("Threads kept.");
+   echo "<TABLE BORDER=0 WIDTH=\"95%\" ALIGN=\"center\"><TR><TD class=\"cleartable\"><span class=\"onbody\">";  
+   echo "All of the threads that you checked have been marked as kept and now cannot be deleted by the autodelete feature.  You will now be returned to the main administration page.";
+   echo "</span></TD></TR></TABLE>";
+   $html -> send_admin_footer();
+
+?>
