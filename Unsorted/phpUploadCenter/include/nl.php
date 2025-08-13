a012d7657608947ddaf5d0a186e87b6d4b571c7c
me-suzy <me.suzana@gmail.com>
2025-08-14 01:00:07 +0300

    Add Unsorted/phpUploadCenter/include/nl.php

---
+++ Unsorted/phpUploadCenter/include/nl.php
@@ -0,0 +1,235 @@
+<?
+
+// Header and footer files
+
+$headerpage="include/header.htm";
+$footerpage="include/footer.htm";
+$infopage="include/info.htm";
+
+$charsetencoding="iso-8859-1";
+$uploadcentercaption="SatTV - Upload Center";
+$uploadcentermessage="Het kijken naar Betaal TV zonder geldig abonnement is iligaal!<br>Alle bestanden die zich hier bevinden zijn uitsluitend voor educatieve doeleinden!<br>Niemand draagt verantwoordelijkheid voor het plaatsen van deze bestanden, hun inhoud,<br>en de eventuele schade die ze zouden kunnen aanrichten aan uw systeem!";
+
+$mess=array(
+"0" => "",
+"1" => "Januari",
+"2" => "Februari",
+"3" => "Maart",
+"4" => "April",
+"5" => "Mei",
+"6" => "Juni",
+"7" => "Juli",
+"8" => "Augustus",
+"9" => "September",
+"10" => "Oktober",
+"11" => "November",
+"12" => "December",
+"13" => "Vandaag",
+"14" => "Gisteren",
+"15" => "Bestandsnaam",
+"16" => "Waardering",
+"17" => "Groote",
+"18" => "Geupload",
+"19" => "Omschrijving",
+"20" => "Upload bestand",
+"21" => "Lokaal bestand",
+"22" => "Bestands omschrijving",
+"23" => "Download",
+"24" => "Volgorde",
+"25" => "Home",
+"26" => "Bestand",
+"27" => "Afdrukken",
+"28" => "Sluit",
+"29" => "Ga terug",
+"30" => "Dit bestand is verwijderd",
+"31" => "Bestand kan niet geopend worden",
+"32" => "Ga terug",
+"33" => "Fout tijdens upload !",
+"34" => "U moet een bestand selecteren",
+"35" => "terug",
+"36" => "Het Bestand",
+"37" => "is succesvol geupload",
+"38" => "Bestand met naam",
+"39" => "bestaat al",
+"40" => "Het bestand is succesvol geupload",
+"41" => "Taal succesvol gekozen [Vertaald door Salem]",
+"42" => "Welkom bij het Sat-TV Upload Center!",
+"43" => "Totaal gebruikte ruimte",
+"44" => "Laat bestanden zien voor alle dagen",
+"45" => "Ongeldig ZIP archief!",
+"46" => "Archief inhoud:",
+"47" => "Datum/Tijd",
+"48" => "Directory",
+"49" => "Het uploaden is niet toegestaan voor het bestand met naam %s",
+"50" => "Overschrijd maximum toegestane groote",
+"51" => "Informatie",
+"52" => "Kies Skin",
+"53" => "Skin",
+"54" => "Welkom",
+"55" => "Huidige Tijd",
+"56" => "Gebruiker",
+"57" => "Gebruikersnaam",
+"58" => "Register",
+"59" => "Registratie",
+"60" => "Zondag",
+"61" => "Maandag",
+"62" => "Dinsdag",
+"63" => "Woensdag",
+"64" => "Donderdag",
+"65" => "Vrijdag",
+"66" => "Zaterdag",
+"67" => "Verstuur",
+"68" => "Verstuur Bestand",
+"69" => "Bestand is verstuurd naar %s address.",
+"70" => "Bestand geupload door user: %s",
+"71" => "Log in",
+"72" => "Log uit",
+"73" => "Enter",
+"74" => "Anoniem",
+"75" => "Normale gebruiker",
+"76" => "Power gebruiker",
+"77" => "Administrator",
+"78" => "Prive zone",
+"79" => "Publieke zone",
+"80" => "U gaf een ongeldige gebruikersnaam of password.",
+"81" => "Mijn profiel",
+"82" => "Bekijk/wijzig mijn profiel",
+"83" => "Password",
+"84" => "Kies taal",
+"85" => "Kies tijdzone",
+"86" => "Uw huidige tijd",
+"88" => "Gebruik alstublieft een geldig e-mail adres.",
+"89" => "Uw activatiecode zal verstuurd worden naar uw e-mail.",
+"90" => "Bestand geupload: ",
+"91" => "Geef het e-mailadres dat u gebruikte tijdens de registratie.",
+"92" => "Bestands groote: ",
+"93" => "Noteer alstublieft uw gebruikersnaam en password",
+"94" => "Registratie is noodzakelijk. Registreer alstublieft.",
+"95" => "Registratie is niet noodzakelijk. U kunt registreren als u uw naam wilt vermelden bij al uw uploads. Niemand anders dan u kan uw naam gebruiken voor het uploaden van bestanden.",
+"96" => "Skin gekozen.",
+"97" => "Ververs",
+"98" => "Typ uw gebruikersnaam en wachtwoord alstublieft",
+"99" => "Nog niet geregistreerd? - Registreer hier!",
+"100" => "Wachtwoord vergeten?",
+"101" => "Ga %s Terug %s en probeer opnieuw.",
+"102" => "U bent uitgelogd.",
+"103" => "Gebruikersnaam is ongeldig. De naam mag niet langer zijn dan 12 tekens en mag alleen latijnse letters cijfers en tekens bevatten.De gebruikersnaam mag ook '-', '_', en spaties bevatten.",
+"104" => "De '%s' door u gekozen is al in gebruik.",
+"105" => "Bevestig wachtwoord",
+"106" => "Wachtwoorden komen niet overeen.",
+"107" => "Het formaat van het e-mail adres is ongeldig.",
+"108" => "Hartelijk dank voor het registreren. Onthoud uw wachtwoord goed aangezien het gecodeerd in onze database is opgeslagen en we het daarom niet kunnen terughalen. Mocht u onverhoopt toch uw wachtwoord verliezen dan bieden wij u een makkelijk te gebruiken script wat een nieuw gegenereerd wachtwoord naar uw e-mail adres stuurt.",
+"109" => "U kunt %s hier het Upload Center binnengaan. %s",
+"110" => "Uw activatie code is naar uw e-mail adres verstuurd. U moet deze activatie code binnen 2 dagen gebruiken om uw account te activeren,anders zal uw account worden verwijderd.",
+"111" => "
+Beste %s,
+
+Bedankt voor uw registratie bij ons Upload Center.
+
+Omdat uw veiligheid erg belangrijk voor ons is ,vragen wij u uw account te activeren met een activatie code.
+
+Uw persoonlijke activatie code is: %s
+(Let op: Dit is niet uw wachtwoord)
+
+Activatie van uw account is simpel:
+1. Bezoek ons op $installurl/activate.php en we zullen u stap voor stap door het proces leiden.
+2. Typ uw account naam en activatie code.
+3. klik op de 'Activeer account' knop.
+
+Met vriendelijke groeten,
+$admin_name
+Email: mailto:$admin_email
+Web Page: $installurl",
+"112" => "Activeer account",
+"113" => "Activeer uw account",
+"114" => "Activatie code",
+"115" => "Uw account is nu actief.",
+"116" => "Please %s enter here %s.",
+"117" => "De opgegeven account naam of activatie code is ongeldig.",
+"118" => "Account is al actief.",
+"119" => "Ik wil per e-mail een dagelijks overzicht ontvangen van alle nieuwe files.",
+"120" => "Wijzig uw wachtwoord.",
+"121" => "Uw oude wachtwoord",
+"122" => "De opgegeven account naam bestaat niet.",
+"123" => "Het opgegeven e-mail adres is ongeldig.",
+"124" => "Uw nieuwe password is naar uw e-mail adres verstuurd.",
+"125" => "
+Best Upload Center gebruiker,
+
+Uw nieuwe Upload Center wachtwoord is %s
+
+Met vriendelijke groeten,
+$admin_name
+Email: mailto:$admin_email
+Web Page: $installurl",
+"126" => "Wijzig uw account instellingen",
+"127" => "bevestig",
+"128" => "Uw profiel is opgeslagen.",
+"129" => "Uw wachtwoord is gewijzigd.",
+"130" => "U gaf een ongeldig oud wachtwoord.",
+"131" => "U moet een nieuw wachtwoord opgeven.",
+"132" => "CONFIGURATIE",
+"133" => "Upload",
+"134" => "Taal & Tijdzone",
+"135" => "Account statistieken",
+"136" => "Uw account is aangemaakt:",
+"137" => "Gebruikers beheer",
+"138" => "Viewer (Alleen bekijken)",
+"139" => "Uploader (Alleen uploaden)",
+"140" => "Account '%s' succevol veranderd",
+"141" => "Nieuwste",
+"142" => "Allemaal",
+"143" => "Het nieuwe e-mail adres is geldig na confirmatie. De Confirmatie code is naar uw nieuwe e-mail adres verstuurd. Zie voor verdere instructies in de e-mail.",
+"144" => "
+Beste %s,
+
+Omdat uw veiligheid erg belangrijk voor ons is ,vragen wij u uw nieuwe e-mail adres te confimeren met een confirmatie code.
+
+Uw persoonlijke confirmatie code is: %s
+
+Confirmatie van een e-mail addres is simpel:
+1. Bezoek ons op $installurl/confirm.php en we zullen u stap voor stap door het proces leiden.
+2. Geef uw account naam en confirmatie code.
+3. klik op de 'Bevestig' knop.
+
+Met vriendelijke groeten,
+$admin_name
+Email: mailto:$admin_email
+Web Page: $installurl",
+"145" => "Bevestig uw nieuwe e-mail adres.",
+"146" => "Confirmatie code",
+"147" => "Bevestig",
+"148" => "Niets om te bevestigen.",
+"149" => "De opgegeven account naam of confirmatie code is ongeldig.",
+"150" => "Uw nieuwe e-mail addres '%s' bevestigd.",
+"151" => "Bestanden geupload",
+"152" => "Bestanden gedownload",
+"153" => "Bestanden per mail",
+"154" => "Account aangemaakt",
+"155" => "Laatst bezocht op",
+"156" => "Status",
+"157" => "Actieve status",
+"158" => "Ontvang overzicht",
+"159" => "e-mail",
+"160" => "Totaal:",
+"161" => "account(s)",
+"162" => "Verwijder account",
+"163" => "Shown %s account(s) van %s",
+"164" => "Configureer Upload Center",
+"165" => "Edit bestanden",
+"166" => "Edit bestand",
+"167" => "Bestand %s is gewijzigd",
+"168" => "Opslaan",
+"169" => "Verwijder",
+"170" => "Verwijder bestanden",
+"171" => "Mirror",
+"172" => "Ja",
+"173" => "Nee",
+"174" => "Actief",
+"175" => "Niet actief",
+"176" => "Niet Toegestaan",
+"177" => "Sorry, but server could not execute the mail program.",
+"178" => "Your registration failed. Please, try again later.",
+"179" => "Please, try again later."
+);
+?>
