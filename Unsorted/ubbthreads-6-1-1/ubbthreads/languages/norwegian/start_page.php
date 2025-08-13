57210aebbc657e3bdd0564b547a63a44fabb0f81
me-suzy <me.suzana@gmail.com>
2025-08-14 02:42:37 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"Passord";
+$ubbt_lang['PASS_REQ_BOD1']  = 	"En person fra IP-adressen";
+$ubbt_lang['PASS_REQ_BOD2']  = 	"ba om et midlertidig passord for brukernavnet";
+$ubbt_lang['PASS_REQ_BOD3']  = 	"for nettstedet '{$config['title']}'.  Dette er et midlertidig passord som kan brukes hvis du har glemt ditt opprinnelige passord. Hvis du ikke har bedt om dette, vennligst ignorerer denne meldingen. Det midlertidige passordet for dette brukernavnet er --";
+$ubbt_lang['PASS_MAILED']  = 	"Passordet er sendt";
+$ubbt_lang['PASS_MAILED2']  = 	"Et midlertidig passord for denne brukeren har blitt sendt.";
+$ubbt_lang['NO_EMAIL']  = 	"Vi kan dessverre ikke finne e-postadressen for";
+?>
