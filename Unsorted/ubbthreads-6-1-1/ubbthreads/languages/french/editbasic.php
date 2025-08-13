d3ac6de7c0dfd3e6ecc44ece37a084fbd16f9ab1
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:41 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =      "Votre photo ({$theme['PictureWidth']}x{$theme['PictureHeight']} pixels maximums, elle peut être plus petite) (.gif, .jpg, .png acceptés)";
+$ubbt_lang['PROF_PIC']  =       "Votre photo (URL complète - la taille de l'image est {$theme['PictureWidth']}x{$theme['PictureHeight']} pixels maximums, elle peut être plus petite) (.gif, .jpg, .png acceptés)";
+$ubbt_lang['PROF_PRIV']  = 	"Accepter les messages privés des autres utilisateurs ?";
+$ubbt_lang['NO_PROF']  = 	"Nous ne pouvons pas trouver de profil pour ce nom d'usager";
+$ubbt_lang['PROF_HEAD']  = 	"Éditer le profil pour";
+$ubbt_lang['PROF_EDIT']  = 	"Vous pouvez éditer n'importe laquelle des informations ci-dessous.";
+$ubbt_lang['BASIC_PROF']  = 	"Profil de base";
+$ubbt_lang['PROF_PASS']  = 	"Mot de passe (entre 4 et 20 caractères)";
+$ubbt_lang['VER_PASS']  = 	"Vérifiez le mot de passe";
+$ubbt_lang['EMAIL_ADD']  = 	"Votre adresse email (non disponible aux autres utilisateurs)";
+$ubbt_lang['FAKE_EMAIL']  = 	"L'adresse email à afficher dans vos messages";
+$ubbt_lang['FULL_NAME']  = 	"Nom complet";
+$ubbt_lang['PROF_SIG']  = 	"Signature (jusqu'à  {$config['Sig_length']} caractères). Vous pouvez utiliser du code UBB dans votre signature";
+$ubbt_lang['PROF_HOME']  = 	"URL de votre page personnelle (jusqu'à 150 caractères)";
+$ubbt_lang['PROF_OCC']  = 	"Occupation";
+$ubbt_lang['PROF_HOBB']  = 	"Passe-temps (jusqu'à 200 caractères)";
+$ubbt_lang['PROF_LOC']  = 	"Emplacement géographique (état, pays, etc...)";
+$ubbt_lang['PROF_BIO']  = 	"Biographie (jusqu'à 250 caractères)";
+$ubbt_lang['PROF_ICQ']  = 	"Numéro ICQ";
+$ubbt_lang['PROF_VIS']  = 	"Voulez-vous être visible sur Qui est en ligne?";
+$ubbt_lang['AUX_ONLINE']  =      "Souhaitez-vous que les utilisateurs puissent savoir le forum/message que vous regardez ?";
+?>
