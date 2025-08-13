e99a47a3f7350e9abc4c15e38b3b7b404714416c
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:42 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/donotifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/donotifymod.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['POST_PROB']  =  "¬d¸ß¸ê®Æ®w®Éµo¥Í°ÝÃD¡A§ä¤£¨ì³o½g¤å³¹¡C";
+$ubbt_lang['BAD_GROUP'] =	"©Úµ´¦s¨ú¡I";
+$ubbt_lang['NO_B_INFO'] =	"§ä¤£¨ì°Q½×°Ï¡I";
+$ubbt_lang['NOTIF_HEAD']  = 	"³qª¾±zÀ³¸Ó¬Ý¤@¬Ý¥H¤Uªº¤å³¹¡A¼ÐÃD¬O¡G ";
+$ubbt_lang['TO_READ'] =	"¥i¥H¦b³o¸ÌÀËµø³o½g¤å³¹¡G";
+$ubbt_lang['NOTIFY_SENT']  = 	"¤w¸g³qª¾ªO¥D¤F";
+$ubbt_lang['NOTIFY_CONFIRM'] = "¤w¸g³qª¾¥»ªOªO¥D¤F¡C";
+?>
