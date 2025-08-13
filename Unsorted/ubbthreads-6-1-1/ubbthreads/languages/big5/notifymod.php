e5299df266332577a2fe0566ca69eff1b0ddfe00
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:20 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/notifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/notifymod.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NOTIFY_MOD']  =  	"³qª¾ªO¥D";
+$ubbt_lang['NOTIFY_HEAD'] =	"³o­Ó°Ê§@±N¦Û°Ê³qª¾¥»ªOªO¥DÀËµø³o½g¤å³¹¡C";
+$ubbt_lang['SEND_IT'] = "¬Oªº¡A§Ú­n³qª¾ªO¥D¦³Ãö³o½g¤å³¹ªº°ÝÃD¡I";
+$ubbt_lang['NO_POST'] =	"Åª¨ú¸ê®Æ®w®Éµo¥Í¤@ÂIÂI¿ù»~¡AµLªkÅª¨ú..¡C";
+?>
