db5f545ead4da537e5fee3d15a85762d2d3227f5
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:41 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/dosearch.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['NO_MORE']   =  "There are no more search results in this direction.";
+$ubbt_lang['NO_WORDS']  = 	"Adjon meg néhány kulcsszót a kereséshez.";
+$ubbt_lang['SEARCH_RES']  = 	"A keresés eredménye";
+$ubbt_lang['PREV_PAGE']  = 	"Elõzõ oldal";
+$ubbt_lang['MAIN_SEARCH']  = 	"Fõ keresõ lap";
+$ubbt_lang['NEXT_PAGE']  = 	"Következõ lap";
+$ubbt_lang['NO_MATCH']  = 	"Nincs a keresési feltételnek megfelelõ.";
+?>
