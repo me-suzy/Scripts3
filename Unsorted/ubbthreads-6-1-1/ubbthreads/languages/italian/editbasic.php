7d174fc907084751889afecd86acbeb6d43b4542
me-suzy <me.suzana@gmail.com>
2025-08-14 02:40:19 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =      "Your picture ({$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['PROF_PIC']  =       "Your Picture (Entire URL - Picture size is {$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['AUX_ONLINE']  =      "Do you want users to see the actual forum/post you are looking at?";
+$ubbt_lang['PROF_PRIV']  = 	"Accept private messages from other users?";
+$ubbt_lang['NO_PROF']  = 	"Non riusciamo a trovare un profilo per il Nome Utente";
+$ubbt_lang['PROF_HEAD']  = 	"Modifica profilo per";
+$ubbt_lang['PROF_EDIT']  = 	"Puoi modificare ciascuna delle informazioni qui sotto.";
+$ubbt_lang['BASIC_PROF']  = 	"Profilo di Base";
+$ubbt_lang['PROF_PASS']  = 	"Password (tra i 4 e i 20 caratteri)";
+$ubbt_lang['VER_PASS']  = 	"Verifica Password";
+$ubbt_lang['EMAIL_ADD']  = 	"Indirizzo Email (Non disponibile per gli altri utenti)";
+$ubbt_lang['FAKE_EMAIL']  = 	"Indirizzo Email mostrato nei messaggi";
+$ubbt_lang['FULL_NAME']  = 	"Nome Completo";
+$ubbt_lang['PROF_SIG']  = 	"Nome in codice (fino a  {$config['Sig_length']}  caratteri). Puoi anche usare HTML nel tuo nome in codice";
+$ubbt_lang['PROF_HOME']  = 	"Homepage (più di 150 caratteri)";
+$ubbt_lang['PROF_OCC']  = 	"Occupazione";
+$ubbt_lang['PROF_HOBB']  = 	"Hobby (più di 200 caratteri)";
+$ubbt_lang['PROF_LOC']  = 	"Locazione Geografica (Stato, Paese, ecc.)";
+$ubbt_lang['PROF_BIO']  = 	"Bio (più di 250 caratteri)";
+$ubbt_lang['PROF_ICQ']  = 	"Numero ICQ";
+$ubbt_lang['PROF_VIS']  = 	"Vuoi essere visibile sulla videata Chi è Online?";
+?>
