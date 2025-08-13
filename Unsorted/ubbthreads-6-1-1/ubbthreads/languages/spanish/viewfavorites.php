c662af708ca2548f18e00eb111eca498bbe594af
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:18 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/viewfavorites.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/viewfavorites.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['VIEW_FAV']  =  "Viendo todos los temas favoritos";
+$ubbt_lang['VIEW_REM']  =  "Viendo todos los recordatorios";
+$ubbt_lang['LAST_POST']  =  "&Uacute;ltimo Mensaje";
+$ubbt_lang['REMOVE']     =  "Borrar";
+?>
