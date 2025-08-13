066ae102f3fe8f56ec58736b23969b0c77c21e36
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:58 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/sendmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/sendmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['CHOOSE_ADD']  = 	"Escoge un nombre";
+$ubbt_lang['TEXT_DELETE']  = 	"Borrar";
+$ubbt_lang['NO_PRIVATE']  = 	"Este miembro no est&aacute; aceptando mensajes privados.";
+$ubbt_lang['NO_RECORD']  = 	"No hay constancia de nadie con el Nombre de Miembro al que intentas enviar este mensaje.";
+$ubbt_lang['MESS_PREV']  = 	"Previsualizar mensaje privado a";
+$ubbt_lang['PREVIEW_MESS']  = 	"Esto es una previsualizaci&oacute;n de tu mensaje.  Si parece estar bien pulsa sobre 'Continuar' para enviar el mensaje.  De lo contrario, usa el formulario de abajo para continuar editando tu mensaje.";
+$ubbt_lang['EDIT_MORE']  = 	"Continuar editando";
+$ubbt_lang['PRIV_SUB']  = 	"Tienes mensajes nuevos";
+$ubbt_lang['PRIV_BODY_R']  = 	"Hola, este mensaje es para informarte de que has recibido un mensaje privado en los foros de {$config['title']}.  Puedes ir a {$config['phpurl']}/login.php para leerlo.  Saludos.";
+$ubbt_lang['MESS_SENT']  = 	"Tu mensaje ha sido enviado.";
+?>
