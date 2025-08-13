869539dbc2b22eea753c302a66e0d74d99d7b431
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:09 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/mailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/mailthread.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['MAIL_FRIEND']  = 	"Skicka denna tråd till en vän";
+$ubbt_lang['FRIEND_HEAD']  = 	"Skriv i e-postadressen till den person du vill skicka detta inlägg till.";
+$ubbt_lang['SENT_FROM1']  = 	"E-postmeddelandet kommer skickas från din registrerade e-postadress";
+$ubbt_lang['SENT_FROM2']  = 	"med ditt Användarnamn";
+$ubbt_lang['USE_REAL']  = 	"Om du vill använda ditt riktiga namn istället, fyll i det här:";
+$ubbt_lang['WHAT_EMAIL']  = 	"E-postadress att skicka till";
+$ubbt_lang['SEND_IT']  = 	"Skicka iväg!";
+?>
