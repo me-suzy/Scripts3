1221320a8011dcaf08352e11ffc14e8c3ff2b944
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:26 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/domailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/domailthread.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['START']  = 	"heeft je een link gestuurd naar een bericht met de naam";
+$ubbt_lang['START2']  = 	"op '{$config['title']}'!";
+$ubbt_lang['TO_READ']  = 	"Bekijk dit bericht online via";
+$ubbt_lang['NOTE_FROM']   =    "Je ontving deze notificatie van";
+$ubbt_lang['POST_BELOW']  =    "Dit is een kopie van het originele bericht:";
+$ubbt_lang['EMAIL_SENT']  = 	"De email is verstuurd";
+$ubbt_lang['EMAIL_CONFIRM']  =  "Een email met dit bericht is verstuurd. ";
+$ubbt_lang['FORUM_RETURN']   =  "Ga nu terug naar het forum.";
+?>
