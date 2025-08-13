f206d9aad25cdd32c0bf1a36a3800284529be76b
me-suzy <me.suzana@gmail.com>
2025-08-14 01:12:19 +0300

    Add Unsorted/ppntopsites/config.php

---
+++ Unsorted/ppntopsites/config.php
@@ -0,0 +1,20 @@
+<?php
+$siteurl = "http://";
+$scripturl = "http://";
+$bannerurl = "http://";
+$tname = "PPN Test";
+$title = "";
+$bannerext = ".gif";
+$numban = "25";
+$trank = "20";
+$mode = "1";
+$imagewidth = "88";
+$imageheight = "31";
+$adminemail = "test@software.pp-network.com";
+$header = <<< HEADER
+<center><SCRIPT language="JavaScript" src="http://www.pp-network.com/advert.js"></SCRIPT></center>
+HEADER;
+$footer = <<< FOOTER
+<center><SCRIPT language="JavaScript" src="http://www.pp-network.com/advert.js"></SCRIPT></center>
+FOOTER;
+?>
