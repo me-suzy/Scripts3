8524ef02a97f0cb179d7c6516b5ed9b7577a29b8
me-suzy <me.suzana@gmail.com>
2025-08-14 02:18:57 +0300

    Add Unsorted/turbothumbs2free/settings.php

---
+++ Unsorted/turbothumbs2free/settings.php
@@ -0,0 +1,15 @@
+<?php
+$dbname ="DATEBASE_NAME";
+$dbuser ="DATABASE_USERNAME";
+$dbpass ="DATABASE_PASSWORD";
+$dbhost ="localhost";
+$admin_path ="/FULL_PATH_TO_YOUR_ADMIN_FOLDER/";
+$import ="/FULL_PATH_TO_YOUR_ADMIN_FOLDER/import/";
+$files ="/FULL_PATH_TO_YOUR_ADMIN_FOLDER/files/";
+$dest ="/FULL_PATH_TO_WHERE_YOU_WANT_THE_SCRIPT_TO_PUT_THE_THUMBNAILS/";
+$thumbnaills ="http://www.YOURDOMAIN.com/thumbs/";
+$convert_path = 'convert';
+$wget ="/usr/bin/wget";
+$yourdomain="YOUR_DOMAIN.com";
+$license=12345;
+?>
