2ea2a4eaea0a56e759f8eb5e88c4efbcc289c641
me-suzy <me.suzana@gmail.com>
2025-08-14 00:34:26 +0300

    Add Unsorted/PHPCheckout/phpcheckout/surveyQuestion.php

---
+++ Unsorted/PHPCheckout/phpcheckout/surveyQuestion.php
@@ -0,0 +1,8 @@
+<!-- start of surveyQuestion.inc -->
+<span style="font-style:italic;font-weight:bold;">
+	<p>Did you find any pages loading slowly on this site?
+
+	<br><br><span class="note">p.s. if 'yes', please <a href="email.php?to=slowpage&domain=dreamriver.com">tell the webmaster which page/s</a></span>
+	</p>
+</span>
+<!-- end of surveyQuestion.inc -->
