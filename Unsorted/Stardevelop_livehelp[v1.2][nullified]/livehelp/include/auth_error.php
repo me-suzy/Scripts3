ed1e1ce872d0936dc688e572620998e5b7a935e7
me-suzy <me.suzana@gmail.com>
2025-08-14 01:52:06 +0300

    Add Unsorted/Stardevelop_livehelp[v1.2][nullified]/livehelp/include/auth_error.php

---
+++ Unsorted/Stardevelop_livehelp[v1.2][nullified]/livehelp/include/auth_error.php
@@ -0,0 +1,36 @@
+<?php
+/*
+stardevelop.com Live Help
+International Copyright stardevelop.com
+
+You may not distribute this program in any manner,
+modified or otherwise, without the express, written
+consent from stardevelop.com
+
+You may make modifications, but only for your own 
+use and within the confines of the License Agreement.
+All rights reserved.
+
+Selling the code for this program without prior 
+written consent is expressly forbidden. Obtain 
+permission before redistributing this program over 
+the Internet or in any other medium.  In all cases 
+copyright and header must remain intact.  
+*/
+?>
+<html>
+<head>
+<title><?php echo($livehelp_name); ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body>
+<div align="center"> 
+  <p><font size="4" face="Verdana, Arial, Helvetica, sans-serif">Authorisation 
+    Error<br>
+    <font size="2">You must be authenticated through the administration client 
+    to view this page.<br>
+    Please consult our documentation or support if you believed to have received 
+    this in error.</font></font></p>
+</div>
+</body>
+</html>
