57719a43f5dda85d73a9b012a0cb94f0809c6127
me-suzy <me.suzana@gmail.com>
2025-08-14 00:37:15 +0300

    Add Unsorted/phpclassifieds608v3/phpclassifieds/admin/test.php

---
+++ Unsorted/phpclassifieds608v3/phpclassifieds/admin/test.php
@@ -0,0 +1,11 @@
+<html>
+<form method=post action=test.php>
+<input type=text name=tekst value=test>
+<input type=submit name=submit value=submit>
+</form>
+<?
+
+print "LALA: $tekst";
+?>
+
+</html>
