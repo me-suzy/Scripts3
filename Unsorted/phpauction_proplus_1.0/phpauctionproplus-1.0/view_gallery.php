96e34df30c529d9c3e26af6a4d61ea66f489cc79
me-suzy <me.suzana@gmail.com>
2025-08-14 00:12:53 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/view_gallery.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/view_gallery.php
@@ -0,0 +1,8 @@
+<?#//v.1.0.0
+    include "includes/config.inc.php";
+?>
+<HTML>
+<BODY TOPMARGIN=0 LEFTMARGIN=0 MARGINWIDTH=0 MARGINHEIGHT=0>
+<A HREF="Javascript:window.close()"><IMG SRC=<?=$uploaded_path.$GALLERY_DIR."/".$UPLOADED_PICTURES[$img]?> BORDER=0></A>
+</BODY>
+</HTML>
