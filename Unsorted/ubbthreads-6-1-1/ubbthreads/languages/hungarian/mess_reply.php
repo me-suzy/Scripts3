7407bbdea947e4884868d5952ffccc788705be89
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:04 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/mess_reply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/mess_reply.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['EDIT_MORE']  = 	"Edit some more";
+$ubbt_lang['REPLY_PREV']  = 	"Preview of your reply to";
+$ubbt_lang['PREVIEW_MESS']  = 	"The following is a preview of your message.  If everything looks ok then you can click 'Continue' and your message will be sent.  If not then use the bottom form below to edit some more.";
+$ubbt_lang['MESS_PREVIEW']  = 	"I want to preview my message";
+$ubbt_lang['PRIV_SUB']  = 	"Üzenet érkezett";
+$ubbt_lang['PRIV_BODY_R']  = 	"Önnek privát üzenete érkezett a következõ fórumon: .  A  {$config['phpurl']}/login.php címen olvashatja el.";
+?>
