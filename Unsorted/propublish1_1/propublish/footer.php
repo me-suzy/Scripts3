8192e3925136076bd2b61c13a1eb3f826f323995
me-suzy <me.suzana@gmail.com>
2025-08-14 01:16:10 +0300

    Add Unsorted/propublish1_1/propublish/footer.php

---
+++ Unsorted/propublish1_1/propublish/footer.php
@@ -0,0 +1,26 @@
+</td>
+    </tr>
+
+</table>
+
+
+
+					</td>
+
+				</tr>
+			</table>
+		</td>
+	</tr>
+
+	<tr height=1><td colspan=3 bgcolor="#000000"><img src="spacer.gif" border=0 width=1 height=1></td></tr>
+
+	<tr>
+		<td colspan=3 align="center">
+           <span class="gentxt"><small>Copyright© 2002&nbsp; <a href="http://www.webber.no">Webber.no</a> </small></span>
+		</td>
+	</tr>
+</table>
+
+</body>
+</html>
+
