981f99442e4432594b0927af94f3951113ee87b3
me-suzy <me.suzana@gmail.com>
2025-08-14 02:48:00 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "Move this post & any replies";
+$ubbt_lang['DELETE_THREAD']  =  "Delete this thread";
+$ubbt_lang['NO_EDIT']  = 	"Íåâîçìîæíî ïðîâåðèòü Âàøè ðàçðåøåíèÿ íà ðåäàêòèðîâàíèå ýòîãî ñîîáùåíèÿ.";
+$ubbt_lang['EDITTIME']  = 	"This post can no longer be edited because the maximum";
+$ubbt_lang['PEDIT_HEAD']  = 	"Ðåäàêòèðîâàíèå ñîîáùåíèÿ";
+$ubbt_lang['PEDIT_BODY']  = 	"Âû ìîæåòå èçìåíèòü ëþáóþ ïðèâåä¸ííóþ íèæå èíôîðìàöèþ ëèáî óäàëèòü Âàøå ñîîáùåíèå. Åñëè Âû ðåøèòå óäàëèòü Âàøå ñîîáùåíèå, íî íà íåãî èìåþòñÿ îòâåòû, òåìà Âàøåãî ñîîáùåíèÿ áóäåò èçìåíåíà íà \"Ýòî ñîîáùåíèå óäàëåíî\". Òåêñò ñîîáùåíèÿ è âñÿ äîïîëíèòåëüíàÿ èíôîðìàöèÿ áóäóò î÷èùåíû. Ýòî ñäåëàíî äëÿ òîãî, ÷òîáû ñîõðàíèòü äðåâîîáðàçíóþ ñòðóêòóðó ôîðóìà íåòðîíóòîé.";
+$ubbt_lang['EDIT_POLL']  = 	"Because this post has a poll in it, you may delete it but not edit it.";
+$ubbt_lang['POST_ICON']  = 	"Ïèêòîãðàììà";
+$ubbt_lang['MARK_EDIT']  = 	"Mark as Edited?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Èçìåíèòü ýòî ñîîáùåíèå";
+$ubbt_lang['PEDIT_DELETE']  = 	"Óäàëèòü ýòî ñîîáùåíèå";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Óòâåðäèòü ýòî ñîîáùåíèå";
+$ubbt_lang['RET_NORM']  = 	"Return to normal date";
+$ubbt_lang['MAKE_STICKY']  = 	"Make post sticky";
+$ubbt_lang['MANAGE_THREAD']  = 	"Manage this thread";
+$ubbt_lang['OPEN_THREAD']  = 	"Open this thread";
+$ubbt_lang['CLOSE_THREAD']  = 	"Close this thread";
+$ubbt_lang['UNKEEP_THREAD']  = 	"Unkeep this thread";
+$ubbt_lang['KEEP_THREAD']  = 	"Keep this thread";
+$ubbt_lang['MOVE_THREAD']  = 	"Move this thread";
+?>
