754ea5215fcf890050f676025234ad09bb9902f3
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:35 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/controlpanel.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/controlpanel.php
@@ -0,0 +1,27 @@
+<?
+$ubbt_lang['SHOW_ALL']    = "...Åã¥Ü©Ò¦³";
+$ubbt_lang['PERSONAL']     =  "­Ó¤H¬ÛÃö¸ê°T¡BEmail¡B±K½X¡A...µ¥µ¥¡C";
+$ubbt_lang['DISPLAY']     = "­Ó¤HÅã¥Ü°¾¦n¡B¨C­¶Åã¥Ü¤å³¹¼Æ¡B»y¨¥¡BÃC¦â¡Aµ¥µ¥¡C";
+$ubbt_lang['SUB_UN']      = "­q¾\ / ¨ú®ø­q¾\ °Q½×°Ï²§°Ê¡B·s¤å³¹³qª¾¡Aµ¥µ¥¡C";
+$ubbt_lang['YOUR_VIEW']   = "¨ä¥Lºô¤ÍÀËµø±zªº¨­¤À¬°¡G";
+$ubbt_lang['YOUR_VIEW_WITH'] = "¦@µoªí¹L";
+$ubbt_lang['VIEW_POSTS']     = "½g¤å³¹";
+$ubbt_lang['TEXT_TOTAL']     = "¦@";
+$ubbt_lang['TEXT_UNREAD']    = "¼Ð¥Ü¥¼Åª";
+$ubbt_lang['EDIT_ICON']       =  "½s¿è";
+$ubbt_lang['WEL_CONTROL'] = "­Ó¤H±±¨î¥x¡G";
+$ubbt_lang['MAIN_CONFIG']  =  "¥D­n³]©w";
+$ubbt_lang['EDIT_TEXT']    =  "½s¿è";
+$ubbt_lang['BASIC_TEXT']   =  "°ò¥»";
+$ubbt_lang['DISPLAY_TEXT']  =  "Åã¥Ü";
+$ubbt_lang['SUBS_TEXT']    = "­q¾\°O¿ý";
+$ubbt_lang['PRIVATE_BOX']   = "«H¥ó§X";
+$ubbt_lang['FRIENDS_ON']    = "½u¤W¦n¤Í²M³æ";
+$ubbt_lang['FROM_ADDR']     = "(¨Ó¦Û³q°T¿ý)";
+$ubbt_lang['FAV_THREADS']   = "­Ó¤H°¾¦n¤å³¹¦¬ÂÃ";
+$ubbt_lang['TEXT_REMOVE']    =  "²¾°£";
+$ubbt_lang['REM_THREADS']   = "³Æ§Ñ¿ý¡A°O±o­n¦^ÂÐ®@";
+$ubbt_lang['SEND_A_PM']     = "±H°e¨p¤H°T®§";
+$ubbt_lang['FAV_FORUMS']    = "­Ó¤H°¾¦n°Q½×°Ï";
+$ubbt_lang['BY_TEXT']       = "µo«H¤H¡G";
+?>
