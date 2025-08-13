90994a5f85abdb5ab4e01202d8d39b939931fc94
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:26 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/search.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/search.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['SEARCH_INFO']  = 	"½Ð¶ñ¤J±z­n·j´Mªº±ø¥ó¡C";
+$ubbt_lang['SEARCH_FORUM']  = 	"±z­n·j´Mªº°Q½×°Ï";
+$ubbt_lang['ALL_FORUMS']  = 	"©Ò¦³°Q½×°Ï";
+$ubbt_lang['SEARCH_WORDS']  = 	"­n·j´Mªº¦r¦ê";
+$ubbt_lang['SEARCH_OPT']  = 	"·j´M¿ï¶µ";
+$ubbt_lang['SEARCH_ENT']  = 	" ¾ã­Ó¦r¦ê ";
+$ubbt_lang['SEARCH_AND']  = 	" AND ";
+$ubbt_lang['SEARCH_OR']  = 	" OR ";
+$ubbt_lang['SEARCH_USER']  = 	"¥Hµn¤J¦WºÙ·j´M";
+$ubbt_lang['SEARCH_DATE']  = 	"¥H¤é´Á½d³ò·j´M";
+$ubbt_lang['SEARCH_1DAY']  = 	"¤@¤Ñ¥H¤º";
+$ubbt_lang['SEARCH_2DAY']  = 	"¨â¤Ñ¥H¤º";
+$ubbt_lang['SEARCH_1WEEK']  = 	"¤@¶g¥H¤º";
+$ubbt_lang['SEARCH_2WEEK']  = 	"¨â¶g¥H¤º";
+$ubbt_lang['SEARCH_3WEEK']  = 	"¤T¶g¥H¤º";
+$ubbt_lang['SEARCH_1MONTH']  = 	"¤@­Ó¤ë¥H¤º";
+$ubbt_lang['SEARCH_3MONTH']  = 	"¤T­Ó¤ë¥H¤º";
+$ubbt_lang['SEARCH_6MONTH']  = 	"¤»­Ó¤ë¥H¤º";
+$ubbt_lang['SEARCH_1YEAR']  = 	"¤@¦~¥H¤º";
+$ubbt_lang['SEARCH_ALLPOST']  = 	"©Ò¦³¤å³¹";
+$ubbt_lang['SEARCH_NUM']  = 	"¨C­¶©Ò­nÅã¥Üªº¤å³¹¼Æ ( ·í·j´M©Ò¦³°Q½×°Ï®É ¡A¦¹¿ï¶µµL®Ä)";
+?>
