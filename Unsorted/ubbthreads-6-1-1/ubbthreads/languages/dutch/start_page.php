4bafb3ab42d424f83ba6d5bbdbe8f3a21b3b13bc
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:23 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"Verzoek om een nieuw wantwoord";
+$ubbt_lang['PASS_REQ_BOD1']  = 	"Iemand heeft vanuit IP adres";
+$ubbt_lang['PASS_REQ_BOD2']  = 	"een tijdelijk wachtwoord aangevraagd voor de gebruikersnaam";
+$ubbt_lang['PASS_REQ_BOD3']  = 	"op de website '{$config['title']}'.  Dit is een tijdelijk wachtwoord.  Als je dit wachtwoord niet hebt aangevraagd kun je dit bericht verwijderen.  Het tijdelijke wachtwoord is --";
+$ubbt_lang['PASS_MAILED']  = 	"Wachtwoord verzonden";
+$ubbt_lang['PASS_MAILED2']  = 	"Een tijdelijk wachtwoord is nu naar het opgegeven e-mail adres verstuurd.";
+$ubbt_lang['NO_EMAIL']  = 	"Ik kan geen email adres vinden voor";
+?>
