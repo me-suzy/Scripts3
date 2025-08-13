4f96b12c190206059f8aae4c2b3d47d1dacc7965
me-suzy <me.suzana@gmail.com>
2025-08-14 00:41:12 +0300

    Add Unsorted/phpListPro-1.80/phpListPro-1.80/lang_french.php

---
+++ Unsorted/phpListPro-1.80/phpListPro-1.80/lang_french.php
@@ -0,0 +1,955 @@
+<?
+
+
+#################################################################################################
+
+
+#
+
+
+#  project           	: phpListPro
+
+
+#  filename          	: lang_french.php
+
+
+#  version           	: 1.71
+
+
+#  last modified by  	: Erich Fuchs
+
+
+#  e-mail            	: office@smartisoft.com
+
+
+#  purpose           	: Language File
+
+
+#  last modified     	: 06/01/2001
+
+
+#
+
+
+#################################################################################################
+
+
+
+
+
+#  Category Settings
+
+
+#################################################################################################
+
+
+
+
+
+$category[0]            = "Tout Voir";                // must be all Entries
+
+
+$category[1]            = "Categorie 1";
+
+
+$category[2]            = "Categorie 2";
+
+
+$category[3]            = "Categorie 3";
+
+
+$category[4]            = "Categorie 4";
+
+
+$category[5]            = "Categorie 5";
+
+
+$category[6]            = "Categorie 6";
+
+
+
+
+
+
+
+
+#  Language Settings
+
+
+#################################################################################################
+
+
+
+
+
+$lang[metatag]        	= "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\">";
+
+
+
+
+
+$lang[rank] 		= "#";
+
+
+$lang[site] 		= "Sites";
+
+
+$lang[votes]		= "Arrivés";
+
+
+$lang[hits] 		= "Départs";
+
+
+$lang[rate] 		= "Class";
+
+
+$lang[random] 		= "Au Hasard";
+
+
+
+
+
+$lang[in_msg1]		= "Vous êtes maintenant introduit en provenance de ";
+
+
+$lang[in_msg2]		= "S.v.p appuyez sur le bouton [Votez pour ce site] et votez pour ce site.";
+
+
+$lang[in_msg3]		= "Joignez $list_name sans voter.";
+
+
+$lang[in_chose]		= "S.V.P Choisissez votre Classement";
+
+
+$lang[in_vote]		= "Votez pour ce site";
+
+
+
+
+
+$lang[anti_cheat_msg] 	= "<center><font color=red>Vous avez déjà votez dans les derniers $vote_timeout heures! Votre vote n'a pas été enregistré. Désolé.</font></center>";
+
+
+$lang[vote_log_msg] 	= "<center><font color=red>Merci! Votre vote est enregistré!</font></center>";
+
+
+$lang[not_found_msg] 	= "<center><font color=red>OUPS!: Je ne TROUVES PAS ce site!</font></center>";
+
+
+$lang[tilt_msg] 	= "<center><font color=red>Une routine de maintenance rend ce site temporairement inaccessible! Essayez plus tard, désolé!</center>";
+
+
+$lang[nocookie_msg] 	= "<center><font color=red>Votre fureteur doit croquer les cookies! Autrement nous ne pouvons compté votre vote.</font></center>";
+
+
+$lang[ban_msg] 		= "<center><font color=red>Vous êtes BANNIS de vote sur cette liste. Nous ne pouvons compté votre vote.</font></center>";
+
+
+
+
+
+$lang[emailconfirmed]	= "Email confirmé avec brio!";
+
+
+$lang[emailnotconf]	= "Email NON confirmé !!! SVP contactez le Webmestre.";
+
+
+$lang[redirect]		= "On vous y amène, un moment s'il vous plait...";
+
+
+$lang[confirmation]	= "Confirmation";
+
+
+
+
+
+$lang[pages]		= "Pages: ";
+
+
+$lang[useronline]	= "Usagé en ligne";
+
+
+$lang[usersonline]	= "Usagés en ligne";
+
+
+$lang[userssatisfied]	= "Usagés satisfaits";
+
+
+$lang[sitesindb]	= "Contenu de notre base de données";
+
+
+$lang[inoutresets]	= "Arivés/Départs réinitialisés tous les";
+
+
+$lang[days]		= "jours";
+
+
+$lang[next]		= " ensuite";
+
+
+
+
+
+$lang[addsite]		= "Ajouter un site";
+
+
+$lang[addsitedesc]	= "Proposer un nouveau site";
+
+
+$lang[editsite]		= "Modifier un site";
+
+
+$lang[editsitedesc]	= "Modifier un site existant";
+
+
+$lang[helpsite]		= "Aide";
+
+
+$lang[helpsitedesc]	= "Aide pour installer le code HTML";
+
+
+
+
+
+$lang[error]		= "Erreur! svp retourner et corriger!";
+
+
+$lang[error_duplicate]	= "Il y a DOUBLE entrée pour l'adresse de ce site!";
+
+
+$lang[error_emptysiten]	= "Vous n'avez pas tapé le nom de votre site";
+
+
+$lang[error_emptysitea]	= "Vous n'avez pas tapé l'adresse de votre site";
+
+
+$lang[error_emptypass]	= "Vous n'avez pas fournis de mot de passe";
+
+
+$lang[error_emptyemail]	= "Vous n'avez pas fournis d'adresse email";
+
+
+$lang[error_emptyname]	= "Vous n'avez pas tapé votre nom";
+
+
+$lang[error_emptydesc]	= "Vous n'avez pas donné la description de votre site";
+
+
+$lang[error_bannerw]	= "Vous n'avez pas établie une largeur adéquate pour votre bannière";
+
+
+$lang[error_bannerh]	= "Vous n'avez pas établie une hauteur adéquate pour votre bannière";
+
+
+$lang[error_banners]	= "Le poid (en ko) de votre bannière excède la limite permise.";
+
+
+$lang[error_bannernok]	= "Le URL de votre bannière n'est pas valide";
+
+
+$lang[error_baddesc]	= "Un mot douteux est détecté dans la description de votre site";
+
+
+$lang[error_badname]	= "Un mot douteux est détecté dans le nom de votre site";
+
+
+
+
+
+$lang[back]		= "Retour";
+
+
+$lang[done]		= "Terminer";
+
+
+$lang[submit]		= "Soumettre";
+
+
+$lang[search]           = "Search";
+
+
+$lang[getlost]		= "Perdu votre ID et/ou Mot de Passe ?";
+
+
+$lang[getid]		= "Recevoir votre ID/Mot de passe";
+
+
+$lang[editsite]		= "Modifier un Site";
+
+
+
+
+
+$lang[enterdata]	= "Entrez vos informations";
+
+
+$lang[password]		= "Mot de passe: ";
+
+
+$lang[siteid]		= "ID du site: ";
+
+
+$lang[emailaddr]	= "Adresse Email: ";
+
+
+$lang[yourname]		= "Votre Nom: ";
+
+
+$lang[sitetitle]	= "Nom du site: ";
+
+
+$lang[siteurl]		= "Adresse URL: ";
+
+
+$lang[sitedesc]		= "Description: ";
+
+
+$lang[bannerurl]	= "URL de votre bannière: ";
+
+
+$lang[bannerw]		= "Largeur de la bannière: ";
+
+
+$lang[bannerh]		= "Hauteur de la bannière: ";
+
+
+
+
+
+$lang[getlost2]		= "Retrouver votre ID et mot de passe perdus";
+
+
+$lang[edityoursite]	= "Mettre à jour votre site";
+
+
+
+
+
+$lang[getidok]		= "J'ai trouvé votre information et vous l'ai fais parvenir!";
+
+
+$lang[getidnok]		= "AUCUN site relié a cette adresse email n'a été trouvé!";
+
+
+$lang[editidnok]	= "AUCUN site relié a cette donnée n'a été trouvé!";
+
+
+$lang[editsuccess]	= "Site mis à jour avec brio!";
+
+
+
+
+
+$lang[addthanks]	= "Merci! Votre site a été ajouté!";
+
+
+$lang[addloginid]	= "Votre ID est: ";
+
+
+$lang[addsendmail]	= ", je viens d'envoyer une confirmation à votre adresse email.";
+
+
+$lang[addhint]		= "Vous avez besoin de cet ID et de votre mot de passe pour faire des modifications.";
+
+
+$lang[addapproval]	= "S.v.p patientez, le Webmestre vérifie votre entrée dès que possible.";
+
+
+$lang[addplease]	= "Maintenant s.v.p, insérez une copie (ou multiples) du code HTML suivant sur votre site pour que les gens puissent voter pour vous!";
+
+
+
+
+
+$lang[help]		= "AIDE";
+
+
+$lang[helpreplace1]	= "Remplacez les <b>XXXXXXXX</b> par le numéro ID, que nous vous avons fait parvenir par email.";
+
+
+$lang[helpreplace2]	= "Remplacez <b>www.votredomaine.com</b> par le URL vers votre image.";
+
+
+
+
+
+$lang[addrules]		= "Règles à suivres";
+
+
+$lang[addyoursite]	= "Ajouter votre site";
+
+
+
+
+
+$lang[rules]		= "<ul>
+
+
+			    <li>Votre rang dépend seulement du nombre de cliques sur votre code de votre site
+
+
+			    <li>AUCUN script immitant ou émulant les cliques n'est permis
+
+
+			    <li>SEULS les TOP 10 affichent leur bannière
+
+
+			    <li>Les compteurs sont réinitialisés périodiquement
+
+
+			    <li>!!! TRICHER est EXCLU !!!
+
+
+			   </ul>";
+
+
+
+
+
+
+
+
+#  Help Language Forms
+
+
+#################################################################################################
+
+
+
+
+
+function help($id) {
+
+
+    global $list_name,$list_url,$voteimage_1,$voteimage_2,$voteimage_3;
+
+
+
+
+
+echo "<hr><b><li>Liens Texte:</b><br>";
+
+
+echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt$list_name&lt/a&gt\n";
+
+
+
+
+
+
+
+
+if ($voteimage_1) {
+
+
+  echo "<p><hr><b><li>Liens Images:</b><br>\n";
+
+
+  echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt<br>&ltimg src=\"http://www.votredomaine.com/vote_image1.gif\" alt=\"Joignez $list_name et votez pour ce site !!!\" border=0&gt&lt/a&gt\n";
+
+
+  echo "<p><center><img src=\"$voteimage_1\"><br>Voici votre image. S.v.p la mettre sur votre serveur.<br>(Right Mouseclick - Enregistrer sous ...)</center><br>\n";
+
+
+  }
+
+
+
+
+
+if ($voteimage_2) {
+
+
+  echo "<p><hr><b><li>Liens Images2:</b><br>\n";
+
+
+  echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt<br>&ltimg src=\"http://www.votredomaine.com/vote_image2.gif\" alt=\"Joignez $list_name et votez pour ce site !!!\" border=0&gt&lt/a&gt\n";
+
+
+  echo "<p><center><img src=\"$voteimage_2\"><br>Voici votre image. S.v.p la mettre sur votre serveur.<br>(Right Mouseclick - Enregistrer sous ...)</center><br>\n";
+
+
+  }
+
+
+
+
+
+if ($voteimage_3) {
+
+
+  echo "<p><hr><b><li>Liens Images3:</b><br>\n";
+
+
+  echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt<br>&ltimg src=\"http://www.votredomaine.com/vote_image3.gif\" alt=\"Joignez $list_name et votez pour ce site !!!\" border=0&gt&lt/a&gt\n";
+
+
+  echo "<p><center><img src=\"$voteimage_3\"><br>Voici votre image. S.v.p la mettre sur votre serveur.<br>(Right Mouseclick - Enregister Sous ...)</center><br>\n";
+
+
+  echo "<p>";
+
+
+  }
+
+
+}
+
+
+
+
+
+#  Mail Language Forms
+
+
+#################################################################################################
+
+
+
+
+
+function mail_getidpwd($result) {		// Mail Form for sending lost ID/Password
+
+
+    global $list_name,$list_url,$admin_email;
+
+
+
+
+
+mail("$result[email]", "Votre inforation, demandé à $list_name", "Bonjour $result[name],
+
+
+voici votre information d'accès pour modifier votre site sur $list_name.
+
+
+
+
+
+Votre site:
+
+
+$result[site_name]
+
+
+$result[site_addr]
+
+
+
+
+
+Gardez ceci précieusement!
+
+
+Votre ID: $result[id]
+
+
+Votre mot de passe: $result[password]
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+Votre code HTML:
+
+
+
+
+
+<a href=\"$list_url/in.php?site=$result[id]\" target=\"_blank\"><img src=\"www.votredomaine.com/vote_image1.gif\" border=\"0\"></a>
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_newnotifyuser($email_address,$webmaster_name,$id,$password,$site_name,$site_address,$site_description) {
+
+
+    global $list_name,$list_url,$admin_email,$secret,$webmasterapproval,$emailapproval;
+
+
+
+
+
+if ($webmasterapproval) {$webmastermsg="\nS.v.p soyez patient, le Webmestre vérifiera votre entrée dès que possible.\n";} else {$webmastermsg="";}
+
+
+if ($emailapproval) {$emailmsg="\nS.V.P cliquez ce lien pour confirmer votre entrée:\n$list_url/confirm.php?check=".urlencode(encrypt($id,$secret))."\n\n";} else {$emailmsg="";}
+
+
+
+
+
+mail("$email_address", "Bonjour $webmaster_name et bienvenue à $list_name ", "Merci $webmaster_name de vous joindre à $list_name.
+
+
+$webmastermsg$emailmsg
+
+
+Votre site:
+
+
+$site_name ($site_address)
+
+
+
+
+
+N'oubiez-pas!
+
+
+Votre ID: $id
+
+
+Mot de passe: $password
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+Votre code HTML:
+
+
+
+
+
+<a href=\"$list_url/in.php?site=$id\" target=\"_blank\"><img src=\"http://www.votredomaine.com/$voteimage_1\" alt=\"Joignez $list_name et votez pour ce site !!!\" border=0></a>
+
+
+
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_newnotifyadm($email_address,$webmaster_name,$id,$password,$site_name,$site_address,$site_description) {
+
+
+    global $list_name,$list_url,$admin_email,$notify_email;
+
+
+
+
+
+mail("$notify_email", "NOTIFY $webmaster_name joined the $list_name", "$webmaster_name joined the $list_name.
+
+
+
+
+
+Site coFrom: $id
+
+
+Password: $password
+
+
+Site name: $site_name
+
+
+Site address: $site_address
+
+
+
+
+
+Email: $email_address
+
+
+
+
+
+Banner address (if any): $banner_address
+
+
+
+
+
+Description:
+
+
+$site_description
+
+
+
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_editnotifyadm($email_address,$webmaster_name,$id,$password,$site_name,$site_address,$site_description) {
+
+
+    global $list_name,$list_url,$admin_email,$notify_email;
+
+
+
+
+
+mail("$notify_email", "NOTIFY $webmaster_name edited on $list_name", "$webmaster_name edited on $list_name.
+
+
+
+
+
+Site coFrom: $id
+
+
+Password: $password
+
+
+Site name: $site_name
+
+
+Site address: $site_address
+
+
+
+
+
+Email: $email_address
+
+
+
+
+
+Banner address (if any): $banner_address
+
+
+
+
+
+Description:
+
+
+$site_description
+
+
+
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_tiltuser($result) {		// Mail Form for sending User on TILT
+
+
+    global $list_name,$list_url,$admin_email,$tilt_insp_timeout;
+
+
+
+
+
+mail("$result[email]", "ALERTE Detection de tricheries de la part de $result[site_name] sur $list_name",
+
+
+"Cher Participant,
+
+
+
+
+
+Notre système de détection de tricheries à $list_name a détecté des anomalies dans les votes en provenance de votre site
+
+
+
+
+
+ID: $result[id]
+
+
+$result[site_name] ($result[site_addr])
+
+
+
+
+
+Donc, votre site est maintenant invisible à $list_name ($list_url) pour $tilt_insp_timeout minutes.
+
+
+Tout autre anomalie suspecte dans les votes, à l'intérieur de cette fourchette de temps, entrainera automatiquement une augmentation du temps de votre invisibilité !
+
+
+
+
+
+Sincèrement
+
+
+
+
+
+Le Webmestre
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_tiltadmin($result) {		// Mail Form for sending lost ID/Password
+
+
+    global $list_name,$list_url,$admin_email,$notify_email,$tilt_insp_timeout;
+
+
+
+
+
+mail("$notify_email",  "NOTE Detection de tricheries de la part de $result[site_name] sur $list_name",
+
+
+"Bonjour Maître,
+
+
+
+
+
+Notre système de détection de tricheries à $list_name a détecté des anomalies dans les votes en provenance de ce site.
+
+
+
+
+
+ID: $result[id]
+
+
+$result[site_name] ($result[site_addr])
+
+
+
+
+
+Donc, ce site est invisible à $list_name ($list_url) pour $tilt_insp_timeout minutes.
+
+
+
+
+
+Cheating-Detection (Tilt-Protection)
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+#  End Language
+
+
+#################################################################################################
+
+
+?>
