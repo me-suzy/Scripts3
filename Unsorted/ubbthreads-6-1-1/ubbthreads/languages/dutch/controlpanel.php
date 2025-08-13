d1523ce269d7f90bf4208f204fdff496546e5715
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:21 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/controlpanel.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/controlpanel.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['SHOW_ALL']     =  "...alles bekijken";
+$ubbt_lang['PERSONAL']     =  "Persoonlijke informatie, email, wachtwoord, enz.";
+$ubbt_lang['DISPLAY']      =  "Weergave instellingen, aantal zichtbare onderwerpen, taalinstellingen, kleuren...";
+$ubbt_lang['SUB_UN']       =  "Berichtnotificaties aan- of uitschakelen, notificatie instellingen, enz.";
+$ubbt_lang['YOUR_VIEW']    =  "Andere leden zien jou als:";
+$ubbt_lang['YOUR_VIEW_WITH']  =  "met";
+$ubbt_lang['VIEW_POSTS']      =  "berichten";
+$ubbt_lang['TEXT_TOTAL']      =  "Totaal";
+$ubbt_lang['TEXT_UNREAD']     =  "Ongelezen";
+$ubbt_lang['EDIT_ICON']       =  "Bewerken";
+$ubbt_lang['WEL_CONTROL']  =  "Profiel van ";
+$ubbt_lang['MAIN_CONFIG']  =  "Instellingen";
+$ubbt_lang['EDIT_TEXT']    =  "Bewerken";
+$ubbt_lang['PRIVATE_BOX']    =  "Berichten";
+$ubbt_lang['FRIENDS_ON']     =  "Vrienden online";
+$ubbt_lang['FROM_ADDR']      =  "in mijn adresboekje";
+$ubbt_lang['FAV_THREADS']    =  "Favoriete onderwerpen";
+$ubbt_lang['TEXT_REMOVE']    =  "Verwijderen";
+$ubbt_lang['REM_THREADS']    =  "Niet vergeten!";
+$ubbt_lang['SEND_A_PM']      =  "Stuur een PM";
+$ubbt_lang['FAV_FORUMS']     =  "Favoriete forums";
+$ubbt_lang['BY_TEXT']        =  "door";
+?>
