69d566d05597711ac28f5a19555657b9f1385e62
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:45 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =      "Your picture ({$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['PROF_PIC']  =       "Your Picture (Entire URL - Picture size is {$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['AUX_ONLINE']  =      "Do you want users to see the actual forum/post you are looking at?";
+$ubbt_lang['NO_PROF']  = 	"Nem találtam adatot a következõ névhez:";
+$ubbt_lang['PROF_HEAD']  = 	"Adatmódosítás:";
+$ubbt_lang['PROF_EDIT']  = 	"Módosíthatja a következõ információkat.";
+$ubbt_lang['BASIC_PROF']  = 	"Alap adatok";
+$ubbt_lang['PROF_PASS']  = 	"A jelszó (4 és 20 karakter között)";
+$ubbt_lang['VER_PASS']  = 	"Jelszó ellenõrzése";
+$ubbt_lang['EMAIL_ADD']  = 	"E-mail cím (többi felhasználó nem látja)";
+$ubbt_lang['FAKE_EMAIL']  = 	"E-mail cím (üzeneteknél irja ki)";
+$ubbt_lang['FULL_NAME']  = 	"Teljes név";
+$ubbt_lang['PROF_SIG']  = 	"Aláírás (maximum {$config['Sig_length']}  karakter hosszú).  Ön használhat jelöléseket is az aláírásában.";
+$ubbt_lang['PROF_HOME']  = 	"Home-lap (max 150 karakter)";
+$ubbt_lang['PROF_OCC']  = 	"Foglalkozás";
+$ubbt_lang['PROF_HOBB']  = 	"Hobbi (max 200 karakter)";
+$ubbt_lang['PROF_LOC']  = 	"Lakhely (ország, város. stb.)";
+$ubbt_lang['PROF_BIO']  = 	"Ismertetõ (max 250 karakter)";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ Szám";
+$ubbt_lang['PROF_VIS']  = 	"Szeretné ha látszana a \"Ki van itt\" lapon?";
+$ubbt_lang['PROF_PRIV']  = 	"Accept private messages from other users?";
+?>
