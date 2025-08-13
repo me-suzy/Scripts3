73100a1ca74b2f37051e7ac69889280c72177335
me-suzy <me.suzana@gmail.com>
2025-08-14 02:42:59 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/categories.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/categories.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['NEW_MEM'] = "Welcome to our newest member,";
+$ubbt_lang['VIEW_PAST']  =     "Zobacz wys³ane w ci±gu ostatnich...";
+$ubbt_lang['PAST24']  =        "24 godzin";
+$ubbt_lang['PAST48']  =        "48 godzin";
+$ubbt_lang['PAST7']  =         "7 dni";
+$ubbt_lang['THREAD_TEXT']  = 	"W±tków";
+$ubbt_lang['CAT_FORUMS']  = 	"K±cików";
+$ubbt_lang['REGED_USERS']  = 	"zarejestrowanych u¿ytkowników.";
+$ubbt_lang['WEL_PRIV_NO']  = 	"Nie jeste¶ zalogowany.";
+$ubbt_lang['CAT_TEXT']  = 	"Kategoria";
+$ubbt_lang['POSTS_TEXT']  = 	"Wiadomo¶ci";
+$ubbt_lang['NEW_TEXT']  = 	"nowe";
+$ubbt_lang['NOT_APPROVED']  = 	"NZ";
+?>
