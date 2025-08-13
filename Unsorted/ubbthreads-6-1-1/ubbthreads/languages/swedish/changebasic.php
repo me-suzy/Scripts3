65cbc8661a694ef784a06dd34c84234a5ed36996
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:34 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/changebasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/changebasic.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['PIXEL_TOO_BIG']="Din bild överstiger ({$theme['PictureWidth']}x{$theme['PictureHeight']}) pixel-begränsningen med en bredd/höjd av ";
+$ubbt_lang['FILE_TOO_BIG']="Bilder kan inte vara störra än {$config['avatarsize']} bytes.";
+$ubbt_lang['PASS_MATCH']  = 	"Lösenord matchar inte.";
+$ubbt_lang['BAD_PIC']  = 	"Felaktig bildtyp.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Lösenordet måste vara mellan 4 och 20 tecken långt.";
+$ubbt_lang['HOME_TOO_LONG']  = 	"Hemsidans adress är för lång.";
+$ubbt_lang['OCC_TOO_LONG']  = 	"Sysselsättning för lång.";
+$ubbt_lang['HOBB_TOO_LONG']  = 	"Hobbyer för lång.";
+$ubbt_lang['LOC_TOO_LONG']  = 	"Hemort för lång.";
+$ubbt_lang['SIG_TOO_LONG']  = 	"Signaturen är för lång.";
+$ubbt_lang['BIO_TOO_LONG']  = 	"Presentationen för lång.";
+$ubbt_lang['BASIC_MOD']  = 	"Grundprofilen har ändrats.";
+?>
