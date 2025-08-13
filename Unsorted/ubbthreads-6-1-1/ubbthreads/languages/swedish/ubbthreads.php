ef66abbe09a6346eea532218d812b38c2e8a1d5e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:47 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/ubbthreads.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/ubbthreads.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['NEW_MEM'] = "Välkommen till vår senaste medlem,";
+$ubbt_lang['VIEW_PAST']  =     "Visa meddelanden de senaste";
+$ubbt_lang['PAST24']  =        "24 timmarna";
+$ubbt_lang['PAST48']  =        "48 timmarna";
+$ubbt_lang['PAST7']  =         "7 dagarna";
+$ubbt_lang['THREAD_TEXT']  = 	"Trådar";
+$ubbt_lang['OFFSET_TIME']  = 	"Klockan är:";
+$ubbt_lang['REGED_USERS']  = 	"Registrerade användare.";
+$ubbt_lang['WEL_PRIV_NO']  =   "<a href=\"{$config['phpurl']}/newuser.php\">Registera</a> ett användarnamn.";
+$ubbt_lang['POSTS_TEXT']  = 	"Inlägg";
+$ubbt_lang['LAST_POST']  = 	"Senaste inlägg";
+$ubbt_lang['NEW_BOARD']  = 	"Nytt forum";
+$ubbt_lang['NEW_TEXT']  = 	"Nya";
+$ubbt_lang['NOT_APPROVED']  = 	"Ej godkänd";
+?>
