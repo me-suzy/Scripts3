c2d9e092e0df06926b4d9f4802d18b3f884f7b11
me-suzy <me.suzana@gmail.com>
2025-08-14 02:42:07 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/mess_handler.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/mess_handler.php
@@ -0,0 +1,11 @@
+<?
+$ubbt_lang['REPLY_HEAD']   =   "Bruk dette skjemaet til å sende en privat beskjed til denne brukeren";
+$ubbt_lang['MESS_COPY']  =     "Jeg vil beholde en kopi av denne beskjeden";
+$ubbt_lang['DELETE_MESS']  = 	"Slett dette innlegget";
+$ubbt_lang['REPLY_MESS']  = 	"Svar på dette innlegget";
+$ubbt_lang['RETURN_START']  = 	"Tilbake til første side";
+$ubbt_lang['MESS_RECEIPT']  = 	"Jeg vil ha en bekreftelse når beskjeden er lest";
+$ubbt_lang['MESS_PREVIEW']  = 	"Jeg vil forhåndsvise beskjeden";
+$ubbt_lang['ORIG_MESS']  = 	"Det originale innlegget";
+$ubbt_lang['ORIG_MESS']  = 	"Den orginale beskjeden";
+?>
