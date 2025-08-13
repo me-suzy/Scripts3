b49fc61d34fd5c86cb7b26f0c37d9c15cafb4647
me-suzy <me.suzana@gmail.com>
2025-08-14 01:54:51 +0300

    Add Unsorted/Suggest MY Site v1.0/inc/config.inc.php

---
+++ Unsorted/Suggest MY Site v1.0/inc/config.inc.php
@@ -0,0 +1,20 @@
+<?
+/*
+for english change the  "t" to "e"  and for german to "g" below
+*/
+$l="e"; 
+$sitename="MyPHPscripts";  /* will be displayed in the "From" field of the mail */
+
+$url="http://www.myphpscripts.com";  /* will be displayed in the body of the mail */
+
+$adminmail="support@myphpscripts.com";
+
+                 /*************************************
+                  sends a thank you message to person 
+				  who suggests the site. 
+                  set to "1" if you want to send a thank you mail
+				  to the person who suggests your site
+                  else set it to "0"
+                  ***********************************/
+$mailtosender="1";
+?>
