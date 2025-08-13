fa9786d66748efefb80a258f285ccf91fc627941
me-suzy <me.suzana@gmail.com>
2025-08-14 01:54:54 +0300

    Add Unsorted/Suggest MY Site v1.0/inc/eng.lang.inc.php

---
+++ Unsorted/Suggest MY Site v1.0/inc/eng.lang.inc.php
@@ -0,0 +1,29 @@
+<?
+$suggest_header="SUGGESTION FORM :";
+$suggest_exp="Please fill the form below to suggest this site. Note that All the fields are required but the message";
+$yourname="Your name";
+$yourmail="Your e-mail";
+$recipientname="Recipient's name";
+$recipientmail="Recipient's e-mail";
+$yourmessage="Your message";
+$submitbutton="Suggest";
+$siteownermessage="";
+$mailsubject="I suggest you Myphpscripts.com";
+$thankyoumessage="Thank you for suggesting Myphpscripts.com.";
+$thankyousubject="Thank you for your suggesting Myphpscripts.com.";
+$author="Site Suggestion script, MYSuggest Version 1.0, by MERT YALDIZ\nGet yours for free at http://www.myphpscripts.com";
+/* variables for mail body */
+$suggestionfor="Suggestion mail for";
+$from="From :";
+$sendersmail="E-mail :";
+$wrote="says :";
+
+/* messages */
+$invalidmail="The email address listed below is invalid";
+$emptyfield="Please fill the fields listed below :";
+$emptymail1="Please fill the fields listed below :";
+$mailsent="Your suggestion mail succesfully sent to :";
+$mailproblem="A problem occured while sending your suggestion ! Please try again.";
+$error="ERROR !";
+$goback="Back";
+?>
