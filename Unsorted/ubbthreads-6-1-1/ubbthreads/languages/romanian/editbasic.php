9e0f6925d31466e7d72a978e6b0b06d7f751fa94
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:28 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =      "Your picture ({$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['PROF_PIC']  =       "Your Picture (Entire URL - Picture size is {$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['AUX_ONLINE']  =      "Do you want users to see the actual forum/post you are looking at?";
+$ubbt_lang['PROF_PRIV']  = 	"Accept mesaje private de la ceilalti utilizatori?";
+$ubbt_lang['NO_PROF']  = 	"Nu am gasit profilul utilizatorului";
+$ubbt_lang['PROF_HEAD']  = 	"Editeaza profilul pentru";
+$ubbt_lang['PROF_EDIT']  = 	"Puteti edita oricare din informatiile care urmeaza.";
+$ubbt_lang['BASIC_PROF']  = 	"Profil principal";
+$ubbt_lang['PROF_PASS']  = 	"Parola (intre 4 si 20 de caractere)";
+$ubbt_lang['VER_PASS']  = 	"Verifica parola";
+$ubbt_lang['EMAIL_ADD']  = 	"Adresa de email (nu este disponibila celorlalti utilizatori)";
+$ubbt_lang['FAKE_EMAIL']  = 	"Adresa de email care apare in post-uri";
+$ubbt_lang['FULL_NAME']  = 	"Numele complet";
+$ubbt_lang['PROF_SIG']  = 	"Semnatura (maxim   {$config['Sig_length']} caractere). Se poate folosi UBBCode";
+$ubbt_lang['PROF_HOME']  = 	"Pagina web (maxim 150 caractere)";
+$ubbt_lang['PROF_OCC']  = 	"Ocupatie";
+$ubbt_lang['PROF_HOBB']  = 	"Hobiuri (maxim 200 caractere)";
+$ubbt_lang['PROF_LOC']  = 	"Localizare geografica (stat, tara, etc)";
+$ubbt_lang['PROF_BIO']  = 	"Bio (maxim 250 caractere)";
+$ubbt_lang['PROF_ICQ']  = 	"Numar ICQ";
+$ubbt_lang['PROF_VIS']  = 	"Vreti sa apareti in pagina cu utilizatorii online?";
+?>
