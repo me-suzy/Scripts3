8800daa6c3d79fff6d8b5b9a59847125069c5e23
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:31 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/modifypost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/modifypost.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['FILE_TOO_BIG']="Files can be no larger than {$config['filesize']}";
+$ubbt_lang['FILESALLOWED']  =  "The files you may upload must have the following extension";
+$ubbt_lang['NO_EDIT']  =       "You cannot edit this post.";
+$ubbt_lang['PEDIT_DELETE']  = 	"Delete this post";
+$ubbt_lang['DELETE_CONF']  = 	"If you are sure you want to do this, click the button below.";
+$ubbt_lang['YES_DELETE']  = 	"Yes, I want to delete this post";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Approve this post";
+$ubbt_lang['APPROVE_CONF']  = 	"If you are sure you want to do this, click the button below.";
+$ubbt_lang['YES_APPROVE']  = 	"Yes, I want to approve this post";
+$ubbt_lang['MAKE_STICKY']  = 	"Make post sticky";
+$ubbt_lang['TEXT_STICKY']  = 	"This will make this post always appear on top when sorting by descending date.";
+$ubbt_lang['RET_NORM']  = 	"Return to normal date";
+$ubbt_lang['TEXT_NORM']  = 	"This will return this post to it's normal position when sorting by descending date.";
+$ubbt_lang['MODIF_HEAD']  = 	"The post has been modified";
+$ubbt_lang['FORUM_RETURN']  =  "Return to the forum";
+$ubbt_lang['VIEW_POST']  =     "View your post";
+$ubbt_lang['NO_ATTACH']    = "You may attach a file to this post while editing.";
+$ubbt_lang['HASATTACH']		 = "This post has an attachment you may change it by uploading a new one, or delete it.";
+$ubbt_lang['DELETEATTACH']  = "Delete this attachment";
+$ubbt_lang['EDIT_MORE']  = 	"Edit some more";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Change this post";
+$ubbt_lang['PREV_MODS']  = 	"Preview your modifications.";
+$ubbt_lang['MODS_BODY']  = 	"The following is a preview of your modifications.  If everything looks ok then you can click 'Continue' and your post will be entered.  If not then use the bottom form below to edit some more.";
+$ubbt_lang['POST_TEXT']  = 	"Post";
+?>
