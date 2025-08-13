159c9ce52a704aff34c2c403e4a7f6a84df020ee
me-suzy <me.suzana@gmail.com>
2025-08-14 02:41:57 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "Move this post & any replies";
+$ubbt_lang['DELETE_THREAD']  =  "Delete this thread";
+$ubbt_lang['NO_EDIT']  = 	"Det ser dessverre ut til at du ikke har tillatelse til å redigere dette innlegget.";
+$ubbt_lang['EDITTIME']  = 	"Dette innlegget kan ikke lenger redigeres fordi maks. redigeringstid har utløpt";
+$ubbt_lang['PEDIT_HEAD']  = 	"Rediger dette innlegg";
+$ubbt_lang['PEDIT_BODY']  = 	"Du har mulighet til å endre innlegget og å slette det. Hvis du sletter innlegget og det er svar på det, vil emnet på innlegget bli endret til 'Dette innlegget er slettet'.";
+$ubbt_lang['EDIT_POLL']  = 	"Fordi dette innlegget inneholder en avstemning kan du slette det, men ikke redigere det.";
+$ubbt_lang['POST_ICON']  = 	"Ikon til innlegget";
+$ubbt_lang['MARK_EDIT']  = 	"Merk som redigert?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Endre dette innlegget";
+$ubbt_lang['PEDIT_DELETE']  = 	"Slett dette innlegget";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Godkjenn dette innlegget";
+$ubbt_lang['RET_NORM']  = 	"Gå tilbake til normal dato";
+$ubbt_lang['MAKE_STICKY']  = 	"Gjør innlegget 'sticky'";
+$ubbt_lang['MANAGE_THREAD']  = 	"Administrer dette emnet";
+$ubbt_lang['OPEN_THREAD']  = 	"Åpne dette emnet";
+$ubbt_lang['CLOSE_THREAD']  = 	"Lukk dette emnet";
+$ubbt_lang['UNKEEP_THREAD']  = 	"Slett dette emnet";
+$ubbt_lang['KEEP_THREAD']  = 	"Behold dette emnet";
+$ubbt_lang['MOVE_THREAD']  = 	"Flytt dette emnet";
+?>
