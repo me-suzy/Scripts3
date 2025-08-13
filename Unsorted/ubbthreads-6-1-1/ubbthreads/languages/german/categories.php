f4d52767489a893c1d2e60820123da259763c5bb
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:47 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/categories.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/categories.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['NEW_MEM'] = "Wir begr&uuml;&szlig&;en unser neuestes Mitglied,";
+$ubbt_lang['VIEW_PAST']  =     "Neueste Nachrichten anzeigen";
+$ubbt_lang['PAST24']  =        "letzte 24 Stunden";
+$ubbt_lang['PAST48']  =        "letzte 48 Stunden";
+$ubbt_lang['PAST7']  =         "letzte 7 Tage";
+$ubbt_lang['THREAD_TEXT']  = 	"Threads";
+$ubbt_lang['CAT_FORUMS']  = 	"Foren";
+$ubbt_lang['REGED_USERS']  = 	"registrierte Benutzer.";
+$ubbt_lang['WEL_PRIV_NO']  = 	"Sie sind nicht angemeldet.";
+$ubbt_lang['CAT_TEXT']  = 	"Kategorie";
+$ubbt_lang['POSTS_TEXT']  = 	"Nachrichten";
+$ubbt_lang['NEW_TEXT']  = 	"neu";
+$ubbt_lang['NOT_APPROVED']  = 	"NF";
+?>
