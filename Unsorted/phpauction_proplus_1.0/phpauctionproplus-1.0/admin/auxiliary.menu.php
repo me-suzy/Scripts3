56bcfeca21db4a824ed6cc3e9620918deac40aee
me-suzy <me.suzana@gmail.com>
2025-08-14 00:13:14 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/admin/auxiliary.menu.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/admin/auxiliary.menu.php
@@ -0,0 +1,60 @@
+<?#//v.1.0.0
+
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+?>
+
+<TABLE WIDTH=100% CELLPADDING=2 CELLSPACING="0">
+	<TR> 
+		<TD WIDTH="3%"><B></B></TD>
+		<TD WIDTH="97%"><B><FONT FACE="Tahoma, Verdana" SIZE="2"> 
+			<? print $MSG_368; ?>
+			</FONT></B></TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"><FONT FACE="Tahoma, Verdana" SIZE="2"><IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./newtree.php"  CLASS="links"> 
+			<? print $MSG_369; ?>
+			</A></FONT></TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"> <FONT FACE="Tahoma, Verdana" SIZE="2"> <IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./categories.php"  CLASS="links"> 
+			<? print $MSG_078; ?>
+			</A> </FONT> </TD>
+	</TR>
+	<TR>
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"><B><FONT FACE="Tahoma, Verdana" SIZE="2">
+			<? print $MSG_370; ?>
+			</FONT></B></TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"> <FONT FACE="Tahoma, Verdana" SIZE="2"> <IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./countries.php" CLASS="links"> 
+			<? print $MSG_083; ?>
+			</A> </FONT> </TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"> <FONT FACE="Tahoma, Verdana" SIZE="2"> <IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./payments.php" CLASS="links"> 
+			<? print $MSG_075; ?>
+			</A> </FONT> </TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"> <FONT FACE="Tahoma, Verdana" SIZE="2"> <IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./durations.php" CLASS="links"> 
+			<? print $MSG_069; ?>
+			</A> </FONT> </TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"> <FONT FACE="Tahoma, Verdana" SIZE="2"> <IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./increments.php" CLASS="links"> 
+			<? print $MSG_128; ?>
+			</A> </FONT> </TD>
+	</TR>
+</TABLE>
