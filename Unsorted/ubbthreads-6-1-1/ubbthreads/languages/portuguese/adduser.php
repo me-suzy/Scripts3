cec62fcc6e9bc29ae4e98eb2cd704ace66e23487
me-suzy <me.suzana@gmail.com>
2025-08-14 02:44:28 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =    "Your account needs to be approved by an Administrator before you can login.  You will receive an email once this has been done.";
+$ubbt_lang['APPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} has been approved.";
+$ubbt_lang['NOTAPPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} was not approved.  The reason for this is:";
+$ubbt_lang['NEWREGBODY'] = "A new user has registered at {$config['title']}. To approve this account, please visit";
+$ubbt_lang['NEWREG'] = "New User Registration at";
+$ubbt_lang['INVALID_DATE'] = "That is an invalid date.";
+$ubbt_lang['TOOYOUNG']  =       "Registration for this message board will require approval of your parent or guardian.  You may use the <a href=\"{$config['phpurl']}/coppaform.php\">Permission form</a> for this.";
+$ubbt_lang['UNDER13'] = "Sorry, we do not allow children under 13 to register.";
+$ubbt_lang['BAD_UNAME']  = 	"Não é permitido o uso de caracteres HTML nem espaços antes ou depois do nome.";
+$ubbt_lang['LONG_NAME']  = 	"Comprimento errado do nome.";
+$ubbt_lang['BAD_EMAIL']  = 	"Não é permitido registrar nome de usuário do domínio mail.";
+$ubbt_lang['USER_EXISTS']  = 	"Usuário já existe.";
+$ubbt_lang['NO_MULTI']  = 	"Já existe uma conta com esse email.";
+$ubbt_lang['PASS_MATCH']  = 	"As senhas diferem.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"A senha deve ter entre 4 e 20 caracteres.";
+$ubbt_lang['ILL_PASS']  = 	"Password has illegal characters.  Please use only alphanumeric characters.";
+$ubbt_lang['WEL_MSG']  = 	"Bem vindo(a) a nossos fóruns! Agradecemos por editar seu perfil.";
+$ubbt_lang['PASS_SUB']  = 	"Sua senha para";
+$ubbt_lang['PASS_BODY1']  = 	"Alguem com o IP";
+$ubbt_lang['PASS_BODY2']  = 	"registrou o nome de usuário";
+$ubbt_lang['PASS_BODY3']  = 	"A senha desse usuário é";
+$ubbt_lang['NEW_CONFIRM']  = 	"Nome de usuário registrado.";
+$ubbt_lang['NEW_BODY']  = 	"Seu nome de usuário está sendo registrado. Em breve receberá um mail com sua senha.";
+?>
