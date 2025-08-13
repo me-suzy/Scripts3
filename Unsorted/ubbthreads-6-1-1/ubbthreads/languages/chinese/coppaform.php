4ea7080d6633407fc6d05842e51f86b8974214e3
me-suzy <me.suzana@gmail.com>
2025-08-14 02:29:14 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/coppaform.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/coppaform.php
@@ -0,0 +1,21 @@
+<?
+$ubbt_lang['PARENT_APP'] = "ÐèÒª¼Ò³¤»ò¼à»¤ÈËÍ¬Òâ";
+$ubbt_lang['PARENT_APP_2'] = "Äú±ØÐëÕ÷Çó¼Ò³¤»ò¼à»¤ÈËÍ¬Òâ£¬²ÅÄÜÔÚ±¾Õ¾µÇÂ¼×¢²á¡£ÇëÁÐÓ¡±¾Ò³£¬²¢ÒÀÕÕÒÔÏÂËµÃ÷½øÐÐ£º";
+$ubbt_lang['MORE_INFO'] = "ÏêÏ¸×ÊÑ¶£¬ÇëÇ¢Ñ¯£º<a href=\"mailto:{$config['emailaddy']}\">{$config['emailaddy']}</a>";
+$ubbt_lang['LOGIN_NAME'] = "µÇÈëÃû³Æ";
+$ubbt_lang['LOGIN_NAME2'] = "Äú¿ÉÒÔÌîÐ´¶à¸öµÇÈëÃû³Æ£¬Èç¹ûÓëÆäËûÈËÓÐËùÖØ¸´Ê±£¬ÎÒÃÇ¿ÉÒÔ×Ô¶¯Ê¹ÓÃÄúÌîÐ´µÄÆäËûµÇÈëÃû³ÆÌæÄúÍê³É×¢²á³ÌÐò¡£";
+$ubbt_lang['YOUR_EMAIL'] = "Email ÐÅÏä";
+$ubbt_lang['BIRTHDATE'] = "³öÉúÈÕÆÚ";
+$ubbt_lang['PARENT_INST'] = "¼Ò³¤»ò¼à»¤ÈËÇ©ÊðÖ¸Òý";
+$ubbt_lang['PARENT_INST2'] = "ÇëÌîÐ´ÒÔÏÂÐÅº¯£¬²¢ÓÊ¼Ä¸øÎÒÃÇ¡£";
+$ubbt_lang['COPPA_INST'] = "COPPAÍ¬Òâº¯ÌîÐ´·½Ê½";
+$ubbt_lang['AGREE'] = "ÊÇµÄ£¡ÎÒÒÑ¾­ÏêÏ¸ÔÄ\¶ÁÇÒÁË½âÎÒµÄÐ¡º¢ÔÚ¹óÕ¾Ìá¹©µÄ¸÷Ïî¸öÈË×ÊÁÏ£¬²¢ÇÒÒÑ¾­ÔÄ\¶Á¹óÕ¾µÄÒþË½È¨ÉùÃ÷¡£²¢ÇÒÁË½âËûµÄ¸öÈË×ÊÑ¶¿ÉÒÔÍ¸¹ýµÇÈëÕÊºÅ½øÈëÐÞ¸Ä¡£";
+$ubbt_lang['FULLNAME'] = "¼Ò³¤/¼à»¤ÈËÈ«Ãû";
+$ubbt_lang['SIGNATURE'] = "Ç×±ÊÇ©Ãû";
+$ubbt_lang['RELATION'] = "ÓëÐ¡º¢×Ó¹ØÏµ";
+$ubbt_lang['TELE'] = "ÁªÂçµç»°£¨»òÆäËû¿ÉÒÔÁªÏµµÄ·½Ê½£©";
+$ubbt_lang['PARENT_EMAIL'] = "¼Ò³¤/¼à»¤ÈË Email ÐÅÏä";
+$ubbt_lang['DATE'] = "ÈÕÆÚ";
+$ubbt_lang['REG_DATE'] = "×¢²áÈÕÆÚ";
+$ubbt_lang['QUESTIONS'] = "»¹ÓÐÈÎºÎÎÊÌâÂð£¿¿ÉÒÔ Email µ½";
+?>
