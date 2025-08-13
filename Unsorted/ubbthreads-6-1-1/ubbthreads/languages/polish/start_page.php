4979825c5af4429b10056ecc10325f8cbdd036b6
me-suzy <me.suzana@gmail.com>
2025-08-14 02:44:09 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"Pro¶ba o has³o";
+$ubbt_lang['PASS_REQ_BOD1']  = 	"Kto¶ z adresu";
+$ubbt_lang['PASS_REQ_BOD2']  = 	"poprosi³ o zapasowe has³o dla konta";
+$ubbt_lang['PASS_REQ_BOD3']  = 	"do forum '{$config['title']}'.  Jest to has³o zapasowe. Mo¿esz go u¿yæ je¶li zapomnia³e¶ poprzedniego. Je¶li nie prosi³e¶ o zmianê has³a to po prostu zignoruj ten list. Zapasowe has³o jest nastêpuj±ce: ";
+$ubbt_lang['PASS_MAILED']  = 	"Has³o wys³ane";
+$ubbt_lang['PASS_MAILED2']  = 	"Has³o zapasowe zosta³o wys³ane.";
+$ubbt_lang['NO_EMAIL']  = 	"Brak adresu e-mail dla u¿ytkownika";
+?>
