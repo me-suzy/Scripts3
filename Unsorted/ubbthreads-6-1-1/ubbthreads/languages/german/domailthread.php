8d09d7612d9172c545d945a874d6ff5a7d3c8a52
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:59 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/domailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/domailthread.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['START']  = 	"hat einen Link zu einer Nachricht mit dem Titel";
+$ubbt_lang['START2']  = 	"vom '{$config['title']}' weitergeleitet! Die Nachricht ist unten angefügt.";
+$ubbt_lang['NOTE_FROM']  = 	"Kommentar von";
+$ubbt_lang['POST_BELOW']  = 	"Dies ist der originale Beitrag aus dem Forum:";
+$ubbt_lang['TO_READ']  = 	"Dieser Thread ist zu finden unter";
+$ubbt_lang['EMAIL_SENT']  = 	"eMail wurde versendet";
+$ubbt_lang['EMAIL_CONFIRM']  = "Der Beitrag wurde gesendet an:";
+$ubbt_lang['FORUM_RETURN']  = 	"Zur&uuml;ck zum Forum!";
+?>
