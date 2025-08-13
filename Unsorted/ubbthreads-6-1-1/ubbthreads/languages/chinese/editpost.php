db604d2ace09ee5c9b7141837764dc4a79a85f81
me-suzy <me.suzana@gmail.com>
2025-08-14 02:29:34 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "ÒÆ¶¯ÕâÆªÎÄÕÂ¼°Æä»ØÓ¦µÄÎÄÕÂ";
+$ubbt_lang['DELETE_THREAD']  =  "É¾³ýÕû¸ö»°Ìâ";
+$ubbt_lang['POST_ICON'] =	"ÎÄÕÂÍ¼Ê¾";
+$ubbt_lang['NO_EDIT']  = 	"ÎÒÃÇÎÞ·¨È·ÈÏÄúµÄÈ¨ÏÞ£¬ÄúÎÞ·¨±à¼­ÕâÆªÎÄÕÂ¡£";
+$ubbt_lang['EDITTIME']  = 	"ÒÑ³¬¹ý×î³¤ÐÞ¸ÄÆÚÏÞ£¬ÕâÆªÎÄÕÂ²»ÄÜ×öÈÎºÎÐÞ¸Ä";
+$ubbt_lang['PEDIT_HEAD']  = 	"±à¼­ÎÄÕÂ";
+$ubbt_lang['PEDIT_BODY']  = 	"Äú¿ÉÒÔ¶ÔÕâÆªÎÄÕÂ×öÈÎºÎ¸Ä±ä£¬ÄúÒ²¿ÉÒÔÉ¾³ýËü.  Èç¹ûÄúÑ¡ÔñÉ¾³ýÕâÆªÎÄÕÂ£¬¶øÏÈÇ°ÒÑ¾­ÓÐÈË»ØÓ¦ÁË£¬ÔòÏµÍ³»á°ÑÄúËùÉ¾³ýµÄÕâÆªÎÄÕÂ±êÌâ¸ÄÎª \"This post has been deleted\". ";
+$ubbt_lang['EDIT_POLL']  = 	"ÕâÆªÎÄÕÂÄÚº¬Í¶Æ±£¬ÄúÖ»ÄÜÉ¾³ýËü£¬²»ÄÜÐÞ¸ÄËü¡£";
+$ubbt_lang['MARK_EDIT']  = 	"±êÊ¾±»ÐÞ¸Ä¹ýÁËÂð ?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"¸Ä±äÕâÆªÎÄÕÂ";
+$ubbt_lang['PEDIT_DELETE']  = 	"É¾³ýÕâÆªÎÄÕÂ";
+$ubbt_lang['PEDIT_APPROVE']  = 	"ÈÏ¿ÉÕâÆªÎÄÕÂ";
+$ubbt_lang['RET_NORM']  = 	"»Øµ½Ò»°ãÈÕÆÚ";
+$ubbt_lang['MAKE_STICKY']  = 	"Ê¹ÕâÆªÎÄÕÂ±£ÁôÔÚ×îÉÏ·½";
+$ubbt_lang['MANAGE_THREAD']  = 	"¹ÜÀíÕâ¸ö»°Ìâ";
+$ubbt_lang['OPEN_THREAD']  = 	"´ò¿ªÕâ¸ö»°Ìâ";
+$ubbt_lang['CLOSE_THREAD']  = 	"¹Ø±ÕÕâ¸ö»°Ìâ";
+$ubbt_lang['UNKEEP_THREAD']  = 	"²»ÔÙ±£ÁôÕâ¸ö»°Ìâ";
+$ubbt_lang['KEEP_THREAD']  = 	"±£ÁôÕâ¸ö»°Ìâ";
+$ubbt_lang['MOVE_THREAD']  = 	"ÒÆ¶¯Õâ¸ö»°Ìâ";
+?>
