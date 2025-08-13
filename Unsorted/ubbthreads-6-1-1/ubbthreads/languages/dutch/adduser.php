225bdff35c73b280ccf5503db2c52f1a128a1702
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:10 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =	"Jouw account moet door de administratie worden goedgekeurd.  Je ontvangt een e-mail zodra jouw account is behandelt. Tot die tijd kun je niet inloggen.";
+$ubbt_lang['APPROVED'] = "Jouw account voor {$config['title']} url: {$config['phpurl']} is goedgekeurd.";
+$ubbt_lang['NOTAPPROVED'] = "Jouw account voor {$config['title']} url: {$config['phpurl']} is afgekeurd.  De reden is als volgt:";
+$ubbt_lang['NEWREGBODY'] = "Een nieuw lid heeft zich aangemeld voor {$config['title']}. Om deze account goed te keuren ga je naar:";
+$ubbt_lang['NEWREG'] = "Nieuwe aanmelding voor";
+$ubbt_lang['INVALID_DATE'] = "Dit is een ongeldige datum.";
+$ubbt_lang['TOOYOUNG']  = 	"Registrere voor dit board kan pas na goedkeuring van een ouder of voogd.  Je kunt daar volgend <a href=\"{$config['phpurl']}/coppaform.php\">toestemmingsformulier</a> voor gebruiken.";
+$ubbt_lang['UNDER13'] = "Sorry, kinderen onder de leeftijd van 13 jaar mogen niet registreren";
+$ubbt_lang['BAD_UNAME']  = 	"In je gebruikersnaam mogen alleen alfanumerieke tekens voorkomen.  Speciale tekens zijn geblokkeerd om misbruik van jouw account te voorkomen.  Je kunt het _ teken gebruiken als spatie.";
+$ubbt_lang['LONG_NAME']  = 	"De gebruikersnaam is te lang of te kort.";
+$ubbt_lang['BAD_EMAIL']  = 	"Via volgend email domain is het niet mogelijk te registreren";
+$ubbt_lang['USER_EXISTS']  = 	"Gebruiker bestaat al.";
+$ubbt_lang['NO_MULTI']  = 	"Dit e-mail adres is al eerder gebruikt.";
+$ubbt_lang['PASS_MATCH']  = 	"De paswoorden komen niet overeen.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Het wachtwoord moet tussen 4 en 20 karakters lang zijn.";
+$ubbt_lang['ILL_PASS']  = 	"In jouw wachtwoord komen speciale tekens voor.  Gebruik alleen alfanumerieke tekens a.u.b.";
+$ubbt_lang['WEL_MSG']  = 	"Welkom op ons board!  Neem nu eerst de tijd om op je gemak jouw profiel en voorkeuren aan te passen.";
+$ubbt_lang['PASS_SUB']  = 	"Je wachtwoord voor";
+$ubbt_lang['PASS_BODY1']  = 	"Iemand met IP adres";
+$ubbt_lang['PASS_BODY2']  = 	"heeft volgende gebruikersnaam geregistreerd:";
+$ubbt_lang['PASS_BODY3']  = 	"Het wachtwoord voor deze gebruikersnaam is:";
+$ubbt_lang['NEW_CONFIRM']  = 	"Deze gebruikersnaam is nu geregistreerd.";
+$ubbt_lang['NEW_BODY']  = 	"Jouw gebruikersnaam is nu gereserveerd.  In enkele ogenblikken ontvang je jouw password via email.";
+?>
