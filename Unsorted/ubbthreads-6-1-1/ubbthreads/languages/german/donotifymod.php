188269eb6a083d03671899544e50623f01447891
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:01 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/donotifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/donotifymod.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['POST_PROB']  = 	"Es gab ein Problem beim Auffinden der Nachricht in der Datenbank.";
+$ubbt_lang['BAD_GROUP']  = 	"Sie haben keinen Zugang zu diesem Forum.";
+$ubbt_lang['NO_B_INFO']  = 	"Es konnten keine Informationen zu diesem Forum gefunden werden.";
+$ubbt_lang['NOTIF_HEAD']  = 	"benachrichtigte Sie &uuml;ber eine lesenswerte Nachricht mit dem Titel";
+$ubbt_lang['TO_READ']  = 	"Diese Nachricht kann hier gelesen werden:";
+$ubbt_lang['NOTIFY_SENT']  = 	"Moderator benachrichtigt";
+$ubbt_lang['NOTIFY_CONFIRM']  =  "Der Moderator f&uuml;r dieses Forum wurde benachrichtigt.";
+?>
