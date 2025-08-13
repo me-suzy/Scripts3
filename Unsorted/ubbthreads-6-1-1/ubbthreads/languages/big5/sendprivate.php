adbd9ea1b0eb35500805061599e1a0268a5a1389
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:29 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/sendprivate.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/sendprivate.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['MESS_COPY']  = 	"§Ú·Q­n«O¯d§Ú³o«Ê«H§@¬°±H¥ó³Æ¥÷";
+$ubbt_lang['MESS_RECEIPT']  = 	"·í¹ï¤èÅª¹L§Úªº«H«á¡A§Ú§Æ±æ¨t²Îµo¤@«Ê«Hµ¹§Ú";
+$ubbt_lang['MESS_PREVIEW']  = 	"¦b±H¥X¥h«H¥ó¤§«e¡A§Ú§Æ±æ¯à¹wÄý§Úªº«H¥ó";
+$ubbt_lang['ADD_BOOK']  = 	"¥[¤J§Ú¨p¤Hªº³q°T¿ý·í¤¤";
+$ubbt_lang['USE_BOOK']  = 	"©Î¬O±q§Úªº³q°T¿ý¤¤¬D¿ï¤@¤H";
+$ubbt_lang['CHOOSE_ADD']  = 	"½Ð±q³q°T¿ý¤¤¿ï¾Ü¤@­Ó¦W¦r";
+$ubbt_lang['NO_LOGGED']  = 	"©êºp¡A±z¥²¶·­nµn¤J¤~¯à±H¨p¤H«H¡C";
+$ubbt_lang['NO_PRIVATE'] =	"©êºp¡A³o¦ì¨Ï¥ÎªÌ¤£±µ¨ü¨p¤H«H¥ó¡C";
+$ubbt_lang['PRIV_BODY2'] =	"½Ð¶ñ¼g¥H¤Uªº¤º®e±H«Hµ¹¦¬¥óªÌ¡C¹ï¤è¤U¦¸µn¤J¨t²Î®É´N·|¦¬¨ì±zªº«H¥ó¤F¡C";
+$ubbt_lang['TEXT_DELETE']  = 	"§R°£";
+?>
