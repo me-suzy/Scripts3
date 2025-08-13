75323f178e0fb331525ed876dd187abfce8b5867
me-suzy <me.suzana@gmail.com>
2025-08-14 02:29:02 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =    "Your account needs to be approved by an Administrator before you can login.  You will receive an email once this has been done.";
+$ubbt_lang['APPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} has been approved.";
+$ubbt_lang['NOTAPPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} was not approved.  The reason for this is:";
+$ubbt_lang['NEWREGBODY'] = "A new user has registered at {$config['title']}. To approve this account, please visit";
+$ubbt_lang['NEWREG'] = "New User Registration at";
+$ubbt_lang['INVALID_DATE'] = "¶Ô²»Æð£¬Õâ¸öÈÕÆÚ²»·ûºÏ¹æ·¶¡£";
+$ubbt_lang['TOOYOUNG'] =	"±§Ç¸£¬ÄúÄê¼ÍÌ«Ð¡£¬²»ÄÜ×¢²áÕÊºÅ£¬ ¸ù¾Ý <a href=\"http://www.ftc.gov/opa/1999/9910/childfinal.htm\">COPPA</a> ·¨ÂÉ£¬Ã»ÓÐÄú¸¸Ä¸µÄÍ¬Òâ£¬ÎÒÃÇ²»ÄÜÎªÄú½¨Á¢ÕÊºÅ¡£";
+$ubbt_lang['UNDER13'] = "¶Ô²»Æð£¬±¾Õ¾²»½ÓÊÜÊ®ÈýËêÒÔÏÂ¶ùÍ¯µÇÂ¼×¢²á¡£";
+$ubbt_lang['NEW_BODY']  =  "ÏµÍ³ÒÑ¾­±£ÁôÄúµÄÕÊºÅÁË.  ÄúÓ¦¸ÃºÜ¿ì¾Í»áÊÕµ½±¾Õ¾·¢³öµÄÒ»·â email£¬ÀïÃæ°üº¬ÄúµÄÃÜÂë¡£";
+$ubbt_lang['BAD_UNAME']  = 	"±§Ç¸ £¬Ãû×ÖÖÐ²»ÄÜ²ÎÔÓ HTML Âë £¬ºóÃæÒ²²»ÄÜÓÐ¿Õ°×¡£";
+$ubbt_lang['LONG_NAME']  = 	"Ê¹ÓÃÕßÃû³Æ³¤¶È²»¶Ô¡£";
+$ubbt_lang['BAD_EMAIL']  = 	"±§Ç¸£¬Äú²»ÄÜ´ÓÕâ¸ö email ÍøÓò×¢²áÕÊºÅ";
+$ubbt_lang['USER_EXISTS']  = 	"Õâ¸ö´úºÅÄ¿Ç°ÒÑ¾­ÓÐÈËÊ¹ÓÃ¡£";
+$ubbt_lang['NO_MULTI']  = 	"±§Ç¸£¬ÏµÍ³ÖÐÒÑ¾­ÓÐÒ»¸öÒ»ÑùµÄ Email Address £¬Ò»¸ö Email Address Ö»ÄÜÉêÇëÒ»¸öÕÊºÅ¡£";
+$ubbt_lang['PASS_MATCH']  = 	"ÃÜÂë²»ÕýÈ·¡£";
+$ubbt_lang['PASS_TOO_LONG']  = 	"ÃÜÂë³¤¶È±ØÐë½éÓÚ4µ½20¸ö×ÖÔª¡£";
+$ubbt_lang['ILL_PASS']  = 	"ÃÜÂë°üº¬ÁË·Ç·¨×ÖÔª. ÇëÊ¹ÓÃÓ¢ÎÄÒÔ¼°Êý×Ö×éºÏ³ÉµÄÃÜÂë (A-Za-z0-9)¡£";
+$ubbt_lang['WEL_MSG']  = 	"ÕâÊÇÐÂÊ¹ÓÃÕß¶¼»áÊÕµ½µÄÐÅ¼þ£º<BR>Çë³é¿Õ±à¼­ÄúµÄ¸öÈËµµ°¸¡£Äú¿ÉÒÔÔÚÄúµÄ¸öÈËµµ°¸ÀïÃæÌîÐ´Ò»Ð©¸öÈË×ÊÁÏ(±ðÈË²éÑ¯Ê¹ÓÃÕßÊ±¾Í»áÏÔÊ¾³öÀ´) £¬±à¼­Ç©Ãûµµ(»á¸½¼ÓÔÚÄúµÄÃ¿Ò»ÆªÎÄÕÂºóÃæ) ÒÔ¼°Éè¶¨ÄúµÄ¸öÈËÏÔÊ¾Ï²ºÃ (ÅÅÐò·½·¨£¬ÏÔÊ¾Ä£Ê½,Æ½Ì¹»òÊÇÊ÷×´Ä£Ê½...µÈµÈ.)¡£";
+$ubbt_lang['PASS_SUB']  = 	"ÄúµÄÃÜÂë :";
+$ubbt_lang['PASS_BODY1']  = 	" ÄúºÃ£¬ÕâÊÇÒ»·âÈ·ÈÏÐÅº¯£¬Äú¸Õ²ÅÔÚ±¾Õ¾´Ó IP Î»Ö·£º  ";
+$ubbt_lang['PASS_BODY2']  = 	" µÇÂ¼×¢²áÁËÒ»¸öÐÂµÄ ID£º ";
+$ubbt_lang['PASS_BODY3']  = 	" ÇëÄúÊ¹ÓÃÕâ¸öÃÜÂëµÇÈë£º ";
+$ubbt_lang['NEW_CONFIRM']  = 	"Ê¹ÓÃÕßÃû³ÆÒÑ¾­×¢²áÁË¡£";
+?>
