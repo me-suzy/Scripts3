22122cca6afc31c222fd88807680290385572b49
me-suzy <me.suzana@gmail.com>
2025-08-14 00:41:08 +0300

    Add Unsorted/phpListPro-1.80/phpListPro-1.80/lang_dutch.php

---
+++ Unsorted/phpListPro-1.80/phpListPro-1.80/lang_dutch.php
@@ -0,0 +1,949 @@
+<?
+
+
+#################################################################################################
+
+
+#
+
+
+#  project           	: phpListPro
+
+
+#  filename          	: lang_dutch.php
+
+
+#  version           	: 1.71
+
+
+#  last modified by  	: Erich Fuchs
+
+
+#  e-mail            	: office@smartisoft.com
+
+
+#  purpose           	: Language File
+
+
+#  last modified     	: 03 May 2001
+
+
+#  translated by  	: Reginald (http://www.belgiantop50.com and http://www.dutchtop50.com)
+
+
+#
+
+
+#################################################################################################
+
+
+
+
+
+#  Category Settings
+
+
+#################################################################################################
+
+
+
+
+
+$category[0]            = "Alle categorieën";                // must be all Entries
+
+
+$category[1]            = "Categorie 1";
+
+
+$category[2]            = "Categorie 2";
+
+
+$category[3]            = "Categorie 3";
+
+
+$category[4]            = "Categorie 4";
+
+
+$category[5]            = "Categorie 5";
+
+
+$category[6]            = "Categorie 6";
+
+
+
+
+
+
+
+
+#  Language Settings
+
+
+#################################################################################################
+
+
+
+
+
+$lang[metatag]        	= "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\">";
+
+
+
+
+
+$lang[rank] 		= "#";
+
+
+$lang[site] 		= "Sites";
+
+
+$lang[votes]		= "In";
+
+
+$lang[hits] 		= "Uit";
+
+
+$lang[rate] 		= "Rating";
+
+
+$lang[random] 		= "Willekeurige Link";
+
+
+
+
+
+$lang[in_msg1]		= "Je komt net van";
+
+
+$lang[in_msg2]		= "Druk op het [Stem op deze site] knopje om voor deze site te stemmen.";
+
+
+$lang[in_msg3]		= "Of bezoek $list_name zonder te stemmen.";
+
+
+$lang[in_chose]		= "Gelieve een waarde te selecteren.";
+
+
+$lang[in_vote]		= "Stem voor deze site.";
+
+
+
+
+
+$lang[anti_cheat_msg] 	= "<center><font color=red>Je hebt reeds gestemd de afgelopen $vote_timeout uren! Je stem werd niet opgenomen.</font></center>";
+
+
+$lang[vote_log_msg] 	= "<center><font color=red>Bedankt! Je stem werd gelogd!</font></center>";
+
+
+$lang[not_found_msg] 	= "<center><font color=red>FOUT: site NIET gevonden!</font></center>";
+
+
+$lang[tilt_msg] 	= "<center><font color=red>Deze site is tijdelijk onzichtbaar na TILT detectie! Probeer het later nog eens!</font></center>";
+
+
+$lang[nocookie_msg] 	= "<center><font color=red>Je browser dient cookies te aanvaarden! Je stem werd niet gelogd!</font></center>";
+
+
+$lang[ban_msg] 		= "<center><font color=red>Je kan niet stemmen in deze lijst! Je stem werd niet gelogd.</font></center>";
+
+
+
+
+
+$lang[emailconfirmed]	= "E-mail bevestigd!";
+
+
+$lang[emailnotconf]	= "E-mail niet bevestigd! Gelieve de Webmaster te contacteren.";
+
+
+$lang[redirect]		= "Je wordt doorgestuurd, klein momentje";
+
+
+$lang[confirmation]	= "Bevestiging";
+
+
+
+
+
+$lang[pages]		= "Paginas: ";
+
+
+$lang[useronline]	= "User online";
+
+
+$lang[usersonline]	= "Users online";
+
+
+$lang[userssatisfied]	= "Users satisfied";
+
+
+$lang[sitesindb]	= "Sites in onze Database";
+
+
+$lang[inoutresets]	= "In/Uit wordt iedere";
+
+
+$lang[days]		= "dagen gereset.";
+
+
+$lang[next]		= " volgende";
+
+
+
+
+
+$lang[addsite]		= "Site toevoegen";
+
+
+$lang[addsitedesc]	= "Nieuwe site toevoegen";
+
+
+$lang[editsite]		= "Site wijzigen";
+
+
+$lang[editsitedesc]	= "Een bestaande site wijzigen";
+
+
+$lang[helpsite]		= "Help";
+
+
+$lang[helpsitedesc]	= "Hulp met de HTML code";
+
+
+
+
+
+$lang[error]		= "Fout! Gelieve terug te gaan en aan te passen!";
+
+
+$lang[error_duplicate]	= "Deze site staat al in onze database!";
+
+
+$lang[error_emptysiten]	= "Je moet de title van je site nog ingeven.";
+
+
+$lang[error_emptysitea]	= "Je met het adres van je site nog ingeven.";
+
+
+$lang[error_emptypass]	= "Je hebt geen paswoord opgegeven.";
+
+
+$lang[error_emptyemail]	= "Je hebt je e-mail adres niet ingevuld.";
+
+
+$lang[error_emptyname]	= "Je hebt je naam niet ingevuld.";
+
+
+$lang[error_emptydesc]	= "Je hebt geen omschrijving bij je site ingevuld.";
+
+
+$lang[error_bannerw]	= "De breedte van je banner is ongeldig.";
+
+
+$lang[error_bannerh]	= "De hoogte van je banner is ongeldig.";
+
+
+$lang[error_banners]	= "Je banner is groter dan de toegestane limiet.";
+
+
+$lang[error_bannernok]	= "Je banner URL is ongeldig.";
+
+
+$lang[error_baddesc]	= "Er staat een ontoelaatbaar woord in je site omschrijving.";
+
+
+$lang[error_badname]	= "Er staat een ontoelaatbaar woord in de titel van je site.";
+
+
+
+
+
+$lang[back]		= "Terug";
+
+
+$lang[done]		= "Klaar";
+
+
+$lang[submit]		= "Toevoegen";
+
+
+$lang[search]           = "Search";
+
+
+$lang[getlost]		= "ID en/of paswoord vergeten?";
+
+
+$lang[getid]		= "ID en/of paswoord ophalen.";
+
+
+$lang[editsite]		= "Site wijzigen";
+
+
+
+
+
+$lang[enterdata]	= "Vul je gegevens in.";
+
+
+$lang[password]		= "Paswoord: ";
+
+
+$lang[siteid]		= "Site ID: ";
+
+
+$lang[emailaddr]	= "Email adres: ";
+
+
+$lang[yourname]		= "Je naam: ";
+
+
+$lang[sitetitle]	= "Site titel: ";
+
+
+$lang[siteurl]		= "Site URL: ";
+
+
+$lang[sitedesc]		= "Site omschrijving: ";
+
+
+$lang[bannerurl]	= "Banner URL: ";
+
+
+$lang[bannerw]		= "Banner breedte: ";
+
+
+$lang[bannerh]		= "Banner hoogte: ";
+
+
+
+
+
+$lang[getlost2]		= "ID en/of paswoord ophalen.";
+
+
+$lang[edityoursite]	= "Wijzig je site";
+
+
+
+
+
+$lang[getidok]		= "Site gevonden en de informatie werd verstuurd!";
+
+
+$lang[getidnok]		= "Er werd geen site met dat e-mail adres gevonden!";
+
+
+$lang[editidnok]	= "Er werd geen site met die gegevens gevonden!";
+
+
+$lang[editsuccess]	= "Je gegevens werden aangepast!";
+
+
+
+
+
+$lang[addthanks]	= "Bedankt! Je site werd toegeveogd!";
+
+
+$lang[addloginid]	= "Je site ID is: ";
+
+
+$lang[addsendmail]	= ", we hebben een bevestiging per e-mail gestuurd.";
+
+
+$lang[addhint]		= "Je hebt dit ID en je paswoord nodig als je je gegevens wil zijzigen.";
+
+
+$lang[addapproval]	= "Even geduld, onze Webmaster zal je site eerst nakijken!";
+
+
+$lang[addplease]	= "Gelieve de volgende HTML code op je site te plaatsen zodat jouw bezoekers op je site kunnen stemmen!";
+
+
+
+
+
+$lang[help]		= "Help";
+
+
+$lang[helpreplace1]	= "Vervang <b>XXXXXXXX</b> door je ID nummer dat naar je e-mail adres werd gestuurd.";
+
+
+$lang[helpreplace2]	= "Vervang <b>www.domeinnaam.com</b> door de URL naar je button of banner.";
+
+
+
+
+
+$lang[addrules]		= "Regels";
+
+
+$lang[addyoursite]	= "Voeg je site toe";
+
+
+
+
+
+$lang[rules]            = "<ul>
+
+
+                            <li>Ranking depends only on INCOMING Hit's
+
+
+                            <li>NO Script's allowed for incoming Hit's
+
+
+                    	    <li>Only TOP 10 Sites displayed with banners
+
+
+                            <li>The Counter's will be reset periodically
+
+
+                    	    <li>!!! Cheating is NOT allowed !!!
+
+
+                           </ul>";
+
+
+
+
+
+#  Help Language Forms
+
+
+#################################################################################################
+
+
+
+
+
+function help($id) {
+
+
+    global $list_name,$list_url,$voteimage_1,$voteimage_2,$voteimage_3;
+
+
+
+
+
+echo "<hr><b><li>TextLink:</b><br>";
+
+
+echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt$list_name&lt/a&gt\n";
+
+
+
+
+
+
+
+
+if ($voteimage_1) {
+
+
+  echo "<p><hr><b><li>PictureLink:</b><br>\n";
+
+
+  echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt<br>&ltimg src=\"http://www.yourdomain.com/vote_image1.gif\" alt=\"Stem voor deze site bij $list_name!\" border=0&gt&lt/a&gt\n";
+
+
+  echo "<p><center><img src=\"$voteimage_1\"><br>Dit is de afbeelding. Bewaar dit op je eigen server (niet hotlinken).<br>(Rechter muisklik en kies Save As)</center><br>\n";
+
+
+  }
+
+
+
+
+
+if ($voteimage_2) {
+
+
+  echo "<p><hr><b><li>PictureLink 2:</b><br>\n";
+
+
+  echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt<br>&ltimg src=\"http://www.yourdomain.com/vote_image2.gif\" alt=\"Stem voor deze site bij $list_name!\" border=0&gt&lt/a&gt\n";
+
+
+  echo "<p><center><img src=\"$voteimage_2\"><br> Dit is de afbeelding. Bewaar dit op je eigen server (niet hotlinken).<br>(Rechter muisklik en kies Save As)</center><br>\n";
+
+
+  }
+
+
+
+
+
+if ($voteimage_3) {
+
+
+  echo "<p><hr><b><li>PictureLink 3:</b><br>\n";
+
+
+  echo "&lta href=\"$list_url/in.php?site=$id\" target=\"_blank\"&gt<br>&ltimg src=\"http://www.yourdomain.com/vote_image3.gif\" alt=\" Stem voor deze site bij $list_name!\" border=0&gt&lt/a&gt\n";
+
+
+  echo "<p><center><img src=\"$voteimage_3\"><br> Dit is de afbeelding. Bewaar dit op je eigen server (niet hotlinken).<br>(Rechter muisklik en kies Save As)</center><br>\n";
+
+
+  echo "<p>";
+
+
+  }
+
+
+}
+
+
+
+
+
+#  Mail Language Forms
+
+
+#################################################################################################
+
+
+
+
+
+function mail_getidpwd($result) {		// Mail Form for sending lost ID/Password
+
+
+    global $list_name,$list_url,$admin_email;
+
+
+
+
+
+mail("$result[email]", "Gegevens bij $list_name", "Hallo $result[name],
+
+
+Dit zijn de gegevens van je site bij $list_name:
+
+
+
+
+
+$result[site_name]
+
+
+$result[site_addr]
+
+
+Site ID: $result[id]
+
+
+Paswoord: $result[password]
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+HTML code:
+
+
+<a href=\"$list_url/in.php?site=$result[id]\" target=\"_blank\"><img
+
+
+src=\"www.yourdomain.com/vote_image1.gif\" border=\"0\"></a>
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_newnotifyuser($email_address,$webmaster_name,$id,$password,$site_name,$site_address,$site_description) {
+
+
+    global $list_name,$list_url,$admin_email,$secret,$webmasterapproval,$emailapproval;
+
+
+
+
+
+if ($webmasterapproval) {$webmastermsg="\nEven geduld, we zullen je site zo snel mogelijk nazien.\n";} else {$webmastermsg="";}
+
+
+if ($emailapproval) {$emailmsg="\nJe dient op deze link te klikken om je inschrijving te bevestigen:\n$list_url/confirm.php?check=".urlencode(encrypt($id,$secret))."\n\n";} else {$emailmsg="";}
+
+
+
+
+
+mail("$email_address", "Welkom $webmaster_name bij de $list_name", "Bedankt $webmaster_name voor je inschrijving in de $list_name.
+
+
+$webmastermsg$emailmsg
+
+
+Site gegevens:
+
+
+$site_name ($site_address)
+
+
+
+
+
+Site code: $id
+
+
+Password: $password
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+HTML code:
+
+
+
+
+
+<a href=\"$list_url/in.php?site=$id\" target=\"_blank\"><img
+
+
+src=\"http://www.yourdomain.com/$voteimage_1\" alt=\"Stem voor mij in de $list_name!\"
+
+
+border=0></a>
+
+
+
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_newnotifyadm($email_address,$webmaster_name,$id,$password,$site_name,$site_address,$site_description) {
+
+
+    global $list_name,$list_url,$admin_email,$notify_email;
+
+
+
+
+
+mail("$notify_email", "NOTIFY $webmaster_name joined the $list_name", "$webmaster_name joined the $list_name.
+
+
+
+
+
+Site code: $id
+
+
+Password: $password
+
+
+Site name: $site_name
+
+
+Site address: $site_address
+
+
+
+
+
+Email: $email_address
+
+
+
+
+
+Banner address (if any): $banner_address
+
+
+
+
+
+Description:
+
+
+$site_description
+
+
+
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_editnotifyadm($email_address,$webmaster_name,$id,$password,$site_name,$site_address,$site_description) {
+
+
+    global $list_name,$list_url,$admin_email,$notify_email;
+
+
+
+
+
+mail("$notify_email", "NOTIFY $webmaster_name edited on $list_name", "$webmaster_name edited on $list_name.
+
+
+
+
+
+Site code: $id
+
+
+Password: $password
+
+
+Site name: $site_name
+
+
+Site address: $site_address
+
+
+
+
+
+Email: $email_address
+
+
+
+
+
+Banner address (if any): $banner_address
+
+
+
+
+
+Description:
+
+
+$site_description
+
+
+
+
+
+
+
+
+$list_url/list.php
+
+
+
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_tiltuser($result) {		// Mail Form for sending User on TILT
+
+
+    global $list_name,$list_url,$admin_email,$tilt_insp_timeout;
+
+
+
+
+
+mail("$result[email]", "Cheating Detection van $result[site_name] op $list_name","Hallo,
+
+
+Ons Cheat Detection systeem bij $list_name heeft enkele vreemde stemmen vastgesteld bij jouw
+
+
+site:
+
+
+
+
+
+ID: $result[id]
+
+
+$result[site_name] ($result[site_addr])
+
+
+
+
+
+Bijgevolg werd je site tijdelijk onzichtbaar gemaakt bij $list_name ($list_url) gedurende
+
+
+$tilt_insp_timeout minuten.
+
+
+
+
+
+Als er nog verdachte stemmen volgen, blijft je site enkel maar langer onzichtbaar.
+
+
+Mvg,
+
+
+
+
+
+Webmaster $list_name
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mail_tiltadmin($result) {		// Mail Form for sending lost ID/Password
+
+
+    global $list_name,$list_url,$admin_email,$notify_email,$tilt_insp_timeout;
+
+
+
+
+
+mail("$notify_email",  "Cheating Detection vastgesteld door $result[site_name] bij $list_name","Hallo Webmaster,
+
+
+Het Cheat Detection systeem bij $list_name heft enkele vreemde stemmen vastgesteld bij de
+
+
+volgende site.
+
+
+
+
+
+ID: $result[id]
+
+
+$result[site_name] ($result[site_addr])
+
+
+
+
+
+Bijgevolg is de site onzichtbaar bij $list_name ($list_url) gedurende $tilt_insp_timeout minuten.
+
+
+
+
+
+
+
+
+Cheating-Detection (Tilt-Protection)
+
+
+", "From: $admin_email");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+#  End Language
+
+
+#################################################################################################
+
+
+?>
