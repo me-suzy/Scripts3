f226e503d64d83d79abd86e55a0c17737f4fb977
me-suzy <me.suzana@gmail.com>
2025-08-14 01:22:44 +0300

    Add Unsorted/Relata v0.3.0/Relata v0.3.0/wwwroot/group_email/group_email_tabs.js

---
+++ Unsorted/Relata v0.3.0/Relata v0.3.0/wwwroot/group_email/group_email_tabs.js
@@ -0,0 +1,48 @@
+// add event listeners to all functions
+tabone = document.getElementById("tab1");
+addevent(tabone,"click",tabclick,false);
+
+tabtwo = document.getElementById('tab2');
+addevent(tabtwo,"click",tabclick,false);
+
+tabthree = document.getElementById('tab3');
+addevent(tabthree,"click",tabclick,false);
+
+tabfour = document.getElementById('tab4');
+addevent(tabfour,"click",tabclick,false);
+
+// event listener function... called when you click on a tab
+function tabclick(e)
+{
+	// if ie
+	if(window.event)
+	{
+		e = event;
+		tabid = e.srcElement.id;
+	}
+	else
+	{
+		tabid = e.currentTarget.id;
+	}
+	
+	// get the tab #
+	tabid = tabid.charAt(3);
+
+	// change the visibility of the IFRAME and change the buttons
+	for(i = 1; i <= 4; i++)
+	{	
+		if(i == tabid) 
+		{
+			visibility = "visible";
+			tabstatus = "tab-selected";
+		}
+		else 
+		{
+			visibility = "hidden";
+			tabstatus = "tab-normal";
+		}
+		
+		document.getElementById("tab" + i).className = tabstatus;
+		document.getElementById("iframe" + i).style.visibility = visibility;
+	}
+}
