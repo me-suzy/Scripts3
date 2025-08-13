766d9486e7777c71f36ef13ed9269a1b224a20a4
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:20 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/doratethread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/doratethread.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['INVALID_RATING'] = "That is an invalid rating.";
+$ubbt_lang['NOMORERATE'] = "Ya has votado por este tema.";
+$ubbt_lang['THANKS'] = "Muchas gracias.";
+$ubbt_lang['RATED']	= "Tu voto por este tema ha sido aceptado.";
+?>
