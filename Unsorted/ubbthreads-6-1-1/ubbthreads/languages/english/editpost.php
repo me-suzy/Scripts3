17b24c3900f4ace535651211188c6873daaf3c13
me-suzy <me.suzana@gmail.com>
2025-08-14 02:34:13 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my modifications and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "Move this post & any replies";
+$ubbt_lang['NO_EDIT']  = 	"You cannot edit this post.";
+$ubbt_lang['EDITTIME']  = 	"This post can no longer be edited because the maximum edit time has expired";
+$ubbt_lang['PEDIT_HEAD']  = 	"Edit post";
+$ubbt_lang['PEDIT_BODY']  = 	"Edit or delete this post.";
+$ubbt_lang['EDIT_POLL']  = 	"Because this post has a poll in it, you may delete it but not edit it.";
+$ubbt_lang['POST_ICON']  = 	"Post Icon";
+$ubbt_lang['MARK_EDIT']  = 	"Mark as Edited?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Change this post";
+$ubbt_lang['PEDIT_DELETE']  = 	"Delete this post";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Approve this post";
+$ubbt_lang['RET_NORM']  = 	"Return to normal date";
+$ubbt_lang['MAKE_STICKY']  = 	"Make post sticky";
+$ubbt_lang['MANAGE_THREAD']  = 	"Manage this thread";
+$ubbt_lang['DELETE_THREAD']  =  "Delete this thread";
+$ubbt_lang['OPEN_THREAD']  = 	"Open this thread";
+$ubbt_lang['CLOSE_THREAD']  = 	"Close this thread";
+$ubbt_lang['UNKEEP_THREAD']  = 	"Unkeep this thread";
+$ubbt_lang['KEEP_THREAD']  = 	"Keep this thread";
+$ubbt_lang['MOVE_THREAD']  = 	"Move this thread";
+?>
