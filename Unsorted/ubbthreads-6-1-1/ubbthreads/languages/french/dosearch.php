2587faa37655d8acd423757a1e729bcb20873459
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:37 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/dosearch.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_WORDS']  = 	"Vous devez entrer au moins un mots-clés à rechercher.";
+$ubbt_lang['SEARCH_RES']  = 	"Résultats de recherche pour la requête";
+$ubbt_lang['PREV_PAGE']  = 	"Page précédente";
+$ubbt_lang['MAIN_SEARCH']  = 	"Page principale de recherche";
+$ubbt_lang['NEXT_PAGE']  = 	"Page suivante";
+$ubbt_lang['NO_MATCH']  = 	"Nous n'avons trouvé aucun résultat répondant à votre requête.";
+?>
