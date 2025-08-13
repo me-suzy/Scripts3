cd6e1d1ecdc7aa6f78696f58a154382b87ee44d1
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:37 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"Çíà÷åíèå &quot;ñîîáùåíèé íà ñòðàíèöó&quot; íå ìîæåò áûòü èñïîëüçîâàíî.";
+$ubbt_lang['FLAT_BAD']  = 	"Flat Posts per page must be between 1 and 99.";
+$ubbt_lang['TEXTAREA_NO']  = 	"Çíà÷åíèå òåêñòîâîãî ïîëÿ íå ìîæåò áûòü èñïîëüçîâàíî.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Âàøè óñòàíîâêè ïàðàìåòðîâ ýêðàíà óñïåøíî èçìåíåíû.";
+?>
