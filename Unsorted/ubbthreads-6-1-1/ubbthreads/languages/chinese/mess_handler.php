d5da0f592302554aa2413da9f813b57b39a3b10d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:29:45 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/mess_handler.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/mess_handler.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['REPLY_HEAD']   =   "¼ÄÐÅ¸ø";
+$ubbt_lang['MESS_COPY']  =     "ÎÒÏëÒª±£ÁôÎÒÕâ·âÐÅ×÷Îª¼Ä¼þ±¸·Ý";
+$ubbt_lang['DELETE_MESS']  = 	"É¾³ýÕâÆªÎÄÕÂ";
+$ubbt_lang['REPLY_MESS']  = 	"»ØÓ¦ÕâÆªÎÄÕÂ";
+$ubbt_lang['RETURN_START']  = 	"»Øµ½ÄúµÄÊ×Ò³";
+$ubbt_lang['MESS_RECEIPT']  = 	"µ±¶Ô·½¶Á¹ýÎÒµÄÐÅºó£¬ÎÒÏ£ÍûÏµÍ³·¢Ò»·âÐÅ¸øÎÒ";
+$ubbt_lang['MESS_PREVIEW']  = 	"ÔÚ¼Ä³öÈ¥ÐÅ¼þÖ®Ç°£¬ÎÒÏ£ÍûÄÜÔ¤ÀÀÎÒµÄÐÅ¼þ";
+$ubbt_lang['ORIG_MESS']  = 	"Ô­Ê¼ÎÄÕÂ";
+?>
