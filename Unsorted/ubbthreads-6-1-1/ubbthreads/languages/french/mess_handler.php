34d304361ea7a907a35d4b5090365d0b191d5e72
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:55 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/mess_handler.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/mess_handler.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['REPLY_HEAD']   =   "Utilisez ce formulaire afin d'envoyer un message privé à cette utilisateur";
+$ubbt_lang['MESS_COPY']  = 	"Je souhaite conserver une copie de mon message";
+$ubbt_lang['DELETE_MESS']  = 	"Effacer ce message";
+$ubbt_lang['REPLY_MESS']  = 	"Répondre à ce message";
+$ubbt_lang['MESS_RECEIPT']  = 	"Je souhaite recevoir un accusé de lecture";
+$ubbt_lang['MESS_PREVIEW']  = 	"Je souhaite voir un aperçu de mon message";
+$ubbt_lang['ORIG_MESS']  = 	"Message d'origine";
+?>
