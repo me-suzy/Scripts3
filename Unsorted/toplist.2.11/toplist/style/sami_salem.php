4bfcfd5bcb026954cc4c52ebbf25927121270bf8
me-suzy <me.suzana@gmail.com>
2025-08-14 02:05:25 +0300

    Add Unsorted/toplist.2.11/toplist/style/sami_salem.php

---
+++ Unsorted/toplist.2.11/toplist/style/sami_salem.php
@@ -0,0 +1,23 @@
+<?
+
+// Style style for Best Top List by Sami Salem - http://www.best-scripts.tk
+// Support: support@best-scripts.tk
+//
+// Copyright 2002 (c) Szymon Kosok
+
+echo "<style>
+
+body{background:#000066;color:#ffffff;font-family:verdana;font-size:11pt;}
+a{color:#999999;font-family:verdana;font-size:10pt;}
+a:visited{color:#999999;font-family:verdana;font-size:10pt;}
+a:hover{color:#0099FF;font-family:verdana;font-size:10pt;}
+td{font-family:verdana;font-size:11pt;}
+input{font-family:verdana;font-size:11pt;}
+textarea{font-family:verdana;font-size:11pt;}
+</style>";
+
+$tableheader = "#000000";
+$tablebody = "#0000FF";
+$tableheadfont = "#0033FF";
+$tableborder = "#ffffff";
+?> 
