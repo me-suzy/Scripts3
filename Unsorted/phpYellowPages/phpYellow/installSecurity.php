97d52ca59788b5f2549dc5590bf33615c99ebd8c
me-suzy <me.suzana@gmail.com>
2025-08-14 01:05:22 +0300

    Add Unsorted/phpYellowPages/phpYellow/installSecurity.php

---
+++ Unsorted/phpYellowPages/phpYellow/installSecurity.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+<?php include("util.php");?>
+
+	<script language='JavaScript' type='text/javascript'>
+	<!--
+	function showSecurityPage() {
+		var myWindow = open('', 'securityPage');
+		myWindow.location.href="docs/DOCSsecurity.html";				
+	}
+	function showCustomTips() {
+		var myWindow2 = open('', 'customPage');
+		myWindow2.location.href="docs/DOCScustomize.html";				
+	}
+	//-->
+	</script>
+
+
+<title>Install phpYellow Pages - Dreamriver.com - Fine Internet Applications Since 1995</title>
+</head>
+
+<body bgcolor="navy" text="white">
+
+
+<blockquote>
+
+
+<table border=10 width=600 align="center" bgcolor="gray" cellpadding=0 cellspacing=0><tr>
+<td bgcolor="yellow" align="center"><font color="blue">87%</font></td>
+<td bgcolor="black" width=75>&nbsp;</td>
+</tr></table>
+
+
+
+<h1>Install - Security</h1>
+
+
+<p>
+This step provides security information to help you safeguard your phpYellow Pages installation.  
+Click the button below to launch a security reminder page. Perform the security recommendations within 
+this page AFTER you have completed your phpYellow Pages installation.
+
+<form name="securityInfoForm">
+<input type="button" name="securityInfoButton" value="Launch a security reminder page" onClick="showSecurityPage()"> 
+<input type="button" name="customInfoButton" value="Launch tips on how to customize&nbsp;" onClick="showCustomTips()">
+</form>
+</p>
+
+
+
+
+
+		
+<div style="color:yellow;font-weight:bold;">
+<ul>
+<li>to proceed click &quot;Next Step&quot;</li>
+<li>to stop click &quot;Cancel&quot;</li>
+</ul>
+</div>
+
+
+
+<form name="installForm">
+<input type="button" name="nextStepButton" value="Next Step" onClick='location.href="installFinish.php"'>
+<input type="button" name="cancel" value="Cancel" onClick='location.href="installCancel.html"'>
+</form>
+
+
+</blockquote>
+
+
+</body>
+</html>
+
+
+	
