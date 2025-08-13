6301cae946aaea69b1c0dd641f39530affa40dda
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:29 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/dopoll.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/dopoll.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NO_VOTE']  = 	"Je hebt nergens op gestemd!";
+$ubbt_lang['POLL_REGED']  = 	"Alleen geregistreerde leden mogen stemmen.";
+$ubbt_lang['POLL_IP']  = 	"Dit IP adres is al gebruikt om te stemmen op deze enquête.";
+$ubbt_lang['POLL_USER']  = 	"Jij hebt al gestemd. Meermaals stemmmen is niet toegestaan.";
+?>
