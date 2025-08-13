fc20653e9100c083b3311fe363af500d2942d685
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:58 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "²¾°Ê³o½g¤å³¹¤Î¨ä¦^À³ªº¤å³¹";
+$ubbt_lang['DELETE_THREAD']  =  "§R°£¾ã­Ó¸ÜÃD";
+$ubbt_lang['POST_ICON'] =	"¤å³¹¹Ï¥Ü";
+$ubbt_lang['NO_EDIT']  = 	"§Ú­ÌµLªk½T»{±zªºÅv­­¡A±zµLªk½s¿è³o½g¤å³¹¡C";
+$ubbt_lang['EDITTIME']  = 	"¤w¶W¹L³Ìªø­×§ï´Á­­¡A³o½g¤å³¹¤£¯à°µ¥ô¦ó­×§ï";
+$ubbt_lang['PEDIT_HEAD']  = 	"½s¿è¤å³¹";
+$ubbt_lang['PEDIT_BODY']  = 	"±z¥i¥H¹ï³o½g¤å³¹°µ¥ô¦ó§ïÅÜ¡A±z¤]¥i¥H§R°£¥¦.  ¦pªG±z¿ï¾Ü§R°£³o½g¤å³¹¡A¦Ó¥ý«e¤w¸g¦³¤H¦^À³¤F¡A«h¨t²Î·|§â±z©Ò§R°£ªº³o½g¤å³¹¼ÐÃD§ï¬° \"This post has been deleted\". ";
+$ubbt_lang['EDIT_POLL']  = 	"³o½g¤å³¹¤º§t§ë²¼¡A±z¥u¯à§R°£¥¦¡A¤£¯à­×§ï¥¦¡C";
+$ubbt_lang['MARK_EDIT']  = 	"¼Ð¥Ü³Q­×§ï¹L¤F¶Ü ?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"§ïÅÜ³o½g¤å³¹";
+$ubbt_lang['PEDIT_DELETE']  = 	"§R°£³o½g¤å³¹";
+$ubbt_lang['PEDIT_APPROVE']  = 	"»{¥i³o½g¤å³¹";
+$ubbt_lang['RET_NORM']  = 	"¦^¨ì¤@¯ë¤é´Á";
+$ubbt_lang['MAKE_STICKY']  = 	"¨Ï³o½g¤å³¹«O¯d¦b³Ì¤W¤è";
+$ubbt_lang['MANAGE_THREAD']  = 	"ºÞ²z³o­Ó¸ÜÃD";
+$ubbt_lang['OPEN_THREAD']  = 	"¥´¶}³o­Ó¸ÜÃD";
+$ubbt_lang['CLOSE_THREAD']  = 	"Ãö³¬³o­Ó¸ÜÃD";
+$ubbt_lang['UNKEEP_THREAD']  = 	"¤£¦A«O¯d³o­Ó¸ÜÃD";
+$ubbt_lang['KEEP_THREAD']  = 	"«O¯d³o­Ó¸ÜÃD";
+$ubbt_lang['MOVE_THREAD']  = 	"²¾°Ê³o­Ó¸ÜÃD";
+?>
