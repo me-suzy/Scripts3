bdd958aff4d9a74924acb9ce066db68a75873a16
me-suzy <me.suzana@gmail.com>
2025-08-14 01:55:01 +0300

    Add Unsorted/Suggest MY Site v1.0/inc/turk.lang.inc.php

---
+++ Unsorted/Suggest MY Site v1.0/inc/turk.lang.inc.php
@@ -0,0 +1,30 @@
+<?
+$suggest_header="TAVSÝYE FORMU :";
+$suggest_exp="Sitemi tavsiye etmek için lütfen aþaðýdaki formu doldurun. Mesaj bölümü haricindeki tüm alanlarýn doldurulmasý zorunludur.";
+$yourname="Ýsminiz";
+$yourmail="E-mail adresiniz";
+$recipientname="Alýcýnýn ismi";
+$recipientmail="Alýcýnýn  e-mail adresi";
+$yourmessage="Mesajýnýz";
+$submitbutton="Tavsiye et";
+$siteownermessage="";
+$mailsubject="Myphpscripts.com sitesini tavsiye ediyorum";
+$thankyoumessage="Myphpscripts sitesini tavsiye ettiðiniz için teþekkür ederim.";
+$thankyousubject="Myphpscripts.com 'u tavsiye ettiðiniz  için teþekkürler !.";
+$author="Site Suggestion script, MYSuggest Version 1.0, by MERT YALDIZ\nGet yours for free at http://www.myphpscripts.com";
+
+/* variables for mail body */
+$suggestionfor="Sayýn";
+$from="Tavsiye eden :";
+$sendersmail="E-mail :";
+$wrote="sizin için aþaðýdaki özel mesajý iletti :";
+
+/* messages */
+$invalidmail="Aþaðýdaki email adresi geçersizdir :";
+$emptyfield="Lütfen aþaðýda belirtilen alanlarý doldurun !";
+$emptymail1="Lütfen aþaðýda belirtilen alanlarý doldurun !";
+$mailsent="Tavsiye mesajýnýz aþaðýdaki adrese gönderilmiþtir.";
+$mailproblem="Mesajýnýz gönderilemedi !  Lütfen tekrar deneyin.";
+$error="HATA !";
+$goback="Geri Dön";
+?>
