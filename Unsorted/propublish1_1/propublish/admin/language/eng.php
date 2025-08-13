ede87cfedc0e85fead9b3fd69e49c1f26cf1c988
me-suzy <me.suzana@gmail.com>
2025-08-14 01:17:04 +0300

    Add Unsorted/propublish1_1/propublish/admin/language/eng.php

---
+++ Unsorted/propublish1_1/propublish/admin/language/eng.php
@@ -0,0 +1,220 @@
+<?
+
+// menu_inc.php
+$la_art_list = "Articlelist";
+$la_new_art = "New article";
+$la_new_art_html = "New WYSIWYG article";
+$la_change_user = "Change user";
+$la_cat = "Categories";
+$la_level = "Levels";
+$la_setup = "Settings";
+
+// category.php
+$la_cat_saved = "Category is saved!";
+$la_editing = "Now editing";
+$la_deleted_cat = "Category is deleted";
+$la_catname = "Categoryname";
+
+
+
+// user.php
+$la_users = "Users";
+$la_user_saved = "User saved!";
+$la_now_editing = "Now editing";
+$la_this_author = "This author has";
+$la_this_author2 = "article(s). Do you wish to delete anyway ?";
+$la_deleted_user = "User is deleted";
+$la_notall = "You are not allowed to do this";
+$la_user_title = "Registration / Change";
+$la_firstname = "Firstname";
+$la_lastname = "Lastname";
+$la_about = "About";
+$la_accesslevel = "Accesslevel";
+$la_levels = "Levels";
+$la_stat_for = "Statistics for";
+$la_ant_art = "Num. art.";
+$la_read = "Read";
+$la_name = "Name";
+$la_change = "Change";
+
+// editor.php / editor_html.php
+$la_newseditor = "Newseditor";
+$la_back_news = "Back to newslist";
+$la_mand_error1 = "Mandatory fields missing!";
+$la_saved = "Article saved !";
+$la_edit_now = "Now editing";
+$la_newedit = "Registration / Change";
+$la_tagsapproved = "Characters allowed";
+$la_title = "Title";
+$la_intro = "Intro";
+$la_content = "Content";
+$la_status = "Status";
+$la_activated = "Activated";
+$la_deactivated = "Deactivated";
+$la_pic = "Pictures";
+$la_num_pic = "Num images attached to this article:";
+$la_pic_up = "Imageupload/edit";
+$la_rem_save = "You must save your article before img upload option";
+$la_save = "Save";
+$la_new_art_validate = "New article is up for validation";
+$la_new_art_validate2 = " Hi\nLog in to $set_url for approval of article. The article name is";
+$la_edit_sh = "Plain editor heigh";
+$la_edit_sw = "Plain editor width";
+$la_edit_wh = "WYSIWYG editor height";
+$la_edit_ww = "WYSIWYG editor width";
+
+
+// editor_html.php
+$la_size = "Size";
+$la_size1 = "Very small";
+$la_size2 = "Small";
+$la_size3 = "Medium";
+$la_size4 = "Large";
+$la_size5 = "Larger";
+$la_size6 = "Largest";
+$la_show_html = "Show HTML tags (translates bold to &lt;b&gt; in article)";
+
+
+
+// list.php
+$la_art = "Articlelist for";
+$la_num_art_page = "Num. articles per page";
+$la_art_approved = "Article is approved";
+$la_art_upg = "Article is upgraded to topnews";
+$la_art_upg_fp = "Article is upgraded to frontpagenews";
+$la_art_dwn_fp = "Article is downgraded from frontpagenews";
+$la_sure = "Are you sure you want to delete ?";
+$la_y = "Yes";
+$la_n = "No";
+$la_delete_conf = "Article is deleted!";  
+$la_article = "Article";
+$la_first_pub = "First pub";
+$la_cat = "Category";
+$la_list_feedb = "Grade"; 
+$la_list_change = "Change";
+$la_details = "Details";
+$la_del = "Delete";
+$la_fp = "FP";
+$la_nfp = "!FP";
+$la_approve = "Approve";
+$la_prev = "Prev";
+$la_next = "Next";
+$la_stats_header = "Statistics for";
+$la_author = "Author";
+$la_art_shown = "Article shown (totalt)";
+$la_art_grade = "Grade";
+$la_art_voters = "Num votes";
+$la_art_created = "Created";
+$la_art_changed = "Last changed";
+$la_list_opt = "Also show";
+$la_date = "Date";
+$la_stat = "Stat";
+$la_respage = "Choose resultpage";
+$la_perpage = "per page";
+
+// auth.php
+$la_member_funk_info = "Memberfunction";
+$la_member_funk_1 = "This service demands a higher clearance than you are assigned. You have level";
+$la_member_funk2 = "but demanded level at this page is";
+$la_login_as = "You are logged in as";
+$la_withlevel = "with level";
+$la_demands_login = "This service demands login. Please login again, and come back.";
+
+
+// art.php
+$la_by = "by";
+$la_changed = "changed";
+$la_news = "News";
+$la_print = "Print";
+$la_rate_this = "Grade this article";
+$la_tellafriend = "Tell-a-friend";
+
+// search.php
+$la_search_result = "SEARCHRESULT";
+$la_search_found1 = "Search resulted in";
+$la_search_found2 = "hits in your searh for";
+$la_search_header = "<h3>SEARCH</h3><br>Searh on all articles.";
+$la_search_now = "Search now!";
+
+// feedback.php 
+$la_thanks = "Thanks for votes and comments on how to make this article better!";
+$la_email_admin_subj = "New grade";
+$la_voted = "As follow";
+$la_article = "Article:";
+$la_grade = "Grade:";
+$la_comment = "Comment:"; 
+$la_email = "Email"; 
+$la_feedback_info = "By giving votes and/or comments, you give us valuable feedback so that we can make this website even better! Note: All fields is optionable.";
+$la_bad = "Bad";
+$la_perfect = "Perfect";
+$la_feedbak = "Feedback";
+
+// tellafriend.php
+$la_tellafriend = "Tell a friend";
+$la_tellafriend_sent = "Mail is sent to";
+$la_tellafriend_subj = "Tell a friend about this article";
+$la_tellafriend_msg = "wish to tell you about this article";
+$la_sent = "Sent from:";
+$la_tellafriend_comment = "Below is comment from sender:";
+$la_tellafriend_error1 = "Error, sender or recievers emailaddress is not filled in.";
+$la_tellafriend_intro = "Her you can tell a friend about this article. Type reciever, sender and any comments";
+$la_reciever_mail = "Reciever - email.";
+$la_sender_mail = "Sender - email.";
+$la_close = "Close";
+
+// func.php
+$la_read_more = "Read more";
+
+// login.php
+$la_login_wel = "Welcome";
+$la_login_ready = "You are now logged in, and can post/edit articles";
+$la_newslist = "Newslist";
+$la_error = "Errormsg";
+$la_error_2 = "A person with your emailaddress and password could not be found.";
+$la_passwd = "Password";
+$la_login = "Login";
+$la_login_logout1 = "Logged out!";
+$la_login_logout2 = "You are logged out.";
+$la_logout = "Log out";
+
+// level.php
+$la_level_saved = "Level is saved!";
+$la_deleted_level = "Level is deleted";
+$la_levelname = "Levelname";
+$la_level = "Level";
+
+// img.php
+$la_picupload = "Pictureupload";
+$la_return = "Return to newseditor";
+$la_pic_success1 = "Picture is now uploaded. Use code";
+$la_pic_success2 = "where picture shal appear in article.";
+$la_upload_error = "Picture is larger than allowed.";
+$la_img_already = "Pictures already uploaded to this article:";
+$la_del_img = "Following picture is now deleted:";
+$la_img = "Picture";
+
+// setup.php
+$la_config = "Settings";
+$la_change_config = "Here you can change settings for the system.";
+$la_saved_conf = "Settings saved!";
+$la_language = "Language";
+$la_email_admin = "Webmaster email";
+$la_email_admin1 = "Emailaddress where validtion mail etc should be sent.";
+$la_url1 = "Url to file art.php. Do not include art.php, only http://www.domain.com/news for instance.";
+$la_html_editor = "WYSIWYG editor";
+$la_html_editor1 = "Should be checked if you allow use of wysiwyg editor.";
+$la_norm_editor = "Normal editor";
+$la_norm_editor1 = "Should be checked if you allow use of normal editor.";
+$la_news_frontpage = "Num news on frontpage";
+$la_news_frontpage1 = "Listed on default style.";
+$la_inform = "Inform webmaster";
+$la_inform1 = "If you want notification of arrival of new articles, check this.";
+$la_validate = "Autovalidation?";
+$la_validate1 = "With this option, all articles will be published at once.";
+
+//
+$la_help = "Help";
+$la_new_user = "New user";
+
+
+?> 
