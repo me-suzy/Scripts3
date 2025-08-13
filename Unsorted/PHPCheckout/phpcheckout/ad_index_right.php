489df6aaf53ee9719b58caa40a846b9660b981cb
me-suzy <me.suzana@gmail.com>
2025-08-14 00:30:52 +0300

    Add Unsorted/PHPCheckout/phpcheckout/ad_index_right.php

---
+++ Unsorted/PHPCheckout/phpcheckout/ad_index_right.php
@@ -0,0 +1,45 @@
+<?php if(ORGANIZATION == "DreamRiver"):?>
+					<table>
+					<tr>
+						<td colspan=2 align="center">
+							<div class="required">Profit $$$ from your digital items with phpcheckout<sup>TM</sup></div>
+						</td>
+					</tr>
+					<tr>
+						<td valign="center" class="note">					
+							<ul>
+								<li>FREE download
+								<li>easy install
+								<li>digital only
+							</ul>
+						</td>
+						<td valign="center">
+							<img src="featureproduct/cashRegister.gif" width="85" height="102" border=0 alt="Put $$$ in your cash register with phpcheckout">
+						</td>
+					</tr>
+					<tr>
+						<td colspan=2 align="center" class="note">
+							<?php //<a href="showcase.php?productnumber=5">Pro Edition |</a> <a href="">Free Edition |</a> <a href="startBuy.php">Buy</a><br-->  ?>
+							<a href="showcase.php?productnumber=5">| more info |</a>
+
+							<br><br>PICK A THEME!
+							<br><a href="themeGray.php">Gray Theme |</a> <a href="themeBright.php">Bright Theme |</a> <a href="themeMuted.php">Muted Theme</a>
+							<br><br><div align="center" class="note">phpcheckout BETA testers wanted - <a href="email.php">contact webmaster for copy</a></div>
+						</td>
+					</tr>
+					</table>
+<?php else:?>
+	<div align="left" class="required">
+		To edit this ad:
+		<ol>
+			<li>in an html editor open ad_index_right.php 
+			<li>modify this file
+			<li>save this file to web server
+		</ol>
+	</div>
+<?php endif;?>
+					
+					
+
+
+
