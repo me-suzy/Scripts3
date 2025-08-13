63f66fda90da36f72c19100f98d905427ed000dc
me-suzy <me.suzana@gmail.com>
2025-08-14 00:01:53 +0300

    Add Unsorted/phpaffiliate/user/admin/logout.php

---
+++ Unsorted/phpaffiliate/user/admin/logout.php
@@ -0,0 +1,35 @@
+<?
+  session_start();
+
+  $old_user = $valid_admin;  // store  to test if they *were* logged in
+  $result = session_unregister("valid_admin");
+  session_destroy();
+   
+  include "../header.290"; 
+  include "../../config.php"; 
+   
+   
+  if (!empty($old_user))
+  {
+    if ($result)
+    { 
+      // if they were logged in and are not logged out 
+      echo "Logged out.<br>";
+    }
+    else
+    {
+     // they were logged in and could not be logged out
+      echo "Could not log you out.<br>";
+    } 
+  }
+  else
+  {
+    // if they weren't logged in but came to this page somehow
+    echo "You were not logged in, and so have not been logged out.<br>"; 
+  }
+?> 
+<a href="index.php">Back to main page</a>
+
+<?    include "../footer.290"; ?> 
+</body>
+</html>
