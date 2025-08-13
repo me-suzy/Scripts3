bcd1b7f6ce4dc017fd82ab44a8791de55eb5e273
me-suzy <me.suzana@gmail.com>
2025-08-14 02:05:24 +0300

    Add Unsorted/toplist.2.11/toplist/style/default.php

---
+++ Unsorted/toplist.2.11/toplist/style/default.php
@@ -0,0 +1,24 @@
+<?
+
+// Default style for Best Top List by Szymon Kosok - http://www.best-scripts.tk
+// Support: support@best-scripts.tk
+//
+// Copyright 2002 (c) Szymon Kosok
+
+echo "<style>
+
+body{background:#FFFFFF;color:black;font-family:verdana;font-size:8pt;}
+a{color:blue;font-family:verdana;font-size:8pt;}
+a:visited{color:darkblue;font-family:verdana;font-size:8pt;}
+a:hover{color:darkblue;font-family:verdana;font-size:8pt;}
+td{font-family:verdana;font-size:8pt;}
+input{font-family:verdana;font-size:8pt;}
+textarea{font-family:verdana;font-size:8pt;}
+</style>";
+
+$tableheader = "#0033CC";
+$tablebody = "#F0F0F0";
+$tableheadfont = "white";
+$tableborder = "white";
+?>
+
