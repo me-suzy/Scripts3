5cc46f5a2a977a36ac6caf1b931e71822a86bcfb
me-suzy <me.suzana@gmail.com>
2025-08-14 00:33:39 +0300

    Add Unsorted/PHPCheckout/phpcheckout/position.php

---
+++ Unsorted/PHPCheckout/phpcheckout/position.php
@@ -0,0 +1,10 @@
+<?php echo"\n\n\n";?>
+<!-- START OF position.php -->				
+<SELECT NAME="position" SIZE=1>
+	<OPTION VALUE="<?php echo $position;?>" SELECTED><?php echo $position;?></OPTION>
+	<!-- edit any option below this line - delete, add or change -->							
+	<OPTION VALUE="1">First Page</OPTION>
+	<OPTION VALUE="2">Preferred</OPTION>
+	<OPTION VALUE="3">Basic</OPTION>
+</SELECT>
+<!-- END OF position.php -->	
