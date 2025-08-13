0a83bf02e6bb99980832d5a652f4330c6df3bb7d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:29:19 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/donotifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/donotifymod.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['POST_PROB']  =  "²éÑ¯×ÊÁÏ¿âÊ±·¢ÉúÎÊÌâ£¬ÕÒ²»µ½ÕâÆªÎÄÕÂ¡£";
+$ubbt_lang['BAD_GROUP'] =	"¾Ü¾ø´æÈ¡£¡";
+$ubbt_lang['NO_B_INFO'] =	"ÕÒ²»µ½ÌÖÂÛÇø£¡";
+$ubbt_lang['NOTIF_HEAD']  = 	"Í¨ÖªÄúÓ¦¸Ã¿´Ò»¿´ÒÔÏÂµÄÎÄÕÂ£¬±êÌâÊÇ£º ";
+$ubbt_lang['TO_READ'] =	"¿ÉÒÔÔÚÕâÀï¼ìÊÓÕâÆªÎÄÕÂ£º";
+$ubbt_lang['NOTIFY_SENT']  = 	"ÒÑ¾­Í¨Öª°åÖ÷ÁË";
+$ubbt_lang['NOTIFY_CONFIRM'] = "ÒÑ¾­Í¨Öª±¾°å°åÖ÷ÁË¡£";
+?>
