69d01645b8644ff679407af6e376706f95fd65e9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:15 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/newpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/newpost.php
@@ -0,0 +1,17 @@
+<?
+$ubbt_lang['CHOOSE_NAME']  = 	"Anonyma inlägg är tillåtna, så du kan välja valfritt oregistrerat namn till ditt inlägg.";
+$ubbt_lang['READ_PERM']  = 	"Du har endast läsrättigheter i detta forum.";
+$ubbt_lang['MAKENEW_HEAD']  = 	"Skriv ett nytt inlägg - Forum";
+$ubbt_lang['FILL_FORM']  = 	"Fyll i fältet nedan för att skriva ett inlägg på forumet.";
+$ubbt_lang['NO_HTML']  = 	"Html är avstängt.";
+$ubbt_lang['YES_HTML']  = 	"Html är aktiverat.";
+$ubbt_lang['NO_MARKUP']  = 	"UBBCode är avstängt.";
+$ubbt_lang['YES_MARKUP']  = 	"UBBCode är aktiverat, så du kan använda";
+$ubbt_lang['MARK_POSTS']  = 	"UBBCode i ditt inlägg";
+$ubbt_lang['POST_ICON']  = 	"Inläggsikon";
+$ubbt_lang['POST_TEXT']  = 	"Skriv inlägg";
+$ubbt_lang['SPELL_CHECK']  = 	"Kontrollera stavning";
+$ubbt_lang['DO_EMAIL']  = 	"Skicka alla svar till min riktiga e-postadress";
+$ubbt_lang['DO_PREVIEW']  = 	"Jag vill förhandsgranska mitt inlägg";
+$ubbt_lang['CAN_ATTACH']  = 	"och/eller bifoga en fil";
+?>
