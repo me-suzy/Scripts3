b52c4926ea23864598d5808c9ab68398ab346917
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:27 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/sendmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/sendmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['CHOOSE_ADD']  = 	"½Ð±q³q°T¿ý¤¤¿ï¾Ü¤@­Ó¦W¦r";
+$ubbt_lang['TEXT_DELETE']  = 	"§R°£";
+$ubbt_lang['NO_PRIVATE']  = 	"©êºp ¡A³o¦ì¨Ï¥ÎªÌ¤£±µ¨ü¨p¤H«H¥ó¡C";
+$ubbt_lang['NO_RECORD']  = 	"©êºp¡A¨t²Î§ä¤£¨ì±z«H¥óªº¦¬¥ó¤Hªº¬ÛÃö¸ê®Æ¡C";
+$ubbt_lang['MESS_PREV'] =	"¹wÄý«H¥ó¡G";
+$ubbt_lang['PREVIEW_MESS'] =	"¥H¤U«h¬O±z±N­n±H¥Xªº«H¥ó¡A¦pªG¨S¦³°ÝÃD¡A½Ð«ö¡uÄ~Äò¡v¡A¨t²Î´N·|§â«H±H¥X¡C¦pªGÁÙ·Q­×§ï¡A½Ð¨ì©³¤U¥h°µ­×§ï¡C";
+$ubbt_lang['EDIT_MORE']  = 	"­×§ï¤º®e";
+$ubbt_lang['PRIV_SUB']  = 	"±z¦³·sªº«H¥ó";
+$ubbt_lang['PRIV_BODY_R'] =	"±z¦b $config['title'] °Q½×°Ï¤º¦³¦ì¨Ï¥ÎªÌ±H¤F¤@«Ê¨p¤H«Hµ¹±z¡A±z¥i¥H«e©¹ $config['phpurl']/login.php ¨Ó¾\\Åª¡C";
+$ubbt_lang['MESS_SENT']  = 	"±zªº¤å³¹¤w¸g³Q°e¥X¤F¡C";
+?>
