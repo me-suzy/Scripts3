134e7a3f8ff17f1b2ae396ba88f64aa9174acb63
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:12 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/mess_reply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/mess_reply.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['EDIT_MORE']  = 	"­×§ï¤º®e";
+$ubbt_lang['REPLY_PREV'] =	"¹wÄýµ¹¦¹¤Hªº«H¥ó¡G";
+$ubbt_lang['PREVIEW_MESS'] =	"¥H¤U«h¬O±z±N­n±H¥Xªº«H¥ó¡A¦pªG¨S¦³°ÝÃD¡A½Ð«ö'Ä~Äò'¡A¨t²Î´N·|§â«H±H¥X¡A¦pªGÁÙ·Q­×§ï¡A½Ð¨ì©³¤U¥h°µ­×§ï¡C";
+$ubbt_lang['MESS_PREVIEW']  = 	"¦b±H¥X¥h«H¥ó¤§«e¡A§Ú§Æ±æ¯à¹wÄý§Úªº«H¥ó";
+$ubbt_lang['PRIV_SUB']  = 	"±z¦³·sªº«H¥ó";
+$ubbt_lang['PRIV_BODY_R'] =	"±z¦b $config['title'] ¦³¦ì¨Ï¥ÎªÌ±H¤F¤@«Ê¨p¤H«Hµ¹±z ¡A ±z¥i¥H«e©¹ $config['phpurl']/login.php ¨Ó¾\\Åª¡C";
+?>
