725d27d4ab61843723259115bcdcce149dc96783
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:07 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"Valoarea pentru post-uri pe pagina nu poate fi folosita.";
+$ubbt_lang['FLAT_BAD']  = 	"Numarul de mesaje pe pagina in mod flat trebuie sa fie intre 1 si 99.";
+$ubbt_lang['TEXTAREA_NO']  = 	"Valoarea pentru zona de text nu poate fi folosita.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Optiunile legate de afisare au fost modificate.";
+?>
