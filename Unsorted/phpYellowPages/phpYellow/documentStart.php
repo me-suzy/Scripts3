b68a5e0ee5675385f0b06c1bebe9205c7e744eea
me-suzy <me.suzana@gmail.com>
2025-08-14 01:04:49 +0300

    Add Unsorted/phpYellowPages/phpYellow/documentStart.php

---
+++ Unsorted/phpYellowPages/phpYellow/documentStart.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+
+	<script language="Javascript" src="yellow.js"></script>
+	<script language="Javascript">loadCSSFile();</script>
+
+
+    <?php echo "<TITLE>phpYellow - " . YOURPHPYELLOWNAME . " - " . COMPANY . "</TITLE>";?>
+
+	<META NAME="keywords" CONTENT="yellow, pages, phpYellow, php, Yellow, Make, your, website, happen, with, phpYellow, and, Dreamriver.com">
+	<META NAME="description" CONTENT="Make your website happen - create interactive dynamic and custom directories - with phpYellow and Dreamriver.com">
+	<META NAME="Author" CONTENT="Richard Creech, DreamRiver: Web: http://www.dreamriver.com Email: richardc@dreamriver.com">
+	<META NAME="GENERATOR" CONTENT="Notepad">
+
+	</HEAD>
+
+<BODY>
+
+<?php 
+include("header.php");
+echo "<h1>" . YOURPHPYELLOWNAME . "<br>$goal</h1>";
+?>
