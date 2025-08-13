7f8ad86e7cccc1891036f531e4a575db855b6c3e
me-suzy <me.suzana@gmail.com>
2025-08-14 00:29:40 +0300

    Add Unsorted/PHPCart.v3.1.Nullified/PHPCart31/admin/configuration_1.php

---
+++ Unsorted/PHPCart.v3.1.Nullified/PHPCart31/admin/configuration_1.php
@@ -0,0 +1,76 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+// Program Name         : PHPCart                                          //
+// Release Version      : 3.1                                              //
+// Program Author       : Mr B Wiencierz                                   //
+// Home Page            : http://www.phpcart.co.uk                         //
+// Supplied by          : CyKuH [WTN]                                      //
+// Nullified by         : CyKuH [WTN]                                      //
+// Distribution         : via WebForum, ForumRU and associated file dumps  //
+//                                                                         //
+//                All code is ©2000-2004 Barry Wiencierz.                  //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
+// YOU MAY EDIT THESE VARIABLES
+
+// GMT Offset Time zone
+$zone = "+3";
+
+// Company Name
+$companyName = "Cart";
+
+// Homepage URL
+$home = "index.html";
+
+// Sales tax / VAT (in percentage)
+$salesVAT = "";
+
+// Postage Amount
+$PostalAmount = "2.00";
+
+// Company Phone  -  This will be placed at the 
+// 		     bottom of the email confirmation
+//		     that the customer receives.  -  Leave blank if not wanted
+$companyPhone =  "00000000000";
+
+// Company Sales Email - Address orders will be sent to.
+$salesEmail = "test@yourdomain.com";
+
+// Domains that can access your Cart - Separate multiple domains with one space
+$referers = "";
+
+// Background Color - Header Row
+$headerColor = "#6878A7";
+
+// Background Color - Product Rows
+$rowsColor = "#EEEEEE";
+
+// Title Color
+$titleColor = "#FFFFFF";
+
+// Text Colour - Black works best but test it out
+$TextColor = "#000000";
+
+// Page Background
+$PgBack = "#FFFFFF";
+
+// Font (preferred order, comma-separated)
+$font = "Verdana";
+
+// Font size
+$fontSize = 1;
+
+// Language (English included in this distribution)
+// See the PHPCart homepage for user-contributed language files.
+$language = "english";
+
+// Select which currency you would like to deal in
+$currency = "£";
+
+// Place a copy of each order into a file
+$enableCopy = "Activate";
+
+// Display notes section on order
+$notes_active = "Yes";
+?>
