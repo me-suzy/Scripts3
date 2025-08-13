a0a929d1d805bb78a92dadeee5d424232051f3c9
me-suzy <me.suzana@gmail.com>
2025-08-14 00:32:40 +0300

    Add Unsorted/PHPCheckout/phpcheckout/loginChooseGoal.php

---
+++ Unsorted/PHPCheckout/phpcheckout/loginChooseGoal.php
@@ -0,0 +1,130 @@
+<!-- start of loginChooseGoal.php -->
+<table class="favcolor2">
+<tr>
+	<th>
+		Task Panel
+	</th>
+<tr>
+	<td>
+		<form name="myForm" method="post" action="loginResult.php">
+		<input class="submit" type="submit" value="Help">
+		<input type="hidden" name="task" value="Help">
+		<input type="hidden" name="email" value="<?php echo $email;?>">
+		<input type="hidden" name="customerid" value="<?php echo $customerid;?>">
+		<input type="hidden" name="password" value="<?php echo $password;?>">
+		<input type="hidden" name="organization" value="<?php echo $organization;?>">
+		<input type="hidden" name="firstname" value="<?php echo $firstname;?>">
+		<input type="hidden" name="lastname" value="<?php echo $lastname;?>">
+		<input type="hidden" name="address" value="<?php echo $address;?>">		
+		<input type="hidden" name="city" value="<?php echo $city;?>">		
+		<input type="hidden" name="stateprov" value="<?php echo $stateprov;?>">
+		<input type="hidden" name="country" value="<?php echo $country;?>">		
+		<input type="hidden" name="postalcode" value="<?php echo $postalcode;?>">
+		<input type="hidden" name="areacode" value="<?php echo $areacode;?>">
+		<input type="hidden" name="phone" value="<?php echo $phone;?>">
+		<input type="hidden" name="fax" value="<?php echo $fax;?>">
+		<input type="hidden" name="cellphone" value="<?php echo $cellphone;?>">		
+		<input type="hidden" name="website" value="<?php echo $website;?>">		
+		<input type="hidden" name="customersince" value="<?php echo $customersince;?>">
+		<input type="hidden" name="visits" value="<?php echo $visits;?>">		
+		<input type="hidden" name="role" value="<?php echo $role;?>">
+		<input type="hidden" name="privacy" value="<?php echo $privacy;?>">
+		<input type="hidden" name="news" value="<?php echo $news;?>">		
+		<input type="hidden" name="lastupdate" value="<?php echo $lastupdate;?>">
+		</form>
+	</td>
+</tr>
+</tr>
+<tr>
+	<td>
+		<form name="myForm" method="post" action="loginResult.php">
+		<input class="submit" type="submit" value="Retrieve Contact Info">
+		<input type="hidden" name="task" value="Retrieve Contact Info">
+		<input type="hidden" name="email" value="<?php echo $email;?>">
+		<input type="hidden" name="customerid" value="<?php echo $customerid;?>">
+		<input type="hidden" name="password" value="<?php echo $password;?>">
+		<input type="hidden" name="organization" value="<?php echo $organization;?>">
+		<input type="hidden" name="firstname" value="<?php echo $firstname;?>">
+		<input type="hidden" name="lastname" value="<?php echo $lastname;?>">
+		<input type="hidden" name="address" value="<?php echo $address;?>">		
+		<input type="hidden" name="city" value="<?php echo $city;?>">		
+		<input type="hidden" name="stateprov" value="<?php echo $stateprov;?>">
+		<input type="hidden" name="country" value="<?php echo $country;?>">		
+		<input type="hidden" name="postalcode" value="<?php echo $postalcode;?>">
+		<input type="hidden" name="areacode" value="<?php echo $areacode;?>">
+		<input type="hidden" name="phone" value="<?php echo $phone;?>">
+		<input type="hidden" name="fax" value="<?php echo $fax;?>">
+		<input type="hidden" name="cellphone" value="<?php echo $cellphone;?>">		
+		<input type="hidden" name="website" value="<?php echo $website;?>">		
+		<input type="hidden" name="customersince" value="<?php echo $customersince;?>">
+		<input type="hidden" name="visits" value="<?php echo $visits;?>">		
+		<input type="hidden" name="role" value="<?php echo $role;?>">
+		<input type="hidden" name="privacy" value="<?php echo $privacy;?>">
+		<input type="hidden" name="news" value="<?php echo $news;?>">		
+		<input type="hidden" name="lastupdate" value="<?php echo $lastupdate;?>">
+		</form>
+	</td>
+</tr>
+<tr>
+	<td>
+		<form name="myForm" method="post" action="loginResult.php">
+		<input class="submit" type="submit" value="Send Feedback">
+		<input type="hidden" name="task" value="Send Feedback">
+		<input type="hidden" name="email" value="<?php echo $email;?>">
+		<input type="hidden" name="customerid" value="<?php echo $customerid;?>">
+		<input type="hidden" name="password" value="<?php echo $password;?>">
+		<input type="hidden" name="organization" value="<?php echo $organization;?>">
+		<input type="hidden" name="firstname" value="<?php echo $firstname;?>">
+		<input type="hidden" name="lastname" value="<?php echo $lastname;?>">
+		<input type="hidden" name="address" value="<?php echo $address;?>">		
+		<input type="hidden" name="city" value="<?php echo $city;?>">		
+		<input type="hidden" name="stateprov" value="<?php echo $stateprov;?>">
+		<input type="hidden" name="country" value="<?php echo $country;?>">		
+		<input type="hidden" name="postalcode" value="<?php echo $postalcode;?>">
+		<input type="hidden" name="areacode" value="<?php echo $areacode;?>">
+		<input type="hidden" name="phone" value="<?php echo $phone;?>">
+		<input type="hidden" name="fax" value="<?php echo $fax;?>">
+		<input type="hidden" name="cellphone" value="<?php echo $cellphone;?>">		
+		<input type="hidden" name="website" value="<?php echo $website;?>">		
+		<input type="hidden" name="customersince" value="<?php echo $customersince;?>">
+		<input type="hidden" name="visits" value="<?php echo $visits;?>">		
+		<input type="hidden" name="role" value="<?php echo $role;?>">
+		<input type="hidden" name="privacy" value="<?php echo $privacy;?>">
+		<input type="hidden" name="news" value="<?php echo $news;?>">		
+		<input type="hidden" name="lastupdate" value="<?php echo $lastupdate;?>">
+		</form>
+	</td>
+</tr>
+<tr>
+	<td>
+		<form name="myForm" method="post" action="loginResult.php">
+		<input class="submit" type="submit" value="View My Licences">
+		<input type="hidden" name="task" value="View My Licences">
+		<input type="hidden" name="email" value="<?php echo $email;?>">
+		<input type="hidden" name="customerid" value="<?php echo $customerid;?>">
+		<input type="hidden" name="password" value="<?php echo $password;?>">
+		<input type="hidden" name="organization" value="<?php echo $organization;?>">
+		<input type="hidden" name="firstname" value="<?php echo $firstname;?>">
+		<input type="hidden" name="lastname" value="<?php echo $lastname;?>">
+		<input type="hidden" name="address" value="<?php echo $address;?>">		
+		<input type="hidden" name="city" value="<?php echo $city;?>">		
+		<input type="hidden" name="stateprov" value="<?php echo $stateprov;?>">
+		<input type="hidden" name="country" value="<?php echo $country;?>">		
+		<input type="hidden" name="postalcode" value="<?php echo $postalcode;?>">
+		<input type="hidden" name="areacode" value="<?php echo $areacode;?>">
+		<input type="hidden" name="phone" value="<?php echo $phone;?>">
+		<input type="hidden" name="fax" value="<?php echo $fax;?>">
+		<input type="hidden" name="cellphone" value="<?php echo $cellphone;?>">		
+		<input type="hidden" name="website" value="<?php echo $website;?>">		
+		<input type="hidden" name="customersince" value="<?php echo $customersince;?>">
+		<input type="hidden" name="visits" value="<?php echo $visits;?>">		
+		<input type="hidden" name="role" value="<?php echo $role;?>">
+		<input type="hidden" name="privacy" value="<?php echo $privacy;?>">
+		<input type="hidden" name="news" value="<?php echo $news;?>">		
+		<input type="hidden" name="lastupdate" value="<?php echo $lastupdate;?>">
+		</form>
+	</td>
+</tr>
+</table>
+<!-- end of loginChooseGoal.php -->
+
