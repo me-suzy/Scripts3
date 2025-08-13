fcec12093d5d8dc29d1915c3c942402b4336f99f
me-suzy <me.suzana@gmail.com>
2025-08-14 00:16:02 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/fontcolor.inc.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/fontcolor.inc.php
@@ -0,0 +1,43 @@
+<?#//v.1.0.0
+
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+
+
+	/*
+		NOTE: change the following array as you wish but
+		be sure to always have a 1 chars long index.
+		Example:
+			"a" => "#333333"  <--- This is valid 
+			"ab" => "#333333" <-- This is NOT valid
+	*/
+	$FONTCOLOR = array("a" => "#000000",
+				       "b" => "#666666",
+				       "c" => "#EEEEEE",
+				       "d" => "#FFFFFF",
+				       "e" => "#CC0000",
+				       "f" => "#FF0000",
+				       "g" => "#FF9999",
+				       "h" => "#FFDDDD",
+				       "i" => "#000099",
+				       "l" => "#0066FF",
+				       "m" => "#6699FF",
+				       "n" => "#D2E9FF",
+				       "o" => "#006633",
+				       "p" => "#009933",
+				       "q" => "#99CC99",
+				       "r" => "#FFB03C",
+				       "s" => "#CC6666",
+				       "t" => "#9933CC",
+				       "u" => "#FFCCFF",
+				       "v" => "#99CCCC",
+				       "z" => "#EEE8DB",
+				       "x" => "#CC9966",
+				       "w" => "#D6C9A3",
+				       "y" => "#A6B3A6"
+				  );
+?>
