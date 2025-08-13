6b4299209b97e13db25fc080925467be83d0401e
me-suzy <me.suzana@gmail.com>
2025-08-14 00:34:10 +0300

    Add Unsorted/PHPCheckout/phpcheckout/showcaseEnd.php

---
+++ Unsorted/PHPCheckout/phpcheckout/showcaseEnd.php
@@ -0,0 +1,21 @@
+<!-- showcaseEnd.php -->
+<br>
+		<h1>Showcase</h1>
+
+<p>You have reached the end of the Showcase.</p>
+
+<form name="goToShowcaseIndexForm" method="post" action="showcaseIndex.php">
+
+<input type="submit" name="submit" class="submit" value="Go to Showcase Start">
+</form>
+
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+<p><br></p>
+
+<!-- end of file called showcaseEnd.php -->
