6d507721beaabc37d49ba68a59a424b55636eded
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:30 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/mess_reply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/mess_reply.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['EDIT_MORE']  = 	"Nachricht ver&auml;ndern";
+$ubbt_lang['REPLY_PREV']  = 	"Voransicht der Antwort an";
+$ubbt_lang['PREVIEW_MESS']  = 	"Dies ist eine Voransicht Ihrer Nachricht. Wenn alles korrekt aussieht, klicken Sie auf 'Weiter', um sie endg&uuml;ltig abzuschicken. Ansonsten k&ouml;nnen unten noch &Auml;nderungen vorgenommen werden.";
+$ubbt_lang['MESS_PREVIEW']  = 	"Ich m&ouml;chte eine Voransicht meiner Nachricht";
+$ubbt_lang['PRIV_SUB']  = 	"Sie haben neue Nachrichten";
+$ubbt_lang['PRIV_BODY_R']  = 	"Sie haben eine private Nachricht über {$config['title']} erhalten.  Sie können direkt zu {$config['phpurl']}/login.php gehen, um sie zu lesen.";
+?>
