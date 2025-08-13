f4a275ea204db5b5ddddd412912eabd429343ca2
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:00 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/sendprivate.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/sendprivate.php
@@ -0,0 +1,13 @@
+<?
+$ubbt_lang['NOPRIVS']   = "Private messages are disabled.";
+$ubbt_lang['MESS_COPY']  = 	"Vreau sa pastrez o copie a acestui mesaj";
+$ubbt_lang['MESS_RECEIPT']  = 	"Vreau sa primesc o confirmare de citire";
+$ubbt_lang['MESS_PREVIEW']  = 	"Vreau sa previzualizez measajul";
+$ubbt_lang['ADD_BOOK']  = 	"Adauga in agenda";
+$ubbt_lang['USE_BOOK']  = 	"sau foloseste numele din agenda";
+$ubbt_lang['CHOOSE_ADD']  = 	"Alege un nume";
+$ubbt_lang['NO_LOGGED']  = 	"Nu puteti trimite un mesaj privat altui utilizator inainte sa va conectati cu numele/parola dvs.";
+$ubbt_lang['NO_PRIVATE']  = 	"Acest utilizator nu accepta mesaje private.";
+$ubbt_lang['PRIV_BODY2']  = 	"Completati datele urmatoare pentru a trimite un mesaj privat acestui utilizator. Acesta il va primi la urmatoarea conectare.";
+$ubbt_lang['TEXT_DELETE']  = 	"Sterge";
+?>
