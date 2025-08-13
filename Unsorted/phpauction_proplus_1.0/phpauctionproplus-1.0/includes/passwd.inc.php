0b7b5d909c644bf3d245b8dd8a561656f902e6c6
me-suzy <me.suzana@gmail.com>
2025-08-14 00:16:28 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/passwd.inc.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/passwd.inc.php
@@ -0,0 +1,18 @@
+<?#//v.1.0.0
+  
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+
+
+//--Set the variables below according to your system configuration
+
+$DbHost     = "localhost"; // The host where the MySQL server resides
+$DbDatabase = "hrnb2b_plus"; // The database you are going to use
+$DbUser     = "hrnb2b"; // Username
+$DbPassword = "lolisfun"; // Password
+
+?>
