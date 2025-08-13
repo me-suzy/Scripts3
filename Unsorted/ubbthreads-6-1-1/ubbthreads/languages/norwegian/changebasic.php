6620b13e36752cd4a908902fe2f0580f3b152cb6
me-suzy <me.suzana@gmail.com>
2025-08-14 02:41:30 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/changebasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/changebasic.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['PIXEL_TOO_BIG']="Your picture exceeds the ({$theme['PictureWidth']}x{$theme['PictureHeight']}) pixel limitation width a width/height of ";
+$ubbt_lang['FILE_TOO_BIG']="Pictures can be no larger than {$config['avatarsize']} bytes.";
+$ubbt_lang['BAD_PIC']  =       "Feil bildetype.";
+$ubbt_lang['PASS_MATCH']  = 	"Passordene du oppga er ikke like.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Passordet skal være på mellom 4 og 20 tegn.";
+$ubbt_lang['HOME_TOO_LONG']  = 	"'Hjemmeside'-oppføringen er for lang.";
+$ubbt_lang['OCC_TOO_LONG']  = 	"'Yrkes'-oppføringen er for lang.";
+$ubbt_lang['HOBB_TOO_LONG']  = 	"'Fritidsinteresser'-oppføringen er for lang.";
+$ubbt_lang['LOC_TOO_LONG']  = 	"'Bosted'-oppføringen er for lang.";
+$ubbt_lang['SIG_TOO_LONG']  = 	"Signaturen er for lang.";
+$ubbt_lang['BIO_TOO_LONG']  = 	"'Om deg selv'-oppføringen er for lang.";
+$ubbt_lang['BASIC_MOD']  = 	"Din grunnleggende profil har nå blitt endret.";
+?>
