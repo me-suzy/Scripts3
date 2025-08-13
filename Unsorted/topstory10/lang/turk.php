d2367aacf4fdc65e53c0853ea35b89fa098f5bc8
me-suzy <me.suzana@gmail.com>
2025-08-14 02:07:53 +0300

    Add Unsorted/topstory10/lang/turk.php

---
+++ Unsorted/topstory10/lang/turk.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * ADMIN SECTION
+ */
+$tst["lang"]["postNewsFormHeader"]="Haber Ekle"; 
+$tst["lang"]["newsHeading"]="Baþlýk"; 
+$tst["lang"]["newsBody"]="Haber Gövdesi"; 
+$tst["lang"]["postUnsuccess"]="Haber eklenemedi. Lütfen tekrar deneyin..."; # if failure to post the news to the db
+$tst["lang"]["postSuccess"]="Haber baþarý ile eklendi..."; # if success for posting the message
+$tst["lang"]["resetFormBtn"]="Sil"; # reset button
+$tst["lang"]["postNewsBtn"]="Gönder"; # post news button
+$tst["lang"]["edit"]="Düzenle"; 
+$tst["lang"]["delete"]="Sil"; 
+$tst["lang"]["management"]="Yönetim";
+$tst["lang"]["delSelectedBtn"]="Seçilen Haberleri Sil";
+$tst["lang"]["deleteRecordSuccess"]="Haber baþarý ile silindi";
+$tst["lang"]["redirecting"]="3 saniye içinde haber listesine yönlendirileceksiniz...Lütfen bekleyin....";
+$tst["lang"]["redirectingIndex"]="3 saniye içinde yönetim sayfasýna yönlendirileceksiniz...Lütfen bekleyin....";
+$tst["lang"]["click2redirect"]="Eðer 3 saniye içinde yönlendirme gerçekleþmezse haber listesine dönmek için buraya týklayýn";
+$tst["lang"]["click2redirectIndex"]="Eðer 3 saniye içinde yönlendirme gerçekleþmezse ana sayfaya dönmek için buraya týklayýn";
+$tst["lang"]["deleteRecordFailure"]="Haber silinemedi ! Lütfen tekrar deneyin.";
+$tst["lang"]["sure2delete"]="Seçilen haberi silmek istediðinizden emin misiniz ?";
+$tst["lang"]["sure2deleteSelected"]="Seçilen haber(ler)i silmek istediðinizden emin misiniz ?";
+$tst["lang"]["yes"]="Evet";
+$tst["lang"]["no"]="HAYIR";
+$tst["lang"]["deleteAllBtn"]="Tüm haberleri sil";
+$tst["lang"]["deleteAllSuccess"]="Tüm haberler baþarý ile silindi";
+$tst["lang"]["deleteAllFailure"]="TÜm haberler silinemedi! Lütfen tekrar deneyin.";
+$tst["lang"]["sure2deleteAll"]="Tüm haberleri silmek istediðinizden emin misiniz ?";
+$tst["lang"]["records"]="Haberler";
+$tst["lang"]["updateNewsBtn"]="Güncelle"; # update news button
+$tst["lang"]["editNewsFormHeader"]="Haberi düzenle";
+$tst["lang"]["updateSuccess"]="Haber baþarý ile güncellendi"; # if success for updating record
+$tst["lang"]["updateFailure"]="Haber güncellenemedi!";
+$tst["lang"]["status"]="Durum";
+$tst["lang"]["statusUpdateSuccess"]="Durum deðiþtirildi";
+$tst["lang"]["statusUpdateFailure"]="Durum deðiþtirilemedi";
+$tst["lang"]["statusActive"]="Aktif";
+$tst["lang"]["statusPassive"]="Pasif";
+$tst["lang"]["click2ChangeStatus"]="Haberin statüsünü deðiþtirmek için týklayýn";
+$tst["lang"]["postNews"]="Haber Ekle";
+$tst["lang"]["listManage"]="Haberleri Listele / Yönet";
+$tst["lang"]["country"]="Ülke";
+$tst["lang"]["home"]="Ana Sayfa";
+$tst["lang"]["isEmpty"]=" boþ olmamalý";
+$tst["lang"]["pleaseCheckFields"]="Lütfen geri dönerek aþaðýdaki alanlarý düzeltin :";
+$tst["lang"]["emailInvalid"]="E-mail adresiniz geçersiz";
+$tst["lang"]["welcomeText"]="Top Story - Basic - Kontrol Paneline Hoþ Geldiniz";
+$tst["lang"]["totalActive"]="Aktif Haberler";
+$tst["lang"]["totalPassive"]="Pasif Haberler";
+$tst["lang"]["totalArticles"]="Tüm haberler";
+$tst["lang"]["statSummary"]="Veritabaný istatistikleri özeti";
+/**
+ * COMMON VARIABLES (BOTH ADMIN AND USER)
+ */
+$tst["lang"]["name"]="Ýsim";
+$tst["lang"]["surname"]="Soyad";
+$tst["lang"]["email"]="Email";
+$tst["lang"]["website"]="Websitesi";
+$tst["lang"]["datePosted"]="Eklenme Tarihi";
+/**
+ * USER SECTION
+ */
+$tst["lang"]["noArticles"]="Hiç haber yok..."; # if no articles to display
+$tst["lang"]["headlinesHeader"]="Haberler :"; # headlines header
+$tst["lang"]["moreArticles"]="Diðer haberler"; # if more articles than displayed, value of the button
+$tst["lang"]["postedBy"]="Gönderen";
+$tst["lang"]["ip"]="Ip";
+$tst["lang"]["new"]="yeni";
+$tst["lang"]["latestHeadlinesHeader"]="Son Haberler :";
+$tst["lang"]["back2Annc"]="Haberlere geri dön";
+$tst["lang"]["displayed"]="";
+$tst["lang"]["times"]="kere okundu";
+?>
