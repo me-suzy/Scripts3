b7a72d906997791afb096a7ffe6b765611ccecaf
me-suzy <me.suzana@gmail.com>
2025-08-14 02:31:21 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/mess_reply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/mess_reply.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['EDIT_MORE']  = 	"Edit some more";
+$ubbt_lang['REPLY_PREV']  = 	"Preview of your reply to";
+$ubbt_lang['PREVIEW_MESS']  = 	"The following is a preview of your message.  If every thing looks ok then you can click 'Continue' and your message will be sent.  If not then use the bottom form below to edit some more.";
+$ubbt_lang['MESS_PREVIEW']  = 	"I want to preview my message";
+$ubbt_lang['PRIV_SUB']  = 	"Der er kommet nye beskeder";
+$ubbt_lang['PRIV_BODY_R']  = 	"Du har modtaget en privat besked fra forumerne på . Beskeden kan læses ved at gå til {$config['phpurl']}/login.php";
+?>
