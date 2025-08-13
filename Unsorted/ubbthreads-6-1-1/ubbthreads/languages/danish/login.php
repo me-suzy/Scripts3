71235bde7bfc24b050092ec44c09d25d66fe5be5
me-suzy <me.suzana@gmail.com>
2025-08-14 02:31:16 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/login.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/login.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['FORGOT_PASS']   = "Forget your password?";
+$ubbt_lang['FORGOT_ENTER']  = "Enter your email address or username that you registered with to have a temporary password mailed to you.";
+$ubbt_lang['REMEMBER_ME']   =  "Remember me on each visit.";
+$ubbt_lang['LOGIN_PROMPT']  = 	"Log ind";
+$ubbt_lang['LOGIN_PROMPT2']  = 	"Indtast dit brugernavn og adgangskode. Hvis du endnu ikke har et brugernavn, kan du";
+$ubbt_lang['REG_ONE']  = 	"registrer et";
+$ubbt_lang['PASSWORD_TEXT']  = 	"Adgangskode";
+?>
