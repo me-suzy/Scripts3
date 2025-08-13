c47d7538b5ca822293c5475266e36fbe82e4b9d3
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:46 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/approvepost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/approvepost.php
@@ -0,0 +1,5 @@
+<?
+$ubbt_lang['REPLY_SUB']  = 	"Antwort auf Ihre Nachricht";
+$ubbt_lang['REPLY_BOD']  = 	"antwortete auf Ihre Nachricht: .";
+$ubbt_lang['APPR_HEAD']  = 	"Die Nachricht wurde freigegeben.";
+?>
