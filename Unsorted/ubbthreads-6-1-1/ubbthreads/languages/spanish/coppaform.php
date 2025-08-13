bb621693db86876400309d7a0ed575108648f12e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:12 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/coppaform.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/coppaform.php
@@ -0,0 +1,21 @@
+<?
+
+$ubbt_lang['PARENT_APP'] = "Es necesaria la Aprobaci&oacute;n Paterna";
+$ubbt_lang['PARENT_APP_2'] = "Uno de tus padres o tutores debe autorizar tu registro.  No podr&aacute;s registrarte hasta que no recibamos dicha aprobaci&oacute;n.  Para crear una cuenta, imprime esta página y sigue las instrucciones.";
+$ubbt_lang['MORE_INFO'] = "Para mayor informaci&oacute;n contacta con <a href=\"mailto:{$config['emailaddy']}\">{$config['emailaddy']}</a>";
+$ubbt_lang['LOGIN_NAME'] = "Nombre de Miembro";
+$ubbt_lang['LOGIN_NAME2'] = "Puedes poner m&uacute;ltiples nombres de miembros por si alguno de ellos no est&aacute; disponible.";
+$ubbt_lang['YOUR_EMAIL'] = "Direcci&oacute;n de email";
+$ubbt_lang['BIRTHDATE'] = "Fecha de nacimiento";
+$ubbt_lang['PARENT_INST'] = "Instrucciones para Padre/Madre o Tutor";
+$ubbt_lang['PARENT_INST2'] = "Rogamos firme el formulario, y nos lo envíe.";
+$ubbt_lang['COPPA_INST'] = "Instrucciones COPPA";
+$ubbt_lang['AGREE'] = "SI: He recibido la informaci&oacute;n que me ha dado mi hijo/a y he le&iacute;do la Pol&iacute;tica de Privacidad del sitio web. Entiendo que el perfil de usuario puede cambiar usando los datos de entrada para este registro.";
+$ubbt_lang['FULLNAME'] = "Nombre de Padre/Madre o Tutor";
+$ubbt_lang['SIGNATURE'] = "Firma";
+$ubbt_lang['RELATION'] = "Relaci&oacute;n con el hijo/a";
+$ubbt_lang['TELE'] = "N&uacute;mero de tel&eacute;fono (u otro m&eacute;todo de contacto fuera de internet)";
+$ubbt_lang['PARENT_EMAIL'] = "Email del Padre/madre o Tutor";
+$ubbt_lang['DATE'] = "Fecha";
+$ubbt_lang['REG_DATE'] = "Fecha de Registro";
+$ubbt_lang['QUESTIONS'] = "Si tiene preguntas, escribanos por email";
