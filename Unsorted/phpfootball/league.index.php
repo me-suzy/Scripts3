aa4954de18cd1b75a964ac5fa7466318051a1ef3
me-suzy <me.suzana@gmail.com>
2025-08-14 00:38:27 +0300

    Add Unsorted/phpfootball/league.index.php

---
+++ Unsorted/phpfootball/league.index.php
@@ -0,0 +1,14 @@
+<?php include("scripts/inc.layout.php"); ?>
+
+<html>
+<head>
+<title>PHPFootball league module</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<frameset cols="*" rows="*,106" frameborder="NO" border="0" framespacing="0"> 
+  <frame src="<?php echo $leaguesrc; ?>" name="top">
+  <frame src="league.php" name="bottom">
+</frameset>
+<noframes><body bgcolor="#FFFFFF">
+</body></noframes>
+</html>
