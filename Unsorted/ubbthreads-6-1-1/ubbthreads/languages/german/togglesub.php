6ea79b1a8169316b5d0181708ffeb8269a91a14d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:02 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/togglesub.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/togglesub.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['UN_HEADER']  = 	"Forum abbestellt";
+$ubbt_lang['HEADER']  = 	"Forum abonniert";
+$ubbt_lang['UN_BODY']  = 	"Folgendes Forum wurde abbestellt: ";
+$ubbt_lang['BODY']  =          "Folgendes Forum wurde abonniert: ";
+?>
