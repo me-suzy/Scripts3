692ced893bab91e5ee1ac6423df9be9a1bd8bfb2
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:04 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/viewmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/viewmessage.php
@@ -0,0 +1,11 @@
+<?
+$ubbt_lang['ABOUT']  = 	"about";
+$ubbt_lang['RR_YOUR_MESS']  = 	"Your private message to";
+$ubbt_lang['RR_READ']  = 	"has been read.";
+$ubbt_lang['RR_NOTIFY']  = 	"You have received a Read Receipt";
+$ubbt_lang['READ_RECEIPT']  = 	"Read Receipt";
+$ubbt_lang['TEXT_REC']  = 	"Received";
+$ubbt_lang['SENT']  = 	"Sent";
+$ubbt_lang['DELETE_MESS']  = 	"Delete this message";
+$ubbt_lang['REPLY_MESS']  = 	"Reply to this message";
+?>
