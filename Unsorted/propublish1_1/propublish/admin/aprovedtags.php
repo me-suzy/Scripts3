4fb26003faf5aaea2c3cd20542aecf535a589077
me-suzy <me.suzana@gmail.com>
2025-08-14 01:16:29 +0300

    Add Unsorted/propublish1_1/propublish/admin/aprovedtags.php

---
+++ Unsorted/propublish1_1/propublish/admin/aprovedtags.php
@@ -0,0 +1,51 @@
+<html>
+<h3>Tegn tillatt</h3>
+<a href="javascript:window.close();"><b>Lukk vindu</b></a><p>
+
+<p>
+Siden Webber.no spesiellt er beregnet for artikler relatert til kode,
+filtreres html og php ut og vises som kode i stedet for å vise
+layoutmessig. Derfor må spesiell html kode benyttes i stedet for 
+tradisjonell kode, [b] erstatter &ltb&gt;, for eksempel.
+<p>
+<u>Eksempel på tegn:</u><p>
+<table width=100% border=0 cellspacing="1" cellpadding="3">
+<tr><td><b>TAGG</b><br>&nbsp;</td><td><b>RESULTAT</b><br>&nbsp;</td></tr>
+<tr><td>&ltcode&gt;&lt? print "bla"; ?&gt;&lt/code&gt;</td><td>(Viser koden på en pen måte i tabell)<br></td></tr>
+<tr><td>&ltimage&gt;23&lt/image&gt</td><td>(Viser opplastet bilde fra db)<br></td></tr>
+<tr><td>&ltimage_left&gt;23&lt/image&gt</td><td>(Viser opplastet bilde fra db)<br></td></tr>
+<tr><td>&ltimage_right&gt;23&lt/image&gt</td><td>(Viser opplastet bilde fra db)<br></td></tr>
+<tr><td>[b]Dette er en fet skrift[/b]</td><td><b>Dette er en fet skrift</b><br></td></tr>
+<tr><td>[url]http://www.sol.no[/url]</td><td><a href="http://www.sol.no">Link</a><br></td>
+<tr><td>[i]Dette er en ingress skrift[/i]</td><td><i>Dette er ingress skrift</i><br></td></tr>
+<tr><td>[u]Dette er en understreket skrift[/u]</td><td><u>Dette er understreket skrift</u><br></td></tr>
+<tr><td>[img]http://banner.gif[/img]</td><td>(Viser bilde)<br></td></tr>
+<tr><td>[h1]Største overskrift[/h1]</td><td>(Viser største overskrift)<br></td></tr>
+<tr><td>[h2]Noe mindre overskrift[/h2]</td><td>(Viser noe mindre)<br></td></tr>
+<tr><td>[h3]Noe mindre overskrift[/h3]</td><td>(Viser noe mindre)<br></td></tr>
+<tr><td>[h4]Noe mindre overskrift[/h4]</td><td>(Viser noe mindre)<br></td></tr>
+<tr><td>[h5]Minste overskrift[/h5]</td><td>(Viser minste)<br></td></tr>
+<tr><td>[bq]Blabla[/bq]</td><td>(Viser innrykk)<br></td></tr>
+<tr><td>[ol]Ordnet liste[/ol]</td><td>(Ordnet listeområdet)<br></td></tr>
+<tr><td>[ul]Start ordnet liste[/ul]</td><td>(Unummerert listeområdet)<br></td></tr>
+<tr><td>[li]Listeelement[/li]</td><td>(Viser prikk ved hvert element)<br></td></tr>
+</table>
+<p>
+Ekspempel på liste:
+<ul type="square">
+	<li>Punkt 1</li>
+	<li>Punkt 2</li>
+	<li>Punkt 3</li>
+</ul>
+
+Lages ved kode:<br>
+[ul]<br>
+	<BLOCKQUOTE>
+	[li] Punkt 1 [/li]<br>
+	[li] Punkt 2 [/li]<br>
+	[li] Punkt 3 [/li]<br>
+	</BLOCKQUOTE>
+[/ul]<br>
+
+
+</html>
