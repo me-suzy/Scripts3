062903c658c052f188ba0590c0f058006f2ad1ef
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:46 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/donotifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/donotifymod.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['POST_PROB']  =  "Ett problem uppstod när vi försökte hämta detta inlägg från databasen.";
+$ubbt_lang['BAD_GROUP']  = 	"Tillträde nekas!";
+$ubbt_lang['NO_B_INFO']  = 	"Kunde inte hitta forumet!";
+$ubbt_lang['NOTIF_HEAD']  = 	"har meddelat dig om ett inlägg som du borde kontrollera som har ämnet";
+$ubbt_lang['TO_READ']  = 	"Inlägget kan läsas här:";
+$ubbt_lang['NOTIFY_SENT']  = 	"Moderatorer meddelade";
+$ubbt_lang['NOTIFY_CONFIRM']  =  "Moderatorerna för detta forum har meddelats.";
+?>
