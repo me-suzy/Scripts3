f2593351d65546e1e1db64e6e0e329253e0bb300
me-suzy <me.suzana@gmail.com>
2025-08-14 01:01:00 +0300

    Add Unsorted/phpx/admin/includes/config.inc.php

---
+++ Unsorted/phpx/admin/includes/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+#$Id: config.inc.php,v 1.6 2003/06/11 21:18:38 ryan Exp $
+$dbserver = "localhost"; //Database Server
+$dbuser = "USER"; //Database User
+$dbpass = "PASSWORD"; //Database User Password
+$database = "phpx"; //Database
+
+?>
