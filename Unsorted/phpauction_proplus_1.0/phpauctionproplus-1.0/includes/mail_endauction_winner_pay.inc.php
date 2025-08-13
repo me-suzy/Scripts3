8f734a9ea5bd644442077788d77937e0cf531c70
me-suzy <me.suzana@gmail.com>
2025-08-14 00:16:17 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/mail_endauction_winner_pay.inc.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/mail_endauction_winner_pay.inc.php
@@ -0,0 +1,55 @@
+#
+# --Send winner email address to seller
+# 
+# 			This file contains the message your customers
+# 			will receive when someone sends them an auction.
+#			Lines starting with # will be skipped.
+#			Blank lines will be maintained.
+#
+#			Change the message below as needed considering the 
+#			following tags to reflect your customer's personal data:
+#
+#        --------------------------------------------------------
+#			TAG SYNTAX				EFFECT
+#        --------------------------------------------------------
+#
+#			<#s_name#>              Seller Name
+#			<#s_nick#>              Seller Nickname
+#			<#s_email#>             Seller email
+#			<#s_address#>           Seller Address
+#			<#s_city#>              Seller City
+#			<#s_prov#>              Seller State/Province
+#			<#s_country#>           Seller Country
+#			<#s_zip#>               Seller Zip Code
+#			<#s_phone#>             Seller Phone
+#			<#i_title#>             auction item title 
+#			<#i_description#>       auction item description 
+#			<#i_url#>               URL to view auction 
+#			<#i_ends#>              Auction End date/time
+#			<#i_qty#>              Auction End date/time
+#           <#w_report#>            Winner report/list
+#           <#c_sitename#>          Auction Site Name
+#           <#c_siteurl#>           main URL of auction site
+#           <#c_adminemail#>        email address of Auction site webmaster
+#        --------------------------------------------------------
+#
+#			USAGE:
+#			Insert the above tags in the text of your message			
+#			where you want each value to appear.			
+#			Modify the message to reflect your needs.
+#			Change [...] with to your correct data.
+#
+# 
+#
+
+Dear <#s_name#>,
+
+The auction "<#i_title#>" you created at <#c_sitename#> has closed.
+To get the winner(s) information you have to pay the Final Value Fee.
+Please login at <#c_siteurl#> and access the "Winners contact info" section in you 
+Control Panel to proceed to the payment.
+
+If you have received this message in error, please reply to this email,
+write to <#c_adminemail#>, or visit <#c_sitename#> at <#c_siteurl#>.
+
+
