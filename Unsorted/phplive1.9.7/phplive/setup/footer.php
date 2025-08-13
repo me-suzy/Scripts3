f5276d382c46c2bd58bb6394be83e9df69a35bfd
me-suzy <me.suzana@gmail.com>
2025-08-14 00:49:03 +0300

    Add Unsorted/phplive1.9.7/phplive/setup/footer.php

---
+++ Unsorted/phplive1.9.7/phplive/setup/footer.php
@@ -0,0 +1,3 @@
+<? if ( file_exists( "$DOCUMENT_ROOT/web/VERSION_KEEP.php" ) ) { include_once( "$DOCUMENT_ROOT/web/VERSION_KEEP.php" ) ; } ?>
+<hr>
+<font color="#9999B5" size=1><? echo $LANG['DEFAULT_BRANDING'] ?> - v<? echo $PHPLIVE_VERSION ?> &copy; OSI Codes Inc.</font>
