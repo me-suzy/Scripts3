a3e3f1a118c72338513dfda963d552b8f65c68c0
me-suzy <me.suzana@gmail.com>
2025-08-14 00:58:25 +0300

    Add Unsorted/phpProMembers__nullified_/phpProMembers/admin/admin_members_activate.php

---
+++ Unsorted/phpProMembers__nullified_/phpProMembers/admin/admin_members_activate.php
@@ -0,0 +1,23 @@
+<?php
+/*********************************************************************/
+/* Program Name         : phpProMembers                              */
+/* Home Page            : http://www.gacybertech.com                 */
+/* Retail Price         : $149.99 United States Dollars              */
+/* WebForum Price       : $000.00 Always 100% Free                   */
+/* xCGI Price           : $000.00 Always 100% Free                   */
+/* Supplied by          : South [WTN]                                */
+/* Nullified by         : CyKuH [WTN]                                */
+/* Distribution         : via WebForum and Forums File Dumps         */
+/*********************************************************************/
+$page_account = "admin";
+require "include.php";
+
+$this_id = $GLOBALS["id"];
+
+$clean_up_sql = "UPDATE `members` SET active = \"1\" WHERE id = \"$this_id\"";
+mysql_query($clean_up_sql);
+
+header("Location: admin_search_in_active_members.php");
+exit;
+
+?>	
