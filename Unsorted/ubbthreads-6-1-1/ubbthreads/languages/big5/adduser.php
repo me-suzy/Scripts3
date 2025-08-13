69ac49631420c6063ee57bc36d0bf2bffbb9f1da
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:25 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =    "±zªº±b¸¹¥²¶·³q¹L¨t²ÎºÞ²zªÌªº®Ö¥i¡C·í¨t²ÎºÞ²zªÌ®Ö­ã«á¡A¨t²Î·|µo Email ³qª¾±z¡C";
+$ubbt_lang['APPROVED'] = "±zªº±b¸¹ {$config['title']} at {$config['phpurl']} ¤w¸g³Q®Ö¥i¤F¡C";
+$ubbt_lang['NOTAPPROVED'] = "±zªº±b¸¹ {$config['title']} at {$config['phpurl']} ¨Ã¨S¦³³Q®Ö¥i¡A­ì¦]¦p¤U¡G";
+$ubbt_lang['NEWREGBODY'] = "·s¨Ï¥ÎªÌµù¥U©ó {$config['title']}. ¦p­n§å­ã¥L¡A½ÐÂI¿ï";
+$ubbt_lang['NEWREG'] = "·s¨Ï¥ÎªÌµù¥U©ó";
+$ubbt_lang['INVALID_DATE'] = "¹ï¤£°_¡A³o­Ó¤é´Á¤£²Å¦X³W½d¡C";
+$ubbt_lang['TOOYOUNG'] =	"©êºp¡A±z¦~¬ö¤Ó¤p¡A¤£¯àµù¥U±b¸¹¡A ®Ú¾Ú <a href=\"http://www.ftc.gov/opa/1999/9910/childfinal.htm\">COPPA</a> ªk«ß¡A¨S¦³±z¤÷¥Àªº¦P·N¡A§Ú­Ì¤£¯à¬°±z«Ø¥ß±b¸¹¡C";
+$ubbt_lang['UNDER13'] = "¹ï¤£°_¡A¥»¯¸¤£±µ¨ü¤Q¤T·³¥H¤U¨àµ£µn¿ýµù¥U¡C";
+$ubbt_lang['NEW_BODY']  =  "¨t²Î¤w¸g«O¯d±zªº±b¸¹¤F.  ±zÀ³¸Ó«Ü§Ö´N·|¦¬¨ì¥»¯¸µo¥Xªº¤@«Ê email¡A¸Ì­±¥]§t±zªº±K½X¡C";
+$ubbt_lang['BAD_UNAME']  = 	"©êºp ¡A¦W¦r¤¤¤£¯à°ÑÂø HTML ½X ¡A«á­±¤]¤£¯à¦³ªÅ¥Õ¡C";
+$ubbt_lang['LONG_NAME']  = 	"¨Ï¥ÎªÌ¦WºÙªø«×¤£¹ï¡C";
+$ubbt_lang['BAD_EMAIL']  = 	"©êºp¡A±z¤£¯à±q³o­Ó email ºô°ìµù¥U±b¸¹";
+$ubbt_lang['USER_EXISTS']  = 	"³o­Ó¥N¸¹¥Ø«e¤w¸g¦³¤H¨Ï¥Î¡C";
+$ubbt_lang['NO_MULTI']  = 	"©êºp¡A¨t²Î¤¤¤w¸g¦³¤@­Ó¤@¼Ëªº Email Address ¡A¤@­Ó Email Address ¥u¯à¥Ó½Ð¤@­Ó±b¸¹¡C";
+$ubbt_lang['PASS_MATCH']  = 	"±K½X¤£¥¿½T¡C";
+$ubbt_lang['PASS_TOO_LONG']  = 	"±K½Xªø«×¥²¶·¤¶©ó4¨ì20­Ó¦r¤¸¡C";
+$ubbt_lang['ILL_PASS']  = 	"±K½X¥]§t¤F«Dªk¦r¤¸. ½Ð¨Ï¥Î­^¤å¥H¤Î¼Æ¦r²Õ¦X¦¨ªº±K½X (A-Za-z0-9)¡C";
+$ubbt_lang['WEL_MSG']  = 	"³o¬O·s¨Ï¥ÎªÌ³£·|¦¬¨ìªº«H¥ó¡G<BR>½Ð©âªÅ½s¿è±zªº­Ó¤HÀÉ®×¡C±z¥i¥H¦b±zªº­Ó¤HÀÉ®×¸Ì­±¶ñ¼g¤@¨Ç­Ó¤H¸ê®Æ(§O¤H¬d¸ß¨Ï¥ÎªÌ®É´N·|Åã¥Ü¥X¨Ó) ¡A½s¿èÃ±¦WÀÉ(·|ªþ¥[¦b±zªº¨C¤@½g¤å³¹«á­±) ¥H¤Î³]©w±zªº­Ó¤HÅã¥Ü³ß¦n (±Æ§Ç¤èªk¡AÅã¥Ü¼Ò¦¡,¥­©Z©Î¬O¾ðª¬¼Ò¦¡...µ¥µ¥.)¡C";
+$ubbt_lang['PASS_SUB']  = 	"±zªº±K½X :";
+$ubbt_lang['PASS_BODY1']  = 	" ±z¦n¡A³o¬O¤@«Ê½T»{«H¨ç¡A±z­è¤~¦b¥»¯¸±q IP ¦ì§}¡G  ";
+$ubbt_lang['PASS_BODY2']  = 	" µn¿ýµù¥U¤F¤@­Ó·sªº ID¡G ";
+$ubbt_lang['PASS_BODY3']  = 	" ½Ð±z¨Ï¥Î³o­Ó±K½Xµn¤J¡G ";
+$ubbt_lang['NEW_CONFIRM']  = 	"¨Ï¥ÎªÌ¦WºÙ¤w¸gµù¥U¤F¡C";
+?>
