0d257f6497994da3e7294ac8c7906cd4d6da1ce0
me-suzy <me.suzana@gmail.com>
2025-08-14 00:32:13 +0300

    Add Unsorted/PHPCheckout/phpcheckout/installSecurityInfo.php

---
+++ Unsorted/PHPCheckout/phpcheckout/installSecurityInfo.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+	<head>
+
+		<script language="Javascript" src="appinstaller.js"></script>
+		<script language="Javascript">loadCSSFile();</script>
+		<title>Install Helper - a DreamRiver Software Product - Easily install DreamRiver Software - php software mysql database - download - web database - dreamriver - freeware - sql - file downloader - internet application development - hot scripts - good value - database software - custom code - php e-mail software - ultimate web apps software - phpYellow Pages - EasySQL - phpTellAFriend - mysql php download</title>
+	</head> 
+
+<body>
+<!-- START of body -->
+
+<h1>DreamRiver Install Helper</h1>
+
+<H2>phpcheckout<sup>TM</sup></H2>
+
+
+<form name="startForm">
+<table bgcolor="silver" cellspacing=3 cellpadding=10 width="80%">
+<tr>
+	<th colspan=2>Install Helper</th>
+</tr>
+<tr>
+	<td bgcolor="white" valign="top">
+		<p><br></p>
+
+		<h4>After Installation Security Information</h4>
+		
+
+		<p>
+		Complete these instructions to enhance your security:
+
+		<ul>
+			<li>remove all files that start with the word &quot;install&quot; from the <b>/phpcheckout</b> folder</li>
+			<li>immediately change the name of the folder called <b>/secret</b> to any other word</li>
+			<li>perodically <i>> admin.php > View Download Log</i> to keep tabs on file downloads</li>
+			<li>manually empty the garbage <i>> admin.php > Collect the Garbage ></i> ... so hackers can not download anything in it</li>
+			<li>set up the automatic garbage collection script - info in <i>> admin.php</i></li>		
+			<li>use a good, long and varied password in configure.php</li>
+			<li>periodically change your password in configure.php</li>
+		</ul>
+
+		</p>
+
+ 
+
+	</td>
+	<td bgcolor="ivory" align="center">
+		<img src="installBOP.jpg" width="180" height="180" border=0>
+	</td>
+</tr>
+<tr>
+	<td align="right" colspan=2>
+		<hr>
+		&nbsp;
+	</td>
+</tr>
+</table>
+</form>
+
+
+</body>
+</html>
