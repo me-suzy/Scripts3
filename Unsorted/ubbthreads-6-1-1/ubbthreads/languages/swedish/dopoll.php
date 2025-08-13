c189943da7d20b45086c973e2eed4ad6b6e7867b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:47 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/dopoll.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/dopoll.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NO_VOTE']  = 	"Du röstade inte på något!";
+$ubbt_lang['POLL_REGED']  = 	"Bara registrerade användare som är inloggade kan rösta.";
+$ubbt_lang['POLL_IP']  = 	"Vi har redan en röst från denna IP-adress.";
+$ubbt_lang['POLL_USER']  = 	"Vi har redan en röst från ditt Användarnamn.";
+?>
