84cbe8b5e174e97455d01de6483cf84599399041
me-suzy <me.suzana@gmail.com>
2025-08-14 01:40:19 +0300

    Add Unsorted/Shop-Script.2.0.(NEW).suupl.&.null.by.[GTT]/includes/language/russian.php

---
+++ Unsorted/Shop-Script.2.0.(NEW).suupl.&.null.by.[GTT]/includes/language/russian.php
@@ -0,0 +1,362 @@
+<?
+/*****************************************************************************
+ *                                                                           *
+ * Shop-Script 2.0                                                           *
+ * Copyright (c) 2002-2003 Articus consulting group. All rights reserved.    *
+ *                                                                           *
+ ****************************************************************************/
+ 
+ 
+ 
+//language file
+
+//		RUSSIAN		//
+
+
+//default character set, that will be used
+define('DEFAULT_CHARSET', 'windows-1251');
+
+
+//miscellanious: buttons and links
+define('LINK_TO_HOMEPAGE', 'Ãëàâíàÿ');
+define('PRODUCTS_BEST_CHOISE', 'Ëó÷øèé âûáîð');
+define('MORE_INFO_ON_PRODUCT', 'ïîäðîáíåå...');
+define('ENLARGE_PICTURE', 'óâåëè÷èòü...');
+define('DISCUSS_ITEM_LINK', 'îáñóäèòü');
+define('POSTS_FOR_ITEM_STRING', 'ìíåíèé');
+define('ADD_TO_CART_STRING', 'äîáàâèòü â êîðçèíó');
+define('LIST_PRICE', 'Ñòàðàÿ öåíà');
+define('CURRENT_PRICE', 'Íàøà öåíà');
+define('YOU_SAVE', 'Âû ýêîíîìèòå');
+define('IN_STOCK', 'Íàëè÷èå íà ñêëàäå');
+define('VOTING_FOR_ITEM_TITLE', 'Îöåíèòå òîâàð!');
+define('MARK_EXCELLENT', 'Îòëè÷íî!');
+define('MARK_GOOD', 'Õîðîøî');
+define('MARK_AVERAGE', 'Ñðåäíå');
+define('MARK_POOR', 'Ïëîõî');
+define('MARK_PUNY', 'Î÷åíü ïëîõî');
+define('VOTE_BUTTON', 'Îöåíèòü!');
+define('VOTES_FOR_ITEM_STRING', 'ãîëîñîâ');
+
+define('MY_ACCOUNT_LINK', 'Ìîé ñ÷åò');
+define('REGISTER_LINK', 'Ðåãèñòðàöèÿ');
+define('CHANGE_CURRENCY_LINK', 'Èçìåíèòü âàëþòó');
+define('LOGOUT_LINK', 'Âûéòè èç ñåàíñà...');
+define('ADVANCED_SEARCH_LINK', 'ðàñøèðåííûé ïîèñê');
+define('FORGOT_PASSWORD_LINK', 'çàáûëè ïàðîëü?');
+define('ADMINISTRATE_LINK', '>> ÀÄÌÈÍÈÑÒÐÈÐÎÂÀÍÈÅ <<');
+define('TERMINATE_ACCOUNT_LINK', 'óäàëèòü àêêàóíò');
+
+define('ANSWER_YES', 'äà');
+define('ANSWER_NO', 'íåò');
+define('POST_BUTTON', 'Äîáàâèòü ñîîáùåíèå');
+define('OK_BUTTON', 'OK');
+define('RESET_BUTTON', 'Î÷èñòèòü');
+define('SAVE_BUTTON', 'Ñîõðàíèòü');
+define('DELETE_BUTTON', 'Óäàëèòü');
+define('CLOSE_BUTTON', 'Çàêðûòü');
+define('CANCEL_BUTTON', 'Îòìåíà');
+define('UPDATE_BUTTON', 'Îáíîâèòü');
+define('ADD_BUTTON', 'Äîáàâèòü');
+define('CONFIG_BUTTON', 'Íàñòðîéêè...');
+define('EDIT_BUTTON', 'Ðåäàêòèðîâàòü');
+define('FIND_BUTTON', 'Íàéòè!');
+define('COMPLETE_ORDERS_BUTTON', 'Çàêàçû âûïîëíåíû!');
+
+//misc strings
+define('STRING_BEFORE', 'äî');
+define('STRING_AFTER', 'ïîñëå');
+define('STRING_SHOW', 'ïîêàçûâàòü');
+define('STRING_NUMBER', '÷èñëà');
+define('STRING_AUTHORIZATION', 'Âõîä äëÿ ïîëüçîâàòåëåé');
+define('STRING_RELATED_ITEMS', 'Ñ ýòèì òîâàðîì ìû ðåêîìåíäóåì');
+define('STRING_NUMBER_ONLY', 'òîëüêî ÷èñëî');
+define('STRING_UNIVERSAL_CURRENCY', 'ó.å.');
+define('STRING_EMPTY_CATEGORY', 'íåò òîâàðîâ');
+define('STRING_NO_ORDERS', '< íåò çàêàçîâ >');
+define('STRING_EMPTY_LIST', '<b><font color=brown>ÏÓÑÒÎÉ ÑÏÈÑÎÊ</font></b>');
+define('STRING_SEARCH', 'Ïîèñê:');
+define('STRING_SEARCH_IN_RESULTS', 'èñêàòü â íàéäåííîì');
+define('STRING_THANK_YOU', 'Ñïàñèáî!');
+define('STRING_SUCCESSFUL_ACCOUNT_TERMINATION', 'Âàøà ðåãèñòðàöèÿ áûëà óñïåøíî îòìåíåíà. Ñïàñèáî çà ïîêóïêè!');
+define('STRING_PASSWORD_SENT', 'Âàø ïàðîëü áûë îòïðàâëåí íà ');
+define('STRING_CANT_FIND_USER_IN_DB', 'Ïîëüçîâàòåëü ñ òàêèì ëîãèíîì íå íàéäåí â áàçå äàííûõ');
+define('STRING_PRICELIST', 'Ïðàéñ-ëèñò');
+define('STRING_SELECT_CURRENCY_TYPE', 'Ïîæàëóéñòà, âûáåðèòå âàëþòó, â êîòîðîé áóäóò ïîêàçàíû öåíû:');
+define('STRING_FORGOT_PASSWORD_FIX', '<u>Çàáûëè ïàðîëü?</u> Ïîæàëóéñòà, ââåäèòå Âàø ëîãèí:');
+define('STRING_ACOOUNT_UPDATE_SUCCESSFUL', 'Äàííûå óñïåøíî èçìåíåíû!<br>Ñîîáùåíèå ñ îáíîâëåííûìè äàííûìè îòïðàâëåíî Âàì ïî ïî÷òå.');
+define('STRING_REGISTRATION_SUCCESSFUL', 'Ðåãèñòðàöèÿ ïðîøëà óñïåøíî!<br>Ñîîáùåíèå ñ ââåäåííûìè äàííûìè îòïðàâëåíî Âàì ïî ïî÷òå.');
+define('STRING_GREETINGS', 'Çäðàâñòâóéòå, äîáðî ïîæàëîâàòü â ìàãàçèí Shop-Script!');
+define('STRING_CC_SELECTED', 'Âû âûáðàëè êðåäèòíóþ êàðòó â êà÷åñòâå îïëàòû. Ïîæàëóéñòà, ïðåäîñòàâüòå ñëåäóþùóþ èíôîðìàöèþ î Âàøåé êàðòå.');
+define('STRING_CC_NUMBER', 'Íîìåð êàðòû:');
+define('STRING_CC_EXP_DATE', 'Ñðîê äåéñòâèÿ (MMYY):');
+define('STRING_FOUND', 'Íàéäåíî ');
+define('STRING_PRODUCTS', 'òîâàð(îâ)');
+define('STRING_CATEGORIES', 'êàòåãîðèé');
+define('STRING_SEARCH_TIP', '<font color=gray>// èùóòñÿ ñîâïàäåíèÿ â íàçâàíèÿõ è îïèñàíèÿõ òîâàðîâ/êàòåãîðèé //</font>');
+define('STRING_ADVANCED_SEACH_TITLE', 'Ðàñøèðåííûé ïîèñê');
+define('STRING_NO_MATCHES_FOUND', '// íè÷åãî íå íàéäåíî //');
+define('STRING_ADV_SEARCH_ANY', 'íå èìååò çíà÷åíèÿ');
+define('STRING_PRICE_FROM', 'îò');
+define('STRING_PRICE_TO', 'äî');
+define('STRING_ORDER_ID', 'Íîìåð çàêàçà');
+define('STRING_CHECK_YOUR_ORDER', 'Ïðîâåðêà çàêàçà');
+define('STRING_CHOOSE_PAYMENT_SHIPPING_TYPES', 'Ïîæàëóéñòà, âûáåðèòå ñïîñîá äîñòàâêè è îïëàòû');
+define('STRING_ORDER_CONFIRMATION', 'Ïîäòâåðæäåíèå çàêàçà');
+define('STRING_CASH_PAYMENT', 'Íàë');
+define('STRING_NOT_CASH_PAYMENT', 'Á/íàë');
+define('STRING_TAX', 'Íàëîã');
+define('STRING_SHIPPING_TYPE', 'Äîñòàâêà');
+define('STRING_PAYMENT_TYPE', 'Îïëàòà');
+define('STRING_CUSTOMER_COMMENTS', 'Êîììåíòàðèè/ïîæåëàíèÿ');
+define('STRING_ORDER_PLACED', 'Ñïàñèáî çà Âàø çàêàç!<br>Â áëèæàéøåå âðåìÿ íàø ìåíåäæåð ñâÿæåòñÿ ñ Âàìè');
+define('STRING_YOUR_ORDER', 'Âàø çàêàç');
+define('STRING_SHIPPING_ADDRESS', 'Àäðåñ äîñòàâêè');
+define('STRING_PLACE_ORDER', 'Îôîðìèòü çàêàç!');
+define('STRING_CONTACT_INFORMATION', 'ÊÎÍÒÀÊÒÍÀß ÈÍÔÎÐÌÀÖÈß');
+define('STRING_UPDATE_DETAILS', 'Èçìåíèòü ðåãèñòðàöèîííûå äàííûå');
+define('STRING_REGISTRATION_FORM', 'Ðåãèñòðàöèîííàÿ ôîðìà');
+define('STRING_ORDER_CONTINUE_TIP', 'Äëÿ îôîðìëåíèÿ çàêàçà Âû äîëæíû áûòü çàðåãèñòðèðîâàííû â ìàãàçèíå.<br>
+	Åñëè Âû óæå çàðåãèñòðèðîâàëèñü, ïîæàëóéñòà, âîñïîëüçóéòåñü ôîðìîé ñïðàâà.');
+define('STRING_REQUIRED', '<font color=red>*</font> îáÿçàòåëüíû äëÿ çàïîëíåíèÿ');
+define('STRING_AUTHORIZATION_FIELDS', 'ÂÕÎÄ Â ÌÀÃÀÇÈÍ');
+define('STRING_SHOPPING_HISTORY', 'Èñòîðèÿ çàêàçîâ');
+define('STRING_ORDER_NOT_COMPLETED', 'Íå âûïîëíåí');
+define('STRING_ORDER_COMPLETED', 'Âûïîëíåí');
+define('STRING_CONTACTS', 'Êîíòàêòíàÿ èíôîðìàöèÿ');
+define('STRING_ALL', 'âñå');
+define('STRING_DEFAULT', 'ïî óìîë÷àíèþ');
+define('STRING_NEXT', 'ñëåä');
+define('STRING_PREVIOUS', 'ïðåä');
+
+define('STRING_NAME', 'Íàçâàíèå');
+define('STRING_DESCRIPTION', 'Îïèñàíèå');
+define('STRING_SHIPPING_LUMPSUM', 'Ìèíèìàëüíàÿ ñòîèìîñòü äîñòàâêè');
+define('STRING_SHIPPING_PERCENT', 'Ïðîöåíò îò ñóììû çàêàçà');
+define('STRING_PAYMENT_CALCTAX', 'âû÷èñëÿòü íàëîã äëÿ ýòîãî òèïà îïëàòû');
+
+//item discussion phrases
+
+define('DISCUSSION_TITLE', 'îáñóæäåíèå');
+define('DISCUSSION_NO_POSTS_ON_ITEM_STRING', 'íåò ñîîáùåíèé íà ýòîò òîâàð');
+define('DISCUSSION_NICKNAME', 'Èìÿ');
+define('DISCUSSION_SUBJECT', 'Òåìà ñîîáùåíèÿ');
+define('DISCUSSION_BODY', 'Òåêñò');
+define('DISCUSSION_DELETE_POST_LINK', 'óäàëèòü ñîîáùåíèå');
+define('DISCUSSION_ADD_MESSAGE', 'Íîâîå ñîîáùåíèå');
+
+//shopping cart
+
+define('CART_CONTENT_EMPTY', '(íåò òîâàðîâ)');
+define('CART_CONTENT_NOT_EMPTY', 'òîâàð(îâ): ');
+define('CART_TITLE', 'Âàøà êîðçèíà');
+define('CART_CLEAR', 'î÷èñòèòü');
+define('CART_UPDATE', 'Îáíîâèòü');
+define('CART_PROCEED_TO_CHECKOUT', 'Îôîðìèòü çàêàç >>');
+define('CART_EMPTY', 'Âàøà êîðçèíà ïóñòà');
+
+//table titles
+
+define('TABLE_PRODUCT_NAME', 'Íàçâàíèå');
+define('TABLE_PRODUCT_QUANTITY', 'Êîëè÷åñòâî');
+define('TABLE_PRODUCT_COST', 'Ñòîèìîñòü');
+define('TABLE_TOTAL', 'Èòîãî:');
+define('TABLE_ORDER_TIME', 'Âðåìÿ çàêàçà');
+define('TABLE_ORDERED_PRODUCTS', 'Çàêàçàííûå òîâàðû');
+define('TABLE_ORDER_TOTAL', 'Ñòîèìîñòü çàêàçà');
+define('TABLE_CUSTOMER', 'Ïîêóïàòåëü');
+define('TABLE_ORDER_COMMENTS', 'Êîììåíòàðèè');
+
+//different admin strings
+
+define('ADMIN_TITLE', 'ÀÄÌÈÍÈÑÒÐÀÒÎÐ');
+
+define('ADMIN_NEW_ORDERS', 'Íîâûå çàêàçû');
+define('ADMIN_CATEGORIES_PRODUCTS', 'Êàòåãîðèè è òîâàðû');
+define('ADMIN_PRODUCT_OPTIONS', 'Äîïîëíèòåëüíûå õàðàêòåðèñòèêè òîâàðîâ');
+define('ADMIN_SETTINGS', 'Íàñòðîéêè');
+define('ADMIN_STATISTICS', 'Ñòàòèñòèêà');
+define('ADMIN_CUSTOMERS', 'Ïîêóïàòåëè');
+define('ADMIN_VOTING', 'Ãîëîñîâàíèå');
+define('ADMIN_SHIPPING_PAYMENT', 'Ñïîñîáû äîñòàâêè è îïëàòû');
+define('ADMIN_CC_PROCESSING', 'Îáðàáîòêà êðåäèòíûõ êàðò');
+define('ADMIN_IMPORT_FROM_EXCEL', 'Èìïîðò èç Excel');
+define('ADMIN_NEWS', 'Íîâîñòè');
+define('ADMIN_ABOUT_PAGE', 'Î ìàãàçèíå');
+define('ADMIN_SHIPPING_PAGE', 'Äîñòàâêà è îïëàòà');
+define('ADMIN_SYNCHRONIZE_TOOLS', 'Ñèíõðîíèçàöèÿ áàç äàííûõ');
+define('ADMIN_AUX_INFO', 'Äîïîëíèòåëüíàÿ èíôîðìàöèÿ');
+define('ADMIN_BACK_TO_SHOP', '>> <u>â îáùåäîñòóïíóþ ÷àñòü</u> &lt;&lt;');
+
+define('ADMIN_PRIMARY_COLUMN', 'Êîëîíêà èäåíòèôèêàöèè: ');
+define('ADMIN_PRIMARY_COLUMN_DESC', '(óêàæèòå êîëîíêó, çíà÷åíèå â êîòîðîé îäíîçíà÷íî èäåíòèôèöèðóåò òîâàð)<br><b>Áóäüòå âíèìàòåëüíû ïðè åå âûáîðå - íåâåðíûé âûáîð ìîæåò ïðèâåñòè ê íåïðåäñêàçóåìûì ïîñëåäñòâèÿì!</b>');
+
+define('ADMIN_CHOOSE_CREDIT_CARD_TYPE', 'Ïîæàëóéñòà, âûáåðèòå òèï îïëàòû, êîòîðûé ïðåäïîëàãàåò<br> îáðàáîòêó ïëàòåæà ïî êðåäèòíûì êàðòàì ÷åðåç ïðîöåññèíãîâûé öåíòð');
+define('ADMIN_CONNECTION_TYPE', 'Òèï ñîåäèíåíèÿ:');
+define('ADMIN_UPDATE_SUCCESSFUL', '<font color=blue><b>Îáíîâëåíèå ïðîøëî óñïåøíî!</b></font>');
+define('ADMIN_CHOOSE_GATEWAY', 'Ïîæàëóéñòà, âûáåðèòå ïðîöåññèíãîâûé öåíòð');
+define('ADMIN_NO_PRODUCT_OPTIONS', 'íåò äîïîëíèòåëüíûõ õàðàêòåðèñòèê òîâàðîâ');
+define('ADMIN_ADD_NEW_OPTION', '<b>Äîáàâèòü õàðàêòåðèñòèêó:</b>');
+define('ADMIN_UPDATE_GC_VALUE_BUTTON', 'Îáíîâèòü çíà÷åíèå `Êîëè÷åñòâî òîâàðîâ` äëÿ êàòåãîðèé');
+define('ADMIN_ROOT_WARNING', '<font color=red>Âñå òîâàðû, íàõîäÿùèåñÿ â êîðíå, íå âèäíî ïîëüçîâàòåëÿì!</font>');
+define('ADMIN_ABOUT_PRICES', '<font class=small>// öåíû àêòóàëüíû íà ìîìåíò çàêàçà è óêàçàíû áåç íàëîãà //</font>');
+define('ADMIN_NEW_NEWSARTICLE', 'Äîáàâèòü íîâîñòè');
+define('ADMIN_CURRENT_DATE', 'Äàòà');
+define('ADMIN_SEND_NEWS_TO_SUBSCRIBERS', 'Ðàçîñëàòü ýòó íîâîñòü ïîäïèñ÷èêàì');
+define('ADMIN_NEWS_SUBSCRIBERS', 'Ïîäïèñ÷èêè íà íîâîñòè');
+define('ADMIN_ALL_USERS', 'Âñå ïîêóïàòåëè:');
+define('ADMIN_USERS_WITH_LOGIN_ON', 'Ïîêóïàòåëè ñ ëîãèí íà áóêâó');
+define('ADMIN_EXPORT_USER_TO_FILE', 'Ýêñïîðòèðîâàòü ïîëüçîâàòåëåé â Excel ôàéë');
+define('ADMIN_EXPORT_DB_TO_FILE', 'Ýêñïîðòèðîâàòü áàçó â SQL-ôàéë');
+define('ADMIN_USERS_EXPORTED_TO', 'Ôàéë ñ çàðåãèñòðèðîâàííûìè ïîëüçîâàòåëÿìè ñîçäàí: ');
+define('ADMIN_DB_EXPORTED_TO', 'Áàçà óñïåøíî ýêñïîðòèðîâàíà â ôàéë: ');
+define('ADMIN_IMPORT_FROM_SQL', 'Èìïîðòèðîâàòü áàçó èç SQL-ôàéëà<br>(èñïîëüçóéòå ôàéëû, ñîçäàííûå Shop-Script)<br>
+	<font color=red><b>Âñå òåêóùåå ñîäåðæèìîå áàçû äàííûõ òîâàðîâ è êàòåãîðèé áóäåò óäàëåíî!</b></font>');
+define('ADMIN_SHOP_NAME', 'Íàçâàíèå ìàãàçèíà');
+define('ADMIN_SHOP_URL', 'URL ìàãàçèíà');
+define('ADMIN_DESKTOP', 'ÎÔÎÐÌËÅÍÈÅ');
+define('ADMIN_MAX_PRODUCTS_COUNT_PER_PAGE', 'Ìàêñèìàëüíîå êîëè÷åñòâî òîâàðîâ íà ñòðàíèöå');
+define('ADMIN_MAX_COLUMNS_PER_PAGE', 'Êîëè÷åñòâî ñòîëáöîâ ïðè ïîêàçå òîâàðîâ');
+define('ADMIN_MAIN_COLORS', 'Öâåòà, èñïîëüçóåìûå äëÿ îòîáðàæåíèÿ òàáëèö:');
+define('ADMIN_COLOR', 'Öâåò');
+define('ADMIN_CURRENCY_TYPES', 'ÂÀËÞÒÛ');
+define('ADMIN_CURRENCIES_DESCRIPTION', '
+Âñå âèäû âàëþò ïîêàçàíû â ñëåäóþùåé òàáëèöå.<br>
+Ñëåâà óêàçûâàåòñÿ îáîçíà÷åíèå âàëþòû,<br>ñïðàâà - åå êóðñ ïî îòíîøåíèþ ê óñëîâíîé åäèíèöå.
+<br>
+×òîáû óäàëèòü êàêîé-ëèáî âèä âàëþòû,<br>
+îñòàâüòå ñîîòâåòñòâóþùèå åé äâà ïîëÿ ïóñòûìè.<br><br>
+');
+define('ADMIN_POPULAR_PRODUCTS', 'Ñàìûå <b>ïîïóëÿðíûå</b> òîâàðû (âûñîêèé ðåéòèíã)');
+define('ADMIN_SALABLE_PRODUCTS', '<b>Áåñòñåëëåðû</b>');
+define('ADMIN_OUT_OF_STOCK', '<b><font color=red>Îòñóòñòâóþò íà ñêëàäå!</font><b>');
+define('ADMIN_SYNCR_DESC', 'Â ýòîì ðàçäåëå Âû ìîæåòå ñèíõðîíèçèðîâàòü ñîäåðæèìîå áàç äàííûõ ìåæäó
+íåñêîëüêèìè ìàãàçèíàìè íà îñíîâå Shop-Script 2.0.<br>
+Ê ïðèìåðó, ýêñïîðòèðîâàòü áàçó òîâàðîâ è êàòåãîðèé èç îäíîãî ìàãàçèíà, è çàìåíèòü åé áàçó äðóãîãî ìàãàçèíà.
+');
+define('ADMIN_START_NEW_POLL', 'íà÷àòü íîâîå ãîëîñîâàíèå');
+define('ADMIN_POLL_WARNING', '// ðåçóëüòàòû ïðåäûäóùåãî ãîëîñîâàíèÿ áóäóò àííóëèðîâàíû //');
+define('ADMIN_POLL_QUESTION', 'Âîïðîñ:');
+define('ADMIN_POLL_OPTIONS', 'Âàðèàíòû îòâåòîâ:<br><font class=average color=#555555>(êàæäûé â<br>îòäåëüíîé ñòðîêå)</font>');
+
+define('ADMIN_CATEGORY_TITLE', 'Êàòåãîðèè');
+define('ADMIN_CATEGORY_NEW', 'Ñîçäàòü íîâóþ êàòåãîðèþ');
+define('ADMIN_CATEGORY_PARENT', 'Ðîäèòåëü:');
+define('ADMIN_CATEGORY_MOVE_TO', 'Ïåðåìåñòèòü â:');
+define('ADMIN_CATEGORY_NAME', 'Íàçâàíèå êàòåãîðèè:');
+define('ADMIN_CATEGORY_LOGO', 'Ëîãîòèï:');
+define('ADMIN_CATEGORY_ROOT', 'Êîðåíü');
+define('ADMIN_CATEGORY_DESC', 'Îïèñàíèå');
+
+define('ADMIN_PRODUCT_TITLE', 'Òîâàðû');
+define('ADMIN_PRODUCT_NEW', 'Äîáàâèòü íîâûé òîâàð');
+define('ADMIN_PRODUCT_PARENT', 'Êàòåãîðèÿ');
+define('ADMIN_PRODUCT_CODE', 'Âíóòðåííèé êîä (àðòèêóë)');
+define('ADMIN_PRODUCT_NAME', 'Íàèìåíîâàíèå');
+define('ADMIN_PRODUCT_RATING', 'Ðåéòèíã');
+define('ADMIN_PRODUCT_PRICE', 'Öåíà');
+define('ADMIN_PRODUCT_LISTPRICE', 'Ñòàðàÿ öåíà');
+define('ADMIN_PRODUCT_INSTOCK', 'Íà ñêëàäå');
+define('ADMIN_PRODUCT_PICTURE', 'Ôîòîãðàôèÿ');
+define('ADMIN_PRODUCT_THUMBNAIL', 'Ìàëåíüêàÿ ôîòîãðàôèÿ');
+define('ADMIN_PRODUCT_BIGPICTURE', 'Áîëüøàÿ ôîòîãðàôèÿ');
+define('ADMIN_PRODUCT_ROOT', 'Êîðåíü');
+define('ADMIN_PRODUCT_DESC', 'Îïèñàíèå');
+define('ADMIN_PRODUCT_BRIEF_DESC', 'Êðàòêîå îïèñàíèå');
+define('ADMIN_PRODUCT_SHOW_IN_SPECIAL_OFFERS', 'Ïîêàçûâàòü â ñïåöèàëüíûõ ïðåäëîæåíèÿõ');
+define('ADMIN_PRODUCT_SOLD', 'Ïðîäàíî');
+define('ADMIN_PRODUCT_VOTES', 'Ãîëîñîâ');
+
+define('ADMIN_MAIN_MENU', 'Ãëàâíîå ìåíþ');
+define('ADMIN_IMPORT_FROM_EXCEL_ABOUT', 'Î ñòðóêòóðå ïðàéñ-ëèñòà');
+define('ADMIN_DB_UPDATE_SUCCESSFUL', 'Áàçà óñïåøíî îáíîâëåíà!');
+define('ADMIN_EXCEL_DESC1', 'Äëÿ çàïîëíåíèÿ áàçû äàííûõ òîâàðàìè èç ïðàéñ-ëèñòà íåîáõîäèìî ñîõðàíèòü ïðàéñ-ëèñò â ôîðìàòå <b>.CSV</b>.<br>Çàòåì çàêà÷àòü åãî íà ñåðâåð. Äëÿ ýòîãî ïðîñòî ââåäèòå ôàéë â ñëåäóþùåé ôîðìå.
+<p>Àíàëèçàòîð ïðàéñ-ëèñòà ñíà÷àëà ïðåäëîæèò âàì ñîîòíåñòè ñòîëáöû ôàéëà ñ ïîëÿìè â áàçå äàííûõ<br>è óêàçàòü ñòîëáåö èäåíòèôèêàöèè (òîò, ïî êîòîðîìó áóäåò ïðîèñõîäèòü ñðàâíåíèå ïîçèöèé),<br>è çàòåì îáíîâèò ñîäåðæèìîå áàçû.
+<p>
+Ñêðèïò òàêæå äîáàâëÿåò â áàçó äàííûõ êàòåãîðèè (êàòåãîðèÿìè ñ÷èòàþòñÿ ïîçèöèè<br>â ôàéëå, ó êîòîðûõ çàäàíî îáÿçàòåëüíîå ïîëå "Íàèìåíîâàíèå" è íåîáÿçàòåëüíîå "Îïèñàíèå", à îñòàëüíûå ïîëÿ ïóñòûå).<br>Ïåðåä íàèìåíîâàíèåì êàòåãîðèè âû ìîæåòå ñòàâèòü îäèí èëè íåñêîëüêî âîñêëèöàòåëüíûõ çíàêîâ (!),<br>ñ ïîìîùüþ êîòîðûõ óêàçàòü èåðàðõèþ (îäíà êàòåãîðèÿ äîëæíà áûòü âëîæåíà â äðóãóþ è ò.ï.).<br>Êîëè÷åñòâî `!` óêàçûâàåò óðîâåíü êàòåãîðèè â äåðåâå. Ïîäðîáíåå â ïðèìåðå:
+<p><a href="example.csv">Ñêà÷àòü ïðèìåð ïðàéñ-ëèñòà...</a>');
+
+define('ADMIN_EXCEL_UPDATE_DB', '<b>Îáíîâèòü áàçó</b>');
+define('ADMIN_EXCEL_CLEAR_DB', '<b>Î÷èñòèòü áàçó:</b>');
+define('ADMIN_EXCEL_CLEAR_DB_DESC', 'Ïîëíîñòüþ î÷èñòèòü áàçó äàííûõ êàòåãîðèé è òîâàðîâ (â ýòîì ñëó÷àå ìîæíî íå óêàçûâàòü ïóòü ê ïðàéñ-ëèñòó).');
+
+
+define('ADMIN_IMPORT_DESC1', 'Â çàêà÷åíîì ôàéëå îáíàðóæåíû ñëåäóþùèå êîëîíêè.<br>Ñîîòíåñèòå êàæäóþ èç ýòèõ êîëîíîê ñ ïîëåì â áàçå äàííûõ.<br>Â ëåâîé êîëîíêå óêàçàíû íàçâàíèÿ ñòîëáöîâ. Åñëè äëÿ òåêóùåãî ñòîëáöà<br>âûáðàíî "äîáàâèòü êàê äîïîëíèòåëüíûé ïàðàìåòð", òî â êà÷åñòâå íàçâàíèÿ<br>ýòîãî ïàðàìåòðà áóäåò òåêñò, êîòîðûé âû ââåëè â ïðàâîé êîëîíêå.');
+
+define('ADMIN_IMPORT_DESC2', 'Åñëè òîâàð åñòü è â áàçå, è â ôàéëå (èùåòñÿ ñîâïàäåíèå â êîëîíêå èäåíòèôèêàöèè), òî îáíîâèòü èíôîðìàöèþ î íåì.<br>Åñëè æå òîâàð íàéäåí òîëüêî â ôàéëå, òî âíåñòè åãî â áàçó äàííûõ.<br>Èíà÷å - òîâàð åñòü òîëüêî â áàçå äàííûõ - îñòàâèòü åãî áåç èçìåíåíèé.<br>');
+
+
+define('ADMIN_IGNORE', '- èãíîðèðîâàòü ñòîëáåö');
+define('ADMIN_ADD_AS_NEW_PARAMETER', '- äîáàâèòü êàê äîïîëíèòåëüíûé ïàðàìåòð');
+
+
+define('ADMIN_CUSTOMER_TITLE', 'Ïîêóïàòåëü');
+define('CUSTOMER_LOGIN', 'Ëîãèí:');
+define('CUSTOMER_PASSWORD', 'Ïàðîëü:');
+define('CUSTOMER_EMAIL', 'E-mail:');
+define('CUSTOMER_FIRST_NAME', 'Èìÿ:');
+define('CUSTOMER_LAST_NAME', 'Ôàìèëèÿ:');
+define('CUSTOMER_MIDDLE_NAME', 'Îò÷åñòâî:');
+define('CUSTOMER_ZIP', 'Ïî÷òîâûé èíäåêñ:');
+define('CUSTOMER_STATE', 'Îáëàñòü:');
+define('CUSTOMER_COUNTRY', 'Ñòðàíà:');
+define('CUSTOMER_CITY', 'Ãîðîä:');
+define('CUSTOMER_ADDRESS', 'Àäðåñ:');
+define('CUSTOMER_PHONE_NUMBER', 'Òåëåôîí:');
+define('CUSTOMER_SUBSCRIBE_FOR_NEWS', 'Ïîäïèñàòüñÿ íà íîâîñòè');
+define('CUSTOMER_CONFIRM_PASSWORD', 'Ïîäòâåðäèòå ïàðîëü:');
+
+define('ADMIN_RELATED_PRODUCTS_SELECT', 'Ïîæàëóéñòà, âûáåðèòå òîâàð');
+define('ADMIN_SELECTED_PRODUCTS', 'Âûáðàííûå òîâàðû:');
+
+define('ADMIN_PICTURE_NOT_UPLOADED', '(ôîòîãðàôèÿ íå çàêà÷àíà)');
+
+
+//errors
+
+define('ERROR_FAILED_TO_UPLOAD_FILE', '<b><font color=red>Íå óäàëîñü çàêà÷àòü ôàéë(û) íà ñåðâåð. Óáåäèòåñü, ÷òî ó Âàñ åñòü ïðàâà íà ñîçäàíèå ôàéëîâ íà ñåðâåðå</font></b>');
+define('ERROR_CANT_FIND_REQUIRED_PAGE', 'Èçâèíèòå, çàïðàøèâàåìûé äîêóìåíò íå áûë íàéäåí íà ñåðâåðå');
+define('ERROR_CANT_READ_FILE', 'Íå óäàëîñü ïðî÷èòàòü ôàéë');
+define('ERROR_INPUT_PRODUCT_NAME', 'Ïîæàëóéñòà, ââåäèòå íàçâàíèå òîâàðà');
+define('ERROR_FORBIDDEN', 'Ó Âàñ íåò ïðàâ íà ïðîñìîòð ýòîé ñòðàíèöû.');
+define('ERROR_WRONG_PASSWORD', 'Íåâåðíûé ëîãèí è/èëè ïàðîëü');
+define('ERROR_INPUT_LOGIN', 'Ïîæàëóéñòà, ââåäèòå ëîãèí');
+define('ERROR_INPUT_EMAIL', 'Ïîæàëóéñòà, ââåäèòå email');
+define('ERROR_LOGIN_SHOULD_START_WITH_LATIN_SYMBOL', 'Ëîãèí äîëæåí íà÷èíàòüñÿ ñ ëàòèíñêîãî ñèìâîëà');
+define('ERROR_WRONG_PASSWORD_CONFIRMATION', 'Íåâåðíûé ïàðîëü');
+define('ERROR_INPUT_NAME', 'Ïîæàëóéñòà, ââåäèòå Âàøè ÔÈÎ');
+define('ERROR_INPUT_COUNTRY', 'Ïîæàëóéñòà, ââåäèòå ñòðàíó');
+define('ERROR_INPUT_CITY', 'Ïîæàëóéñòà, ââåäèòå íàçâàíèå ãîðîäà');
+define('ERROR_INPUT_ZIP', 'Ïîæàëóéñòà, ââåäèòå ïî÷òîâûé èíäåêñ');
+define('ERROR_INPUT_STATE', 'Ïîæàëóéñòà, ââåäèòå îáëàñòü');
+define('ERROR_INPUT_ADDRESS', 'Ïîæàëóéñòà, ââåäèòå Âàø àäðåñ');
+define('ERROR_INPUT_PHONE', 'Ïîæàëóéñòà, ââåäèòå Âàø íîìåð òåëåôîíà');
+define('ERROR_INPUT_OTCH', 'Ïîæàëóéñòà, ââåäèòå Âàøå îò÷åñòâî');
+define('ERROR_USER_ALREADY_EXISTS', 'Ïîëüçîâàòåëü â óêàçàííûì ëîãèíîì óæå çàðåãèñòðèðîâàí');
+define('ERROR_UPDATE_COLUMN_IS_NOT_SET', 'Íå óêàçàí ñòîëáåö èäåíòèôèêàöèè, îäíîçíà÷íî îïðåäåëþùèé òîâàð');
+
+//questions
+
+define('QUESTION_DELETE_PICTURE', 'Óäàëèòü ôîòîãðàôèþ?');
+define('QUESTION_DELETE_CONFIRMATION', 'Óäàëèòü?');
+
+//emails
+define('EMAIL_DELETE_CUSTOMER_SUBJECT', 'Âàøà ðåãèñòðàöèÿ îòìåíåíà');
+define('EMAIL_FORGOT_PASSWORD_SUBJECT', 'Âàø ïàðîëü');
+define('EMAIL_ADMIN_ORDER_NOTIFICATION_SUBJECT', 'Íîâûé çàêàç!');
+define('EMAIL_CUSTOMER_ORDER_NOTIFICATION_SUBJECT', 'Âàø çàêàç');
+define('EMAIL_MESSAGE_PARAMETERS', 'Content-Type: text/plain; charset="'.DEFAULT_CHARSET.'"');
+define('EMAIL_HELLO', 'Çäðàâñòâóéòå');
+define('EMAIL_SINCERELY', 'Ñ óâàæåíèåì');
+define('EMAIL_THANK_YOU_FOR_SHOPPING_AT', 'Ñïàñèáî çà Âàø âûáîð');
+define('EMAIL_ORDER_WILL_BE_SHIPPED_TO', 'Âàø çàêàç áóäåò äîñòàâëåí ïî àäðåñó');
+define('EMAIL_OUR_MANAGER_WILL_CONTACT_YOU', 'Íàø ìåíåäæåð ñâÿæåòñÿ ñ Âàìè â áëèæàéøåå âðåìÿ.');
+define('EMAIL_YOUVE_MODIFIED_YOUR_DATA_AT', 'Îáíîâëåííûå ðåãèñòðàöèîííûå äàííûå â');
+define('EMAIL_YOUVE_BEEN_REGISTERED_AT', 'Âû óñïåøíî çàðåãèñòðèðîâàëèñü â');
+define('EMAIL_REGISTRATION', 'Ðåãèñòðàöèÿ');
+define('EMAIL_YOUR_REGTRATION_INFO', 'Âàøà ðåãèñòðàöèîííàÿ èíôîðìàöèÿ:');
+define('EMAIL_NEWS_OF', 'Íîâîñòè');
+define('EMAIL_YOUR_PASSWORD', 'Âàø ïàðîëü');
+define('EMAIL_ACCOUNT_CLOSED', 'Âàø àêêàóíò áûë óäàëåí èç áàçû äàííûõ ïî âîëå ìåíåäæåðà! Ñïàñèáî çà ïîêóïêè!');
+
+?>
