7db6c0a97f56c9cc0f425a7c4e9e6c91e00bf507
me-suzy <me.suzana@gmail.com>
2025-08-14 01:10:12 +0300

    Add Unsorted/powerdownload303/pdl303/downloads.php

---
+++ Unsorted/powerdownload303/pdl303/downloads.php
@@ -0,0 +1,26 @@
+<? include("pdl-inc/pdl_header.inc.php"); ?>
+<link href="pdl-admin/style.css" rel="stylesheet" type="text/css">
+<body bgcolor="#000000" text="#FFFFFF">
+<center>
+<table border="0">
+  <tr>
+    <td>
+      <? include("pdl-inc/pdl_stats.inc.php"); ?>
+    </td>
+    <td>
+      <? include("pdl-inc/pdl_top.inc.php"); ?>
+    </td>
+    <td>
+      <? include("pdl-inc/pdl_flop.inc.php"); ?>
+    </td>
+    <td>
+      <? include("pdl-inc/pdl_latest.inc.php"); ?>
+    </td>
+    <td>
+      <? include("pdl-inc/pdl_rated.inc.php"); ?>
+    </td>
+  </tr>
+</table>
+<hr>
+<? include("pdl-inc/pdl_downloads.inc.php"); ?>
+</center>
