1ee47155804b35a6b12e5a355b728d9ff81fda49
me-suzy <me.suzana@gmail.com>
2025-08-14 02:48:55 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/addfav.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/addfav.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  =  "No tienes autorizaci&oacute;n para ver este tema, por lo tanto no puedes incluirla en tu lista de favoritos o recordatorios.";
+$ubbt_lang['MAIN_ONLY']  =  "&Uacute;nicamente puedes a&ntilde;adir temas principales a tu lista de favoritos o de recordatorios.";
+$ubbt_lang['DUP_ENTRY']  =  "Esto ya fu&eacute; a&ntilde;adido anteriormente a tu lista de favoritos/recordatorios.";
+$ubbt_lang['ENTRY_IN']  =  "Mensaje/Tema a&ntilde;adido";
+$ubbt_lang['FAV_CONFIRM']  =  "El mensaje/tema que has seleccionado acaba de ser a&ntilde;adido a la lista.  Si lo has a&ntilde;adido a la lista de favoritos podr&aacute;s verlo en en 'Panel de Control' hasta que decidas quitarlo.  Si has decidido a&ntilde;adirlo a la lista de Recordatorios continuar&aacute; en la lista del Panel de Control hasta que contestes al mensaje.";
+$ubbt_lang['FORUM_RETURN']  =  "Volver r&aacute;pidamente al foro.";
+?>
