07748c5d6728821bfa9fcf754c901aad5a76910b
me-suzy <me.suzana@gmail.com>
2025-08-14 00:15:14 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/admin/users.menu.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/admin/users.menu.php
@@ -0,0 +1,44 @@
+<?#//v.1.0.0
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+
+
+?>
+<TABLE WIDTH=100% CELLPADDING=2 CELLSPACING="0">
+	<TR> 
+		<TD WIDTH="3%"><B></B></TD>
+		<TD WIDTH="97%"><B><FONT FACE="Tahoma, Verdana" SIZE="2"> 
+			<? print $MSG_365; ?>
+			</FONT></B></TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"><FONT FACE="Tahoma, Verdana" SIZE="2"><IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./newadminuser.php" CLASS="links"> 
+			<? print $MSG_367; ?>
+			</A></FONT></TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"> 
+			<P><FONT FACE="Tahoma, Verdana" SIZE="2"> <IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./adminusers.php" CLASS="links"> 
+				<? print $MSG_525; ?>
+				</A></FONT></P>
+		</TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%"><B></B></TD>
+		<TD WIDTH="97%"><B><FONT FACE="Tahoma, Verdana" SIZE="2"> 
+			<? print $MSG_366; ?>
+			</FONT></B></TD>
+	</TR>
+	<TR> 
+		<TD WIDTH="3%">&nbsp;</TD>
+		<TD WIDTH="97%"> <FONT FACE="Tahoma, Verdana" SIZE="2"> <IMG SRC="./images/ball.gif" WIDTH="12" HEIGHT="12"><A HREF="./listusers.php" CLASS="links"> 
+			<? print $MSG_045; ?>
+			</A></FONT> </TD>
+	</TR>
+</TABLE>
