3736122c83e2c910311460e9bed3bd0737cb43db
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:52 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/viewmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/viewmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['ABOUT']  = 	"Ãö©ó";
+$ubbt_lang['RR_YOUR_MESS']  = 	"±z±Hµ¹";
+$ubbt_lang['RR_READ']  = 	"ªº«H¥ó¡A¹ï¤è¤w¸gÅª¹L¤F¡C";
+$ubbt_lang['RR_NOTIFY']  = 	"±z¤w¸g¦¬¨ì¤F¹ï¤èÅª¹L«H¥óªºÅª«H¦¬¾Ú¤F";
+$ubbt_lang['READ_RECEIPT']  = 	"¨t²Î°T®§¡G¦¬¥óªÌ¤w¸gÅª¹L«H¤F";
+$ubbt_lang['TEXT_REC']  = 	"¦¬«H¤é´Á";
+$ubbt_lang['SENT']  = 	"±H«H®É¶¡";
+$ubbt_lang['DELETE_MESS']  = 	"§R°£³o½g¤å³¹";
+$ubbt_lang['REPLY_MESS']  = 	"¦^À³³o½g¤å³¹";
+$ubbt_lang['RETURN_START']  = 	"¦^¨ì±zªº­º­¶";
+?>
