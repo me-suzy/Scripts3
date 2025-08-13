5f493a564d3fc966d0a26f1e9e43e991d81278b5
me-suzy <me.suzana@gmail.com>
2025-08-14 02:48:40 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"Çàïðîñ ïàðîëÿ";
+$ubbt_lang['PASS_REQ_BOD1']  = 	"Someone from the ip address";
+$ubbt_lang['PASS_REQ_BOD2']  = 	"requested a temporary password for the Username";
+$ubbt_lang['PASS_REQ_BOD3']  = 	"for the website '{$config['title']}'.  This is a temporary password that can be used if you have forgotten your original password.  If you did not request this, please ignore this message.  The temporary password for this Username is --";
+$ubbt_lang['PASS_MAILED']  = 	"Ïàðîëü îòïðàâëåí íà àäðåñ E-mail, óêàçàííûé ïðè ðåãèñòðàöèè.";
+$ubbt_lang['PASS_MAILED2']  = 	"A temporary password for this user has been mailed.";
+$ubbt_lang['NO_EMAIL']  = 	"Íå óäà¸òñÿ íàéòè àäðåñ  E-mail";
+?>
