4b42d38cc026c48b8dc74c0b4330bd7c259a38f7
me-suzy <me.suzana@gmail.com>
2025-08-14 02:30:36 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/approvepost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/approvepost.php
@@ -0,0 +1,5 @@
+<?
+$ubbt_lang['REPLY_SUB']  = 	"Der er kommet svar på dit indlæg";
+$ubbt_lang['REPLY_BOD']  = 	"replied to your post at the site: .";
+$ubbt_lang['APPR_HEAD']  = 	"Indlægget er godkendt.";
+?>
