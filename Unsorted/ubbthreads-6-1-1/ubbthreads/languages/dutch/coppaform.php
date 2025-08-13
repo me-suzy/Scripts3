034ee61c47ecacbc621789d04f7e3e45fae29073
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:22 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/coppaform.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/coppaform.php
@@ -0,0 +1,21 @@
+<?
+
+$ubbt_lang['PARENT_APP'] = "Toestemming van een ouder of voogd benodigd";
+$ubbt_lang['PARENT_APP_2'] = "Jouw ouder of voogd moet jouw aanmelding goedkeuren.  Je kunt niet registreren totdat wij deze goedkeuring hebben ontvangen.  Om een account aan te maken kun je deze pagina afdrukken en onderstaande instructies volgen.";
+$ubbt_lang['MORE_INFO'] = "Voor meer informatie neem je contact op met <a href=\"mailto:{$config['emailaddy']}\">{$config['emailaddy']}</a>";
+$ubbt_lang['LOGIN_NAME'] = "Gebruikersnaam";
+$ubbt_lang['LOGIN_NAME2'] = "Je kunt het beste meerdere gebruikersnamen opgeven zodat wij er zelf een kunnen kiezen als jouw eerste voorkeur al bezet is..";
+$ubbt_lang['YOUR_EMAIL'] = "Email adres";
+$ubbt_lang['BIRTHDATE'] = "Geboortedatum";
+$ubbt_lang['PARENT_INST'] = "Instructies voor de ouder of voogd";
+$ubbt_lang['PARENT_INST2'] = "Onderteken onderstaand formulier en stuur het naar ons terug.";
+$ubbt_lang['COPPA_INST'] = "COPPA Instructies";
+$ubbt_lang['AGREE'] = "ACCOORD: Ik heb de informatie die mijn kind heeft doorgegeven begrepen en uw Privacy Policy doorgeleze. Ik begrijp ook dat de profiel gegevens naderhand met deze registratiegegevens kunnen worden aangepast.";
+$ubbt_lang['FULLNAME'] = "Volledige naam van de ouder/voogd";
+$ubbt_lang['SIGNATURE'] = "Handtekening";
+$ubbt_lang['RELATION'] = "Relatie tot het kind";
+$ubbt_lang['TELE'] = "Telefoon- of faxnummer";
+$ubbt_lang['PARENT_EMAIL'] = "Email adres van de ouder/voogd";
+$ubbt_lang['DATE'] = "Datum";
+$ubbt_lang['REG_DATE'] = "Registratie datum";
+$ubbt_lang['QUESTIONS'] = "Vragen? Email";
