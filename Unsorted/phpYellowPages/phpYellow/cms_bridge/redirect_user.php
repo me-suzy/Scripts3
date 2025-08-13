380c62467a58aead44e1552d88c3a3d78b114276
me-suzy <me.suzana@gmail.com>
2025-08-14 01:06:08 +0300

    Add Unsorted/phpYellowPages/phpYellow/cms_bridge/redirect_user.php

---
+++ Unsorted/phpYellowPages/phpYellow/cms_bridge/redirect_user.php
@@ -0,0 +1,22 @@
+<?
+  Feel free to modify this file to tell your users whatever
+  you want.
+
+?>
+<!-- redirect_user.php -->
+
+ <h2> Ooopps </h2>
+ We are running phpBB inside a portal system that
+ handles user name registration.  Registration must be handled
+ from the main portal.
+ <p>
+ Please user your browser "back" button to return to the
+ discussion forums.
+ 
+ <? 
+  print "
+  <p>Debug stuff:
+  <br> user = $cms_user_cc, port= $cms_bridge_cc"; 
+ ?>
+
+ 
