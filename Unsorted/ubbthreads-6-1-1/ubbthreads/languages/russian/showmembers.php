ec17e5c6d7c20f340b65206f2eda6cef06e3c66e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:48:35 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/showmembers.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/showmembers.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['USER_STATUS']  = 	"Ñòàòóñ";
+$ubbt_lang['TEXT_HOME']  = 	"WEB-Ñòðàíèöà";
+$ubbt_lang['TOTAL_POSTS']  = 	"Âñåãî ñîîáùåíèé";
+$ubbt_lang['PROF_ICQ']  = 	"Íîìåð ICQ";
+$ubbt_lang['REGGED_ON']  = 	"Çàðåãèñòðèðîâàí(à)";
+$ubbt_lang['PAGE_TEXT']  = 	"Pages";
+?>
