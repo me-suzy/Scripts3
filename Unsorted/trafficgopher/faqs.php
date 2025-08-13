b87a42a7477cbedaf96b7672493a0b6e5394fc92
me-suzy <me.suzana@gmail.com>
2025-08-14 02:09:43 +0300

    Add Unsorted/trafficgopher/faqs.php

---
+++ Unsorted/trafficgopher/faqs.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+ob_start();
+include("includes/header.php");
+?>
+<b>FAQ's</b><br><br>
+<p align=left>
+<b>Q. Can I use <?php print $sitename; ?> to track all the services listed or just one?</b><br>
+A. You may use all the counting services offered for your site, not just one like other services.
+</p>
+<p align=left>
+<b>Q. Do I require any special skills to setup your service on my website?</b><br>
+A. No, even the most inexperienced web user could setup our tracking system on their site due to the full automation of the software we use
+</p>
+<p align=left>
+<b>Q. How long will it take to setup the software on my site?</b><br>
+A. After signing up it should take less than 5 minutes to get the tracking system working on your site, you will only have to enter the code given to you (automatically by the software) into your index page and then re-upload and the software will begin counting.
+</p>
+<p align=left>
+<b>Q. Will my personal info kept private?</b><br>
+A. Yes, all personal info is used only by <?php print $sitename; ?> and will not be sold or given to anyone for an reason
+</p>
+<p align=left>
+<b>Q. Will I be obligated to <?php print $sitename; ?> for any period of time?</b><br>
+A. No, you may cancel anytime you no longer need our services
+</p>
+<p align=left>
+<b>Q. Does plan A and plan B offer the same features?</b><br>
+A. Yes, plan A is cheaper only because a small link to our site will be displayed below the counter on your site where plan B there is no link.
+</p>
+<p align=left>
+<b>Q. If I have a question that is not listed above what should I do?</b><br>
+A. You can use the contact us link to send a email to us, we take pride in customer service and will get a response to you A.S.A.P
+</p>
+
+<BR><BR><BR><!------------------------------------------------------------------------------------------>
+<?php
+include("includes/footer.php");
+?>
