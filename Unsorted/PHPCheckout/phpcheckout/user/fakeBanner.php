dd90488206f5b3aaa67b59688e86d27e2dd0b2cc
me-suzy <me.suzana@gmail.com>
2025-08-14 00:35:02 +0300

    Add Unsorted/PHPCheckout/phpcheckout/user/fakeBanner.php

---
+++ Unsorted/PHPCheckout/phpcheckout/user/fakeBanner.php
@@ -0,0 +1,21 @@
+<!-- start of fakeBanner.php -->
+<table align="center" width="468" height="60" border=0 bgcolor="salmon">
+<tr>
+	<td align="center">
+		<font color="white">
+		<b><i>Take a look at the ...</i></b>
+		</font>
+	</td>
+	<td align="center">
+		<font color="black">
+		<b><i>FEATURE PRODUCT!</i></b>
+		</font>
+	</td>
+	<td bgcolor="green" style="color:black;" valign="middle" align="center"><br>
+		<form>
+			<input type="button" name="goToFeatureProductButton" value="GO" onclick='location.href="featureProduct.php"'>
+		</form>
+	</td>
+</tr>
+</table>
+<!-- end of fakeBanner.php -->
