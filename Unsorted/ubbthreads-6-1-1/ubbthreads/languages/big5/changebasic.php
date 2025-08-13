5de073ed0f1ff0ba7e720a646e22748ca1adc68a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:30 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/changebasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/changebasic.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['PIXEL_TOO_BIG']="±zªº¹Ï¤ù¶W¹L ({$theme['PictureWidth']}x{$theme['PictureHeight']}) ¹³¯À­­¨î¡G";
+$ubbt_lang['FILE_TOO_BIG']="¹Ï§Î¤£¯à¤j©ó {$config['avatarsize']} bytes.";
+$ubbt_lang['BAD_PIC']  =       "¹Ï¤ù®æ¦¡¤£¹ï¡C";
+$ubbt_lang['PASS_MATCH']  = 	"±K½X¤£¥¿½T¡C";
+$ubbt_lang['PASS_TOO_LONG']  = 	"±K½Xªø«×¥²¶·¤¶©ó4¨ì20­Ó¦r¤¸¡C";
+$ubbt_lang['HOME_TOO_LONG']  = 	"±zªººô­¶Äæ¦ì¤Óªø¤F¡C";
+$ubbt_lang['OCC_TOO_LONG']  = 	"±zªºÂ¾·~Äæ¦ì¤Óªø¤F¡C";
+$ubbt_lang['HOBB_TOO_LONG']  = 	"±zªº¿³½ì¼g±o¤Óªø¤F¡C";
+$ubbt_lang['LOC_TOO_LONG']  = 	"±zªº©Ò¦b¦a¤Óªø¤F¡C";
+$ubbt_lang['SIG_TOO_LONG']  = 	"±zªºÃ±¦WÀÉ¤Óªø¤F¡C";
+$ubbt_lang['BIO_TOO_LONG']  = 	"±zªº¦Û§Ú¤¶²Ð¤Óªø¤F¡C";
+$ubbt_lang['BASIC_MOD']  = 	"±zªº­Ó¤HÀÉ®×¤w¸g§ó§ï¤F¡C";
+?>
