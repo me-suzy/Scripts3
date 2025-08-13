2edcea94836d10a926033e344c9d1e1eabd1b5a2
me-suzy <me.suzana@gmail.com>
2025-08-14 02:22:13 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/notifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/notifymod.php
@@ -0,0 +1,109 @@
+<?
+/*
+# UBB.threads, Version 6
+# Official Release Date for UBB.threads Version6: 06/05/2002
+
+# First version of UBB.threads created July 30, 1996 (by Rick Baker).
+# This entire program is copyright Infopop Corporation, 2002.
+# For more info on the UBB.threads and other Infopop
+# Products/Services, visit: http://www.infopop.com
+
+# Program Author: Rick Baker.
+
+# You may not distribute this program in any manner, modified or otherwise,
+# without the express, written written consent from Infopop Corporation.
+
+# Note: if you modify ANY code within UBB.threads, we at Infopop Corporation
+# cannot offer you support-- thus modify at your own peril :)
+# ---------------------------------------------------------------------------
+*/
+
+// Require the library
+   require ("./main.inc.php");
+
+// -----------------
+// Get the user info
+   $userob = new user;
+   $user = $userob -> authenticate("U_Email,U_Groups");
+   $Username = $user['U_Username'];
+   $html = new html;
+
+// -----------------------------------------
+// require the language file for this script
+   require "languages/${$config['cookieprefix']."w3t_language"}/notifymod.php";
+
+   if (!$Username) {
+      $html -> not_right($ubbt_lang['NO_AUTH'],$Cat);
+   }
+
+// -----------------------------------------------------------------------------
+// Once and a while if people try to just put a number into the url, lets trap it
+   if (!$Number) {
+      $html -> not_right($ubbt_lang['NO_POST'],$Cat);
+   }
+
+// ------------------------------------------------------
+// Let's find out what Groups we are dealing with
+   $Board_q = addslashes($Board);
+   $Groups = $user['U_Groups'];
+   if (!$Groups) { $Groups = "-4-"; }
+
+// --------------------------------------------------------------
+// We need to format a SQL query to see what boards this user can
+// view
+   $Grouparray = split("-",$Groups);
+   $gsize = sizeof($Grouparray);
+   $groupquery = "AND (";
+   $g = 0;
+   for ($i=0; $i<=$gsize;$i++) {
+      if (!preg_match("/[0-9]/",$Grouparray[$i])) { continue; };
+      $g++;
+      if ($g > 1) {
+         $groupquery .= " OR ";
+      }
+      $groupquery .= "Bo_Read_Perm LIKE '%-$Grouparray[$i]-%'";
+   }
+   $groupquery .= ")";
+
+
+// --------------------------------------------
+// Let's find out if they should be here or not
+   $query = "
+    SELECT Bo_Title,Bo_Write_Perm,Bo_CatName,Bo_Cat
+    FROM   {$config['tbprefix']}Boards
+    WHERE  Bo_Keyword = '$Board_q'
+    $groupquery
+   ";
+   $sth = $dbh -> do_query($query);
+   list($title,$CanWrite,$CatName,$CatNumber) = $dbh -> fetch_array($sth);
+
+   if (!$title) {
+      $html -> not_right($ubbt_lang['BAD_GROUP'],$Cat);
+   }
+
+// --------------------------------------------------
+// Now let's see if the mod has already been notified
+   $query = "
+    SELECT M_Number
+    FROM   {$config['tbprefix']}ModNotify
+    WHERE  M_Number = '$Number'
+   ";
+   $sth = $dbh -> do_query($query);
+   list($check) = $dbh -> fetch_array($sth); 
+   if ($check) {
+      $html -> not_right("{$ubbt_lang['NO_NOTIFY']}",$Cat);
+   }
+ 
+// ---------------
+// Send the header
+   $html -> send_header($ubbt_lang['NOTIFY_MOD'],$Cat,0,$user);
+
+	if (!$debug) { 
+   	include("$thispath/templates/$tempstyle/notifymod.tmpl"); 
+	}
+
+// -------------
+// Send a footer
+   $html -> send_footer();
+
+?>
