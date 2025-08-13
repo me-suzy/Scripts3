2e7c3e38f299feb09ee2269f62794cf36ba7c444
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:57 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/search.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/search.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['SEARCH_INFO']  = 	"Completati datele urmatoare pentru a cauta in forumuri.";
+$ubbt_lang['SEARCH_FORUM']  = 	"Forum de cautat";
+$ubbt_lang['ALL_FORUMS']  = 	"Toate forumurile";
+$ubbt_lang['SEARCH_WORDS']  = 	"Cuvinte de cautat";
+$ubbt_lang['SEARCH_OPT']  = 	"Optiuni de cautare";
+$ubbt_lang['SEARCH_ENT']  = 	"Toata fraza";
+$ubbt_lang['SEARCH_AND']  = 	"Si";
+$ubbt_lang['SEARCH_OR']  = 	"Sau";
+$ubbt_lang['SEARCH_USER']  = 	"Dupa Nume";
+$ubbt_lang['SEARCH_DATE']  = 	"Interval de timp";
+$ubbt_lang['SEARCH_1DAY']  = 	"Ultima zi";
+$ubbt_lang['SEARCH_2DAY']  = 	"Ultimele 2 zile";
+$ubbt_lang['SEARCH_1WEEK']  = 	"Ultima saptamina";
+$ubbt_lang['SEARCH_2WEEK']  = 	"Ultimele 2 saptamini";
+$ubbt_lang['SEARCH_3WEEK']  = 	"Ultimele 3 saptamini";
+$ubbt_lang['SEARCH_1MONTH']  = 	"Ultima luna";
+$ubbt_lang['SEARCH_3MONTH']  = 	"Ultimele 3 luni";
+$ubbt_lang['SEARCH_6MONTH']  = 	"Ultimele 6 luni";
+$ubbt_lang['SEARCH_1YEAR']  = 	"Ultimul an";
+$ubbt_lang['SEARCH_ALLPOST']  = 	"Toate mesajele";
+$ubbt_lang['SEARCH_NUM']  = 	"Numar de rezultate pe pagina (ignorat cind se cauta in toate forumurile)";
+?>
