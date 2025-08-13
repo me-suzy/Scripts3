89d6fc478d854072d4df55bf02cdd70c2d14761c
me-suzy <me.suzana@gmail.com>
2025-08-14 02:30:04 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/sendprivate.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/sendprivate.php
@@ -0,0 +1,13 @@
+<?
+$ubbt_lang['NOPRIVS']   = "Private messages are disabled.";
+$ubbt_lang['MESS_COPY']  = 	"ÎÒÏëÒª±£ÁôÎÒÕâ·âÐÅ×÷Îª¼Ä¼þ±¸·Ý";
+$ubbt_lang['MESS_RECEIPT']  = 	"µ±¶Ô·½¶Á¹ýÎÒµÄÐÅºó£¬ÎÒÏ£ÍûÏµÍ³·¢Ò»·âÐÅ¸øÎÒ";
+$ubbt_lang['MESS_PREVIEW']  = 	"ÔÚ¼Ä³öÈ¥ÐÅ¼þÖ®Ç°£¬ÎÒÏ£ÍûÄÜÔ¤ÀÀÎÒµÄÐÅ¼þ";
+$ubbt_lang['ADD_BOOK']  = 	"¼ÓÈëÎÒË½ÈËµÄÍ¨Ñ¶Â¼µ±ÖÐ";
+$ubbt_lang['USE_BOOK']  = 	"»òÊÇ´ÓÎÒµÄÍ¨Ñ¶Â¼ÖÐÌôÑ¡Ò»ÈË";
+$ubbt_lang['CHOOSE_ADD']  = 	"Çë´ÓÍ¨Ñ¶Â¼ÖÐÑ¡ÔñÒ»¸öÃû×Ö";
+$ubbt_lang['NO_LOGGED']  = 	"±§Ç¸£¬Äú±ØÐëÒªµÇÈë²ÅÄÜ¼ÄË½ÈËÐÅ¡£";
+$ubbt_lang['NO_PRIVATE'] =	"±§Ç¸£¬ÕâÎ»Ê¹ÓÃÕß²»½ÓÊÜË½ÈËÐÅ¼þ¡£";
+$ubbt_lang['PRIV_BODY2'] =	"ÇëÌîÐ´ÒÔÏÂµÄÄÚÈÝ¼ÄÐÅ¸øÊÕ¼þÕß¡£¶Ô·½ÏÂ´ÎµÇÈëÏµÍ³Ê±¾Í»áÊÕµ½ÄúµÄÐÅ¼þÁË¡£";
+$ubbt_lang['TEXT_DELETE']  = 	"É¾³ý";
+?>
