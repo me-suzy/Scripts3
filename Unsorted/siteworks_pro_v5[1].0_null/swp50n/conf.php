64fa48fbee5225eee89cadf4a0547e07186a0a42
me-suzy <me.suzana@gmail.com>
2025-08-14 01:43:33 +0300

    Add Unsorted/siteworks_pro_v5[1].0_null/swp50n/conf.php

---
+++ Unsorted/siteworks_pro_v5[1].0_null/swp50n/conf.php
@@ -0,0 +1,24 @@
+<?php
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//   Program Name         : SiteWorks Professional                           //
+//   Release Version      : 5.0                                              //
+//   Program Author       : SiteCubed Pty. Ltd.                              //
+//   Supplied by          : CyKuH [WTN]                                      //
+//   Nullified by         : CyKuH [WTN]                                      //
+//   Packaged by          : WTN Team                                         //
+//   Distribution         : via WebForum, ForumRU and associated file dumps  //
+//                                                                           //
+//                       WTN Team `2000 - `2002                              //
+///////////////////////////////////////////////////////////////////////////////
+	/*
+
+		Do not modify the contents of this file without talking to SiteCubed support first.
+		Modifying this file could stop SiteWorks Professional from operating correctly
+		/$admindir/includes/php/variables.php
+
+	*/
+
+	$admindir = 'admin';
+
+?>
