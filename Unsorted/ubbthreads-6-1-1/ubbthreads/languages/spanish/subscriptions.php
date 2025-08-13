a4f6e270b7a801fedd87f693ac4de28c987bbf2e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:13 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/subscriptions.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/subscriptions.php
@@ -0,0 +1,9 @@
+<? 
+$ubbt_lang['SUB_INTRO']      = "Daily digest of";
+$ubbt_lang['SUB_DESC']      = "Digest of yesterday's posts to";
+$ubbt_lang['SUB_DESC2']      = "To unsubscribe to this digest or reply to a post visit {$config['phpurl']}/ubbthreads.php";
+$ubbt_lang['SUBJECT_TEXT']   = "Subject";
+$ubbt_lang['POSTER_TEXT']    = "Poster ";
+$ubbt_lang['POSTON_TEXT']    = "Date   ";
+
+?>
