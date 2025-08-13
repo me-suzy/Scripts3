80153e20f6213f31a9bc51ed365e6e9b861caf56
me-suzy <me.suzana@gmail.com>
2025-08-14 00:16:04 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/fonts.inc.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/includes/fonts.inc.php
@@ -0,0 +1,16 @@
+<?#//v.1.0.0
+
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+
+
+	$FONTS = array("1" => "Verdana,Arial,Helvetica",
+				   "2" => "Tahoma,Verdana,Arial",
+				   "3" => "Times New Roman,Times,serif",
+				   "4" => "Courier,Courier New"
+				  );
+?>
