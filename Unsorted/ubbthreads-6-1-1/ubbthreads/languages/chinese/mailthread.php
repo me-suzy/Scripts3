411c2a857aa0785355d860bbb530152890958894
me-suzy <me.suzana@gmail.com>
2025-08-14 02:29:43 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/mailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/mailthread.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['MAIL_FRIEND'] =	"·ÖÏíºÃÓÑ";
+$ubbt_lang['FRIEND_HEAD'] =	"ÇëÌîÈëÊÕÐÅÈËµÄ Email ÐÅÏä";
+$ubbt_lang['SENT_FROM1'] =	"Õâ·âÐÅ½«»áÒÔÄú×¢²áµÄ Email ¼Ä³ö";
+$ubbt_lang['SENT_FROM2'] =	"Ê¹ÓÃÕâ¸öÃû³Æ";
+$ubbt_lang['USE_REAL'] =	"¼ÙÈçÄúÏëÓÃ×Ô¼ºµÄÕæÃû£¬ÇëÔÚÕâÀïÌîÐ´£º";
+$ubbt_lang['WHAT_EMAIL'] =	"ÊÕÐÅÈËµÄ Email ÐÅÏä£º";
+$ubbt_lang['SEND_IT'] =	"ËÍ³ö£¡";
+?>
