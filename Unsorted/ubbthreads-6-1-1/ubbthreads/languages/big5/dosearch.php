c5613ef03790eb04ad924fa11c4d3def4bb6476b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:49 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/dosearch.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_WORDS']  = 	"±z¥²¶·¿é¤JÃöÁä¦r¤~¯à·j´M¡C";
+$ubbt_lang['SEARCH_RES']  = 	"·j´Mµ²ªG";
+$ubbt_lang['PREV_PAGE']  = 	"«e¤@­¶";
+$ubbt_lang['MAIN_SEARCH']  = 	"·j´M¤ÞÀº­¶­±";
+$ubbt_lang['NEXT_PAGE']  = 	"¤U¤@­¶";
+$ubbt_lang['NO_MATCH']  = 	"©êºp¡A§Ú­Ì§ä¤£¨ì²Å¦X±z±ø¥óªº¤å³¹¡C";
+?>
