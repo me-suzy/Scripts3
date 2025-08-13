3f099a016f78897354ca1d6e7852d762d77acd29
me-suzy <me.suzana@gmail.com>
2025-08-14 01:12:38 +0300

    Add Unsorted/ppntopsites/style.php

---
+++ Unsorted/ppntopsites/style.php
@@ -0,0 +1,14 @@
+<?php
+$bgcolour = "navy";
+$headfont = "Helvetica, Tahoma, Georgia, Arial, serif";
+$fonthcolour = "Black";
+$tablehbg = "orange";
+$font = "Tahoma, Arial, Helvetica, Georgia, sans-serif";
+$fontcolour = "silver";
+$lcolour = "white";
+$vcolour = "white";
+$acolour = "orange";
+$hcolour = "orange";
+$tablewidth = "75%";
+$tablebg = "black";
+?>
