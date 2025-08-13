34d9c1f3805302055071ad890216a4cb36da3c10
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:28 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/ubbthreads.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/ubbthreads.php
@@ -0,0 +1,16 @@
+<?
+$ubbt_lang['NEW_MEM'] = "Wij verwelkomen ons nieuwste lid,";
+$ubbt_lang['VIEW_PAST']  =     "Bekijk recente berichten";
+$ubbt_lang['PAST24']  =        "van de laatste 24 uur";
+$ubbt_lang['PAST48']  =        "van de laatste 48 uur";
+$ubbt_lang['PAST7']  =         "van de afgelopen week";
+$ubbt_lang['THREAD_TEXT']  = 	"Onderwerpen";
+$ubbt_lang['OFFSET_TIME']  = 	"De aangepaste datum is:";
+$ubbt_lang['REGED_USERS']  = 	"Geregistreerde leden.";
+$ubbt_lang['WEL_PRIV_NO']  =   "<a href=\"{$config['phpurl']}/newuser.php\">Registreer</a> een eigen account a.u.b.";  
+$ubbt_lang['POSTS_TEXT']  = 	"Berichten";
+$ubbt_lang['LAST_POST']  = 	"Laatste bericht";
+$ubbt_lang['NEW_BOARD']  = 	"Nieuw forum";
+$ubbt_lang['NEW_TEXT']  = 	"nieuw";
+$ubbt_lang['NOT_APPROVED']  = 	"n.b.";
+?>
