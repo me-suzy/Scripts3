5d089abcf3cbf42bca501ec6f9605d2e6248da71
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:17 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/search.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/search.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['SEARCH_INFO']  = 	"Töltse ki a kerséshez szükséges mezõket.";
+$ubbt_lang['SEARCH_FORUM']  = 	"Hol keressen";
+$ubbt_lang['ALL_FORUMS']  = 	"Minden fórum";
+$ubbt_lang['SEARCH_WORDS']  = 	"A keresett szó -";
+$ubbt_lang['SEARCH_OPT']  = 	" Keresési opciók";
+$ubbt_lang['SEARCH_ENT']  = 	"Teljes kifejezés";
+$ubbt_lang['SEARCH_AND']  = 	"És";
+$ubbt_lang['SEARCH_OR']  = 	"Vagy";
+$ubbt_lang['SEARCH_USER']  = 	"Név szerint";
+$ubbt_lang['SEARCH_DATE']  = 	"Dátum határ";
+$ubbt_lang['SEARCH_1DAY']  = 	"1 napon belül";
+$ubbt_lang['SEARCH_2DAY']  = 	"2 napon belül";
+$ubbt_lang['SEARCH_1WEEK']  = 	"1 héten belül";
+$ubbt_lang['SEARCH_2WEEK']  = 	"2 héten belül";
+$ubbt_lang['SEARCH_3WEEK']  = 	"3 héten belül";
+$ubbt_lang['SEARCH_1MONTH']  = 	"1 hónapon belül";
+$ubbt_lang['SEARCH_3MONTH']  = 	"3 hónapon belül";
+$ubbt_lang['SEARCH_6MONTH']  = 	"6 hónapon belül";
+$ubbt_lang['SEARCH_1YEAR']  = 	"1 éven belül";
+$ubbt_lang['SEARCH_ALLPOST']  = 	"Minden üzenet";
+$ubbt_lang['SEARCH_NUM']  = 	"Keresési eredmények száma egy lapon ( Figyelmen kívül hagyva ha Minden fórumon keres)";
+?>
