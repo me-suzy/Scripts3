8fad1c61fe518351c7ad8b7f2b05c7f62ff10e93
me-suzy <me.suzana@gmail.com>
2025-08-14 00:31:08 +0300

    Add Unsorted/PHPCheckout/phpcheckout/cookiesAbout.php

---
+++ Unsorted/PHPCheckout/phpcheckout/cookiesAbout.php
@@ -0,0 +1,13 @@
+<?php
+echo"<table class=\"favcolor\" style=\"border-style:groove;\" width=\"60%\"><tr><td style=\"font-size:xx-small;\">\n";
+echo"<b>About Cookies</b>\n";
+echo"<ul>\n";
+echo"<li>you requested to be remembered</li>\n";
+echo"<li>a cookie was set to remember you by</li>\n";
+echo"<li>a cookie is a piece of digital information stored on your computer and containing simple information</li>\n";
+echo"<li>the name of the cookie is <i>customeridCookie</i> and the value is $customerid</li>\n";
+echo"<li>the cookie automatically expires in one year</li>\n";
+echo"<li>our domain is the only website which can retrieve that cookie</li>\n";
+echo"<li>we will use that cookie to help you log in to your account</li>\n";
+echo"</td></tr></table>\n";
+?>
