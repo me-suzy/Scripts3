f4db26865461e138cafe032f38ddd9a5c8c6daf2
me-suzy <me.suzana@gmail.com>
2025-08-14 02:31:24 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/newpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/newpost.php
@@ -0,0 +1,17 @@
+<?
+$ubbt_lang['CHOOSE_NAME']  = 	"Anonymous postings are allowed, so you may choose any unregistered name to post under.";
+$ubbt_lang['READ_PERM']  = 	"You have read only privileges for this forum";
+$ubbt_lang['MAKENEW_HEAD']  = 	"Skriv et nyt indlæg";
+$ubbt_lang['FILL_FORM']  = 	"Udfyld felterne herunder for at skrive et indlæg til forumet.";
+$ubbt_lang['NO_HTML']  = 	"Det er ikke muligt at bruge HTML i dette forum.";
+$ubbt_lang['YES_HTML']  = 	"Det er muligt at bruge HTML, når du skriver indlæg til dette forum.";
+$ubbt_lang['NO_MARKUP']  = 	"Det er ikke muligt at bruge tekstformatering i dette forum.";
+$ubbt_lang['YES_MARKUP']  = 	"Tekstformatering er muligt, så det er tilladt at bruge";
+$ubbt_lang['MARK_POSTS']  = 	"tekstformatering i dine indlæg";
+$ubbt_lang['POST_ICON']  = 	"Ikon til indlægget";
+$ubbt_lang['POST_TEXT']  = 	"Indlæg";
+$ubbt_lang['SPELL_CHECK']  = 	"Stavekontrol";
+$ubbt_lang['DO_EMAIL']  = 	"Send alle svar til min e-postadresse";
+$ubbt_lang['DO_PREVIEW']  = 	"I want to preview my post";
+$ubbt_lang['CAN_ATTACH']  = 	"and/or attach a file";
+?>
