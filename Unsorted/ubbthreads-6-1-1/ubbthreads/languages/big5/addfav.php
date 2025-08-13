cf5b43441de97e5d672b8b5ab0494635b285809a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:20 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/addfav.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/addfav.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  =  "±z¨S¦³Åv¤O¾\\Åª³o½g¤å³¹¡A¦]¦¹±z¤£¯à±N¨ä¥[¨ì¡u§Úªº³Ì·R¡v·í¤¤";
+$ubbt_lang['MAIN_ONLY']  =  "§A¥u¯à§â¯S©w¸ÜÃD¥[¤J¨ì­Ó¤H¦¬ÂÃ§X©Î³Æ§Ñ¿ý¸Ì";
+$ubbt_lang['DUP_ENTRY']  =  "³o½g¤å³¹¤w¸g¦b§Aªº­Ó¤H¦¬ÂÃ§X/³Æ§Ñ¿ý¸Ì¤F¡C";
+$ubbt_lang['ENTRY_IN']  =  "¤å³¹¤w¸g¥[¤J§Aªº­Ó¤H¦¬ÂÃ§X";
+$ubbt_lang['FAV_CONFIRM']  =  "§A­è­è¿ï¾Üªº¤å³¹¤w¸g¥[¤J¨ì§Aªº­Ó¤H³Æ§Ñ¿ý¸Ì¤F¡A§A¥i¥H±q¡u­Ó¤H±±¨î¥x¡v¤¤²¾¥X©ÎÅª¨ú¡C¦pªG§A§â¥L¥[¤J¨ì³Æ§Ñ¿ý¡A·í§A¦^«Hµ¹¦¬«H¤H®É¡A±N·|¦Û°Ê²¾°£¡C"; 
+$ubbt_lang['FORUM_RETURN']  =  "²{¦b¦^¨ì°Q½×°Ï¦Cªí¡C";
+?>
