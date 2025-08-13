5d7b314eeeb0e18895258ff0dc8b35c8ffd82622
me-suzy <me.suzana@gmail.com>
2025-08-14 01:10:09 +0300

    Add Unsorted/postaffiliate/user/lang/eng.php

---
+++ Unsorted/postaffiliate/user/lang/eng.php
@@ -0,0 +1,89 @@
+<?
+// global
+define(AFF_G_DATE, 'Date');
+define(AFF_G_TIME, 'Time');
+define(AFF_G_AFFID, 'Affiliate ID');
+define(AFF_G_PASSWORD, 'Password');
+define(AFF_G_USERNAME, 'Username');
+define(AFF_G_SHOW, 'Show');
+define(AFF_G_DELETE, 'Delete');
+define(AFF_G_BROWSER, 'Browser');
+define(AFF_G_EARNINGS, 'Earnings');
+define(AFF_D_WARNING, '<b>WARNING! </b>All deletions are final and cannot be recovered');
+define(AFF_R_WARNING, '<b>WARNING! - </b>Reset will reset all clickthroughs and sales for the selected affiliate to prevent multiple payouts. This action cannot be taken back.');
+define(AFF_G_ACTION, 'Action');
+
+
+// menu
+define(AFF_M_CHANGEDETAILS, 'Change Details');
+define(AFF_M_CLICKTHROUGHS, 'Click Throughs');
+define(AFF_M_SALES, 'Sales');
+define(AFF_M_BANNER, 'Banners/Links');
+define(AFF_M_CONTACT, 'Contact Us');
+define(AFF_M_LOGOUT, 'Logout');
+define(AFF_M_AFFILIATES, 'Manage Affiliates');
+define(AFF_M_CHANGEAMOUNT, 'Change Payout Amount');
+
+
+
+// index.php
+define(AFF_I_CANNOTLOG, 'Could not log you in.');
+define(AFF_I_NOTLOGGED, 'You are not logged in.');
+define(AFF_I_NOTAFFILIATE, 'You are not our affiliate?');
+define(AFF_I_SIGNUP, 'Sign up here');
+
+// details.php
+define(AFF_D_CHANGED, 'Your Details Have Now Been Changed.');
+
+// clicks.php
+define(AFF_C_CLICKS, 'Clickthroughs From Your Site');
+define(AFF_C_REFERREDFROM, 'Referred From');
+define(AFF_C_REFERRER, 'Referrer');
+define(AFF_C_REFERRERURL, 'Referrer URL');
+define(AFF_C_CLICKSFROM, 'Clickthroughs From');
+define(AFF_C_TOSITE, 'To Your Site');
+define(AFF_C_SHOWCLICKSFOR, 'Show The Clickthrouhs For:');
+
+
+// sales.php
+define(AFF_S_SALES, 'Sales From Your Site');
+define(AFF_S_EARNED, 'You Earned');
+define(AFF_S_TOTAL, 'Your Total Earnings Are');
+define(AFF_S_SHOWSALESFOR, 'Show The Sales For:');
+define(AFF_S_LASTSALES, 'Lastest Sales On Your Site:');
+define(AFF_S_SALESFROM, 'Sales From');
+define(AFF_S_FORSITE, 'For Your Site');
+
+// banners.php
+define(AFF_B_BANNERS, 'Here are some links you may like to use');
+define(AFF_B_BANNERSOFFER, 'Here are the banners you offer');
+define(AFF_B_ADDBANNER, 'Add Banner');
+define(AFF_B_BANNERNAME, 'Banner Name:');
+define(AFF_B_BANNERURL, 'Banner URL:');
+define(AFF_B_BANNERDESCRIPTION, 'Banner Description:');
+define(AFF_B_EDITBANNER, 'Edit Banner');
+define(AFF_B_DELETEBANNER, 'Delete Banner');
+define(AFF_B_CHANGEBANNER, 'Change Banner');
+
+
+// contact.php
+define(AFF_C_CONTACT, 'Feel free to contact us at anytime by emailing us at');
+
+
+// signup.php
+define(AFF_SI_USEREXISTS, 'Username already exists, please choose different username!');
+define(AFF_SI_UNAMEMISSING, 'Username must be filled!');
+define(AFF_SI_PWDMISSING, 'Password must be filled!');
+define(AFF_SI_EMAILMISSING, 'Email must be filled!');
+
+
+// affiliates.php
+define(AFF_A_DELETEAFF, 'Delete Affiliate:');
+define(AFF_A_REWARDAFF, 'Reward Affiliate:');
+define(AFF_A_AFFILIATE, 'Affiliate');
+define(AFF_A_SALESCOUNT, '# Of Sales');
+define(AFF_A_EARNED, 'Earned');
+define(AFF_A_INVOICE, 'Invoice');
+define(AFF_A_RESET, 'Reset');
+define(AFF_A_TOTAL, 'Total');
+?>
