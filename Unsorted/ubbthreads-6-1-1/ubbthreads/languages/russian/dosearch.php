1448cc91fb1c39db8d5a8d9ff2d16e1a957a6c5c
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:52 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/dosearch.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['NO_MORE']   =  "There are no more search results in this direction.";
+$ubbt_lang['NO_WORDS']  = 	"Ïîìåñòèòå ïîæàëóéñòà, íåñêîëüêî êëþ÷åâûõ ñëîâ äëÿ ïîèñêà ïî íèì.";
+$ubbt_lang['SEARCH_RES']  = 	"Ðåçóëüòàòû ïîèñêà ïî çàïðîñó";
+$ubbt_lang['PREV_PAGE']  = 	"Ïðåäûäóùàÿ ñòðàíèöà";
+$ubbt_lang['MAIN_SEARCH']  = 	"Ñòðàíèöà ïîèñêà";
+$ubbt_lang['NEXT_PAGE']  = 	"Ñëåäóþùàÿ ñòðàíèöà";
+$ubbt_lang['NO_MATCH']  = 	"Ðåçóëüòàòîâ, ñîîòâåòñòâóþùèõ Âàøåìó çàïðîñó íå íàéäåíî.";
+?>
