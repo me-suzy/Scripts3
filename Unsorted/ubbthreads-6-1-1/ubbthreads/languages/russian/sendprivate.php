e4697002c37bfe6795d9b82058aa34dade991f78
me-suzy <me.suzana@gmail.com>
2025-08-14 02:48:29 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/sendprivate.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/sendprivate.php
@@ -0,0 +1,13 @@
+<?
+$ubbt_lang['NOPRIVS']   = "Private messages are disabled.";
+$ubbt_lang['MESS_COPY']  = 	"I want to keep a copy of my message";
+$ubbt_lang['MESS_RECEIPT']  = 	"I want to receive a read receipt";
+$ubbt_lang['MESS_PREVIEW']  = 	"I want to preview my message";
+$ubbt_lang['ADD_BOOK']  = 	"Add to address book";
+$ubbt_lang['USE_BOOK']  = 	"or use your address book";
+$ubbt_lang['CHOOSE_ADD']  = 	"Choose a name";
+$ubbt_lang['NO_LOGGED']  = 	"Âû íå ìîæåòå îòñûëàòü ëè÷íûå ñîîáùåíèÿ íå âîéäÿ â ñèñòåìó.";
+$ubbt_lang['NO_PRIVATE']  = 	"Ýòîò ïîëüçîâàòåëü íå æåëàåò ïðèíèìàòü ëè÷íûå ñîîáùåíèÿ.";
+$ubbt_lang['PRIV_BODY2']  = 	"Fill in the following info to send a private message to this user.  They will get it the next time they login.";
+$ubbt_lang['TEXT_DELETE']  = 	"Óäàëèòü";
+?>
