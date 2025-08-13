3207020d4c7cc399720e5166b2af292bc572d229
me-suzy <me.suzana@gmail.com>
2025-08-14 01:56:06 +0300

    Add Unsorted/sunshop_v21.6nullified/sunshop26/admin/config.php

---
+++ Unsorted/sunshop_v21.6nullified/sunshop26/admin/config.php
@@ -0,0 +1,18 @@
+<?PHP
+///////////////////////////////////////////////////////
+// Program Name         : SunShop
+// Program Author       : Turnkey Solutions 2001-2002.
+// Program Version      : 2.6
+// Supplied by          : Stive [WTN], CyKuH [WTN]                            
+// Nullified by         : CyKuH [WTN]                                   
+///////////////////////////////////////////////////////
+// Hostname or ip of server
+$servername="localhost";
+// Username and password to log onto db server
+$dbusername="root";
+$dbpassword="";
+// Name of database
+$dbname="sunshop";
+// Table prefixes. Do not change the value after the install has been finished!
+//$dbprefix="ss_"; // Uncomment this line if this is a new install.
+?>
