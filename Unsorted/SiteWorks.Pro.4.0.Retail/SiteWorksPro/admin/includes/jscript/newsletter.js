95c59f92c04c32b6506b6a92ff8df6fd55402afe
me-suzy <me.suzana@gmail.com>
2025-08-14 01:42:20 +0300

    Add Unsorted/SiteWorks.Pro.4.0.Retail/SiteWorksPro/admin/includes/jscript/newsletter.js

---
+++ Unsorted/SiteWorks.Pro.4.0.Retail/SiteWorksPro/admin/includes/jscript/newsletter.js
@@ -0,0 +1,18 @@
+
+        <script language="JavaScript">
+
+                /*
+                        Filename: newsletter.js
+                        Desc: Handles all of the newsletter functions
+                */
+
+
+                function ConfirmDelSub()
+                {
+					if(confirm('WARNING: You are about to permanently remove this email address from the database.'))
+						return true;
+					else
+						return false;
+                }
+
+        </script>
