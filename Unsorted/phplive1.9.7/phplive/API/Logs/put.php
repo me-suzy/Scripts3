f28a4efaa71f1808766f629b3802d49defe4d5c7
me-suzy <me.suzana@gmail.com>
2025-08-14 00:47:50 +0300

    Add Unsorted/phplive1.9.7/phplive/API/Logs/put.php

---
+++ Unsorted/phplive1.9.7/phplive/API/Logs/put.php
@@ -0,0 +1,31 @@
+<?
+	/*****  ServiceLogs::put  ***************************************
+	 *
+	 *  $Id: put.php,v 1.2 2002/06/11 09:02:34 osicodes Exp $
+	 *
+	 ****************************************************************/
+
+	if ( ISSET( $_OFFICE_PUT_ServiceLogs_LOADED ) == true )
+		return ;
+
+	$_OFFICE_PUT_ServiceLogs_LOADED = true ;
+
+	/*****
+
+	   Internal Dependencies
+
+	*****/
+
+	/*****
+
+	   Module Specifics
+
+	*****/
+
+	/*****
+
+	   Module Functions
+
+	*****/
+
+?>
