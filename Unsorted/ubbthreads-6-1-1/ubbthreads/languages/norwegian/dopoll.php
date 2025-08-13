11a7f8c4c36ce179a20e2542e5267d4eb0c84c6b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:41:44 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/dopoll.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/dopoll.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NO_VOTE']  = 	"Du stemte ikke på noe!";
+$ubbt_lang['POLL_REGED']  = 	"Kun registrerte brukere kan stemme.";
+$ubbt_lang['POLL_IP']  = 	"Vi har allerede mottatt en stemme fra dette ip-nummeret.";
+$ubbt_lang['POLL_USER']  = 	"Vi har allerede mottatt en stemme fra dette brukernavnet.";
+?>
