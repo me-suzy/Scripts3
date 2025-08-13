f6a22dcb59d2de6612cac353feba38290061f45d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:10:40 +0300

    Add Unsorted/trafficgopher/includes/footer.php

---
+++ Unsorted/trafficgopher/includes/footer.php
@@ -0,0 +1,20 @@
+</table>
+              
+              
+            </td>
+          </tr>
+        </table></td>
+    </tr>
+    <tr> 
+      <td height="45" background="images/bottom_bg.gif"><table width="770" border="0" cellpadding="0" cellspacing="0" class="text">
+          <tr> 
+            <td width="254"><img src="images/spacer.gif" width="254" height="10"></td>
+           
+            <td align="center"><font color="#FFFFFF"><strong>Copyright &copy; 2003 <?php print $sitename; ?> - All Rights Reserved!</strong></font></td>
+          </tr>
+        </table></td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>
