1ae1cb9ce736730cbb97f9cde95139b46fdedf8d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:44:35 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/portuguese/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"Mensagens inválidas por página.";
+$ubbt_lang['FLAT_BAD']  = 	"Flat Posts per page must be between 1 and 99.";
+$ubbt_lang['TEXTAREA_NO']  = 	"O valor da área de texto é inválido.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Preferências de visualização alteradas.";
+?>
