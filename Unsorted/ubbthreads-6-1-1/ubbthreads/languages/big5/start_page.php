2c6c196ac214adcea38a3415df5ed7698e02777d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:41 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"½Ð¿é¤J±K½X";
+$ubbt_lang['PASS_REQ_BOD1'] =	"±z­è¤~¦V¥»¯¸´£¥X¸É±H±K½X­n¨D¡A±q³o­Ó IP ¦ì§}µn¤J";
+$ubbt_lang['PASS_REQ_BOD2'] =	"­n¨D¤F¸É±H¤@­ÓÁ{®É±K½X";
+$ubbt_lang['PASS_REQ_BOD3'] =	"³o¬O¤@«Ê¸É±H±K½X³qª¾«H¨ç¡A¦pªG±z§Ñ°O¤F±zªº±K½X¡A³o¨à¬O¤@­ÓÁ{®Éªº±K½X¡A±z¥i¥H¨Ï¥Î¥¦¨Óµn¤J¥»¯¸¡A­×§ï¦¨§AºD¥Îªº±K½X¡C¦pªG±z¨S¦³¦V¥»ºô¯¸­n¨D·sªº±K½X¡A½Ðª½±µ©¿²¤¦¹«Ê«H§Y¥i¡A¥H¤U¬O±zªºÁ{®É±K½X¡G";
+$ubbt_lang['PASS_MAILED']  = 	"±K½X¤w¸g±H¥X";
+$ubbt_lang['PASS_MAILED2']  = 	"Á{®É±K½X¤w¸g±H¥X¤F¡C";
+$ubbt_lang['NO_EMAIL'] =	"§ä¤£¨ì³o¦ì¨Ï¥ÎªÌªº Email «H½c¡G";
+?>
