0df386691f64ddffb7e345ec86684d54acbf9628
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:43 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/editpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/editpost.php
@@ -0,0 +1,24 @@
+<?
+$ubbt_lang['DOPREVIEW'] = "I want to preview my edit and add or edit an attachment.";
+$ubbt_lang['POST_TEXT']  =      "Post";
+$ubbt_lang['MOVE_POST']  =     "Verplaats dit onderwerp en alle antwoorden";
+$ubbt_lang['NO_EDIT']  = 	"Dit bericht kan niet meer bewerkt worden.";
+$ubbt_lang['EDITTIME']  = 	"Dit bericht kan niet meer bewerkt worden omdat de maximale tijd voor berichtaanpassingen verstreken is.";
+$ubbt_lang['PEDIT_HEAD']  = 	"Bericht bewerken";
+$ubbt_lang['PEDIT_BODY']  = 	"Bewerk of wis dit bericht.";
+$ubbt_lang['EDIT_POLL']  = 	"Omdat er een enquête in dit bericht geplaatst is, mag het gewist maar niet bewerkt worden.";
+$ubbt_lang['POST_ICON']  = 	"Bericht icoon";
+$ubbt_lang['MARK_EDIT']  = 	"Als bewerkt markeren?";
+$ubbt_lang['PEDIT_CHANGE']  = 	"Bewerk dit bericht";
+$ubbt_lang['PEDIT_DELETE']  = 	"Wis dit bericht";
+$ubbt_lang['PEDIT_APPROVE']  = 	"Dit bericht toestaan";
+$ubbt_lang['RET_NORM']  = 	"Zet terug op normale datum";
+$ubbt_lang['MAKE_STICKY']  = 	"Maak van dit bericht een 'Sticky'";
+$ubbt_lang['MANAGE_THREAD']  = 	"Onderwerpbeheer";
+$ubbt_lang['DELETE_THREAD']  =  "Wissen";
+$ubbt_lang['OPEN_THREAD']  = 	"Openen";
+$ubbt_lang['CLOSE_THREAD']  = 	"Sluiten";
+$ubbt_lang['UNKEEP_THREAD']  = 	"Vrijgeven";
+$ubbt_lang['KEEP_THREAD']  = 	"Bewaren";
+$ubbt_lang['MOVE_THREAD']  = 	"Verplaatsen";
+?>
