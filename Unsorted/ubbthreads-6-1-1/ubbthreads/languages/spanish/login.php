5ed0b9fd911a9e8a3e2dd9a892131d013c0f8ca6
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:38 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/login.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/login.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['FORGOT_PASS']   = "¿Olvidaste la contraseña?";
+$ubbt_lang['FORGOT_ENTER']  = "Escribe tu direcci&oacute;n de email o el nombre de miembro con el que te registraste para que se te envíe una contrase&ntilde;a temporal.";
+$ubbt_lang['REMEMBER_ME']   =  "Siempre recordar quén soy.";
+$ubbt_lang['LOGIN_PROMPT']  = 	"Se ruega Identificarse como miembro";
+$ubbt_lang['LOGIN_PROMPT2']  = 	"Para IDENTIFICARTE necesitas introducir tu nombre de Miembro y contrase&ntilde;a.  Tambi&eacute;n puedes entrar an&oacute;nimamente, pero te perder&aacute;s algunas funciones especiales.  Si no est&aacute;s registrado/a puedes hacerlo ";
+$ubbt_lang['REG_ONE']  = 	"pulsando aqu&iacute;";
+$ubbt_lang['PASSWORD_TEXT']  = 	"Tu contrase&ntilde;a";
+?>
