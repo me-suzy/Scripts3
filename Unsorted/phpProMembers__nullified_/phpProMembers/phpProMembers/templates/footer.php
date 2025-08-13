25e8aa253a27ecbd647d8f08989af188a99fdd6b
me-suzy <me.suzana@gmail.com>
2025-08-14 00:59:16 +0300

    Add Unsorted/phpProMembers__nullified_/phpProMembers/phpProMembers/templates/footer.php

---
+++ Unsorted/phpProMembers__nullified_/phpProMembers/phpProMembers/templates/footer.php
@@ -0,0 +1,39 @@
+	  </FONT></P>
+						  	</td>
+                          </tr>
+                        </table>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            &nbsp;
+          </td>
+        </tr>
+      </table>
+      
+    <p>&nbsp;</p></td>
+  </tr>
+</table>
+<table width="776" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td><!--CyKuH--> 
+      <div align="center"><font face="Arial, Helvetica, sans-serif"><b><font color="#FFFFFF">COPYRIGHT 2002 GACyberTech <a href="mailto:info@mydomain.com" style="color:white">info@mydomain.com</a></font></b></font></div>
+    </td>
+  </tr>
+</table>
+<table width="776" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td> 
+<center><font face="Arial, Helvetica, sans-serif" size="1"><b>best viewed in internet explorer or netscape 4.72 or higher</b></font></center>
+</td>
+</tr>
+</table>
+</center>
+</body>
+</html>
