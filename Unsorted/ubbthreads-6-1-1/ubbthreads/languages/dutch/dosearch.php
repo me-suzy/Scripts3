1f11f2d7140966045a8a4a6d86633b0669b2d456
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:34 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/dosearch.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['NO_MORE']   =  "There are no more search results in this direction.";
+$ubbt_lang['NO_WORDS']  = 	"Geen zoekwoorden!";
+$ubbt_lang['SEARCH_RES']  = 	"Zoekresultaten";
+$ubbt_lang['PREV_PAGE']  = 	"Vorige pagina";
+$ubbt_lang['MAIN_SEARCH']  = 	"Zoek index";
+$ubbt_lang['NEXT_PAGE']  = 	"Volgende pagina";
+$ubbt_lang['NO_MATCH']  = 	"Geen treffers.";
+?>
