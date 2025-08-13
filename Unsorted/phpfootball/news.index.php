7a6e028e12e0926d1e4cab72da0163529546f12a
me-suzy <me.suzana@gmail.com>
2025-08-14 00:38:32 +0300

    Add Unsorted/phpfootball/news.index.php

---
+++ Unsorted/phpfootball/news.index.php
@@ -0,0 +1,14 @@
+<?php include("scripts/inc.layout.php"); ?>
+
+<html>
+<head>
+<title>PHPFootball news module</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<frameset cols="*" rows="*,106" frameborder="NO" border="0" framespacing="0"> 
+  <frame src="<?php echo $newssrc; ?>" name="top">
+  <frame src="news.php" name="bottom">
+</frameset>
+<noframes><body bgcolor="#FFFFFF">
+</body></noframes>
+</html>
