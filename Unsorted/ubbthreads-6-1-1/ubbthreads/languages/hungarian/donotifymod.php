74ae3c7aa54e985d7d7e3112933518f23b922efc
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:35 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/donotifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/donotifymod.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['POST_PROB']  =  "There was a problem looking up this post in our database.";
+$ubbt_lang['BAD_GROUP']  = 	"Access Denied!";
+$ubbt_lang['NO_B_INFO']  = 	"Forum not found!";
+$ubbt_lang['NOTIF_HEAD']  = 	"has notified you of a post you should look at entitled";
+$ubbt_lang['TO_READ']  = 	"This post can be viewed here:";
+$ubbt_lang['NOTIFY_SENT']  = 	"Moderator(s) notified";
+$ubbt_lang['NOTIFY_CONFIRM']  =  "The moderator(s) for this forum have been notified.";
+?>
