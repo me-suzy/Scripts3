5b423d40ccff7f42caa6062ca07185bdec98d78d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:34:08 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =	"Upload your picture ({$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['PROF_PIC']  = 	"URL to your picture ({$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['PROF_PRIV']  = 	"Accept private messages from other users?";
+$ubbt_lang['NO_PROF']  = 	"We could not find a profile for the Username";
+$ubbt_lang['PROF_HEAD']  = 	"Edit profile for";
+$ubbt_lang['PROF_EDIT']  = 	"You can edit any of the information below.";
+$ubbt_lang['BASIC_PROF']  = 	"Basic Profile";
+$ubbt_lang['PROF_PASS']  = 	"Password (between 4 and 20 characters)";
+$ubbt_lang['VER_PASS']  = 	"Verify Password";
+$ubbt_lang['EMAIL_ADD']  = 	"Email Address (for our administrative use only...not visible to other users)";
+$ubbt_lang['FAKE_EMAIL']  = 	"Email address (displayed on posts)";
+$ubbt_lang['FULL_NAME']  = 	"Full Name";
+$ubbt_lang['PROF_SIG']  = 	"Signature (up to {$config['Sig_length']}  characters).  You may use <a href=\"{$config['phpurl']}/faq_english.php#html\" target=\"_new\">UBBCode</a> in your signature";
+$ubbt_lang['PROF_HOME']  = 	"Homepage (up to 150 characters)";
+$ubbt_lang['PROF_OCC']  = 	"Occupation";
+$ubbt_lang['PROF_HOBB']  = 	"Hobbies (up to 200 characters)";
+$ubbt_lang['PROF_LOC']  = 	"Geographic Location (State, Country, etc.)";
+$ubbt_lang['PROF_BIO']  = 	"Bio (up to 250 characters)";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ Number";
+$ubbt_lang['PROF_VIS']  = 	"Do you want to be visible on the \"Who's Online\" screen?";
+$ubbt_lang['AUX_ONLINE']  = 	"Do you want users to see the actual forum/post you are looking at?";
+?>
