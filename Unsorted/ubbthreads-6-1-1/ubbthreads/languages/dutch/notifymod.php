9e8e3edd3eb07d4025517847fed7ac13205edd29
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:02 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/notifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/notifymod.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['NOTIFY_MOD']  =  	"Moderator waarschuwen";
+$ubbt_lang['NOTIFY_HEAD']  = 	"Deze functie waarschuwd een moderator over dit bericht.";
+$ubbt_lang['SEND_IT']  =  "Ja! Ik wil nu de moderator(s) van dit forum waarschuwen.";
+$ubbt_lang['NO_POST']  = 	"Dit bericht kon niet in onze database worden gevonden.";
+?>
