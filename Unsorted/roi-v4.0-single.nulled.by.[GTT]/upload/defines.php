da7aa427d3736b9c0ee7ad44b7424f0b90ac78ac
me-suzy <me.suzana@gmail.com>
2025-08-14 01:26:29 +0300

    Add Unsorted/roi-v4.0-single.nulled.by.[GTT]/upload/defines.php

---
+++ Unsorted/roi-v4.0-single.nulled.by.[GTT]/upload/defines.php
@@ -0,0 +1,17 @@
+<?php
+
+    define("PASSWORD","root");
+
+    define("USER","root");
+
+    define("TIMEOUT",1800);
+
+    define("DBHOST","localhost");
+
+    define("DBUSER","root");
+
+    define("DBPASS","");
+
+    define("DBNAME","roi");
+
+    ?>
