092bd60718f42a50aca5155cad2990943037b955
me-suzy <me.suzana@gmail.com>
2025-08-14 01:39:06 +0300

    Add Unsorted/Shop-Script.2.0.(NEW).suupl.&.null.by.[GTT]/cfg/currency.inc.php

---
+++ Unsorted/Shop-Script.2.0.(NEW).suupl.&.null.by.[GTT]/cfg/currency.inc.php
@@ -0,0 +1,9 @@
+<?
+	$currency_name[] = "USD ";
+	$currency_value[] = "1";
+	$currency_where[] = 0;
+	$currency_name[] = " ðóá.";
+	$currency_value[] = "1";
+	$currency_where[] = 1;
+	$invoice_currency = 1;
+?>
