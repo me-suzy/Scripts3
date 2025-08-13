452565293ff9fd7558eeed4161cefc3fb3c4cce9
me-suzy <me.suzana@gmail.com>
2025-08-14 02:42:25 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/sendprivate.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/sendprivate.php
@@ -0,0 +1,13 @@
+<?
+$ubbt_lang['NOPRIVS']   = "Private messages are disabled.";
+$ubbt_lang['MESS_COPY']  = 	"Jeg vil beholde en kopi av beskjeden";
+$ubbt_lang['MESS_RECEIPT']  = 	"Jeg ønsker å få en bekreftelse når beskjeden er lest";
+$ubbt_lang['MESS_PREVIEW']  = 	"Jeg vil forhåndsvise beskjeden";
+$ubbt_lang['ADD_BOOK']  = 	"Legg brukeren i adresseboken";
+$ubbt_lang['USE_BOOK']  = 	"eller velg en bruker fra adresseboken";
+$ubbt_lang['CHOOSE_ADD']  = 	"Velg en bruker";
+$ubbt_lang['NO_LOGGED']  = 	"Du kan ikke sende private beskjeder før du har logget inn.";
+$ubbt_lang['NO_PRIVATE']  = 	"Denne brukeren ønsker ikke å motta private beskjeder.";
+$ubbt_lang['PRIV_BODY2']  = 	"Fyll ut følgende felter for å sende en privat beskjed til denne brukeren. Brukeren vil motta den neste gang brukeren logger inn.";
+$ubbt_lang['TEXT_DELETE']  = 	"Slett";
+?>
