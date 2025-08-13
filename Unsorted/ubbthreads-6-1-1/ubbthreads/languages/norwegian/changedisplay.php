f654e9fe00842afdb7716349251eb85e5638eae4
me-suzy <me.suzana@gmail.com>
2025-08-14 02:41:32 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/changedisplay.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/changedisplay.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['POSTS_PER']  = 	"Det ønskede antall innlegg pr. side er ugyldig.";
+$ubbt_lang['FLAT_BAD']  = 	"Totalt antall innlegg må være mellom 1 og 99.";
+$ubbt_lang['TEXTAREA_NO']  = 	"Den ønskede størrelse på tekstfeltet er ugyldig.";
+$ubbt_lang['DISPLAY_MOD']  = 	"Innstillingene for utseendet har nå blitt endret.";
+?>
