e1a9634611ef58ddfb32125534aefd58664e6a2d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:12 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"Cerere pentru parola";
+$ubbt_lang['PASS_REQ_BOD1']  = 	"Cineva de la IP-ul";
+$ubbt_lang['PASS_REQ_BOD2']  = 	"a cerut o parola temporara pentru utilizatorul";
+$ubbt_lang['PASS_REQ_BOD3']  = 	"pentru site-ul '{$config['title']}'. Aceasta este o parola temporara care poate fi folosita in caz ca ati uitat parola curenta. Daca nu ati cerut asa ceva, ignorati acest mesaj. Parola temporara pentru acest cont este --";
+$ubbt_lang['PASS_MAILED']  = 	"Parola a fost trimisa";
+$ubbt_lang['PASS_MAILED2']  = 	"O parola temporara pentru acest utilizator a fost trimisa prin email.";
+$ubbt_lang['NO_EMAIL']  = 	"Nu gasesc adresa de email pentru";
+?>
