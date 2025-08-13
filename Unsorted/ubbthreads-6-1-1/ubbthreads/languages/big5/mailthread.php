75781e16f202bb9116a4bcd2c4078f8d5b5e8943
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:08 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/mailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/mailthread.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['MAIL_FRIEND'] =	"¤À¨É¦n¤Í";
+$ubbt_lang['FRIEND_HEAD'] =	"½Ð¶ñ¤J¦¬«H¤Hªº Email «H½c";
+$ubbt_lang['SENT_FROM1'] =	"³o«Ê«H±N·|¥H±zµù¥Uªº Email ±H¥X";
+$ubbt_lang['SENT_FROM2'] =	"¨Ï¥Î³o­Ó¦WºÙ";
+$ubbt_lang['USE_REAL'] =	"°²¦p±z·Q¥Î¦Û¤vªº¯u¦W¡A½Ð¦b³o¸Ì¶ñ¼g¡G";
+$ubbt_lang['WHAT_EMAIL'] =	"¦¬«H¤Hªº Email «H½c¡G";
+$ubbt_lang['SEND_IT'] =	"°e¥X¡I";
+?>
