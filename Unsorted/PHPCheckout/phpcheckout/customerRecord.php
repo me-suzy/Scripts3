72acc371abd644dc442142022f5ffe9a9f42fe20
me-suzy <me.suzana@gmail.com>
2025-08-14 00:31:16 +0300

    Add Unsorted/PHPCheckout/phpcheckout/customerRecord.php

---
+++ Unsorted/PHPCheckout/phpcheckout/customerRecord.php
@@ -0,0 +1,9 @@
+<!-- start of customerRecord.php -->
+
+<?php print "<p><span style=\"font-size:medium;\">ID# $customerid</span><br><br>
+Email: <a href=\"mailto:$email\">$email</a><br>
+Privacy: $privacy Newsletter: $news<br>
+Customer Since: $customersince Visits: $visits 
+<span style=\"font-size:xx-small;\">$lastupdate</span><br><br>
+</p>";?>
+<!-- end of customerRecord.php -->
