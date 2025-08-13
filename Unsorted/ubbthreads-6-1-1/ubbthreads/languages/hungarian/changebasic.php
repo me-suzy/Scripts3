01228b67a3fcfe8b92c4bfe48c7b63d7ddfd6d52
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:23 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/changebasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/changebasic.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['PIXEL_TOO_BIG']="Your picture exceeds the ({$theme['PictureWidth']}x{$theme['PictureHeight']}) pixel limitation width a width/height of ";
+$ubbt_lang['FILE_TOO_BIG']="Pictures can be no larger than {$config['avatarsize']} bytes.";
+$ubbt_lang['BAD_PIC']  =       "Improper picture type.";
+$ubbt_lang['PASS_MATCH']  = 	"A jelszavak nem egyeznek meg.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"A jelszó hossza 4 és 20 karakter között lehet.";
+$ubbt_lang['HOME_TOO_LONG']  = 	"A home-lap bejegyzés túl hosszú.";
+$ubbt_lang['OCC_TOO_LONG']  = 	"A foglalkozás bejegyzés túl hosszú.";
+$ubbt_lang['HOBB_TOO_LONG']  = 	"A hobbi bejegyzés túl hosszú.";
+$ubbt_lang['LOC_TOO_LONG']  = 	"A lakhely bejegyzés túl hosszú.";
+$ubbt_lang['SIG_TOO_LONG']  = 	"Az aláírás túl hosszú.";
+$ubbt_lang['BIO_TOO_LONG']  = 	"Az ismertetõ túl hosszú.";
+$ubbt_lang['BASIC_MOD']  = 	"Az ön adatai módosítva.";
+?>
