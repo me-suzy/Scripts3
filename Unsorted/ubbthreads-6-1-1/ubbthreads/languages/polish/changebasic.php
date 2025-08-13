537533d79e61274a8e1278411f074029f828da7a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:43:01 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/changebasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/changebasic.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['PIXEL_TOO_BIG']="Your picture exceeds the ({$theme['PictureWidth']}x{$theme['PictureHeight']}) pixel limitation width a width/height of ";
+$ubbt_lang['FILE_TOO_BIG']="Pictures can be no larger than {$config['avatarsize']} bytes.";
+$ubbt_lang['BAD_PIC']  =       "Niew³a¶ciwy format rysunku.";
+$ubbt_lang['PASS_MATCH']  = 	"Has³a siê nie zgadzaj± ze sob±.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Has³o powinno mieæ 4-20 znaków.";
+$ubbt_lang['HOME_TOO_LONG']  = 	"Adres strony jest zbyt d³ugi.";
+$ubbt_lang['OCC_TOO_LONG']  = 	"Nazwa zawodu jest zbyt d³uga.";
+$ubbt_lang['HOBB_TOO_LONG']  = 	"Nazwa hobby jest zbyt d³uga.";
+$ubbt_lang['LOC_TOO_LONG']  = 	"Nazwa miasta jest zbyt d³uga.";
+$ubbt_lang['SIG_TOO_LONG']  = 	"Podpis jest zbyt d³ugi.";
+$ubbt_lang['BIO_TOO_LONG']  = 	"Twoja biografia jest zbyt d³uga.";
+$ubbt_lang['BASIC_MOD']  = 	"Twoje ustawienia podstawowe zosta³y zmienione.";
+?>
