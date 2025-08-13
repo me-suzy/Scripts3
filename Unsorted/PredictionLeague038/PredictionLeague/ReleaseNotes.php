c2da1aa93df7a69edab485f487413f652cce9e41
me-suzy <me.suzana@gmail.com>
2025-08-14 01:13:53 +0300

    Add Unsorted/PredictionLeague038/PredictionLeague/ReleaseNotes.php

---
+++ Unsorted/PredictionLeague038/PredictionLeague/ReleaseNotes.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Prediction Football Release Notes</title>
+  </head>
+  <link rel>
+  <body>
+  <pre>
+    <?php include "ReleaseNotes.txt"?>
+  </pre>
+  </body>
+</html>
