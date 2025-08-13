d75fe5133d0101c45cc9e221937c0afbf154bddd
me-suzy <me.suzana@gmail.com>
2025-08-14 02:41:52 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =      "Your picture ({$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['PROF_PIC']  =       "Your Picture (Entire URL - Picture size is {$theme['PictureWidth']}x{$theme['PictureHeight']} max pixels, may be smaller) (.gif, .jpg, .png accepted)";
+$ubbt_lang['AUX_ONLINE']  =      "Do you want users to see the actual forum/post you are looking at?";
+$ubbt_lang['PROF_PRIV']  = 	"Akseptere private beskjeder fra andre brukere?";
+$ubbt_lang['NO_PROF']  = 	"Vi kan dessverre ikke finne en profil for brukernavnet";
+$ubbt_lang['PROF_HEAD']  = 	"Endre profilen for";
+$ubbt_lang['PROF_EDIT']  = 	"Innstillingene under kan endres.";
+$ubbt_lang['BASIC_PROF']  = 	"Grunnleggende profil";
+$ubbt_lang['PROF_PASS']  = 	"Passord (4-20 tegn)";
+$ubbt_lang['VER_PASS']  = 	"Skriv inn passordet igjen";
+$ubbt_lang['EMAIL_ADD']  = 	"E-postadresse (er ikke synlig for andre brukere)";
+$ubbt_lang['FAKE_EMAIL']  = 	"E-postadresse som er synlig for andre brukere";
+$ubbt_lang['FULL_NAME']  = 	"Fullt navn";
+$ubbt_lang['PROF_SIG']  = 	"Signatur (opp til  {$config['Sig_length']} tegn). Du kan bruke tagger i din signatur";
+$ubbt_lang['PROF_HOME']  = 	"Hjemmeside (maks. 150 tegn)";
+$ubbt_lang['PROF_OCC']  = 	"Yrke";
+$ubbt_lang['PROF_HOBB']  = 	"Fritidsinteresser (maks. 200 tegn)";
+$ubbt_lang['PROF_LOC']  = 	"Bosted";
+$ubbt_lang['PROF_BIO']  = 	"Om deg selv (maks. 250 tegn)";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ-nummer";
+$ubbt_lang['PROF_VIS']  = 	"Ønsker du å være synlig på 'Hvem er online?'-siden?";
+?>
