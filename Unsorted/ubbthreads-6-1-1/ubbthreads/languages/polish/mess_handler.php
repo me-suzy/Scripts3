68a249dc7b51a00477ddc873303a1877e10fcc44
me-suzy <me.suzana@gmail.com>
2025-08-14 02:43:39 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/mess_handler.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/mess_handler.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['REPLY_HEAD']   =   "U¿yj poni¿szego formularza, ¿eby wys³aæ wiadomo¶æ prywatn± u¿ytkownikowi ";
+$ubbt_lang['MESS_COPY']  =     "Zachowaj kopiê tej wiadomo¶ci";
+$ubbt_lang['DELETE_MESS']  = 	"Skasuj tê wiadomo¶æ";
+$ubbt_lang['REPLY_MESS']  = 	"Odpowiedz";
+$ubbt_lang['RETURN_START']  = 	"Powrót do strony pocz±tkowej";
+$ubbt_lang['MESS_RECEIPT']  = 	"Potwierdzenie przeczytania wiadomo¶ci";
+$ubbt_lang['MESS_PREVIEW']  = 	"Podgl±d wiadomo¶ci przed wys³aniem";
+$ubbt_lang['ORIG_MESS']  = 	"Wiadomo¶æ oryginalna";
+?>
