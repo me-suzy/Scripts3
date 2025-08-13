478215b35be4b492fa5fb4dac1bc40b59fe057bc
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:41 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/english/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =	"Your account needs to be approved by an Administrator before you can login.  You will receive an email once this has been done.";
+$ubbt_lang['APPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} has been approved.";
+$ubbt_lang['NOTAPPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} was not approved.  The reason for this is:";
+$ubbt_lang['NEWREGBODY'] = "A new user has registered at {$config['title']}. To approve this account, please visit";
+$ubbt_lang['NEWREG'] = "New User Registration at";
+$ubbt_lang['INVALID_DATE'] = "That is an invalid date.";
+$ubbt_lang['TOOYOUNG']  = 	"Registration for this message board will require approval of your parent or guardian.  You may use the <a href=\"{$config['phpurl']}/coppaform.php\">Permission form</a> for this.";
+$ubbt_lang['UNDER13'] = "Sorry, we do not allow children under 13 to register.";
+$ubbt_lang['BAD_UNAME']  = 	"You can only have alphanumeric characters in your username.  Special characters are not allowed because this allows users to spoof other usernames.  You may use the _ character to represent a space.";
+$ubbt_lang['LONG_NAME']  = 	"Username is not the proper length.";
+$ubbt_lang['BAD_EMAIL']  = 	"You are not allowed to register a Username from the email domain";
+$ubbt_lang['USER_EXISTS']  = 	"User exists.";
+$ubbt_lang['NO_MULTI']  = 	"We already have an account for that email address.";
+$ubbt_lang['PASS_MATCH']  = 	"Passwords do not match.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Password needs to be between 4 and 20 characters.";
+$ubbt_lang['ILL_PASS']  = 	"Password has illegal characters.  Please use only alphanumeric characters.";
+$ubbt_lang['WEL_MSG']  = 	"Welcome to our boards!  Please take the time to edit your profile to take full advantage of all of our features.";
+$ubbt_lang['PASS_SUB']  = 	"Your password for";
+$ubbt_lang['PASS_BODY1']  = 	"Someone from the ip address";
+$ubbt_lang['PASS_BODY2']  = 	"registered the Username";
+$ubbt_lang['PASS_BODY3']  = 	"The password for this Username is";
+$ubbt_lang['NEW_CONFIRM']  = 	"Username has been registered.";
+$ubbt_lang['NEW_BODY']  = 	"Your username has been reserved.  You should be receiving an email shortly with your password.";
+?>
