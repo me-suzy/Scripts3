8525081d7edf298160b8b769688d1b25826304a1
me-suzy <me.suzana@gmail.com>
2025-08-14 02:42:57 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/approvepost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/approvepost.php
@@ -0,0 +1,5 @@
+<?
+$ubbt_lang['REPLY_SUB']  = 	"Odpowied¼ na Twoj± wiadomo¶æ";
+$ubbt_lang['REPLY_BOD']  = 	"odpowiedzia³ na Twoj± wiadomo¶æ na forum: .";
+$ubbt_lang['APPR_HEAD']  = 	"Wiadomo¶æ zosta³a zaakceptowana.";
+?>
