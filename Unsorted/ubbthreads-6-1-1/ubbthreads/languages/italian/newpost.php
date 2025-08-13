7bdd7cfc7792840d5bcabeaacecb0f832eb37f35
me-suzy <me.suzana@gmail.com>
2025-08-14 02:40:39 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/newpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/newpost.php
@@ -0,0 +1,17 @@
+<?
+$ubbt_lang['CHOOSE_NAME']  = 	"Anonymous postings are allowed, so you may choose any unregistered name to post under.";
+$ubbt_lang['READ_PERM']  = 	"You have read only privileges for this forum";
+$ubbt_lang['MAKENEW_HEAD']  = 	"Invia un Nuovo Messaggio - Forum";
+$ubbt_lang['FILL_FORM']  = 	"Compila il modulo qui sotto per inviare un messaggio sul forum.";
+$ubbt_lang['NO_HTML']  = 	"l'HTML è disabilitato, non puoi utilizzarlo nei tuoi messaggi.";
+$ubbt_lang['YES_HTML']  = 	"l'HTML è abilitato, quindi puoi utilizzarlo nei tuoi messaggi.";
+$ubbt_lang['NO_MARKUP']  = 	"UBBCode è disabilitato per questo forum così non puoi utilizzare UBBCode nei tuoi messaggi.";
+$ubbt_lang['YES_MARKUP']  = 	"La modalità UBBCode è abilitata, puoi quindi utilizzare";
+$ubbt_lang['MARK_POSTS']  = 	"UBBCode nei tuoi messaggi";
+$ubbt_lang['POST_ICON']  = 	"Posta Icona";
+$ubbt_lang['POST_TEXT']  = 	"Posta";
+$ubbt_lang['SPELL_CHECK']  = 	"Controllo ortografico";
+$ubbt_lang['DO_EMAIL']  = 	"Invia in email tutte le risposte al mio reale indirizzo email";
+$ubbt_lang['DO_PREVIEW']  = 	"I want to preview my post";
+$ubbt_lang['CAN_ATTACH']  = 	"and/or attach a file";
+?>
