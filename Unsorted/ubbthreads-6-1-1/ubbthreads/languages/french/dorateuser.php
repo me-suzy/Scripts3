6fd679d2f74bf5841cf2ca1fbec479f17c972121
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:36 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/dorateuser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/dorateuser.php
@@ -0,0 +1,7 @@
+<?
+$ubbt_lang['INVALID_RATING'] = "That is an invalid rating.";
+$ubbt_lang['NOMORERATE'] = "Vous avez déjà évalué ce membre.";
+$ubbt_lang['THANKS'] = "Merci.";
+$ubbt_lang['RATED']	= "L'évaluation de ce membre a été enregistrée.";
+$ubbt_lang['CANTRATE']  = "Vous ne pouvez pas vous évaluer vous-même.";
+?>
