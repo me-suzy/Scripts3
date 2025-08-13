b1939c83d8b568e81237040fd33459f7c0251e47
me-suzy <me.suzana@gmail.com>
2025-08-14 02:05:53 +0300

    Add Unsorted/topsites/languages/spanish.php

---
+++ Unsorted/topsites/languages/spanish.php
@@ -0,0 +1,297 @@
+<?php
+//=================================================================\\
+// Aardvark Topsites PHP 4.1.0                                     \\
+//-----------------------------------------------------------------\\
+// Copyright (C) 2003 Jeremy Scheff - http://www.aardvarkind.com/  \\
+//-----------------------------------------------------------------\\
+// This program is free software; you can redistribute it and/or   \\
+// modify it under the terms of the GNU General Public License     \\
+// as published by the Free Software Foundation; either version 2  \\
+// of the License, or (at your option) any later version.          \\
+//                                                                 \\
+// This program is distributed in the hope that it will be useful, \\
+// but WITHOUT ANY WARRANTY; without even the implied warranty of  \\
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the   \\
+// GNU General Public License for more details.                    \\
+//-----------------------------------------------------------------\\
+// This translation is by Jon K. Artetxe                           \\
+// http://www.i-barakaldo.com/                                     \\
+//=================================================================\\
+
+// Global
+$LNG['g_form_submit_short'] = "IR";
+$LNG['g_form_submit_long'] = "Enviar";
+$LNG['g_form_submit_reset'] = "Borrar";
+$LNG['g_id'] = "ID";
+$LNG['g_url'] = "URL";
+$LNG['g_title'] = "Title";
+$LNG['g_description'] = "Descripcion";
+$LNG['g_category'] = "Category"; // 4.1.0
+$LNG['g_email'] = "Email";
+$LNG['g_bannerurl'] = "Banner URL";
+$LNG['g_password'] = "Contraseña";
+$LNG['g_average'] = "Promedio";
+$LNG['g_today'] = "Hoy";
+$LNG['g_yesterday'] = "Ayer";
+$LNG['g_2days'] = "Hace 2 dias";
+$LNG['g_3days'] = "Hace 3 dias";
+$LNG['g_thismonth'] = "Este mes"; // 3.2.1
+$LNG['g_lastmonth'] = "El mes Pasado"; // 3.2.1
+$LNG['g_2months'] = "Hace 2 meses"; // 3.2.1
+$LNG['g_3months'] = "Hace 3 meses"; // 3.2.1
+$LNG['g_unq_pv'] = "Visitas Unicas";
+$LNG['g_tot_pv'] = "Visitas Totales de Pag.";
+$LNG['g_unq_in'] = " Hits Unicos de Entrada";
+$LNG['g_tot_in'] = "Total Hits de Entrada";
+$LNG['g_unq_out'] = " Hits Unicos de Salida";
+$LNG['g_tot_out'] = "Total Hits de Salida";
+
+// Edit Member Info
+$LNG['edit_header'] = "Editar Informacion de Usuario";
+$LNG['edit_error_id_password'] = "Verifica tu ID y contraseña e intentalo de nuevo";
+$LNG['edit_error'] = "ERROR";
+$LNG['edit_error_forgot'] = "Has olvidado:";
+$LNG['edit_error_url'] = "Introducir una URL valido.";
+$LNG['edit_error_email'] = "Introducir un email valido.";
+$LNG['edit_error_title'] = "Introducir un titulo para tu web.";
+$LNG['edit_error_password'] = "Introducir una Contraseña.";
+$LNG['edit_error_back'] = "Vuelve atras y corrige los errores.";
+$LNG['edit_success'] = "EXITO";
+$LNG['edit_info_edited'] = "Tu informacion ha sido editada con exito.";
+
+// Gateway Page
+$LNG['gateway_header'] = "Pagina de ACCESO ";
+$LNG['gateway_text'] = "Para preveer abusos, se ha creado esta pagina.  Pincha en los enlaces para entar en el TOP.";
+$LNG['gateway_vote'] = "Entrar y Votar.";
+$LNG['gateway_novote'] = "Entar sin Votar.";
+$LNG['gateway_cheating'] = "La web desde la que que vienes es incorrecta.  Tu voto no sera contabilizado.";
+$LNG['gateway_enter'] = "ENTRAR";
+
+// Graph
+$LNG['graph_header'] = "Grafico";
+
+// Join
+$LNG['join_header'] = "Unirse al top";
+$LNG['join_error'] = "ERROR";
+$LNG['join_error_forgot'] = "Has olvidado:";
+$LNG['join_error_url'] = "Introducir una URL valido.";
+$LNG['join_error_email'] = "Introducir un email valido.";
+$LNG['join_error_title'] = "Introducir un titulo para tu web.";
+$LNG['join_error_password'] = "Introducir una Contraseña.";
+$LNG['join_error_urlbanner'] = "Introducir un banner valido (dejalo en blanco si no tienes).  < "; // 4.0
+$LNG['join_error_back'] = "Vuelve atras y corrige los errores.";
+$LNG['join_error_time'] = "Less than 120 seconds have passed since you joined.  Do not refresh the join confirmation page."; // 4.0
+$LNG['join_success'] = "EXITO";
+$LNG['join_thanks'] = "Gracias . Pon este codigo en tu web para que pueda ser puntuada en el top.";
+$LNG['join_changewarning'] = "Si cambias el codigo,puede que no funcione.";
+$LNG['join_welcome'] = "Bienvenido al top";
+$LNG['join_welcome_look'] = "Si no funciona, utiliza este codigo:";
+$LNG['join_welcome_admin'] = "Un nuevo miembro se ha unido al top";
+
+// Lost Code
+$LNG['lostcode_header'] = "¿Codigo?";
+
+// Lost Password
+$LNG['lostpw_header'] = "Olvido de Contraseña";
+$LNG['lostpw_email'] = "Para conseguir una nueva contraseña para tu web, debes ir ala siguiente direccion."; // 4.0.1
+$LNG['lostpw_emailed'] = "Instructions on how to reset your password have been emailed to you.";
+$LNG['lostpw_newpassword'] = "Introduce una nueva contraseña"; // 4.0.1
+$LNG['lostpw_finish'] = "Tu contraeña ha sido actualizada a la nueva que has elegido."; // 4.0.1
+$LNG['lostpw_error'] = "Introduce el ID correcto e intentalo de nuevo.";
+
+// Main Template
+$LNG['main_header'] = "Principal"; // 4.0
+$LNG['main_all'] = "All Sites"; // 4.2.0
+$LNG['main_method'] = "Metodo de Ranking";
+$LNG['main_members'] = "Miembros";
+$LNG['main_menu_main'] = "Principal";
+$LNG['main_menu_join'] = "Inscribirse";
+$LNG['main_menu_random'] = "Miembro aleatorio";
+$LNG['main_menu_search'] = "Buscar";
+$LNG['main_menu_lostcode'] = "¿Codigo?";
+$LNG['main_menu_lostpw'] = "¿Contraseña?";
+$LNG['main_menu_edit'] = "Editar Cuenta";
+$LNG['main_featured'] = "Featured Member"; // 4.0.2
+$LNG['main_executiontime'] = "Tiempo de  Ejecucion"; // 4.0
+$LNG['main_queries'] = "SQL Queries"; // 4.0
+$LNG['main_powered'] = "Desarrollado por";
+
+// Ranking Table
+$LNG['table_stats'] = "Estadisticas";
+$LNG['table_unique'] = "Unico";
+$LNG['table_total'] = "Total";
+$LNG['table_rank'] = "Ranking";
+$LNG['table_title'] = "Titulo"; // 4.0
+$LNG['table_description'] = "Descripcion"; // 4.0
+
+// Rate and Review
+$LNG['rate_header'] = "Valorar y Comentar";
+$LNG['rate_rating'] = "Votos";
+$LNG['rate_review'] = "Comentarios";
+$LNG['rate_thanks'] = "Thanks for rating and reviewing this site!";
+$LNG['rate_error'] = "You have already rated or reviewed this site.";
+$LNG['rate_back'] = "Back To Stats";
+
+// Search
+$LNG['search_header'] = "Buscar";
+$LNG['search_internet'] = "En Internet"; // 4.0
+$LNG['search_topsites'] = "En el Top"; // 4.0
+$LNG['search_off'] = "La opcion de Busqueda ha sido desabilitada del Top.";
+$LNG['search_rank'] = "Ranking";
+$LNG['search_search'] = "Buscar";
+$LNG['search_for'] = "Tu busqueda de:";
+$LNG['search_string'] = "Buscar una palabra?";
+$LNG['search_nosites'] = "No se han encontrado webs con ese criterio.";
+$LNG['search_prev'] = "Anterior"; // 3.2.1
+$LNG['search_next'] = "Siguiente"; // 3.2.1
+
+// Stats
+$LNG['stats_header'] = "Estadisticas";
+$LNG['stats_rating_avg'] = "Media de Votos";
+$LNG['stats_rating_num'] = "Numero de Votos";
+$LNG['stats_rate'] = "Valorar y Comentar";
+$LNG['stats_reviews'] = "Comentarios";
+$LNG['stats_error_id'] = "ID incorrecto."; // 4.0
+$LNG['stats_allreviews'] = "Ver todos los Comentarios"; // 4.0
+
+// ssi.php
+$LNG['ssi_top'] = "Top %s de top "; //4.0
+$LNG['ssi_newmembers'] = "%s Nuevos miembros del  top"; //4.0
+$LNG['ssi_join'] = "inscribirse"; //4.0
+$LNG['ssi_all'] = "Todas las Webs"; //4.0
+
+// Admin > Approve New Memers // 4.0
+$LNG['a_approve'] = "Aprovar"; // 4.0
+$LNG['a_approve_del'] = "Borrar"; // 4.0
+$LNG['a_approve_none'] = "No hay miembros esperando ser aprovados."; // 4.0
+$LNG['a_approve_done'] = "La web ha sido aprovada con exito."; // 4.0
+
+// Admin > Delete
+$LNG['a_del_mems'] = "El Miembro ha sido borrado con exito.";
+$LNG['a_del_mem'] = "Borrar un Miembror";
+
+// Admin > Edit Member Info
+$LNG['a_edit_password_blank'] = "Dejar en blanco para mantener la actual contraseña"; // 4.0
+$LNG['a_edit_site_is'] = "Esta web esta"; // 4.0
+$LNG['a_edit_active'] = "Activa (en en Top)"; // 4.0
+$LNG['a_edit_inactive'] = "Inactiva (Temporalmente Baneada, No incluida en el top)"; // 4.0
+$LNG['a_edit_error_id'] = "ID incorrecto.";
+$LNG['a_edit_error'] = "ERROR";
+$LNG['a_edit_error_forgot'] = "Has olvidado:";
+$LNG['a_edit_error_url'] = "Introducir una url valida.";
+$LNG['a_edit_error_email'] = "Introducir un email valido.";
+$LNG['a_edit_error_title'] = "Introducir el Titulo de tu Web.";
+$LNG['a_edit_error_password'] = "Introducir una Contraseña.";
+$LNG['a_edit_error_back'] = "Vuelve atras y Corrige los errores.";
+$LNG['a_edit_success'] = "EXITO!";
+$LNG['a_edit_info_edited'] = "Tu informacion de miembro ha sido actualizada con exito.";
+
+// Admin > Delete Review
+$LNG['a_del_rev'] = "Delete One Review";
+$LNG['a_del_rev_done'] = "The review has been successfully deleted.";
+$LNG['a_del_rev_id'] = "Review ID";
+
+// Admin > Email Members
+$LNG['a_email_addresses'] = "Estas son las direcciones de todos los miembros.";
+
+// Admin > Find ID
+$LNG['a_find_find'] = "Para encontra un ID, utiliza la opcion de busqueda normal que ofrece el top.  <a href=\"index.php?a=search\">Pincha Aqui</a>.";
+
+// Admin > Login
+$LNG['a_login'] = "Estas indentificado con exito.  Para salir, pincha en salir o cierra tu navegador."; // 4.1.0
+$LNG['a_login_enter'] = "ENTRAR";
+$LNG['a_login_invalidpw'] = "Contraseña Incorrecta";
+$LNG['a_login_session'] = "Your session has expired, please <a href=\"admin.php\">click here</a> to login again."; // 4.1.0
+
+// Admin > Logout
+$LNG['a_logout'] = "Acabas de Salir";
+
+// Admin > Main
+$LNG['a_main'] = "Usa los enlaces de la izquierda para ir alas diferentes areas de Administracion.";
+$LNG['a_main_approve'] = "Hay %s webs esperando ser aprovadas.";
+
+// Admin > Menu
+$LNG['a_menu'] = "Menu";
+$LNG['a_menu_main'] = "Principal";
+$LNG['a_menu_approve'] = "Aprovar Nuevos Miembros";
+$LNG['a_menu_delete'] = "Borrar Miembros";
+$LNG['a_menu_edit'] = "Editar Miembros";
+$LNG['a_menu_delete_review'] = "Delete Review";
+$LNG['a_menu_email'] = "Email Miembros";
+$LNG['a_menu_find'] = "Encontrar ID";
+$LNG['a_menu_logout'] = "SALIR";
+$LNG['a_menu_reset'] = "Reset Stats"; // 4.1.0
+$LNG['a_menu_settings'] = "Configuracion del Top";
+$LNG['a_menu_version'] = "Verificar Version";
+
+// Admin > Reset
+$LNG['a_reset_confirm'] = "This will delete all the stats.  It will not delete your members, it will only reset all their stats to 0."; // 4.1.0
+$LNG['a_reset_stats'] = "Reset Stats"; // 4.1.0
+$LNG['a_reset_done'] = "All members' stats have been reset to 0."; // 4.1.0
+
+// Admin > Settings
+$LNG['a_s_general'] = "Configuracion General";
+$LNG['a_s_list_name'] = "Nombre de tu Top";
+$LNG['a_s_deflanguage'] = "Lenguaje por defecto";
+$LNG['a_s_path'] = "El  path completo al top";
+$LNG['a_s_list_url'] = "URL para el top";
+$LNG['a_s_templates_path'] = "El path completo al directorio templates";
+$LNG['a_s_templates_url'] = "URL para el directorio templates";
+$LNG['a_s_youremail'] = "Tu Email";
+
+$LNG['a_s_sql'] = "Configuracion SQL";
+$LNG['a_s_sql_type'] = "Database Type"; // 4.1.0
+$LNG['a_s_sql_host'] = "Host";
+$LNG['a_s_sql_database'] = "Base de Datos";
+$LNG['a_s_sql_user'] = "Usuario";
+$LNG['a_s_sql_pass'] = "Contraseña";
+
+$LNG['a_s_ranking'] = "Configuracion del Ranking";
+$LNG['a_s_categories'] = "Categories (separate with commas)"; // 4.1.0
+$LNG['a_s_numlist'] = "Numero de miembros por pagina";
+$LNG['a_s_daymonth'] = "Mantener hits para dias o meses";
+$LNG['a_s_rankingmethod'] = "Metodo de Ranking"; // 4.0.2
+$LNG['a_s_featured'] = "Activate \"Featured Member\"?  You have to add <#featured> to template.html after you turn this on."; // 4.1.0
+$LNG['a_s_top'] = "Numero de miembros para el top especial";
+$LNG['a_s_adbreak'] = "Mostrar un anuncio despues del ranking (separados por comas)";
+
+$LNG['a_s_members'] = "Configuracion de Miembros";
+$LNG['a_s_active_default'] = "Requerir que los nuevos miembros sean aprovados, para pertener al top";
+$LNG['a_s_delete_after'] = "Delete inactive members after this many days"; // 4.1.0
+$LNG['a_s_email_admin_on_join'] = "Mandar Email cuando hay nuevos miembros";
+$LNG['a_s_max_banner_width'] = "Anchura maxima del Banner de los miembros";
+$LNG['a_s_max_banner_height'] = "Altura maxima del Banner de los miembrost";
+$LNG['a_s_defbanner'] = "Banner por defecto si los usuarios no tienen";
+
+$LNG['a_s_button'] = "Configuracion de la Imagen de Voto";
+$LNG['a_s_ranks_on_buttons'] = "Do you want your member's rank on their button?  See the manual for details"; // 4.0
+$LNG['a_s_button_url'] = "<b>Si -> No</b> - URL de la imagen que los usuarios utilizaran"; // 4.0
+$LNG['a_s_button_dir'] = "<b>Si -> Si</b> -  URL donde estan las imagenes"; // 4.0
+$LNG['a_s_button_ext'] = "<b>Si -> Si</b> - Extension de las imagenes (gif, png, jpg, etc.)"; // 4.0
+$LNG['a_s_button_num'] = "<b>Si -> Si</b> - Numero de images que has echo tu - Cuanto mas nuemero mas recursos ocupara el script"; // 4.0
+
+$LNG['a_s_searchs'] = "Configuracion de la Busqueda"; // 4.0
+$LNG['a_s_search_id'] = "<a href=\"http://www.10ringofhits.com/index.php?refer=50\" target=\"_blank\">10 Ring Of Hits</a> Affiliate ID - See the manual for more details"; // 4.0
+$LNG['a_s_search'] = "Caracteristicas de la Busqueda";
+$LNG['a_s_searchresults'] = "Resultados de la Busqueda por pagina";
+
+$LNG['a_s_other'] = "Otras configuraciones";
+$LNG['a_s_gzip'] = "Activar gzip - gzip comprime el codigo y acelera la carga";
+$LNG['a_s_timeoffset'] = "Tiempo de tu Servidor";
+$LNG['a_s_gateway'] = "Pagina de Salida para evitar malos usos";
+
+$LNG['a_s_on'] = "Activado";
+$LNG['a_s_off'] = "Desactivado";
+$LNG['a_s_days'] = "Dias";
+$LNG['a_s_months'] = "Meses";
+$LNG['a_s_yes'] = "Si";
+$LNG['a_s_no'] = "No";
+
+$LNG['a_s_updated'] = "Las configuraciones  han sido actualizadas.";
+
+// Admin > Version Check
+$LNG['a_version_using'] = "Estas utilizando la ultima version del Script.";
+$LNG['a_version_your'] = "Tu version";
+$LNG['a_version_latest'] = "Ultima version";
+$LNG['a_version_new'] = "Nueva version disponible: <a href=\"http://www.aardvarkind.com/index.php?page=topsitesphp\">Pincha Aqui</a>.";
+?>
