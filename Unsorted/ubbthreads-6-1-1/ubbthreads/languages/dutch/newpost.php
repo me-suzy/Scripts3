82119591c0c47ac4bb85ab8c83bf365245f76437
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:58 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/newpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/newpost.php
@@ -0,0 +1,17 @@
+<?
+$ubbt_lang['CHOOSE_NAME']  = 	"Anonieme berichten worden toegestaan, dus kun je onder een willekeurige naam berichten plaatsen.";
+$ubbt_lang['READ_PERM']  = 	"In dit forum heb jij alleen leesrechten.";
+$ubbt_lang['MAKENEW_HEAD']  = 	"Nieuw bericht plaatsen in dit forum";
+$ubbt_lang['FILL_FORM']  = 	"Vul onderstaand formulier volledig in.";
+$ubbt_lang['NO_HTML']  = 	"HTML is uitgeschakeld.";
+$ubbt_lang['YES_HTML']  = 	"HTML is ingeschakeld.";
+$ubbt_lang['NO_MARKUP']  = 	"UBBCode is uitgeschakeld.";
+$ubbt_lang['YES_MARKUP']  = 	"UBBCode is ingeschakeld. Voor informatie over UBBCode";
+$ubbt_lang['MARK_POSTS']  = 	"klik hier";
+$ubbt_lang['POST_ICON']  = 	"Bericht icoon";
+$ubbt_lang['POST_TEXT']  = 	"Bericht";
+$ubbt_lang['SPELL_CHECK']  = 	"Spellingcontrole";
+$ubbt_lang['DO_EMAIL']  = 	"Email alle reacties naar mijn email adres";
+$ubbt_lang['DO_PREVIEW']  = 	"Ik wil eerst een voorbeeld bekijken";
+$ubbt_lang['CAN_ATTACH']  = 	"en/of een bestand bijvoegen";
+?>
