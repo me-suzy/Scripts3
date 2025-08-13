c9280687c0403de09c72438bfcc1daad5da4d655
me-suzy <me.suzana@gmail.com>
2025-08-14 02:05:48 +0300

    Add Unsorted/topsites/languages/deutsch.php

---
+++ Unsorted/topsites/languages/deutsch.php
@@ -0,0 +1,297 @@
+<?php
+//=================================================================\\
+// Aardvark Topsites PHP 4.1.0                                     \\
+//-----------------------------------------------------------------\\
+// Copyright (C) 2003 Jeremy Scheff - http://www.aardvarkind.com/  \\
+//-----------------------------------------------------------------\\
+// This program is free software; you can redistribute it and/or   \\
+// modify it under the terms of the GNU General Public License     \\
+// as published by the Free Software Foundation; either version 2  \\
+// of the License, or (at your option) any later version.          \\
+//                                                                 \\
+// This program is distributed in the hope that it will be useful, \\
+// but WITHOUT ANY WARRANTY; without even the implied warranty of  \\
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the   \\
+// GNU General Public License for more details.                    \\
+//-----------------------------------------------------------------\\
+// This translation is by Ole Hansen                               \\
+// http://www.toplist.collienet.de/                                \\
+//=================================================================\\
+
+// Global
+$LNG['g_form_submit_short'] = "Los";
+$LNG['g_form_submit_long'] = "Eintragen";
+$LNG['g_form_submit_reset'] = "Reset";
+$LNG['g_id'] = "ID";
+$LNG['g_url'] = "URL";
+$LNG['g_title'] = "Titel";
+$LNG['g_description'] = "Beschreibung";
+$LNG['g_category'] = "Category"; // 4.1.0
+$LNG['g_email'] = "Email";
+$LNG['g_bannerurl'] = "Banner URL";
+$LNG['g_password'] = "Passwort";
+$LNG['g_average'] = "Durchschnitt";
+$LNG['g_today'] = "Heute";
+$LNG['g_yesterday'] = "Gestern";
+$LNG['g_2days'] = "Vor 2 Tagen";
+$LNG['g_3days'] = "Vor 3 Tagen";
+$LNG['g_thismonth'] = "Diesen Monat"; // 3.2.1
+$LNG['g_lastmonth'] = "Letzter Monat"; // 3.2.1
+$LNG['g_2months'] = "Vor 2 Monaten"; // 3.2.1
+$LNG['g_3months'] = "Vor 3 Monaten"; // 3.2.1
+$LNG['g_unq_pv'] = "Eindeutiger Besuch";
+$LNG['g_tot_pv'] = "Besuche insgesamt";
+$LNG['g_unq_in'] = "Eindeutige Hits In";
+$LNG['g_tot_in'] = "Hits insgesamt In";
+$LNG['g_unq_out'] = "Eindeutige Hits Out";
+$LNG['g_tot_out'] = "Hits insgesamt Out";
+
+// Edit Member Info - Editiere Mitglieder Info
+$LNG['edit_header'] = "Editiere Mitglied-Info";
+$LNG['edit_error_id_password'] = "Bitte ünerprüfen sie ihre ID und Passwort!";
+$LNG['edit_error'] = "Fehler";
+$LNG['edit_error_forgot'] = "Sie haben vergessen:";
+$LNG['edit_error_url'] = "Gültige URL eingeben.";
+$LNG['edit_error_email'] = "Gültige Email eingeben.";
+$LNG['edit_error_title'] = "Titel ihrer Website eingeben.";
+$LNG['edit_error_password'] = "Passwort eingeben.";
+$LNG['edit_error_back'] = "Bitte gehen sie zurück und berichtigen die Fehler.";
+$LNG['edit_success'] = "Erfolgreich!";
+$LNG['edit_info_edited'] = "Ihre Informationen sind erfolgreich übetragen.";
+
+// Gateway Page
+$LNG['gateway_header'] = "Topsites Zugangs-Seite";
+$LNG['gateway_text'] = "Um Betrug vorzubeugen, ist eine Zugangsseite eingerichtet worden. Klicken sie auf den folgenden Link um zur Topsites-Liste zu gelangen.";
+$LNG['gateway_vote'] = "Stimmen sie ab.";
+$LNG['gateway_novote'] = "Versehenttlich hier? Stimmen sie nicht ab.";
+$LNG['gateway_cheating'] = "Die Seite von der Sie kommen versucht die Topliste zu betrügen. Diese Abstimmung wird NICHT aufgenomen.";
+$LNG['gateway_enter'] = "Anmelden";
+
+// Graph
+$LNG['graph_header'] = "Diagramm";
+
+// Join
+$LNG['join_header'] = "Anmelden";
+$LNG['join_error'] = "Fehler";
+$LNG['join_error_forgot'] = "Sie haben vergessen:";
+$LNG['join_error_url'] = "Gültige URL eingeben.";
+$LNG['join_error_email'] = "Gültige Email eingeben.";
+$LNG['join_error_title'] = "Geben sie den Titel ihrer Website ein.";
+$LNG['join_error_password'] = "Passwort eingeben.";
+$LNG['join_error_urlbanner'] = "Gültigen Banner eingeben (auslassen wenn sie keinen Banner haben).  < "; // 4.0
+$LNG['join_error_back'] = "Bitte gehen sie zurück um die Fehler zu beheben.";
+$LNG['join_error_time'] = "Weniger als 120 Sekunden sind vergangen seit dem sie diese Seite besucht haben.  Bitte laden sie die Bestätigungsseite NICHT neu."; // 4.0
+$LNG['join_success'] = "Erfolgreich!";
+$LNG['join_thanks'] = "Vielen Dank!  Bitte tragen sie diesen HTML-Code in den Quellcode ihrer Seite ein, damit sie gelistet werden.";
+$LNG['join_changewarning'] = "Wenn sie den HTML-Code ändern, kann dies Fehler verursachen.";
+$LNG['join_welcome'] = "Willkommen in der Topsites-Liste!";
+$LNG['join_welcome_look'] = "Wenn es nicht richtig aussieht, benutzen sie diesen HTML-Code:";
+$LNG['join_welcome_admin'] = "Ein neues Mitglied ist ihrer Topsites-Liste beigetreten";
+
+// Lost Code
+$LNG['lostcode_header'] = "Code verloren";
+
+// Lost Password
+$LNG['lostpw_header'] = "Passwort verloren";
+$LNG['lostpw_email'] = "Um ein neues Passwort zu bekommen, gehen sie auf die folgende URL."; // 4.0.1
+$LNG['lostpw_emailed'] = "Die Anleitung wie sie ihr passwort resetten ist ihnen via Email gesendet worden.";
+$LNG['lostpw_newpassword'] = "Neues Passwort eingeben"; // 4.0.1
+$LNG['lostpw_finish'] = "Ihr Passwort entspricht nun dem neuen Passwort welches sie eben angegeben haben."; // 4.0.1
+$LNG['lostpw_error'] = "Überprüfen sie die richtige Angabe ihrer ID und wiederholen sie den vorgang.";
+
+// Main Template
+$LNG['main_header'] = "Hauptseite"; // 4.0
+$LNG['main_all'] = "All Sites"; // 4.2.0
+$LNG['main_method'] = "Rangordnung";
+$LNG['main_members'] = "Mitglieder";
+$LNG['main_menu_main'] = "Hauptseite";
+$LNG['main_menu_join'] = "Anmelden";
+$LNG['main_menu_random'] = "Zufalls Mitglied";
+$LNG['main_menu_search'] = "Suche";
+$LNG['main_menu_lostcode'] = "Code verloren";
+$LNG['main_menu_lostpw'] = "Passwort verloren";
+$LNG['main_menu_edit'] = "Mitglieds-Zugang";
+$LNG['main_featured'] = "Besonderes Mitglied"; // 4.0.2
+$LNG['main_executiontime'] = "Script Execution Time"; // 4.0
+$LNG['main_queries'] = "SQL Queries"; // 4.0
+$LNG['main_powered'] = "Powered By";
+
+// Ranking Table
+$LNG['table_stats'] = "Statistik";
+$LNG['table_unique'] = "Eindeutig";
+$LNG['table_total'] = "Insgesamt";
+$LNG['table_rank'] = "Rang";
+$LNG['table_title'] = "Titel"; // 4.0
+$LNG['table_description'] = "Beschreibung"; // 4.0
+
+// Rate and Review
+$LNG['rate_header'] = "Bewerten und Beurteilen";
+$LNG['rate_rating'] = "Bewertung";
+$LNG['rate_review'] = "Beurteilung";
+$LNG['rate_thanks'] = "Danke für die Bewertung und Beurteilung dieser Seite!";
+$LNG['rate_error'] = "Sie haben diese Seite bereits bewertet und beurteilt.";
+$LNG['rate_back'] = "Zurückt zu den Statistiken";
+
+// Search
+$LNG['search_header'] = "Suche";
+$LNG['search_internet'] = "Das Internet"; // 4.0
+$LNG['search_topsites'] = "Die Topsites-Liste"; // 4.0
+$LNG['search_off'] = "Die suche ist abgeschaltet.";
+$LNG['search_rank'] = "Rang";
+$LNG['search_search'] = "Suche";
+$LNG['search_for'] = "Sie haben gesucht nach";
+$LNG['search_string'] = "Suche als Kette?";
+$LNG['search_nosites'] = "Leider keine Suchergebnisse.";
+$LNG['search_prev'] = "Zurück"; // 3.2.1
+$LNG['search_next'] = "Weiter"; // 3.2.1
+
+// Stats
+$LNG['stats_header'] = "Statistikens";
+$LNG['stats_rating_avg'] = "Rang Mittelwert";
+$LNG['stats_rating_num'] = "Anzahl Bewertungen";
+$LNG['stats_rate'] = "Bewerten und beurteilen dieser Seite";
+$LNG['stats_reviews'] = "Beurteilungen";
+$LNG['stats_error_id'] = "ID nicht gültig."; // 4.0
+$LNG['stats_allreviews'] = "Zeige alle Beurteilungen"; // 4.0
+
+// ssi.php
+$LNG['ssi_top'] = "Top %s Sites"; //4.0
+$LNG['ssi_newmembers'] = "%s Neustes Mitglied"; //4.0
+$LNG['ssi_join'] = "Anmelden"; //4.0
+$LNG['ssi_all'] = "Alle Seiten"; //4.0
+
+// Admin > Approve New Memers // 4.0
+$LNG['a_approve'] = "Bestätigen"; // 4.0
+$LNG['a_approve_del'] = "Löschen"; // 4.0
+$LNG['a_approve_none'] = "Keine Mitglieder müssen bestätigt werden."; // 4.0
+$LNG['a_approve_done'] = "Diese Seite ist erfolgreich bestätigt worden."; // 4.0
+
+// Admin > Delete
+$LNG['a_del_mems'] = "Das Mitglied ist erfolgreich gelöscht worden.";
+$LNG['a_del_mem'] = "Mitglied löschen";
+
+// Admin > Edit Member Info
+$LNG['a_edit_password_blank'] = "Leave this blank to keep the current password"; // 4.0
+$LNG['a_edit_site_is'] = "This site is"; // 4.0
+$LNG['a_edit_active'] = "Active (Listed)"; // 4.0
+$LNG['a_edit_inactive'] = "Inactive (Temporarily Banned, Not Listed)"; // 4.0
+$LNG['a_edit_error_id'] = "Invalid ID.";
+$LNG['a_edit_error'] = "Error";
+$LNG['a_edit_error_forgot'] = "You forgot to:";
+$LNG['a_edit_error_url'] = "Enter a valid url.";
+$LNG['a_edit_error_email'] = "Enter a valid email address.";
+$LNG['a_edit_error_title'] = "Enter a title for your web site.";
+$LNG['a_edit_error_password'] = "Enter a password.";
+$LNG['a_edit_error_back'] = "Please go back and fix those errors.";
+$LNG['a_edit_success'] = "Success!";
+$LNG['a_edit_info_edited'] = "The member's information has been successfully edited.";
+
+// Admin > Delete Review
+$LNG['a_del_rev'] = "Delete One Review";
+$LNG['a_del_rev_done'] = "The review has been successfully deleted.";
+$LNG['a_del_rev_id'] = "Review ID";
+
+// Admin > Email Members
+$LNG['a_email_addresses'] = "These are the email addresses of all members.";
+
+// Admin > Find ID
+$LNG['a_find_find'] = "To find a site's ID number, just use the normal search feature of your topsites list.  <a href=\"index.php?a=search\">Click here</a>.";
+
+// Admin > Login
+$LNG['a_login'] = "You are now logged in.  To logout, click logout or exit your browser."; // 4.1.0
+$LNG['a_login_enter'] = "Enter";
+$LNG['a_login_invalidpw'] = "Invalid password!";
+$LNG['a_login_session'] = "Your session has expired, please <a href=\"admin.php\">click here</a> to login again."; // 4.1.0
+
+// Admin > Logout
+$LNG['a_logout'] = "You are logged out.";
+
+// Admin > Main
+$LNG['a_main'] = "Use the links at the left to go to the different areas of the admin.";
+$LNG['a_main_approve'] = "There are %s sites waiting to be approved.";
+
+// Admin > Menu
+$LNG['a_menu'] = "Menu";
+$LNG['a_menu_main'] = "Main Page";
+$LNG['a_menu_approve'] = "Approve New Members";
+$LNG['a_menu_delete'] = "Delete Member";
+$LNG['a_menu_edit'] = "Edit Member";
+$LNG['a_menu_delete_review'] = "Delete Review";
+$LNG['a_menu_email'] = "Email Members";
+$LNG['a_menu_find'] = "Find ID";
+$LNG['a_menu_logout'] = "Logout";
+$LNG['a_menu_reset'] = "Reset Stats"; // 4.1.0
+$LNG['a_menu_settings'] = "Settings";
+$LNG['a_menu_version'] = "Version Check";
+
+// Admin > Reset
+$LNG['a_reset_confirm'] = "This will delete all the stats.  It will not delete your members, it will only reset all their stats to 0."; // 4.1.0
+$LNG['a_reset_stats'] = "Reset Stats"; // 4.1.0
+$LNG['a_reset_done'] = "All members' stats have been reset to 0."; // 4.1.0
+
+// Admin > Settings
+$LNG['a_s_general'] = "General Settings";
+$LNG['a_s_list_name'] = "Name of your topsites list";
+$LNG['a_s_deflanguage'] = "Default language";
+$LNG['a_s_path'] = "The full path to the topsites directory";
+$LNG['a_s_list_url'] = "URL to the topsites directory";
+$LNG['a_s_templates_path'] = "The full path to the templates directory";
+$LNG['a_s_templates_url'] = "URL to the directory the templates directory";
+$LNG['a_s_youremail'] = "Your email address";
+
+$LNG['a_s_sql'] = "SQL Settings";
+$LNG['a_s_sql_type'] = "Database Type"; // 4.1.0
+$LNG['a_s_sql_host'] = "Host";
+$LNG['a_s_sql_database'] = "Database";
+$LNG['a_s_sql_user'] = "Username";
+$LNG['a_s_sql_pass'] = "Password";
+
+$LNG['a_s_ranking'] = "Ranking Settings";
+$LNG['a_s_categories'] = "Categories (separate with commas)"; // 4.1.0
+$LNG['a_s_numlist'] = "Number of members to list per page";
+$LNG['a_s_daymonth'] = "Keep hits for days or months";
+$LNG['a_s_rankingmethod'] = "Ranking method"; // 4.0.2
+$LNG['a_s_featured'] = "Activate \"Featured Member\"?  You have to add <#featured> to template.html after you turn this on."; // 4.1.0
+$LNG['a_s_top'] = "Number of members to use the special top templates for";
+$LNG['a_s_adbreak'] = "Show an ad break after these ranks (separate with commas)";
+
+$LNG['a_s_members'] = "Members Settings";
+$LNG['a_s_active_default'] = "Require new members to be approved by you before being listed";
+$LNG['a_s_delete_after'] = "Delete inactive members after this many days"; // 4.1.0
+$LNG['a_s_email_admin_on_join'] = "Email you when a new member joins";
+$LNG['a_s_max_banner_width'] = "Member's maximum banner width";
+$LNG['a_s_max_banner_height'] = "Member's maximum banner height";
+$LNG['a_s_defbanner'] = "Default banner to give members if they don't supply one when they join";
+
+$LNG['a_s_button'] = "Button Settings";
+$LNG['a_s_ranks_on_buttons'] = "Do you want your member's rank on their button?  See the manual for details"; // 4.0
+$LNG['a_s_button_url'] = "<b>If No</b> - URL to the button you want to appear on members' sites"; // 4.0
+$LNG['a_s_button_dir'] = "<b>If Yes</b> - URL to the directory the buttons are in"; // 4.0
+$LNG['a_s_button_ext'] = "<b>If Yes</b> - Extension of the buttons (gif, png, jpg, etc.)"; // 4.0
+$LNG['a_s_button_num'] = "<b>If Yes</b> - Number of buttons you have made - The higher you set this, the more resources the script will use"; // 4.0
+
+$LNG['a_s_searchs'] = "Search Settings"; // 4.0
+$LNG['a_s_search_id'] = "<a href=\"http://www.10ringofhits.com/index.php?refer=50\" target=\"_blank\">10 Ring Of Hits</a> Affiliate ID - See the manual for more details";
+$LNG['a_s_search'] = "The search feature";
+$LNG['a_s_searchresults'] = "Search results per page";
+
+$LNG['a_s_other'] = "Other Settings";
+$LNG['a_s_gzip'] = "Enable gzip - gzip saves bandwidth but uses a little more CPU power";
+$LNG['a_s_timeoffset'] = "Time offset from your server";
+$LNG['a_s_gateway'] = "Gateway page to deter cheating for hits in";
+
+$LNG['a_s_on'] = "On";
+$LNG['a_s_off'] = "Off";
+$LNG['a_s_days'] = "Days";
+$LNG['a_s_months'] = "Months";
+$LNG['a_s_yes'] = "Yes";
+$LNG['a_s_no'] = "No";
+
+$LNG['a_s_updated'] = "Your settings have been updated.";
+
+// Admin > Version Check
+$LNG['a_version_using'] = "You are using the latest version of this script.";
+$LNG['a_version_your'] = "Your version";
+$LNG['a_version_latest'] = "Latest version";
+$LNG['a_version_new'] = "New version available: <a href=\"http://www.aardvarkind.com/index.php?page=topsitesphp\">Click Here</a>.";
+?>
