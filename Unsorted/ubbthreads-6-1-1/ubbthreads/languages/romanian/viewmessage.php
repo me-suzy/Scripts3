b2393841aaf2cdab5be8d6d59f32f26af2e5e200
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:20 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/viewmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/viewmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['ABOUT']  = 		"despre";
+$ubbt_lang['RR_YOUR_MESS']  = 	"Mesajul privat pentru";
+$ubbt_lang['RR_READ']  = 	"a fost citit.";
+$ubbt_lang['RR_NOTIFY']  = 	"Ati primit o notificare de citire";
+$ubbt_lang['READ_RECEIPT']  = 	"Notificare de citire";
+$ubbt_lang['TEXT_REC']  = 	"Primit";
+$ubbt_lang['SENT']  = 		"Trimis";
+$ubbt_lang['DELETE_MESS']  = 	"Sterge";
+$ubbt_lang['REPLY_MESS']  = 	"Raspunde";
+$ubbt_lang['RETURN_START']  = 	"Pagina anterioara";
+?>
