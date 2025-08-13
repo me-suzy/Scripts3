bcb2d0a62eac38f117ab9cb487e03f44372085d1
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:30 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/adduser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/adduser.php
@@ -0,0 +1,26 @@
+<?
+$ubbt_lang['BAD_FORMAT'] = "The email address you provided has an invalid format. Make sure you're entering a valid email address (username@domain.com). A valid email address address is needed in order to mail your password.";
+$ubbt_lang['NEEDAPPROVAL'] =    "Your account needs to be approved by an Administrator before you can login.  You will receive an email once this has been done.";
+$ubbt_lang['APPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} has been approved.";
+$ubbt_lang['NOTAPPROVED'] = "Your account for {$config['title']} at {$config['phpurl']} was not approved.  The reason for this is:";
+$ubbt_lang['NEWREGBODY'] = "A new user has registered at {$config['title']}. To approve this account, please visit";
+$ubbt_lang['NEWREG'] = "New User Registration at";
+$ubbt_lang['INVALID_DATE'] = "That is an invalid date.";
+$ubbt_lang['TOOYOUNG']  =       "Registration for this message board will require approval of your parent or guardian.  You may use the <a href=\"{$config['phpurl']}/coppaform.php\">Permission form</a> for this.";
+$ubbt_lang['UNDER13'] = "Sorry, we do not allow children under 13 to register.";
+$ubbt_lang['BAD_UNAME']  = 	"You cannot use html characters in your username or have spaces before or after it.";
+$ubbt_lang['LONG_NAME']  = 	"Íåêîððåêòíàÿ äëèíà èìåíè ïîëüçîâàòåëÿ.";
+$ubbt_lang['BAD_EMAIL']  = 	"You are not allowed to register a Username from the email domain";
+$ubbt_lang['USER_EXISTS']  = 	"Ýòî èìÿ ïîëüçîâàòåëÿ óæå èñïîëüçóåòñÿ.";
+$ubbt_lang['NO_MULTI']  = 	"Íà íàøåì ôîðóìå íå ðàçðåøåíî èìåòü îäèí e-mail àäðåñ íåñêîëüêèì ó÷¸òíûì çàïèñÿì. Ó÷¸òíàÿ çàïèñü ñ óêàçàííûì Âàìè àäðåñîì E-mail óæå ñóùåñòâóåò â íàøåé áàçå äàííûõ.";
+$ubbt_lang['PASS_MATCH']  = 	"Ïàðîëè íå ñîâïàäàþò.";
+$ubbt_lang['PASS_TOO_LONG']  = 	"Ïàðîëü äîëæåí ñîäåðæàòü îò 4 äî 20 çíàêîâ.";
+$ubbt_lang['ILL_PASS']  = 	"Password has illegal characters.  Please use only alphanumeric characters.";
+$ubbt_lang['WEL_MSG']  = 	"Please take the time to edit your profile.  By editing your profile you can set some info about yourself, add a signature that will be appended to all of your posts and choose your default display properties (sort order, display mode and display view, flat or threaded mode, etc.).";
+$ubbt_lang['PASS_SUB']  = 	"Âàø ïàðîëü äëÿ";
+$ubbt_lang['PASS_BODY1']  = 	"Someone from the ip address";
+$ubbt_lang['PASS_BODY2']  = 	"registered the Username";
+$ubbt_lang['PASS_BODY3']  = 	"The password for this Username is";
+$ubbt_lang['NEW_CONFIRM']  = 	"Èìÿ ïîëüçîâàòåëÿ óñïåøíî çàðåãèñòðèðîâàíî.";
+$ubbt_lang['NEW_BODY']  = 	"Your username has been reserved.  You should be receiving an email shortly with your password.  Once you receive your password you can Login and post with this Username.  Thanks for registering a Username for our forum.  Please take the time to edit your profile before posting on the forums.  Editing your profile will allow you to customize the layout of the forums to suit your particular tastes and needs.";
+?>
