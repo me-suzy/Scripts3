b2bb62bf1970360e74ca1aa18447775acff3b120
me-suzy <me.suzana@gmail.com>
2025-08-14 01:49:35 +0300

    Add Unsorted/SSM-Beta1/release 1_standard/language.php

---
+++ Unsorted/SSM-Beta1/release 1_standard/language.php
@@ -0,0 +1,23 @@
+<?php
+//---------------------------------------------------------------------/*
+######################################################################
+# Support Services Manager											 #
+# Copyright 2002 by Shedd Technologies International | sheddtech.com #
+# All rights reserved.												 #
+######################################################################
+# Distribution of this software is strictly prohibited except under  #
+# the terms of the STI License Agreement.  Email info@sheddtech.com  #
+# for information.  												 #
+######################################################################
+# Please visit sheddtech.com for technical support.  We ask that you #
+# read the enclosed documentation thoroughly before requesting 		 #
+# support.															 #
+######################################################################*/
+//---------------------------------------------------------------------
+//messages
+$l_dberror="ERROR: Could not connect to MySQL!";
+$l_dbselecterr="Could not access database!";
+$l_account="Account:";
+$l_usercpanel="User Control Panel";
+$l_ticketsystem="Ticket System";
+?>
