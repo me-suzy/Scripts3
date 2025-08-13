89b75d0872c755b0242f547319a8139d553d4b25
me-suzy <me.suzana@gmail.com>
2025-08-14 00:59:52 +0300

    Add Unsorted/phpUploadCenter/include/de.php

---
+++ Unsorted/phpUploadCenter/include/de.php
@@ -0,0 +1,236 @@
+<?
+
+// Header and footer files
+
+$headerpage="include/header.htm";
+$footerpage="include/footer.htm";
+$infopage="include/info.htm";
+
+$charsetencoding="iso-8859-1";
+$uploadcentercaption="SatTV - Upload Center";
+$uploadcentermessage="Bitte beachten Sie: Das betrachten von Pay TV ohne gültigem Abonnement ist illegal!<br>Alle Dateien dienen nur zu Forschungszwecken!<br>Wir übernehmen keine Verantwortung für die hier gespeicherten Dateien,<br>ihr Inhalt könnte Ihr System beschädigen!";
+
+$mess=array(
+"0" => "",
+"1" => "Januar",
+"2" => "Februar",
+"3" => "März",
+"4" => "April",
+"5" => "Mai",
+"6" => "Juni",
+"7" => "July",
+"8" => "August",
+"9" => "September",
+"10" => "Oktober",
+"11" => "November",
+"12" => "Dezember",
+"13" => "Heute",
+"14" => "Gestern",
+"15" => "Dateiname",
+"16" => "Downloads",
+"17" => "Größe",
+"18" => "Upload Datum",
+"19" => "Beschreibung",
+"20" => "Datei Upload",
+"21" => "Lokale Datei",
+"22" => "Datei Beschreibung",
+"23" => "Download",
+"24" => "Sortierung",
+"25" => "Home",
+"26" => "Datei",
+"27" => "Drucken",
+"28" => "Schließen",
+"29" => "Zurück",
+"30" => "Diese Datei wurde entfernt",
+"31" => "Kann Datei nicht öffnen",
+"32" => "Zurück",
+"33" => "Datei Upload Fehler !",
+"34" => "Sie müssen eine Datei selektieren",
+"35" => "Zurück",
+"36" => "Die Datei",
+"37" => "wurde erfolgreich geladen",
+"38" => "Datei mit dem Namen",
+"39" => "existiert bereits",
+"40" => "Die Datei wurde erfolgreich geladen",
+"41" => "Sprache erfolgreich ausgewählt [übersetzung: Udo Kinzebach]",
+"42" => "Willkommen beim Sat-TV Upload Center!",
+"43" => "benutzter Speicherplatz",
+"44" => "Zeige Dateien aller Tage",
+"45" => "ungültiges ZIP-Archiv!",
+"46" => "Archiv beinhaltet:",
+"47" => "Datum / Zeit",
+"48" => "Verzeichnis",
+"49" => "es ist verboten, eine Datei mit dem Namen %s hochzuladen!",
+"50" => "überschreitet die maximal erlaubte Grösse",
+"51" => "Information",
+"52" => "wähle Design",
+"53" => "Design",
+"54" => "Willkommen",
+"55" => "aktuelle Zeit",
+"56" => "Benutzer",
+"57" => "Benutzername",
+"58" => "Register",
+"59" => "Registrierung",
+"60" => "Sonntag",
+"61" => "Montag",
+"62" => "Dienstag",
+"63" => "Mittwoch",
+"64" => "Donnerstag",
+"65" => "Freitag",
+"66" => "Samstag",
+"67" => "Senden",
+"68" => "Datei mailen",
+"69" => "Datei wurde an %s gesandt.",
+"70" => "Datei durch Nutzer %s hochgeladen",
+"71" => "einloggen",
+"72" => "ausloggen",
+"73" => "Eingeben",
+"74" => "Anonym",
+"75" => "normaler Benutzer",
+"76" => "Benutzer mit Sonderrechten",
+"77" => "Administrator",
+"78" => "privater Bereich",
+"79" => "öffentlicher Bereich",
+"80" => "Sie haben ungültigen Benutzernamen oder Kennwort eingegeben.",
+"81" => "mein Profil",
+"82" => "Anzeigen / verändern meines Profils",
+"83" => "Kennwort",
+"84" => "wählen Sie die Sprache",
+"85" => "wählen Sie die Zeitzone",
+"86" => "Ihre aktuelle Zeit",
+"88" => "bitte, geben Sie eine gültige e-mail Adresse ein.",
+"89" => "Ihr Aktivierungscode wird Ihnen per e-mail zugesandt.",
+"90" => "Datei hochgeladen: ",
+"91" => "Bitte, geben Sie die e-mail Adresse ein, die Sie bei der Registrierung benutzt haben.",
+"92" => "Dateigrösse: ",
+"93" => "Bitte, schreiben Sie sich Ihren Namen und Ihr Passwort auf",
+"94" => "Registrierung ist nötig, bitte, registrieren Sie sich.",
+"95" => "Registrierung ist nicht nötig. Sie können sich registrieren, damit bei allen von Ihnen hochgeladenen Dateien Ihr Name hinzugefügt wird",
+"96" => "Erscheigungsbild ausgewählt.",
+"97" => "erneuern",
+"98" => "Bitte geben Sie Benutzername und Passwort ein",
+"99" => "immer noch nicht registriert? - Registrieren Sie sich hier!",
+"100" => "Haben Sie Ihr Passwort vergessen?",
+"101" => "Bitte gehen Sie %s zurück %s und versuchen es noch einmal.",
+"102" => "Sie haben Sich erfolgreich abgemeldet.",
+"103" => "Benutzername ist ungültig. Der Name darf nicht länger als 12 Zeichen haben und darf nur lateinische Buchstaben und Ziffern beinhalten. Es dürfen ebenfalls '-'. '_' und Leerzeichen enthalten sein.",
+"104" => "Der von Ihnen gewählte Name '%s' ist bereits vergeben.",
+"105" => "Bestätigen Sie das Passwort",
+"106" => "Die Passwörter stimmen nicht überein.",
+"107" => "Das Format der eingegebenen E-mail Adresse ist ungültig.",
+"108" => "Vielen Dank für die Registrierung. Bitte vergessen Sie nicht Ihr Passwort, da es verschlüsselt in unserer Datenbank gespeichert wurde und wir es aus diesem Grund nicht für Sie heraussuchen können. Sollten Sie jedoch ihr Passwort einmal vergessen haben, können wir Ihnen ein neues, zufälliges Passwort zustellen.",
+"109" => "Sie können %s das Upload Center %s hier betreten",
+"110" => "Ihr Aktivierungscode wurde Ihnen zugemailt. Sie müssen Ihren Zugang innerhalb von 2 Tagen aktivieren oder er wird automatisch entfernt.",
+"111" => "
+Lieber %s,
+
+Danke dass Sie sich in unserem Upload-Center registriert haben.
+
+Da Ihre Sicherheit für uns wichtig ist, wird Ihre Anmeldung erst aktiviert, nachdem wir Ihre Bestätigung erhalten haben.
+
+Ihr persönlicher Aktivierungscode ist %s
+(Bitte beachten Sie: dies ist nicht Ihr Passwort)
+
+Die Aktivierung Ihres Zugangs ist einfach:
+1. Besuchen Sie uns unter $installurl/activate.php und wir führen Sie durch die Anmeldung
+2. Geben Sie Ihren Zugangsnamen und den Aktivierungscode ein.
+3. Klicken Sie auf den 'Zugang aktivieren' Knopf
+
+Grüsse,
+$admin_name
+Email: mailto:$admin_email
+Web Seite: $installurl",
+"112" => "Zugang aktivieren",
+"113" => "Bitte aktivieren Sie Ihren Zugang",
+"114" => "Aktivierungscode",
+"115" => "Ihr Zugang ist nun freigeschaltet.",
+"116" => "Bitte %s geben Sie hier %s ein.",
+"117" => "Der eingegebene Zugangsname oder Aktivierungscode ist ungültig.",
+"118" => "Der Zugang ist bereits aktiviert.",
+"119" => "Ich möchte die Liste der hochgeladenen Dateien als tägliche Zusammenfassung.",
+"120" => "Ändern Sie Ihr Passwort.",
+"121" => "Ihr altes Passwort",
+"122" => "Der eingegebene Zugangsname existiert nicht.",
+"123" => "Die eingegebene E-mail Adresse ist ungültig.",
+"124" => "Das neue Passwort wurde Ihnen per E-mail zugesandt.",
+"125" => "
+Lieber Benutzer des Upload-Center,
+
+Ihr neues Upload-Center-Passwort lautet %s
+
+Grüsse,
+$admin_name
+Email: mailto:$admin_email
+Web Seite: $installurl",
+"126" => "Anpassung der Zugangseinstellung",
+"127" => "Anwenden",
+"128" => "Ihr Profil wurde gespeichert.",
+"129" => "Ihr Passwort wurde gespeichert.",
+"130" => "Sie haben ein falsches altes Passwort eingegeben.",
+"131" => "Sie müssen Ihr neues Passwort eingeben.",
+"132" => "Konfiguration",
+"133" => "Hochladen",
+"134" => "Sprache und Zeitzone",
+"135" => "Zugangsstatistik",
+"136" => "Ihr Zugang wurde angelegt:",
+"137" => "Benutzerverwaltung",
+"138" => "Leser (nur betrachten)",
+"139" => "Uploader (nur hochladen)",
+"140" => "Zugang von '%s' erfolgreich geändert",
+"141" => "zuletzt",
+"142" => "alle",
+"143" => "Neue E-mail Adresse wird erst nach Bestätigung wirksam. Der Bestätigungscode wurde Ihnen an Ihre neue E-mail Adresse geschickt. Beachten Sie die Anleitung in der Mail.",
+
+"144" => "
+Lieber %s,
+
+Da Ihre Sicherheit für uns wichtig ist, wird Ihre Anmeldung erst aktiviert, nachdem wir Ihre Bestätigung erhalten haben.
+
+Ihr persönlicher Bestätigungscode lautet %s
+
+Die aktivierung der E-mail Adresse ist einfach
+1. Besuchen Sie uns unter $installurl/confirm.php und wir werden Sie durch den Vorgang führen
+2. Geben Sie Ihren Zugangsnamen und den Aktivierungscode ein.
+3. Klicken Sie auf den 'Bestätigen' Knopf
+
+Grüsse,
+$admin_name
+Email: mailto:$admin_email
+Web Seite: $installurl",
+"145" => "Bitte bestätigen Sie Ihre neue E-mail Adresse.",
+"146" => "Bestätigungscode",
+"147" => "Bestätigung",
+"148" => "nichts zu bestätigen.",
+"149" => "Der eingegebene Zugangsname oder Bestätigungscode ist ungültig.",
+"150" => "Ihre neue E-mail Adresse '%s' wurde bestätigt.",
+"151" => "Dateien hochgeladen",
+"152" => "Dateien heruntergeladen",
+"153" => "Dateien gemailt",
+"154" => "Zugang erzeugt",
+"155" => "letzter Zugriff",
+"156" => "Status",
+"157" => "gegenwärtiger Status",
+"158" => "empfangen von Informationszusammenfassungen",
+"159" => "E-mail",
+"160" => "gesamt:",
+"161" => "Zugang(e)",
+"162" => "Zugang löschen",
+"163" => "%s von %s Zugängen angezeigt",
+"164" => "Konfigurieren des Upload Centers",
+"165" => "Dateien editieren",
+"166" => "Datei editieren",
+"167" => "Datei %s wurde erfolgreich geändert",
+"168" => "Speichern",
+"169" => "löschen",
+"170" => "Dateien löschen",
+"171" => "Kopie",
+"172" => "ja",
+"173" => "nein",
+"174" => "aktiv",
+"175" => "inaktiv",
+"176" => "unberechtigt",
+"177" => "Sorry, but server could not execute the mail program.",
+"178" => "Your registration failed. Please, try again later.",
+"179" => "Please, try again later."
+);
+?>
