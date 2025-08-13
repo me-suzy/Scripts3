23b6cbf37f3ebc26992d47abd3bb859fd247ae02
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:06 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/showmembers.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/showmembers.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['USER_STATUS']  = 	"Rango";
+$ubbt_lang['TEXT_HOME']  = 	"Pagina Web";
+$ubbt_lang['TOTAL_POSTS']  = 	"Total de mensajes";
+$ubbt_lang['PROF_ICQ']  = 	"N&uacute;mero de ICQ";
+$ubbt_lang['REGGED_ON']  = 	"Registrado el";
+$ubbt_lang['PAGE_TEXT']  = 	"P&aacute;ginas";
+?>
