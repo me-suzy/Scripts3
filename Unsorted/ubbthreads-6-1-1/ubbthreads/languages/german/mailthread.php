24dc79680b5639d20e9b3495ac936635c8ef2e0d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:37:26 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/mailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/mailthread.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['MAIL_FRIEND']  = 	"Diesen Thread als eMail an einen Freund senden";
+$ubbt_lang['FRIEND_HEAD']  = 	"Geben Sie bitte die eMail-Adresse der Person an, die den Thread erhalten soll.";
+$ubbt_lang['SENT_FROM1']  = 	"Der Absender dieser eMail ist Ihre registrierte eMail-Adresse";
+$ubbt_lang['SENT_FROM2']  = 	"und Ihr Benutzername";
+$ubbt_lang['USE_REAL']  = 	"Wenn Sie statt dessen Ihren echten Namen verwenden m&ouml;chten, geben Sie ihn hier ein:";
+$ubbt_lang['WHAT_EMAIL']  = 	"eMail-Adresse des Empf&auml;ngers:";
+$ubbt_lang['SEND_IT']  = 	"Senden!";
+?>
