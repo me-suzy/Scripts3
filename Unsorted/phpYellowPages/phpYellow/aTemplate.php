d4e629bfc7400647d74e45eb1d25544e42a0292a
me-suzy <me.suzana@gmail.com>
2025-08-14 01:04:36 +0300

    Add Unsorted/phpYellowPages/phpYellow/aTemplate.php

---
+++ Unsorted/phpYellowPages/phpYellow/aTemplate.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+	<HEAD>
+	<script language="Javascript" src="yellow.js"></script>
+	<script language="Javascript">loadCSSFile();</script>
+
+    <?php require("util.php"); ?>
+    <?php echo "<TITLE>" . YOURPHPYELLOWNAME . " - " . COMPANY . " - A Dreamriver.com Software Product - Easily create classified ads or Yellow Page directories! phpYellow Pages from Dreamriver.com - php software mysql database software download apps</TITLE>"; ?>
+
+	<META NAME="keywords" CONTENT="phpYellow, php, Yellow, Search, for, add, change, or, delete, your, product, or, service, listing(s), on, our, Yellow, Pages, It's, fast, free.">
+	<META NAME="description" CONTENT="Backup for phpYellow Pages.">
+	<META NAME="Author" CONTENT="Richard Creech, Web: http://www.dreamriver.com Email: richardc@dreamriver.com">
+	<META NAME="GENERATOR" CONTENT="Blood, Sweat & Tears">
+	</HEAD>
+
+<BODY>
+
+<?php include("header.php");?> 
+
+
+
+<h1>Template</h1>
+
+
+<p>Content goes here</p>
+
+
+<?php include("footer.php");?>
+
+
+
