133cb5f90a61c4ab807caa07bcfb0da21ad0bbdb
me-suzy <me.suzana@gmail.com>
2025-08-14 02:29:42 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/login.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/login.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['FORGOT_PASS']   = "Forget your password?";
+$ubbt_lang['FORGOT_ENTER']  = "Enter your email address or username that you registered with to have a temporary password mailed to you.";
+$ubbt_lang['REMEMBER_ME']   =  "Remember me on each visit.";
+$ubbt_lang['LOGIN_PROMPT2'] =	"ÇëÊäÈëÕÊºÅºÍÃÜÂëµÇÈëÏµÍ³£¬Èç¹ûÄúÊÇÐÂ½øÍøÓÑ£¬ÉÐÎ´×¢²á×¨ÊôÕÊºÅ£¬Çë¼ÇµÃÏÈ×¢²áÈ¡µÃ×¨ÊôÕÊºÅ£¬²Å¿ÉÒÔ´æÈ¡±¾Õ¾Ïà¹ØÌÖÂÛÇø¼°¹¦ÄÜ£¡";
+$ubbt_lang['LOGIN_PROMPT'] =	"ÇëÏÈµÇÈë";
+$ubbt_lang['REG_ONE'] =	"[µã°´´ËÁ¢¼´×¢²á]";
+$ubbt_lang['PASSWORD_TEXT'] =	"Í¨¹ØÃÜÂë";
+?>
