2000a72de09bb2fac4679a39873ca43045f6a065
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:02 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/newreply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/newreply.php
@@ -0,0 +1,19 @@
+<?
+$ubbt_lang['READ_PERM']  = 	"Les privilèges associés à votre compte sur ce forum ne vous permettent que la lecture";
+$ubbt_lang['NOT_APP']  = 	"Vous ne pouvez pas répondre à un message qui n'a pas été approuvé.";
+$ubbt_lang['REPLY_HEAD']  = 	"Répondre à";
+$ubbt_lang['FILL_FORM']  = 	"Complétez le formulaire ci-dessous pour écrire un message dans le forum.";
+$ubbt_lang['NO_HTML']  = 	"Le code HTML est désactivé dans ce forum, vous ne pouvez donc pas l'utiliser.";
+$ubbt_lang['YES_HTML']  = 	"Le code HTML est activé, vous pouvez donc l'utiliser.";
+$ubbt_lang['NO_MARKUP']  = 	"Le code UBB est désactivé dans ce forum, vous ne pouvez donc pas l'utiliser.";
+$ubbt_lang['YES_MARKUP']  = 	"Le code UBB est activé, vous pouvez donc l'utiliser";
+$ubbt_lang['MARK_POSTS']  = 	"Code UBB dans vos messages";
+$ubbt_lang['CHOOSE_NAME']  = 	"Les messages anonymes sont autoriés, vous pouvez donc choisir un nom qui n'a pas encore été enregistré pour écrire.";
+$ubbt_lang['POST_ICON']  = 	"Icône du Message";
+$ubbt_lang['POST_TEXT']  = 	"Message";
+$ubbt_lang['SPELL_CHECK']  = 	"Vérifier l'orthographe";
+$ubbt_lang['DO_EMAIL']  = 	"Envoyer toutes les réponses à ma vraie adresse email";
+$ubbt_lang['DO_PREVIEW']  = 	"Je souhaite voir un aperçu de mon message";
+$ubbt_lang['CAN_ATTACH']  = 	"et/ou joindre un fichier";
+$ubbt_lang['IN_RESPONSE']  = 	"En réponse à";
+?>
