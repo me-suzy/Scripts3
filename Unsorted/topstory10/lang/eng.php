bfd53acb69ed33680d63fae8f69b968af033440f
me-suzy <me.suzana@gmail.com>
2025-08-14 02:07:51 +0300

    Add Unsorted/topstory10/lang/eng.php

---
+++ Unsorted/topstory10/lang/eng.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * ADMIN SECTION
+ */
+$tst["lang"]["postNewsFormHeader"]="Post News"; 
+$tst["lang"]["newsHeading"]="Headline"; 
+$tst["lang"]["newsBody"]="Body"; 
+$tst["lang"]["postUnsuccess"]="Unable to post the article. Please try again..."; # if failure to post the news to the db
+$tst["lang"]["postSuccess"]="Your article succesfully posted..."; # if success for posting the message
+$tst["lang"]["resetFormBtn"]="Reset"; # reset button
+$tst["lang"]["postNewsBtn"]="Post"; # post news button
+$tst["lang"]["edit"]="Edit"; 
+$tst["lang"]["delete"]="Delete"; 
+$tst["lang"]["management"]="Management";
+$tst["lang"]["delSelectedBtn"]="Delete Selected Articles";
+$tst["lang"]["deleteRecordSuccess"]="Record Deleted succesfully";
+$tst["lang"]["redirecting"]="Redirecting to articles listings in 3 seconds.... Please wait....";
+$tst["lang"]["redirectingIndex"]="Redirecting to admin pages in 3 seconds.... Please wait....";
+$tst["lang"]["click2redirect"]="If you are not redirected autmatically in 3 seconds, click here to return to listing";
+$tst["lang"]["click2redirectIndex"]="If you are not redirected autmatically in 3 seconds, click here to return to Main page";
+$tst["lang"]["deleteRecordFailure"]="Unable to delete the record ! Please try again.";
+$tst["lang"]["sure2delete"]="Are you sure you want to delete the selected record ?";
+$tst["lang"]["sure2deleteSelected"]="Are you sure you want to delete the selected record(s) ?";
+$tst["lang"]["yes"]="YES";
+$tst["lang"]["no"]="NO";
+$tst["lang"]["deleteAllBtn"]="Delete All the Records";
+$tst["lang"]["deleteAllSuccess"]="All the records deleted succesfully";
+$tst["lang"]["deleteAllFailure"]="Unable to delete all the records ! Please try again.";
+$tst["lang"]["sure2deleteAll"]="Are you sure you want to delete all the records ?";
+$tst["lang"]["records"]="Records";
+$tst["lang"]["updateNewsBtn"]="Update"; # update news button
+$tst["lang"]["editNewsFormHeader"]="Edit News";
+$tst["lang"]["updateSuccess"]="Record updated succesfully"; # if success for updating record
+$tst["lang"]["updateFailure"]="Unable to update the record!";
+$tst["lang"]["status"]="Status";
+$tst["lang"]["statusUpdateSuccess"]="Status changed succesfully";
+$tst["lang"]["statusUpdateFailure"]="Unable to change the status of the record";
+$tst["lang"]["statusActive"]="Active";
+$tst["lang"]["statusPassive"]="Passive";
+$tst["lang"]["click2ChangeStatus"]="Click to change the status of the record";
+$tst["lang"]["postNews"]="Post News";
+$tst["lang"]["listManage"]="List / Manage Articles";
+$tst["lang"]["country"]="Country";
+$tst["lang"]["home"]="Home";
+$tst["lang"]["isEmpty"]=" should not be empty";
+$tst["lang"]["pleaseCheckFields"]="Please goback and check the fileds below :";
+$tst["lang"]["emailInvalid"]="Your email address seems to be invalid";
+$tst["lang"]["welcomeText"]="Welcome to Top Story - Basic - Administration area";
+$tst["lang"]["totalActive"]="Active Articles";
+$tst["lang"]["totalPassive"]="In-active Articles";
+$tst["lang"]["totalArticles"]="Total Articles";
+$tst["lang"]["statSummary"]="Database Statistics Summary";
+/**
+ * COMMON VARIABLES (BOTH ADMIN AND USER)
+ */
+$tst["lang"]["name"]="Name";
+$tst["lang"]["surname"]="Surname";
+$tst["lang"]["email"]="Email";
+$tst["lang"]["website"]="Website";
+$tst["lang"]["counrty"]="Counrty";
+$tst["lang"]["datePosted"]="Date Posted";
+/**
+ * USER SECTION
+ */
+$tst["lang"]["noArticles"]="No articles to display..."; # if no articles to display
+$tst["lang"]["headlinesHeader"]="Articles :"; # headlines header
+$tst["lang"]["moreArticles"]="More"; # if more articles than displayed, value of the button
+$tst["lang"]["postedBy"]="Posted By";
+$tst["lang"]["ip"]="Ip";
+$tst["lang"]["new"]="new";
+$tst["lang"]["latestHeadlinesHeader"]="Latest Articles :";
+$tst["lang"]["back2Annc"]="Back to articles";
+$tst["lang"]["displayed"]="Displayed";
+$tst["lang"]["times"]="times";
+?>
