fb5296082de3018f082599cd384a52435bb50326
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:07 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/addfavforum.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/addfavforum.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM']  =  "Omdat je geen rechten hebt dit forum te bekijken kun je het ook niet als 'Favoriet' toevoegen";
+$ubbt_lang['DUP_ENTRY']  =  "Dit forum komt al in jouw 'Favoriet' lijst voor.";
+$ubbt_lang['FORUM_IN']  =  "Forum toegevoegd";
+$ubbt_lang['FORUM_CONFIRM']  =  "Het geselecteerde forum is aan de lijst toegevoegd.  Het forum kun je in de lijst vinden onder 'Mijn profiel' tot op het moment dat je het verwijderd.";
+$ubbt_lang['FORUM_RETURN']  =  "Ga nu terug naar het forum.";
+$ubbt_lang['TOO_MANY']  =  "Je kunt maximaal 83 forums in je favorieten lijst opslaan";
+?>
