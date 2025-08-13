65949b7b80d32a876ec25cadc2b628dc314992f0
me-suzy <me.suzana@gmail.com>
2025-08-14 02:39:32 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/hungarian/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"Jelszó megkérdezése";
+$ubbt_lang['PASS_REQ_BOD1']  = 	"Valaki a következõ ip címrõl";
+$ubbt_lang['PASS_REQ_BOD2']  = 	"átmeneti jelszót kért a névhez";
+$ubbt_lang['PASS_REQ_BOD3']  = 	"a következõ fórumhoz: '{$config['title']}'. Ezt használhatja akkor ha elfelejtette a jelszavát. Ha nem Ön kérte akkor enlnézést kérünk és hagyja figyelmen kívül ezt a levelet.  Az ehez a névhez tartozó jelszó --";
+$ubbt_lang['PASS_MAILED']  = 	"A jelszó elküldve";
+$ubbt_lang['PASS_MAILED2']  = 	"Az átmeneti jelszó elküldve.";
+$ubbt_lang['NO_EMAIL']  = 	"Nincs e-mail cím:";
+?>
