447eec1fbbe46896699a9df702f8294ef5230b6a
me-suzy <me.suzana@gmail.com>
2025-08-14 01:00:15 +0300

    Add Unsorted/phpx/forums.php

---
+++ Unsorted/phpx/forums.php
@@ -0,0 +1,26 @@
+<?php
+#$Id: forums.php,v 1.8 2003/10/15 17:23:09 ryan Exp $
+/*  PHPX
+Copyright 2003 Ryan "Xnuiem" Meinzer <xnuiem@phpx.org">
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+require_once('includes/functions.inc.php');
+//require_once('includes/global.inc.php');
+//$startTime = getStartTime();
+new website();
+//getEndTime($startTime, $_SERVER["PHP_SELF"], $_SERVER["QUERY_STRING"]);
+?>
