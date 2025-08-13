67003a858454528805af77173518d46194d92138
me-suzy <me.suzana@gmail.com>
2025-08-14 02:42:32 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/showmembers.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/norwegian/showmembers.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['USER_STATUS']  = 	"Status";
+$ubbt_lang['TEXT_HOME']  = 	"Hjemmeside";
+$ubbt_lang['TOTAL_POSTS']  = 	"Totalt antall innlegg";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ-nummer";
+$ubbt_lang['REGGED_ON']  = 	"Registrerte seg som bruker den";
+$ubbt_lang['PAGE_TEXT']  = 	"Sider";
+?>
