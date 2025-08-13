795c8b5e525f3a1cb8460c2daa0da94ede4410c3
me-suzy <me.suzana@gmail.com>
2025-08-14 02:48:15 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/newpost.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/newpost.php
@@ -0,0 +1,17 @@
+<?
+$ubbt_lang['CHOOSE_NAME']  = 	"Anonymous postings are allowed, so you may choose any unregistered name to post under.";
+$ubbt_lang['READ_PERM']  = 	"You have read only privileges for this forum";
+$ubbt_lang['MAKENEW_HEAD']  = 	"Äîáàâèòü ñîîáùåíèå - Ôîðóì";
+$ubbt_lang['FILL_FORM']  = 	"Äëÿ äîáàâëåíèÿ ñîîáùåíèÿ íà ýòîò ôîðóì çàïîëíèòå ïðèâåä¸ííóþ ôîðìó.";
+$ubbt_lang['NO_HTML']  = 	"HTML is disabled for this forum so you may not use it in your posts.";
+$ubbt_lang['YES_HTML']  = 	"Ðàçìåòêà HTML íà ýòîì ôîðóìå ðàçðåøåíà è Âû ìîæåòå èñïîëüçîâàòü å¸ â ñâîèõ ñîîáùåíèÿõ.";
+$ubbt_lang['NO_MARKUP']  = 	"UBBCode is disabled for this forum so you may not use UBBCode in your posts.";
+$ubbt_lang['YES_MARKUP']  = 	"UBBCode is enabled, so you may use";
+$ubbt_lang['MARK_POSTS']  = 	"UBBCode in your posts";
+$ubbt_lang['POST_ICON']  = 	"Ïèêòîãðàììà";
+$ubbt_lang['POST_TEXT']  = 	"Ñîîáùåíèå";
+$ubbt_lang['SPELL_CHECK']  = 	"Check spelling";
+$ubbt_lang['DO_EMAIL']  = 	"Îòïðàâëÿòü ìíå ïî ýëåêòðîííîé ïî÷òå îòâåòû";
+$ubbt_lang['DO_PREVIEW']  = 	"I want to preview my post";
+$ubbt_lang['CAN_ATTACH']  = 	"and/or attach a file";
+?>
