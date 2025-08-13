31e3e33a17b43ca8eea5a0cab711abd3716730ec
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:44 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/mess_reply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/mess_reply.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['EDIT_MORE']  = 	"Editeaza mai departe";
+$ubbt_lang['REPLY_PREV']  = 	"Previzualizeaza raspunsul mesajului catre";
+$ubbt_lang['PREVIEW_MESS']  = 	"Acesta este mesajul in forma finala. Daca totul este in regula apasati pe 'Continua' si mesajul va fi trimis. Daca nu, folositi formularul de mai jos pentru a-l edita in continuare.";
+$ubbt_lang['MESS_PREVIEW']  = 	"Vreau sa previzualizez measajul";
+$ubbt_lang['PRIV_SUB']  = 	"Aveti mesaje noi";
+$ubbt_lang['PRIV_BODY_R']  = 	"Ati primit un mesaj privat pe forum. Puteti merge la {$config['phpurl']}/login.php pentru a-l vedea.";
+?>
