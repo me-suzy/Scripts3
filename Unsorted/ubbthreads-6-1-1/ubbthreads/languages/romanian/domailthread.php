bd95cde9cbca5cd4570fa20acf3ddf7ec3195598
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:16 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/domailthread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/domailthread.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['START']  = 		"v-a trimis un link catre un mesaj intitulat";
+$ubbt_lang['START2']  = 		"de pe '{$config['title']}'.";
+$ubbt_lang['TO_READ']  = 	"Atentie: *NU* raspundeti la acest email pentru ca nu va ajunge unde trebuie.\nRaspunsurile la acest mesaj pot fi facute numai pe web, la adresa: ";
+$ubbt_lang['NOTE_FROM']  = 	"Mentiuni suplimentare (pot sa lipseasca) facute de ";
+$ubbt_lang['POST_BELOW']  = 	"Mesajul propriu-zis urmeaza in continuare:";
+$ubbt_lang['EMAIL_SENT']  = 	"Email-ul a fost trimis";
+$ubbt_lang['EMAIL_CONFIRM']  = 	"Un email cu mesajul selectat a fost trimis catre";
+$ubbt_lang['FORUM_RETURN']  = 	"Intoarcere la forum"; 
+?>
