30529506ca1df07342c9d6197d89221d228015d7
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:15 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/togglesub.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/togglesub.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['UN_HEADER']  = 	"Ati fost dezabonat";
+$ubbt_lang['HEADER']  = 		"Ati fost abonat";
+$ubbt_lang['UN_BODY']  = 	"Ati fost dezabonat de pe forumul: ";
+$ubbt_lang['BODY']  = 		"Ati fost abonat la forumul: ";
+?>
