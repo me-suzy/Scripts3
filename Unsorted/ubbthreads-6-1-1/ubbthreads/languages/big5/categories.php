7be78d4bf8b1cd14d7b283301c928c525740808b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:28 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/categories.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/categories.php
@@ -0,0 +1,15 @@
+<?
+$ubbt_lang['NEW_MEM'] = "Åwªï·s¥[¤Jªººô¤Í¡G";
+$ubbt_lang['VIEW_PAST']  =     "Æ[¬Ý³Ìªñªº¤å³¹";
+$ubbt_lang['PAST24']  =        "³Ìªñ¤@¤Ñ";
+$ubbt_lang['PAST48']  =        "³Ìªñ¨â¤Ñ";
+$ubbt_lang['PAST7']  =         "³Ìªñ¤@¶g";
+$ubbt_lang['THREAD_TEXT']  = 	"¸ÜÃD";
+$ubbt_lang['CAT_FORUMS']  = 	"°Q½×°Ï";
+$ubbt_lang['REGED_USERS'] =	"¦ìµù¥U¨Ï¥ÎªÌ¡C";
+$ubbt_lang['WEL_PRIV_NO']  = 	"±zÁÙ¨S¦³µn¤J¡C";
+$ubbt_lang['CAT_TEXT']  = 	"¤ÀÃþ";
+$ubbt_lang['POSTS_TEXT']  = 	"¤å³¹";
+$ubbt_lang['NEW_TEXT']  = 	"·s";
+$ubbt_lang['NOT_APPROVED'] =	"©|¥¼®Ö¥i";
+?>
