b6e37513d47c7335f8e4f87c94065f3320c68416
me-suzy <me.suzana@gmail.com>
2025-08-14 02:40:50 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/sendmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/italian/sendmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['CHOOSE_ADD']  = 	"Choose a name";
+$ubbt_lang['TEXT_DELETE']  = 	"Cancella";
+$ubbt_lang['NO_PRIVATE']  = 	"Questo utente non accetta messaggi in privato.";
+$ubbt_lang['NO_RECORD']  = 	"Stai cercando di inviare un messaggio ad un nome utente non registrato.";
+$ubbt_lang['MESS_PREV']  = 	"Preview private message to";
+$ubbt_lang['PREVIEW_MESS']  = 	"The following is a preview of your message.  If everything looks ok then you can click 'Continue' and your message will be sent.  If not then use the bottom form below to edit some more.";
+$ubbt_lang['EDIT_MORE']  = 	"Edit some more";
+$ubbt_lang['PRIV_SUB']  = 	"Hai nuovi messaggi";
+$ubbt_lang['PRIV_BODY_R']  = 	"Puoi ricevere un messaggio privato nei forum a .  Puoi visualizzarlo alla URL  {$config['phpurl']}/login.php";
+$ubbt_lang['MESS_SENT']  = 	"Il tuo messaggio è stato inviato.";
+?>
