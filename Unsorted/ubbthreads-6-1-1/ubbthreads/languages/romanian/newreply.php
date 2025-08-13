7aeaad3d1cbc26d6952745ca1bf58c161a025dd5
me-suzy <me.suzana@gmail.com>
2025-08-14 02:46:49 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/newreply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/romanian/newreply.php
@@ -0,0 +1,19 @@
+<?
+$ubbt_lang['READ_PERM']  = 	"In acest forum aveti drepturi doar de citire";
+$ubbt_lang['NOT_APP']  = 	"Nu puteti raspunde la un post care nu a fost aprobat inca.";
+$ubbt_lang['REPLY_HEAD']  = 	"Raspunde";
+$ubbt_lang['FILL_FORM']  = 	"Pentru a face un post, completati formularul care urmeaza.";
+$ubbt_lang['NO_HTML']  = 	"HTML este dezactivat in acest forum si nu poate fi folosit.";
+$ubbt_lang['YES_HTML']  = 	"HTML este activat si poate fi folosit.";
+$ubbt_lang['NO_MARKUP']  = 	"UBBCode-ul este dezactivat in acest forum si nu poate fi folosit.";
+$ubbt_lang['YES_MARKUP']  = 	"UBBCode este activat, asa ca puteti folosi";
+$ubbt_lang['MARK_POSTS']  = 	"UBBCode in mesaje";
+$ubbt_lang['CHOOSE_NAME']  = 	"Post-urile anonime sint permise asa ca puteti folosi orice nume care nu este intrgistrat.";
+$ubbt_lang['POST_ICON']  = 	"Icon pentru mesaj";
+$ubbt_lang['POST_TEXT']  = 	"Post";
+$ubbt_lang['SPELL_CHECK']  = 	"Corecteaza";
+$ubbt_lang['DO_EMAIL']  = 	"Trimite toate raspunsurile la adresa reala de email.";
+$ubbt_lang['DO_PREVIEW']  = 	"Vreau sa vad un preview";
+$ubbt_lang['CAN_ATTACH']  = 	"si/sau sa atasez un fisier";
+$ubbt_lang['IN_RESPONSE']  = 	"Ca raspuns la";
+?>
