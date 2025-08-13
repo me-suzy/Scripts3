55d57b287216f20f6002b23aef4c4d20c4c8ad7b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:31 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/viewmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/viewmessage.php
@@ -0,0 +1,11 @@
+<?
+$ubbt_lang['ABOUT']  = 	"Onderwerp";
+$ubbt_lang['RR_YOUR_MESS']  = 	"Jouw privé-bericht aan";
+$ubbt_lang['RR_READ']  = 	"is ontvangen.";
+$ubbt_lang['RR_NOTIFY']  = 	"Je hebt een ontvangstbevestiging ontvangen";
+$ubbt_lang['READ_RECEIPT']  = 	"Ontvangstbevestiging";
+$ubbt_lang['TEXT_REC']  = 	"Ontvangen";
+$ubbt_lang['SENT']  = 	"Verzonden";
+$ubbt_lang['DELETE_MESS']  = 	"Wissen";
+$ubbt_lang['REPLY_MESS']  = 	"Beantwoorden";
+?>
