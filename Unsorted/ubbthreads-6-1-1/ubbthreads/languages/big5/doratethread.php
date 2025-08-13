9dd4e6a35eb12ec494d19d22fb170070f9aede8d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:46 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/doratethread.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/doratethread.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['INVALID_RATING'] = "That is an invalid rating.";
+$ubbt_lang['NOMORERATE'] = "±z¤w¸g¹ï¦¹Ä³ÃD¥´¹L¤À¼Æ¤F¡C";
+$ubbt_lang['THANKS'] = "ÁÂÁÂ±z¡C";
+$ubbt_lang['RATED']	= "±z©Òµûªº¤À¼Æ¤w¸g³Q¥[¨ì¨t²Î¤º¡C";
+?>
