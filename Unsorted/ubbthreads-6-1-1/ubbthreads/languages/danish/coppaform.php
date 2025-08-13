b858cb564b4a34f1cc3ed7955201baad45fd364b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:30:49 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/coppaform.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/danish/coppaform.php
@@ -0,0 +1,21 @@
+<?
+
+$ubbt_lang['PARENT_APP'] = "Parental Approval Required";
+$ubbt_lang['PARENT_APP_2'] = "Your parent or guardian must approve your registration.  You will not be able to register an account until this approval is received.  To create your account, please print out this page and follow the directions below.";
+$ubbt_lang['MORE_INFO'] = "For more information, contact <a href=\"mailto:{$config['emailaddy']}\">{$config['emailaddy']}</a>";
+$ubbt_lang['LOGIN_NAME'] = "Username";
+$ubbt_lang['LOGIN_NAME2'] = "You may wish to provide multiple usernames so that we may use an alternative if your username has been taken.";
+$ubbt_lang['YOUR_EMAIL'] = "Email address";
+$ubbt_lang['BIRTHDATE'] = "Birth Date";
+$ubbt_lang['PARENT_INST'] = "Instructions for Parent or Guardian";
+$ubbt_lang['PARENT_INST2'] = "Please sign the form below and send it to us.";
+$ubbt_lang['COPPA_INST'] = "COPPA Instructions";
+$ubbt_lang['AGREE'] = "YES: I have reviewed the information my child has supplied and I have read the Privacy Policy for the web site. I understand that the profile information may be changed using the login and password for this registration.";
+$ubbt_lang['FULLNAME'] = "Parent/Guardian Full Name";
+$ubbt_lang['SIGNATURE'] = "Signature";
+$ubbt_lang['RELATION'] = "Relation to Child";
+$ubbt_lang['TELE'] = "Telephone number (or other offline contact method)";
+$ubbt_lang['PARENT_EMAIL'] = "Parent/Guardian Email address";
+$ubbt_lang['DATE'] = "Date";
+$ubbt_lang['REG_DATE'] = "Registration Date";
+$ubbt_lang['QUESTIONS'] = "Questions? Email";
