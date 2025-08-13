3f568545d2e465f74787c5842d05b2d84089f3eb
me-suzy <me.suzana@gmail.com>
2025-08-14 00:33:02 +0300

    Add Unsorted/PHPCheckout/phpcheckout/navAllAdmin.php

---
+++ Unsorted/PHPCheckout/phpcheckout/navAllAdmin.php
@@ -0,0 +1,5 @@
+<div class="favcolor2" align="center">
+<a href="workWithCustomer.php">Work with Customer</a> | 
+<a href="workWithItem.php">Work with Item</a> | 
+<a href="workWithSurvey.php">Work With Survey</a>
+</div>
