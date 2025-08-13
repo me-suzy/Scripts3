466ad1812cc7da1df0731741a9d87e6be6b4f0f6
me-suzy <me.suzana@gmail.com>
2025-08-14 02:36:26 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/start_page.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/start_page.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['PASS_REQ_SUB']  = 	"Demande de mot de passe";
+$ubbt_lang['PASS_REQ_BOD1']  = 	"Quelqu'un avec l'adresse IP";
+$ubbt_lang['PASS_REQ_BOD2']  = 	"a demandé un mot de passe temporaire pour l'identifiant";
+$ubbt_lang['PASS_REQ_BOD3']  = 	"pour le site Internet '{$config['title']}'.  Ceci est un mot de passe temporaire qui peut être utilisé si vous avez oubliez votre mot de passe d'origine.  Si vous n'avez pas fait de demande, merci d'ignorer ce message.  Le mot de passe temporaire pour cet identifiant est --";
+$ubbt_lang['PASS_MAILED']  = 	"Mot de passe envoyé";
+$ubbt_lang['PASS_MAILED2']  = 	"Un mot de passe temporaire pour cet utilisateur a été envoyé par email.";
+$ubbt_lang['NO_EMAIL']  = 	"Nous ne trouvons pas d'adresse email pour";
+?>
