8a257afa362e7c4183e52bede16c1991481edc4c
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:10 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/mess_handler.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/mess_handler.php
@@ -0,0 +1,10 @@
+<?
+$ubbt_lang['REPLY_HEAD']   =   "±H«Hµ¹";
+$ubbt_lang['MESS_COPY']  =     "§Ú·Q­n«O¯d§Ú³o«Ê«H§@¬°±H¥ó³Æ¥÷";
+$ubbt_lang['DELETE_MESS']  = 	"§R°£³o½g¤å³¹";
+$ubbt_lang['REPLY_MESS']  = 	"¦^À³³o½g¤å³¹";
+$ubbt_lang['RETURN_START']  = 	"¦^¨ì±zªº­º­¶";
+$ubbt_lang['MESS_RECEIPT']  = 	"·í¹ï¤èÅª¹L§Úªº«H«á¡A§Ú§Æ±æ¨t²Îµo¤@«Ê«Hµ¹§Ú";
+$ubbt_lang['MESS_PREVIEW']  = 	"¦b±H¥X¥h«H¥ó¤§«e¡A§Ú§Æ±æ¯à¹wÄý§Úªº«H¥ó";
+$ubbt_lang['ORIG_MESS']  = 	"­ì©l¤å³¹";
+?>
