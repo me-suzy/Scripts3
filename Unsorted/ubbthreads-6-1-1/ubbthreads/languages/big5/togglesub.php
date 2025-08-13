fc3c768e88dc681bac6d54d9a90e930f420cdd1d
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:45 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/togglesub.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/togglesub.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['UN_HEADER']  = 	"±z¤w¸g¨ú®ø­q¾\\¤F";
+$ubbt_lang['HEADER']  = 	"±z¤w¸g­q¾\\¤F";
+$ubbt_lang['UN_BODY'] =	"±z¤w¸g¨ú®ø­q¾\\³o­Ó°Q½×°Ï¡G";
+$ubbt_lang['BODY'] =         "±z¤w¸g­q¾\\¤F¥H¤Uªº°Q½×°Ï¡G";
+?>
