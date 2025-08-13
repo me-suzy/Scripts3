f6a4a1057c5aba04ed9b3a8adf35045a5b167c35
me-suzy <me.suzana@gmail.com>
2025-08-14 00:28:49 +0300

    Add Unsorted/PHPCart.v3.0.(c).PHPCart-WDYL/PHPCart30/admin/main.php

---
+++ Unsorted/PHPCart.v3.0.(c).PHPCart-WDYL/PHPCart30/admin/main.php
@@ -0,0 +1,45 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+// Program Name         : PHPCart                                          //
+// Release Version      : 3.0                                              //
+// Program Author       : Mr B Wiencierz                                   //
+// Home Page            : http://www.phpcart.co.uk                         //
+// Supplied by          : CyKuH [WTN]                                      //
+// Nullified by         : CyKuH [WTN]                                      //
+// Tested by            : WTN Team                                         //
+// Packaged by          : WTN Team                                         //
+// Distribution         : via WebForum, ForumRU and associated file dumps  //
+//                                                                         //
+//                All code is ©2000-2004 Barry Wiencierz.                  //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
+require("hf.php");
+pageHeader();?>
+<style>
+<!--
+A:link {	COLOR: #maroon; TEXT-DECORATION: none}
+A:visited {	COLOR: #maroon; TEXT-DECORATION: none}
+A:hover {	COLOR: #666666; TEXT-DECORATION: underline}
+a{text-decoration:none}
+//-->
+</style>
+
+<p align="center"><b><font face="Verdana" size="3" color="#FF0000">Main Menu</font><font size="2" face="Verdana"><br>
+<br>
+<font color="#FF0000">Admin Options</font><br>
+</font></b><font size="2" face="Verdana"><a href="index.php">Edit Admin User/Pass</a><br>
+<br>
+<a href="configuration.php">Edit Configuration</a><br>
+<br>
+<a href="payment.php">Edit Payment Configuration</a><br>
+<br>
+<br>
+<font color="#FF0000"><b>Templates</b></font><br>
+<a href="header.php">Edit Header File</a><br>
+<br>
+<a href="footer.php">Edit Footer File</a></font>
+<br>
+<font color="#FF0000"><b>Search Orders</b></font><br>
+<a href="search.php.php">Search for Order</a></font></p>
+<?pageFooter();?>
