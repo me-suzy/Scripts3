83554885d35ed9642b05d0ecfd7a058efe161045
me-suzy <me.suzana@gmail.com>
2025-08-14 02:29:25 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/dosearch.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/chinese/dosearch.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['NO_MORE']   =  "There are no more search results in this direction.";
+$ubbt_lang['NO_WORDS']  = 	"Äú±ØÐëÊäÈë¹Ø¼ü×Ö²ÅÄÜËÑÑ°¡£";
+$ubbt_lang['SEARCH_RES']  = 	"ËÑÑ°½á¹û";
+$ubbt_lang['PREV_PAGE']  = 	"Ç°Ò»Ò³";
+$ubbt_lang['MAIN_SEARCH']  = 	"ËÑÑ°ÒýÇæÒ³Ãæ";
+$ubbt_lang['NEXT_PAGE']  = 	"ÏÂÒ»Ò³";
+$ubbt_lang['NO_MATCH']  = 	"±§Ç¸£¬ÎÒÃÇÕÒ²»µ½·ûºÏÄúÌõ¼þµÄÎÄÕÂ¡£";
+?>
