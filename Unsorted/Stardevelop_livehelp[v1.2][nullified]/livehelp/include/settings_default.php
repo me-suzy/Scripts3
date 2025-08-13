3ae05277856d49ed0e9be5946df89abcc5519ac4
me-suzy <me.suzana@gmail.com>
2025-08-14 01:52:20 +0300

    Add Unsorted/Stardevelop_livehelp[v1.2][nullified]/livehelp/include/settings_default.php

---
+++ Unsorted/Stardevelop_livehelp[v1.2][nullified]/livehelp/include/settings_default.php
@@ -0,0 +1,30 @@
+<?php
+/*
+stardevelop.com Live Help
+International Copyright stardevelop.com
+
+You may not distribute this program in any manner,
+modified or otherwise, without the express, written
+consent from stardevelop.com
+
+You may make modifications, but only for your own 
+use and within the confines of the License Agreement.
+All rights reserved.
+
+Selling the code for this program without prior 
+written consent is expressly forbidden. Obtain 
+permission before redistributing this program over 
+the Internet or in any other medium.  In all cases 
+copyright and header must remain intact.  
+*/
+
+$livehelp_name = 'stardevelop.com Live Help';
+$livehelp_logo = 'icons/helplogo_sm.gif';
+$site_name = 'stardevelop.com Live Help';
+$font_type = 'Verdana, Arial, Helvetica, sans-serif';
+$font_size = '2';
+$font_color = '#000000';
+$font_link_color = '#000000';
+$background_color = '#FFFFFF';
+
+?>
