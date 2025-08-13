5b625b2d6912fb0b306de99782f51e0f1110155c
me-suzy <me.suzana@gmail.com>
2025-08-14 01:00:35 +0300

    Add Unsorted/phpx/admin/helpbody.php

---
+++ Unsorted/phpx/admin/helpbody.php
@@ -0,0 +1,14 @@
+<?php
+# $Id: helpbody.php,v 1.1 2003/07/08 18:29:56 ryan Exp $
+//! Creates Help text on right side of pop-up frame
+$title = "Help";
+require("includes/help.inc.php");
+require("includes/auth.inc.php");
+
+new helpDocs("body");
+
+
+
+
+?>
+
