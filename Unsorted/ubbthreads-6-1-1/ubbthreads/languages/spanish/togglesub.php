4c98aea02642d688435bed7e0e2149fcaf2ea522
me-suzy <me.suzana@gmail.com>
2025-08-14 02:50:15 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/togglesub.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/togglesub.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['UN_HEADER']  = 	"Te has desapuntado";
+$ubbt_lang['HEADER']  = 	"Te has apuntado";
+$ubbt_lang['UN_BODY']  = 	"Te has desapuntado del tablero: ";
+$ubbt_lang['BODY']  =          "Te has apuntado al tablero: ";
+?>
