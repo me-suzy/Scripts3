56c6d08a4a12f2601249d09b0a5742192c82d54b
me-suzy <me.suzana@gmail.com>
2025-08-14 02:43:46 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/newreply.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/polish/newreply.php
@@ -0,0 +1,19 @@
+<?
+$ubbt_lang['READ_PERM']  = 	"W tym forum mo¿esz tylko czytaæ";
+$ubbt_lang['NOT_APP']  = 	"Nie mo¿esz odpowiedzieæ na niezatwierdzon± wiadomo¶æ.";
+$ubbt_lang['REPLY_HEAD']  = 	"Odpowiedz";
+$ubbt_lang['FILL_FORM']  = 	"Wype³nij poni¿sze pola, ¿eby wys³aæ now± wiadomo¶æ.";
+$ubbt_lang['NO_HTML']  = 	"HTML jest niedozwolony w tym forum, wiêc nie mo¿esz go u¿ywaæ w wiadomo¶ci.";
+$ubbt_lang['YES_HTML']  = 	"HTML jest dozwolony, wiêc mo¿esz go u¿ywaæ w wiadomo¶ci.";
+$ubbt_lang['NO_MARKUP']  = 	"Nie mo¿esz u¿ywaæ MarkUp'u, bo jest zablokowany w tym forum.";
+$ubbt_lang['YES_MARKUP']  = 	"UBBCode jest zezwolony, wiêc mo¿esz u¿ywaæ";
+$ubbt_lang['MARK_POSTS']  = 	"go w wiadomo¶ciach";
+$ubbt_lang['CHOOSE_NAME']  = 	"Mo¿liwe jest pisanie jako anonim, mo¿esz wiêc wybraæ dowoln± nazwê u¿ytkownika, o ile nie jest ona ju¿ zajêta.";
+$ubbt_lang['POST_ICON']  = 	"Ikonka do wiadomo¶ci";
+$ubbt_lang['POST_TEXT']  = 	"Tekst wiadomo¶ci";
+$ubbt_lang['SPELL_CHECK']  = 	"Sprawd¼ pisowniê";
+$ubbt_lang['DO_EMAIL']  = 	"Wy¶lij mi odpowiedzi na tê wiadomo¶æ emailem";
+$ubbt_lang['DO_PREVIEW']  = 	"Chcê podejrzeæ moj± wiadomo¶æ";
+$ubbt_lang['CAN_ATTACH']  = 	"i/lub do³±czyæ plik";
+$ubbt_lang['IN_RESPONSE']  = 	"W odpowiedzi na wiadomo¶æ";
+?>
