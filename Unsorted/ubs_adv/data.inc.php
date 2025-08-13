e754599ba473da16c21bdc58ce92669447177c53
me-suzy <me.suzana@gmail.com>
2025-08-14 02:52:22 +0300

    Add Unsorted/ubs_adv/data.inc.php

---
+++ Unsorted/ubs_adv/data.inc.php
@@ -0,0 +1,27 @@
+<?
+// Hostname or IP of your MySQL-Server
+$mysqlhost = "localhost";
+// Username and Password to Login to your Database
+$mysqluser = "Home";
+$mysqlpassword = "";
+// Name of your Database
+$mysqldb = "rpg";
+// Email Address of the Administrator
+$adminmail = "";
+//What kind of board are you running?
+$mytypeofboard = "wbb";
+//what's your boards name?
+$myboardname = "";
+//logo url
+$myimageurl = "images/ghban.jpg";
+//my path to the rpg scripts
+$myscriptpaths = "rpg/";
+//position of the logo
+$mylogopostion = "center";
+//the lobby music
+$mylobbymusic = "battle_music/lobby.mid";
+//the shop music
+$myshopmusic = "battle_music/shop.mid";
+//music for the battle entrance
+$mybattleentrancemusic = "battle_music/battle14.mid";
+?>
