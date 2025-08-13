c09356c92825a1bd6d09bfe8ddd157e76911f097
me-suzy <me.suzana@gmail.com>
2025-08-14 02:47:50 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/dorateuser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/russian/dorateuser.php
@@ -0,0 +1,7 @@
+<?
+$ubbt_lang['INVALID_RATING'] = "That is an invalid rating.";
+$ubbt_lang['NOMORERATE'] = "You have already rated this user.";
+$ubbt_lang['THANKS'] = "Thank you.";
+$ubbt_lang['RATED']	= "Your rating for this user has been accepted.";
+$ubbt_lang['CANTRATE']  = "You can't rate yourself.";
+?>
