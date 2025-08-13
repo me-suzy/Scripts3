3b177471ef98ffe27a4a26c7f56cbbf577f3a4a9
me-suzy <me.suzana@gmail.com>
2025-08-14 00:25:03 +0300

    Add Unsorted/phpBazar_1.60n/phpBazar-1.60n/termsofuse.php

---
+++ Unsorted/phpBazar_1.60n/phpBazar-1.60n/termsofuse.php
@@ -0,0 +1,28 @@
+<?
+
+#################################################################################################
+#
+#  project           : phpBazar
+#  filename          : termsofuse.php
+#  last modified by  : Erich Fuchs
+#  supplied by       : Reseting
+#  nullified by      : CyKuH [WTN]
+#  purpose           : Show the Terms of USE
+#
+#################################################################################################
+
+
+
+
+
+#  Include Configs & Variables
+
+#################################################################################################
+
+require ("library.php");
+
+include ("$language_dir/termsofuse.inc");
+
+
+
+?>
