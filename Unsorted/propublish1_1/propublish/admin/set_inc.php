a576a694646511dd26b5bd710a41ad54bb4988bc
me-suzy <me.suzana@gmail.com>
2025-08-14 01:16:59 +0300

    Add Unsorted/propublish1_1/propublish/admin/set_inc.php

---
+++ Unsorted/propublish1_1/propublish/admin/set_inc.php
@@ -0,0 +1,28 @@
+<? 
+
+ $set_email="are@webber.no";
+
+ $set_htmleditor="1";
+
+ $set_normaleditor="1";
+
+ $fp_limit="20";
+
+ $lang="eng.php";
+
+ $set_url="http://www.webber.no";
+
+ $set_inform_new="1";
+
+ $set_autovalidate="";
+
+ $text_w_plain="64";
+
+ $text_h_plain="25";
+
+ $text_w_html="534";
+
+ $text_h_html="300";
+
+ 
+?>
