4b59f181f4ab96de843cd9ae223c4b8926d0b425
me-suzy <me.suzana@gmail.com>
2025-08-14 00:14:24 +0300

    Add Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/admin/newtree.php

---
+++ Unsorted/phpauction_proplus_1.0/phpauctionproplus-1.0/admin/newtree.php
@@ -0,0 +1,112 @@
+<?#//v.1.0.0
+	include "loggedin.inc.php";
+
+
+#///////////////////////////////////////////////////////
+#//  COPYRIGHT 2002 PHPAuction.org ALL RIGHTS RESERVED//
+#//  For Source code for the GPL version go to        //  
+#//  http://phpauction.org and download               //
+#//  Supplied by CyKuH [WTN]                          //
+#///////////////////////////////////////////////////////
+
+   require('../includes/messages.inc.php');
+   require('../includes/config.inc.php');
+   
+   if($HTTP_POST_VARS[action] == "start")
+   {
+   		if(!file_exists("categories.txt"))
+   		{
+   			$ERR = $ERR_118;
+   		}
+   		else
+   		{
+   			Header("Location: populate_categories.php");
+   		}
+   	}
+	
+	
+	require("./header.php");
+	require('../includes/styles.inc.php'); 
+?>
+
+
+<SCRIPT Language=Javascript>
+
+function window_open(pagina,titulo,ancho,largo,x,y){
+
+  var Ventana= 'toolbar=0,location=0,directories=0,scrollbars=1,screenX='+x+',screenY='+y+',status=0,menubar=0,resizable=0,width='+ancho+',height='+largo;
+  open(pagina,titulo,Ventana);
+
+}
+
+</SCRIPT>
+<TABLE BORDER=0 WIDTH=100% CELLPADDING=0 CELLSPACING=0 BGCOLOR="#FFFFFF">
+<TR>
+		<TD ALIGN=CENTER><BR>
+<FORM NAME=conf ACTION=<?=basename($PHP_SELF)?> METHOD=POST>
+				<TABLE WIDTH="650" BORDER="0" CELLSPACING="0" CELLPADDING="1" BGCOLOR="#296FAB">
+					<TR> 
+						<TD ALIGN=CENTER><FONT COLOR=#FFFFFF FACE="Verdana, Arial, Helvetica, sans-serif" SIZE="4"><B>
+							<? print $MSG_369; ?>
+							</B></FONT></TD>
+					</TR>
+					<TR> 
+						<TD> 
+							<TABLE WIDTH=100% CELLPADDING=2 ALIGN="CENTER" BGCOLOR="#FFFFFF">
+								<TR> 
+									<TD WIDTH=50 HEIGHT="21"></TD>
+									<TD COLSPAN=4 HEIGHT="21"> <FONT FACE="Verdana, Verdana, Arial, Helvetica, sans-serif" SIZE="2"> 
+										<FONT COLOR=red> 
+										<?=$ERR?>
+										</FONT> <BR>
+										<BR>
+										<?=$MSG_644; ?>
+										</FONT></TD>
+								</TR>
+								<TR> 
+									<TD WIDTH=50></TD>
+									<TD WIDTH="302"><FONT FACE="Verdana, Verdana, Arial, Helvetica, sans-serif" SIZE="2"> 
+										<? 
+print $MSG_405; 
+if($$ERR){
+	print "<FONT COLOR=red><BR><BR>".$$ERR;
+}else{
+	if($$MSG){
+		print "<FONT COLOR=red><BR><BR>".$$MSG;
+	}else{
+		print "<BR><BR>";
+	}
+}
+?>
+										</FONT></TD>
+								</TR>
+								<TR> 
+									<TD COLSPAN="2" HEIGHT="22"> 
+										<CENTER>
+											<INPUT TYPE=hidden NAME=action VALUE=start>
+											<INPUT TYPE="submit" NAME="Submit" VALUE="<?=$MSG_406?>">
+										</CENTER>
+									</TD>
+								</TR>
+								<TR> 
+									<TD WIDTH=50></TD>
+									<TD WIDTH="302"> </TD>
+								</TR>
+							</TABLE>
+						</TD>
+					</TR>
+				</TABLE>
+				</FORM>
+<BR><BR>
+
+<FONT FACE="Verdana, Verdana, Arial, Helvetica, sans-serif" SIZE="2">
+<A HREF="admin.php" CLASS="links">Admin Home</A>
+</FONT>
+<BR><BR>
+
+</TD>
+</TR>
+</TABLE>
+
+
+<? require("./footer.php"); ?>
