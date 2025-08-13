9f6e58dc1e821ea816c18a3f53b1b5790acb41f0
me-suzy <me.suzana@gmail.com>
2025-08-14 02:33:10 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/sendmessage.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/sendmessage.php
@@ -0,0 +1,12 @@
+<?
+$ubbt_lang['CHOOSE_ADD']  = 	"Kies een naam";
+$ubbt_lang['TEXT_DELETE']  = 	"Wissen";
+$ubbt_lang['NO_PRIVATE']  = 	"Dit lid accepteert geen privé-berichten.";
+$ubbt_lang['NO_RECORD']  = 	"De gebruikersnaam die jij hebt ingevuld, komt niet in onze database voor.";
+$ubbt_lang['MESS_PREV']  = 	"Bekijk een voorbeeld van jouw bericht aan";
+$ubbt_lang['PREVIEW_MESS']  = 	"Dit is een voorbeeld van jouw bericht.  Als alles zo in orde is, druk je op 'Doorgaan' om het bericht te versturen.  Als je aanpassingen wilt maken, doe je dat via het formulier onderaan de pagina.";
+$ubbt_lang['EDIT_MORE']  = 	"Meer veranderen";
+$ubbt_lang['PRIV_SUB']  = 	"Je hebt een nieuw bericht ontvangen";
+$ubbt_lang['PRIV_BODY_R']  = 	"Je hebt een nieuw privé-bericht ontvangen via {$config['title']}.  Je kunt je nu {$config['phpurl']}/login.php inloggen om het bericht te bekijken.";
+$ubbt_lang['MESS_SENT']  = 	"Jouw bericht is verzonden.";
+?>
