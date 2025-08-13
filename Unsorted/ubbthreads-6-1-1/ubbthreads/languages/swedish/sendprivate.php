074c9ecb5c154037da4f1c23744861c07b007309
me-suzy <me.suzana@gmail.com>
2025-08-14 02:51:29 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/sendprivate.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/swedish/sendprivate.php
@@ -0,0 +1,13 @@
+<?
+$ubbt_lang['NOPRIVS']   = "Private messages are disabled.";
+$ubbt_lang['MESS_COPY']  = 	"Jag vill spara en kopia av mitt meddelande";
+$ubbt_lang['MESS_RECEIPT']  = 	"Jag vill få ett läskvitto";
+$ubbt_lang['MESS_PREVIEW']  = 	"Jag vill förhandsgranska mitt meddelande";
+$ubbt_lang['ADD_BOOK']  = 	"Lägg till i adressboken";
+$ubbt_lang['USE_BOOK']  = 	"...eller välj från din adressbok";
+$ubbt_lang['CHOOSE_ADD']  = 	"Välj ett namn";
+$ubbt_lang['NO_LOGGED']  = 	"Du kan inte skicka ett privat meddelande om du inte är inloggad.";
+$ubbt_lang['NO_PRIVATE']  = 	"Den här användaren tar inte emot privata meddelanden.";
+$ubbt_lang['PRIV_BODY2']  = 	"Fyll i följande information för att skicka privat till den här användaren.  Användaren får det omgående eller vid nästa inloggning.";
+$ubbt_lang['TEXT_DELETE']  = 	"Ta bort";
+?>
