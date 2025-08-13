58873e5a8c10a65313e2e3782d17e41fce318511
me-suzy <me.suzana@gmail.com>
2025-08-14 02:35:31 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/donotifymod.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/french/donotifymod.php
@@ -0,0 +1,9 @@
+<?
+$ubbt_lang['POST_PROB']  = 	"Il y a eu un problème en recherchant le message dans notre base de données.";
+$ubbt_lang['BAD_GROUP']  = 	"Accès refusé !";
+$ubbt_lang['NO_B_INFO']  = 	"Forum non trouvé !";
+$ubbt_lang['NOTIF_HEAD']  = 	"vous a prévenu d'un message à vérifier et intitulé";
+$ubbt_lang['TO_READ']  = 	"Ce message peut être vu ici :";
+$ubbt_lang['NOTIFY_SENT']  = 	"Moderateur(s) notifié(s)";
+$ubbt_lang['NOTIFY_CONFIRM']  =  "Le(s) Moderateur(s) de ce forum ont été notifié(s).";
+?>
