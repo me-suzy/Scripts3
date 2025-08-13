e878d7eda3a894d8cf7889356b421d04d81fd0e3
me-suzy <me.suzana@gmail.com>
2025-08-14 00:28:34 +0300

    Add Unsorted/PHPCart.v3.0.(c).PHPCart-WDYL/PHPCart30/admin/admin_1.php

---
+++ Unsorted/PHPCart.v3.0.(c).PHPCart-WDYL/PHPCart30/admin/admin_1.php
@@ -0,0 +1,21 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+// Program Name         : PHPCart                                          //
+// Release Version      : 3.0                                              //
+// Program Author       : Mr B Wiencierz                                   //
+// Home Page            : http://www.phpcart.co.uk                         //
+// Supplied by          : CyKuH [WTN]                                      //
+// Nullified by         : CyKuH [WTN]                                      //
+// Tested by            : WTN Team                                         //
+// Packaged by          : WTN Team                                         //
+// Distribution         : via WebForum, ForumRU and associated file dumps  //
+//                                                                         //
+//                All code is ©2000-2004 Barry Wiencierz.                  //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
+$adminUsername	= "";
+$adminPassword	= "";
+$adminPassword2	= "";
+$version = "3.0n";
+?>
