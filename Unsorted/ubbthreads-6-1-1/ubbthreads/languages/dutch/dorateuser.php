d669911faea9fbc6c0e4e884cde05fd33c7ca937
me-suzy <me.suzana@gmail.com>
2025-08-14 02:32:33 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/dorateuser.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/dutch/dorateuser.php
@@ -0,0 +1,7 @@
+<?
+$ubbt_lang['INVALID_RATING'] = "That is an invalid rating.";
+$ubbt_lang['NOMORERATE'] = "Je hebt dit lid al een waardering gegeven.";
+$ubbt_lang['THANKS'] = "Dank je.";
+$ubbt_lang['RATED']	= "Jouw waardering van dit lid is verwerkt.";
+$ubbt_lang['CANTRATE']  = "Je kunt jezelf geen waardering geven.";
+?>
