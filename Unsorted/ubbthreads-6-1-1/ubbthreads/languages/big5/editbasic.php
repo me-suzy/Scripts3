4b742491d67a180e651c87e15105ee8309c6efca
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:53 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =      "±z¥i¦b¦¹¤W¶Ç±zªº·Ó¤ù (·Ó¤ù³Ì¤j¬° {$theme['PictureWidth']}x{$theme['PictureHeight']} ¹³¯À) (¥u¤¹³\ .gif, .jpg, .png ®æ¦¡)";
+$ubbt_lang['PROF_PIC']  =       "±zªº¤jÀY·Ó (§¹¾ãªººô§} - ¹Ï§Î¤j¤p³Ì¤j¬° {$theme['PictureWidth']}x{$theme['PictureHeight']} ¹³¯À¡A¤£¥i¥H¶W¹L) (¥u¤¹³\ .gif, .jpg, .png ®æ¦¡)";
+$ubbt_lang['AUX_ONLINE']  =      "¦b¬d¸ß¤W½u¸ê°T®É¡A±z§Æ±æ¨ä¥Lºô¤Íª¾¹D±z¥Ø«e¦b¯¸¤W¥¿¦bÂsÄýªº¸ê°T¶Ü¡H";
+$ubbt_lang['PROF_SIG'] =     "Ã±¦WÀÉ (³Ì¦h $config[Sig_length] ­Ó¦r¤¸)¡AÃ±¦WÀÉ·|Åã¥Ü¦b±z¨C½g¤å³¹ªº§ÀºÝ¡C";
+$ubbt_lang['PROF_PRIV']  = 	"¤¹³\\§O¤H¨p¤U±H«H¨ì±z¦b¥»¯¸ªº«H½c¶Ü?";
+$ubbt_lang['NO_PROF']  = 	"§Ú­Ì§ä¤£¨ì³o¦ì¨Ï¥ÎªÌªº¸ê®Æ";
+$ubbt_lang['PROF_HEAD']  = 	"½s¿è¨Ï¥ÎªÌÀÉ®×:";
+$ubbt_lang['PROF_EDIT']  = 	"±z¥i¥H½s¿è¤U­±ªº¥ô¦ó¸ê®Æ¡C";
+$ubbt_lang['BASIC_PROF']  = 	"½s¿è­Ó¤HÀÉ®×";
+$ubbt_lang['PROF_PASS']  = 	"±K½X (¤¶©ó 4 ¨ì 20 ­Ó¦r¤¸)";
+$ubbt_lang['VER_PASS']  = 	"½Ð¦A¿é¤J¤@¦¸±K½X";
+$ubbt_lang['EMAIL_ADD']  = 	"¯u¹ê Email Address (¨t²Î»P±zÁpµ¸ªº¯u¹êEmail¡A¤@©w­n¶ñ)";
+$ubbt_lang['FAKE_EMAIL']  = 	"°²ªº Email address (¤½¶}µ¹§O¤H¬Ýªº Email Address ¡A±z¤£¤@©w­n¶ñ¼g¯uªº Email Address ¡A·íµM¡A±z¤]¥i¥H¤£¶ñ)";
+$ubbt_lang['FULL_NAME']  = 	"¦W¦r/¥N¸¹";
+$ubbt_lang['PROF_HOME']  = 	"ºô­¶ (150 ­Ó¦r¤¸¥H¤º)";
+$ubbt_lang['PROF_OCC']  = 	"Â¾·~";
+$ubbt_lang['PROF_HOBB']  = 	"¿³½ì (200 ­Ó¦r¤¸¥H¤º)";
+$ubbt_lang['PROF_LOC']  = 	"©Ò¦b¦a (°ê®a¡A¶mÂí...µ¥µ¥)";
+$ubbt_lang['PROF_BIO']  = 	"¦Û§Ú¤¶²Ð (250 ­Ó¦r¤¸¥H¤º)";
+$ubbt_lang['PROF_ICQ']  = 	"ICQ ¸¹½X";
+$ubbt_lang['PROF_VIS']  = 	"±z§Æ±æ§O¤H¥i¥H¬Ý¨ì§A¦b½u¤W¶Ü?";
+?>
