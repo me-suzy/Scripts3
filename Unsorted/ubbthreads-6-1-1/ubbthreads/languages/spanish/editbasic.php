3bf43ef520ca5d23a12bba55666b4b8907aa606a
me-suzy <me.suzana@gmail.com>
2025-08-14 02:49:27 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/editbasic.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/spanish/editbasic.php
@@ -0,0 +1,23 @@
+<?
+$ubbt_lang['UPLOAD_PIC'] =      "Tu foto (m&aacute;ximo de pixeles: {$theme['PictureWidth']}x{$theme['PictureHeight']}, puede ser de menor tamaño) (se acepta .gif, .jpg, y .png)";
+$ubbt_lang['PROF_PIC']  =       "Tu foto (Direcci&oacute;n URL completa - Tamaño de foto debe ser de {$theme['PictureWidth']}x{$theme['PictureHeight']} m&aacute;ximo) (se acepta .gif, .jpg, y .png)";
+$ubbt_lang['AUX_ONLINE']  =      "¿Deseas permitir que los demás ven el foro y mensaje específico que estás leyendo?";
+$ubbt_lang['PROF_PRIV']  = 	"&iquest;Deseas aceptar mensajes privados de parte de otros miembros del foro?";
+$ubbt_lang['NO_PROF']  = 	"No se ha podido encontrar un perfil del Miembro con este nombre";
+$ubbt_lang['PROF_HEAD']  = 	"Editar perfil de";
+$ubbt_lang['PROF_EDIT']  = 	"Puedes editar cualquiera de los datos siguientes:";
+$ubbt_lang['BASIC_PROF']  = 	"Perfil B&aacute;sico";
+$ubbt_lang['PROF_PASS']  = 	"contrase&ntilde;a (entre 4 y 20 letras o n&uacute;meros)";
+$ubbt_lang['VER_PASS']  = 	"Verificar la contrase&ntilde;a";
+$ubbt_lang['EMAIL_ADD']  = 	"Direcci&oacute;n de email (No disponible a otros miembros)";
+$ubbt_lang['FAKE_EMAIL']  = 	"Direcci&oacute;n de email presentado en los mensajes";
+$ubbt_lang['FULL_NAME']  = 	"Nombre entero";
+$ubbt_lang['PROF_SIG']  = 	"Firma (hasta   {$config['Sig_length']} car&aacute;cteres).  Puedes usar Etiquetador en tu firma";
+$ubbt_lang['PROF_HOME']  = 	"P&aacute;gina Web (hasta 150 car&aacute;cteres)";
+$ubbt_lang['PROF_OCC']  = 	"Ocupaci&oacute;n";
+$ubbt_lang['PROF_HOBB']  = 	"Aficiones (hasta 200 car&aacute;cteres)";
+$ubbt_lang['PROF_LOC']  = 	"Lugar Geogr&aacute;fico (Pa&iacute;s, Provincia, etc.)";
+$ubbt_lang['PROF_BIO']  = 	"Informaci&oacute;n Personal.  Biograf&iacute;a. (hasta 250 letras)";
+$ubbt_lang['PROF_ICQ']  = 	"N&uacute;mero de ICQ";
+$ubbt_lang['PROF_VIS']  = 	"&iquest;Deseas estar visible en la pantalla de gente conectada al foro?";
+?>
