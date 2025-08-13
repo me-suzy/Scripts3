6aa8dda940930beb99b2a7f90946a88829af16dd
me-suzy <me.suzana@gmail.com>
2025-08-14 02:28:48 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/viewfavorites.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/viewfavorites.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['VIEW_FAV']  =  "Æ[¬Ý©Ò¦³§Ú³Ì·Rªº¸ÜÃD";
+$ubbt_lang['VIEW_REM']  =  "Æ[¬Ý©Ò¦³³Æ§Ñ¿ý¤¤ªº¤å³¹";
+$ubbt_lang['LAST_POST']  =  "³Ì«á±i¶K®É¶¡";
+$ubbt_lang['REMOVE']     =  "²¾°£";
+?>
