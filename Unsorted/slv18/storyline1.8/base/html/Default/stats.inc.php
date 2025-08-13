97688586a805ed79e18e5386b4a51f0a798ea6e3
me-suzy <me.suzana@gmail.com>
2025-08-14 01:47:15 +0300

    Add Unsorted/slv18/storyline1.8/base/html/Default/stats.inc.php

---
+++ Unsorted/slv18/storyline1.8/base/html/Default/stats.inc.php
@@ -0,0 +1,10 @@
+<table border='0' width='100%'>
+	<tr>
+		<td class='catdis'>
+		<font class='small'>
+		Number of Authors [<? authors() ?>] -:- Stories Published [<? stats() ?>]<br>
+		Currently Online [<? online() ?>]
+		</font>
+		</td>
+	</tr>
+</table>
