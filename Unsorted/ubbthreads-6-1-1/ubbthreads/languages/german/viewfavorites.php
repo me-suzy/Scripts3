5942f581c554a0bc173c250e9fa58061f70cbeae
me-suzy <me.suzana@gmail.com>
2025-08-14 02:38:05 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/viewfavorites.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/german/viewfavorites.php
@@ -0,0 +1,6 @@
+<?
+$ubbt_lang['VIEW_FAV']  =  "Ansicht aller Favoriten-Threads";
+$ubbt_lang['VIEW_REM']  =  "Ansicht des gesamten 'Antwort schreiben'-Merkzettels";
+$ubbt_lang['LAST_POST']  =  "letzter Eintrag";
+$ubbt_lang['REMOVE']     =  "L&ouml;schen";
+?>
