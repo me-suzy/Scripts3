5dbc5870ae84f58822a14c5ab02d10532c37ef34
me-suzy <me.suzana@gmail.com>
2025-08-14 01:47:13 +0300

    Add Unsorted/slv18/storyline1.8/base/html/Default/menu.inc.php

---
+++ Unsorted/slv18/storyline1.8/base/html/Default/menu.inc.php
@@ -0,0 +1,12 @@
+<table border='0' width='100%'>
+	<tr>
+		<td class='catdis'>
+			<a href='<?=SL_ROOT_URL?>'>Home</a> &#8226; 
+			<a href='<?=SL_ROOT_URL?>/register.php'>Register</a> &#8226; 
+			<a href='<?=SL_ROOT_URL?>/userpanel.php'>User Panel</a> &#8226; 
+			<a href='<?=SL_ROOT_URL?>/search.php'>Search</a> &#8226; 
+			<a href='<?=SL_ROOT_URL?>/authors.php'>Author Directory</a> &#8226; 
+			<a href='<?=SL_ROOT_URL?>/latest.php'>Latest</a>
+		</td>
+	</tr>
+</table>
