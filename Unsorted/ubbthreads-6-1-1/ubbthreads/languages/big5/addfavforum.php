843ba7495c49b9d9e48248a59ae4fcf3e0c9073e
me-suzy <me.suzana@gmail.com>
2025-08-14 02:27:21 +0300

    Add Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/addfavforum.php

---
+++ Unsorted/ubbthreads-6-1-1/ubbthreads/languages/big5/addfavforum.php
@@ -0,0 +1,8 @@
+<?
+$ubbt_lang['NO_PERM'] = "§A¨S¦³Åv­­Åª¨ú³o­Ó°Q½×°Ï¡A©Ò¥H«Ü©êºp¡A±z¤£¯à±N¤§¥[¤J¨ì­Ó¤H¦¬ÂÃ§X©Î´£¥Ü²M³æ¸Ì¡C";
+$ubbt_lang['DUP_ENTRY'] = "³o­Ó°Q½×°Ï¤w¸g¦b§Aªº­Ó¤H¦¬ÂÃ§X/´£¥Ü²M³æ¸Ì¤F¡C";
+$ubbt_lang['FORUM_IN'] = "°Q½×°Ï¤w¥[¤J­Ó¤H¦¬ÂÃ§X";
+$ubbt_lang['FORUM_CONFIRM'] = "§A­è­è¿ï¾Üªº°Q½×°Ï¤w¸g¥[¤J¨ì§Aªº­Ó¤H¦¬ÂÃ§X¸Ì¤F¡A§A¥i¥H±q¡u­Ó¤H±±¨î¥x¡v¤¤§ä¨ì¡C";
+$ubbt_lang['FORUM_RETURN'] = "²{¦b¦^¨ì°Q½×°Ï¦Cªí¡C";
+$ubbt_lang['TOO_MANY'] = "§A³Ì¦h¥u¯à¥[¤J 83 ­Ó°Q½×°Ï¨ì§Aªº­Ó¤H¦¬ÂÃ§X¸Ì¡C";
+?>
