94f63bdc4bb4f557a042d3e046c0edad36fb3c12
me-suzy <me.suzana@gmail.com>
2025-08-14 04:44:10 +0300

    Add Web Hosting Tools/amfr-short-url/login.php

---
+++ Web Hosting Tools/amfr-short-url/login.php
@@ -0,0 +1,29 @@
+<?php
+include('config.php');
+session_start();
+header("Cache-control: private");
+if($_POST['user'] == $config['username'] && $_POST['pass'] == $config['password']) {
+$_SESSION['amfradmin'] = $_POST['user'];
+$_SESSION['amfrpassw'] = $_POST['pass'];
+header('Location: admin');
+}
+else {
+?>
+<HTML>
+<HEAD>
+<TITLE>Please login</TITLE>
+</HEAD>
+<BODY BGCOLOR="white">
+<CENTER>
+<FORM ACTION="login.php" METHOD="post">
+<H2>Please login:</H2><BR>
+Username: <INPUT TYPE="text" NAME="user"><BR>
+Password: <INPUT TYPE="password" NAME="pass"><BR>
+<INPUT TYPE="submit" VALUE="Login!">
+</FORM>
+</CENTER>
+</BODY>
+</HTML>
+<?php
+}
+?>
