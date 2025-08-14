4e75fdf5eb5832ac7a604695725d6225bbbddc8b
me-suzy <me.suzana@gmail.com>
2025-08-14 05:04:37 +0300

    Add Web Traffic Analysis/webmastered_v2.01/web/var_ml.php

---
+++ Web Traffic Analysis/webmastered_v2.01/web/var_ml.php
@@ -0,0 +1,5 @@
+<?PHP
+$subscribemail = "Thank You and Welcome to my Mailing List. You will be kept up to date with news regarding this business.
+If you have enjoyed my website, it would be great if you could tell a friend!";
+$message = "";
+?>
