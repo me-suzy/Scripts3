0bca467b4faac732c4f5eebd461edaa9151ed789
me-suzy <me.suzana@gmail.com>
2025-08-14 05:05:17 +0300

    Add XML and PHP/instantfeedwriter/instantfeedwriter/instantfeed.php

---
+++ XML and PHP/instantfeedwriter/instantfeedwriter/instantfeed.php
@@ -0,0 +1,30 @@
+<?
+// ************************************************************************************
+// Program File Name : instantfeed.php
+// Programmer : Lim Hock Leong Benjamin
+//
+// Purpose:
+// Contains the coding for input form.
+//
+// Version Stable Build 1.1
+// Last Updated 25 May 2005
+// Date Completed : 15 June 2004
+//
+// Revision History
+//
+// Version 1.0  15 Jun 2004
+// Initial Release of the Instant Feed Writer, to automate the generation of XML
+// RSS files.
+//
+// Version 1.1 25 May 2005
+// Recovered source codes of 1.0 from old backup after a major server crash.
+// Reworked the code to follow a MVC design pattern structure.
+// Reworked code to allow public download and use on their servers.
+//
+// Copyright 2005 BENRUTH SOFTWARE CONSULTANCY
+// ************************************************************************************
+
+@require("instantfeed.inc.php");
+displayInstantFeed();
+
+?>
