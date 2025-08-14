a012f2d9cd7ed4dc7b5db18206c4c2420b28e86a
me-suzy <me.suzana@gmail.com>
2025-08-14 03:56:08 +0300

    Add Unsorted/xtcommerce/account_password.php

---
+++ Unsorted/xtcommerce/account_password.php
@@ -0,0 +1,193 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: account_password.php,v 1.1 2003/09/06 21:38:27 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(account_password.php,v 1.1 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (account_password.php,v 1.14 2003/08/17); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  include( 'includes/application_top.php');
+
+  // include needed functions
+  require_once(DIR_FS_INC . 'xtc_draw_hidden_field.inc.php');
+  require_once(DIR_FS_INC . 'xtc_draw_password_field.inc.php');
+  require_once(DIR_FS_INC . 'xtc_validate_password.inc.php');
+  require_once(DIR_FS_INC . 'xtc_encrypt_password.inc.php');
+  require_once(DIR_FS_INC . 'xtc_image_button.inc.php');
+
+  if (!isset($_SESSION['customer_id'])) {
+    $_SESSION['navigation']->set_snapshot();
+    xtc_redirect(xtc_href_link(FILENAME_LOGIN, '', 'SSL'));
+  }
+
+// needs to be included earlier to set the success message in the messageStack
+  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . FILENAME_ACCOUNT_PASSWORD);
+
+  if (isset($_POST['action']) && ($_POST['action'] == 'process')) {
+    $password_current = xtc_db_prepare_input($_POST['password_current']);
+    $password_new = xtc_db_prepare_input($_POST['password_new']);
+    $password_confirmation = xtc_db_prepare_input($_POST['password_confirmation']);
+
+    $error = false;
+
+    if (strlen($password_current) < ENTRY_PASSWORD_MIN_LENGTH) {
+      $error = true;
+
+      $messageStack->add('account_password', ENTRY_PASSWORD_CURRENT_ERROR);
+    } elseif (strlen($password_new) < ENTRY_PASSWORD_MIN_LENGTH) {
+      $error = true;
+
+      $messageStack->add('account_password', ENTRY_PASSWORD_NEW_ERROR);
+    } elseif ($password_new != $password_confirmation) {
+      $error = true;
+
+      $messageStack->add('account_password', ENTRY_PASSWORD_NEW_ERROR_NOT_MATCHING);
+    }
+
+    if ($error == false) {
+      $check_customer_query = xtc_db_query("select customers_password from " . TABLE_CUSTOMERS . " where customers_id = '" . (int)$_SESSION['customer_id'] . "'");
+      $check_customer = xtc_db_fetch_array($check_customer_query);
+
+      if (xtc_validate_password($password_current, $check_customer['customers_password'])) {
+        xtc_db_query("update " . TABLE_CUSTOMERS . " set customers_password = '" . xtc_encrypt_password($password_new) . "' where customers_id = '" . (int)$_SESSION['customer_id'] . "'");
+
+        xtc_db_query("update " . TABLE_CUSTOMERS_INFO . " set customers_info_date_account_last_modified = now() where customers_info_id = '" . (int)$_SESSION['customer_id'] . "'");
+
+        $messageStack->add_session('account', SUCCESS_PASSWORD_UPDATED, 'success');
+
+        xtc_redirect(xtc_href_link(FILENAME_ACCOUNT, '', 'SSL'));
+      } else {
+        $error = true;
+
+        $messageStack->add('account_password', ERROR_CURRENT_PASSWORD_NOT_MATCHING);
+      }
+    }
+  }
+
+  $breadcrumb->add(NAVBAR_TITLE_1, xtc_href_link(FILENAME_ACCOUNT, '', 'SSL'));
+  $breadcrumb->add(NAVBAR_TITLE_2, xtc_href_link(FILENAME_ACCOUNT_PASSWORD, '', 'SSL'));
+?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html <?php echo HTML_PARAMS; ?>>
+<head>
+<?php include(DIR_WS_MODULES.FILENAME_METATAGS); ?>
+<base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<?php require('includes/form_check.js.php'); ?>
+</head>
+<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0">
+<!-- header //-->
+<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+<!-- header_eof //-->
+
+<!-- body //-->
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+  <tr>
+    <td class="navLeft" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- left_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+<!-- left_navigation_eof //-->
+    </table></td>
+<!-- body_text //-->
+    <td class="tableBody" width="100%" valign="top"><?php echo xtc_draw_form('account_password', xtc_href_link(FILENAME_ACCOUNT_PASSWORD, '', 'SSL'), 'post', 'onSubmit="return check_form(account_password);"') . xtc_draw_hidden_field('action', 'process'); ?><table border="0" width="100%" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
+            <td class="pageHeading" align="right"><?php echo xtc_image(DIR_WS_IMAGES . 'table_background_account.gif', HEADING_TITLE, HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+<?php
+  if ($messageStack->size('account_password') > 0) {
+?>
+      <tr>
+        <td><?php echo $messageStack->output('account_password'); ?></td>
+      </tr>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+<?php
+  }
+?>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+          <tr>
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td class="main"><b><?php echo MY_PASSWORD_TITLE; ?></b></td>
+                <td class="inputRequirement" align="right"><?php echo FORM_REQUIRED_INFORMATION; ?></td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td><table border="0" width="100%" cellspacing="1" cellpadding="2" class="infoBox">
+              <tr class="infoBoxContents">
+                <td><table border="0" cellspacing="2" cellpadding="2">
+                  <tr>
+                    <td class="main"><?php echo ENTRY_PASSWORD_CURRENT; ?></td>
+                    <td class="main"><?php echo xtc_draw_password_field('password_current') . '&nbsp;' . (xtc_not_null(ENTRY_PASSWORD_CURRENT_TEXT) ? '<span class="inputRequirement">' . ENTRY_PASSWORD_CURRENT_TEXT . '</span>': ''); ?></td>
+                  </tr>
+                  <tr>
+                    <td colspan="2"><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+                  </tr>
+                  <tr>
+                    <td class="main"><?php echo ENTRY_PASSWORD_NEW; ?></td>
+                    <td class="main"><?php echo xtc_draw_password_field('password_new') . '&nbsp;' . (xtc_not_null(ENTRY_PASSWORD_NEW_TEXT) ? '<span class="inputRequirement">' . ENTRY_PASSWORD_NEW_TEXT . '</span>': ''); ?></td>
+                  </tr>
+                  <tr>
+                    <td class="main"><?php echo ENTRY_PASSWORD_CONFIRMATION; ?></td>
+                    <td class="main"><?php echo xtc_draw_password_field('password_confirmation') . '&nbsp;' . (xtc_not_null(ENTRY_PASSWORD_CONFIRMATION_TEXT) ? '<span class="inputRequirement">' . ENTRY_PASSWORD_CONFIRMATION_TEXT . '</span>': ''); ?></td>
+                  </tr>
+                </table></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="1" cellpadding="2" class="infoBox">
+          <tr class="infoBoxContents">
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td width="10"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td><?php echo '<a href="' . xtc_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">' . xtc_image_button('button_back.gif', IMAGE_BUTTON_BACK) . '</a>'; ?></td>
+                <td align="right"><?php echo xtc_image_submit('button_continue.gif', IMAGE_BUTTON_CONTINUE); ?></td>
+                <td width="10"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></form></td>
+<!-- body_text_eof //-->    <td class="navRight" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- right_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_right.php'); ?>
+<!-- right_navigation_eof //-->
+    </table></td>
+  </tr>
+</table>
+<!-- body_eof //-->
+
+<!-- footer //-->
+<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+<!-- footer_eof //-->
+<br>
+</body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
