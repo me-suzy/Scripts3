031607746275c8c1a70dc2079abfbc9bc21de9b1
me-suzy <me.suzana@gmail.com>
2025-08-14 03:56:45 +0300

    Add Unsorted/xtcommerce/index.php

---
+++ Unsorted/xtcommerce/index.php
@@ -0,0 +1,93 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: index.php,v 1.1 2003/09/06 21:38:27 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on:
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(default.php,v 1.84 2003/05/07); www.oscommerce.com
+   (c) 2003	 nextcommerce (default.php,v 1.13 2003/08/17); www.nextcommerce.org
+
+   Released under the GNU General Public License
+   -----------------------------------------------------------------------------------------
+   Third Party contributions:
+   Enable_Disable_Categories 1.3        	Autor: Mikel Williams | mikel@ladykatcostumes.com
+   Customers Status v3.x  (c) 2002-2003 Copyright Elari elari@free.fr | www.unlockgsm.com/dload-osc/ | CVS : http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/elari/?sortby=date#dirlist
+
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+  include( 'includes/application_top.php');
+
+  // the following cPath references come from application_top.php
+  $category_depth = 'top';
+  if (isset($cPath) && xtc_not_null($cPath)) {
+    $categories_products_query = xtc_db_query("select count(*) as total from " . TABLE_PRODUCTS_TO_CATEGORIES . " where categories_id = '" . $current_category_id . "'");
+    $cateqories_products = xtc_db_fetch_array($categories_products_query);
+    if ($cateqories_products['total'] > 0) {
+      $category_depth = 'products'; // display products
+    } else {
+      $category_parent_query = xtc_db_query("select count(*) as total from " . TABLE_CATEGORIES . " where parent_id = '" . $current_category_id . "'");
+      $category_parent = xtc_db_fetch_array($category_parent_query);
+      if ($category_parent['total'] > 0) {
+        $category_depth = 'nested'; // navigate through the categories
+      } else {
+        $category_depth = 'products'; // category has no products, but display the 'no products' message
+      }
+    }
+  }
+
+  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . FILENAME_DEFAULT);
+?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html <?php echo HTML_PARAMS; ?>>
+<head>
+<?php include(DIR_WS_MODULES.FILENAME_METATAGS); ?>
+<base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0">
+<?php
+  // Added For testing customers_status v3.x
+  if ($customer_status_value['customers_status_public'] == 1) {
+    echo "Hello " . $customer_id ." your status is " . $customer_status_value['customers_status_name'] . " and you have " . $customer_status_value['customers_status_discount'] . "% discount. Vos droits de membre :" .  $customer_status_value['customers_status_ot_discount_flag'] . 'avec une remise totale de :' .  $customer_status_value['customers_status_ot_discount'] ;
+  }
+  // End Add V3.x //-->
+?>
+<!-- header //-->
+<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+<!-- header_eof //-->
+
+<!-- body //-->
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+  <tr>
+    <td class="navLeft" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- left_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+<!-- left_navigation_eof //-->
+    </table></td>
+<!-- body_text //-->
+<?php
+  include (DIR_WS_MODULES . 'default.php');
+?>
+<!-- body_text_eof //-->
+    <td class="navRight" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- right_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_right.php'); ?>
+<!-- right_navigation_eof //-->
+    </table></td>
+  </tr>
+</table>
+<!-- body_eof //-->
+
+<!-- footer //-->
+<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+<!-- footer_eof //-->
+<br>
+</body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
