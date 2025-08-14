d222f4a487507ed94c5d5ccfdb2d2024a9f11ecf
me-suzy <me.suzana@gmail.com>
2025-08-14 03:17:05 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/user_properties_fr.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/user_properties_fr.lang.php
@@ -0,0 +1,34 @@
+0|Préférences - |
+1|Catégories|
+2|Liste des contacts|
+3|n°|
+4|Champ n° |
+5| ---- Aucun ---- |
+6|Survol des contacts|
+7|Recherche|
+8|Dans les options de recherche, sélectionner par défaut :|
+9|les contacts publics |
+10|les contacts des groupes |
+11|les contacts privés |
+12|Tous|
+13|Recherche de type '<b>Commence par</b>' |
+14|Recherche de type '<b>contient</b>' |
+15|Recherche limitée au champ '<b>Nom</b>' |
+16|Recherche dans <b>tous</b> les champs  |
+17|Divers|
+18|Langue |
+19|Afficher le <b>NOM</b> des contacts en majuscule |
+20|Confirmer les suppressions |
+21|Nombre de contacts par page |
+22| (0 pour désactiver)|
+23|Couleurs et polices|
+24|Couleur des barres de menu |
+25|Couleur des boutons |
+26|Police principale |
+27|Test |
+28|Taille |
+29|Police secondaire |
+30|OK |
+31|<< Annuler et retour|
+32| -- Par défaut -- |
+33|###### PLEASE DO NOT ERASE THIS LINE #####||
