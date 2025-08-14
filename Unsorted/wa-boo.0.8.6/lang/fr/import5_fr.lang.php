35103cd57e1811692f8397acd810a848931f4fa2
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:57 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/import5_fr.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/import5_fr.lang.php
@@ -0,0 +1,8 @@
+0|Etape 5 : Résultat de l'importation|
+1|L'importation est terminée.|
+2|Vous avez importé |
+3| contacts.|
+4|Durant cet import, vous avez remplacé |
+5| contacts existants.|
+6|OK|
+7|###### PLEASE DO NOT ERASE THIS LINE #####||
