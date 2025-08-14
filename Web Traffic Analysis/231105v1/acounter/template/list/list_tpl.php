c35b1df1b7c28c1ffb29ed35a260d2a128e09f2c
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:12 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/list/list_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/list/list_tpl.php
@@ -0,0 +1,10 @@
+<?php die("Access restricted");?>
+<table border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+      <select name="%%list%%"%%ext%%>
+        %%items%%
+      </select>
+    </td>
+  </tr>
+</table>
