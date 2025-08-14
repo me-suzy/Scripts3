9cc2e6994b1fb33f9038cad3be55cb67563db446
me-suzy <me.suzana@gmail.com>
2025-08-14 04:50:28 +0300

    Add Web Hosting Tools/phpstatus/phpstatus/language/lang_english.php

---
+++ Web Hosting Tools/phpstatus/phpstatus/language/lang_english.php
@@ -0,0 +1,118 @@
+<?php
+/****************************************************************/
+/*                         phpStatus                            */
+/*                    lang_english.php file                     */
+/*                      (c)copyright 2003                       */
+/*                       By hinton design                       */
+/*                 http://www.hintondesign.org                  */
+/*                  support@hintondesign.org                    */
+/*                                                              */
+/* This program is free software. You can redistrabute it and/or*/
+/* modify it under the terms of the GNU General Public Licence  */
+/* as published by the Free Software Foundation; either version */
+/* 2 of the license.                                            */
+/*                                                              */
+/****************************************************************/
+
+$lang['navigation'] = "Navigation";
+$lang['error'] = "Error";
+$lang['success'] = "Success";
+$lang['error_mess1'] = "You didn\'t fill in a username or password.";
+$lang['error_mess2'] = "You could not be logged in. Please make sure your username and password are correct.";
+$lang['error_mess3'] = "Thare are no ports.";
+$lang['error_mess4'] = "There are no servers.";
+$lang['error_mess5'] = "You do not have permission to view this page.";
+$lang['error_mess6'] = "You are not logged in.";
+$lang['error_mess7'] = "You need to fill in a group name";
+$lang['error_mess8'] = "That group name already exists.";
+$lang['error_mess9'] = "Could not add the group.";
+$lang['error_mess10'] = "Could not edit the group";
+$lang['error_mess11'] = "Could not delete the group.";
+$lang['error_mess12'] = "Please fill in these required fields.";
+$lang['error_mess13'] = "Please fix the following errors.";
+$lang['error_mess14'] = "That port number has already been submitted.";
+$lang['error_mess15'] = "That port name has already been submitted.";
+$lang['error_mess16'] = "Could not add the service.";
+$lang['error_mess17'] = "Could not edit the service.";
+$lang['error_mess18'] = "Could not delete the service.";
+$lang['error_mess19'] = "That server name has already been submitted.";
+$lang['error_mess20'] = "That server ip has already been submitted.";
+$lang['error_mess21'] = "Could not add the server.";
+$lang['error_mess22'] = "Could not edit the server.";
+$lang['error_mess23'] = "Could not delete the server.";
+$lang['error_mess24'] = "Could not edit the settings.";
+$lang['error_mess25'] = "Please fill in a language name.";
+$lang['error_mess26'] = "That language has already been submitted.";
+$lang['error_mess27'] = "Could not add the language.";
+$lang['error_mess28'] = "Could not edit the language.";
+$lang['error_mess29'] = "Could not delete the language.";
+$lang['error_mess30'] = "Please fill in the theme name.";
+$lang['error_mess31'] = "That theme has already been submitted.";
+$lang['error_mess32'] = "Could not add the theme.";
+$lang['error_mess33'] = "Could not edit the theme.";
+$lang['error_mess34'] = "Could not delete the theme.";
+$lang['success_mess1'] = "The group has been added.";
+$lang['success_mess2'] = "The group has been edited.";
+$lang['success_mess3'] = "The group has been deleted.";
+$lang['success_mess4'] = "The service has been added.";
+$lang['success_mess5'] = "The service has been updated.";
+$lang['success_mess6'] = "The service has been deleted.";
+$lang['success_mess7'] = "The server has been added.";
+$lang['success_mess8'] = "The server has been edited.";
+$lang['success_mess9'] = "The server has been deleted.";
+$lang['success_mess10'] = "The settings have been edited.";
+$lang['success_mess11'] = "The language has been added.";
+$lang['success_mess12'] = "The language has been edited.";
+$lang['success_mess13'] = "The language has been deleted.";
+$lang['success_mess14'] = "The theme has been added.";
+$lang['success_mess15'] = "The theme has been edited.";
+$lang['success_mess16'] = "The theme has been deleted.";
+$lang['success_mess17'] = "You are now logging out. Sorry to see you go so soon, Bye.";
+$lang['server'] = "Server";
+$lang['online'] = "Online";
+$lang['offline'] = "Offline";
+$lang['login'] = "Login";
+$lang['username'] = "Username";
+$lang['password'] = "Password";
+$lang['admin'] = "PHPStatus Admin CP";
+$lang['groups'] = "Edit/Add Groups";
+$lang['ports'] = "Edit/add Services";
+$lang['servers'] = "Edit/Add Servers";
+$lang['groups'] = "Groups";
+$lang['add_group'] = "Add Group";
+$lang['group_name'] = "Group Name";
+$lang['group_id'] = "Groupd Id";
+$lang['action'] = "Action";
+$lang['services'] = "Services";
+$lang['edit_group'] = "Edit Group";
+$lang['port_id'] = "Port ID";
+$lang['port_name'] = "Port Name";
+$lang['add_service'] = "Add Service";
+$lang['port_number'] = "Port Number";
+$lang['edit_service'] = "Edit Service";
+$lang['servers'] = "Servers";
+$lang['server_id'] = "Server ID";
+$lang['server_ip'] = "Server IP";
+$lang['server_name'] = "Server Name";
+$lang['add_server'] = "Add Server";
+$lang['group'] = "Group";
+$lang['edit_server'] = "Edit Server";
+$lang['settings'] = "Settings";
+$lang['site_title'] = "Site Title";
+$lang['domain'] = "Domain";
+$lang['script_path'] = "Script Path";
+$lang['default_lang'] = "Default Language";
+$lang['default_theme'] = "Default Theme";
+$lang['languages'] = "Languages";
+$lang['add_lang'] = "Add Language";
+$lang['lang_id'] = "Language ID";
+$lang['lang_name'] = "Language Name";
+$lang['edit_lang'] = "Language Name";
+$lang['themes'] = "Themes";
+$lang['add_theme'] = "Add Theme";
+$lang['theme_id'] = "Theme ID";
+$lang['theme_name'] = "Theme Name";
+$lang['edit_theme'] = "Edit Theme";
+$lang['logout'] = "Logout";
+$lang['logout_mess'] = "<center>Are you sure you want to logout</center><br><a href=\"logout.php?logmeout\">Yes</a> | <a href=\"javascript:history.back()\">No</a></center>";
+?>
