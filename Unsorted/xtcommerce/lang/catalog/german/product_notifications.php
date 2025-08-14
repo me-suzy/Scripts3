54952460000e3a578f6e8bae60947a97e3a29606
me-suzy <me.suzana@gmail.com>
2025-08-14 04:15:16 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/product_notifications.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/product_notifications.php
@@ -0,0 +1,36 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: product_notifications.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(product_notifications.php,v 1.3 2003/02/16); www.oscommerce.com 
+   (c) 2003	 nextcommerce (product_notifications.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Ihr Konto');
+define('NAVBAR_TITLE_2', 'Benachrichtigungen');
+define('HEADING_TITLE', 'Ihre Benachrichtigungen');
+
+define('TEXT_PRODUCT_NOTIFICATIONS_INTRODUCTION', 'Mit der Benachrichtigungsfunktion erm&ouml;glichen wir es Ihnen, &uuml;ber alle unsere Produkte informiert zu sein. Sie erhalten zeitnah die aktuellsten Informationen &uuml;ber unsere Produkte und Sonderangebote.');
+
+define('HEADING_GLOBAL_PRODUCT_NOTIFICATIONS', 'Globale Benachrichtigung (&uuml;ber alle Produkte)');
+define('HEADING_PRODUCT_NOTIFICATIONS', 'Benachrichtigungen (&uuml;ber einzelne Produkte)');
+
+define('TEXT_GLOBAL_PRODUCT_NOTIFICATIONS_DISABLED', 'Ihre globale Benachrichtigung ist zur Zeit <b>deaktiviert</b>.');
+define('TEXT_GLOBAL_PRODUCT_NOTIFICATIONS_ENABLED', 'Ihre globale Benachrichtigung ist zur Zeit <b>aktiviert</b>.');
+
+define('TEXT_GLOBAL_PRODUCT_NOTIFICATIONS_DESCRIPTION_DISABLED', 'Wenn Sie von uns &uuml;ber alle Produkte aktuell informiert werden m&ouml;chten, aktivieren Sie bitte diese Option unterhalb.');
+define('TEXT_GLOBAL_PRODUCT_NOTIFICATIONS_DESCRIPTION_ENABLED', 'Wenn Sie von uns nicht mehr aktuell informiert werden m&ouml;chten, deaktivieren Sie bitte diese Option unterhalb.');
+
+define('TEXT_PRODUCT_NOTIFICATIONS_LIST', 'Nachfolgend aufgef&uuml;hrt ist eine Liste mit Produkten, &uuml;ber die Sie aktuell informiert werden wollen. Wenn Sie ein oder mehrere Produkte von dieser Liste l&ouml;schen wollen, so w&auml;hlen Sie diese bitte aus und klicken anschliessend auf den Button \'Benachrichtigung löschen\'.');
+
+define('TEXT_ENABLE_GLOBAL_NOTIFICATIONS', 'Bitte senden Sie mir aktuelle Nachrichten zu allen Produkten');
+?>
