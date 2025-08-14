356184ab5480effdfeadcb0300311ec516346a10
me-suzy <me.suzana@gmail.com>
2025-08-14 03:55:58 +0300

    Add Unsorted/xtcommerce/account.php

---
+++ Unsorted/xtcommerce/account.php
@@ -0,0 +1,247 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: account.php,v 1.1 2003/09/06 21:38:27 fanta2k Exp $
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce (account.php,v 1.59 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (account.php,v 1.12 2003/08/17); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  include( 'includes/application_top.php');
+  
+  // include needed functions
+  require_once(DIR_FS_INC . 'xtc_count_customer_orders.inc.php');
+  require_once(DIR_FS_INC . 'xtc_date_short.inc.php');
+  require_once(DIR_FS_INC . 'xtc_image_button.inc.php');
+
+  if (!xtc_session_is_registered('customer_id')) {
+    $_SESSION['navigation']->set_snapshot();
+    xtc_redirect(xtc_href_link(FILENAME_LOGIN, '', 'SSL'));
+  }
+
+  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . FILENAME_ACCOUNT);
+
+  $breadcrumb->add(NAVBAR_TITLE, xtc_href_link(FILENAME_ACCOUNT, '', 'SSL'));
+?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html <?php echo HTML_PARAMS; ?>>
+<head>
+<?php include(DIR_WS_MODULES.FILENAME_METATAGS); ?>
+<base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<script language="javascript"><!--
+function rowOverEffect(object) {
+  if (object.className == 'moduleRow') object.className = 'moduleRowOver';
+}
+
+function rowOutEffect(object) {
+  if (object.className == 'moduleRowOver') object.className = 'moduleRow';
+}
+//--></script>
+</head>
+<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0">
+<!-- header //-->
+<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+<!-- header_eof //-->
+
+<!-- body //-->
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+  <tr>
+    <td class="navLeft" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- left_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+<!-- left_navigation_eof //-->
+    </table></td>
+<!-- body_text //-->
+    <td class="tableBody" width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
+            <td class="pageHeading" align="right"><?php echo xtc_image(DIR_WS_IMAGES . 'table_background_account.gif', HEADING_TITLE, HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+<?php
+  if ($messageStack->size('account') > 0) {
+?>
+      <tr>
+        <td><?php echo $messageStack->output('account'); ?></td>
+      </tr>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+<?php
+  }
+
+  if (xtc_count_customer_orders() > 0) {
+?>
+      <tr>
+        <td><table border="0" cellspacing="0" cellpadding="2">
+          <tr>
+            <td class="main"><b><?php echo OVERVIEW_TITLE; ?></b></td>
+            <td class="main"><?php echo '<a href="' . xtc_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL') . '"><u>' . OVERVIEW_SHOW_ALL_ORDERS . '</u></a>'; ?></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="1" cellpadding="2" class="infoBox">
+          <tr class="infoBoxContents">
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td class="main" align="center" valign="top" width="130"><?php echo '<b>' . OVERVIEW_PREVIOUS_ORDERS . '</b><br>' . xtc_image(DIR_WS_IMAGES . 'arrow_south_east.gif'); ?></td>
+                <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+<?php
+    $orders_query = xtc_db_query("select o.orders_id, o.date_purchased, o.delivery_name, o.delivery_country, o.billing_name, o.billing_country, ot.text as order_total, s.orders_status_name from " . TABLE_ORDERS . " o, " . TABLE_ORDERS_TOTAL . " ot, " . TABLE_ORDERS_STATUS . " s where o.customers_id = '" . (int)$customer_id . "' and o.orders_id = ot.orders_id and ot.class = 'ot_total' and o.orders_status = s.orders_status_id and s.language_id = '" . (int)$languages_id . "' order by orders_id desc limit 3");
+    while ($orders = xtc_db_fetch_array($orders_query)) {
+      if (xtc_not_null($orders['delivery_name'])) {
+        $order_name = $orders['delivery_name'];
+        $order_country = $orders['delivery_country'];
+      } else {
+        $order_name = $orders['billing_name'];
+        $order_country = $orders['billing_country'];
+      }
+     
+      echo '                  <tr class="moduleRow" onMouseOver="rowOverEffect(this)" onMouseOut="rowOutEffect(this)" onClick="document.location.href=\'' . xtc_href_link(FILENAME_ACCOUNT_HISTORY_INFO, 'order_id=' . $orders['orders_id'], 'SSL') . '\'">' . "\n" .
+           '                    <td class="main" width="80">' . xtc_date_short($orders['date_purchased']) . '</td>' . "\n" .
+           '                    <td class="main">#' . $orders['orders_id'] . '</td>' . "\n" .
+           '                    <td class="main">' . $order_name . ', ' . $order_country . '</td>' . "\n" .
+           '                    <td class="main">' . $orders['orders_status_name'] . '</td>' . "\n" .
+           '                    <td class="main" align="right">' . $orders['order_total'] . '</td>' . "\n" .
+           '                    <td class="main" align="right"><a href="' . xtc_href_link(FILENAME_ACCOUNT_HISTORY_INFO, 'order_id=' . $orders['orders_id'], 'SSL') . '">' . xtc_image_button('small_view.gif', SMALL_IMAGE_BUTTON_VIEW) . '</a></td>' . "\n" .           '                  </tr>' . "\n";
+    }
+?>
+                </table></td>
+                <td><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+<?php
+  }
+?>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+          <tr>
+            <td class="main"><b><?php echo MY_ACCOUNT_TITLE; ?></b></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="1" cellpadding="2" class="infoBox">
+          <tr class="infoBoxContents">
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td width="10"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td width="60"><?php echo xtc_image(DIR_WS_IMAGES . 'account_personal.gif'); ?></td>
+                <td width="10"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+                  <tr>
+                    <td class="main"><?php echo xtc_image(DIR_WS_IMAGES . 'arrow_green.gif'); ?> <?php echo '<a href="' . xtc_href_link(FILENAME_ACCOUNT_EDIT, '', 'SSL') . '">' . MY_ACCOUNT_INFORMATION . '</a>'; ?></td>
+                  </tr>
+                  <tr>
+                    <td class="main"><?php echo xtc_image(DIR_WS_IMAGES . 'arrow_green.gif'); ?> <?php echo '<a href="' . xtc_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL') . '">' . MY_ACCOUNT_ADDRESS_BOOK . '</a>'; ?></td>
+                  </tr>
+                  <tr>
+                    <td class="main"><?php echo xtc_image(DIR_WS_IMAGES . 'arrow_green.gif'); ?> <?php echo '<a href="' . xtc_href_link(FILENAME_ACCOUNT_PASSWORD, '', 'SSL') . '">' . MY_ACCOUNT_PASSWORD . '</a>'; ?></td>
+                  </tr>
+                </table></td>
+                <td width="10" align="right"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+          <tr>
+            <td class="main"><b><?php echo MY_ORDERS_TITLE; ?></b></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="1" cellpadding="2" class="infoBox">
+          <tr class="infoBoxContents">
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td width="10"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td width="60"><?php echo xtc_image(DIR_WS_IMAGES . 'account_orders.gif'); ?></td>
+                <td width="10"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+                  <tr>
+                    <td class="main"><?php echo xtc_image(DIR_WS_IMAGES . 'arrow_green.gif'); ?> <?php echo '<a href="' . xtc_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL') . '">' . MY_ORDERS_VIEW . '</a>'; ?></td>
+                  </tr>
+                </table></td>
+                <td width="10" align="right"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+          <tr>
+            <td class="main"><b><?php echo EMAIL_NOTIFICATIONS_TITLE; ?></b></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="1" cellpadding="2" class="infoBox">
+          <tr class="infoBoxContents">
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td width="10"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td width="60"><?php echo xtc_image(DIR_WS_IMAGES . 'account_notifications.gif'); ?></td>
+                <td width="10"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+                  <tr>
+                    <td class="main"><?php echo xtc_image(DIR_WS_IMAGES . 'arrow_green.gif'); ?> <?php echo '<a href="' . xtc_href_link(FILENAME_ACCOUNT_NEWSLETTERS, '', 'SSL') . '">' . EMAIL_NOTIFICATIONS_NEWSLETTERS . '</a>'; ?></td>
+                  </tr>
+                  <tr>
+                    <td class="main"><?php echo xtc_image(DIR_WS_IMAGES . 'arrow_green.gif'); ?> <?php echo '<a href="' . xtc_href_link(FILENAME_ACCOUNT_NOTIFICATIONS, '', 'SSL') . '">' . EMAIL_NOTIFICATIONS_PRODUCTS . '</a>'; ?></td>
+                  </tr>
+                </table></td>
+                <td width="10" align="right"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+<!-- body_text_eof //-->    <td class="navRight" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- right_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_right.php'); ?>
+<!-- right_navigation_eof //-->
+    </table></td>
+  </tr>
+</table>
+<!-- body_eof //-->
+
+<!-- footer //-->
+<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+<!-- footer_eof //-->
+<br>
+</body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
