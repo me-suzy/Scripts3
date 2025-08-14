14d41bc43fc16c2cccb2fd90139c79dabc625ac2
me-suzy <me.suzana@gmail.com>
2025-08-14 03:22:19 +0300

    Add Unsorted/webauction2/webauction/admin/footer.php

---
+++ Unsorted/webauction2/webauction/admin/footer.php
@@ -0,0 +1,28 @@
+<?
+/*
+   Copyright (c), 1999, 2000 - phpauction.org
+   Copyright (c), 2001, 2002 - webauction.de.vu
+
+   Lizenz siehe lizenz.txt & license
+
+   Die neueste Version kostenfrei zum Download unter:
+   http://webauction.de.vu
+
+*/
+
+?>
+
+<TABLE WIDTH="100%" BORDER=0 ALIGN=LEFT>
+<TR><TD>
+<CENTER>
+    <? print $sml_font; ?>
+    
+    
+    <FONT COLOR="#999999"><? print $MSG_516; ?></FONT><BR>
+    </FONT>
+  </CENTER>
+</TD></TR>
+</TABLE>
+
+</BODY>
+</HTML>
