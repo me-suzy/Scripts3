7e80bbaff24fccd1f39e218f2cf1608a9bd3eda3
me-suzy <me.suzana@gmail.com>
2025-08-14 03:32:14 +0300

    Add Unsorted/WGS-eDate.v2.5.Nullified/WGS-eDate/_footer.php

---
+++ Unsorted/WGS-eDate.v2.5.Nullified/WGS-eDate/_footer.php
@@ -0,0 +1,19 @@
+      </td>
+  </tr>
+  <tr> 
+    <td height="1" bgcolor="#DDDDDD"></td>
+  </tr>
+  <tr> 
+    <td bgcolor="#F0F0F0"><div align="center">Copyright © 2003 by <a href="http://www.yourdomain.com">yourdomain.com</a>. All rights reserved. </div></td>
+  </tr>
+  <tr> 
+    <td height="1" bgcolor="#000000"></td>
+  </tr>
+</table>
+<BR>
+<CENTER>
+
+</CENTER>
+<BR>
+</body>
+</html>
