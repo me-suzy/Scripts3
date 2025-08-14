6ea5353886933ae344e2f3ee440fdabe97bd39e0
me-suzy <me.suzana@gmail.com>
2025-08-14 04:12:58 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/create_account_success.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/create_account_success.php
@@ -0,0 +1,22 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: create_account_success.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(create_account_success.php,v 1.9 2002/11/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (create_account_success.php,v 1.4 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Create an Account');
+define('NAVBAR_TITLE_2', 'Success');
+define('HEADING_TITLE', 'Your Account Has Been Created!');
+define('TEXT_ACCOUNT_CREATED', 'Congratulations! Your new account has been successfully created! You can now take advantage of member priviledges to enhance your online shopping experience with us. If you have <small><b>ANY</b></small> questions about the operation of this online shop, please email the <a href="' . xtc_href_link(FILENAME_CONTACT_US) . '">store owner</a>.<br><br>A confirmation has been sent to the provided email address. If you have not received it within the hour, please <a href="' . xtc_href_link(FILENAME_CONTACT_US) . '">contact us</a>.');
+?>
