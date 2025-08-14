1cfef42f151f17e5f688e341e5779fa7f1b2b7ee
me-suzy <me.suzana@gmail.com>
2025-08-14 04:10:17 +0300

    Add Unsorted/xtcommerce/lang/admin/english/index.php

---
+++ Unsorted/xtcommerce/lang/admin/english/index.php
@@ -0,0 +1,48 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: index.php,v 1.1 2003/09/06 21:54:33 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   --------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(index.php,v 1.2 2002/03/30); www.oscommerce.com 
+   (c) 2003	 nextcommerce (index.php,v 1.4 2003/08/14); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Choose an action..');
+
+define('BOX_TITLE_ORDERS', 'Orders');
+define('BOX_TITLE_STATISTICS', 'Statistics');
+
+define('BOX_ENTRY_SUPPORT_SITE', 'Support Site');
+define('BOX_ENTRY_SUPPORT_FORUMS', 'Support Forums');
+define('BOX_ENTRY_MAILING_LISTS', 'Mailing Lists');
+define('BOX_ENTRY_BUG_REPORTS', 'Bug Reports');
+define('BOX_ENTRY_FAQ', 'FAQ');
+define('BOX_ENTRY_LIVE_DISCUSSIONS', 'Live Discussions');
+define('BOX_ENTRY_CVS_REPOSITORY', 'CVS Repository');
+define('BOX_ENTRY_INFORMATION_PORTAL', 'Information Portal');
+
+define('BOX_ENTRY_CUSTOMERS', 'Customers:');
+define('BOX_ENTRY_PRODUCTS', 'Products:');
+define('BOX_ENTRY_REVIEWS', 'Reviews:');
+
+define('BOX_CONNECTION_PROTECTED', 'You are protected by a %s secure SSL connection.');
+define('BOX_CONNECTION_UNPROTECTED', 'You are <font color="#ff0000">not</font> protected by a secure SSL connection.');
+define('BOX_CONNECTION_UNKNOWN', 'unknown');
+
+define('CATALOG_CONTENTS', 'Contents');
+
+define('REPORTS_PRODUCTS', 'Products');
+define('REPORTS_ORDERS', 'Orders');
+
+define('TOOLS_BACKUP', 'Backup');
+define('TOOLS_BANNERS', 'Banners');
+define('TOOLS_FILES', 'Files');
+?>
