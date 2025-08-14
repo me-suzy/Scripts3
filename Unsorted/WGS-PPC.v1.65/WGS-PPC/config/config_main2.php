098c252014d03f5c916576a049e0749e1d3a2e5d
me-suzy <me.suzana@gmail.com>
2025-08-14 03:35:11 +0300

    Add Unsorted/WGS-PPC.v1.65/WGS-PPC/config/config_main2.php

---
+++ Unsorted/WGS-PPC.v1.65/WGS-PPC/config/config_main2.php
@@ -0,0 +1,34 @@
+<?
+	/////////////////////////////////////
+	// PPC Search Engine.              //
+	// (c) Ettica.com, 2002            //
+	// Developed by Ettica             //
+	/////////////////////////////////////
+	//                                 //
+	//  Main settings                  //
+	//                                 //
+	/////////////////////////////////////
+	$MAIN_URL = "index.php";
+	$CLICK_SCRIPT = "ppcclick.php";
+	$CLICK_TARGET = "_blank";
+	
+	$BANNER_CLICK = "ppcbannerclick.php";
+	
+	$CATEGORIES_COLUMNS = 2;
+	$PAGES_PER_WINDOW = 11;
+	
+	//Templates
+	$MAIN_MAIN_SCREEN = $TEMPLATES_DIR."main_mainscreen.html";
+	$MAIN_TOP_SEARCHES = $TEMPLATES_DIR."main_topsearches.html";
+	$MAIN_TOP_SEARCHES_PARTS = $TEMPLATES_DIR."main_topsearches_parts.html";
+	$MAIN_TOP_SEARCHES_PARTS_EMPTY = $TEMPLATES_DIR."main_topsearches_parts_empty.html";
+	$MAIN_CATEGORIESLIST_TMP = $TEMPLATES_DIR."main_categorieslist.html";
+	$MAIN_CATEGORIESLIST_PARTS_TMP = $TEMPLATES_DIR."main_categorieslist_parts.html";
+	$MAIN_CATEGORIESLIST_PARTS2_TMP = $TEMPLATES_DIR."main_categorieslist_parts2.html";
+		
+	$MAIN_SRES_TMP = $TEMPLATES_DIR."main_sresults.html";
+	$PPC_RESULTS_PART = $TEMPLATES_DIR."main_sresults_part.html";
+	
+	$PAGES_LINKS_TMP = $TEMPLATES_DIR."main_pageslinks.html";
+	//End Of Templates
+?>
