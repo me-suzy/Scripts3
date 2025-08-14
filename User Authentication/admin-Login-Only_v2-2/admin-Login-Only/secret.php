457a169949159d3e72fc3637b501a04f8527c10b
me-suzy <me.suzana@gmail.com>
2025-08-14 04:33:05 +0300

    Add User Authentication/admin-Login-Only_v2-2/admin-Login-Only/secret.php

---
+++ User Authentication/admin-Login-Only_v2-2/admin-Login-Only/secret.php
@@ -0,0 +1,11 @@
+<?php
+/* secret.php
+ CONSTANT DECLARATIONS - DO NOT CHANGE UPPERCASE CONSTANTS.
+ Change lowercase values only
+*/
+/* Administration */
+define("ADMINUSER", "admin"); /* your administration login name - modify - you make it up */
+define("ADMINPASSWORD", "admin"); /* your administration password - modify - you make it up */
+/* below is the webpage you will go to if your login is successful */
+define("ADMINHOME", "admin.php"); /* your administration page name - modify - the page you go to if the login is successful: Example: admin.php */
+?>
