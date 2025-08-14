730aa2f3667dfc33618e25fb1b0583e97fdaf7cd
me-suzy <me.suzana@gmail.com>
2025-08-14 03:34:56 +0300

    Add Unsorted/WGS-PPC.v1.65/WGS-PPC/step2.php

---
+++ Unsorted/WGS-PPC.v1.65/WGS-PPC/step2.php
@@ -0,0 +1,43 @@
+<? If(!$Error){ ?>
+<br><b>Setup is completed</b><br><br>
+
+<table><tr><td>
+<u>URLs:</u><br><br>
+
+Administrator's control panel: <b>http://www.yoursite.com/..../admin/</b><br>
+Member's control panel: <b>http://www.yoursite.com/..../members/</b><br>
+Registration of new member: <b>http://www.yoursite.com/..../members/registration.php</b><br>
+Main page: <b>http://www.yoursite.com/..../main/</b><br>
+<br>
+<u>Templates:</u><br>
+<ul>
+	<li>All templates locate in templates/ directory
+	</li>
+	<li>Adminitrator's pages:
+		<ul>
+			<li>All templates begins with &quot;admin_&quot;</li>
+			<li>Main template is &quot;admin_templates.html&quot;</li>
+		</ul>
+	</li>
+	<li>Member's pages:
+		<ul>
+			<li>All templates begins with &quot;member_&quot;</li>
+			<li>Main template is &quot;member_cp.html&quot;</li>
+		</ul>
+	</li>	
+	<li>Main pages:
+		<ul>
+			<li>All templates begins with &quot;main_&quot;</li>
+			<li>Main template is &quot;main_mainscreen.html&quot;</li>
+		</ul>
+	</li>
+	<li>E-mails:
+		<ul>
+			<li>All templates begins with &quot;email_&quot;</li>
+		</ul>
+	</li>
+</ul>
+<br><br>
+</td></tr>
+</table>
+<? } ?>
