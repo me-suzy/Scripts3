439f67612e5e50299d8c976c2452559248bd1462
me-suzy <me.suzana@gmail.com>
2025-08-14 04:12:34 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/account_password.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/account_password.php
@@ -0,0 +1,27 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: account_password.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(account_password.php,v 1.1 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (account_password.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'My Account');
+define('NAVBAR_TITLE_2', 'Change Password');
+
+define('HEADING_TITLE', 'My Password');
+
+define('MY_PASSWORD_TITLE', 'My Password');
+
+define('SUCCESS_PASSWORD_UPDATED', 'Your password has been successfully updated.');
+define('ERROR_CURRENT_PASSWORD_NOT_MATCHING', 'Your Current Password did not match the password in our records. Please try again.');
+?>
