82a398d460be4425d7b854102f6ea1dc5f7bdb58
me-suzy <me.suzana@gmail.com>
2025-08-14 04:14:46 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/checkout_payment.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/checkout_payment.php
@@ -0,0 +1,40 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: checkout_payment.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(checkout_payment.php,v 1.20 2003/02/16); www.oscommerce.com 
+   (c) 2003	 nextcommerce (checkout_payment.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Kasse');
+define('NAVBAR_TITLE_2', 'Zahlungsweise');
+
+define('HEADING_TITLE', 'Zahlungsweise');
+
+define('TABLE_HEADING_BILLING_ADDRESS', 'Rechnungsadresse');
+define('TEXT_SELECTED_BILLING_DESTINATION', 'Bitte w&auml;hlen Sie aus Ihrem Adressbuch die gew&uuml;nschte Rechnungsadresse f&uuml;r Ihre Bestellung aus.');
+define('TITLE_BILLING_ADDRESS', 'Rechnungsadresse:');
+
+define('TABLE_HEADING_PAYMENT_METHOD', 'Zahlungsweise');
+define('TEXT_SELECT_PAYMENT_METHOD', 'Bitte w&auml;hlen Sie die gew&uuml;nschte Zahlungsweise f&uuml;r Ihre Bestellung aus.');
+define('TITLE_PLEASE_SELECT', 'Bitte w&auml;hlen Sie');
+define('TEXT_ENTER_PAYMENT_INFORMATION', 'Zur Zeit bieten wir Ihnen nur eine Zahlungsweise an.');
+
+define('TABLE_HEADING_COMMENTS', 'F&uuml;gen Sie hier Ihre Anmerkungen zu dieser Bestellung ein');
+
+define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Fortsetzung des Bestellvorganges');
+define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'zur Best&auml;tigung Ihrer Bestellung.');
+
+define('HEADING_CONDITIONS_INFORMATION', 'Allgemeine Gesch&auml;fts- und Lieferbedingungen');
+define('TEXT_CONDITIONS_CONFIRM', 'Ich akzeptiere Ihre allgemeinen Gesch&auml;fts- und Lieferbedingungen');
+define('TEXT_CONDITIONS_DOWNLOAD', 'AGB\'s herunterladen');
+?>
