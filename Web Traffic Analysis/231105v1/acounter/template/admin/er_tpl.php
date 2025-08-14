0d432a6c09bd32e2f952d5c5f4b759c8194aa0bf
me-suzy <me.suzana@gmail.com>
2025-08-14 04:55:40 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/admin/er_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/admin/er_tpl.php
@@ -0,0 +1,14 @@
+<?php die("Access restricted");?>
+<table height="100" width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+      <td colspan=4 align=center>
+          <font size=3 color="#000000">
+             <SPAN class="%%class%%">%%emes%%</b></SPAN>
+          </font></td>
+  </tr>
+  <tr>
+      <td width=150></td>
+      <td align=center><input name="aaccount_x" type="submit" class=b7 value="Return"></td>
+      <td width=150></td>
+  </tr>
+</table>
