76a0a5a4c4b53c09c5e280c0cf712afb0135e72d
me-suzy <me.suzana@gmail.com>
2025-08-14 05:05:35 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/cache/CacheUtil.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/cache/CacheUtil.class.php
@@ -0,0 +1,25 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: CacheUtil.class.php,v 1.2 2005/10/11 15:57:30 slizardo Exp $
+ * @package poop.cache
+ */
+
+class CacheUtil {
+
+	const T_SECOND = 1;
+	const T_MINUTE = 60; // 1*60
+	const T_HOUR = 3600; // 60*60
+	const T_DAY = 86400; // 3600*24
+	const T_WEEK = 604800; // 86400*7
+}
+	
+?>
