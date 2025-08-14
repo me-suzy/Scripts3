47c354867438b0d714d3866d06483a7d10925671
me-suzy <me.suzana@gmail.com>
2025-08-14 04:12:48 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/checkout_shipping.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/checkout_shipping.php
@@ -0,0 +1,36 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: checkout_shipping.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(checkout_shipping.php,v 1.3 2003/02/06); www.oscommerce.com 
+   (c) 2003	 nextcommerce (checkout_shipping.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Checkout');
+define('NAVBAR_TITLE_2', 'Shipping Method');
+
+define('HEADING_TITLE', 'Delivery Information');
+
+define('TABLE_HEADING_SHIPPING_ADDRESS', 'Shipping Address');
+define('TEXT_CHOOSE_SHIPPING_DESTINATION', 'Please choose from your address book where you would like the items to be delivered to.');
+define('TITLE_SHIPPING_ADDRESS', 'Shipping Address:');
+
+define('TABLE_HEADING_SHIPPING_METHOD', 'Shipping Method');
+define('TEXT_CHOOSE_SHIPPING_METHOD', 'Please select the preferred shipping method to use on this order.');
+define('TITLE_PLEASE_SELECT', 'Please Select');
+define('TEXT_ENTER_SHIPPING_INFORMATION', 'This is currently the only shipping method available to use on this order.');
+
+define('TABLE_HEADING_COMMENTS', 'Add Comments About Your Order');
+
+define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Continue Checkout Procedure');
+define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'to select the preferred payment method.');
+?>
