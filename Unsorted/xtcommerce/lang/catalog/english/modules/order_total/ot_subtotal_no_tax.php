0755ca2c60f573dc5dfb75a160fdf39e6055d57c
me-suzy <me.suzana@gmail.com>
2025-08-14 04:13:36 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/modules/order_total/ot_subtotal_no_tax.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/modules/order_total/ot_subtotal_no_tax.php
@@ -0,0 +1,28 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: ot_subtotal_no_tax.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(ot_subtotal.php,v 1.1 2002/04/03); www.oscommerce.com 
+   (c) 2003	 nextcommerce (ot_subtotal_no_tax.php,v 1.5 2003/08/24); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_TITLE', 'Total, no tax');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_DESCRIPTION', 'Order Sub-Total');
+  
+
+  
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS_TITLE','Display Sub-Total without tax');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS_DESC','Do you want to display the order sub-total cost?');
+  
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_SORT_ORDER_TITLE','Sort Order');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_SORT_ORDER_DESC', 'Sort order of display.');
+?>
