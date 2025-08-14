f2994a214bef08409768eebbaf47f5cb2e450e19
me-suzy <me.suzana@gmail.com>
2025-08-14 03:23:32 +0300

    Add Unsorted/webauction2/webauction/includes/durations.inc.php

---
+++ Unsorted/webauction2/webauction/includes/durations.inc.php
@@ -0,0 +1,24 @@
+<?
+
+ /*
+   Copyright (c), 1999, 2000 - phpauction.org
+   Copyright (c), 2001, 2002 - webauction.de.vu
+
+   Lizenz siehe lizenz.txt & license
+
+   Die neueste Version kostenfrei zum Download unter:
+   http://webauction.de.vu
+
+*/
+
+
+$durations = array (
+		 0 =>		"1 Tag",
+		 1 =>		"1 Tag",
+		 2 =>		"2 Tage",
+		 3 =>		"3 Tage",
+		 4 =>		"4 Tage",
+		 5 =>		"5 Tage"
+);
+ sort($durations); 
+ ?>
