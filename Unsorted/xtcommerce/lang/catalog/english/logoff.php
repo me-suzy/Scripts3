9152ef6000096ef7639b224375256a8f5a52104e
me-suzy <me.suzana@gmail.com>
2025-08-14 04:13:06 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/logoff.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/logoff.php
@@ -0,0 +1,21 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: logoff.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(logoff.php,v 1.1 2003/01/14); www.oscommerce.com 
+   (c) 2003	 nextcommerce (logoff.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Log Off');
+define('NAVBAR_TITLE', 'Log Off');
+define('TEXT_MAIN', 'You have been logged off your account. It is now safe to leave the computer.<br><br>Your shopping cart has been saved, the items inside it will be restored whenever you log back into your account.');
+?>
