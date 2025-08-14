c81070a3094a420bc1beb171ddeb41a3eeec66de
me-suzy <me.suzana@gmail.com>
2025-08-14 04:57:14 +0300

    Add Web Traffic Analysis/BCB_Spider/Spider/includes/spider_array.php

---
+++ Web Traffic Analysis/BCB_Spider/Spider/includes/spider_array.php
@@ -0,0 +1,114 @@
+<?php
+/************************************************************************/
+/* BCB Spider Tracker: Simple Search Engine Bot Tracking                */
+/* ============================================                         */
+/*                                                                      */
+/* Copyright (c) 2004 by www.bluecollarbrain.com                        */
+/* http://bluecollarbrain.com                                           */
+/*                                                                      */
+/* This program is free software. You may use it as you wish.           */
+/*   This File: spider_array.php                                        */ 
+/*   This is the array that spider_bcb.php uses to match against the    */
+/*   incoming $_SERVER['HTTP_USER_AGENT']. If it ain't on this list,    */
+/*   it ain't getting tracked. You can add as many new spiders/bots     */
+/*   as you want, just make sure not to duplicate, or you will get      */
+/*   multiple matches and multiple records in your db table.            */ 
+/************************************************************************/
+
+$spider_array[] = "googlebot"; 				// Google's bot
+$spider_array[] = "msnbot";					// MicroSoft's bot
+$spider_array[] = "slurp";					// Yahoo!'s bot
+$spider_array[] = "ask jeeves";				// Ask Jeeves
+$spider_array[] = "fast";
+$spider_array[] = "infoseek";
+$spider_array[] = "tecnoseek";
+$spider_array[] = "scooter";
+$spider_array[] = "crawler";
+$spider_array[] = "firefly";
+$spider_array[] = "rabaz";
+$spider_array[] = "zyborg";
+$spider_array[] = "spade";
+$spider_array[] = "webbug";
+$spider_array[] = "appie";
+$spider_array[] = "www.galaxy.com";
+$spider_array[] = "girafabot";
+$spider_array[] = "webfindbot";
+$spider_array[] = "nationaldirectory";
+$spider_array[] = "teoma_agent_1";
+$spider_array[] = "gulliver";
+$spider_array[] = "ABCdatos";
+$spider_array[] = "Arachnophilia";
+$spider_array[] = "Araneo";
+$spider_array[] = "Anthill";
+$spider_array[] = "AraybOt";
+$spider_array[] = "ArchitextSpider";
+$spider_array[] = "arks";
+$spider_array[] = "ASpider";
+$spider_array[] = "ATN_Worldwide";
+$spider_array[] = "Atomz";
+$spider_array[] = "Auresys";
+$spider_array[] = "BackRub";
+$spider_array[] = "bbot";
+$spider_array[] = "Bjaaland";
+$spider_array[] = "BlackWidow";
+$spider_array[] = "borg";
+$spider_array[] = "BSpider";
+$spider_array[] = "Checkbot";
+$spider_array[] = "ChristCrawler";
+$spider_array[] = "Internet Cruiser Robot";
+$spider_array[] = "CyberSpyder";
+$spider_array[] = "CydralSpider";
+$spider_array[] = "Digger";
+$spider_array[] = "grabber";
+$spider_array[] = "EIT-Link-Verifier-Robot";
+$spider_array[] = "Emacs";
+$spider_array[] = "explorersearch";
+$spider_array[] = "FastCrawler";
+$spider_array[] = "Freecrawl";
+$spider_array[] = "Gulper";
+$spider_array[] = "wired-digital-newsbot";
+$spider_array[] = "HTMLgobble";
+$spider_array[] = "IncyWincy";
+$spider_array[] = "InfoSpiders";
+$spider_array[] = "IAGENT";
+$spider_array[] = "Lycos";
+$spider_array[] = "Monster";
+$spider_array[] = "NetCarta";
+$spider_array[] = "NetMechanic";
+$spider_array[] = "NetScoop";
+$spider_array[] = "newscan-online";
+$spider_array[] = "Orbsearch";
+$spider_array[] = "phpdig";
+$spider_array[] = "PortalJuice";
+$spider_array[] = "PortalBSpider";
+$spider_array[] = "SiteTech";
+$spider_array[] = "ESISmartSpider";
+$spider_array[] = "Snooper";
+$spider_array[] = "Speedy Spider";
+$spider_array[] = "spiderline";
+$spider_array[] = "SpiderMan";
+$spider_array[] = "Tarantula";
+$spider_array[] = "TechBOT";
+$spider_array[] = "UCSD-Crawler";
+$spider_array[] = "URL Spider Pro";
+$spider_array[] = "Valkyrie";
+$spider_array[] = "Verticrawl";
+$spider_array[] = "w3index";
+$spider_array[] = "Subjex";
+$spider_array[] = "searchit";
+$spider_array[] = "entireweb";
+$spider_array[] = "alexa";
+$spider_array[] = "scrubtheweb";
+$spider_array[] = "whatUseek";
+$spider_array[] = "aeiwi";
+$spider_array[] = "electrostatic";
+$spider_array[] = "SearchWarp";
+$spider_array[] = "SearchRamp";
+$spider_array[] = "Unasked";
+$spider_array[] = "Search-o-rama";
+$spider_array[] = "DinoSearch";
+$spider_array[] = "SonicQuest";
+$spider_array[] = "Jayde";
+$spider_array[] = "SearchSite";
+
+?>
