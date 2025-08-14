84bedccf57adba8a5f7b62f3c31a22dcdd74889c
me-suzy <me.suzana@gmail.com>
2025-08-14 05:09:22 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/mvc/config/DataSourceConfig.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/mvc/config/DataSourceConfig.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: DataSourceConfig.class.php,v 1.3 2005/10/12 21:00:43 slizardo Exp $
+ * @package poop.mvc.config
+ */
+
+class DataSourceConfig {
+}
+	
+?>
