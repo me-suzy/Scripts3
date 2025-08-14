14d3ed230dca7bbd72b362c2ad59d75716a45ab2
me-suzy <me.suzana@gmail.com>
2025-08-14 04:58:52 +0300

    Add Web Traffic Analysis/lite264/lite/data/htmlgen.php

---
+++ Web Traffic Analysis/lite264/lite/data/htmlgen.php
@@ -0,0 +1,17 @@
+<?php
+
+$code=<<<CODE
+<!-- ActualAnalyzer %%SERIES%%(%%VERSION%%) -->
+<!-- YOU DO NOT NEED TO EDIT ANYTHING IN THIS HTML CODE -->
+<script language="Javascript"><!--
+aw=window; aatd=aw.top.document; aasd=aw.self.document;
+aar=escape(aasd.referrer); aap=escape(aw.self.location.href);
+if(self!=top) {aafr=escape(aw.top.location.href);if(aafr==aar) aar=escape(aatd.referrer)}
+document.write("<img border=0 alt='ActualAnalyzer' src='%%URL%%aa.php?anr=" + aar + "&anp="+aap + "'>");
+//--></script><noscript>
+<img border=0 alt='ActualAnalyzer' src='%%URL%%aa.php'>
+</noscript>
+<!-- ActualAnalyzer -->
+CODE;
+
+?>
