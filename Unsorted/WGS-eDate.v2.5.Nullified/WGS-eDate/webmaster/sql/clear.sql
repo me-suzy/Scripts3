754099092a5db3a19001e0b2da35f84c5576d2da
me-suzy <me.suzana@gmail.com>
2025-08-14 03:33:08 +0300

    Add Unsorted/WGS-eDate.v2.5.Nullified/WGS-eDate/webmaster/sql/clear.sql

---
+++ Unsorted/WGS-eDate.v2.5.Nullified/WGS-eDate/webmaster/sql/clear.sql
@@ -0,0 +1,7 @@
+drop table IF EXISTS members;
+drop table IF EXISTS profiles;
+drop table IF EXISTS pictures;
+drop table IF EXISTS sysvars;
+drop table IF EXISTS menus;
+drop table IF EXISTS event;
+
