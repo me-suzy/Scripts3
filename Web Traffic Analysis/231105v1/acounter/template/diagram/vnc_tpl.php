51bcb40c019d7aa37a6a780290fc6ddeb7cc5b44
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:05 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/diagram/vnc_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/diagram/vnc_tpl.php
@@ -0,0 +1,8 @@
+<?php die("Access restricted");?>
+    <td align=center vAlign=bottom>
+<table width=30 border="0" cellspacing="0" cellpadding="0">
+  <tr> 
+    <td width="30" height=210></td>
+  </tr>
+</table>
+    </td>  
