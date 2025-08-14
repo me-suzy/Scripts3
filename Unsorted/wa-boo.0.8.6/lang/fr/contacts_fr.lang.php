689464622da1194c5280e0b5e5ebb3adc58a94f8
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:41 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/contacts_fr.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/contacts_fr.lang.php
@@ -0,0 +1,55 @@
+0|Gestion des contacts  - |
+1|Compte / Groupes|
+2|Préférences|
+3|Nouveau contact|
+4|Import|
+5|Export|
+6|Entretien|
+7|Déconnecter|
+8|Rechercher les contacts|
+9|privés|
+10|publics|
+11|Rechercher|
+12|Aide|
+13|commençant par |
+14| contenant |
+15|dans le champ Non |
+16| dans tous les champs|
+17|Tous|
+18|Liste des |
+19|Modifier|
+20|Supprimer|
+21| contacts |
+22|contacts |
+23| à |
+24| sur |
+25|Fiche détaillée |
+26|créé par |
+27|Fiche contact |
+28|Effacer |
+29|enregistrer |
+30|Prénom |
+31|Nom |
+32|Adresse |
+33|Adresse (suite) |
+34|Code postal |
+35|Ville |
+36|Pays |
+37|E-mail |
+38|Pseudo messagerie |
+39|Site Web |
+40|Téléphone |
+41|Téléphone portable |
+42|Téléphone professionnel |
+43|Fax |
+44|Entreprise |
+45|Poste occupé / métier |
+46|Observations |
+47|Catégorie |
+48|Créé par |
+49|Vous ne pouvez sélectionner un groupe que lorsque le type 'Groupe' est sélectionné !|
+50|Privé|
+51|Public|
+52|Groupe|
+53|Type|
+52|###### PLEASE DO NOT ERASE THIS LINE #####||
