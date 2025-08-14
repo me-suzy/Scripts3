8f6acd8e363dbc15958e8887e44b1bf794b01c7b
me-suzy <me.suzana@gmail.com>
2025-08-14 03:21:22 +0300

    Add Unsorted/webauction2/webauction/sofortkauf.php

---
+++ Unsorted/webauction2/webauction/sofortkauf.php
@@ -0,0 +1,27 @@
+<?
+        require('./includes/messages.inc.php');
+        require('./includes/config.inc.php');
+
+
+
+
+        if (!$result)
+
+        {
+
+                include("header.php");
+
+
+                      include("templates/sofortkauf.html");
+
+                include("footer.php");
+
+                exit;
+
+        }
+
+
+
+
+
+?>
