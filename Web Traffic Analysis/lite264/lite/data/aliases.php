e7680aedbcbacc90775f4d7f8680325b37aeb4b7
me-suzy <me.suzana@gmail.com>
2025-08-14 04:58:46 +0300

    Add Web Traffic Analysis/lite264/lite/data/aliases.php

---
+++ Web Traffic Analysis/lite264/lite/data/aliases.php
@@ -0,0 +1,21 @@
+<?php
+
+/*------------------------------------------------------------------------------
+Add the names of the domains you want to track below this block.
+Example:
+
+$alias[]='actualscripts.net';
+$alias[]='actualscripts.org';
+
+ActualAnalyzer will now store the statistics for actualscripts.net and
+actualscripts.org also.
+
+Add the names of the domains you want to track under this line!
+------------------------------------------------------------------------------*/
+
+
+
+
+
+
+?>
