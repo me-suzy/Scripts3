08bebfbd336fe5eb7aefd370b19911692937ef05
me-suzy <me.suzana@gmail.com>
2025-08-14 04:10:01 +0300

    Add Unsorted/xtcommerce/lang/admin/english/content_manager.php

---
+++ Unsorted/xtcommerce/lang/admin/english/content_manager.php
@@ -0,0 +1,74 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: content_manager.php,v 1.1 2003/09/06 21:54:33 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   --------------------------------------------------------------
+   based on:
+   (c) 2003	 nextcommerce (content_manager.php,v 1.3 2003/08/2); www.nextcommerce.org
+   
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+   
+ define('HEADING_TITLE','Content Manager');
+ define('TABLE_HEADING_CONTENT_ID','LinkID');
+ define('TABLE_HEADING_CONTENT_TITLE','Title');
+ define('TABLE_HEADING_CONTENT_FILE','File');
+ define('TABLE_HEADING_CONTENT_STATUS','Visible in box');
+ define('TABLE_HEADING_CONTENT_BOX','Box');
+ define('TEXT_GROUP','Group:');
+ define('TEXT_FILE_FLAG','Box:');
+ define('TABLE_HEADING_PRODUCTS_ID','ID');
+ define('TABLE_HEADING_PRODUCTS','Products');
+ define('TABLE_HEADING_PRODUCTS_CONTENT_ID','ID');
+ define('TABLE_HEADING_LANGUAGE','Language');
+ define('TABLE_HEADING_CONTENT_NAME','Name/Filename');
+ define('TABLE_HEADING_CONTENT_LINK','Link');
+ define('TABLE_HEADING_CONTENT_HITS','Viewed');
+ define('TABLE_HEADING_CONTENT_GROUP','Group');
+ 
+ define('HEADING_CONTENT','Site Content');
+ define('HEADING_PRODUCTS_CONTENT','Products Content');
+ define('TEXT_YES','Yes');
+ define('TEXT_NO','No');
+ define('TABLE_HEADING_CONTENT_ACTION','Action');
+ define('TEXT_DELETE','Delete');
+ define('TEXT_EDIT','Edit');
+ define('TEXT_PREVIEW','Preview');
+ define('CONFIRM_DELETE','Delete Content ?');
+ define('CONTENT_NOTE','Content marked with <font color="ff0000">*</font> is part of system and cannot be deleted');
+ 
+ // edit
+ define('TEXT_LANGUAGE','Language:');
+ define('TEXT_STATUS','Visible:');
+ define('TEXT_STATUS_DESCRIPTION','if checked, Link will be displayed in Information Box');
+ define('TEXT_TITLE','Title:');
+ define('TEXT_SELECT','-Select-');
+ define('TEXT_HEADING','Heading:');
+ define('TEXT_CONTENT','Text:');
+ define('TEXT_PRODUCT','Product:');
+ define('TEXT_TITLE_FILE','Filename/Name:');
+ define('TEXT_LINK','Link:');
+ define('TEXT_FILE_DESC','Description:');
+ define('TEXT_FILENAME','Used File:');
+ define('TEXT_CHOOSE_FILE_DESC','You can choose an existing file');
+ define('TEXT_CONTENT_DESCRIPTION','With this Content Manager you can add any filetype to a product, like technical sheets, productdetails, videos,....');
+ 
+ define('TEXT_UPLOAD_FILE','Load file:');
+ define('TEXT_UPLOAD_FILE_LOCAL','(from local system)');
+ define('TEXT_CHOOSE_FILE','Choose File:');
+ define('TEXT_NO_FILE','Delete Selection');
+ define('TEXT_CHOOSE_FILE_SERVER','(If you uploaded your files already via FTP to <i>(media/content)</i>, you can select them.');
+ define('TEXT_CURRENT_FILE','Uses file:');
+ define('TEXT_FILE_DESCRIPTION','<b>Info:</b><br>You also got the oportunity to upload a  <b>.htlm</b> or <b>.htm</b> File, an display it as Shop content..<br> If you select or upload a file, the text in the box will ne ignored.<br><br>'); 
+ define('ERROR_FILE','Wrong File format (only .html or .htm)');
+ define('ERROR_TITLE','Please enter a title');
+ 
+  define('USED_SPACE','Used Space:');
+ define('TABLE_HEADING_CONTENT_FILESIZE','Filesize');
+   
+ 
+ ?>
