4250695f83af8ef4843111a26e16bb727d423745
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:26 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/import3_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/import3_en.lang.php
@@ -0,0 +1,16 @@
+0|Import 3/5 : fields selection|
+1|You file contents |
+2| lines and |
+3| columns.|
+4|Display |
+5| preview lines.|
+6|Preview|
+7| First line of my file is fields names, do not import it|
+8| Show truncated fields as import will reduce them.|
+9| Warning, you must select at least the 'Name' field|
+10|Imported contacts will be link to category |
+11|<< Back|
+12|Import !|
+13|Line #|
+14| -- NOT IMPORTED -- |
+14|###### PLEASE DO NOT ERASE THIS LINE #####||
