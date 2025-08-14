38b7f7603f7d67782ff57eca1523a20d44eca03b
me-suzy <me.suzana@gmail.com>
2025-08-14 05:11:52 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/util/Random.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/util/Random.class.php
@@ -0,0 +1,49 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: Random.class.php,v 1.3 2005/10/12 21:01:04 slizardo Exp $
+ * @package poop.util
+ */
+	
+class Random {
+
+	/**
+	 * @param int $seed
+	 */
+	public function __construct($seed = null) {
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function nextBoolean() {
+	}
+	
+	/**
+	 * @return double
+	 */
+	public function nextDouble() {
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function nextInt() {
+	}
+
+	/**
+	 * @param int $seed
+	 */
+	public function setSeed($seed) {
+	}
+}
+	
+?>
