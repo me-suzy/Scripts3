e8df79e097c5859afb519a85a59a9462ab7761cd
me-suzy <me.suzana@gmail.com>
2025-08-14 03:22:21 +0300

    Add Unsorted/webauction2/webauction/admin/header.php

---
+++ Unsorted/webauction2/webauction/admin/header.php
@@ -0,0 +1,36 @@
+<SCRIPT Language=PHP>
+/*
+   Copyright (c), 1999, 2000 - phpauction.org
+   Copyright (c), 2001, 2002 - webauction.de.vu
+
+   Lizenz siehe lizenz.txt & license
+
+   Die neueste Version kostenfrei zum Download unter:
+   http://webauction.de.vu
+
+*/
+	
+
+   include "../includes/dates.inc.php";
+	
+
+</SCRIPT>
+<TITLE></TITLE>
+
+
+</HEAD>
+
+<BODY BGCOLOR="#FFFFFF" TEXT="#000000" LINK="#000000" VLINK="#000000" ALINK="#000000">
+<TABLE width="100%" border="0" cellspacing="0" cellpadding="1" bgcolor="#32972d">
+  <TR> 
+    <TD>
+      <TABLE width="100%" border="0" cellspacing="0" cellpadding="8" bgcolor="#EEEEEE">
+        <TR> 
+          <TD width="35%" VALIGN=MIDDLE>
+          <A HREF="http://webauction.de.vu"><IMG src="../images/logo.gif" border="0"></A></TD>
+          <TD width="65%" valign="MIDDLE" align=center>
+            <FONT FACE="Verdana,Arial,Helvetica" SIZE=4 COLOR="#32972d">
+            <B>Adminbereich (Einstellungen)</B></FONT>
+          </TD>
+        </TR>
+      </TABLE>
