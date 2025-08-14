c5eb47e3353bd9794959ec21bbcda596483905c7
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:22 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/import1_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/import1_en.lang.php
@@ -0,0 +1,14 @@
+0|Import 1/5 : file selection and upload|
+1|Import process will add contacts in wa-boo <br>from files built or exported by other applications (Outlook Express&#153;, PDAs, etc...)|
+2|file format must be text ' with separator ', <br> means that fields are separated by a special char.|
+3|Usual separators are :|
+4|comma|
+5|semicolon|
+6|Tab char [On your keyboard : ]|
+7|select file to upload by ckicking on 'Browse...' button |
+8|This file must be text, with separator limits.|
+9|Field separator |
+10| (for Tab char, please type letter ' t ')|
+11|Quit import process|
+12|Next >>|
+13|###### PLEASE DO NOT ERASE THIS LINE #####||
