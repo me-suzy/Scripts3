f20a94f5b1df42e724bcfbc7032a759f06358dcf
me-suzy <me.suzana@gmail.com>
2025-08-14 03:17:07 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/admin_help/credits.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/admin_help/credits.php
@@ -0,0 +1,50 @@
+<?
+    include ("../../../includes/global_vars.php");
+    include ("../../../includes/fotools.php");
+
+    session_start();
+?>
+<html>
+    <head>
+    <title>wa-boo</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <style><? include ("../../../includes/css.php"); ?></style> 
+    </head>
+
+    <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" background="../../../images/bg1.gif">
+        <table border="0" cospan="0">
+            <tr>
+                <td>
+                    <div align="center" class="hlptitle">
+                         Crédits
+                    </div>
+                    
+                    <br>
+                   
+                    <div align="center" class="hlpsubtitle">
+                        Merci à :
+                        <br><br>
+                    </div>
+                    <div align="center" class="hlpfont">
+                        Jérôme Benoît :: Seb 'Pika' Biagioni :: Annabel Camilleri :: Monika Castella :: David 'Dad' Darmon :: Greg Frayssinet :: Lap :: Renaud Paillard :: les Rats :: Christine Simon :: Jean-Christophe Urrutia ::
+                        
+                        <br><br><br>
+                    </div>
+                    <div align="center" class="hlpsubtitle">
+                    Concept / développement
+                    </div>
+                    <br>
+                    </div>
+                    <div align="center" class="hlpfont">
+                         Alexandre VICQ <i>[Aaaxl]</i> - <a href="mailto:axl@nocive.com">axl@nocive.com</a>
+                         <br>for <br><i>The GroinkWare Factory Inc (Fast, quick and dirty)&nbsp;&&nbsp;nocive.com (nocive.com est ton ami :-)</i>
+                         <br><br><a href="http://www.gnu.org/copyleft/copyleft.fr.html">Copyleft</a> <strike>&copy;</strike> 2002
+                         <br>Made in Marseille / France
+                    </div>
+                    
+                </td>
+            </tr>
+        </table>
+        
+    </body>
+</html>
