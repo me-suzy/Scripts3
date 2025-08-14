1b5681d393b2352bd25bc208f6480289395a4668
me-suzy <me.suzana@gmail.com>
2025-08-14 04:10:57 +0300

    Add Unsorted/xtcommerce/lang/admin/english/whos_online.php

---
+++ Unsorted/xtcommerce/lang/admin/english/whos_online.php
@@ -0,0 +1,31 @@
+<?php
+/* --------------------------------------------------------------
+   $Id: whos_online.php,v 1.1 2003/09/06 21:54:33 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   --------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(whos_online.php,v 1.5 2002/03/30); www.oscommerce.com 
+   (c) 2003	 nextcommerce (whos_online.php,v 1.4 2003/08/1); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Who\'s Online');
+
+define('TABLE_HEADING_ONLINE', 'Online');
+define('TABLE_HEADING_CUSTOMER_ID', 'ID');
+define('TABLE_HEADING_FULL_NAME', 'Full Name');
+define('TABLE_HEADING_IP_ADDRESS', 'IP Address');
+define('TABLE_HEADING_ENTRY_TIME', 'Entry Time');
+define('TABLE_HEADING_LAST_CLICK', 'Last Click');
+define('TABLE_HEADING_LAST_PAGE_URL', 'Last URL');
+define('TABLE_HEADING_ACTION', 'Action');
+define('TABLE_HEADING_SHOPPING_CART', 'Users Shopping Cart');
+define('TEXT_SHOPPING_CART_SUBTOTAL', 'Subtotal');
+define('TEXT_NUMBER_OF_CUSTOMERS', 'Currently there are %s customers online');
+?>
