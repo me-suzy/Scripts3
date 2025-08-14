907e88652b975c312799bd8d7fd197112e162569
me-suzy <me.suzana@gmail.com>
2025-08-14 04:37:41 +0300

    Add User Authentication/vSignup_2.5/vSignup/members/page2.php

---
+++ User Authentication/vSignup_2.5/vSignup/members/page2.php
@@ -0,0 +1,34 @@
+<?
+/*
+# File: members/page2.php
+# Script Name: vSignup 2.5
+# Author: Vincent Ryan Ong
+# Email: support@beanbug.net
+#
+# Description:
+# vSignup is a member registration script which utilizes vAuthenticate
+# for its security handling. This handy script features email verification,
+# sending confirmation email message, restricting email domains that are 
+# allowed for membership, and much more.
+#
+# This script is a freeware but if you want to give donations,
+# please send your checks (coz cash will probably be stolen in the
+# post office) them to:
+#
+# Vincent Ryan Ong
+# Rm. 440 Wellington Bldg.
+# 655 Condesa St. Binondo, Manila
+# Philippines, 1006
+*/
+?>
+<?
+	include_once ("../auth.php");
+	include_once ("../authconfig.php");
+	include_once ("../check.php");
+?>
+<head><title>Sample Page</title></head>
+<body bgcolor="#FFFFFF">
+<font color="#000066"><b><font face="Verdana, Arial, Helvetica, sans-serif" size="3">If 
+you can see this, you have logged in successfully a while ago and you're trying 
+to access this page via direct method. You will continue to 
+be able to directly access files until you close all browsers.</font></b></font>
