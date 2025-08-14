21ac70354357c99502dff00273960813399b6ada
me-suzy <me.suzana@gmail.com>
2025-08-14 05:10:54 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/sql/config/DataSourceConfig.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/sql/config/DataSourceConfig.class.php
@@ -0,0 +1,89 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: DataSourceConfig.class.php,v 1.2 2005/10/12 21:01:00 slizardo Exp $
+ * @package poop.sql.config
+ */
+ 
+import('poop.sql.Connection');
+
+class DataSourceConfig {
+
+	private $id;
+	private $url;
+	private $username;
+	private $password;
+
+	/**
+	 * @return string
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * @param string $id
+	 */
+	public function setId($id) {
+		$this->id = $id;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getUrl() {
+		return $this->url;
+	}
+
+	/**
+	 * @param string $url
+	 */
+	public function setUrl($url) {
+		$this->url = $url;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getUsername() {
+		return $this->username;
+	}
+
+	/**
+	 * @param string $username
+	 */
+	public function setUsername($username) {
+		$this->username = $username;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPassword() {
+		return $this->password;
+	}
+
+	/**
+	 * @param string $password
+	 */
+	public function setPassword($password) {
+		$this->password = $password;
+	}
+
+	/**
+	 * @return Connection
+	 */
+	public function getConnection() {
+		return new Connection($this->url, $this->username, $this->password);
+	}
+}
+	
+?>
