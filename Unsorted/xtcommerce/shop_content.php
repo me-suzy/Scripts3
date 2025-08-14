5c9e877336a3a6e05ae9172c5c27b4d0dddf4dbe
me-suzy <me.suzana@gmail.com>
2025-08-14 03:57:26 +0300

    Add Unsorted/xtcommerce/shop_content.php

---
+++ Unsorted/xtcommerce/shop_content.php
@@ -0,0 +1,105 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: shop_content.php,v 1.1 2003/09/06 21:38:27 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(conditions.php,v 1.21 2003/02/13); www.oscommerce.com 
+   (c) 2003	 nextcommerce (shop_content.php,v 1.1 2003/08/19); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  require('includes/application_top.php');
+  // include needed functions
+  require_once('inc/xtc_image_button.inc.php');
+  
+  
+  	 $shop_content_query=xtc_db_query("SELECT
+ 					content_title,
+ 					content_heading,
+ 					content_text,
+ 					content_file
+ 					FROM ".TABLE_CONTENT_MANAGER."
+ 					WHERE content_group='".$_GET['coID']."'
+ 					AND languages_id='".$_SESSION['languages_id']."'");
+ 	$shop_content_data=xtc_db_fetch_array($shop_content_query);
+
+  $breadcrumb->add($shop_content_data['content_title'], xtc_href_link(FILENAME_CONTENT.'coID='.$_GET['coID']));
+?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html <?php echo HTML_PARAMS; ?>>
+<head>
+<?php include(DIR_WS_MODULES.FILENAME_METATAGS); ?>
+<base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0">
+<!-- header //-->
+<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+<!-- header_eof //-->
+
+<!-- body //-->
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+  <tr>
+    <td class="navLeft" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- left_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+<!-- left_navigation_eof //-->
+    </table></td>
+<!-- body_text //-->
+    <td class="tableBody" width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td class="pageHeading"><?php echo $shop_content_data['content_heading']; ?></td>
+            <td class="pageHeading" align="right"></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td><br><table border="0" width="100%" cellspacing="0" cellpadding="2">
+          <tr>
+            <td class="main">
+ <?php
+ if ($shop_content_data['content_file']!=''){
+if (strpos($shop_content_data['content_file'],'.txt')) echo '<pre>';
+include(DIR_FS_CATALOG.'media/content/'.$shop_content_data['content_file']);	
+if (strpos($shop_content_data['content_file'],'.txt')) echo '</pre>';
+ } else {	      
+echo $shop_content_data['content_text'];
+}
+?>
+</td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td align="right" class="main"><br><?php echo '<a href="' . xtc_href_link(FILENAME_DEFAULT) . '">' . xtc_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE) . '</a>'; ?></td>
+      </tr>
+    </table></td>
+<!-- body_text_eof //-->
+    <td class="navRight" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- right_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_right.php'); ?>
+<!-- right_navigation_eof //-->
+    </table></td>
+  </tr>
+</table>
+<!-- body_eof //-->
+
+<!-- footer //-->
+<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+<!-- footer_eof //-->
+<br>
+</body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
