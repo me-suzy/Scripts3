643f72280b90b14b5ab6140f6d9bcc41a6aa1e9e
me-suzy <me.suzana@gmail.com>
2025-08-14 05:09:45 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/portlet/PortletConfig.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/portlet/PortletConfig.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: PortletConfig.class.php,v 1.1 2005/10/19 15:25:32 slizardo Exp $
+ * @package poop.portlet
+ */
+
+interface PortletConfig {
+}
+	
+?>
