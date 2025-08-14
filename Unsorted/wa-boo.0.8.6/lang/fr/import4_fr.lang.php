6ee40193da44b52b21227651e01b877865c3a851
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:55 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/import4_fr.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/import4_fr.lang.php
@@ -0,0 +1,25 @@
+0|Etape 4 : Traitement des doublons|
+1|Votre fichier contient |
+2| doublon(s) de votre wa-boo actuel.|
+3|Par doublons, vous devez entendre que certains contacts déjà présents <br>dans wa-boo existent aussi dans le fichier que vous essayez d'importer.|
+4|Les doublons sont détectés si, sans tenir compte de la casse [MAJUSCULES ou minuscules] :|
+5|- le couple |
+6| nom - prénom |
+7|existe ET dans wa-boo ET dans le fichier|
+8| nom - email |
+9|Exemple : wa-boo détectera un doublon dans les cas suivants :|
+10|Julia Dupont existe dans le fichier et Julia Dupont existe déjà dans wa-boo|
+11|Eléonore Giraud a pour email egiraud@freee.fr dans le fichier alors qu'il existe déjà <br>un contact du nom de Giraud dans wa-boo avec la même adresse email egiraud@freee.fr.|
+12|A présent, à vous de choisir : vous pouvez remplacer le contact existant dans la base <br>par la nouvelle entrée trouvée dans le fichier, ou conserver la version actuelle.|
+13|Sélectionnez ci dessous le contact qui doit être|
+14| présent |
+15|dans wa-boo après l'import.|
+16|Contacts du fichier|
+17|Contacts de la base|
+18|Tous|
+19|Importer avec mes choix|
+20|<< Retour|
+21|Importer sans contrôler les doublons|
+22|Attention !!|
+23|L'état de votre base ne permettra pas à wa-boo de vous aiser à trier <br>les doublons à conserver ou à remplacer. Pour cela, il faudrait que votre base ne contienne pas <br>de doublon avant l'import. Si vous dérirez nettoyer préalablement votre base, faites le maintenant.|
+14|###### PLEASE DO NOT ERASE THIS LINE #####||
