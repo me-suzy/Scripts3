94aa98477c72aa7620b61ffe50ee5f7112b748a0
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:02 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/diagram/vdtm_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/diagram/vdtm_tpl.php
@@ -0,0 +1,6 @@
+<?php die("Access restricted");?>
+<td align="center" width="%%vdtimewidth%%">
+  <font size="%%fsize%%" color="%%fcolor%%">
+ <SPAN class="%%class%%">%%item%%</SPAN>
+  </font>
+</td>
