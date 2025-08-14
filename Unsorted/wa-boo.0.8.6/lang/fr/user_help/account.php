3ddce0d231d008c767eaf14b26b2fbc79ce15ce1
me-suzy <me.suzana@gmail.com>
2025-08-14 03:17:15 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/user_help/account.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/user_help/account.php
@@ -0,0 +1,48 @@
+<?
+    include ("../../../includes/global_vars.php");
+    include ("../../../includes/fotools.php");
+
+    session_start();
+?>
+<html>
+    <head>
+    <title>wa-boo</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <style><? include ("../../../includes/css.php"); ?></style> 
+    </head>
+
+    <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" background="../../../images/bg1.gif">
+        <table border="0" cospan="0">
+            <tr>
+                <td>
+                    <div align="center" class="hlptitle">
+                         Compte / Groupes [Menu]
+                    </div>
+                    <br>
+                    <span class="hlpsubtitle">
+                        Gestion du compte utilisateur : 
+                    </span>
+                    <span class="hlpfont">
+                        Cette page permet de modifier son login et son mot de passe<br>
+                         
+                    </span>
+                   
+                    <br>
+                    <span class="hlpsubtitle">
+                        Communication avec les groupes :
+                    </span>
+                   
+                    <span class="hlpfont">
+                        Si vous faites partie d'un ou plusieurs groupes, ils apparaîtront automatiquement sous la partie du compte utilisateur. 
+                        Vous pourrez alors envoyer un emails à l'un des membres ou envoyer un email signé de votre adresse à l'ensemble des membres et des administrateurs du groupe choisi 
+                        (pour information, les administrateurs peuvent créer, modifier et supprimer les utilisateurs).Ils peuvent aussi exclure un membre des groupes qu'ils administrent.
+                    </span>
+                    <br>
+                    <br>
+                    
+                </td>
+            </tr>
+        </table>
+        
+    </body>
+</html>
