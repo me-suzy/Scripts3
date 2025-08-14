9b481f7ab971910f2ddbe3424640564bbe86779d
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:33 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/table/sts_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/table/sts_tpl.php
@@ -0,0 +1,15 @@
+<?php die("Access restricted");?>
+  <tr bgcolor="#CCCCCC">
+    <td height="25" align="center"><font size=3 color="#000000">
+       <SPAN class="%%class%%">%%val1%%</SPAN> 
+    </font></td>
+    <td align="left"><font size=3 color="#000000">
+       <SPAN class="%%class%%">%%val2%%</SPAN> 
+    </font></td>
+    <td align="center"><font size=3 color="#000000">
+       <SPAN class="%%class%%">%%val3%%</SPAN> 
+    </font></td>
+    <td align="center"><font size=3 color="#000000">
+       <SPAN class="%%class%%">%%val4%%</SPAN> 
+    </font></td>
+  </tr>
