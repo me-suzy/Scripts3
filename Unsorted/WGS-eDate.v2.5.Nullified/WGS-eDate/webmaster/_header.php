bba83871bc9b4d36b3098ccdbe05a9f8bc19a7c8
me-suzy <me.suzana@gmail.com>
2025-08-14 03:33:06 +0300

    Add Unsorted/WGS-eDate.v2.5.Nullified/WGS-eDate/webmaster/_header.php

---
+++ Unsorted/WGS-eDate.v2.5.Nullified/WGS-eDate/webmaster/_header.php
@@ -0,0 +1,10 @@
+<?php include("admin.php"); ?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<title>NeoDate Admin area</title>
+<link rel="stylesheet" href="../style.css">
+</head>
+<body bgcolor=FFFFFF class=bodytext link=0 vlink=0 alink=0 text=0>
+<br><h3><?php echo "Welcome $admin_login !"; ?></h3>
+<br><br>
