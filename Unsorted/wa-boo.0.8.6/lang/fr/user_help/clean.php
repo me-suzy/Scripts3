64a64593f780f148f0aee3ea331c42e5c2d22d90
me-suzy <me.suzana@gmail.com>
2025-08-14 03:17:17 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/user_help/clean.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/user_help/clean.php
@@ -0,0 +1,44 @@
+<?
+    include ("../../../includes/global_vars.php");
+    include ("../../../includes/fotools.php");
+
+    session_start();
+?>
+<html>
+    <head>
+    <title>wa-boo</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <style><? include ("../../../includes/css.php"); ?></style> 
+    </head>
+
+    <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" background="../../../images/bg1.gif">
+        <table border="0" cospan="0">
+            <tr>
+                <td>
+                    <div align="center" class="hlptitle">
+                         Entretien [Menu]
+                    </div>
+                    
+                    <br>
+                   
+                    <span class="hlpfont">
+                        L'entretien permet d'effacer des parties (des lots) de votre wa-boo. Associé à  l'import-export, il permet des transferts complexes vers d'autres applications. 
+                        <br><br>Attention !! Commencez toujours par réaliser une savegarde avant de supprimer des ensembles de contacts !
+                        <br><br>Attention !! Alors que l'export permet d'exporter tous les types de contacts (y compris le type GROUPE - voir rubrique Contacts), l'import crée toujours des contacts de type PRIVE (tout du moins dans cette version).
+                        <br>
+                    </span>
+                    
+                    <br>
+                    <span class="hlpsubtitle">
+                        Suppression de tout ou partie de vos contacts : 
+                    </span>
+                    <span class="hlpfont">
+                       Attention ! La suppression est définitive et wa-boo ne fait pas de sauvegardes pour vous !!.                          
+                    </span>
+                    
+                </td>
+            </tr>
+        </table>
+        
+    </body>
+</html>
