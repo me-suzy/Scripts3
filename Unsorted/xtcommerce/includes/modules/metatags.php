794125c8ff3da3829a5a1c24773c05564ba12834
me-suzy <me.suzana@gmail.com>
2025-08-14 04:08:33 +0300

    Add Unsorted/xtcommerce/includes/modules/metatags.php

---
+++ Unsorted/xtcommerce/includes/modules/metatags.php
@@ -0,0 +1,43 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: metatags.php,v 1.1 2003/09/06 22:13:54 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2003	 nextcommerce (metatags.php,v 1.7 2003/08/14); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+?>
+<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $_SESSION['language_charset']; ?>">
+<meta name="robots" content="<?php echo META_ROBOTS; ?>">
+<meta name="language" content="<?php echo $language; ?>">
+<meta name="author" content="<?php echo META_AUTHOR; ?>">
+<?php
+/*
+  The following copyright announcement is in compliance
+  to section 2c of the GNU General Public License, and
+  thus can not be removed, or can only be modified
+  appropriately.
+
+  Please leave this comment intact together with the
+  following copyright announcement.
+  
+*/
+?>
+<meta name="generator" content="(c) by <?php echo PROJECT_VERSION; ?> , http://www.xt-commerce.com">
+
+<meta name="publisher" content="<?php echo META_PUBLISHER; ?>">
+<meta name="company" content="<?php echo META_COMPANY; ?>">
+<meta name="page-topic" content="<?php echo META_TOPIC; ?>">
+<meta name="reply-to" content="<?php echo META_REPLY_TO; ?>">
+<meta name="distribution" content="global">
+<meta name="revisit-after" content="<?php echo META_REVISIT_AFTER; ?>">
+<META NAME="description" CONTENT="<?php echo META_DESCRIPTION; ?>">
+<META NAME="keywords" CONTENT="<?php echo META_KEYWORDS; ?>">
+
+<title><?php echo TITLE; ?></title>
