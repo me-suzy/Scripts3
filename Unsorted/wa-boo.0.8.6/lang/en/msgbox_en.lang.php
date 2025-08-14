007bea48db6a381d9fcce0ea3c4564a65323a04d
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:34 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/msgbox_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/msgbox_en.lang.php
@@ -0,0 +1,56 @@
+0|Confirm|
+1|Attention !|
+2|Interdit !|
+3|Error !|
+4|Security !|
+5|<< Back|
+6|OK|
+7|Session error !|
+8|You can't directly access this page !|
+9|Please do not play with URLs !|
+10|This contact is not yours. You can't modify / delete it|
+11|Error in selected group(s) !|
+12|Please login !|
+13|Invalid email address|
+14|Name field is required|
+15|Please select at least one group !|
+16|You selected nothing !|
+17|Unable to open file.|
+18|You can't import the same field more than one time !|
+19|'Name' field is required. Please select it|
+20|Password and confirmation doesn't match !|
+21|Login, password and email are required !|
+22|This login is already used by someone else. Please choose another one !|
+23|'Subject' and 'Message' fields are both required|
+24|You can't send email to this group !|
+25|You have to select one group|
+26|Please confirm message delivery to group |
+27|Message sucessfully sent to group's member*|
+28|Unable to connect to SMTP server. <br><br>The email was not sent !|
+29| is not matching with <br>'number of contacts per page' field format !|
+30| is not matching with <br>Main font size field format !|
+31| is not matching with <br>Alt font Size field format !|
+32|Invalid Color format. Please follow '#XXXXXX' format, <br>where X is a figure or a letter of set {A,B,C,D,E;F}<br><br>Exemple : #03A7F8|
+33|You're not an Administrator of this group!|
+34|---not used---|
+35|---not used---|
+36|---not used---|
+37|---not used---|
+38|---not used---|
+39|Error in color table|
+40|Do you really want to delete contact |
+41|Contact sucessfully deleted !|
+42|Invalid password !|
+43|Result : |
+44|Success !|
+45|Selected contacts have been deleted successfully !|
+46|No contact match your selection. <br><br>The database wasn't modified|
+47|OBJECT : |
+48|MESSAGE : |
+49|Cancel / Modify|
+50|From : |
+51|Delete|
+52|Cancel|
+53|Please choose a separator !|
+54|<< Modify|
+55|###### PLEASE DO NOT ERASE THIS LINE #####||
