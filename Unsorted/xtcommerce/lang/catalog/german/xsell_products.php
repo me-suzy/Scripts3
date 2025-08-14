0091735276c3c7201d2dd68abed5f0d5f1b80c51
me-suzy <me.suzana@gmail.com>
2025-08-14 04:15:31 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/xsell_products.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/xsell_products.php
@@ -0,0 +1,26 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: xsell_products.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommercebased on original files from OSCommerce CVS 2.2 2002/08/28 02:14:35 www.oscommerce.com 
+   (c) 2003	 nextcommerce (xsell_products.php,v 1.4 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   --------------------------------------------------------------
+   Third Party contribution:
+   Cross-Sell (X-Sell) Admin 1				Autor: Joshua Dechant (dreamscape)
+   
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/ 
+
+  define('TEXT_BUY', 'Buy 1 \'');
+  define('TEXT_NOW', '\' now');
+
+?>
