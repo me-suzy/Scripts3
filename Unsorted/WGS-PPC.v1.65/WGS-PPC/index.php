2526376736310832104e951c266ee1bfeb6d9106
me-suzy <me.suzana@gmail.com>
2025-08-14 03:34:49 +0300

    Add Unsorted/WGS-PPC.v1.65/WGS-PPC/index.php

---
+++ Unsorted/WGS-PPC.v1.65/WGS-PPC/index.php
@@ -0,0 +1,242 @@
+<html><head><title>Ettica.com PPC Search Engine</title>
+<link rel="stylesheet" href="asstyles.css" type="text/css">
+</head>
+<body bgcolor="#FFFFFF">
+<table width="700" class="tborder" cellspacing="1" cellpadding="6">
+  <tr> 
+    <td align="right" bgcolor="#6699CC" id="top"><br>
+      <img src="as_images/as_logo_ppc.gif" width="360" height="70"><br>
+      <br>
+    </td>
+  </tr>
+  <tr> 
+    <td align="center" bgcolor="#f2f2f2"> 
+      <table width="100%" border="0" cellspacing="1" cellpadding="5">
+			<tr>
+				
+          <td align="center" id="m0">&nbsp; </td>
+			</tr>
+			<tr>
+				<td align="center" id="m0">
+					<b>Current version is 1.61 (Updated 02 oct 2002) <a href="#demo">Demo &gt;&gt; (1.61)</a></b>
+				</td>
+			</tr>
+			<tr>
+				
+          <td align="center" id="m0">&nbsp; </td>
+			</tr>
+		<tr>
+			<td>
+<b>New features in version 1.61:</b>
+<ul>
+	<li>external results from Dmoz.org</li>
+	<li>external results from MSN.com</li>
+	<li>installation program</li>
+</ul>
+<b>New features in version 1.57:</b>
+<ul>
+		<li>PayPal available by default</li>
+		<li>Members can define amount of transfer to their account!</li>
+		<li>Money can automatically put on members balanse using PayPal IPN !!!</li>
+		<li>Ready to support 2CheckOut, Authorize.net</li>
+		<li>Admin can charge members accounts manually. Members just transfer money to account of system and admin manualy accept or decline incoming transfers </li>
+		<li>After each charging members and admin get notification message from Ettica-PPC</li>
+		<li>Members can request to discharge their accounts. Admin make this discharging</li>
+</ul>
+
+<b>New features in version 1.50:</b>
+<ul>
+	<li>Affiliate system
+	<ul>
+		<li>Affiliates searches and clicks are counted</li>
+		<li>Members are affiliates by default.</li>
+		<li>Members gets money from affiliate searches and clicks directly to their system accounts.</li>
+		<li>Members can view affiliate statistics of searches and clicks of any period.</li>
+		<li>Members can download statistics report</li>
+		<li>Members can clear statistics log ( after getting money statistics log clears automaticaly )</li>
+		<li>Members can get html code directly from control panel</li>
+		<li>Admin can view statistics of all affiliates</li>
+		<li>Admin can set cost of affiliate search and click</li>
+	</ul>
+	</li>
+        <li>Restore DB - you can restore DB from saved backup file</li>
+	<li>Member can upload image for each listing. If image is not specified default logo will be used</li>
+        <li>Lost password lookup feature for members</li>
+</ul>
+<br><b>Features of previous version</b><br>
+<ul>
+<li>
+	Ettica PPC Search engine is PHP and MySQL based software. So it is fast and easy to use and install.
+</li>
+  <li>Members bid money to be listed in your catalog, and they only pay every 
+    time someone clicks through to their site from your search.</li>
+  <li>Only 24-hour unique clicks are counted (to exclude fake clicking).</li>
+  <li>Fully customizable and easy configurable templates for every page, including 
+    the search listings, members and admin control panels.</li>
+  <li>Visitors clicked any link will be redirected to the appropriate members 
+    site.</li>
+  <li>All clicks are counted, so member will pay for every counted click</li>
+  <li>Sites are only listed under keywords they have bid on, and if they have 
+    enough money in their accounts.</li>
+  <li><strong>Members area includes:</strong> 
+    <ul>
+      <li>Members registration form</li>
+      <li>Members login page</li>
+      <li>Main page with account creation date, account balance value, average 
+        money spent per day, and estimated time until they run out of money.</li>
+      <li>Manage Listings, where members can add/edit/delete search listings and 
+        change their bids. Include special type of bids - &quot;no matches&quot; 
+        listings - when visitor enters search string like &quot;kjhqkwekqjwey 
+        qweuqyiweuqyeijhqkdj&quot; members sites with that special bids will be 
+        shown. </li>
+      <li>Info and password changing page.</li>
+      <li>Special search to quickly check bids placed on keywords by others.</li>
+      <li>Advanced stats: 
+        <ul>
+          <li>stats for any period - member just select start and end date</li>
+          <li>stats includes search term, clicks count, total money spent on term 
+            and percent value that shows what term is more popular</li>
+          <li>stats for &quot;no matches&quot; bids</li>
+        </ul>
+      </li>
+      <li>Link to instructions on how to make payments and add funds to their 
+        accounts.</li>
+      <li>Page where all members bids are listed to make easy to update and check 
+        bids </li>
+      <li>Manage banners, where members can add/modify/delete their banners. When 
+        member adds a banner he(she) must specify show bid, click bid and search 
+        terms which will be linked with banner. Member can activate and deactivate 
+        banner displays also.</li>
+      <li>Banners stats</li>
+      <li>Special mass-submission form for people to submit multiple submissions 
+        at once.</li>
+    </ul>
+  </li>
+  <li><b>Admin area includes: </b>
+    <ul>
+      <li>Main page with common info such as number of members, collected money 
+        etc.</li>
+      <li>Advanced listings statistics: 
+        <ul>
+          <li>stats of all searches that visitors were made: keywords, searches 
+            number, matches number, clicks number, money collected</li>
+          <li>monthly stats - you can view stats for specified month for each 
+            day</li>
+          <li>daily searches - you can view detailed stats for specified day</li>
+          <li>top searches - stats of the most popular searches</li>
+          <li>top bids - stats of the most expencive bids</li>
+        </ul>
+      </li>
+      <li>Banners statistics - you can view stats for selected member</li>
+      <li>Categories - you can create/edit/delete your catalog categories</li>
+      <li>Keywords filter - you can add/delete keywords that should be filtered</li>
+      <li>Members: 
+        <ul>
+          <li>list of registered members with number of terms, members balance, 
+            number of clicks</li>
+          <li>you can view/edit members details</li>
+          <li>you can view/delete members term bids</li>
+          <li>you can block/unblock/delete members accounts</li>
+          <li>you can send a e-mail to all members or to selected members</li>
+          <li>you can change members account values manualy</li>
+        </ul>
+      </li>
+      <li>Waiting bids - here you can submit or deny all incoming bids ( if option 
+        of autosubmition is off )</li>
+      <li>System settings: 
+        <ul>
+          <li>Sign up bonus - you can set any account bonus for new members</li>
+          <li>you can set minimal allowed bid on search terms</li>
+          <li>you can set minimal allowed bid on &quot;no matches&quot; searches</li>
+          <li>accept bids automaticaly option</li>
+          <li>allow special search option</li>
+          <li>you can set minimal allowed members balance</li>
+          <li>you can set your currency symbol</li>
+          <li>you can set number of &quot;top&quot; values like &quot;top searches&quot;</li>
+          <li>you can set number of records per page in control panels</li>
+          <li>you can save changes and restore previous settings one step back</li>
+        </ul>
+      </li>
+      <li>Banners - you can add/modify/delete/activate/deactivate your own banners</li>
+      <li>Backup DB - you can backup system database in SQL format</li>
+      <li>Generate stats report - you can get a text file with common stats</li>
+      <li>Clear Search Log - you can clear search log database to increase speed 
+        of stats engine</li>
+      <li>Payment 
+        <ul>
+          <li>view all incoming money transfers from members</li>
+          <li>process payment</li>
+        </ul>
+      </li>
+    </ul>
+  </li>
+  <li>E-mail notifications sent to members for members registration, account updates, 
+    bids submission, account blocking ( account can be blocked manualy by admin 
+    or automaticaly if account balance value is less then minimal allowed).</li>
+  <li>Warning e-mail can be sent out before a customer's account runs out of funds 
+    (any amount of days in advance that you want).</li>
+  <li>Search keywords are logged.</li>
+  <li>Special mass-submission form for people to submit multiple submissions at 
+    once.</li>
+  <li>Lost password lookup tool.</li>
+  <li>Users cannot signup more than once.</li>
+  <li>Create low-level admin accounts for your employees to manage submissions 
+    only!</li>
+  <li>We can set up any payment gateway that you want</li>
+</ul>
+
+
+
+
+			</td>
+		</tr>
+	</table>
+<a name="demo"></a>
+      <table width="100%" border="0" cellspacing="1" cellpadding="5">
+        <tr> 
+          <td align="center" id="m1"><b>2. Members Area </b></td>
+	</tr>
+        <tr> 
+          <td><a href="http://www.ettica.com/ppc2/members/index.php">http://www.ettica.com/ppc2/members/index.php</a><br>
+            <br>Login: <b>test</b><br>
+            Password: <b>test</b> </td>
+        </tr>
+        <tr> 
+          <td align="left">New member registration: <a href="http://www.ettica.com/ppc2/members/registration.php">http://www.ettica.com/ppc2/members/registration.php</a> 
+          </td>
+        </tr>
+        <tr> 
+          <td align="center" id="m2"><b>3. Admin Area</b></td>
+        </tr>
+        <tr> 
+          <td><a href="http://www.ettica.com/ppc2/admin/">http://www.ettica.com/ppc2/admin/<br>
+            </a><br>
+            Login: <b>admin</b><br>
+            Password:<b> nefertiti</b></td>
+        </tr>
+        <tr> 
+          <td align="center" id="m3"><b>4. Visitors Area</b></td>
+        </tr>
+        <tr> 
+          <td> <a href="http://www.ettica.com/ppc2/main/">http://www.ettica.com/ppc2/main/</a></td>
+        </tr>
+        <tr> 
+          <td><a href="#top">to the top</a></td>
+        </tr>
+        <tr> 
+          <td>&nbsp;</td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+<table width="700" border="0" cellspacing="0" cellpadding="3">
+  <tr bgcolor="#4A4AD4"> 
+    <td align="left" class="tdmenu" bgcolor="#6699CC"> 
+    </td>
+    <td align="right" class="tdmenu" bgcolor="#6699CC"><font size="1">&copy; <a href="http://www.Ettica.com" class="menu">Ettica.com</a> 
+      2001-2002</font></td>
+  </tr>
+</table>
+<br><br>
+</body></html>
