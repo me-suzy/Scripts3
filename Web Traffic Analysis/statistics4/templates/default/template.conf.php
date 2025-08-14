2a93bdb74762450d4f14468c7c72c55b1613625e
me-suzy <me.suzana@gmail.com>
2025-08-14 05:03:21 +0300

    Add Web Traffic Analysis/statistics4/templates/default/template.conf.php

---
+++ Web Traffic Analysis/statistics4/templates/default/template.conf.php
@@ -0,0 +1,25 @@
+<?php
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// + template.conf.php - templatespecific configuration-file
+// +
+// + creation:		03.05.2005 - Daniel Sokoll
+// + Last Change:	03.05.2005 - Daniel Sokoll
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// --- colors ---------------------------------------------------------------------------------
+$color["DEF"][1] = "#f0f0f0";		// color default one
+$color["DEF"][2] = "#e0e0e0";	// color default two
+$color["BOT"][1] = "#efefff";		// color crawler one
+$color["BOT"][2] = "#dadaff";		// color crawler two
+$color["UKN"][1] = "#ffeeee";		// color unknown one
+$color["UKN"][2] = "#ffe0e0";		// color unknown two
+$color["MAX"][1] = "#eeffee";	// color maximum one
+$color["MAX"][2] = "#e0ffe0";	// color maximum two
+
+// --- image-files ----------------------------------------------------------------------------
+$pic["h_bar_1"] = $tpl_dir.$tpl_to_use."/"."h_red.png";	// horizontal-bar-image
+$pic["h_bar_2"] = $tpl_dir.$tpl_to_use."/"."h_blue.png";	// horizontal-bar-image
+$pic["v_bar_1"] = $tpl_dir.$tpl_to_use."/"."v_blue.png";	// vertical-bar-image
+$pic["v_bar_2"] = $tpl_dir.$tpl_to_use."/"."v_red.png";	// vertical-bar-image
+$pic["v_t_back"] = $tpl_dir.$tpl_to_use."/"."v_t_back.png";	// vertical-percent-background
+$pic["h_t_back"] = $tpl_dir.$tpl_to_use."/"."h_t_back.png";	// horizontal-percent-background
+?>
