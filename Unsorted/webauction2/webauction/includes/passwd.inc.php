576aab8815d723831eba620870e03f428802ea00
me-suzy <me.suzana@gmail.com>
2025-08-14 03:24:08 +0300

    Add Unsorted/webauction2/webauction/includes/passwd.inc.php

---
+++ Unsorted/webauction2/webauction/includes/passwd.inc.php
@@ -0,0 +1,28 @@
+<?
+  
+/*
+   Copyright (c), 1999, 2000 - phpauction.org
+   Copyright (c), 2001, 2002 - webauction.de.vu
+
+   Lizenz siehe lizenz.txt & license
+
+   Die neueste Version kostenfrei zum Download unter:
+   http://webauction.de.vu
+
+*/
+
+$DbHost     = ""; // The host where the MySQL server resides
+$DbDatabase = ""; // The database you are going to use
+$DbUser     = ""; // Username
+$DbPassword = ""; // Password
+
+//--Control Panel password
+//This is the password you will have to use to access the admin program
+ 
+$AdminPasswd = "";
+
+//Tabell Präfix //
+
+$dbfix = "a";
+
+?>
