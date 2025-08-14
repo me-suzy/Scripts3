1a541419e99ab5a4ba71129e24cfa916ab813690
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:39 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/cleaning_fr.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/cleaning_fr.lang.php
@@ -0,0 +1,18 @@
+0|Suppression par lots de vos contacts|
+1|Vous pouvez supprimer des lots de contacts pour des besoins de maintenance.<br>Tous les contacts que vous pouvez détruire ici ont été créés / importés par vos soins.|
+2|La procédure étant irréversible, il est conseillé de réaliser un export de vos contacts |
+3|avant |
+4|de lancer la suppression par lot.|
+5|Sélectionnez les ensembles de contacts que vous désirez supprimer, <br>entrez votre mot de passe et cliquez sur OK.|
+6|Attention !! |
+7|Aucune confirmation ne sera demandée.|
+8|Supprimer :|
+9|Tous mes contacts publics |
+10|Tous mes contacts privés |
+11| de la catégorie |
+12|Tous|
+13|Tous mes contacts visibles par les groupes |
+14|Votre mot de passe (obligatoire) |
+15|<< Retour|
+16|Supprimer les contacts !|
+17|###### PLEASE DO NOT ERASE THIS LINE #####||
