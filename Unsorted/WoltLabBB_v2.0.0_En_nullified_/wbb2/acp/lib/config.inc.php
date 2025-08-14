15a65a4cd003de45f014737c74c51f5d5a315094
me-suzy <me.suzana@gmail.com>
2025-08-14 03:44:33 +0300

    Add Unsorted/WoltLabBB_v2.0.0_En_nullified_/wbb2/acp/lib/config.inc.php

---
+++ Unsorted/WoltLabBB_v2.0.0_En_nullified_/wbb2/acp/lib/config.inc.php
@@ -0,0 +1,13 @@
+<?php
+// Hostname or IP of your MySQL Server
+$sqlhost = "localhost";
+// Username and Password to Login to your Database
+$sqluser = "root";
+$sqlpassword = "";
+// Name of your Database
+$sqldb = "wbb2";
+// Number of this Forum
+$n = "1";
+// Email Address of the Administrator
+$adminmail = "xyz@xyz.com";
+?>
