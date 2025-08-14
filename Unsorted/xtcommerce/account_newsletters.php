70f5ff69da4a39c3d0902c57c4d09a14b0c21572
me-suzy <me.suzana@gmail.com>
2025-08-14 03:56:05 +0300

    Add Unsorted/xtcommerce/account_newsletters.php

---
+++ Unsorted/xtcommerce/account_newsletters.php
@@ -0,0 +1,168 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: account_newsletters.php,v 1.1 2003/09/06 21:38:27 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(account_newsletters.php,v 1.2 2003/05/22); www.oscommerce.com 
+   (c) 2003	 nextcommerce (account_newsletters.php,v 1.13 2003/08/17); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  include( 'includes/application_top.php');
+
+  // include needed functions
+  require_once(DIR_FS_INC . 'xtc_draw_hidden_field.inc.php');
+  require_once(DIR_FS_INC . 'xtc_draw_checkbox_field.inc.php');
+  require_once(DIR_FS_INC . 'xtc_draw_selection_field.inc.php');
+  require_once(DIR_FS_INC . 'xtc_image_button.inc.php');
+
+  if (!isset($_SESSION['customer_id'])) {
+    $_SESSION['navigation']->set_snapshot();
+    xtc_redirect(xtc_href_link(FILENAME_LOGIN, '', 'SSL'));
+  }
+
+// needs to be included earlier to set the success message in the messageStack
+  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . FILENAME_ACCOUNT_NEWSLETTERS);
+
+  $newsletter_query = xtc_db_query("select customers_newsletter from " . TABLE_CUSTOMERS . " where customers_id = '" . (int)$_SESSION['customer_id'] . "'");
+  $newsletter = xtc_db_fetch_array($newsletter_query);
+
+  if (isset($_POST['action']) && ($_POST['action'] == 'process')) {
+    if (isset($_POST['newsletter_general']) && is_numeric($_POST['newsletter_general'])) {
+      $newsletter_general = xtc_db_prepare_input($_POST['newsletter_general']);
+    } else {
+      $newsletter_general = '0';
+    }
+
+    if ($newsletter_general != $newsletter['customers_newsletter']) {
+      $newsletter_general = (($newsletter['customers_newsletter'] == '1') ? '0' : '1');
+
+      xtc_db_query("update " . TABLE_CUSTOMERS . " set customers_newsletter = '" . (int)$newsletter_general . "' where customers_id = '" . (int)$_SESSION['customer_id'] . "'");
+    }
+
+    $messageStack->add_session('account', SUCCESS_NEWSLETTER_UPDATED, 'success');
+
+    xtc_redirect(xtc_href_link(FILENAME_ACCOUNT, '', 'SSL'));
+  }
+
+  $breadcrumb->add(NAVBAR_TITLE_1, xtc_href_link(FILENAME_ACCOUNT, '', 'SSL'));
+  $breadcrumb->add(NAVBAR_TITLE_2, xtc_href_link(FILENAME_ACCOUNT_NEWSLETTERS, '', 'SSL'));
+?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html <?php echo HTML_PARAMS; ?>>
+<head>
+<?php include(DIR_WS_MODULES.FILENAME_METATAGS); ?>
+<base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<script language="javascript"><!--
+function rowOverEffect(object) {
+  if (object.className == 'moduleRow') object.className = 'moduleRowOver';
+}
+
+function rowOutEffect(object) {
+  if (object.className == 'moduleRowOver') object.className = 'moduleRow';
+}
+
+function checkBox(object) {
+  document.account_newsletter.elements[object].checked = !document.account_newsletter.elements[object].checked;
+}
+//--></script>
+</head>
+<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0">
+<!-- header //-->
+<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+<!-- header_eof //-->
+
+<!-- body //-->
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+  <tr>
+    <td class="navLeft" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- left_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+<!-- left_navigation_eof //-->
+    </table></td>
+<!-- body_text //-->
+    <td class="tableBody" width="100%" valign="top"><?php echo xtc_draw_form('account_newsletter', xtc_href_link(FILENAME_ACCOUNT_NEWSLETTERS, '', 'SSL')) . xtc_draw_hidden_field('action', 'process'); ?><table border="0" width="100%" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
+            <td class="pageHeading" align="right"><?php echo xtc_image(DIR_WS_IMAGES . 'table_background_account.gif', HEADING_TITLE, HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td class="main"><b><?php echo MY_NEWSLETTERS_TITLE; ?></b></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="1" cellpadding="2" class="infoBox">
+          <tr class="infoBoxContents">
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+                  <tr class="moduleRow" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)" onclick="checkBox('newsletter_general')">
+                    <td class="main"><?php echo xtc_draw_checkbox_field('newsletter_general', '1', (($newsletter['customers_newsletter'] == '1') ? true : false), 'onclick="checkBox(\'newsletter_general\')"'); ?></td>
+                    <td class="main"><b><?php echo MY_NEWSLETTERS_GENERAL_NEWSLETTER; ?></b></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td><table border="0" cellspacing="0" cellpadding="2">
+                      <tr>
+                        <td width="10"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                        <td class="main"><?php echo MY_NEWSLETTERS_GENERAL_NEWSLETTER_DESCRIPTION; ?></td>
+                        <td width="10"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                      </tr>
+                    </table></td>
+                  </tr>
+                </table></td>
+                <td><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="1" cellpadding="2" class="infoBox">
+          <tr class="infoBoxContents">
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td width="10"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td><?php echo '<a href="' . xtc_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">' . xtc_image_button('button_back.gif', IMAGE_BUTTON_BACK) . '</a>'; ?></td>
+                <td align="right"><?php echo xtc_image_submit('button_continue.gif', IMAGE_BUTTON_CONTINUE); ?></td>
+                <td width="10"><?php echo xtc_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></form></td>
+<!-- body_text_eof //-->    <td class="navRight" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- right_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_right.php'); ?>
+<!-- right_navigation_eof //-->
+    </table></td>
+  </tr>
+</table>
+<!-- body_eof //-->
+
+<!-- footer //-->
+<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+<!-- footer_eof //-->
+<br>
+</body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
