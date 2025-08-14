bad626d6a3289155f2df3e34edf758b354c81eef
me-suzy <me.suzana@gmail.com>
2025-08-14 04:19:46 +0300

    Add Unsorted/X_Cart_3.0.1_Gold/xcart/include/check_useraccount.php

---
+++ Unsorted/X_Cart_3.0.1_Gold/xcart/include/check_useraccount.php
@@ -0,0 +1,73 @@
+<?
+/*****************************************************************************\
++-----------------------------------------------------------------------------+
+| X-Cart                                                                      |
+| Copyright (c) 2001-2002 RRF.ru development. All rights reserved.            |
++-----------------------------------------------------------------------------+
+| The RRF.RU DEVELOPMENT forbids, under any circumstances, the unauthorized   |
+| reproduction of software or use of illegally obtained software. Making      |
+| illegal copies of software is prohibited. Individuals who violate copyright |
+| law and software licensing agreements may be subject to criminal or civil   |
+| action by the owner of the copyright.                                       |
+|                                                                             |
+| 1. It is illegal to copy a software, and install that single program for    |
+| simultaneous use on multiple machines.                                      |
+|                                                                             |
+| 2. Unauthorized copies of software may not be used in any way. This applies |
+| even though you yourself may not have made the illegal copy.                |
+|                                                                             |
+| 3. Purchase of the appropriate number of copies of a software is necessary  |
+| for maintaining legal status.                                               |
+|                                                                             |
+| DISCLAIMER                                                                  |
+|                                                                             |
+| THIS SOFTWARE IS PROVIDED BY THE RRF.RU DEVELOPMENT TEAM ``AS IS'' AND ANY  |
+| EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED |
+| WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE      |
+| DISCLAIMED.  IN NO EVENT SHALL THE RRF.RU DEVELOPMENT TEAM OR ITS           |
+| CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,       |
+| EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,         |
+| PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; |
+| OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,    |
+| WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR     |
+| OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF      |
+| ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                  |
+|                                                                             |
+| The Initial Developer of the Original Code is RRF.ru development.           |
+| Portions created by RRF.ru development are Copyright (C) 2001-2002          |
+| RRF.ru development. All Rights Reserved.                                    |
++-----------------------------------------------------------------------------+
+\*****************************************************************************/
+
+#
+# $Id: check_useraccount.php,v 1.10 2002/05/31 06:44:12 zorg Exp $
+#
+# This script authenticates user (session variables "login" and "login_type"
+#
+
+session_register("login");
+session_register("login_type");
+
+session_register("logged");
+
+if ($login_type!=$current_area && !empty($login)) {
+    $logged=$login;
+    $login="";
+}
+elseif ($login_type==$current_area && !empty($logged)) {
+        $login=$logged;
+        $logged="";
+}
+
+if($login) {
+	$user_account=func_query_first("select login, membership from customers where login='$login'");
+	if(empty($user_account)) {
+		$login="";
+		$login_type="";
+	}
+}
+
+$smarty->assign("login",$login);
+$smarty->assign("usertype",$current_area);
+
+?>
