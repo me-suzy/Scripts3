97ccaa0e01f6ee16e8fdccf62f43c51676e55bf1
me-suzy <me.suzana@gmail.com>
2025-08-14 04:06:01 +0300

    Add Unsorted/xtcommerce/includes/boxes/languages.php

---
+++ Unsorted/xtcommerce/includes/boxes/languages.php
@@ -0,0 +1,47 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: languages.php,v 1.1 2003/09/06 22:13:53 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(languages.php,v 1.14 2003/02/12); www.oscommerce.com
+   (c) 2003	 nextcommerce (languages.php,v 1.8 2003/08/17); www.nextcommerce.org 
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  // include needed functions
+  require_once(DIR_FS_INC . 'xtc_get_all_get_params.inc.php');
+?>
+<!-- languages //-->
+          <tr>
+            <td><?php
+  $info_box_contents = array();
+  $info_box_contents[] = array('text' => BOX_HEADING_LANGUAGES);
+
+  new infoBoxHeading($info_box_contents, false, false);
+
+  if (!isset($lng) && !is_object($lng)) {
+    include(DIR_WS_CLASSES . 'language.php');
+    $lng = new language;
+  }
+
+  $languages_string = '';
+  reset($lng->catalog_languages);
+  while (list($key, $value) = each($lng->catalog_languages)) {
+    $languages_string .= ' <a href="' . xtc_href_link(basename($PHP_SELF), xtc_get_all_get_params(array('language', 'currency')) . 'language=' . $key, $request_type) . '">' . xtc_image('lang/catalog/' .  $value['directory'] . '/images/' . $value['image'], $value['name']) . '</a> ';
+  }
+
+  $info_box_contents = array();
+  $info_box_contents[] = array('align' => 'center',
+                               'text' => $languages_string);
+
+  new infoBox($info_box_contents);
+?></td>
+          </tr>
+<!-- languages_eof //-->
