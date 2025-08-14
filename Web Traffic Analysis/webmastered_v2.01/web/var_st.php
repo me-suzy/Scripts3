473f5b883c1490cb47b28f3933b79ceb244b0e02
me-suzy <me.suzana@gmail.com>
2025-08-14 05:04:39 +0300

    Add Web Traffic Analysis/webmastered_v2.01/web/var_st.php

---
+++ Web Traffic Analysis/webmastered_v2.01/web/var_st.php
@@ -0,0 +1,11 @@
+<?PHP
+$zone = "+15";
+$zone2 = ($zone - 24);
+$zone3 = ($zone - 48);
+$zone4 = ($zone - 72);
+$zone5 = ($zone - 96);
+$zone6 = ($zone - 120);
+$zone7 = ($zone - 144);
+$zone8 = ($zone - 168);
+
+?>
