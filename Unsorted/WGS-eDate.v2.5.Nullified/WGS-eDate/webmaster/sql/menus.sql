856610a95a4481d04438be423f67eac8769a6a26
me-suzy <me.suzana@gmail.com>
2025-08-14 03:33:10 +0300

    Add Unsorted/WGS-eDate.v2.5.Nullified/WGS-eDate/webmaster/sql/menus.sql

---
+++ Unsorted/WGS-eDate.v2.5.Nullified/WGS-eDate/webmaster/sql/menus.sql
@@ -0,0 +1,7 @@
+DELETE FROM menus WHERE 1;
+INSERT INTO menus VALUES ('','Members','','10');
+INSERT INTO menus VALUES ('','Search members','searchmembers.php','50');
+INSERT INTO menus VALUES ('','Marketing','','10');
+INSERT INTO menus VALUES ('','Process payments','process.php','50');
+INSERT INTO menus VALUES ('','System Configuration','','10');
+INSERT INTO menus VALUES ('','Settings','sqledit.php?table=sysvars&cond=WHERE 1 ORDER BY vtype DESC','30');
