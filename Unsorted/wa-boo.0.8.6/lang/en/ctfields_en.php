10489e2131f597d54b5136aa70cd00680eaf3e40
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:16 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/ctfields_en.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/ctfields_en.php
@@ -0,0 +1,18 @@
+cfirstname|50|Firstname|
+cname|50|Name|
+caddr1|100|Address|
+caddr2|100|Address (end)|
+czip|10|Zip code|
+ccity|50|City|
+ccountry|50|Country|
+cemail|50|email address|
+cinstantmsg|50|Instant messenger|
+cwww|100|Web site|
+cphone|50|Phone|
+ccell|50|Mobile phone|
+cphonepro|50|Pro. phone|
+cfax|50|Fax|Fax |
+cfirm|50|Company|
+cposition|50|Position|
+cmisc|255|Miscellaneous|
+############
