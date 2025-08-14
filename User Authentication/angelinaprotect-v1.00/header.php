74368f19957b1c8b7c8564a3fb32489932f9ac3c
me-suzy <me.suzana@gmail.com>
2025-08-14 04:33:13 +0300

    Add User Authentication/angelinaprotect-v1.00/header.php

---
+++ User Authentication/angelinaprotect-v1.00/header.php
@@ -0,0 +1,23 @@
+<html> 
+<head> 
+<title>User Section</title> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
+<style type="text/css">
+<!--
+.style1 {
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-weight: bold;
+}
+-->
+</style>
+</head> 
+
+<body> 
+<div align="center" class="style1">
+<p><B><i><font size="+3">ANGELINAPROTECT USER SECTION</font></I></B></p> 
+ 
+<p><a href="logout.php">Logout</a>
+<b>|</b>
+<a href="main.php">Page 01</a>
+<b>|</b>
+<a href="main02.php">Page 02</a> </center>
