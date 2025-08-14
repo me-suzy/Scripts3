ef9d1c131aa3236e6afa6912ee3d04d8e283c106
me-suzy <me.suzana@gmail.com>
2025-08-14 04:46:25 +0300

    Add Web Hosting Tools/phpcoin_v122_full_2005-09-26/coin_lang/lang_english/lang_licenses.php

---
+++ Web Hosting Tools/phpcoin_v122_full_2005-09-26/coin_lang/lang_english/lang_licenses.php
@@ -0,0 +1,25 @@
+<?php
+/**************************************************************
+ * File: 	Language- Licenses Module
+ * Author:	Stephen M. Kitching (http://phpcoin.com)
+ * Date:	2004-05-01 (V1.0)
+ * License:	DO NOT Remove this text block. See /coin_docs/license.txt
+ *			Copyright © 2004 Stephen M. Kitching
+ * Notes:	- Global language ($_LANG) vars.
+ *			- Language: 		English (USA)
+ *			- Translation By:
+ *				Stephen Kitching (http://www.phpcoin.com)
+**************************************************************/
+
+/**************************************************************
+ * Configuration Loaded Flag
+**************************************************************/
+	global $F_LANG_LICENSES;
+	$F_LANG_LICENSES	= 1;
+
+/**************************************************************
+ * Language Variables
+**************************************************************/
+# Language Variables: Common
+
+?>
