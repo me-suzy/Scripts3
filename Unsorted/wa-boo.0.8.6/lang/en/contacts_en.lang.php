2f62352f7d74187cab6d88c2f9d0f4f9272e804f
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:14 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/contacts_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/contacts_en.lang.php
@@ -0,0 +1,55 @@
+0|Contacts management - |
+1|Account / Groups|
+2|Preferences|
+3|New contact|
+4|Import|
+5|Export|
+6|wash|
+7|Log-Off|
+8|Search contacts|
+9|private|
+10|  public|
+11|Search|
+12|Help|
+13|Begin with |
+14|contents|
+15|in name field|
+16|in all fields|
+17|All|
+18|List of |
+19|Update|
+20|Delete|
+21| contacts |
+22|contacts |
+23| to |
+24| on |
+25|Fiche détaillée |
+26|créé par |
+27|Fiche contact |
+28|reset |
+29|Save |
+30|Firstname |
+31|Name |
+32|Address |
+33|Address (end) |
+34|Zip Code |
+35|City |
+36|Country |
+37|E-mail |
+38|Instant messenger |
+39|Web site |
+40|Phone |
+41|Mobile phone |
+42|Professionnal phone |
+43|Fax |
+44|Company |
+45|Position |
+46|Miscellaneous |
+47|Category |
+48|Owner |
+49|You can't select a group if 'Group' type is not selected !|
+50|Private|
+51|Public|
+52|Group|
+53|Type|
+52|###### PLEASE DO NOT ERASE THIS LINE #####||
