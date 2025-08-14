86f0cf995ccb3d30c957098dce5792944d71876d
me-suzy <me.suzana@gmail.com>
2025-08-14 04:57:17 +0300

    Add Web Traffic Analysis/count_visitors_class/config.php

---
+++ Web Traffic Analysis/count_visitors_class/config.php
@@ -0,0 +1,9 @@
+<?php 
+define("DB_SERVER", "localhost");
+define("DB_NAME", "finalwebsites_com_1");
+define ("DB_USER", "admin");
+define ("DB_PASSWORD", "admin");
+
+define ("DB_TABLE", "visits");
+define ("IMG", "./1px.png"); // change this constant to use dif. image/path
+?>
