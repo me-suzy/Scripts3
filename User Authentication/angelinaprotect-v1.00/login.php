f6ecd8d8f0c0c5e45d5bd6a8dd947bdf9a350548
me-suzy <me.suzana@gmail.com>
2025-08-14 04:33:14 +0300

    Add User Authentication/angelinaprotect-v1.00/login.php

---
+++ User Authentication/angelinaprotect-v1.00/login.php
@@ -0,0 +1,61 @@
+<?
+session_start(); 
+
+$errorMessage = ''; 
+if (isset($_POST['User']) && isset($_POST['Password'])) { 
+
+	require("library/config.php");
+     
+    $userId   = $_POST['User']; 
+    $password = $_POST['Password']; 
+     
+  require("library/opendb.php");
+	dbconnect();
+$mysql_link = mysql_connect($dbhost, $dbuname, $dbpass);
+    $sql = "SELECT user_id 
+            FROM member_user 
+            WHERE user_id = '$userId' AND user_password = PASSWORD('$password')"; 
+     
+    $result = mysql_query($sql) or die('Query failed. ' . mysql_error()); 
+     
+    if (mysql_num_rows($result) == 1) { 
+        $_SESSION['user_is_logged_in'] = true; 
+        header('Location: main.php'); 
+        exit; 
+    } else { 
+        $errorMessage = 'Sorry, wrong user and-or password'; 
+    } } 
+?> 
+<html> 
+<head> 
+<title>User Login</title> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
+</head> 
+<body> 
+<font size=+3><b><i><center>User Login</center></i></b></font>
+<?
+if ($errorMessage != '') { 
+?> 
+<p align="center"><strong><font color="#990000"><?php echo $errorMessage; ?></font></strong></p> 
+<?php 
+} 
+?> 
+<form action="" method="post" name="frmLogin" id="frmLogin"> 
+<table width="400" bgcolor=red align="center" cellpadding="2" cellspacing="2"> 
+  <tr> 
+   <th width="150" bgcolor=white>User</th> 
+   <th bgcolor=white><input name="User" type="text" id="User"></th> 
+  </tr> 
+  <tr> 
+   <th bgcolor=white width="150">Password</th> 
+   <th bgcolor=white><input name="Password" type="password" id="Password"></th> 
+  </tr> 
+  <tr> 
+   <th bgcolor=white width="150">&nbsp;</th> 
+   <th bgcolor=white><input name="btnLogin" type="submit" id="btnLogin" value="Login"></th> 
+  </tr> 
+</table> 
+</form> 
+<? include('footer.php'); ?>
+</body> 
+</html> 
