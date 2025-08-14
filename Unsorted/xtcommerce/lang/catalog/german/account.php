1906f8bfdd7651deb5535b20b58a93b91456513e
me-suzy <me.suzana@gmail.com>
2025-08-14 04:14:25 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/account.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/account.php
@@ -0,0 +1,32 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: account.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce                                    
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(account.php,v 1.12 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (account.php,v 1.4 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE', 'Ihr Konto');
+define('HEADING_TITLE', 'Ihre pers&ouml;nlichen Daten');
+define('OVERVIEW_TITLE', '&Uuml;bersicht');
+define('OVERVIEW_SHOW_ALL_ORDERS', '(alle Bestellungen anzeigen)');
+define('OVERVIEW_PREVIOUS_ORDERS', 'Bisherige Bestellungen');
+define('MY_ACCOUNT_TITLE', 'Ihr Konto');
+define('MY_ACCOUNT_INFORMATION', 'Meine Kontoinformationen anzeigen oder &auml;ndern');
+define('MY_ACCOUNT_ADDRESS_BOOK', 'Adressbucheintr&auml;ge anzeigen oder &auml;ndern');
+define('MY_ACCOUNT_PASSWORD', 'Mein Passwort &auml;ndern');
+define('MY_ORDERS_TITLE', 'Meine Bestellungen');
+define('MY_ORDERS_VIEW', 'Meine bisherigen Bestellungen anzeigen');
+define('EMAIL_NOTIFICATIONS_TITLE', 'E-Mail Benachrichtigungen');
+define('EMAIL_NOTIFICATIONS_NEWSLETTERS', 'Newsletter abonnieren oder abbestellen.');
+define('EMAIL_NOTIFICATIONS_PRODUCTS', 'Meine Produktbenachrichtigungen anzeigen oder &auml;ndern.');
+?>
