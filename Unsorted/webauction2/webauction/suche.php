fbcbacf22fc4e2395c8b550efa910708a6853954
me-suzy <me.suzana@gmail.com>
2025-08-14 03:21:25 +0300

    Add Unsorted/webauction2/webauction/suche.php

---
+++ Unsorted/webauction2/webauction/suche.php
@@ -0,0 +1,19 @@
+<SCRIPT Language=PHP>
+/*
+   Copyright (c), 1999, 2000 - phpauction.org
+   Copyright (c), 2001, 2002 - webauction.de.vu
+
+   Lizenz siehe lizenz.txt & license
+
+   Die neueste Version kostenfrei zum Download unter:
+   http://webauction.de.vu
+
+*/
+        // Include messages file
+   require('includes/messages.inc.php');
+   // Connect to sql server & inizialize configuration variables
+   require('includes/config.inc.php');
+include("./header.php");
+include("./templates/adsearch_php3.html");
+include("./footer.php");
+</SCRIPT>
