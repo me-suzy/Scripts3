0cc9c0e24df683a7852c56b2b6b4d6259f80d0d3
me-suzy <me.suzana@gmail.com>
2025-08-14 04:12:55 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/cookie_usage.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/cookie_usage.php
@@ -0,0 +1,25 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: cookie_usage.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(cookie_usage.php,v 1.1 2003/03/10); www.oscommerce.com 
+   (c) 2003	 nextcommerce (cookie_usage.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE', 'Cookie Usage');
+define('HEADING_TITLE', 'Cookie Usage');
+
+define('TEXT_INFORMATION', 'We have detected that your browser does not support cookies, or has set cookies to be disabled.<br><br>To continue shopping online, we encourage you to enable cookies on your browser.<br><br>For <b>Internet Explorer</b> browsers, please follow these instructions:<br><ol><li>Click on the Tools menubar, and select Internet Options</li><li>Select the Security tab, and reset the security level to Medium</li></ol>We have taken this measurement of security for your benefit, and apologize upfront if any inconveniences are caused.<br><br>Please contact the store owner if you have any questions relating to this requirement, or to continue purchasing products offline.');
+
+define('BOX_INFORMATION_HEADING', 'Cookie Privacy and Security');
+define('BOX_INFORMATION', 'Cookies must be enabled to purchase online on this store to embrace privacy and security related issues regarding your visit to this site.<br><br>By enabling cookie support on your browser, the communication between you and this site is strengthened to be certain it is you who are making transactions on your own behalf, and to prevent leakage of your privacy information.');
+?>
