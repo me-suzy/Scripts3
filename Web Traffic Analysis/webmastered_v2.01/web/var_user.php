1c100abed63ce573310095b5a79738df2e1321ef
me-suzy <me.suzana@gmail.com>
2025-08-14 05:04:40 +0300

    Add Web Traffic Analysis/webmastered_v2.01/web/var_user.php

---
+++ Web Traffic Analysis/webmastered_v2.01/web/var_user.php
@@ -0,0 +1,34 @@
+<?PHP
+$company = "Your Company";
+$pw = "test";
+$emailto = "";
+$adminemail = "";
+$signature = "Kind Regards
+Fred Bloggs
+http://www.yoursite.com
+
+";
+$back = "";
+$logo = "mock_logo.gif";
+$background = "white";
+$font = "verdana";
+$size = "10";
+$color = "black";
+$link = "blue";
+$visit = "blue";
+$hover = "red";
+$active = "red";
+$decoration = "none";
+$fieldcolor = "ivory";
+$fieldtextcolor = "navy";
+$align = "CENTER";
+$url = "www.yoursite.com";
+$zone = "+15";
+$zone2 = ($zone - 24);
+$zone3 = ($zone - 48);
+$zone4 = ($zone - 72);
+$zone5 = ($zone - 96);
+$zone6 = ($zone - 120);
+$zone7 = ($zone - 144);
+$zone8 = ($zone - 168);
+?>
