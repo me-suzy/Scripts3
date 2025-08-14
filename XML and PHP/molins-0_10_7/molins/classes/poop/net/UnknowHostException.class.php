e4c0ebc05e6f03f8f3030b53842cbc11b987216e
me-suzy <me.suzana@gmail.com>
2025-08-14 05:09:38 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/net/UnknowHostException.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/net/UnknowHostException.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: UnknowHostException.class.php,v 1.1 2005/10/16 20:01:04 slizardo Exp $
+ * @package poop.net
+ */
+
+/**
+ * Thrown to indicate that the IP address of a host could not be determined. 
+ */
+class UnknowHostException extends IOException {
+}
+
+?>
