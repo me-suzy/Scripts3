cef17a8cc9fe597c464a4e676889974748f586d4
me-suzy <me.suzana@gmail.com>
2025-08-14 04:55:52 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/diagram/hdnr_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/diagram/hdnr_tpl.php
@@ -0,0 +1,16 @@
+<?php die("Access restricted");?>
+  <tr bgcolor="#CCCCCC">
+          <td  valign=center align=center class=f8 height=25 width="30"><font size=3 color="#000000">
+            <SPAN class="%%class%%">%%num%%</SPAN>
+          </font></td>
+          <td valign=center><font size=3 color="#000000">
+            <SPAN class="%%class%%">%%name%%</SPAN>
+          </font></td>
+          <td width="210">
+            <table border="0" cellspacing="0" cellpadding="0">
+              <tr> 
+                <td height=25 width="100%"></td>
+              </tr> 
+            </table>
+          </td>
+  </tr>
