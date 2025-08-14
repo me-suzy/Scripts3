fe2262715b9a4ff8879bf9fb435cab02d73d957d
me-suzy <me.suzana@gmail.com>
2025-08-14 04:15:21 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/product_reviews_write.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/product_reviews_write.php
@@ -0,0 +1,28 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: product_reviews_write.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(product_reviews_write.php,v 1.9 2003/02/16); www.oscommerce.com 
+   (c) 2003	 nextcommerce (product_reviews_write.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE', 'Meinungen');
+define('HEADING_TITLE', 'Was meinen Sie?');
+define('SUB_TITLE_PRODUCT', 'Produkt:');
+define('SUB_TITLE_FROM', 'Autor:');
+define('SUB_TITLE_REVIEW', 'Ihre Meinung:');
+define('SUB_TITLE_RATING', 'Bewertung:');
+define('TEXT_NO_HTML', '<small><font color="#ff0000"><b>ACHTUNG:</b></font></small>&nbsp;HTML wird nicht unterst&uuml;tzt!');
+define('TEXT_BAD', '<small><font color="#ff0000"><b>SCHLECHT</b></font></small>');
+define('TEXT_GOOD', '<small><font color="#ff0000"><b>SEHR GUT</b></font></small>');
+define('ERROR_INVALID_PRODUCT', 'Das von Ihnen gew&auml;hlte Produkt wurde nicht gefunden!');
+?>
