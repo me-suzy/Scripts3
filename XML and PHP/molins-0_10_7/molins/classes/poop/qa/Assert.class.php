06667bc8430d56edce6cf38277c30add879bdedb
me-suzy <me.suzana@gmail.com>
2025-08-14 05:10:03 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/qa/Assert.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/qa/Assert.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: Assert.class.php,v 1.5 2005/10/12 21:00:59 slizardo Exp $
+ * @package poop.qa
+ */
+	
+final class Assert {
+
+	/**
+	 * @param mixed $a
+	 * @param mixed $b
+	 * @return boolean
+	 */
+	public static function notEquals($a, $b) {
+	}
+}
+	
+?>
