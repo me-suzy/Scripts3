6b66f2200b431162751b77c9135dfb1f15585fbf
me-suzy <me.suzana@gmail.com>
2025-08-14 05:09:34 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/net/ServerSocket.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/net/ServerSocket.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: ServerSocket.class.php,v 1.4 2005/10/12 21:00:59 slizardo Exp $
+ * @package poop.net
+ */
+
+class ServerSocket {
+
+	private $portNumber;
+	
+	/**
+	 * @param int $portNumber
+	 */
+	public function __construct($portNumber) {
+		$this->portNumber = $portNumber;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function accept() {
+	}
+}
+	
+?>
