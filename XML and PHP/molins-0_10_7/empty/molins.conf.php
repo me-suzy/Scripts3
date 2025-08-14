36d405e16607453dc11eb59ad9adf338802469ba
me-suzy <me.suzana@gmail.com>
2025-08-14 05:05:29 +0300

    Add XML and PHP/molins-0_10_7/empty/molins.conf.php

---
+++ XML and PHP/molins-0_10_7/empty/molins.conf.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Basic Molins configuration file.
+ * More options are available in each xml located within the WEB-INF directory.
+ */
+
+/** Web application context path. */
+define('CONF_CONTEXT_PATH', '/empty');
+
+/** Directories for Molins and this application. */
+define('CONF_DIR_MOLINS', 'c:'.DIRECTORY_SEPARATOR.'inam'.DIRECTORY_SEPARATOR.'htdocs'.DIRECTORY_SEPARATOR.'molins');
+
+/** Default locale for use with Molins. */
+define('CONF_DEFAULT_LOCALE', 'es_US');
+
+?>
