2040fc55893ed1fae54d771e722713ccc4a965d8
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:45 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/export1_fr.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/export1_fr.lang.php
@@ -0,0 +1,15 @@
+0|Etape 1 : Sélection des contacts|
+1|L'export vous permet de fabriquer un fichier que vous pourrez ensuite réimporter dans un autre logiciel, un PDA, etc. |
+2|Le format du fichier que vous allez exporter doit être de type 'avec séparateur', <br>ce qui signifie que les champs du fichier sont délimités par un caractère particulier. |
+3|Choisissez maintenant les informations que vous allez exporter et le séparateur de champs :|
+4|Tous mes contacts privés |
+5| Les contacts correspondant à ma dernière requète|
+6|Il s'agit de tous les contacts que j'ai créés et importés |
+7| Cette option peut potentiellement exporter tous les contacts <br>que je suis autorisés à voir (publics, groupes et privés)|
+8|Séparateur de champ |
+9| (pour le caractère tabulation, entrez la lettre ' t ')|
+10|Ajouter le nom des champs sur la première ligne |
+11| (utile pour réimporter dans certains logiciels)|
+12|<< Quitter l'export|
+13|Exporter !|
+14|###### PLEASE DO NOT ERASE THIS LINE #####||
