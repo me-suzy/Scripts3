7bcca66a4bfc9b648b481fff94b10844e1b9eae8
me-suzy <me.suzana@gmail.com>
2025-08-14 04:14:51 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/checkout_shipping.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/checkout_shipping.php
@@ -0,0 +1,36 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: checkout_shipping.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(checkout_shipping.php,v 1.5 2003/02/16); www.oscommerce.com 
+   (c) 2003	 nextcommerce (checkout_shipping.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Kasse');
+define('NAVBAR_TITLE_2', 'Versandinformationen');
+
+define('HEADING_TITLE', 'Versandinformationen');
+
+define('TABLE_HEADING_SHIPPING_ADDRESS', 'Versandadresse');
+define('TEXT_CHOOSE_SHIPPING_DESTINATION', 'Bitte w&auml;hlen Sie aus Ihrem Adressbuch die gew&uuml;nschte Versandadresse f&uuml;r Ihre Bestellung aus.');
+define('TITLE_SHIPPING_ADDRESS', 'Versandadresse:');
+
+define('TABLE_HEADING_SHIPPING_METHOD', 'Versandart');
+define('TEXT_CHOOSE_SHIPPING_METHOD', 'Bitte w&auml;hlen Sie die gew&uuml;nschte Versandart f&uuml;r Ihre Bestellung aus.');
+define('TITLE_PLEASE_SELECT', 'Bitte w&auml;hlen Sie');
+define('TEXT_ENTER_SHIPPING_INFORMATION', 'Zur Zeit bieten wir Ihnen nur eine Versandart an.');
+
+define('TABLE_HEADING_COMMENTS', 'F&uuml;gen Sie hier Ihre Anmerkungen zu dieser Bestellung ein');
+
+define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Fortsetzung des Bestellvorganges');
+define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'zur Auswahl der gew&uuml;nschten Zahlungsweise.');
+?>
