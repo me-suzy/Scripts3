f20cd6cab00263fc875b5e02d99c058a8eb7b8a7
me-suzy <me.suzana@gmail.com>
2025-08-14 03:23:46 +0300

    Add Unsorted/webauction2/webauction/includes/helps.inc.php

---
+++ Unsorted/webauction2/webauction/includes/helps.inc.php
@@ -0,0 +1,22 @@
+<?
+ /*
+   Copyright (c), 1999, 2000 - phpauction.org
+   Copyright (c), 2001, 2002 - webauction.de.vu
+
+   Lizenz siehe lizenz.txt & license
+
+   Die neueste Version kostenfrei zum Download unter:
+   http://webauction.de.vu
+
+*/
+	$HLP_001 = "";
+	$HLP_002 = "";
+	$HLP_003 = "";
+	$HLP_004 = "";
+	$HLP_005 = "";
+	$HLP_006 = "";
+	$HLP_007 = "";
+	$HLP_008 = "";
+?>	
+	
+	
