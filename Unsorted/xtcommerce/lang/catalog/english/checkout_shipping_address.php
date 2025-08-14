ca7ef7b7700016e69cfd4413345f12232fe619a3
me-suzy <me.suzana@gmail.com>
2025-08-14 04:12:50 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/checkout_shipping_address.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/checkout_shipping_address.php
@@ -0,0 +1,36 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: checkout_shipping_address.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(checkout_shipping_address.php,v 1.2 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (checkout_shipping_address.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Checkout');
+define('NAVBAR_TITLE_2', 'Change Shipping Address');
+
+define('HEADING_TITLE', 'Delivery Information');
+
+define('TABLE_HEADING_SHIPPING_ADDRESS', 'Shipping Address');
+define('TEXT_SELECTED_SHIPPING_DESTINATION', 'This is the currently selected shipping address where the items in this order will be delivered to.');
+define('TITLE_SHIPPING_ADDRESS', 'Shipping Address:');
+
+define('TABLE_HEADING_ADDRESS_BOOK_ENTRIES', 'Address Book Entries');
+define('TEXT_SELECT_OTHER_SHIPPING_DESTINATION', 'Please select the preferred shipping address if the items in this order are to be delivered elsewhere.');
+define('TITLE_PLEASE_SELECT', 'Please Select');
+
+define('TABLE_HEADING_NEW_SHIPPING_ADDRESS', 'New Shipping Address');
+define('TEXT_CREATE_NEW_SHIPPING_ADDRESS', 'Please use the following form to create a new shipping address to use for this order.');
+
+define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Continue Checkout Procedure');
+define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'to select the preferred shipping method.');
+?>
