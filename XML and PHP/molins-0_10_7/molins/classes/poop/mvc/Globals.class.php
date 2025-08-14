0e98e539aaef10cde5afd8015f3095be05828f98
me-suzy <me.suzana@gmail.com>
2025-08-14 05:09:09 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/mvc/Globals.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/mvc/Globals.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: Globals.class.php,v 1.4 2005/10/12 21:00:42 slizardo Exp $
+ * @package poop.mvc
+ */
+
+class Globals {
+
+	const MESSAGE_KEY = 'message.key';
+	const LOCALE_KEY = 'locale.key';
+}
+	
+?>
