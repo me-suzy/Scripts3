d423b4a080ec4b40705dd6d353be930f2f88bd20
me-suzy <me.suzana@gmail.com>
2025-08-14 04:15:14 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/product_info.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/product_info.php
@@ -0,0 +1,28 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: product_info.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(product_info.php,v 1.18 2003/02/16); www.oscommerce.com 
+   (c) 2003	 nextcommerce (product_info.php,v 1.8 2003/08/24); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+define('TEXT_PRODUCT_PRINT', 'Datenblatt Drucken:');
+define('TEXT_PRODUCT_NOT_FOUND', 'Produkt wurde nicht gefunden!');
+define('TEXT_CURRENT_REVIEWS', 'Aktuelle Meinungen:');
+define('TEXT_MORE_INFORMATION', 'F&uuml;r weitere Informationen, besuchen Sie bitte die <a href="%s" target="_blank"><u>Homepage</u></a> zu diesem Produkt.');
+define('TEXT_DATE_ADDED', 'Dieses Produkt haben wir am %s in unseren Katalog aufgenommen.');
+define('TEXT_DATE_AVAILABLE', '<font color="#ff0000">Dieses Produkt wird voraussichtlich ab dem %s wieder vorr&auml;tig sein.</font>');
+define('TEXT_ALSO_PURCHASED_PRODUCTS', 'Kunden, die dieses Produkt gekauft haben, haben auch folgende Produkte gekauft:');
+define('TEXT_PRODUCT_OPTIONS', 'Optionen:');
+define('TEXT_CLICK_TO_ENLARGE', 'F&uuml;r eine gr&ouml;ssere Darstellung<br>klicken Sie auf das Bild.');
+define('TEXT_XSELL_PRODUCTS','Wir empfehlen Ihnen:');
+define('TITLE_MEDIA_CONTENT','Media Daten:')
+?>
