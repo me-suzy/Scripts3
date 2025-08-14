a41e6345cb301da79768cdf143cf122964516904
me-suzy <me.suzana@gmail.com>
2025-08-14 03:41:06 +0300

    Add Unsorted/WGS-Rotate.v1.0.Nullified/WGS-Rotate/faq.php

---
+++ Unsorted/WGS-Rotate.v1.0.Nullified/WGS-Rotate/faq.php
@@ -0,0 +1,31 @@
+<?
+
+include_once("config.inc.php");
+print $design_top;
+
+?>
+
+<h3>FAQ</h3>
+<strong><em><u>How many URLs am I allowed to enter into the rotator?</u></em></strong><br />
+     You are allowed as many URLs as you want.  We run an UNLIMITED rotator.<br /><br />
+
+
+<strong><em><u>What kind of sites are not allowed to be used in the rotator?</u></em></strong><br />
+     You are not allowed to include any adult content, illegal content, or other rotators.  We do not let you use other rotators because we allow for an unlimited amount of sites, so you should not need to use them.  They also make your page load slower.<br /><br />
+
+
+<strong><em><u>Can I remove the advertisement from my rotator?</u></em></strong><br />
+     Yes you can remove the advertisement for the low cost of $1.00 per month.<br /><br />
+
+
+<strong><em><u>Why is my rotator displaying your site?</u></em></strong><br />
+Check your URL box in the members area and see if there are any blank lines. The 
+blank lines will rotate the URL Rotator site.<br />
+<br />
+
+
+<?
+
+print $design_bottom;
+
+?>
