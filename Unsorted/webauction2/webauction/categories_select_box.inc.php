5f2a78babadf11026407fcf7447ad68e102786ac
me-suzy <me.suzana@gmail.com>
2025-08-14 03:19:40 +0300

    Add Unsorted/webauction2/webauction/categories_select_box.inc.php

---
+++ Unsorted/webauction2/webauction/categories_select_box.inc.php
@@ -0,0 +1,8 @@
+<SELECT NAME="id">
+	<OPTION VALUE="0"></OPTION>
+	<OPTION VALUE="492">Auto & Mtorrad</OPTION>
+	<OPTION VALUE="329">Briefmarken & Münzen</OPTION>
+	<OPTION VALUE="75">Computer & Computerspiele</OPTION>
+	<OPTION VALUE=""></OPTION>
+	<OPTION VALUE="0">Alle Kategorien</OPTION>
+</SELECT>
