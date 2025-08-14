66025a4d136732bf09449bb663289fe49d715565
me-suzy <me.suzana@gmail.com>
2025-08-14 05:07:25 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/lang/ClassCastException.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/lang/ClassCastException.class.php
@@ -0,0 +1,18 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: ClassCastException.class.php,v 1.4 2005/10/13 13:42:25 slizardo Exp $
+ * @package poop.lang
+ */
+
+class ClassCastException extends Exception { }
+
+?>
