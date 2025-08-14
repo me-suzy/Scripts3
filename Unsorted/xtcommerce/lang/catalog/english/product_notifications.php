2b7f267be7b2f408be15d1205ff04fb0f0906627
me-suzy <me.suzana@gmail.com>
2025-08-14 04:13:13 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/product_notifications.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/product_notifications.php
@@ -0,0 +1,36 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: product_notifications.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(product_notifications.php,v 1.1 2002/03/10); www.oscommerce.com 
+   (c) 2003	 nextcommerce (product_notifications.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'My Account');
+define('NAVBAR_TITLE_2', 'Product Notifications');
+define('HEADING_TITLE', 'My Product Notifications');
+
+define('TEXT_PRODUCT_NOTIFICATIONS_INTRODUCTION', 'The product notification list allows you to stay up to date on the products we have available, which gives you the chance to take advantage of our products on special.');
+
+define('HEADING_GLOBAL_PRODUCT_NOTIFICATIONS', 'Global Product Notifications');
+define('HEADING_PRODUCT_NOTIFICATIONS', 'Product Notifications');
+
+define('TEXT_GLOBAL_PRODUCT_NOTIFICATIONS_DISABLED', 'Your current global product notification status is <b>disabled</b>.');
+define('TEXT_GLOBAL_PRODUCT_NOTIFICATIONS_ENABLED', 'Your current global product notification status is <b>enabled</b>.');
+
+define('TEXT_GLOBAL_PRODUCT_NOTIFICATIONS_DESCRIPTION_DISABLED', 'If you wish to be notified on all our product updates, please enable the global product notification feature by selecting the option below.');
+define('TEXT_GLOBAL_PRODUCT_NOTIFICATIONS_DESCRIPTION_ENABLED', 'If you no longer wish to be notified on all our product updates, please disable the global product notification feature by deselecting the option below.');
+
+define('TEXT_PRODUCT_NOTIFICATIONS_LIST', 'Below is a list of products you selected to be notified on. If you wish to remove one or more products from the product notification list, please select the desired products and click on \'Remove Notifications\'.');
+
+define('TEXT_ENABLE_GLOBAL_NOTIFICATIONS', 'Please send me notifications on all products');
+?>
