a5939aee3468e25876c403f03be262521d3ddd43
me-suzy <me.suzana@gmail.com>
2025-08-14 05:07:14 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/io/IOException.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/io/IOException.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: IOException.class.php,v 1.5 2005/10/13 13:42:25 slizardo Exp $
+ * @package poop.io
+ */
+
+class IOException extends Exception {
+
+	/**
+	 * @param string $file
+	 */
+	public function __construct($file) {
+		parent::__construct(_('error leyendo/escribiendo fichero').' '.$file);
+	}
+}
+
+?>
