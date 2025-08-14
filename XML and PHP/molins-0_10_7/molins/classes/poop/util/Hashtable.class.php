814acf7e527bc372b0b93edc5e16c9b599b5e9ab
me-suzy <me.suzana@gmail.com>
2025-08-14 05:11:31 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/util/Hashtable.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/util/Hashtable.class.php
@@ -0,0 +1,75 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: Hashtable.class.php,v 1.3 2005/10/12 21:01:04 slizardo Exp $
+ * @package poop.util
+ */
+
+class Hashtable extends Dictionary {
+
+	/**
+	 * @return int
+	 */
+	public function size() {
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isEmpty() {
+	}
+	
+	/**
+	 * @param string $key
+	 * @return int
+	 */
+	public function get($key) {
+	}
+
+	/**
+	 * @param string $key
+	 * @param mixed $value
+	 */
+	public function put($key, $value) {
+	}
+
+	/**
+	 * @param string $key
+	 */
+	public function remove($key) {
+	}
+
+	/**
+	 * @return Set
+	 */
+	public function keySet() {
+	}
+
+	/**
+	 * @return array
+	 */
+	public function keys() {
+	}
+	
+	/**
+	 * @return array
+	 */
+	public function elements() {
+	}
+	
+	/**
+	 * @return array
+	 */
+	public function values() {
+	}	
+}
+ 
+?>
