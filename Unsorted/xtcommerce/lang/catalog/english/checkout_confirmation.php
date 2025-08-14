b722759791549e7cabcd3414b849e399e81b1b15
me-suzy <me.suzana@gmail.com>
2025-08-14 04:12:42 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/checkout_confirmation.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/checkout_confirmation.php
@@ -0,0 +1,38 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: checkout_confirmation.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(checkout_confirmation.php,v 1.24 2003/02/06); www.oscommerce.com 
+   (c) 2003	 nextcommerce (checkout_confirmation.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Checkout');
+define('NAVBAR_TITLE_2', 'Confirmation');
+
+define('HEADING_TITLE', 'Order Confirmation');
+
+define('HEADING_DELIVERY_ADDRESS', 'Delivery Address');
+define('HEADING_SHIPPING_METHOD', 'Shipping Method');
+define('HEADING_PRODUCTS', 'Products');
+define('HEADING_TAX', 'Tax');
+define('HEADING_TOTAL', 'Total');
+define('HEADING_BILLING_INFORMATION', 'Billing Information');
+define('HEADING_BILLING_ADDRESS', 'Billing Address');
+define('HEADING_PAYMENT_METHOD', 'Payment Method');
+define('HEADING_PAYMENT_INFORMATION', 'Payment Information');
+define('HEADING_ORDER_COMMENTS', 'Comments About Your Order');
+
+define('TEXT_EDIT', 'Edit');
+
+define('HEADING_CONDITIONS_INFORMATION', 'Right of revocation');
+define('HEADING_CONDITIONS_TEXT', 'Put here your information about the right of revocation.');
+?>
