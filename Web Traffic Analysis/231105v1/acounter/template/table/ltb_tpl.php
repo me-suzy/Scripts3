cedab3941478ed81da332d2f7daa327059c27cd7
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:17 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/table/ltb_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/table/ltb_tpl.php
@@ -0,0 +1,6 @@
+<?php die("Access restricted");?>
+  <tr bgcolor="#CCCCCC">
+    <td height="25" colspan="5" align="center"><font size=3 color="#000000">
+       <SPAN class="%%class%%">No records</SPAN>
+    </font></td>
+  </tr>
