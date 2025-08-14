e903b31754058cbaa947a02d77ba7885e96cc9cf
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:25 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/table/pts_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/table/pts_tpl.php
@@ -0,0 +1,12 @@
+<?php die("Access restricted");?>
+  <tr bgcolor="#CCCCCC">
+    <td height="25" align="center">
+        <input type="hidden" name="vdcol%%num%%" value="%%inttim%%">
+        <input class=b22 type="submit" name="vdcolumn%%num%%_x" value="%%val2%%">
+    <td align="center"><font size=3 color="#000000">
+       <SPAN class="%%class%%">%%val3%%</SPAN> 
+    </font></td>
+    <td align="center"><font size=3 color="#000000">
+       <SPAN class="%%class%%">%%val4%%</SPAN> 
+    </font></td>
+  </tr>
