747cac017708fb49a0adcabbed01745f7f75e6c2
me-suzy <me.suzana@gmail.com>
2025-08-14 03:09:04 +0300

    Add Unsorted/vCard v2.60 nullified/vcard26/upload/admin/phpinfo.php

---
+++ Unsorted/vCard v2.60 nullified/vcard26/upload/admin/phpinfo.php
@@ -0,0 +1,20 @@
+<?php
+//***************************************************************************//
+//                                                                           //
+//  Program Name    	: vCard PRO                                          //
+//  Program Version     : 2.6                                                //
+//  Program Author      : Joao Kikuchi,  Belchior Foundry                    //
+//  Home Page           : http://www.belchiorfoundry.com                     //
+//  Retail Price        : $80.00 United States Dollars                       //
+//  WebForum Price      : $00.00 Always 100% Free                            //
+//  Supplied by         : South [WTN]                                        //
+//  Nullified By        : CyKuH [WTN]                                        //
+//  Distribution        : via WebForum, ForumRU and associated file dumps    //
+//                                                                           //
+//                (C) Copyright 2001-2002 Belchior Foundry                   //
+//***************************************************************************//
+define('IN_VCARD', true);
+require('./lib.inc.php');
+phpinfo();
+
+?>
