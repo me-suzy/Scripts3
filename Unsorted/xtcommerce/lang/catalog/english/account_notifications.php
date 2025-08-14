657cbc8c94929a57179b98d135f6843fe529ed67
me-suzy <me.suzana@gmail.com>
2025-08-14 04:12:32 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/account_notifications.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/account_notifications.php
@@ -0,0 +1,34 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: account_notifications.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(account_notifications.php,v 1.1 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (account_notifications.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'My Account');
+define('NAVBAR_TITLE_2', 'Product Notifications');
+
+define('HEADING_TITLE', 'Product Notifications');
+
+define('MY_NOTIFICATIONS_TITLE', 'My Product Notifications');
+define('MY_NOTIFICATIONS_DESCRIPTION', 'The product notification list allows you to stay up to date on products you find of interest.<br><br>To be up to date on all product changes, select <b>Global Product Notifications</b>.');
+
+define('GLOBAL_NOTIFICATIONS_TITLE', 'Global Product Notifications');
+define('GLOBAL_NOTIFICATIONS_DESCRIPTION', 'Recieve notifications on all available products.');
+
+define('NOTIFICATIONS_TITLE', 'Product Notifications');
+define('NOTIFICATIONS_DESCRIPTION', 'To remove a product notification, clear the products checkbox and click on Continue.');
+define('NOTIFICATIONS_NON_EXISTING', 'There are currently no products marked to be notified on.<br><br>To add products to your product notification list, click on the notification link available on the detailed product information page.');
+
+define('SUCCESS_NOTIFICATIONS_UPDATED', 'Your product notifications have been successfully updated.');
+?>
