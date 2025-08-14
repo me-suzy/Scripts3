cac01a21dd1f9f9fa536539f3b4876d3a92446c3
me-suzy <me.suzana@gmail.com>
2025-08-14 05:04:35 +0300

    Add Web Traffic Analysis/webmastered_v2.01/web/var_gb.php

---
+++ Web Traffic Analysis/webmastered_v2.01/web/var_gb.php
@@ -0,0 +1,13 @@
+<?PHP
+$gtext_2 = "Thank you for taking the time to sign our guestbook!
+
+I'd like to take this opportunity to tell you about our new product...blah...blah
+
+Thank you
+Fred Bloggs";
+$ttext = "Thank you for taking the time to send your private message!
+
+I'd like to take this opportunity to tell you about our new product...blah...blah";
+$posts = "15";
+
+?>
