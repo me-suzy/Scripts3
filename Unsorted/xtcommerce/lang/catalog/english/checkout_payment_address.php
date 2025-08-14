b3da467dbac9ed7ba3199e9013d380e7211d6923
me-suzy <me.suzana@gmail.com>
2025-08-14 04:12:45 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/checkout_payment_address.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/checkout_payment_address.php
@@ -0,0 +1,36 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: checkout_payment_address.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(checkout_payment_address.php,v 1.2 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (checkout_payment_address.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Checkout');
+define('NAVBAR_TITLE_2', 'Change Billing Address');
+
+define('HEADING_TITLE', 'Payment Information');
+
+define('TABLE_HEADING_PAYMENT_ADDRESS', 'Billing Address');
+define('TEXT_SELECTED_PAYMENT_DESTINATION', 'This is the currently selected billing address where the invoice to this order will be delivered to.');
+define('TITLE_PAYMENT_ADDRESS', 'Billing Address:');
+
+define('TABLE_HEADING_ADDRESS_BOOK_ENTRIES', 'Address Book Entries');
+define('TEXT_SELECT_OTHER_PAYMENT_DESTINATION', 'Please select the preferred billing address if the invoice to this order is to be delivered elsewhere.');
+define('TITLE_PLEASE_SELECT', 'Please Select');
+
+define('TABLE_HEADING_NEW_PAYMENT_ADDRESS', 'New Billing Address');
+define('TEXT_CREATE_NEW_PAYMENT_ADDRESS', 'Please use the following form to create a new billing address to use for this order.');
+
+define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Continue Checkout Procedure');
+define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'to select the preferred payment method.');
+?>
