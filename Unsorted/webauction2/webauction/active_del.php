1715e7a91f80f174340c497fa249c1e1e69e3d4f
me-suzy <me.suzana@gmail.com>
2025-08-14 03:19:14 +0300

    Add Unsorted/webauction2/webauction/active_del.php

---
+++ Unsorted/webauction2/webauction/active_del.php
@@ -0,0 +1,113 @@
+<?php
+
+/*
+   Copyright (c), 1999, 2000 - phpauction.org
+   Copyright (c), 2001, 2002 - webauction.de.vu
+
+   Lizenz siehe lizenz.txt & license
+
+   Die neueste Version kostenfrei zum Download unter:
+   http://webauction.de.vu
+
+*/
+
+
+
+	require('includes/messages.inc.php');
+	require('includes/config.inc.php');
+
+
+
+
+
+   $query = "SELECT * FROM ".$dbfix."_auctions WHERE id='$id'";
+   $result = mysql_query($query);
+   if ( !$result )
+   {
+		print $ERR_001;
+        exit;
+   }
+
+   $user_id        = mysql_result ( $result, 0, "user" );
+   $title          = mysql_result ( $result, 0, "title" );
+   $description    = mysql_result ( $result, 0, "description" );
+   $pict_url       = mysql_result ( $result, 0, "pict_url" );
+   $category       = mysql_result ( $result, 0, "category" );
+   $reserve_price  = mysql_result ( $result, 0, "reserve_price" );
+   $auction_type   = mysql_result ( $result, 0, "auction_type" );
+   $location       = mysql_result ( $result, 0, "location" );
+   $location_zip   = mysql_result ( $result, 0, "location_zip" );
+   $shipping       = mysql_result ( $result, 0, "shipping" );
+   $payment        = mysql_result ( $result, 0, "payment" );
+   $photo_uploaded = mysql_result ( $result, 0, "photo_uploaded" );
+   $international  = mysql_result ( $result, 0, "international" );
+   $reserve_price  = mysql_result ( $result, 0, "reserve_price" );
+   $date_old = mysql_result ( $result, 0, "date");
+   $ends_old = mysql_result($result,0,"ends");
+
+   $query = "UPDATE ".$dbfix."_auctions SET date='$date_old', ends='$ends_old', closed='del' where id='$id'";
+
+
+     //-- Counter Update
+
+            $sql = "select auctions from ".$dbfix."_counters";
+			$result_counters = mysql_query($sql);
+			if($result_counters){
+		    $auction_counter = mysql_result($result_counters,0,"auctions") - 1;
+			$sql_update = "update ".$dbfix."_counters set auctions = $auction_counter";
+			$result = mysql_query($sql_update);
+			}
+
+// and increase category counters
+
+		$ct = $category ;
+
+		$row = mysql_fetch_array(mysql_query("SELECT * FROM ".$dbfix."_categories WHERE cat_id=$ct"));
+
+		$counter = $row[counter]-1;
+
+		$subcoun = $row[sub_counter]-1;
+
+		$parent_id = $row[parent_id];
+
+		mysql_query("UPDATE ".$dbfix."_categories SET counter=$counter, sub_counter=$subcoun WHERE cat_id=$ct");
+   
+
+// update recursive categories
+
+		while ( $parent_id!=0 )
+
+		{
+
+			// update this parent's subcounter
+
+				$rw = mysql_fetch_array(mysql_query("SELECT * FROM ".$dbfix."_categories WHERE cat_id=$parent_id"));
+
+				$subcoun = $rw[sub_counter]-1;
+
+				mysql_query("UPDATE ".$dbfix."_categories SET sub_counter=$subcoun WHERE cat_id=$parent_id");
+
+			// get next parent
+
+				$parent_id = intval($rw[parent_id]);
+
+		}
+
+
+		
+		if (!mysql_query($query))
+			print $ERR_001.mysql_error()."$query";
+		else
+		{
+			
+
+
+			
+			
+			header ("location:my_active_auctions_del.php?SESSION_ID=$SESSION_ID&user_id=$user_id&TPL_nick=$TPL_nick&TPL_password=$TPL_password&name=$name&page=$page");
+		}
+
+
+
+
+?>
