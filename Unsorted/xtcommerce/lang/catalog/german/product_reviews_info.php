326fb5639ed1914e8b9d89f6f9fadbaf2953d8d3
me-suzy <me.suzana@gmail.com>
2025-08-14 04:15:19 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/product_reviews_info.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/product_reviews_info.php
@@ -0,0 +1,27 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: product_reviews_info.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce 
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(product_reviews_info.php,v 1.8 2003/02/16); www.oscommerce.com 
+   (c) 2003	 nextcommerce (product_reviews_info.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE', 'Meinungen');
+define('HEADING_TITLE', 'Meinung zu \'%s\'');
+define('SUB_TITLE_PRODUCT', 'Produkt:');
+define('SUB_TITLE_FROM', 'Autor:');
+define('SUB_TITLE_DATE', 'Datum:');
+define('SUB_TITLE_REVIEW', 'Meinung:');
+define('SUB_TITLE_RATING', 'Bewertung:');
+define('TEXT_OF_5_STARS', '%s von 5 Sternen!');
+define('TEXT_CLICK_TO_ENLARGE', 'F&uuml;r eine gr&ouml;ssere Darstellung<br>klicken Sie auf das Bild.');
+?>
