7a616c7d837025f77529b6428952a1ca64dbfe4f
me-suzy <me.suzana@gmail.com>
2025-08-14 03:56:54 +0300

    Add Unsorted/xtcommerce/logoff.php

---
+++ Unsorted/xtcommerce/logoff.php
@@ -0,0 +1,95 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: logoff.php,v 1.1 2003/09/06 21:38:27 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(logoff.php,v 1.12 2003/02/13); www.oscommerce.com 
+   (c) 2003	 nextcommerce (logoff.php,v 1.16 2003/08/17); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  include( 'includes/application_top.php');
+
+  // include needed functions
+  require_once(DIR_FS_INC . 'xtc_image_button.inc.php');
+
+  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . FILENAME_LOGOFF);
+
+  $breadcrumb->add(NAVBAR_TITLE);
+
+  xtc_session_destroy();
+
+  unset($_SESSION['customer_id']);
+  unset($_SESSION['customer_default_address_id']);
+  unset($_SESSION['customer_first_name']);
+  unset($_SESSION['customer_country_id']);
+  unset($_SESSION['customer_zone_id']);
+  unset($_SESSION['comments']);
+  unset($_SESSION['user_info']);
+  unset($_SESSION['customers_status']);
+  unset($_SESSION['selected_box']);
+  unset($_SESSION['navigation']);
+  unset($_SESSION['shipping']);
+  unset($_SESSION['payment']);
+  $_SESSION['cart']->reset();
+  // write customers status guest in session again
+  require(DIR_WS_INCLUDES . 'write_customers_status.php');
+?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html <?php echo HTML_PARAMS; ?>>
+<head>
+<?php include(DIR_WS_MODULES.FILENAME_METATAGS); ?>
+<base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0">
+<!-- header //-->
+<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+<!-- header_eof //-->
+
+<!-- body //-->
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+  <tr>
+    <td class="navLeft" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- left_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+<!-- left_navigation_eof //-->
+    </table></td>
+<!-- body_text //-->
+    <td class="tableBody" width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td><?php echo xtc_image(DIR_WS_IMAGES . 'table_background_man_on_board.gif', HEADING_TITLE); ?></td>
+            <td valign="top" class="main"><div align="center" class="pageHeading"><?php echo HEADING_TITLE; ?></div><br><?php echo TEXT_MAIN; ?></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td align="right"><br><?php echo '<a href="' . xtc_href_link(FILENAME_DEFAULT) . '">' . xtc_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE) . '</a>'; ?></td>
+      </tr>
+    </table></td>
+<!-- body_text_eof //-->
+    <td class="navRight" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- right_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_right.php'); ?>
+<!-- right_navigation_eof //-->
+    </table></td>
+  </tr>
+</table>
+<!-- body_eof //-->
+
+<!-- footer //-->
+<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+<!-- footer_eof //-->
+<br>
+</body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
