630cf43648491b1c693de4afe8f626ecd7b09cbb
me-suzy <me.suzana@gmail.com>
2025-08-14 04:15:01 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/create_account_success.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/create_account_success.php
@@ -0,0 +1,22 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: create_account_success.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(create_account_success.php,v 1.9 2003/02/16); www.oscommerce.com 
+   (c) 2003	 nextcommerce (create_account_success.php,v 1.4 2003/08/1); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Konto erstellen');
+define('NAVBAR_TITLE_2', 'Erfolg');
+define('HEADING_TITLE', 'Ihr Konto wurde mit Erfolg er&ouml;ffnet!');
+define('TEXT_ACCOUNT_CREATED', 'Herzlichen Gl&uuml;ckwunsch! Ihr neues Konto wurde erfolgreich er&ouml;ffnet! Sie k&ouml;nnen jetzt &uuml;ber Ihr Kundenkonto unseren \'Online-Service\' effizienter nutzen. Wenn Sie Fragen zum diesem Online-Shop haben, wenden Sie sich bitte an den <a href="' . xtc_href_link(FILENAME_CONTACT_US) . '"><u>Vertrieb</u></a>.<br><br>Eine Best&auml;tigung &uuml;ber Ihr neues Konto wird Ihnen zugesandt. Falls Sie diese eMail nicht innerhalb einer Stunde erhalten, wenden Sie sich bitte an den <a href="' . xtc_href_link(FILENAME_CONTACT_US) . '"><u>Vertrieb</u></a>.');
+?>
