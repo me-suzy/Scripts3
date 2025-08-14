eb17f086df695709e57a16656a1849afcaac284c
me-suzy <me.suzana@gmail.com>
2025-08-14 04:58:17 +0300

    Add Web Traffic Analysis/lite264/lite/view.php

---
+++ Web Traffic Analysis/lite264/lite/view.php
@@ -0,0 +1,99 @@
+<?php
+
+/*------------------------------------------------------------------------*/
+// Product: ActualAnalyzer
+// Script: view.php
+// Source: http://www.actualscripts.com/
+// Copyright: (c) 2002-2004 ActualScripts, Company. All rights reserved.
+//
+// YOU DON'T NEED TO EDIT ANYTHING IN THIS SCRIPT.
+// SEE LICENSE AGREEMENT FOR MORE DETAILS
+/*------------------------------------------------------------------------*/
+
+//p3p header
+require './data/p3pheader.php';
+
+//root folder of Actualanalyzer
+$rf='./';
+
+require './common/error.php';
+require './common/global.php';
+require './common/config.php';
+require './common/confdb.php';
+require './common/auth.php';
+require './view/vdb.php';
+require './view/vstat.php';
+
+//HTML code accumulator
+$pagehtml='';
+
+//errors
+$err = & new error($rf);
+
+//config
+$conf = & new config($rf);
+if($err->flag) {
+  $err->reason('view.php||constructor of config class has failed');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//database initialisation
+db_init();
+if($err->flag) {
+  $err->reason('view.php||can\'t init database');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//config in database
+$configdb = & new confdb;
+if($err->flag) {
+  $err->reason('view.php||constructor of confdb class has failed');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//authentication
+$login = & new auth($rf,'view',_VIEWAREA);
+if($err->flag) {
+  $err->reason('view.php||constructor of auth class has failed');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//database
+$vdb = & new vdb;
+if($err->flag) {
+  $err->reason('view.php||constructor of vdb class has failed');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//view
+$vstat = & new vstat($rf);
+if($err->flag) {
+  $err->reason('view.php||constructor of vstat class has failed');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//database closing
+db_close();
+if($err->flag) {
+  $err->reason('admin.php||can\'t close connection with database');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//output HTML page
+out();
+
+?>
