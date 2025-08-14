bc5543fcaf0354041946d81f28d5538def508da9
me-suzy <me.suzana@gmail.com>
2025-08-14 04:33:19 +0300

    Add User Authentication/angelinaprotect-v1.00/main02.php

---
+++ User Authentication/angelinaprotect-v1.00/main02.php
@@ -0,0 +1,12 @@
+<?php 
+require("protect.php"); 
+include('header.php');
+?> 
+<p>This is the user page 02.<p> You are free to add as many pages as you want.<br> Only remember this:<br><i> the first line of the code MUST be</I> </p> 
+<font color=red>&lt;?php require('protect.php');?&gt;</font>
+<p>&nbsp;</p> 
+</body> 
+</html> 
+<?php 
+include('footer.php');
+?>
