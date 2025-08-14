94a01310c1dd9266839a2ecc0f36cda69e527cf9
me-suzy <me.suzana@gmail.com>
2025-08-14 04:13:23 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/specials.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/specials.php
@@ -0,0 +1,20 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: specials.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(specials.php,v 1.7 2002/11/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (specials.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE', 'Specials');
+define('HEADING_TITLE', 'Get Them While They\'re Hot!');
+?>
