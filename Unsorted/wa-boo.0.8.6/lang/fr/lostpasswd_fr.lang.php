33c8420db3ccb5b02f5c53e572114f6a5bccb56c
me-suzy <me.suzana@gmail.com>
2025-08-14 03:17:00 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/lostpasswd_fr.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/lostpasswd_fr.lang.php
@@ -0,0 +1,15 @@
+0|Mot de passe oublié ?|
+1|Vous pouvez recevoir votre mot de passe par email|
+2|Mon email |
+3|< Retour|
+4|Recevoir mon mot de passe|
+5|Vos login et mot de passe pour le service |
+6|Bonjour |
+7|Voici votre login et mot de passe pour |
+8|Login : |
+9|Mot de passe : |
+10|Votre mot de passe a été envoyé à l'adresse |
+11|Problème d'envoi de mail |
+12| ne correspond à aucun utilisateur référencé !|
+13| est une adresse email invalide :-(|
+14|###### PLEASE DO NOT ERASE THIS LINE #####||
