0efbe7890cf7e18907bc92cd8ce0a272445e1cbe
me-suzy <me.suzana@gmail.com>
2025-08-14 04:12:29 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/account_history_info.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/account_history_info.php
@@ -0,0 +1,46 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: account_history_info.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(account_history_info.php,v 1.16 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (account_history_info.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'My Account');
+define('NAVBAR_TITLE_2', 'History');
+define('NAVBAR_TITLE_3', 'Order #%s');
+
+define('HEADING_TITLE', 'Order Information');
+
+define('HEADING_ORDER_NUMBER', 'Order #%s');
+define('HEADING_ORDER_DATE', 'Order Date:');
+define('HEADING_ORDER_TOTAL', 'Order Total:');
+
+define('HEADING_DELIVERY_ADDRESS', 'Delivery Address');
+define('HEADING_SHIPPING_METHOD', 'Shipping Method');
+
+define('HEADING_PRODUCTS', 'Products');
+define('HEADING_TAX', 'Tax');
+define('HEADING_TOTAL', 'Total');
+
+define('HEADING_BILLING_INFORMATION', 'Billing Information');
+define('HEADING_BILLING_ADDRESS', 'Billing Address');
+define('HEADING_PAYMENT_METHOD', 'Payment Method');
+
+define('HEADING_ORDER_HISTORY', 'Order History');
+define('HEADING_COMMENT', 'Comment');
+define('TEXT_NO_COMMENTS_AVAILABLE', 'No comments available.');
+
+define('TABLE_HEADING_DOWNLOAD_DATE', 'Link expires: ');
+define('TABLE_HEADING_DOWNLOAD_COUNT', ' downloads remaining');
+define('HEADING_DOWNLOAD', 'Download links');
+?>
