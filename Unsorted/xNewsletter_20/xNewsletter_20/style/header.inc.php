fb0c74f6703cb4ae0c24f7552a4130015cae79f8
me-suzy <me.suzana@gmail.com>
2025-08-14 03:55:47 +0300

    Add Unsorted/xNewsletter_20/xNewsletter_20/style/header.inc.php

---
+++ Unsorted/xNewsletter_20/xNewsletter_20/style/header.inc.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Newsletter</title>
+	<style type='text/css'>
+	BODY, TD		{ font:10px verdana; }
+	INPUT, TEXTAREA, SELECT	{ font:10px verdana; border:1px solid black; }
+	
+	/* Links */
+	A:link        		{ text-decoration: none; color: #000099}
+	A:visited     		{ text-decoration: none; color: #000099}
+	A:active      		{ color: #000099}
+	A:hover       		{ text-decoration: underline; color: #000099}
+	
+	/* message-box */
+	#box			{ width:300;text-align:left;background:#eeeeee;border:1px solid #cecece;padding:5px }
+	</style>
+</head>
+<body>
+
+<table width='100%' height='100%' align='center'>
+ <tr>
+  <td valign='middle' align='center'>
