24dbcf10f8e9aa4be627fe9da400a1bc113c56ea
me-suzy <me.suzana@gmail.com>
2025-08-14 04:13:30 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/modules/order_total/ot_discount.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/modules/order_total/ot_discount.php
@@ -0,0 +1,26 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: ot_discount.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommercecoding standarts www.oscommerce.com 
+   (c) 2003	 nextcommerce (ot_discount.php,v 1.4 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_ORDER_TOTAL_DISCOUNT_TITLE', 'Discount');
+  define('MODULE_ORDER_TOTAL_DISCOUNT_DESCRIPTION', 'Discount on order');
+  
+  define('MODULE_ORDER_TOTAL_DISCOUNT_STATUS_TITLE', 'Display total-discount');
+  define('MODULE_ORDER_TOTAL_DISCOUNT_STATUS_DESC', 'Do you want to display the order total discount?');
+  
+  define('MODULE_ORDER_TOTAL_DISCOUNT_SORT_ORDER_TITLE', 'Sort Order');
+  define('MODULE_ORDER_TOTAL_DISCOUNT_SORT_ORDER_DESC','Sort order of display.');
+?>
