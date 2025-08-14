977893074a351890596b5be7ae907680ef2e9876
me-suzy <me.suzana@gmail.com>
2025-08-14 03:30:30 +0300

    Add Unsorted/webmail/languagefiles/Russian.php

---
+++ Unsorted/webmail/languagefiles/Russian.php
@@ -0,0 +1,115 @@
+<?php
+$l1 = "ðÒÏÉÚÏÛÌÁ ÏÛÉÂËÁ";
+$l2 = "&lt;&lt;&lt; ÷ÅÒÎÕÔØÓÑ";
+$l3 = "îÅ×ÏÚÍÏÖÎÏ ÓÏÅÄÅÎÉÔÓÑ Ó POP3 ÓÅÒ×ÅÒÏÍ. óÏÏÂÝÅÎÉÅ Ï ÏÛÉÂËÅ:";
+$l4 = "îÅ×ÏÚÍÏÖÎÏ ÚÁÊÔÉ ÎÁ POP ÓÅÒ×ÅÒ. õËÁÖÉÔÅ ÐÒÁ×ÉÌØÎÙÊ login/password.";
+$l5 = "OK";
+$l6 = "úÁÛÌÉ";
+$l7 = "POP ÓÅÒ×ÅÒ password:";
+$l8 = "POP ÓÅÒ×ÅÒ username:";
+$l9 = "POP ÓÅÒ×ÅÒ port:";
+$l10 = "éÍÑ POP ÓÅÒ×ÅÒÁ:";
+$l11 = "÷ ×ÁÛÅÍ ÑÝÉËÅ ÎÅÔ ÓÏÏÂÝÅÎÉÊ.";
+$l12 = "The program could not run TOP command.";
+$l13 = "[îÅÔ ÔÅÍÙ]";
+$l15 = "îÅ×ÏÚÍÏÖÎÏ ÐÏÌÕÞÉÔØ ÓÏÏÂÝÅÎÉÅ #$id.";
+$l16 = "ïÔÐÒÁ×ÉÔÅÌØ:";
+$l17 = "ðÏÌÕÞÁÔÅÌØ:";
+$l18 = "äÁÔÁ:";
+$l19 = "ïÒÇÁÎÉÚÁÃÉÑ:";
+$l20 = "ðÏÞÔÏ×ÙÊ ËÌÉÅÎÔ:";
+$l21 = "ïÔ×ÅÔÉÔØ to:";
+$l22 = "úÁÇÏÌÏ×ÏË";
+$l23 = "äÏÐÏÌÎÉÔÅÌØÎÁÑ ËÏÐÉÑ:";
+$l24 = "ïÔ×ÅÔ";
+$l25 = "ðÅÒÅÓÌÁÔØ";
+$l26 = "õÄÁÌÉÔØ";
+$l27 = "ïÔ×ÅÔÉÔØ ×ÓÅÍ";
+$l28 = "ðÅÞÁÔÁÔØ";
+$l29 = "ðÏËÁÚÁÔØ ÉÓÈÏÄÎÙÊ ËÏÄ";
+$l30 = "ñÚÙË:";
+$l31 = "÷Ù ÄÅÊÓÔ×ÉÔÅÌØÎÏ ÈÏÔÉÔÅ ÕÄÁÌÉÔØ ×ÙÂÒÁÎÎÏÅ ÓÏÏÂÝÅÎÉÅ?";
+$l32 = "óÏÏÂÝÅÎÉÅ #$id ÎÅ ÕÄÁÅÔÓÑ ÕÄÁÌÉÔØ.";
+$l33 = "îÏ×ÏÅ ÓÏÏÂÝÅÎÉÑ";
+$l34 = "ïÂÎÏ×ÉÔØ";
+$l35 = "[îÅÉÚ×ÅÓÔÎÏÅ ÉÍÑ ÆÁÊÌÁ]";
+$l36 = "ôÅÍÁ:";
+$l37 = "ïÔ:";
+$l38 = "ëÏÍÕ:";
+$l39 = "äÏÐÏÌÎÉÔÅÌØÎÁÑ ËÏÐÉÑ:";
+$l40 = "óÌÅÐÁÑ ÄÏÐÏÌÎÉÔÅÌØÎÁÑ ËÏÐÉÑ:";
+$l41 = "ðÒÉÓÏÅÄÅÎÅÎÎÙÊ ÆÁÊÌ:";
+$l42 = "ðÏÓÌÁÔØ ËÁË HTML";
+$l43 = "ðÏÓÌÁÔØ";
+$l44 = "÷Ù Õ×ÅÒÅÎÙ ÞÔÏ ÈÏÔÉÔÅ ÐÏÓÌÁÔØ ÓÏÏÂÝÅÎÉÅ ÓÅÊÞÁÓ?";
+# Do not translate [date] and [sender]. Think of them as variables. The
+# string will be transformed into e.g. "On 08-08-2001 12:42, John Doe
+# <doe@example.com> wrote":
+$l45 = "[date], [sender] ÐÉÓÁÌ:";
+$l46 = "ðÅÒÅÌÁÎÎÏÅ ÓÏÏÂÝÅÎÉÅ";
+$l47 = "×Ù";
+$l48 = "îÅ ××ÅÄÅÎ ÐÒÁ×ÉÌØÎÙÊ ÁÄÒÅÓ ÏÔÐÒÁ×ÉÔÅÌÑ.";
+$l49 = "îÅ ××ÅÄÅÎ ÐÒÁ×ÉÌØÎÙÊ ÁÄÒÅÓ ÐÏÌÕÞÁÔÅÌÑ.";
+# The character set corresponding to your language:
+$l50 = "koi8-r";
+$l51 = "ðÒÏÇÒÁÍÍÁ ÎÅ ÍÏÖÅÔ ÐÒÏÞÉÔÁÔØ ÐÒÉÓÏÅÄÅÎÅÎÎÙÊ ÆÁÊÌ. ÷ÏÚÍÏÖÎÏ ÎÅ È×ÁÔÁÅÔ ÐÒÁ×. ïÂÒÁÔÉÔÅÓØ Ë ÓÉÓÔÅÍÎÏÍÕ ÁÄÍÉÎÉÓÔÒÁÔÏÒÕ.";
+$l52 = "÷ÙÈÏÄ";
+$l53 = "ìÉÃÅÎÚÉÑ: <a href='license.txt' target='_blank'>GNU General Public License</a>";
+$l54 = "ï";
+$l55 = "ðÒÅÄÕÓÔÁÎÏ×ÌÅÎÎÙÊ POP ÓÅÒ×ÅÒ:";
+$l56 = "ðÒÅÄÕÓÔÁÎÏ×ÌÅÎÎÙÊ ÐÏÒÔ ÎÁ POP ÓÅÒ×ÅÒÅ:";
+$l57 = "ðÒÅÄÕÓÔÁÎÏ×ÌÅÎÎÏÅ ÉÍÑ ÎÁ POP ÓÅÒ×ÅÒÅ:";
+$l58 = "òÁÚÄÅÌÑÔØ ÆÒÅÊÍÙ";
+$l59 = "÷ÅÒÔÉËÁÌØÎÏ";
+$l60 = "çÏÒÉÚÏÎÔÁÌØÎÏ";
+$l61 = "îÁÓÔÒÏÊËÉ";
+$l62 = "óÏÈÒÁÎÉÔØ ÎÁÓÔÒÏÊËÉ";
+$l63 = "æÒÅÊÍ ÚÁÇÏÌÏ×ËÁ, ÐÒÏÃÅÎÔÙ:";
+$l64 = "æÒÅÊÍ ÓÏÏÂÝÅÎÉÑ, ÐÒÏÃÅÎÔÙ:";
+$l65 = "Footer (×ÓÅ ÓÔÒÁÎÉÃÙs):";
+$l66 = "Log-in page header:";
+$l67 = "Log-in page footer:";
+$l68 = "úÁÇÏÌÏ×ÏË ÐÏËÁÚÙ×ÁÅÔÓÑ × ÓÔÏËÅ ÚÁÇÏÌÏ×ËÁ:";
+$l69 = "ðÒÏÇÒÁÍÍÅ ÎÅ È×ÁÔÁÅÔ ÐÒÁ× ÄÌÑ ÚÁÐÉÓÉ ÆÁÊÌÁ settingssaved.php. îÅÏÂÈÏÄÉÍÏ ÕÓÔÁÎÏ×ÉÔØ ÐÒÁ×Á 666.";
+$l70 = "îÁÓÔÒÏÊËÉ ÓÏÈÒÁÎÅÎÙ.";
+$l71 = "ïÓÔÁ×ÉÔØ ÓÔÁÒÙÊ ÐÁÒÏÌØ";
+$l72 = "îÏ×ÙÊ ÐÁÒÏÌØ:";
+$l73 = "ðÁÒÏÌØ ÎÁ ÁÄÍÉÎÉÓÔÒÁÔÉ×ÎÕÀ ÓÔÒÁÎÉÃÕ:";
+$l74 = "éÍÑ ÁÄÍÉÎÉÓÔÒÁÔÏÒÁ:";
+$l75 = "äÏÓÔÕÐ ÚÁÐÒÅÝÅÎ.";
+$l76 = "%s Á×ÔÏÍÁÔÉÞÅÓËÕÀ ÐÒÏ×ÅÒËÕ";
+$l77 = "òÁÚÒÅÛÉÔØ";
+$l78 = "úÁÐÒÅÔÉÔØ";
+$l79 = "þÉÓÌÏ ÍÉÎÕÔ ÍÅÖÄÕ ÐÒÏ×ÅÒËÁÍÉ:";
+$l80 = "÷Ù ÄÅÊÓÔ×ÉÔÅÌØÎÏ ÈÏÔÉÔÅ ×ÙÊÔÉ?";
+$l81 = "ôÅÍÁ:";
+$l82 = "<a href='readme.html' target='_blank'>ðÒÏÞÉÔÁÊÔÅ ÆÁÊÌ readme</a>";
+$l83 = "æÏÒÍÁÔÉÒÏ×ÁÎÎÙÊ ÔÅËÓÔ ÓÏÏÂÝÅÎÉÑ:";
+$l84 = "äÁ";
+$l85 = "îÅÔ";
+$l86 = "Ask for a receipt";
+$l87 = "Priority:";
+$l88 = "High";
+$l89 = "Normal";
+$l90 = "Low";
+$l91 = "Highest";
+$l92 = "Lowest";
+$l93 = "This setting turns formatted message text on or off. If you turn it on, plain text messages will be formatted in such a way that *bold* gets converted to <b>bold</b>, /italic/ gets converted to <i>italic</i>, and _underlined_ gets converted to <u>underlined</u>.";
+$l94 = "Help";
+$l95 = "Close";
+$l96 = "If you want to limit the users of this program to only using one POP server or a small number of predefined POP servers, write a list of those servers separated by a comma and a space: <i>mail1.example.com, mail2.example.com, mail3.example.com</i>";
+$l97 = "Enter one or more port numbers separated by a comma and a space, e.g. <i>110, 111</i>.";
+$l98 = "Enter one or more POP usernames separated by a comma and a space, e.g. <i>user1, user2</i>.";
+$l99 = "Your email address:";
+$l100 = "You did not select any messages to be deleted.";
+$l101 = "Are you sure that you want to delete the selected messages?";
+$l102 = "(De)select all";
+$l103 = "Repeat the password";
+$l104 = "The two passwords you entered are not the same.";
+
+
+
+function l14($timeStamp) {
+	return date("m-d-Y h:i a", $timeStamp);
+}
+?>
