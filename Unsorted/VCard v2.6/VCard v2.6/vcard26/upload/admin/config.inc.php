1c101a9073ee45e09469698ee82368f46668ad8f
me-suzy <me.suzana@gmail.com>
2025-08-14 03:06:28 +0300

    Add Unsorted/VCard v2.6/VCard v2.6/vcard26/upload/admin/config.inc.php

---
+++ Unsorted/VCard v2.6/VCard v2.6/vcard26/upload/admin/config.inc.php
@@ -0,0 +1,17 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////////
+// Database Variables:
+////////////////////////////////////////////////////////////////////////////////////
+$hostname	='localhost';		// database host
+$dbUser		='root';		// database username
+$dbPass		='password';		// database password
+$dbName		='vcard26'; 		// database name
+
+$use_admin_password	= 1;			// Do you want use password protected control panel? 1-Yes 0-No
+
+
+// You can set safeupload =1 if you have problem with upload feature. If you have safe mode = on in a webserver
+// with PHP4 or greater you steal can use upload feature, set safeupload=1. Remember it only work in PHP 4 or greater.
+$safeupload = 1;
+
+?>
