32c3c6d59446cac79dd18ad7aa240e39e392a705
me-suzy <me.suzana@gmail.com>
2025-08-14 03:10:04 +0300

    Add Unsorted/VirtuaNews.v1.03.Nullified/upload/help.php

---
+++ Unsorted/VirtuaNews.v1.03.Nullified/upload/help.php
@@ -0,0 +1,61 @@
+<?php
+
+/*======================================================================*\
+|| #################################################################### ||
+||  Program Name         : VirtuaNews Pro                                 
+||  Release Version      : 1.0.3                                          
+||  Program Author       : VirtuaSystems                                  
+||  Supplied by          : Ravish                                         
+||  Nullified by         : WTN Team                                       
+||  Distribution         : via WebForum, ForumRU and associated file dumps
+|| #################################################################### ||
+\*======================================================================*/
+
+require("global.php");
+
+switch($action) {
+
+case "faq":
+
+  $navbar = makenavbar("FAQ's");
+  @include("static/sub_pages/help_faq_".$pagesetid.".php");
+
+break;
+
+case "qhtml":
+
+  $navbar = makenavbar("Quick HTML");
+  @include("static/sub_pages/help_qhtml_".$pagesetid.".php");
+
+break;
+
+case "smilie":
+
+  if ($use_forum) {
+    header_redirect($foruminfo[smilie_path],"Smilies");
+  }
+
+  unset($smilielist);
+
+  $smiliedata = getsmiliedata();
+
+  while ($smilie = fetch_array($smiliedata)) {
+    eval("\$smilielist .= \"".returnpagebit("help_smilie_bit")."\";");
+  }
+
+  $navbar = makenavbar("Smilie Help");
+  @include("static/sub_pages/help_smilie_".$pagesetid.".php");
+
+break;
+
+default:
+  standarderror("invalid_link");
+}
+
+/*======================================================================*\
+|| ####################################################################
+|| # File: help.php
+|| ####################################################################
+\*======================================================================*/
+
+?>
