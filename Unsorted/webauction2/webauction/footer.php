b072df3e25633e75ea3b62458f5bba5dd7f829ac
me-suzy <me.suzana@gmail.com>
2025-08-14 03:20:04 +0300

    Add Unsorted/webauction2/webauction/footer.php

---
+++ Unsorted/webauction2/webauction/footer.php
@@ -0,0 +1,66 @@
+<?
+
+/*
+   Copyright (c), 1999, 2000 - phpauction.org
+   Copyright (c), 2001, 2002 - webauction.de.vu
+
+   Lizenz siehe lizenz.txt & license
+
+   Die neueste Version kostenfrei zum Download unter:
+   http://webauction.de.vu
+
+*/
+
+
+?>
+
+
+   
+    <TABLE WIDTH="764" BORDER=0 cellspacing="0" cellpadding="1" ALIGN=LEFT bgcolor="#0066CC">
+
+    <TR><TD bgcolor="#0066CC">
+
+    <CENTER>
+
+          	<A href="./index.php?SESSION_ID=<? print urlencode($sessionID); ?>"><? print $nav_font.$MSG_274; ?></FONT></A>
+
+          	&nbsp;&nbsp;<font color="#FFFFFF">|&nbsp;&nbsp;</font> 
+
+          	<A href="./einrichten_ver.php?SESSION_ID=<? print urlencode($sessionID); ?>"><? print $nav_font.$MSG_236; ?></FONT></A>
+
+          	&nbsp;&nbsp;<font color="#FFFFFF">|&nbsp;&nbsp;</font>
+
+          	<A href="./anmeldung.php?SESSION_ID=<SCRIPT Language=PHP> print urlencode($sessionID); </SCRIPT>"><? print $nav_font.$MSG_235; ?></FONT></A>
+
+          	&nbsp;&nbsp;<font color="#FFFFFF">|&nbsp;&nbsp;</font>
+
+          	<A href="./my_account_login.php?SESSION_ID=<SCRIPT Language=PHP> print urlencode($sessionID); </SCRIPT>"><? print $nav_font.$MSG_259; ?></FONT></A>
+
+          	&nbsp;&nbsp;<font color="#FFFFFF">|&nbsp;&nbsp;</font>
+
+          	<A href="help.php?SESSION_ID=<? print urlencode($sessionID); ?>"><? print $nav_font.$MSG_164; ?></FONT></A>
+           
+                &nbsp;&nbsp;<font color="#FFFFFF">|&nbsp;&nbsp;</font>
+                
+                <a href="suche.php?SESSION_ID=<SCRIPT Language=PHP> print urlencode($sessionID); </SCRIPT>"><? print $nav_font.$MSG_1000; ?></FONT></a>
+
+
+</CENTER>
+
+
+</TD></TR>
+</TABLE>
+
+</TD></TR>
+</TABLE>
+
+<TABLE WIDTH="740" BORDER=0 bgcolor="#ffffff">
+<TR><TD><DIV align="center"><BR>
+<FONT COLOR="#9999999"><a href="http://webauction.de.vu" target=_neu><? print $sml_font.$MSG_260; ?></a></FONT></DIV>
+</TD></TR>
+</TABLE>
+
+
+</BODY>
+
+</HTML>
