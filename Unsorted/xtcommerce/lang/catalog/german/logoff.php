987006ecee71c68bcedcb97f62124e1f7b092281
me-suzy <me.suzana@gmail.com>
2025-08-14 04:15:09 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/logoff.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/logoff.php
@@ -0,0 +1,21 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: logoff.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(logoff.php,v 1.3 2003/02/16); www.oscommerce.com 
+   (c) 2003	 nextcommerce (logoff.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Abmeldung');
+define('NAVBAR_TITLE', 'Abmeldung');
+define('TEXT_MAIN', 'Sie wurden soeben sicher abgemeldet, so dass niemand mehr Ihre Daten einsehen kann.<br><br>Ihr Warenkorb wurde zwischengespeichert und steht Ihnen nach der n&auml;chsten Anmeldung wieder zur Verf&uuml;gung.');
+?>
