d6ac23d123c3737df7b2634ad4055bf285a41544
me-suzy <me.suzana@gmail.com>
2025-08-14 03:07:45 +0300

    Add Unsorted/vCard v2.60 nullified/vcard26/help/phptest.php

---
+++ Unsorted/vCard v2.60 nullified/vcard26/help/phptest.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+	<title>vCard PRO - PHP Test</title>
+	</head>
+<body>
+<!--
+<?php
+
+// determine if php is running
+if (1==0) {
+  echo "-->PHP is not installed correctly - Please contact your system administrator.<!--";
+} else {
+  echo "--".">";
+}
+
+echo "PHP is installed correctly";
+
+echo "<"."!--";
+?>
+
+-->
+</body>
+</html>
