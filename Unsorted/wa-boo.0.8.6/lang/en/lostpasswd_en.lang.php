7dbbe05ce7317a7d4f641d5a3bfb7b80f51a6608
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:32 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/lostpasswd_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/lostpasswd_en.lang.php
@@ -0,0 +1,15 @@
+0|Lost Password ?|
+1|You can receive your password by email.|
+2|My email |
+3|< Back|
+4|Receive my pass|
+5|Your login and password for |
+6|Hello |
+7|here are you login and password for |
+8|Login : |
+9|Password : |
+10|Your login and password have been sent to |
+11|email send problem !|
+12| is not an a recorded email address !|
+13| is an invalid email address :-(|
+14|###### PLEASE DO NOT ERASE THIS LINE #####||
