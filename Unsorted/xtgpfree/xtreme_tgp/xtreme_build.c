6a25078ab930e3524bb5f73b5463789cca5edb35
me-suzy <me.suzana@gmail.com>
2025-08-14 04:17:37 +0300

    Add Unsorted/xtgpfree/xtreme_tgp/xtreme_build.c

---
+++ Unsorted/xtgpfree/xtreme_tgp/xtreme_build.c
@@ -0,0 +1,19 @@
+/*
+######################################################################
+#                       X-treme TGP 
+#                        Created by Relic
+#                     webmaster@cyphonic.net
+#####################################################################
+You must edit the path below!
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+main()
+{
+system("./xtreme_update.cgi > /home/darkforce/newraw/index.html");
+printf("Location: index.html\n\n");
+}
+
