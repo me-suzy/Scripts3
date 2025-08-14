b0132d9ffa2189c43df14fc66e3a3bf04932b561
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:35 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/table/sumh_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/table/sumh_tpl.php
@@ -0,0 +1,76 @@
+<?php die("Access restricted");?>
+<table width="570" bgcolor="#CCCCCC" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align=center valign="top">
+
+      <table width="560" bgcolor="#FFFFFF" border="1" cellspacing="0" cellpadding="0">
+        <tr height="25" bgcolor="#666666">
+          <td width="240" height="25" align="center"><b><font size=3 color="#FFFFFF">
+            <SPAN class="%%class%%">Interval</SPAN>
+          </font></b></td>
+          <td height="25" width="80" align="center">
+            <input class=b1 type="submit" name="vvisitors_x" value="%%valh3%%">
+          </td>
+          <td height="25" width="80" align="center">
+            <input class=b1 type="submit" name="vhosts_x"    value="%%valh4%%">
+          </td>
+          <td height="25" width="80" align="center">
+            <input class=b1 type="submit" name="vreloads_x"  value="%%valh5%%">
+          </td>
+          <td height="25" width="80" align="center">
+            <input class=b1 type="submit" name="vhits_x"     value="%%valh6%%">
+          </td>
+        </tr>
+
+          %%rows%%
+
+        <tr bgcolor="#666666">
+          <td height=25 align="center"><b><font size=3 color="#FFFFFF">
+            <SPAN class="%%class%%">Total</SPAN>
+          </font></b></td>
+          <td width="80" align="center"><b><font size=3 color="#FFFFFF">
+            <SPAN class="%%class%%">%%vale3%%</SPAN>
+          </font></b></td>
+          <td width="80" align="center"><b><font size=3 color="#FFFFFF">
+            <SPAN class="%%class%%">%%vale4%%</SPAN>
+          </font></b></td>
+          <td width="80" align="center"><b><font size=3 color="#FFFFFF">
+            <SPAN class="%%class%%">%%vale5%%</SPAN>
+          </font></b></td>
+          <td width="80" align="center"><b><font size=3 color="#FFFFFF">
+            <SPAN class="%%class%%">%%vale6%%</SPAN>
+          </font></b></td>
+        </tr>
+      </table>
+
+    </td>
+  </tr>
+  <tr>
+    <td align="center">
+      <table width="560" bgcolor=#000066 height=24 border="0" cellspacing="0" cellpadding="1">
+        <tr bgcolor=#000066>
+          <td><input type="image" border=0 name="hbegin" width="23" src="%%url%%images/scroll/vbegin.gif"></td>
+          <td><input type="image" border=0 name="hleft" width="23" src="%%url%%images/scroll/vup.gif"></td>
+          <td width="100%" align=center>
+      <table width="100%" bgcolor=#FFFFFF height=20 border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td align=center>
+            <font size=3 color="#000066"><b>
+              <SPAN class="%%class%%">%%range%%</SPAN>
+            </b></font>
+            </SPAN>
+          </td>
+        </tr>
+      </table>
+          </td>
+          <td><input type="image" border=0 name="hright" width="23" src="%%url%%images/scroll/vdown.gif"></td>
+          <td><input type="image" border=0 name="hend" width="23" src="%%url%%images/scroll/vend.gif"></td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  <tr>
+    <td width=560 height=10>
+    </td>
+  </tr>
+</table>
