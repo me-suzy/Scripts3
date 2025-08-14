4866b117903b59041d623fb3e4b1fab062d29bf2
me-suzy <me.suzana@gmail.com>
2025-08-14 03:17:29 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/user_help/welcome.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/user_help/welcome.php
@@ -0,0 +1,76 @@
+<?
+    include ("../../../includes/global_vars.php");
+    include ("../../../includes/fotools.php");
+
+    session_start();
+?>
+<html>
+    <head>
+    <title>wa-boo</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <style><? include ("../../../includes/css.php"); ?></style> 
+    </head>
+
+    <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" background="../../../images/bg1.gif">
+        <table border="0" cospan="0">
+            <tr>
+                <td>
+                    <div align="center" class="hlptitle">
+                        Bienvenue dans l'aide d'wa-boo
+                    </div>
+                    <br>
+                    <span class="hlpsubtitle">
+                        Introduction :
+                    </span>
+                   
+                    <div class="hlpfont">
+                        wa-boo est un logiciel de gestion de carnet d'adresse, partageable, multilingue et pratiquant l'import-export de ses données.
+                        <br>Fondé sur PHP4 / mySQL, wa-boo est distribué sous <a href="GPL.php">licence GPL</a>. 
+                    </div>
+                    <br>
+                    <span class="hlpsubtitle">
+                        Fonctions principales :
+                    </span>
+                    <div class="hlpfont">
+                        - Créer/héberger votre carnet d'adresse sur un serveur Web (PHP4/mySQL). Ainsi, Votre carnet d'adresse est potentiellement accessible où que vous soyez
+                        <br><br> - Importer/exporter vos adresses vers/depuis votre logiciel email ou autre application de gestion des adresses à l'aide d'un fichier texte avec séparateur
+                        <br><br> - Gérer des groupes afin de permettre aux membres d'un groupe de partager toute ou partie des contacts. Si, dans une association, chacun est responsable de son adresse, la qualité du contenu partagé est meilleure.
+                        <br><br> - D'envoyer un email aux membres d'un groupe. Petite fonction de Mailing list qui ne remplacera pas un outils dédié, mais qui peut rendre service.
+                        <br><br> - wa-boo est finement configurable (couleurs, polices, organisation des informations, Langues)  
+                    </div>
+                    <br>
+                    <br>
+                    
+                    <div class="hlpsubtitle">
+                        Liste complète des fonctionnalités :
+                    </div>
+                    
+                    <div class="hlpfont">
+                                 - Création, modification et suppression d'un contact,
+                        <br><br> - Possibilité de laisser consulter un contacts par un groupe auquel on appartient,
+                        <br><br> - Création, modification et suppression des catégories de classement des contacts personnels,
+                        <br><br> - Choix des 4 champs affichés sur la page principale,
+                        <br><br> - Choix des 6 champs affichés par survol de l'icône de consultation la page principale,
+                        <br><br> - Configuration complète du moteur de recherche,
+                        <br><br> - Affichage du champ "Nom" en majuscules,
+                        <br><br> - Choix du nombre de lignes simultanées par page, 
+                        <br><br> - Confirmation des suppressions,
+                        <br><br> - Choix de la couleur de la barre de menu,
+                        <br><br> - Choix de la couleur et de la taille de la police principale,
+                        <br><br> - Choix de la couleur et de la taille de la police secondaire,
+                        <br><br> - Choix de la couleur des boutons,
+                        <br><br> - Choix de la langue,
+                        <br><br> - Envoi d'un email aux membres et administrateurs d'un groupe auquel on est inscrit,
+                        <br><br> - Importer des contacts dans wa-boo à partir de fichiers exportés ou construits par d'autres applications,
+                        <br><br> - Exporter des contacts depuis wa-boo pour les ré-importer dans d'autres applications,
+                        <br><br> - Supprimer par lot votre carnet d'adresse (par catégories, par groupe)
+                        <br><br> - Créer, modifier et supprimer des utilisateurs par les administrateurs des groupes (voir l'aide d'aministration).
+                        <br><br> - Créer, modifier et supprimer des utilisateurs et des administrateurs par le super-Administrateur (Mode Godlike :).
+                        <br><br>
+                    </div>
+                </td>
+            </tr>
+        </table>
+        
+    </body>
+</html>
