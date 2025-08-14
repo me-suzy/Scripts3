f5fe381e7da982d440c9ec5e01e610fff463daf8
me-suzy <me.suzana@gmail.com>
2025-08-14 03:15:01 +0300

    Add Unsorted/wa-boo.0.8.6/footer.php

---
+++ Unsorted/wa-boo.0.8.6/footer.php
@@ -0,0 +1,7 @@
+<div align="center">
+    <hr width="280">
+  <span class="stdfont">
+    Hébergement offert par <a href="http://www.ovh.fr/" target="_blank"><u>OVH</u></a> - <a href="http://www.gnu.org/copyleft/copyleft.fr.html">Copyleft</a> <strike>&copy;</strike> 
+    2002&nbsp;<a href="mailto:groinkwarefactory@nocive.com">the GroinkWare Factory Inc.</a>
+  </span>
+</div>
