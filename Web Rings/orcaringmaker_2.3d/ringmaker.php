2c0be33bfebdd8ba8cadfc4a76346f42d745bc3d
me-suzy <me.suzana@gmail.com>
2025-08-14 04:52:47 +0300

    Add Web Rings/orcaringmaker_2.3d/ringmaker.php

---
+++ Web Rings/orcaringmaker_2.3d/ringmaker.php
@@ -0,0 +1,26 @@
+<?php 
+
+include "orca/orm_lang_en.php";
+include "orca/orm_head.php";
+
+?><html>
+<head>
+  <title><?php echo $rData['UA']; ?></title>
+
+  <link rel="stylesheet" type="text/css" href="orca/orm_style.css" />
+  <style type="text/css">
+
+body {
+  background-color:#ffffff;
+  font:normal 100% Arial,sans-serif;
+}
+
+  </style>
+</head>
+
+<body>
+
+  <?php include "orca/orm_body.php"; ?>
+
+</body>
+</html>
