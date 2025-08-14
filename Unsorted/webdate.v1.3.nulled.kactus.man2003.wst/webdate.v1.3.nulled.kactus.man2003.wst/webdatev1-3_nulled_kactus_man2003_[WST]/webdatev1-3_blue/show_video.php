4b9340cc4abdf263bcea5e68f0e78a0b1ca2f330
me-suzy <me.suzana@gmail.com>
2025-08-14 03:27:50 +0300

    Add Unsorted/webdate.v1.3.nulled.kactus.man2003.wst/webdate.v1.3.nulled.kactus.man2003.wst/webdatev1-3_nulled_kactus_man2003_[WST]/webdatev1-3_blue/show_video.php

---
+++ Unsorted/webdate.v1.3.nulled.kactus.man2003.wst/webdate.v1.3.nulled.kactus.man2003.wst/webdatev1-3_nulled_kactus_man2003_[WST]/webdatev1-3_blue/show_video.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Video Clip</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<div align="center">
+  <p><OBJECT ID="mediaPlayer" CLASSID="CLSID:22d6f312-b0f6-11d0-94ab-0080c74c7e95" CODEBASE="http://activex.microsoft.com/activex/ controls/mplayer/en/nsmp2inf.cab#Version=5,1,52,701" STANDBY="Loading Microsoft Windows Media Player components..." TYPE="application/x-oleobject">
+<PARAM NAME="fileName" VALUE="<? echo $video; ?>">
+<PARAM NAME="animationatStart" VALUE="true">
+<PARAM NAME="transparentatStart" VALUE="true">
+<PARAM NAME="autoStart" VALUE="true">
+<PARAM NAME="showControls" VALUE="false">
+</OBJECT></p>
+  <p><a href="javascript:window.close();"><font size="2" face="Arial, Helvetica, sans-serif">close
+    this window</font></a></p>
+</div>
+</body>
+</html>
