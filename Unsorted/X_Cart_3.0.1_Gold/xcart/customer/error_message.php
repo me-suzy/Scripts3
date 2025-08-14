5c73ead5132ee1e39208d09fee15f25ab6c4960c
me-suzy <me.suzana@gmail.com>
2025-08-14 04:19:15 +0300

    Add Unsorted/X_Cart_3.0.1_Gold/xcart/customer/error_message.php

---
+++ Unsorted/X_Cart_3.0.1_Gold/xcart/customer/error_message.php
@@ -0,0 +1,61 @@
+<?
+/*****************************************************************************\
++-----------------------------------------------------------------------------+
+| X-Cart                                                                      |
+| Copyright (c) 2001-2002 RRF.ru development. All rights reserved.            |
++-----------------------------------------------------------------------------+
+| The RRF.RU DEVELOPMENT forbids, under any circumstances, the unauthorized   |
+| reproduction of software or use of illegally obtained software. Making      |
+| illegal copies of software is prohibited. Individuals who violate copyright |
+| law and software licensing agreements may be subject to criminal or civil   |
+| action by the owner of the copyright.                                       |
+|                                                                             |
+| 1. It is illegal to copy a software, and install that single program for    |
+| simultaneous use on multiple machines.                                      |
+|                                                                             |
+| 2. Unauthorized copies of software may not be used in any way. This applies |
+| even though you yourself may not have made the illegal copy.                |
+|                                                                             |
+| 3. Purchase of the appropriate number of copies of a software is necessary  |
+| for maintaining legal status.                                               |
+|                                                                             |
+| DISCLAIMER                                                                  |
+|                                                                             |
+| THIS SOFTWARE IS PROVIDED BY THE RRF.RU DEVELOPMENT TEAM ``AS IS'' AND ANY  |
+| EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED |
+| WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE      |
+| DISCLAIMED.  IN NO EVENT SHALL THE RRF.RU DEVELOPMENT TEAM OR ITS           |
+| CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,       |
+| EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,         |
+| PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; |
+| OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,    |
+| WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR     |
+| OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF      |
+| ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                  |
+|                                                                             |
+| The Initial Developer of the Original Code is RRF.ru development.           |
+| Portions created by RRF.ru development are Copyright (C) 2001-2002          |
+| RRF.ru development. All Rights Reserved.                                    |
++-----------------------------------------------------------------------------+
+\*****************************************************************************/
+
+#
+# $Id: error_message.php,v 1.11 2002/04/26 08:40:04 zorg Exp $
+#
+
+require "../smarty.php";
+require "../config.php";
+@include "./https.php";
+require "./auth.php";
+require "../include/categories.php";
+
+#
+# Assign Smarty variables and show template
+#
+if ($error)
+	$smarty->assign("main",$error);
+else
+	$smarty->assign("main",$QUERY_STRING);
+
+$smarty->display("customer/home.tpl");
+?>
