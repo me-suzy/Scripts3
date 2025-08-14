a5a5e18c0c708c1555094acedff503488e12ce57
me-suzy <me.suzana@gmail.com>
2025-08-14 04:55:42 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/admin/html_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/admin/html_tpl.php
@@ -0,0 +1,6 @@
+<?php die("Access restricted");?>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr><td>
+    <textarea class="box7" cols=68 rows=12 name="textfield">%%ahtml%%</textarea>
+  </td></tr>
+</table>
