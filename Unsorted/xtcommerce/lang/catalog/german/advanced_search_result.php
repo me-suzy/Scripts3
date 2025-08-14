ee4b62196e0e9ef09abe10e82bc178a2e384d942
me-suzy <me.suzana@gmail.com>
2025-08-14 04:14:42 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/advanced_search_result.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/advanced_search_result.php
@@ -0,0 +1,33 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: advanced_search_result.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(advanced_search_result.php,v 1.12 2003/02/16); www.oscommerce.com 
+   (c) 2003	 nextcommerce (advanced_search_result.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE1', 'Erweiterte Suche');
+define('NAVBAR_TITLE2', 'Suchergebnisse');
+define('HEADING_TITLE', 'Artikel, welche den Suchkriterien entsprechen');
+define('TABLE_HEADING_IMAGE', '');
+define('TABLE_HEADING_MODEL', 'Artikelnummer');
+define('TABLE_HEADING_PRODUCTS', 'Bezeichnung');
+define('TABLE_HEADING_MANUFACTURER', 'Hersteller');
+define('TABLE_HEADING_QUANTITY', 'Anzahl');
+define('TABLE_HEADING_PRICE', 'Einzelpreis');
+define('TABLE_HEADING_WEIGHT', 'Gewicht');
+define('TABLE_HEADING_BUY_NOW', 'jetzt bestellen');
+define('TEXT_NO_PRODUCTS', 'Es wurden keine Artikel gefunden, die den Suchkriterien entsprechen.');
+define('TEXT_NO_PRODUCTS2', 'Es wurden keine Artikel gefunden, die den Suchkriterien entsprechen.');
+define('TEXT_BUY', '1 x \'');
+define('TEXT_NOW', '\' bestellen');
+?>
