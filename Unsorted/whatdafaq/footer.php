07250d9f1801b60d2931dee1a2cbe666466eea3d
me-suzy <me.suzana@gmail.com>
2025-08-14 03:41:52 +0300

    Add Unsorted/whatdafaq/footer.php

---
+++ Unsorted/whatdafaq/footer.php
@@ -0,0 +1,23 @@
+<?php
+if ( $loggedin == "yes" ) {
+   echo ( "<form action=$PHP_SELF method=post><center><input type=submit name=logout value=\" Logout \" class=\"small\"></form></center>" );
+}
+?>
+   
+</td>
+</tr>
+<tr>
+<td bgcolor="lime">
+<table width="99%">
+<tr>
+<td class="small" colspan="2" align="center"><a href=index.php>FAQ Page Category 1</a> : <a href=index.php?faqcategory=2>FAQ Page Category 2</a> : <a href=index.php?faqcategory=3>FAQ Page Category 3</a> : <a href=whatdaadmin.php>WhatDaFaq Admin</a></td>
+</tr>
+<tr>
+<td class="small"><img src=http://www.pinkgoblin.com/images/pinkfacesmall.gif> GNU Open Source from <a href=http://www.pinkgoblin.com>Pinkgoblin</a></td><td class="small" align="right">Slap you Questions / Suggestions / Comments in <a href=http://www.pinkgoblin.com/forum/>Pinkgoblin Forums</a></td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</body>
+</html>
