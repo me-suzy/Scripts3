20fb2c937c287ca73a063389b68b56c3407848de
me-suzy <me.suzana@gmail.com>
2025-08-14 04:14:32 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/account_newsletters.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/account_newsletters.php
@@ -0,0 +1,25 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: account_newsletters.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(account_newsletters.php,v 1.1 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (account_newsletters.php,v 1.6 2003/08/16); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Ihr Konto');
+define('NAVBAR_TITLE_2', 'Newsletter Abonnements');
+define('HEADING_TITLE', 'Newsletter Abonnements');
+define('MY_NEWSLETTERS_TITLE', 'Meine Newsletter Abonnements');
+define('MY_NEWSLETTERS_GENERAL_NEWSLETTER', 'Allgmeiner Newsletter');
+define('MY_NEWSLETTERS_GENERAL_NEWSLETTER_DESCRIPTION', 'Beinhaltet allgemeine Nachrichten &uuml;ber unseren Shop, Informationen &uuml;ber neue Produkte, Sonderangebote und andere Informationen von allgemeinem Interesse.');
+define('SUCCESS_NEWSLETTER_UPDATED', 'Ihre Newsletter Abonnements wurden erfolgreich aktualisiert!');
+?>
