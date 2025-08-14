a28619b1fc5452fb0ae6014d24cd9864afe1dec0
me-suzy <me.suzana@gmail.com>
2025-08-14 05:02:49 +0300

    Add Web Traffic Analysis/statistics4/docs/example.php

---
+++ Web Traffic Analysis/statistics4/docs/example.php
@@ -0,0 +1,13 @@
+<?php include_once("statistics/weblog.php"); ?>
+<html>
+<head>
+</head>
+<body>
+
+<script language="javascript">document.write('<img src="statistics/weblog.php?sh='+screen.height+'&sw='+screen.width+'&sc='+screen.colorDepth+'&res=<?php echo $weblog_id; ?>" width="1" height="1" alt="">');</script>
+<noscript><img src="statistics/weblog.php?res=<?php echo $weblog_id; ?>" width="1" height="1" alt=""></noscript>
+
+
+<h1 align="center">Your data has been logged!</h1>
+</body>
+</html>
