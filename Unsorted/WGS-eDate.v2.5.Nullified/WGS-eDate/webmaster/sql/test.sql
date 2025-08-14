6fda3ecc368ff4120dd40a00ae397451e1c15f41
me-suzy <me.suzana@gmail.com>
2025-08-14 03:33:13 +0300

    Add Unsorted/WGS-eDate.v2.5.Nullified/WGS-eDate/webmaster/sql/test.sql

---
+++ Unsorted/WGS-eDate.v2.5.Nullified/WGS-eDate/webmaster/sql/test.sql
@@ -0,0 +1,7 @@
+INSERT INTO members VALUES (1, 'test', 'test', 'test', 'test', 'test@yourdomain.com', 'test', 'International', 'Other-Not Shown', 'test', 'test', 'test', '', 1, 1039462688);
+INSERT INTO members VALUES (2, 'WTN', '1', 'a', 'b', 'contact@yourdomain.com', 'WTN', 'International', 'Romania', '', '', '', '', 1, 1040038146);
+
+
+INSERT INTO profiles VALUES (1, '1982-07-03', '', '', '', '', '', '', '', '', '', '', ',', '', '', ',', 'test', 'test', '', 1040038235);
+INSERT INTO profiles VALUES (2, '1982-07-03', '', '', '', '', 'Male', '', '', '', '', '', 'white romanian', '', '', '', 'computer', 'aaaaaaaaaaaaaaaaaaa', '', 1040038250);
+
