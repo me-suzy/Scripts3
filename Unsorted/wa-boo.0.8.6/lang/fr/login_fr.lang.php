dc8d478c3b3de1479e5a9a7bc8f82e1fa6d24d15
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:59 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/login_fr.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/login_fr.lang.php
@@ -0,0 +1,11 @@
+0|wa-boo|
+1|est distribué sous|
+2|licence GPL|
+3|i d e n t i f i c a t i o n|
+4|Login|
+5|Mot de passe|
+6|log en tant qu'administrateur|
+7|Accès public|
+8|Mot de passe oublié ?|
+9|Si t'es perdu, clique sur le cochon !|
+10|###### PLEASE DO NOT ERASE THIS LINE #####||
