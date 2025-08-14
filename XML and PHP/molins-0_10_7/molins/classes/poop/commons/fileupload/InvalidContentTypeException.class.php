4af2030d021773c911862e511862ea799c4b190b
me-suzy <me.suzana@gmail.com>
2025-08-14 05:05:47 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/commons/fileupload/InvalidContentTypeException.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/commons/fileupload/InvalidContentTypeException.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: InvalidContentTypeException.class.php,v 1.3 2005/10/14 09:52:51 slizardo Exp $
+ * @package poop.commons.fileupload
+ */
+
+import('poop.commons.fileupload.FileUploadException');
+
+/**
+ * Thrown to indicate that the request is not a multipart request.
+ */
+class InvalidContentTypeException extends FileUploadException {}
+
+?>
