db85091040c5440f78d0d7224b6b2be226fade17
me-suzy <me.suzana@gmail.com>
2025-08-14 04:14:27 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/account_edit.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/account_edit.php
@@ -0,0 +1,26 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: account_edit.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(account_edit.php,v 1.11 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (account_edit.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Ihr Konto');
+define('NAVBAR_TITLE_2', 'Ihre pers&ouml;nliche Daten &auml;ndern');
+
+define('HEADING_TITLE', 'Ihre pers&ouml;nliche Daten &auml;ndern');
+
+define('MY_ACCOUNT_TITLE', 'Ihr Konto');
+
+define('SUCCESS_ACCOUNT_UPDATED', 'Your account has been successfully updated.');
+?>
