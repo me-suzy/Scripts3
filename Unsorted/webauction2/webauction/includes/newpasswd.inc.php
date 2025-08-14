4ec9024d59aa96e6299851e28935a46f5a01a755
me-suzy <me.suzana@gmail.com>
2025-08-14 03:23:56 +0300

    Add Unsorted/webauction2/webauction/includes/newpasswd.inc.php

---
+++ Unsorted/webauction2/webauction/includes/newpasswd.inc.php
@@ -0,0 +1,17 @@
+<?
+	$to 		= $EMAIL;
+	$from 	= "From:  <$TPL_sender_mail>\n";
+	$subject	= "Ihr neuese";
+	$message = "Guten Tag $TPL_username,
+	
+Sie oder jemand anderes hat ein neues Passwort für
+Ihren Zugang zu den Online-Auktionen auf inso-shop.de angefordert.
+
+Es lautet: $NEWPASSWD
+
+Bitte melden Sie sich auf <#c_siteurl#>
+mit Ihrem aktuellen Benutzernamen und Ihrem neuen Passwort an und ändern Sie Ihr neues
+Passwort umgehend in ein von Ihnen ausgewähltes.
+
+Weiterhin viel Spass und Erfolg beim Bieten auf <#c_sitename#>";
+?>
