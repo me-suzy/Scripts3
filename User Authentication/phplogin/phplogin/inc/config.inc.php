e939c104cde4f014c6b097315a9edb8f98b4703f
me-suzy <me.suzana@gmail.com>
2025-08-14 04:35:57 +0300

    Add User Authentication/phplogin/phplogin/inc/config.inc.php

---
+++ User Authentication/phplogin/phplogin/inc/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+/* ===============================
+== Basic PHP/MySQL Authentication 
+== by x0x 
+== Email: x0x@ukshells.co.uk
+================================*/
+
+$CONFIG                  = array();
+$CONFIG['HOST_NAME']     = 'localhost';
+$CONFIG['DATABASE_NAME'] = 'phplogin';
+$CONFIG['DB_USERNAME']   = 'root';
+$CONFIG['DB_PASSWORD']   = '';
+?>
