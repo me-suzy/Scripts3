99215a3614a15623ebf90be98083f7722ee98ffb
me-suzy <me.suzana@gmail.com>
2025-08-14 04:14:44 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/checkout_confirmation.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/checkout_confirmation.php
@@ -0,0 +1,38 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: checkout_confirmation.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(checkout_confirmation.php,v 1.27 2003/02/16); www.oscommerce.com 
+   (c) 2003	 nextcommerce (checkout_confirmation.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Kasse');
+define('NAVBAR_TITLE_2', 'Best&auml;tigung');
+
+define('HEADING_TITLE', 'Bestellung aufgeben:');
+
+define('HEADING_DELIVERY_ADDRESS', 'Versandadresse');
+define('HEADING_SHIPPING_METHOD', 'Versandart');
+define('HEADING_PRODUCTS', 'Produkte');
+define('HEADING_TAX', 'MwSt.');
+define('HEADING_TOTAL', 'Summe');
+define('HEADING_BILLING_INFORMATION', 'Rechnungsinformationen');
+define('HEADING_BILLING_ADDRESS', 'Rechnungsadresse');
+define('HEADING_PAYMENT_METHOD', 'Zahlungsweise');
+define('HEADING_PAYMENT_INFORMATION', 'Zahlungsinformationen');
+define('HEADING_ORDER_COMMENTS', 'Anmerkung zu Ihrer Bestellung');
+
+define('TEXT_EDIT', 'Bearbeiten');
+
+define('HEADING_CONDITIONS_INFORMATION', 'Widerrufsrecht nach dem Fernabsatzgesetz');
+define('HEADING_CONDITIONS_TEXT', 'F&uuml;gen Sie hier Ihren Text zum Widerrufsrecht ein.');
+?>
