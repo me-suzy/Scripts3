cb2853d40a00dcbec3867edede8f17bad49560ee
me-suzy <me.suzana@gmail.com>
2025-08-14 03:56:30 +0300

    Add Unsorted/xtcommerce/checkout_success.php

---
+++ Unsorted/xtcommerce/checkout_success.php
@@ -0,0 +1,174 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: checkout_success.php,v 1.1 2003/09/06 21:38:27 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(checkout_success.php,v 1.48 2003/02/17); www.oscommerce.com 
+   (c) 2003	 nextcommerce (checkout_success.php,v 1.14 2003/08/17); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  include( 'includes/application_top.php');
+  
+  // include needed functions
+  require_once(DIR_FS_INC . 'xtc_draw_checkbox_field.inc.php');
+  require_once(DIR_FS_INC . 'xtc_draw_selection_field.inc.php');
+  require_once(DIR_FS_INC . 'xtc_image_button.inc.php');
+
+  // if the customer is not logged on, redirect them to the shopping cart page
+  if (!isset($_SESSION['customer_id'])) {
+    xtc_redirect(xtc_href_link(FILENAME_SHOPPING_CART));
+  }
+
+  if (isset($_GET['action']) && ($_GET['action'] == 'update')) {
+    $notify_string = 'action=notify&';
+    $notify = $_POST['notify'];
+    if (!is_array($notify)) $notify = array($notify);
+    for ($i=0, $n=sizeof($notify); $i<$n; $i++) {
+      $notify_string .= 'notify[]=' . $notify[$i] . '&';
+    }
+    if (strlen($notify_string) > 0) $notify_string = substr($notify_string, 0, -1);
+
+    xtc_redirect(xtc_href_link(FILENAME_DEFAULT, $notify_string));
+  }
+
+  require(DIR_WS_LANGUAGES . $_SESSION['language'] . '/' . FILENAME_CHECKOUT_SUCCESS);
+
+  $breadcrumb->add(NAVBAR_TITLE_1);
+  $breadcrumb->add(NAVBAR_TITLE_2);
+
+  $global_query = xtc_db_query("select global_product_notifications from " . TABLE_CUSTOMERS_INFO . " where customers_info_id = '" . $_SESSION['customer_id'] . "'");
+  $global = xtc_db_fetch_array($global_query);
+
+  if ($global['global_product_notifications'] != '1') {
+    $orders_query = xtc_db_query("select orders_id from " . TABLE_ORDERS . " where customers_id = '" . $_SESSION['customer_id'] . "' order by date_purchased desc limit 1");
+    $orders = xtc_db_fetch_array($orders_query);
+
+    $products_array = array();
+    $products_query = xtc_db_query("select products_id, products_name from " . TABLE_ORDERS_PRODUCTS . " where orders_id = '" . $orders['orders_id'] . "' order by products_name");
+    while ($products = xtc_db_fetch_array($products_query)) {
+      $products_array[] = array('id' => $products['products_id'],
+                                'text' => $products['products_name']);
+    }
+  }
+?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html <?php echo HTML_PARAMS; ?>>
+<head>
+<?php include(DIR_WS_MODULES.FILENAME_METATAGS); ?>
+<base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<script language="javascript"><!--
+function popupWindow(url) {
+  window.open(url,'popupWindow','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=yes,copyhistory=no,width=400,height=500,screenX=150,screenY=150,top=150,left=150')
+}
+//--></script>
+</head>
+<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0">
+<!-- header //-->
+<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+<!-- header_eof //-->
+
+<!-- body //-->
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+  <tr>
+    <td class="navLeft" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- left_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+<!-- left_navigation_eof //-->
+    </table></td>
+<!-- body_text //-->
+    <td class="tableBody" width="100%" valign="top"><?php echo xtc_draw_form('order', xtc_href_link(FILENAME_CHECKOUT_SUCCESS, 'action=update', 'SSL')); ?><table border="0" width="100%" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><table border="0" width="100%" cellspacing="4" cellpadding="2">
+          <tr>
+            <td valign="top"><?php echo xtc_image(DIR_WS_IMAGES . 'table_background_man_on_board.gif', HEADING_TITLE); ?></td>
+            <td valign="top" class="main"><?php echo xtc_draw_separator('pixel_trans.gif', '1', '10'); ?><div align="center" class="pageHeading"><?php echo HEADING_TITLE; ?></div><br><?php echo TEXT_SUCCESS; ?><br><br>
+<?php
+  if ($global['global_product_notifications'] != '1') {
+    echo TEXT_NOTIFY_PRODUCTS . '<br><p class="productsNotifications">';
+
+    $products_displayed = array();
+    for ($i=0, $n=sizeof($products_array); $i<$n; $i++) {
+      if (!in_array($products_array[$i]['id'], $products_displayed)) {
+        echo xtc_draw_checkbox_field('notify[]', $products_array[$i]['id']) . ' ' . $products_array[$i]['text'] . '<br>';
+        $products_displayed[] = $products_array[$i]['id'];
+      }
+    }
+
+    echo '</p>';
+  } else {
+    echo TEXT_SEE_ORDERS . '<br><br>' . TEXT_CONTACT_STORE_OWNER;
+  }
+?>
+            <h3><?php echo TEXT_THANKS_FOR_SHOPPING; ?></h3></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td align="right" class="main"><?php echo xtc_image_submit('button_continue.gif', IMAGE_BUTTON_CONTINUE); ?></td>
+      </tr>
+      <td align="left" class="main">
+      
+<?php  echo  TEXT_ORDER_PRINT; ?><br>
+<img src="<?php echo 'templates/'.CURRENT_TEMPLATE.'/buttons/' . $_SESSION['language'] . '/'; ?>button_print.gif" style="cursor:hand" onClick="window.open('<?php echo xtc_href_link(FILENAME_PRINT_ORDER,'oID='.$orders['orders_id']); ?>', 'popup', 'toolbar=0, width=640, height=600')">
+
+      </td>
+      <tr>
+        <td><?php echo xtc_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="25%"><table border="0" width="100%" cellspacing="0" cellpadding="0">
+              <tr>
+                <td width="50%" align="right"><?php echo xtc_draw_separator('pixel_silver.gif', '1', '5'); ?></td>
+                <td width="50%"><?php echo xtc_draw_separator('pixel_silver.gif', '100%', '1'); ?></td>
+              </tr>
+            </table></td>
+            <td width="25%"><?php echo xtc_draw_separator('pixel_silver.gif', '100%', '1'); ?></td>
+            <td width="25%"><?php echo xtc_draw_separator('pixel_silver.gif', '100%', '1'); ?></td>
+            <td width="25%"><table border="0" width="100%" cellspacing="0" cellpadding="0">
+              <tr>
+                <td width="50%"><?php echo xtc_draw_separator('pixel_silver.gif', '100%', '1'); ?></td>
+                <td width="50%"><?php echo xtc_image(DIR_WS_IMAGES . 'checkout_bullet.gif'); ?></td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td align="center" width="25%" class="checkoutBarFrom"><?php echo CHECKOUT_BAR_DELIVERY; ?></td>
+            <td align="center" width="25%" class="checkoutBarFrom"><?php echo CHECKOUT_BAR_PAYMENT; ?></td>
+            <td align="center" width="25%" class="checkoutBarFrom"><?php echo CHECKOUT_BAR_CONFIRMATION; ?></td>
+            <td align="center" width="25%" class="checkoutBarCurrent"><?php echo CHECKOUT_BAR_FINISHED; ?></td>
+          </tr>
+        </table></td>
+      </tr>
+<?php if (DOWNLOAD_ENABLED == 'true') include(DIR_WS_MODULES . 'downloads.php'); ?>
+    </table></form></td>
+<!-- body_text_eof //-->
+    <td class="navRight" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+<!-- right_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_right.php'); ?>
+<!-- right_navigation_eof //-->
+    </table></td>
+  </tr>
+</table>
+<!-- body_eof //-->
+
+<!-- footer //-->
+<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+<!-- footer_eof //-->
+<br>
+</body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
