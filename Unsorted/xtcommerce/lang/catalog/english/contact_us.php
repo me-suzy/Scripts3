45210c4ca0b40cc215110de1130785e7a2cf00ee
me-suzy <me.suzana@gmail.com>
2025-08-14 04:12:53 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/contact_us.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/contact_us.php
@@ -0,0 +1,26 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: contact_us.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(contact_us.php,v 1.7 2002/11/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (contact_us.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Contact Us');
+define('NAVBAR_TITLE', 'Contact Us');
+define('TEXT_SUCCESS', 'Your enquiry has been successfully sent to the Store Owner.');
+define('EMAIL_SUBJECT', 'Enquiry from ' . STORE_NAME);
+
+define('ENTRY_NAME', 'Full Name:');
+define('ENTRY_EMAIL', 'E-Mail Address:');
+define('ENTRY_ENQUIRY', 'Enquiry:');
+?>
