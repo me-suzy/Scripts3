81f9d4e6df9b0e48d867420e57fb3d7da171b5ad
me-suzy <me.suzana@gmail.com>
2025-08-14 04:32:38 +0300

    Add Usenet Gateway/newsportal-0.36tr1/Newsportal-0.36tr1/extras/poll/poll.php

---
+++ Usenet Gateway/newsportal-0.36tr1/Newsportal-0.36tr1/extras/poll/poll.php
@@ -0,0 +1,26 @@
+<?
+  // experimental!
+  // fills article-cache
+  $group=$_GET['group'];
+  include "config.inc.php";
+  $title.= ' - '.$group;
+  include "head.inc"; ?>
+
+<a name="top"></a>
+<h1 align="center"><?php echo $group; ?></h1>
+
+<p>Lese Overview- und Artikeldaten ein...</p>
+
+<? 
+include("$file_newsportal");
+$ns=OpenNNTPconnection($server,$port);
+flush();
+if ($ns != false) {
+  $headers = readOverview($ns,$group,1,true);
+  closeNNTPconnection($ns);
+}
+?>
+
+<p align="right"><a href="#top"><? echo $text_thread["button_top"];?></a></p>
+
+<? include "tail.inc"; ?>
