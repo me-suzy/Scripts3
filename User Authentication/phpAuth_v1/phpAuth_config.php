270ee983eb0ae0693fad5a9ec787c6231b2cc728
me-suzy <me.suzana@gmail.com>
2025-08-14 04:35:40 +0300

    Add User Authentication/phpAuth_v1/phpAuth_config.php

---
+++ User Authentication/phpAuth_v1/phpAuth_config.php
@@ -0,0 +1,12 @@
+<?
+
+////////////////////////////////////
+// Configuration file for phpAuth //
+////////////////////////////////////
+
+$database_name = "members.txt"; // Make sure you include the .txt in your database name
+
+$login_redirect = "restricted_area_1.php"; // page to directed to when logged in
+$logout_redirect = "index.php"; // redirect to your own logout page.
+
+?>
