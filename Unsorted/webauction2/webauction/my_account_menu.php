d2d9ca6070092f0cf6ebe56a5ac4d6a217b15a37
me-suzy <me.suzana@gmail.com>
2025-08-14 03:20:33 +0300

    Add Unsorted/webauction2/webauction/my_account_menu.php

---
+++ Unsorted/webauction2/webauction/my_account_menu.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+   Copyright (c), 1999, 2000 - phpauction.org
+   Copyright (c), 2001, 2002 - webauction.de.vu
+
+   Lizenz siehe lizenz.txt & license
+
+   Die neueste Version kostenfrei zum Download unter:
+   http://webauction.de.vu
+
+*/
+    include "header.php";
+	include "templates/my_account.php.html";
+
+
+  
+
+
+?>
