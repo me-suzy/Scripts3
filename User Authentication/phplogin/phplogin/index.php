1bf145cb7ab5fd989750343e5b08e4c7e228bc09
me-suzy <me.suzana@gmail.com>
2025-08-14 04:35:48 +0300

    Add User Authentication/phplogin/phplogin/index.php

---
+++ User Authentication/phplogin/phplogin/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/* ===============================
+== Basic PHP/MySQL Authentication 
+== by x0x 
+== Email: x0x@ukshells.co.uk
+================================*/
+
+include_once("inc/auth.inc.php");
+$user = _check_auth($_COOKIE);
+
+/* if user gets here they are authenticated */
+/* the stuff below here is my test data */
+
+_begin_html();
+_fake_nav();
+_display_user_data($user);
+_end_html();
+?>
