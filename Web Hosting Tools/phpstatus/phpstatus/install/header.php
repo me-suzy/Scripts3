928b40c83567142e41071e111019bda808194300
me-suzy <me.suzana@gmail.com>
2025-08-14 04:50:23 +0300

    Add Web Hosting Tools/phpstatus/phpstatus/install/header.php

---
+++ Web Hosting Tools/phpstatus/phpstatus/install/header.php
@@ -0,0 +1,30 @@
+<?php
+/****************************************************************/
+/*                         phpStatus                            */
+/*                       header.php file                        */
+/*                      (c)copyright 2003                       */
+/*                       By hinton design                       */
+/*                 http://www.hintondesign.org                  */
+/*                  support@hintondesign.org                    */
+/*                                                              */
+/* This program is free software. You can redistrabute it and/or*/
+/* modify it under the terms of the GNU General Public Licence  */
+/* as published by the Free Software Foundation; either version */
+/* 2 of the license.                                            */
+/*                                                              */
+/****************************************************************/
+
+$sql = "SELECT site_title FROM ".$prefix."_config";
+$result = $db->query($sql);
+
+$row = $db->fetch($result);
+$site_title = $row['site_title'];
+
+$template->getFile(array(
+                   'header' => 'admin/header.tpl')
+);
+$template->add_vars(array(
+                   'SITE_TITLE' => $site_title)
+);
+$template->parse("header");
+?>
