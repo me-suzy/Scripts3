9047638ad9cc0c07c1f9f994f13488485dc4cc6b
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:24 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/import2_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/import2_en.lang.php
@@ -0,0 +1,16 @@
+0|Import 2/5 : Statistics|
+1|Your file have been sucessfully opened.|
+2| It contents |
+3|lines |
+4|(and |
+5| columns).|
+6|You allready have |
+7| contacts in base, on a maximum of |
+8| contacts available.|
+9|After running import, you will have |
+10| contacts in base. |
+11|To continue with import process, please click on ' Next >> ' button.|
+12|<< Back|
+13|next >>|
+14|You can't run import any longer. <br>Please begin by a base or file number of contacts reduction|
+15|###### PLEASE DO NOT ERASE THIS LINE #####||
