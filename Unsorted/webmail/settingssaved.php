5c3b1ab2949a9af1f299c942e46f1f5966323d4e
me-suzy <me.suzana@gmail.com>
2025-08-14 03:30:00 +0300

    Add Unsorted/webmail/settingssaved.php

---
+++ Unsorted/webmail/settingssaved.php
@@ -0,0 +1,16 @@
+<?php
+$language = "English";
+$adminUsername = "username";
+$adminPassword = "password";
+$preset["host"] = array();
+$preset["port"] = array();
+$preset["username"] = array();
+$colsOrRows = "cols";
+$frameSizeHeaderList = "50";
+$formatText = true;
+$theme = "Dark";
+$title = "Instant Web Mail";
+$footer = "";
+$logInHeader = "<div style=\'margin-top: -10px; margin-left: -10px\'><a href=\'http://instantwebmail.sourceforge.net/\'><img src=\'images/instantwebmail.jpeg\' height=\'87\' width=\'514\' alt=\'Instant Web Mail\'/></a></div><br/><br/><br/><br/>";
+$logInFooter = "";
+?>
