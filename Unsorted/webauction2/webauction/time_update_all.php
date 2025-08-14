d395d4e1b37026a35756972b6b5b51a59453be44
me-suzy <me.suzana@gmail.com>
2025-08-14 03:21:32 +0300

    Add Unsorted/webauction2/webauction/time_update_all.php

---
+++ Unsorted/webauction2/webauction/time_update_all.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+   Copyright (c), 1999, 2000 - phpauction.org
+   Copyright (c), 2001, 2002 - webauction.de.vu
+
+   Lizenz siehe lizenz.txt & license
+
+   Die neueste Version kostenfrei zum Download unter:
+   http://webauction.de.vu
+
+*/
+
+	require('includes/messages.inc');
+	require('includes/config.inc');
+    include "templates/sell_update_all_php3.html";
+    include "footer_active_au.php";
+
+
+?>
