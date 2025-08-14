089af5b6144a5bee0ef9a90aadfa3878d0a086e4
me-suzy <me.suzana@gmail.com>
2025-08-14 05:08:37 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/mail/MailConnection.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/mail/MailConnection.class.php
@@ -0,0 +1,29 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: MailConnection.class.php,v 1.4 2005/10/11 15:57:31 slizardo Exp $
+ * @package poop.mail
+ */
+
+class MailConnection {
+	private static $conn;
+
+	/**
+	 * @param string $URL
+	 * @param string $user
+	 * @param string $password
+	 */
+	public static function getConnection($URL, $user, $password) {
+		self::$conn = imap_open($URL, $user, $password);
+	}
+}
+	
+?>
