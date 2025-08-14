6834c7757779a083a7b3e7bc54a0491c81f5506d
me-suzy <me.suzana@gmail.com>
2025-08-14 04:43:37 +0300

    Add Web Fetching/rss_get/example2.php

---
+++ Web Fetching/rss_get/example2.php
@@ -0,0 +1,15 @@
+<HTML>
+<head> <title> My RSS Feed </title> </head>
+<body>
+
+<?
+
+$url = "http://games.slashdot.org/games.rss"; //URL for Slashdot Games
+$displayname = "Slashdot | Games"; 
+$number = 3;
+include 'rss_get.php';
+
+?>
+
+</body>
+</html>
