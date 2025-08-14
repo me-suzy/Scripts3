fe86c19b12da34c2129a4ccdcd23a7e0ef9c1a9a
me-suzy <me.suzana@gmail.com>
2025-08-14 03:23:34 +0300

    Add Unsorted/webauction2/webauction/includes/endauctionmail.inc.php

---
+++ Unsorted/webauction2/webauction/includes/endauctionmail.inc.php
@@ -0,0 +1,48 @@
+
+$END_AUCTION_NO_WINNER = "Liebe/r $name,
+Die Auktion die Sie erstellt haben, wurde soeben beendet.
+
+Auktions Daten
+------------------------------------
+Artikel: $title
+Auktions ID: $id
+Auktionsbeginn: $starting_date
+Auktionsende: $ending_date
+Auktion URL: $auction_url
+
+Wir müssen Ihnen mitteilen, dass es für diese Auktion keinen Meistbietenden gibt.";
+
+$END_AUCTION_WINNER = "Liebe/r $name,
+Die Auktion die Sie erstellt haben, wurde soeben beendet.
+
+Auktions Daten
+------------------------------------
+Artikel: $title
+Auktions ID: $id
+Auktionsbeginn: $starting_date
+Auktionsende: $ending_date
+Auktion URL: $auction_url
+
+Der Meistbietende Ihrer Auktion ist: $bidder_name.
+Nehmen Sie mit dem Gewinner bitte unter folgender E-Mail Adresse Kontakt auf: $bidder_email";
+
+
+$END_AUCTION_WINNER_CONFIRMATION = "Liebe/r $bidder_name,
+Herzlichen Glückwunsch!!
+
+Sie sind Meistbietender und erhalten den Zuschlag auf untenstehenden Auktion.
+
+Auktions Daten
+------------------------------------
+Artikel: $title
+Auktions ID: $id
+Auktionsbeginn: $starting_date
+Auktionsende: $ending_date
+Auktion URL: $auction_url
+
+Der Verkäufer wird sich per E-Mail mit Ihnen in Kontakt setzen.
+Ist dies nicht der Fall ist hier seine E-Mail Adresse zur Kontaktaufnahme :$email.
+
+Vielen Dank, dass Sie unseren Kostenlosen Service in Anspruch genommen haben.";
+
+
