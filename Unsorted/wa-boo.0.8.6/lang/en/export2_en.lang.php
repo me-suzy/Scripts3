9b4685efe648d872ece5d92fbf75cb5d9786abb2
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:19 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/export2_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/export2_en.lang.php
@@ -0,0 +1,7 @@
+0|Export 2/3 : file take|
+1|Export process ran sucessfully :) |
+2|Now you ca choose between :|
+3|Download the file <br>(Please exit by 'OK' button after download),|
+4|Display file's content in your browser|
+5|OK|
+6|###### PLEASE DO NOT ERASE THIS LINE #####||
