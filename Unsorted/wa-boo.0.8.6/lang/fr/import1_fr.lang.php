95318820c8c081c24f94ab41f23908f0f9d91e2a
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:50 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/import1_fr.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/import1_fr.lang.php
@@ -0,0 +1,14 @@
+0|Etape 1 : Choix du fichier et envoi au serveur|
+1|L'import vous permet d'intégrer des contacts dans wa-boo <br>à partir de fichiers exportés ou construits par d'autres applications (Outlook Express&#153;, PDAs, etc...)|
+2|Le format du fichier que vous allez importer doit être de type ' avec séparateur ', <br> ce qui signifie que les champs du fichier sont délimités par un caractère particulier.|
+3|Les séparateurs les plus courants sont :|
+4|la virgule|
+5|le point-virgule|
+6|la tabulation [touche du clavier : ]|
+7|Sélectionnez votre fichier à l'aide du bouton ' Parcourir... '|
+8|Ce fichier doit être de type 'texte', délimité par un séparateur.|
+9|Séparateur de champ |
+10| (pour le caractère tabulation, entrez la lettre ' t ' )|
+11|Quitter l'import|
+12|Suivant >>|
+13|###### PLEASE DO NOT ERASE THIS LINE #####||
