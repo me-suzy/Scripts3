8d5947b499260ed85fd004a67a307ae56af80971
me-suzy <me.suzana@gmail.com>
2025-08-14 04:13:18 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/product_reviews_write.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/product_reviews_write.php
@@ -0,0 +1,28 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: product_reviews_write.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(product_reviews_write.php,v 1.6 2003/02/06); www.oscommerce.com 
+   (c) 2003	 nextcommerce (product_reviews_write.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE', 'Reviews');
+define('HEADING_TITLE', 'Have You Got Something To Say?');
+define('SUB_TITLE_PRODUCT', 'Product:');
+define('SUB_TITLE_FROM', 'From:');
+define('SUB_TITLE_REVIEW', 'Review:');
+define('SUB_TITLE_RATING', 'Rating:');
+define('TEXT_NO_HTML', '<small><font color="#ff0000"><b>NOTE:</b></font></small>&nbsp;HTML is not translated!');
+define('TEXT_BAD', '<small><font color="#ff0000"><b>BAD</b></font></small>');
+define('TEXT_GOOD', '<small><font color="#ff0000"><b>GOOD</b></font></small>');
+define('ERROR_INVALID_PRODUCT', 'That product is no longer available. Please try again.');
+?>
