9340ac322b8b95e619b18eb7992183b13e0a1615
me-suzy <me.suzana@gmail.com>
2025-08-14 03:06:54 +0300

    Add Unsorted/VCard v2.6/VCard v2.6/vcard26/upload/admin/phpinfo.php

---
+++ Unsorted/VCard v2.6/VCard v2.6/vcard26/upload/admin/phpinfo.php
@@ -0,0 +1,16 @@
+<?php
+/***************************************************************************
+ *   script               : vCard PRO
+ *   copyright            : (C) 2001-2002 Belchior Foundry
+ *   website              : www.belchiorfoundry.com
+ *
+ *   This program is commercial software; you can´t redistribute it under
+ *   any circumstance without explicit authorization from Belchior Foundry.
+ *   http://www.belchiorfoundry.com/
+ *
+ ***************************************************************************/
+define('IN_VCARD', true);
+require('./lib.inc.php');
+phpinfo();
+
+?>
