6187bb7044f774d0be5e0cb6afaf1e6c9ce9157b
me-suzy <me.suzana@gmail.com>
2025-08-14 04:13:10 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/products_new.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/products_new.php
@@ -0,0 +1,24 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: products_new.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(products_new.php,v 1.3 2002/11/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (products_new.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE', 'New Products');
+define('HEADING_TITLE', 'New Products');
+
+define('TEXT_DATE_ADDED', 'Date Added:');
+define('TEXT_MANUFACTURER', 'Manufacturer:');
+define('TEXT_PRICE', 'Price:');
+?>
