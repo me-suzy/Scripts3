8870eac9f1e1a811a25a924af210c57c76444d71
me-suzy <me.suzana@gmail.com>
2025-08-14 03:08:33 +0300

    Add Unsorted/vCard v2.60 nullified/vcard26/upload/admin/config.inc.php

---
+++ Unsorted/vCard v2.60 nullified/vcard26/upload/admin/config.inc.php
@@ -0,0 +1,31 @@
+<?php
+//***************************************************************************//
+//                                                                           //
+//  Program Name    	: vCard PRO                                          //
+//  Program Version     : 2.6                                                //
+//  Program Author      : Joao Kikuchi,  Belchior Foundry                    //
+//  Home Page           : http://www.belchiorfoundry.com                     //
+//  Retail Price        : $80.00 United States Dollars                       //
+//  WebForum Price      : $00.00 Always 100% Free                            //
+//  Supplied by         : South [WTN]                                        //
+//  Nullified By        : CyKuH [WTN]                                        //
+//  Distribution        : via WebForum, ForumRU and associated file dumps    //
+//                                                                           //
+//                (C) Copyright 2001-2002 Belchior Foundry                   //
+//***************************************************************************//
+////////////////////////////////////////////////////////////////////////////////////
+// Database Variables:
+////////////////////////////////////////////////////////////////////////////////////
+$hostname	='localhost';		// database host
+$dbUser		='root';		// database username
+$dbPass		='password';		// database password
+$dbName		='vcard26'; 		// database name
+
+$use_admin_password	= 1;			// Do you want use password protected control panel? 1-Yes 0-No
+
+
+// You can set safeupload =1 if you have problem with upload feature. If you have safe mode = on in a webserver
+// with PHP4 or greater you steal can use upload feature, set safeupload=1. Remember it only work in PHP 4 or greater.
+$safeupload = 1;
+
+?>
