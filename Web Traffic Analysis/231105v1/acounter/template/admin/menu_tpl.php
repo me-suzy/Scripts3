10d7ac61f7a75eed095366213e9301025e79d450
me-suzy <me.suzana@gmail.com>
2025-08-14 04:55:44 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/admin/menu_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/admin/menu_tpl.php
@@ -0,0 +1,21 @@
+<?php die("Access restricted");?>
+<table width=160 border="0" cellspacing="1" cellpadding="0">
+  <tr>
+      <td><input type="hidden" name="voldaction" value="%%action%%"></td>
+  </tr>
+  <tr>
+      <td align=center>
+        <input type="image" border=0 name="aconfig" src="%%url%%images/admin/config.gif">
+      </td>
+  </tr>
+  <tr>
+      <td align=center>
+        <input type="image" border=0 name="ahtml" src="%%url%%images/admin/html.gif">
+      </td>
+  </tr>
+  <tr>
+      <td align=center>
+        <input type="image" border=0 name="aaccount" src="%%url%%images/admin/account.gif">
+      </td>
+  </tr>
+</table>
