3e9c4deebf4fc5af8878b1453fabd80adb5519c7
me-suzy <me.suzana@gmail.com>
2025-08-14 04:57:44 +0300

    Add Web Traffic Analysis/lite264/lite/admin.php

---
+++ Web Traffic Analysis/lite264/lite/admin.php
@@ -0,0 +1,99 @@
+<?php
+
+/*------------------------------------------------------------------------*/
+// Product: ActualAnalyzer
+// Script: admin.php
+// Source: http://www.actualscripts.com/
+// Copyright: (c) 2002-2004 ActualScripts, Company. All rights reserved.
+//
+// YOU DON'T NEED TO EDIT ANYTHING IN THIS SCRIPT.
+// SEE LICENSE AGREEMENT FOR MORE DETAILS
+/*------------------------------------------------------------------------*/
+
+//p3p header
+require './data/p3pheader.php';
+
+//root folder of Actualanalyzer
+$rf='./';
+
+require './common/error.php';
+require './common/global.php';
+require './common/config.php';
+require './common/confdb.php';
+require './common/auth.php';
+require './admin/adb.php';
+require './admin/aset.php';
+
+//HTML code accumulator
+$pagehtml='';
+
+//errors
+$err = & new error($rf);
+
+//config
+$conf = & new config($rf);
+if($err->flag) {
+  $err->reason('admin.php||constructor of config class has failed');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//database initialisation
+db_init();
+if($err->flag) {
+  $err->reason('admin.php||can\'t init database');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//config in database
+$configdb = & new confdb;
+if($err->flag) {
+  $err->reason('admin.php||constructor of confdb class has failed');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//authentication
+$login = & new auth($rf,'admin',_ADMINAREA);
+if($err->flag) {
+  $err->reason('admin.php||constructor of auth class has failed');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//database
+$adb = & new adb;
+if($err->flag) {
+  $err->reason('admin.php||constructor of adb class has failed');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//settings
+$aset = & new aset($rf);
+if($err->flag) {
+  $err->reason('admin.php||constructor of aset class has failed');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//database closing
+db_close();
+if($err->flag) {
+  $err->reason('admin.php||can\'t close connection with database');
+  $err->log_out();
+  $err->scr_out();
+  exit;
+}
+
+//output HTML page
+out();
+
+?>
