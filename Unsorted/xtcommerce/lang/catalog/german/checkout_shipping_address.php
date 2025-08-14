4886320d089d07ef3f84701335d79dc747f7aa82
me-suzy <me.suzana@gmail.com>
2025-08-14 04:14:52 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/checkout_shipping_address.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/checkout_shipping_address.php
@@ -0,0 +1,37 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: checkout_shipping_address.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(checkout_shipping_address.php,v 1.4 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (checkout_shipping_address.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+
+define('NAVBAR_TITLE_1', 'Kasse');
+define('NAVBAR_TITLE_2', 'Versandadresse &auml;ndern');
+
+define('HEADING_TITLE', 'Informationen zur Versandadresse');
+
+define('TABLE_HEADING_SHIPPING_ADDRESS', 'Versandadresse');
+define('TEXT_SELECTED_SHIPPING_DESTINATION', 'Dies ist die aktuell ausgew&auml;hlte Versandadresse, an die Ihre Bestellung geliefert wird.');
+define('TITLE_SHIPPING_ADDRESS', 'Versandadresse:');
+
+define('TABLE_HEADING_ADDRESS_BOOK_ENTRIES', 'Adressbucheintr&auml;ge');
+define('TEXT_SELECT_OTHER_SHIPPING_DESTINATION', 'Bitte w&auml;hlen Sie die gew&uuml;nschte Versandadresse, an die wir die Auslieferung vornehmen sollen.');
+define('TITLE_PLEASE_SELECT', 'Bitte w&auml;hlen Sie');
+
+define('TABLE_HEADING_NEW_SHIPPING_ADDRESS', 'Neue Versandadresse');
+define('TEXT_CREATE_NEW_SHIPPING_ADDRESS', 'Bitte nutzen Sie dieses Formular, um eine neue Versandadresse f&uuml;r Ihre Bestellung zu erfassen.');
+
+define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Fortsetzung des Bestellvorganges');
+define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'zur Auswahl der gew&uuml;nschten Versandart.');
+?>
