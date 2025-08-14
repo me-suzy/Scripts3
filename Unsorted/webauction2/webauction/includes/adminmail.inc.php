1c6ea04bf9941b597e57b1904db7b7c0dc7a8da9
me-suzy <me.suzana@gmail.com>
2025-08-14 03:22:54 +0300

    Add Unsorted/webauction2/webauction/includes/adminmail.inc.php

---
+++ Unsorted/webauction2/webauction/includes/adminmail.inc.php
@@ -0,0 +1,15 @@
+<?
+/*
+   Copyright (c), 1999, 2000 - phpauction.org
+   Copyright (c), 2001, 2002 - webauction.de.vu
+
+   Lizenz siehe lizenz.txt & license
+
+   Die neueste Version kostenfrei zum Download unter:
+   http://webauction.de.vu
+
+*/
+
+  $adminEmail = "test@test.de"; 
+  
+?>
