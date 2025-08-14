f25efd184db81fa1d45a7cb3bbb2807bd13c0b6d
me-suzy <me.suzana@gmail.com>
2025-08-14 03:17:02 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/msgbox_fr.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/msgbox_fr.lang.php
@@ -0,0 +1,56 @@
+0|Confirmation|
+1|Attention !|
+2|Interdit !|
+3|Erreur !|
+4|Sécurité !|
+5|<< Retour|
+6|OK|
+7|Une erreur de session est survenue !|
+8|Impossible d'accéder directement à cette page !<br> Merci de ne pas l'enregistrer dans vos favoris / signets.|
+9|Merci de ne pas tripoter les URLs !|
+10|Ce contact ne vous appartient pas.|
+11|Erreur dans les groupes sélectionnés|
+12|Merci de vous identifier !|
+13|Adresse email invalide|
+14|Nom obligatoire|
+15|Vous n'avez pas sélectionné de groupe|
+16|Vous n'avez rien sélectionné !|
+17|Le fichier n'a pas été ouvert !|
+18|Vous ne pouvez pas importer plusieurs fois le même champ|
+19|Vous n'avez pas sélectionné le champ 'Nom' qui est obligatoire|
+20|Le mot de passe et sa confirmation ne sont pas identiques|
+21|Les champs Login, Mot de passe et Email<br>sont obligatoires|
+22|Ce Login existe déjà<br><br>Merci d'en choisir un autre !|
+23|Les champs 'Objet' et 'Message' sont obligatoires|
+24|Vous ne pouvez pas envoyer d'email à ce Groupe|
+25|Vous devez sélectionner un groupe|
+26|Veuillez confirmer l'envoi du message ci dessous à tous les membres du groupe |
+27|Le message a correctement été envoyé aux membres du groupe !|
+28|Problème de connexion au serveur d'envoi de email (SMTP)<br><br> L'email n'a pas été envoyé !|
+29| ne convient pas dans<br>le champ 'nombre de contacts par page' !|
+30| ne convient pas dans<br>le champ 'Taille' de la police principale !|
+31| ne convient pas dans<br>le champ 'Taille' de la police secondaire !|
+32|Format de couleur incorrect !<br><br>Merci de respecter le format : '#XXXXXX'<br> où X est soit un chiffre soit une lettre de l'ensemble {A,B,C,D,E;F}<br><br>Exemple : #03A7F8"|
+33|Vous n'administrez pas ce groupe !|
+34|Vous n'avez pas le droit de modifier ce contact !|
+35|Vous n'avez pas le droit de supprimer cet utilisateur !|
+36|L'utilisateur a été détruit avec succès !|
+37|Vous devez remplir tous les champs !|
+38|Impossible de désaffecter / affecter cet utilisateur !|
+39|Erreur de calcul dans la table des couleurs !|
+40|Voulez-vous vraiment supprimer le contact |
+41|Le contact a été détruit avec succès !<br>|
+42|Mot de passe invalide !|
+43|Résultat : |
+44|Opération réussie|
+45|Les suppressions on été effectuées|
+46|Aucun contact ne correspond à votre sélection. <br><br>La base n'a pas été modifiée.|
+47|SUJET : |
+48|MESSAGE : |
+49|Annuler / Corriger|
+50|De : |
+51|Supprimer|
+52|Annuler|
+53|Vous devez choisir un séparateur !|
+54|<< Modifier|
+55|###### PLEASE DO NOT ERASE THIS LINE #####||
