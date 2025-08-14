d9fdf5e63711fb94033a33dce8edc86c8826e5d8
me-suzy <me.suzana@gmail.com>
2025-08-14 04:38:34 +0300

    Add User Management/Php-AGTC-IPbanV10a/inc.php

---
+++ User Management/Php-AGTC-IPbanV10a/inc.php
@@ -0,0 +1,15 @@
+<?php
+// *************************************************************************************************
+// Title: 		PHP AGTC-IP Ban v1.0a
+// Developed by: Andy Greenhalgh
+// Email:		andy@agtc.co.uk
+// Website:		agtc.co.uk
+// Copyright:	2005(C)Andy Greenhalgh - (AGTC)
+// Licence:		GPL, You may distribute this software under the terms of this General Public License
+// *************************************************************************************************
+//
+// YOU CAN CHANGE THE ADMIN PASSWORD BELOW
+$password = "admin";
+// PLEASE DO NOT REMOVE OR CHANGE THE COPYRIGHT BELOW
+$copyright = "Copyright &copy;2005 <a href='http://www.agtc.co.uk'> PHP AGTC-IP Ban v1.0a</a>";
+?>
