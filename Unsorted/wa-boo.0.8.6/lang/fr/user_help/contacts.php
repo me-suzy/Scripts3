ea8d47a43358c93f0c84624e8c07e0c2a8b447fc
me-suzy <me.suzana@gmail.com>
2025-08-14 03:17:19 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/user_help/contacts.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/user_help/contacts.php
@@ -0,0 +1,76 @@
+<?
+    include ("../../../includes/global_vars.php");
+    include ("../../../includes/fotools.php");
+
+    session_start();
+?>
+<html>
+    <head>
+    <title>wa-boo</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <style><? include ("../../../includes/css.php"); ?></style> 
+    </head>
+
+    <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" background="../../../images/bg1.gif">
+        <table border="0" cospan="0">
+            <tr>
+                <td>
+                    <div align="center" class="hlptitle">
+                        Gestion des Contacts
+                    </div>
+                    <br>
+                    <span class="hlpsubtitle">
+                        Type privé, groupe et public :
+                    </span>
+                    <span class="hlpfont">
+                        Les contacts sont les éléments de votre carnet d'adresse. Un contact est soit PRIVE soit GROUPE soit PUBLIC : <br>
+                         - Un contact de type PRIVE n'est visible que par son créateur (vous),
+                         <br>
+                         - un contact de type GROUPE est visible par les membres du ou des groupes choisis par son créateur. Sa modification est réservée à son créateur),
+                         <br>
+                         - un contact de type PUBLIC est visible par le monde entier.
+                    </span>
+                    <br>
+                    <br>
+                    <span class="hlpsubtitle">
+                        Création :
+                    </span>
+                   
+                    <span class="hlpfont">
+                        Pour créer un nouveau contact, choisissez le menu Nouveau Contact. Attention, le champ nom est obligatoire. Le type privé est choisi par défaut.  Cela signifie que par défaut, un nouveau contact est supposé privé.
+                    </span>
+                    <br>
+                    <br>
+                    <span class="hlpsubtitle">
+                        Modification :
+                    </span>
+                   
+                    <span class="hlpfont">
+                        Pour Modifier un contact, Sélectionnez l'icône de modification. Attention, vous ne pouvez modifié que les contacts que vous avez créés ou importés.
+                    </span>
+                    <br>
+                    <br>
+                    <span class="hlpsubtitle">
+                        Consultation :
+                    </span>
+                   
+                    <span class="hlpfont">
+                        Pour Consulter la fiche d'un contact, Sélectionnez l'icône de consultation. Vous afficherez alors toute la fiche du contact.
+                    </span>
+                    <br>
+                    <br>
+                    <span class="hlpsubtitle">
+                        Paramétrage :
+                    </span>
+                   
+                    <span class="hlpfont">
+                        La rubrique Préférences explique comment paramétrer les 4 champs principaux et les 6 champs supplémentaires visibles sur la page principale d'wa-boo
+                    </span>
+                    <br>
+                    <br>
+                </td>
+            </tr>
+        </table>
+        
+    </body>
+</html>
