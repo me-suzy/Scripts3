324d84756d3b2c1c472ebbcab917f8a8f1a0d382
me-suzy <me.suzana@gmail.com>
2025-08-14 03:10:38 +0300

    Add Unsorted/VirtuaNews.v1.03.Nullified/upload/admin/config.php

---
+++ Unsorted/VirtuaNews.v1.03.Nullified/upload/admin/config.php
@@ -0,0 +1,55 @@
+<?php
+
+/*======================================================================*\
+|| #################################################################### ||
+||  Program Name         : VirtuaNews Pro                                 
+||  Release Version      : 1.0.3                                          
+||  Program Author       : VirtuaSystems                                  
+||  Supplied by          : Ravish                                         
+||  Nullified by         : WTN Team                                       
+||  Distribution         : via WebForum, ForumRU and associated file dumps
+|| #################################################################### ||
+\*======================================================================*/
+
+// type of database running
+// (only mysql is supported at the moment)
+$dbservertype = "mysql";
+
+// hostname or ip of server
+$dbservername = "localhost";
+
+// username and password to log onto db server
+$dbusername = "root";
+$dbpassword = "";
+
+// name of database
+$dbname = "virtuanews";
+
+// technical email address
+$technicalemail = "your-name@your-host.com";
+
+// set the staff ids for the users which can prune the admin log
+// enter a single id, or a string of ids seperated by a , eg. "1,5,7"
+$canprunelog = "1";
+
+// 0 shows no debug info
+// 1 allows creation times to be viewed by adding showqueries=1 onto the query string,
+// also displays the time in the admin panel
+// 2 allows sql queries to be viewed also by adding showqueries=1 onto the query string
+// 3 will display all the sql queries at the bottom of each admin page
+$debug = 1;
+
+// If you have a problem having the directory /admin/ on your server then change this variable below
+// Please ensure you do not have a / as the first or that last character
+// You must also edit the file admin/toggle.js to replace admin/ with whatever you want
+// Also, you MUST edit global.php and admin.php and edit the line saying require("admin/config.php");
+// to point it to this file
+$admindirectory = "admin";
+
+/*======================================================================*\
+|| ####################################################################
+|| # File: admin/config.php
+|| ####################################################################
+\*======================================================================*/
+
+?>
