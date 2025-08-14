127003bbf551219dcdc0e403483da0bf7e4bb578
me-suzy <me.suzana@gmail.com>
2025-08-14 04:22:01 +0300

    Add Unsorted/X_Cart_3.0.1_Gold/xcart/provider/popup_product.php

---
+++ Unsorted/X_Cart_3.0.1_Gold/xcart/provider/popup_product.php
@@ -0,0 +1,60 @@
+<?
+/*****************************************************************************\
++-----------------------------------------------------------------------------+
+| X-Cart                                                                      |
+| Copyright (c) 2001-2002 RRF.ru development. All rights reserved.            |
++-----------------------------------------------------------------------------+
+| The RRF.RU DEVELOPMENT forbids, under any circumstances, the unauthorized   |
+| reproduction of software or use of illegally obtained software. Making      |
+| illegal copies of software is prohibited. Individuals who violate copyright |
+| law and software licensing agreements may be subject to criminal or civil   |
+| action by the owner of the copyright.                                       |
+|                                                                             |
+| 1. It is illegal to copy a software, and install that single program for    |
+| simultaneous use on multiple machines.                                      |
+|                                                                             |
+| 2. Unauthorized copies of software may not be used in any way. This applies |
+| even though you yourself may not have made the illegal copy.                |
+|                                                                             |
+| 3. Purchase of the appropriate number of copies of a software is necessary  |
+| for maintaining legal status.                                               |
+|                                                                             |
+| DISCLAIMER                                                                  |
+|                                                                             |
+| THIS SOFTWARE IS PROVIDED BY THE RRF.RU DEVELOPMENT TEAM ``AS IS'' AND ANY  |
+| EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED |
+| WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE      |
+| DISCLAIMED.  IN NO EVENT SHALL THE RRF.RU DEVELOPMENT TEAM OR ITS           |
+| CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,       |
+| EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,         |
+| PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; |
+| OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,    |
+| WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR     |
+| OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF      |
+| ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                  |
+|                                                                             |
+| The Initial Developer of the Original Code is RRF.ru development.           |
+| Portions created by RRF.ru development are Copyright (C) 2001-2002          |
+| RRF.ru development. All Rights Reserved.                                    |
++-----------------------------------------------------------------------------+
+\*****************************************************************************/
+
+#
+# $Id: popup_product.php,v 1.7 2002/04/25 12:41:03 zorg Exp $
+#
+
+require "../smarty.php";
+require "../config.php";
+require "./auth.php";
+
+require "../include/popup_product.php";
+require "../include/categories.php";
+
+$provider_condition=($single_mode?"":" and products.provider='$login'");
+
+$products = func_search_products ("products.categoryid='$cat' $provider_condition group by products.productid", $user_account['membership']);
+
+$smarty->assign ("products", $products);
+
+$smarty->display("main/popup_product.tpl");
+?>
