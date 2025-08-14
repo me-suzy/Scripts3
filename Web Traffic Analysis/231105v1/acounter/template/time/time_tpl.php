6c648db1dcf5f105c04c4b73e79397d8aea78764
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:38 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/time/time_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/time/time_tpl.php
@@ -0,0 +1,41 @@
+<?php die("Access restricted");?>
+<table border="0" cellspacing="0" cellpadding="0">
+    <td width=5></td>
+    <td valign=center>
+<table border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+      <select name="vtimeday">
+        %%dayitems%%
+      </select>
+    </td>
+  </tr>
+</table>
+    </td>
+    <td valign=center>
+<table border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+      <select name="vtimemonth">
+        %%monthitems%%
+      </select>
+    </td>
+  </tr>
+</table>
+    </td>
+    <td valign=center>
+<table border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td>
+      <select name="vtimeyear">
+        %%yearitems%%
+      </select>
+    </td>
+  </tr>
+</table>
+    </td>
+    <td>
+      <input class=b3 type="submit" name="vdate" value="Go">
+    </td>
+  </tr>
+</table>
