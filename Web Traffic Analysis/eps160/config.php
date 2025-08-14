822956dc4f80c85ff4b0d7ec724a536bd0090e79
me-suzy <me.suzana@gmail.com>
2025-08-14 04:57:29 +0300

    Add Web Traffic Analysis/eps160/config.php

---
+++ Web Traffic Analysis/eps160/config.php
@@ -0,0 +1,5 @@
+<?
+$path_absolute = "."; 					// path absolute of the website
+$path_img = "$path_absolute/img";		// directory for images
+$path_data = "$path_absolute/data";		// directory for data (chmod 666, or change it to mdb-database/data for windows)
+?>
