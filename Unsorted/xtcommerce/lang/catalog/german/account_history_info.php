0984917c27599e52176df1086cf7a18a288d168c
me-suzy <me.suzana@gmail.com>
2025-08-14 04:14:30 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/account_history_info.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/account_history_info.php
@@ -0,0 +1,48 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: account_history_info.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(account_history_info.php,v 1.19 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (account_history_info.php,v 1.4 2003/08/15); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Ihr Konto');
+define('NAVBAR_TITLE_2', 'Get&auml;tigte Bestellung');
+define('NAVBAR_TITLE_3', 'Bestellnummer %s');
+
+define('HEADING_TITLE', 'Bestellinformationen');
+
+define('HEADING_ORDER_NUMBER', 'Bestellnummer %s');
+define('HEADING_ORDER_DATE', 'Bestelldatum:');
+define('HEADING_ORDER_TOTAL', 'Summe:');
+
+define('HEADING_DELIVERY_ADDRESS', 'Lieferanschrift');
+define('HEADING_SHIPPING_METHOD', 'Versandart');
+
+define('HEADING_PRODUCTS', 'Produkte');
+define('HEADING_TAX', 'MwSt.');
+define('HEADING_TOTAL', 'Summe');
+
+define('HEADING_BILLING_INFORMATION', 'Rechnungsinformationen');
+define('HEADING_BILLING_ADDRESS', 'Rechnungsadresse');
+define('HEADING_PAYMENT_METHOD', 'Zahlungsweise');
+
+define('HEADING_ORDER_HISTORY', 'Bestellhistorie');
+define('HEADING_COMMENT', 'Kommentar');
+define('TEXT_NO_COMMENTS_AVAILABLE', 'Kein Kommentar vorhanden.');
+
+define('TABLE_HEADING_DOWNLOAD_DATE', 'herunterladen m&ouml;glich bis:');
+define('TABLE_HEADING_DOWNLOAD_COUNT', 'max. Anz. Downloads');
+define('HEADING_DOWNLOAD', 'Artikel herunterladen:');
+
+define('TEXT_ORDER_PRINT','<h3>Druckversion der Bestellung</h3><br>Zur Kontrolle können Sie sich die Bestellung ausdrucken.');
+?>
