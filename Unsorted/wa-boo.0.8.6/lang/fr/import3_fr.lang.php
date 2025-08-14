5bf9d38db7f47e5acdee270785b33898027b7a6b
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:54 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/import3_fr.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/import3_fr.lang.php
@@ -0,0 +1,16 @@
+0|Etape 3 : sélection des champs à importer|
+1|Votre fichier contient |
+2| lignes et |
+3| colonnes.|
+4|Afficher |
+5| lignes de prévisualisation.|
+6|Prévisualiser|
+7| La première ligne du fichier correspond aux nom des champs, ne pas l'importer|
+8| Montrer les champs tronqués comme le fera l'import |
+9| Attention, vous devez sélectionner au minimum le champ 'Nom'|
+10|Les contacts importés seront affectés à la catégorie  |
+11|<< Retour|
+12|Importer !|
+13|Ligne n°|
+14| -- NON IMPORTE -- |
+14|###### PLEASE DO NOT ERASE THIS LINE #####||
