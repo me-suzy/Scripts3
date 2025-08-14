b511fdd691824d8b4e2d535d617a9fb5aa15a8bf
me-suzy <me.suzana@gmail.com>
2025-08-14 04:14:47 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/checkout_payment_address.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/checkout_payment_address.php
@@ -0,0 +1,36 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: checkout_payment_address.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(checkout_payment_address.php,v 1.4 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (checkout_payment_address.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Kasse');
+define('NAVBAR_TITLE_2', 'Rechnungsadresse &auml;ndern');
+
+define('HEADING_TITLE', 'Informationen zur Rechnungsadresse');
+
+define('TABLE_HEADING_PAYMENT_ADDRESS', 'Rechnungsadresse');
+define('TEXT_SELECTED_PAYMENT_DESTINATION', 'Dies ist die aktuell ausgew&auml;hlte Rechnungsadresse, auf die die Rechnung ausgestellt wird.');
+define('TITLE_PAYMENT_ADDRESS', 'Rechnungsadresse:');
+
+define('TABLE_HEADING_ADDRESS_BOOK_ENTRIES', 'Adressbucheintr&auml;ge');
+define('TEXT_SELECT_OTHER_PAYMENT_DESTINATION', 'Bitte w&auml;hlen Sie die gew&uuml;nschte Rechnungsadresse, auf die wir die Rechnung ausstellen sollen.');
+define('TITLE_PLEASE_SELECT', 'Bitte w&auml;hlen Sie');
+
+define('TABLE_HEADING_NEW_PAYMENT_ADDRESS', 'Neue Rechnungsadresse');
+define('TEXT_CREATE_NEW_PAYMENT_ADDRESS', 'Bitte nutzen Sie dieses Formular, um eine neue Rechnungsadresse f&uuml;r Ihre Bestellung zu erfassen.');
+
+define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Fortsetzung des Bestellvorganges');
+define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'zur Auswahl der gew&uuml;nschten Zahlungsweise.');
+?>
