0bbbdda5fa11766354acb00b6b7ff51f451aab2c
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:37 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/user_properties_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/user_properties_en.lang.php
@@ -0,0 +1,34 @@
+0|Preferencies - |
+1|Categories|
+2|Contact list|
+3|#|
+4|Field # |
+5| ---- None ---- |
+6|Contacts over fields|
+7|Search|
+8|In search engine, select by default :|
+9|public contacts |
+10|groups contacts |
+11|private contacts |
+12|All|
+13|Search type '<b>begin with</b>' |
+14|Search type '<b>contents</b>' |
+15|Limit search to '<b>Name</b>' field |
+16|Search <b>all</b> fields |
+17|Miscellaneous|
+18|Language |
+19| Show <b>NAME FIELD</b> in uppercase |
+20|Confirm delete |
+21|Number of contact per page |
+22| (0 for no limit) |
+23|Colors and fonts|
+24|Menubar color |
+25|buttons color |
+26|Main font |
+27|Test |
+28|Size |
+29|Other font |
+30|OK |
+31|<< Cancel and Back|
+32| -- Default -- |
+33|###### PLEASE DO NOT ERASE THIS LINE #####||
