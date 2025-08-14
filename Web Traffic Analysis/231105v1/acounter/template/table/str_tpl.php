2065595b424f88d0e692222e84d53bdc310dc50e
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:32 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/table/str_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/table/str_tpl.php
@@ -0,0 +1,3 @@
+<?php die("Access restricted");?>
+<input type="text" class=box2 size=38 name=refname%%num%% value=%%val2%%>
+<input type="button" class=b33 value="Go" onClick="GoRef(this.form.refname%%num%%.value)">
