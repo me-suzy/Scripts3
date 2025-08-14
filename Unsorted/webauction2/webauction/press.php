4d317cccbfb32663bc89712764542878ba6852db
me-suzy <me.suzana@gmail.com>
2025-08-14 03:21:04 +0300

    Add Unsorted/webauction2/webauction/press.php

---
+++ Unsorted/webauction2/webauction/press.php
@@ -0,0 +1,47 @@
+<SCRIPT Language=PHP>
+
+
+/*
+   Copyright (c), 1999, 2000 - phpauction.org
+   Copyright (c), 2001, 2002 - webauction.de.vu
+
+   Lizenz siehe lizenz.txt & license
+
+   Die neueste Version kostenfrei zum Download unter:
+   http://webauction.de.vu
+
+*/
+
+
+	// Include messages file	
+   require('includes/messages.inc.php');
+  
+   // Connect to sql server & inizialize configuration variables
+   require('includes/config.inc.php');
+   
+
+	
+</SCRIPT>
+
+<HTML>
+<HEAD>
+<TITLE></TITLE>
+
+
+</HEAD>
+
+<BODY  BGCOLOR="#FFFFFF" TEXT="#000000" LINK="#000000" VLINK="#000000" ALINK="#000000">
+
+<SCRIPT Language=PHP>
+
+require("header.php");
+
+
+	print "<FONT FACE=\"Verdana,Helvetica,Arial\" SIZE=\"4\">";
+ 	print "<BR><CENTER><B>$MSG_184</B></CENTER>";
+
+</SCRIPT>
+
+<? require("./footer.php"); ?>
+</BODY>
+</HTML>
