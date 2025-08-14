8b59b31cdf692718fae3f147a90a2a199b102d4e
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:17 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/export1_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/export1_en.lang.php
@@ -0,0 +1,15 @@
+0|Export 1/3 : Contacts selection|
+1|This export process allows you to create a text fileyou'll be able to re-import in another software, PDA, etc.|
+2|file format will be text file with separator, <br>that means fields are delimited by a special char|
+3|Please choose now what to export and separator :|
+4|All my privates contacts |
+5| Contacts that match my last query|
+6|Means all contacts I created and / or imported |
+7| This option is potentially able to export all contacts <br>I have rights to se (publics, groups and privates)|
+8|Field separator |
+9| (for tab char, please enter letter ' t ')|
+10|Add fields names on first line |
+11| (usefull to reimport later in another application)|
+12|<< Quit Export|
+13|Explort !|
+14|###### PLEASE DO NOT ERASE THIS LINE #####||
