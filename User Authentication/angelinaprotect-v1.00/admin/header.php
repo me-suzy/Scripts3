a03c59f7d981cff33702a38d1cc02226599a8007
me-suzy <me.suzana@gmail.com>
2025-08-14 04:33:26 +0300

    Add User Authentication/angelinaprotect-v1.00/admin/header.php

---
+++ User Authentication/angelinaprotect-v1.00/admin/header.php
@@ -0,0 +1,23 @@
+<html> 
+<head> 
+<title>Main Admin Page</title> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
+<style type="text/css">
+<!--
+.style1 {
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-weight: bold;
+}
+-->
+</style>
+</head> 
+
+<body> 
+<div align="center" class="style1">
+<p><B><i><font size="+3">ANGELINAPROTECT ADMIN PANEL</font></I></B></p> 
+ 
+<p><a href="logout.php">Logout</a>
+<b>|</b>
+<a href="members.php">Members</a>
+<b>|</b>
+<a href="addmembers.php">Add new member</a> </center>
