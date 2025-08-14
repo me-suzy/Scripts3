002d96f1933389474349c0c0c7984c9f0f8221a3
me-suzy <me.suzana@gmail.com>
2025-08-14 03:33:46 +0300

    Add Unsorted/wgs-expire/SpyName 1.04/domains.php

---
+++ Unsorted/wgs-expire/SpyName 1.04/domains.php
@@ -0,0 +1,28 @@
+<?/*
+dbhost=mysql1.bam2tech.com
+dbname=webguy_domain
+dbuser=webguy
+dbpassword=wgs125
+admlogin=admin
+admpasswd=admin
+admconfirm=admin
+restore_subject=Your password from webguystudios.com
+restore_message=Hello @login@		Your password is "@passwd@"
+monitor_subject=Monitor
+monitor_message=The domain, that you have selected to monitor is available for registration.	domain name: @domain_name@
+welcome_subject=Welcome to webguystudios.com
+welcome_message=Hello @login@		Welcome to webguystudios.com service	You may go to members page right now	http://www.SpyName.com/member
+cron_interval=10
+cron_path=member/monitor.php
+cron_running=true
+mail_from=contact@webguystudios.com
+paypal_email=your@email.com
+paypal_notify=http://yoursite.com/domains/ipn.php
+paypal_return=http://yoursite.com/domains/member
+poweredby=http://www.webguystudios.com
+poweredby_text=WGS-Expire
+poweredby_url=http://www.webguystudios.com
+bugtrack_url=http://www.webguystudios.com
+21232f297a57a5a743894a0e4a801fc3=29e7aa0b20a4f9df3ed22e8f14c8b98b
+sClient=8a7c655e5fff4a7d2ee7e5802cf1ba04
+*/?>
