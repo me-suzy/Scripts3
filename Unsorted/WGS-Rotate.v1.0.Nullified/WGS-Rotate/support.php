87c109d85c080deb3254b55454db17b389ac48ab
me-suzy <me.suzana@gmail.com>
2025-08-14 03:41:18 +0300

    Add Unsorted/WGS-Rotate.v1.0.Nullified/WGS-Rotate/support.php

---
+++ Unsorted/WGS-Rotate.v1.0.Nullified/WGS-Rotate/support.php
@@ -0,0 +1,14 @@
+<?
+
+include_once("config.inc.php");
+print $design_top;
+
+?>
+<h3>Support</h3>
+
+Please contact us with any questions you may have! our e-mail is: contact@yourdomain.com
+<?
+
+print $design_bottom;
+
+?>
