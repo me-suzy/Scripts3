2da8ec18afb965bacd64b0922245aca6b4e1180e
me-suzy <me.suzana@gmail.com>
2025-08-14 03:32:10 +0300

    Add Unsorted/WGS-eDate.v2.5.Nullified/WGS-eDate/settings.php

---
+++ Unsorted/WGS-eDate.v2.5.Nullified/WGS-eDate/settings.php
@@ -0,0 +1,12 @@
+<?php
+$website["password"]="PASSWORD";
+
+$ROOT_HOST = "http://yourdomain.com";
+
+$db_host = "localhost";
+$db_name = "DATABASE_NAME";
+$db_login ="USERNAME";
+$db_pswd = "PASSWORD";
+
+$ADMIN_PAYPAL="contact@yourdomain.com";
+?>
