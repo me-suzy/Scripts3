7b7f052c2017371dfbbbe30dcb6361d46b482a73
me-suzy <me.suzana@gmail.com>
2025-08-14 05:09:36 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/net/Socket.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/net/Socket.class.php
@@ -0,0 +1,38 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: Socket.class.php,v 1.5 2005/10/16 20:01:04 slizardo Exp $
+ * @package poop.net
+ */
+
+class Socket {
+
+	/**
+	 * @param string $machine
+	 * @param int $portNumber
+	 */
+	public function __construct($machine, $portNumber) {
+	}
+
+	/**
+	 * Connects this socket to the server.
+	 */
+	public function connect() {
+	}
+
+	/**
+	 * Returns the closed state of the socket.
+	 */
+	public function isClosed() {
+	}
+}
+	
+?>
