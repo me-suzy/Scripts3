419ff55ec47fd637a3abb30ea607e41d680d3d47
me-suzy <me.suzana@gmail.com>
2025-08-14 04:48:51 +0300

    Add Web Hosting Tools/phpDNA/config.php

---
+++ Web Hosting Tools/phpDNA/config.php
@@ -0,0 +1,7 @@
+<?PHP
+// Upload the files to your server and place them into a folder called whois
+// Place the path to get to the whois folder
+// For Example: http://www.yoursite.com/
+// Do not include the whois folder in the path
+$path="http://www.yoursite.com/";
+?>
