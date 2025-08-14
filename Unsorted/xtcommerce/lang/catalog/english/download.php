0e6677cfb344e523b0406d418ba00caaf9ad9b5f
me-suzy <me.suzana@gmail.com>
2025-08-14 04:13:00 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/download.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/download.php
@@ -0,0 +1,18 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: download.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce 
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(download.php,v 1.2 2002/11/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (download.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+?>
