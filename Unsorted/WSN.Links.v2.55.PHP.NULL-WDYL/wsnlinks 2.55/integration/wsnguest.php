4cd7a028f2aae22283faddea7159efb5182f822e
me-suzy <me.suzana@gmail.com>
2025-08-14 03:53:11 +0300

    Add Unsorted/WSN.Links.v2.55.PHP.NULL-WDYL/wsnlinks 2.55/integration/wsnguest.php

---
+++ Unsorted/WSN.Links.v2.55.PHP.NULL-WDYL/wsnlinks 2.55/integration/wsnguest.php
@@ -0,0 +1,17 @@
+<?php
+// WSN member integration hack for WSN Links
+// coded by Paul Knierim, copyright 2003
+
+$memberstable = 'wsnguest_members';
+
+$otherencoder = 'no'; // use md5 by default. if other, write it up in encoder.php
+
+$admingroup = '3';
+// Note: group listed above should be the administrative usergroup. If not, change the line above.
+// Be sure you do not use the wrong group id, or members may be able to use your admin panel.
+
+$idcookiename = 'wsnuser'; 
+$passwordcookiename = 'wsnpass';
+// fill in the name of the cookie that holds the member id and password, if possible.
+
+?>
