20d33542ddfa9daa479f8a977a2999c25777ecaa
me-suzy <me.suzana@gmail.com>
2025-08-14 03:27:49 +0300

    Add Unsorted/webdate.v1.3.nulled.kactus.man2003.wst/webdate.v1.3.nulled.kactus.man2003.wst/webdatev1-3_nulled_kactus_man2003_[WST]/webdatev1-3_blue/show_photo.php

---
+++ Unsorted/webdate.v1.3.nulled.kactus.man2003.wst/webdate.v1.3.nulled.kactus.man2003.wst/webdatev1-3_nulled_kactus_man2003_[WST]/webdatev1-3_blue/show_photo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Picture</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<div align="center"> 
+  <p><img src="<? echo $image ?>"></p>
+  <p><a href="javascript:window.close();"><font size="2" face="Arial, Helvetica, sans-serif">close 
+    this window</font></a></p>
+</div>
+</body>
+</html>
