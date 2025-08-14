cd3331a9819bd77f5d60f4af90b6f97af9c920b1
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:07 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/diagram/vzc_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/diagram/vzc_tpl.php
@@ -0,0 +1,9 @@
+<?php die("Access restricted");?>
+    <td align=center vAlign=bottom>
+<input type="hidden" name="vdcol%%num%%" value="%%inttim%%">
+<table width="30" border="0" cellspacing="0" cellpadding="0">
+  <tr> 
+    <td align=center><img border=0 width="30" src="%%url%%images/diagram/vertical/%%color%%zero.gif" onclick = "col_select(%%num%%);"></td>
+  </tr>
+</table>
+    </td>  
