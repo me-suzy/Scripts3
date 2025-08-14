67795ad93df948662e24b476a78f3a8b6fda3ddd
me-suzy <me.suzana@gmail.com>
2025-08-14 04:55:53 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/diagram/hdrf_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/diagram/hdrf_tpl.php
@@ -0,0 +1,3 @@
+<?php die("Access restricted");?>
+<input class=box1 type="text" size=34 name=refname%%num%% value=%%ref%%>
+<input class=b33 type="button" value="Go" onClick="GoRef(this.form.refname%%num%%.value)"> 
