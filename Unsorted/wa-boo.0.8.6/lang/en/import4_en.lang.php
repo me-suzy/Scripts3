2c689f7fd31990cdb7e1ec5b735683ddbf28ddcd
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:27 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/import4_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/import4_en.lang.php
@@ -0,0 +1,25 @@
+0|Import 4/5 : Duplicates management|
+1|You file contents |
+2| duplicate(s) of your current wa-boo|
+3|In duplicates, you must understand that severa contacts in <br>current wa-boo also exist in file you working on to import.|
+4|Duplicates are found if, without [UPPERCASE ou lowercase] consideration :|
+5|- Couple |
+6| Name - Firstname |
+7| exists BOTH in wa-boo and in the file to be imported|
+8| Name - Email |
+9|Example : wa-boo will detact duplicates in following cases :|
+10|Julia Smith exists in file and Julia Smith still exists in wa-boo|
+11|Eleanor Martin's email is emartin@freee.com in file while still exists <br>a contact named Martin in wa-boo with same email address emartin@freee.com.|
+12|Now, please choose : you can replace an existing contact in base <br>by new entry found in file, or keep actual base contact unchanged.|
+13|Plesa select BELOW the contact that will|
+14| be visible |
+15|in wa-boo after import completion.|
+16|File's contacts|
+17|Database's contacts|
+18|All|
+19|Import with my selections|
+20|<< Back|
+21|Import without duplicate consideration|
+22|Warning !!|
+23|Yous database state can't allow wa-boo to help you to sort <br>duplicatesto keep or to replace. To make this possible, your base should'nt content duplicates <br>before running the import process. if you want't to clean tou base, please do it now.|
+14|###### PLEASE DO NOT ERASE THIS LINE #####||
