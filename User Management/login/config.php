61c5a221922ff54445774bc337e4e5582e2df04a
me-suzy <me.suzana@gmail.com>
2025-08-14 04:37:52 +0300

    Add User Management/login/config.php

---
+++ User Management/login/config.php
@@ -0,0 +1,8 @@
+<?php
+$hostname = "localhost";	/* Hostname (usually localhost) */
+$user = "username";		/* Username for database */
+$pass = "password";		/* Password for database */
+$database = "dbname";		/* Database name */
+$domain = "yourdomain.com";	/* Do NOT enter www. or http:// */
+$directory = "/login/";		/* this is the directory location of the script ie.. /login/ be sure to add a slash at the beginning and end */
+?>
