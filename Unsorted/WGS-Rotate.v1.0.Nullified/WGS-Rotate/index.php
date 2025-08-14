ad76c56b7572ebbc64f3e35db013fd452572ec36
me-suzy <me.suzana@gmail.com>
2025-08-14 03:41:08 +0300

    Add Unsorted/WGS-Rotate.v1.0.Nullified/WGS-Rotate/index.php

---
+++ Unsorted/WGS-Rotate.v1.0.Nullified/WGS-Rotate/index.php
@@ -0,0 +1,25 @@
+<?
+
+include_once("config.inc.php");
+print $design_top;
+
+?>
+     <strong><u>Promote all your websites at once with Your Rotator!</u></strong><br />
+
+     With Your Rotator you can promote all your websites and referral URLs from
+     one simple URL.  Its simple, every time someone visits your Your Rotator URL
+     they will be directed to one of your websites.  When they come back they will
+     see a different website!  And unlike other rotators, we do not limit the
+     amount of URLs that you can rotate.<br /><br />
+     <strong><font color="#FF0000">You can use an UNLIMITED amount of URLs!  The best part is that it is FREE!</font></strong><br />
+
+     We also offer a Pro account.  For the low price of $1.00 a month you can get the advertisement removed from your rotator.  You will still have all the features of the Free version but you will not have the small advertisement above your website when the rotator page loads.<br /><br />
+
+     What are you waiting for?  Sign up today!<br />
+
+     <center><a href="signup.php">Sign Up</a></center>
+
+
+<?
+print $design_bottom;
+?>
