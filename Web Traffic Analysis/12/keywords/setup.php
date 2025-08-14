294129a3b079caccf19e0576f4f33069f32d321e
me-suzy <me.suzana@gmail.com>
2025-08-14 04:54:24 +0300

    Add Web Traffic Analysis/12/keywords/setup.php

---
+++ Web Traffic Analysis/12/keywords/setup.php
@@ -0,0 +1,15 @@
+<?php
+// $my_host	= Mysql host address
+$my_host = "localhost";
+// $my_user = Mysql username
+$my_user = "root";
+// $my_pass = Mysql password
+$my_pass = "root";
+// $my_db = Mysql database name
+$my_db = "hitinfo";
+
+// $username = Username for administration login
+// $password = Password for administration login
+$username = "Admin";
+$password = "qwerty";
+?>
