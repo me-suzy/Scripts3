4faffa51f3acc254191209e9faf2d6fca1e873c4
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:37 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/table/sums_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/table/sums_tpl.php
@@ -0,0 +1,19 @@
+<?php die("Access restricted");?>
+  <tr bgcolor="#CCCCCC">
+    <td height="25" width="240" align="center">
+        <input type="hidden" name="vdcol%%num%%" value="%%inttim%%">
+        <input class=b2 type="submit" name="vdcolumn%%num%%_x" value="%%vals2%%">
+    </td>
+    <td align="center"><font size=3 color="#000000">
+      <SPAN class="%%class%%">%%vals3%%</SPAN> 
+    </font></td>
+    <td align="center"><font size=3 color="#000000">
+      <SPAN class="%%class%%">%%vals4%%</SPAN> 
+    </font></td>
+    <td align="center"><font size=3 color="#000000">
+      <SPAN class="%%class%%">%%vals5%%</SPAN> 
+    </font></td>
+    <td align="center"><font size=3 color="#000000">
+      <SPAN class="%%class%%">%%vals6%%</SPAN> 
+    </font></td>
+  </tr>
