f72cb19fd9c7a5941f3260cd27e3d19b11da975a
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:12 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/cleaning_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/cleaning_en.lang.php
@@ -0,0 +1,18 @@
+0|Delete contacts set|
+1|You can delete contacts set for maintenance needs.<br>All the contacts you can delete here were previously created / imported by yourself.<br>|
+2|The procedure being irreversible, it is advised beforehand to carry out an export of your contacts |
+3|before |
+4|running contacts set delete|
+5|Please select which contacts set you want to delete, then type your password and click OK.|
+6|Warning !! |
+7|No other confirmation will be required|
+8|Delete :|
+9|All my public contacts |
+10|All my private contacts |
+11| contacts of category |
+12|All|
+13|All contacts displayable by groups |
+14|Yous password (required) |
+15|<< Back|
+16|Delete selected contacts !|
+17|###### PLEASE DO NOT ERASE THIS LINE #####||
