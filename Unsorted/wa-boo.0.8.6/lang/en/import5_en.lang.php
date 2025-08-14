3501d4ecb150bfe9ca2d47af40053c9832b1f58f
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:29 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/import5_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/import5_en.lang.php
@@ -0,0 +1,8 @@
+0|Import 5/5 : Import result|
+1|Import is finished|
+2|You have imported |
+3| contacts.|
+4|During this process, you replaced |
+5| existing contacts.|
+6|OK|
+7|###### PLEASE DO NOT ERASE THIS LINE #####||
