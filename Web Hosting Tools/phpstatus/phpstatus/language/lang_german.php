ad4bd5b4861c9100083de0f0fa710ae3e7fda9f2
me-suzy <me.suzana@gmail.com>
2025-08-14 04:50:30 +0300

    Add Web Hosting Tools/phpstatus/phpstatus/language/lang_german.php

---
+++ Web Hosting Tools/phpstatus/phpstatus/language/lang_german.php
@@ -0,0 +1,119 @@
+<?php
+/****************************************************************/
+/*                         phpStatus                            */
+/*                    lang_german.php file                      */
+/*                      (c)copyright 2005                       */
+/*                       By host49.de                           */
+/*                                                              */
+/*            original File lang_english.php from               */
+/*                 http://www.hintondesign.org                  */
+/*                  support@hintondesign.org                    */
+/*                                                              */
+/* This program is free software. You can redistrabute it and/or*/
+/* modify it under the terms of the GNU General Public Licence  */
+/* as published by the Free Software Foundation; either version */
+/* 2 of the license.                                            */
+/*                                                              */
+/****************************************************************/
+$lang['navigation'] = "Hauptmenü";
+$lang['error'] = "FEHLER";
+$lang['success'] = "ERFOLGT";
+$lang['error_mess1'] = "Benutzername / Passwort fehlt!";
+$lang['error_mess2'] = "Login nicht erfolgt, Benutzernamen und Passwort überprüfen!";
+$lang['error_mess3'] = "Port fehlt!";
+$lang['error_mess4'] = "Server fehlt!";
+$lang['error_mess5'] = "Unzureichende Zugriffsrechte!";
+$lang['error_mess6'] = "Login nicht erfolgt!";
+$lang['error_mess7'] = "Gruppe fehlt!";
+$lang['error_mess8'] = "Gruppe bereits vorhanden!";
+$lang['error_mess9'] = "Gruppe konnte nicht hinzugefügt werden!";
+$lang['error_mess10'] = "Gruppe konnte nicht bearbeitet werden!";
+$lang['error_mess11'] = "Gruppe konnte nicht gelöscht werden!";
+$lang['error_mess12'] = "Alle Felder müssen ausgefüllt werden!";
+$lang['error_mess13'] = "Diesen Fehler korregieren!";
+$lang['error_mess14'] = "Port bereits vorhanden!";
+$lang['error_mess15'] = "Portname bereits vergeben!";
+$lang['error_mess16'] = "Service konnte nicht hinzugefügt!";
+$lang['error_mess17'] = "Service konnte nicht bearbeiet werden!";
+$lang['error_mess18'] = "Service konnte nicht gelöscht werden!";
+$lang['error_mess19'] = "Servername bereits vorhanden!";
+$lang['error_mess20'] = "Server-IP bereits vorhanden!";
+$lang['error_mess21'] = "Server konnte nicht hinzugefügt werden!";
+$lang['error_mess22'] = "Server konnte nicht bearbeitet werden!";
+$lang['error_mess23'] = "Server konnte nicht gelöscht werden!";
+$lang['error_mess24'] = "Einstellungen konnten nicht bearbeitet werden!";
+$lang['error_mess25'] = "Name der Spachdatei fehlt!";
+$lang['error_mess26'] = "Sprachdatei bereits vorhanden!.";
+$lang['error_mess27'] = "Sprachdatei konnte nicht hinzugefügt werden!";
+$lang['error_mess28'] = "Sprachdatei konnte nicht bearbeitet werden!";
+$lang['error_mess29'] = "Sprachdatei konnte nicht gelöscht werden!";
+$lang['error_mess30'] = "Templatename fehlt!";
+$lang['error_mess31'] = "Template bereits vorhanden!";
+$lang['error_mess32'] = "Template konnte nicht hinzugefügt werden!";
+$lang['error_mess33'] = "Template konnte nicht bearbeitet werden!";
+$lang['error_mess34'] = "Template konnte nicht gelöscht werden!";
+$lang['success_mess1'] = "Gruppe wurde hinzugefügt!";
+$lang['success_mess2'] = "Gruppe wurde bearbeitet!";
+$lang['success_mess3'] = "Gruppe wurde gelöscht!";
+$lang['success_mess4'] = "Service wurde hinzugefügt!";
+$lang['success_mess5'] = "Service wurde bearbeitet!";
+$lang['success_mess6'] = "Service wurde gelöscht!";
+$lang['success_mess7'] = "Server wurde hinzugefügt!";
+$lang['success_mess8'] = "Server wurde bearbeitet!";
+$lang['success_mess9'] = "Server wurde gelöscht!";
+$lang['success_mess10'] = "Einstellungen wurden bearbeitet!";
+$lang['success_mess11'] = "Sprachdatei wurde hinzugefügt";
+$lang['success_mess12'] = "Sprachdatei wurde bearbeitet";
+$lang['success_mess13'] = "Sprachdatei wurde gelöscht!";
+$lang['success_mess14'] = "Template wurde hinzugefügt!";
+$lang['success_mess15'] = "Template wurde beabreitet!";
+$lang['success_mess16'] = "Template wurde gelöscht!";
+$lang['success_mess17'] = "Logout erfolgreich!";
+$lang['server'] = "Server";
+$lang['online'] = "Online";
+$lang['offline'] = "Offline";
+$lang['login'] = "Login";
+$lang['username'] = "Benutzername";
+$lang['password'] = "Passwort";
+$lang['admin'] = "Administration";
+$lang['groups'] = "Neu / Edit Gruppe";
+$lang['ports'] = "Neu / Edit Service";
+$lang['servers'] = "Neu / Edit Server";
+$lang['groups'] = "Gruppen";
+$lang['add_group'] = "Neue Gruppe";
+$lang['group_name'] = "Gruppe - Name";
+$lang['group_id'] = "Gruppe - ID";
+$lang['action'] = "Aktion";
+$lang['services'] = "Service";
+$lang['edit_group'] = "Gruppe - bearbeiten";
+$lang['port_id'] = "Port - ID";
+$lang['port_name'] = "Port - Name";
+$lang['add_service'] = "Neuer Service";
+$lang['port_number'] = "Port Nummer";
+$lang['edit_service'] = "Edit Service";
+$lang['servers'] = "Server";
+$lang['server_id'] = "Server - ID";
+$lang['server_ip'] = "Server - IP";
+$lang['server_name'] = "Server - Name";
+$lang['add_server'] = "Neuer Server";
+$lang['group'] = "Gruppe";
+$lang['edit_server'] = "Server - bearbeiten";
+$lang['settings'] = "Einstellungen";
+$lang['site_title'] = "Titel d. Seite";
+$lang['domain'] = "Domain";
+$lang['script_path'] = "Script Pfad";
+$lang['default_lang'] = "Sprache - Standart";
+$lang['default_theme'] = "Templte - Standart";
+$lang['languages'] = "Sprachen";
+$lang['add_lang'] = "Neue Sprache";
+$lang['lang_id'] = "Sprache - ID";
+$lang['lang_name'] = "Sprache - Name";
+$lang['edit_lang'] = "Sprache - bearbeiten";
+$lang['themes'] = "Templates";
+$lang['add_theme'] = "Neues Template";
+$lang['theme_id'] = "Template - ID";
+$lang['theme_name'] = "Template - Name";
+$lang['edit_theme'] = "Template - bearbeiten";
+$lang['logout'] = "Logout";
+$lang['logout_mess'] = "<center>Logout erfolgreich! Neuer Login - </center><br><a href=\"logout.php?logmeout\">Ja</a> | <a href=\"javascript:history.back()\">Nein</a></center>";
+?>
