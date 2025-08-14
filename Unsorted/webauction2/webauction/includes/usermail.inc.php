9919d5349cecf7e56fd6a3e03c9615f878e3ec37
me-suzy <me.suzana@gmail.com>
2025-08-14 03:24:22 +0300

    Add Unsorted/webauction2/webauction/includes/usermail.inc.php

---
+++ Unsorted/webauction2/webauction/includes/usermail.inc.php
@@ -0,0 +1,45 @@
+#
+# --Confirmation e-mail file
+# 
+# 			This file contains the message your customers
+# 			will receive as a confirmation after the registration
+#			process.
+#			Lines starting with # will be skipped.
+#			Blank lines will be maintained.
+#
+#			Change the message below as needed considering the 
+#			following tags to reflect your customer's personal data:
+#
+#        --------------------------------------------------------
+#			TAG SYNTAX				EFFECT
+#        --------------------------------------------------------
+#
+#			<#c_id#>					customer ID
+#			<#c_name#>				name
+#			<#c_nick#>				nick
+#			<#c_address#> 			address
+#			<#c_city#> 			   city
+#			<#c_prov#>  			province
+#			<#c_country#> 			country
+#			<#c_zip#> 				zip
+#			<#c_phone#> 			phone number
+#			<#c_email#> 			e-mail address
+#
+#        --------------------------------------------------------
+#
+#			USAGE:
+#			Insert the above tags in the text of your message			
+#			where you want each value to appear.			
+#			Mofidy the message to reflect your needs.
+#
+# 
+# 
+#  
+
+Sehr geehrte/r <#c_name#>,
+
+Vielen Dank, für Ihre Anmeldung bei <#c_sitename#>.
+
+Alle Daten können Sie unter <#c_siteurl#> einsehen und gegebenenfalls ändern.
+
+<#c_sitename#>
