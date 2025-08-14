b3df4b2a3bf046dca14ef571acd48bc45523f471
me-suzy <me.suzana@gmail.com>
2025-08-14 05:07:41 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/lang/Math.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/lang/Math.class.php
@@ -0,0 +1,95 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: Math.class.php,v 1.5 2005/10/12 21:00:42 slizardo Exp $
+ * @package poop.lang
+ */
+
+final class Math {
+	const E = M_E;
+	const PI = M_PI;
+	
+	/**
+	 * @return boolean
+	 */
+	public static function abs($a) {
+		return abs($a);
+	}
+	
+	public static function acos($a) {
+		return acos($a);
+	}
+
+	public static function asin($a) {
+		return asin($a);
+	}
+
+	public static function atan($a) {
+		return atan($a);
+	}	
+
+	public static function atan2($x, $y) {
+	}
+
+	public static function ceil($a) {
+		return ceil($a);
+	}
+
+	public static function cos($a) {
+	}
+
+	public static function exp($a) {
+	}
+
+	public static function floor($a) {
+	}
+
+	public static function IEEEreminder($f1, $f2) {
+	}
+
+	public static function log($a) {
+	}
+
+	public static function max($a, $b) {
+	}
+
+	public static function min($a, $b) {
+	}
+
+	public static function pow($a, $b) {
+	}
+
+	public static function random() {
+	}
+
+	public static function rint($a) {
+	}
+
+	public static function round($a) {
+	}
+
+	public static function sin($a) {
+	}
+
+	public static function sqrt($a) {
+	}
+
+	public static function tan($a) {
+	}
+
+	public static function toDegrees($a) {
+	}
+
+	public static function toRadians($a) {
+	}
+}
+	
+?>
