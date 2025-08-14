16f882404f8977b58c7ac66c8ad66d518b3c0f1a
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:09 +0300

    Add Unsorted/wa-boo.0.8.6/lang/admin_help_frameset.php

---
+++ Unsorted/wa-boo.0.8.6/lang/admin_help_frameset.php
@@ -0,0 +1,23 @@
+<?
+    include ("../includes/global_vars.php");
+    include ("../includes/fotools.php");
+
+    session_start();
+
+?>
+<html>
+<head>
+<title>:::: wa-boo ADMIN HELP ::::: </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <style><? include ("../includes/css.php"); ?></style> 
+</head>
+
+<frameset cols="150,*" frameborder="NO" border="0" framespacing="0">
+  <frame name="leftFrame" scrolling="NO" noresize src="<? echo $s_lang ?>/admin_help/menu.php">
+  <frame name="mainFrame" src="<? echo $s_lang ?>/admin_help/welcome.php">
+  
+</frameset>
+<noframes><body bgcolor="#FFFFFF" text="#000000">
+
+</body></noframes>
+</html>
