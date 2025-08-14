2379ce1feb8a5d539b3139f93a013efd57671b61
me-suzy <me.suzana@gmail.com>
2025-08-14 03:51:26 +0300

    Add Unsorted/WSN.Links.v2.55.PHP.NULL-WDYL/wsnlinks 2.55/scriptinfo.php

---
+++ Unsorted/WSN.Links.v2.55.PHP.NULL-WDYL/wsnlinks 2.55/scriptinfo.php
@@ -0,0 +1,11 @@
+<?php
+$version = 'WSN Links 2.55';
+$fullscripttitle = 'WSN Links';
+$scriptname = 'wsnlinks';
+$preftype = 'LINKS';
+$pref = '{LINK';
+$nameforlinks = 'link';
+$plurallinks = 'links';
+$memberpref = '{MEMBER';
+$categorypref = '{CAT';
+?>
