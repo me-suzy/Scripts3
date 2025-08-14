336a77a0b9d930d23fa607143743dcabf5f4bd14
me-suzy <me.suzana@gmail.com>
2025-08-14 03:23:49 +0300

    Add Unsorted/webauction2/webauction/includes/mail_endauction_nowinner.inc.php

---
+++ Unsorted/webauction2/webauction/includes/mail_endauction_nowinner.inc.php
@@ -0,0 +1,64 @@
+#
+# --Send 'no winner' annoucement to seller
+#
+#                         This file contains the message your customers
+#                         will receive when someone sends them an auction.
+#                        Lines starting with # will be skipped.
+#                        Blank lines will be maintained.
+#
+#                        Change the message below as needed considering the
+#                        following tags to reflect your customer's personal data:
+#
+#        --------------------------------------------------------
+#                        TAG SYNTAX                                EFFECT
+#        --------------------------------------------------------
+#
+#                        <#s_name#>              Seller Name
+#                        <#s_nick#>              Seller Nickname
+#                        <#s_email#>             Seller email
+#                        <#s_address#>           Seller Address
+#                        <#s_city#>              Seller City
+#                        <#s_prov#>              Seller State/Province
+#                        <#s_country#>           Seller Country
+#                        <#s_zip#>               Seller Zip Code
+#                        <#s_phone#>             Seller Phone
+#                        <#i_title#>             auction item title
+#                        <#i_description#>       auction item description
+#                        <#i_url#>               URL to view auction
+#                        <#i_ends#>              Auction End date/time
+#           <#c_sitename#>          Auction Site Name
+#           <#c_siteurl#>           main URL of auction site
+#           <#c_adminemail#>        email address of Auction site webmaster
+#        --------------------------------------------------------
+#
+#                        USAGE:
+#                        Insert the above tags in the text of your message
+#                        where you want each value to appear.
+#                        Modify the message to reflect your needs.
+#                        Change [...] with to your correct data.
+#
+#
+#
+
+Liebe/r <#s_name#>,
+
+Eine Auktion die Sie auf <#c_sitename#> erstellt haben ist ohne Zuschlag beendet.
+
+Auktions Daten
+------------------------------------
+Titel: <#i_title#>
+Auktion End Datum: <#i_ends#>
+URL: <#i_url#>
+
+Sie können diese Auktion jederzeit in Ihrem Userbereich auf <#c_sitename#> 
+neustarten.
+
+Weiterhin viel Erfolg.
+
+<#c_siteurl#>
+
+
+
+
+
+
