d61d2879849b7d1643f47b617ecae0eaccda4d43
me-suzy <me.suzana@gmail.com>
2025-08-14 03:16:35 +0300

    Add Unsorted/wa-boo.0.8.6/lang/en/user_en.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/en/user_en.lang.php
@@ -0,0 +1,22 @@
+0|Account of |
+1|Your account |
+2|Firstname |
+3|Name |
+4|Login |
+5|Password |
+6|Password (confirm) |
+7|E-mail |
+8|Update changes|
+9|Your groups |
+10|Administrators : |
+11|Show group's members |
+12|Members : |
+13|Send an email to selected group members|
+14|NOT USED|
+15|Show group's Members|
+16|From : |
+17|Subject |
+18|Message |
+19|send email|
+20|Back|
+21|###### PLEASE DO NOT ERASE THIS LINE #####||
