0ab98f17175e644bce5a0df9d1afb0d2ea475a88
me-suzy <me.suzana@gmail.com>
2025-08-14 04:12:43 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/checkout_payment.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/checkout_payment.php
@@ -0,0 +1,40 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: checkout_payment.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce 
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(checkout_payment.php,v 1.14 2003/02/06); www.oscommerce.com 
+   (c) 2003	 nextcommerce (checkout_payment.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'Checkout');
+define('NAVBAR_TITLE_2', 'Payment Method');
+
+define('HEADING_TITLE', 'Payment Information');
+
+define('TABLE_HEADING_BILLING_ADDRESS', 'Billing Address');
+define('TEXT_SELECTED_BILLING_DESTINATION', 'Please choose from your address book where you would like the invoice to be sent to.');
+define('TITLE_BILLING_ADDRESS', 'Billing Address:');
+
+define('TABLE_HEADING_PAYMENT_METHOD', 'Payment Method');
+define('TEXT_SELECT_PAYMENT_METHOD', 'Please select the preferred payment method to use on this order.');
+define('TITLE_PLEASE_SELECT', 'Please Select');
+define('TEXT_ENTER_PAYMENT_INFORMATION', 'This is currently the only payment method available to use on this order.');
+
+define('TABLE_HEADING_COMMENTS', 'Add Comments About Your Order');
+
+define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Continue Checkout Procedure');
+define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'to confirm this order.');
+
+define('HEADING_CONDITIONS_INFORMATION', 'Conditions of Use');
+define('TEXT_CONDITIONS_CONFIRM', 'I accept your conditions of use');
+define('TEXT_CONDITIONS_DOWNLOAD', 'Download Conditions');
+?>
