246ee47065d1c667dc97ff9344ee130fc9123ba1
me-suzy <me.suzana@gmail.com>
2025-08-14 05:14:49 +0300

    Add XML and PHP/molins-0_10_7/sample/WEB-INF/classes/sample/NewsGroup.class.php

---
+++ XML and PHP/molins-0_10_7/sample/WEB-INF/classes/sample/NewsGroup.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * To contact the author write to {@link mailto:santiago.lizardo@gmail.com slizardo}
+ * The latest version of Molins can be obtained from:
+ * {@link http://www.phpize.com/}
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: NewsGroup.class.php,v 1.4 2005/09/28 16:28:21 slizardo Exp $
+ * @package sample
+ */
+
+import('poop.dbobjects.DbGroup');
+
+class NewsGroup extends DbGroup { }
+
+?>
