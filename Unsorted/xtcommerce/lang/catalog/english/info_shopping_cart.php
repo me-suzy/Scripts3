b0aa025d01f882e0ab2b1f1182156e10fc643128
me-suzy <me.suzana@gmail.com>
2025-08-14 04:13:03 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/info_shopping_cart.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/info_shopping_cart.php
@@ -0,0 +1,26 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: info_shopping_cart.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(info_shopping_cart.php,v 1.4 2002/11/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (info_shopping_cart.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Visitors Cart / Members Cart');
+define('SUB_HEADING_TITLE_1', 'Visitors Cart');
+define('SUB_HEADING_TITLE_2', 'Members Cart');
+define('SUB_HEADING_TITLE_3', 'Info');
+define('SUB_HEADING_TEXT_1', 'Every visitor to our online shop will be given a \'Visitors Cart\'. This allows the visitor to store their products in a temporary shopping cart. Once the visitor leaves the online shop, so will the contents of their shopping cart.');
+define('SUB_HEADING_TEXT_2', 'Every member to our online shop that logs in is given a \'Members Cart\'. This allows the member to add products to their shopping cart, and come back at a later date to finalize their checkout. All products remain in their shopping cart until the member has  checked them out, or removed the products themselves.');
+define('SUB_HEADING_TEXT_3', 'If a member adds products to their \'Visitors Cart\' and decides to log in to the online shop to use their \'Members Cart\', the contents of their \'Visitors Cart\' will merge with their \'Members Cart\' contents automatically.');
+define('TEXT_CLOSE_WINDOW', '[ close window ]');
+?>
