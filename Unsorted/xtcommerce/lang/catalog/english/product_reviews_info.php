0bd2a3009aa78cafcc21a0a89fd1c777e0c67c02
me-suzy <me.suzana@gmail.com>
2025-08-14 04:13:16 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/product_reviews_info.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/product_reviews_info.php
@@ -0,0 +1,31 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: product_reviews_info.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+                 _  __ _____ _____                                            
+      ____  ___ | |/ /_  __/ ____/___  ____ ___  ____ ___  ___  _____________ 
+     / __ \/ _ \|   / / / / /   / __ \/ __ `__ \/ __ `__ \/ _ \/ ___/ ___/ _ \
+    / / / /  __/   | / / / /___/ /_/ / / / / / / / / / / /  __/ /  / /__/  __/
+   /_/ /_/\___/_/|_|/_/  \____/\____/_/ /_/ /_/_/ /_/ /_/\___/_/   \___/\___/ 
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(product_reviews_info.php,v 1.6 2002/11/19); www.oscommerce.com 
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE', 'Reviews');
+define('HEADING_TITLE', ' %s Reviews');
+define('SUB_TITLE_PRODUCT', 'Product:');
+define('SUB_TITLE_FROM', 'From:');
+define('SUB_TITLE_DATE', 'Date:');
+define('SUB_TITLE_REVIEW', 'Review:');
+define('SUB_TITLE_RATING', 'Rating:');
+define('TEXT_OF_5_STARS', '%s of 5 Stars!');
+define('TEXT_CLICK_TO_ENLARGE', 'Click to enlarge');
+?>
