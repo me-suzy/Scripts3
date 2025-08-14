dd14fa7426efb05b8ed2786b06d5bc64a0af5ab9
me-suzy <me.suzana@gmail.com>
2025-08-14 05:06:00 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/dbobjects/util/BasePeer.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/dbobjects/util/BasePeer.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: BasePeer.class.php,v 1.2 2005/10/11 15:57:30 slizardo Exp $
+ * @package poop.dbobjects.util
+ */
+	
+abstract class BasePeer {
+
+	public static function doInsert() {
+	}
+
+	public static function doUpdate() {
+	}
+
+	public static function doDelete() {
+	}
+
+	public static function doSelect() {
+	}
+}
+
+?>
