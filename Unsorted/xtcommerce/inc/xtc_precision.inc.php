f068c4f60c9d39d3daa0a31ad234f80225861c47
me-suzy <me.suzana@gmail.com>
2025-08-14 04:04:32 +0300

    Add Unsorted/xtcommerce/inc/xtc_precision.inc.php

---
+++ Unsorted/xtcommerce/inc/xtc_precision.inc.php
@@ -0,0 +1,22 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: xtc_precision.inc.php,v 1.1 2003/09/06 21:47:50 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   by Mario Zanier for XTcommerce
+   
+   based on:
+   (c) 2003	 nextcommerce (xtc_precision.inc.php,v 1.5 2003/08/19); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+   
+function xtc_precision($number,$places)
+	{
+	return $number;
+	}
+ ?>
