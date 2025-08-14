1cac6db2b232035c0fcbbea2b57a11a53920f05e
me-suzy <me.suzana@gmail.com>
2025-08-14 05:11:04 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/text/NumberFormat.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/text/NumberFormat.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: NumberFormat.class.php,v 1.5 2005/10/12 21:01:02 slizardo Exp $
+ * @package poop.text
+ */
+ 
+import('poop.text.Format');
+
+class NumberFormat extends Format {
+}
+
+?>
