b048848343e3a92d3789e5261117cbe9118d0eb2
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:22 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/table/lts_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/table/lts_tpl.php
@@ -0,0 +1,18 @@
+<?php die("Access restricted");?>
+  <tr bgcolor="#CCCCCC">
+    <td class=f8 height="25" align="center"><font size=2 color="#000000">
+        <SPAN class="%%class%%">%%num%%</SPAN> 
+    </font></td>
+    <td class=f8 align="center"><font size=2 color="#000000">
+        <SPAN class="%%class%%">%%val1%%</SPAN> 
+    </font></td>
+    <td class=f8 align="left"><font size=2 color="#000000">
+        <SPAN class="%%class%%">%%val4%%</SPAN> 
+    </font></td>
+    <td width="140" class=f8 align="left"><font size=2 color="#000000">
+        <SPAN class="%%class%%">%%val2%%</SPAN> 
+    </font></td>
+    <td width="60" class=f8 align="center"><font size=2 color="#000000">
+        <SPAN class="%%class%%">%%val3%%</SPAN> 
+    </font></td>
+  </tr>
