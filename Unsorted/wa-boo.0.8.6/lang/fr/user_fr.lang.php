adf3a360881d744fcede53fe64659f912e373601
me-suzy <me.suzana@gmail.com>
2025-08-14 03:17:04 +0300

    Add Unsorted/wa-boo.0.8.6/lang/fr/user_fr.lang.php

---
+++ Unsorted/wa-boo.0.8.6/lang/fr/user_fr.lang.php
@@ -0,0 +1,22 @@
+0|Compte de |
+1|Votre compte |
+2|Prénom |
+3|Nom |
+4|Login |
+5|Mot de passe |
+6|Mot de passe (confirmation) |
+7|E-mail |
+8|Enregistrer les modifications|
+9|Vos groupes |
+10|Administrateurs : |
+11|Afficher les membres du groupe |
+12|Membres : |
+13|Envoyer un email aux Membres du groupe sélectionné|
+14|NON UTILISE|
+15|Afficher les membres du groupe|
+16|De : |
+17|Objet |
+18|Message |
+19|Envoyer l'email|
+20|Retour|
+21|###### PLEASE DO NOT ERASE THIS LINE #####||
