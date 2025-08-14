f87ff6be071532f949ed2e18fec9309ed2b1aa71
me-suzy <me.suzana@gmail.com>
2025-08-14 04:13:11 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/product_info.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/product_info.php
@@ -0,0 +1,28 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: product_info.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(product_info.php,v 1.15 2002/11/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (product_info.php,v 1.7 2003/08/24); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+define('TEXT_PRODUCT_PRINT', 'Print Datasheet:');
+define('TEXT_PRODUCT_NOT_FOUND', 'Product not found!');
+define('TEXT_CURRENT_REVIEWS', 'Current Reviews:');
+define('TEXT_MORE_INFORMATION', 'For more information, please visit this products <a href="%s" target="_blank"><u>webpage</u></a>.');
+define('TEXT_DATE_ADDED', 'This product was added to our catalog on %s.');
+define('TEXT_DATE_AVAILABLE', '<font color="#ff0000">This product will be in stock on %s.</font>');
+define('TEXT_ALSO_PURCHASED_PRODUCTS', 'Customers who bought this product also purchased');
+define('TEXT_PRODUCT_OPTIONS', 'Available Options:');
+define('TEXT_CLICK_TO_ENLARGE', 'Click to enlarge');
+define('TEXT_XSELL_PRODUCTS','We Also Recommend');
+define('TITLE_MEDIA_CONTENT','Media Data:')
+?>
