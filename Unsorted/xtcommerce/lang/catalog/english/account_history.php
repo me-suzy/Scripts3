dde5087300eef3a1a3e5b3f54cc33ab96c3b2d33
me-suzy <me.suzana@gmail.com>
2025-08-14 04:12:27 +0300

    Add Unsorted/xtcommerce/lang/catalog/english/account_history.php

---
+++ Unsorted/xtcommerce/lang/catalog/english/account_history.php
@@ -0,0 +1,33 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: account_history.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(account_history.php,v 1.7 2003/05/19); www.oscommerce.com 
+   (c) 2003	 nextcommerce (account_history.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('NAVBAR_TITLE_1', 'My Account');
+define('NAVBAR_TITLE_2', 'History');
+
+define('HEADING_TITLE', 'My Order History');
+
+define('TEXT_ORDER_NUMBER', 'Order Number:');
+define('TEXT_ORDER_STATUS', 'Order Status:');
+define('TEXT_ORDER_DATE', 'Order Date:');
+define('TEXT_ORDER_SHIPPED_TO', 'Shipped To:');
+define('TEXT_ORDER_BILLED_TO', 'Billed To:');
+define('TEXT_ORDER_PRODUCTS', 'Products:');
+define('TEXT_ORDER_COST', 'Order Cost:');
+define('TEXT_VIEW_ORDER', 'View Order');
+
+define('TEXT_NO_PURCHASES', 'You have not yet made any purchases.');
+?>
