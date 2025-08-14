0cd083417e8e49e115a0e33fe3aa59d435544791
me-suzy <me.suzana@gmail.com>
2025-08-14 04:56:42 +0300

    Add Web Traffic Analysis/231105v1/acounter/template/type/vtyp_tpl.php

---
+++ Web Traffic Analysis/231105v1/acounter/template/type/vtyp_tpl.php
@@ -0,0 +1,8 @@
+<?php die("Access restricted");?>
+<table border="0" cellspacing="0" cellpadding="0">
+  <tr>
+      <td><input type="hidden" name="voldtype" value="%%type%%"></td>
+      <td><input type="image" border=0 name="vdiagram" src="%%url%%images/type/diagram.gif"></td>
+      <td><input type="image" border=0 name="vstattable" src="%%url%%images/type/table.gif"></td>
+  </tr>
+</table>
