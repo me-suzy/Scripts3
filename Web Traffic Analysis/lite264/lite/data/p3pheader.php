c78823e373cab5919864afbef67dd7132ef8d34f
me-suzy <me.suzana@gmail.com>
2025-08-14 04:58:54 +0300

    Add Web Traffic Analysis/lite264/lite/data/p3pheader.php

---
+++ Web Traffic Analysis/lite264/lite/data/p3pheader.php
@@ -0,0 +1,10 @@
+<?php
+
+/*----------------------------------------------------------------------------*/
+// Headers for Platform for Privacy Preferences (P3P)
+// See more about P3P project at http://www.w3.org/P3P/
+/*----------------------------------------------------------------------------*/
+
+Header('p3p: policyref="/w3c/p3p.xml", CP="NOI CURa ADMa DEVa TAIa OUR BUS IND UNI COM NAV INT"');
+
+?>
