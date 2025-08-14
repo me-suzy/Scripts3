bd89a4ba738183d1878dbdb5ad8e3a45875a999a
me-suzy <me.suzana@gmail.com>
2025-08-14 03:10:11 +0300

    Add Unsorted/VirtuaNews.v1.03.Nullified/upload/modules.php

---
+++ Unsorted/VirtuaNews.v1.03.Nullified/upload/modules.php
@@ -0,0 +1,44 @@
+<?php
+
+/*======================================================================*\
+|| #################################################################### ||
+||  Program Name         : VirtuaNews Pro                                 
+||  Release Version      : 1.0.3                                          
+||  Program Author       : VirtuaSystems                                  
+||  Supplied by          : Ravish                                         
+||  Nullified by         : WTN Team                                       
+||  Distribution         : via WebForum, ForumRU and associated file dumps
+|| #################################################################### ||
+\*======================================================================*/
+
+require("global.php");
+
+settype($modid,"integer");
+
+if ($modid) {
+  $sqlwhere = "id = $modid";
+} else {
+  $sqlwhere = "name = '$modname'";
+}
+
+if ($data_arr = query_first("SELECT id,name,enable,description,options FROM news_module WHERE $sqlwhere")) {
+  if ($data_arr[enable]) {
+    $modid = $data_arr[id];
+    $modname = $data_arr[name];
+    $moddescription = $data_arr[description];
+    $modoptions = getmodoptions($modid,"",$data_arr[options]);
+    include("modules/$data_arr[name]/index.php");
+  } else {
+    standarderror("module_disabled");
+  }
+} else {
+  standarderror("invalid_id");
+}
+
+/*======================================================================*\
+|| ####################################################################
+|| # File: modules.php
+|| ####################################################################
+\*======================================================================*/
+
+?>
