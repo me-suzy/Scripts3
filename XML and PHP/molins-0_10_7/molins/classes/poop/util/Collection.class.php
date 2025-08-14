0239659f897eb93b86563b7305d91a7c6eab39ef
me-suzy <me.suzana@gmail.com>
2025-08-14 05:11:20 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/util/Collection.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/util/Collection.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: Collection.class.php,v 1.5 2005/10/12 21:01:03 slizardo Exp $
+ * @package poop.util
+ */
+
+import('poop.lang.Iterable');
+ 
+interface Collection extends Iterable {
+}
+	
+?>
