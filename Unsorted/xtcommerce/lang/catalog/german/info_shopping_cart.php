4b08e95002f7ee4f38b4f68defa29ed2c5eb4ca9
me-suzy <me.suzana@gmail.com>
2025-08-14 04:15:06 +0300

    Add Unsorted/xtcommerce/lang/catalog/german/info_shopping_cart.php

---
+++ Unsorted/xtcommerce/lang/catalog/german/info_shopping_cart.php
@@ -0,0 +1,26 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: info_shopping_cart.php,v 1.1 2003/09/06 21:54:34 fanta2k Exp $   
+
+   XT-Commerce - community made shopping
+   http://www.xt-commerce.com
+
+   Copyright (c) 2003 XT-Commerce
+   -----------------------------------------------------------------------------------------
+   based on: 
+   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+   (c) 2002-2003 osCommerce(info_shopping_cart.php,v 1.6 2002/04/17); www.oscommerce.com 
+   (c) 2003	 nextcommerce (info_shopping_cart.php,v 1.3 2003/08/13); www.nextcommerce.org
+
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+define('HEADING_TITLE', 'Besucherwarenkorb / Kundenwarenkorb');
+define('SUB_HEADING_TITLE_1', 'Besucherwarenkorb');
+define('SUB_HEADING_TITLE_2', 'Kundenwarenkorb');
+define('SUB_HEADING_TITLE_3', 'Information');
+define('SUB_HEADING_TEXT_1', 'Jeder Besucher unseres Online-Shops bekommt einen \'Besucherwarenkorb\'. Damit kann er seine ausgew&auml;hlten Produkte sammeln. Sobald der Besucher den Online-Shop verl&auml;sst, verf&auml;llt dessen Inhalt.');
+define('SUB_HEADING_TEXT_2', 'Jeder angemeldete Kunde verf&uuml;gt &uuml;ber einen \'Kundenwarenkorb\' zum Einkaufen, mit dem er auch zu einem sp&auml;terem Zeitpunkt den Einkauf beenden kann. Jeder Artikel bleibt darin registriert bis der Kunde zur Kasse geht, oder die Produkte darin l&ouml;scht.');
+define('SUB_HEADING_TEXT_3', 'Die Besuchereingaben werden automatisch bei der Registrierung als Kunde in den Kundenwarenkorb &uuml;bernommen.');
+define('TEXT_CLOSE_WINDOW', '<b><u>[Fenster schliessen]</b></u>');
+?>
