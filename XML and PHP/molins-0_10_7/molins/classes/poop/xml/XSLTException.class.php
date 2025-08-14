a6c80d169fc81cd20a3a0e1fc92856b649efeb20
me-suzy <me.suzana@gmail.com>
2025-08-14 05:12:11 +0300

    Add XML and PHP/molins-0_10_7/molins/classes/poop/xml/XSLTException.class.php

---
+++ XML and PHP/molins-0_10_7/molins/classes/poop/xml/XSLTException.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/** 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL License.
+ *
+ * Copyright(c) 2005 by Santiago Lizardo Oscares. All rights reserved.
+ *
+ * The latest version of Molins can be obtained from <http://www.phpize.com>.
+ *
+ * @author slizardo <santiago.lizardo@gmail.com>
+ * @version $Id: XSLTException.class.php,v 1.4 2005/10/13 13:42:26 slizardo Exp $
+ * @package poop.xml
+ */
+
+class XSLTException extends Exception {
+}
+
+?>
